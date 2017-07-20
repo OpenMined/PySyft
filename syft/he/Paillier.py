60b36f0c9dcb5cedfb9c45e8bd080291f06bf77f
Andrew Trask <liamtrask@gmail.com>
2017-07-20 16:13:14 -0400

    created PaillierInteger but haven't hooked it up yet

--- syft/he/Paillier.py
+++ syft/he/Paillier.py
@@ -1,6 +1,68 @@
-from syft.he.interface import HEAlgo
+import phe as paillier
+
+class SecretKey():
+
+    def __init__(self,sk):
+        self.sk = sk
+
+    def decrypt(self,x):
+        return self.sk.decrypt(x)
+
+class PublicKey():
+
+    def __init__(self,pk):
+        self.pk = pk
+
+    def encrypt(self,x):
+        return self.pk.encrypt(x)
 
 class KeyPair():
 
     def __init__(self):
         ""
+
+    def generate(self):
+        pubkey, prikey = paillier.generate_paillier_keypair(n_length=2048*2)
+        self.public_key = PublicKey(pubkey)
+        self.secret_key = SecretKey(prikey)
+
+        return (self.public_key,self.secret_key)
+
+class PaillierInteger():
+
+    def __init__(self,public_key,data=None):
+
+        self.public_key = public_key
+        self.data = data
+
+    def __add__(self,y):
+        out = FVInteger(self.public_key,self.data + y.data)
+        return out
+
+    def __sub__(self,y):
+        out = FVInteger(self.public_key, self.data - y.data)
+        return out
+
+    def __mul__(self,y):
+
+        if(type(y) == type(self)):
+            return FVInteger(self.public_key, self.data * y.data)
+        elif(type(y) == int):
+            return FVInteger(self.public_key, self.data * y)
+        else:
+            return None
+
+    def __truediv__(self,y):
+
+        if(type(y) == type(self)):
+            return FVInteger(self.public_key, self.data / y.data)
+        elif(type(y) == int):
+            return FVInteger(self.public_key, self.data / y)
+        else:
+            return None
+
+    def __repr__(self):
+        return 'e'
+
+    def __str__(self):
+        return 'e'
