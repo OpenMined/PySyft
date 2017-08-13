1dda1ca9c2fcdbdf2bba9f4572c2b115836ce145
Samson <samsontmr@users.noreply.github.com>
2017-08-13 23:29:43 +0800

    Implement matmul() + unit tests

--- syft/tensor.py
+++ syft/tensor.py
@@ -24,6 +24,25 @@ def dot(tensor1, tensor2):
     return TensorBase(np.vdot(tensor1.data, tensor2.data))
 
 
+def matmul(tensor1, tensor2):
+    """Performs matrix multiplication between two tensors.
+
+    Exact behavior depends on the input tensors' dimensionality like so:
+    * If both tensors are 1-dimensional, their dot product is returned.
+    * If both tensors are 2-dimensional, their matrix-matrix product is returned.
+    * If either tensor has dimensionality > 2, the last 2 dimensions are treated as matrices and multiplied.
+    * If tensor1 is 1-dimensional, it is converted to a matrix by prepending a 1 to its dimensions. This prepended dimension is removed after the matrix multiplication.
+    * If tensor2 is 1-dimensional, it is converted to a matrix by prepending a 1 to its dimensions. This prepended dimension is removed after the matrix multiplication.
+    """
+    if tensor1.encrypted is True or tensor2.encrypted is True:
+        return NotImplemented
+
+    if tensor1.dim() == 1 and tensor2.dim() == 1:
+        return dot(tensor1, tensor2)
+    else:
+        return TensorBase(np.matmul(tensor1.data, tensor2.data))
+
+
 class TensorBase(object):
     """
     A base tensor class that perform basic element-wise operation such as
@@ -76,6 +95,11 @@ class TensorBase(object):
             return NotImplemented
 
         return syft.dot(self, tensor)
+
+    def __matmul__(self, arr_like):
+        """Performs matrix multiplication between two tensors"""
+        return syft.matmul(self, arr_like)
+
 
     def __mul__(self, arr_like):
         """Performs element-wise multiplication between two array like objects"""
--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -94,6 +94,46 @@ class DotTests(unittest.TestCase):
         self.assertTrue(syft.dot(t1, t2), 43.07)
 
 
+class MatmulTests(unittest.TestCase):
+    def testMatmul1DInt(self):
+        t1 = TensorBase(np.array([1, 2, 3]))
+        t2 = TensorBase(np.array([4, 5, 6]))
+        self.assertTrue(syft.matmul(t1, t2), syft.dot(t1, t2))
+
+
+    def testMatmul1DFloat(self):
+        t1 = TensorBase(np.array([1.3, 2.5, 3.7]))
+        t2 = TensorBase(np.array([4.9, 5.8, 6.5]))
+        self.assertTrue(syft.matmul(t1, t2), syft.dot(t1, t2))
+
+
+    def testMatmul2DIdentity(self):
+        t1 = TensorBase(np.array([[1, 0],
+                                 [0, 1]]))
+        t2 = TensorBase(np.array([[5.8, 6.5],
+                                 [7.8, 8.9]]))
+        self.assertTrue(syft.matmul(t1, t2), [[5.8, 6.5],
+                                              [7.8, 8.9]])
+
+
+    def testMatmul2DInt(self):
+        t1 = TensorBase(np.array([[1.3, 2.5],
+                                 [3.4, 4.5]]))
+        t2 = TensorBase(np.array([[5.8, 6.5],
+                                 [7.8, 8.9]]))
+        self.assertTrue(syft.matmul(t1, t2), [[19, 22],
+                                              [43, 50]])
+
+
+    def testMatmul2DFloat(self):
+        t1 = TensorBase(np.array([[1.3, 2.5],
+                                 [3.4, 4.5]]))
+        t2 = TensorBase(np.array([[5.8, 6.5],
+                                 [7.8, 8.9]]))
+        self.assertTrue(syft.matmul(t1, t2), [[27.04, 8.45],
+                                              [54.82, 22.1]])
+
+
 def main():
     unittest.main()
 
