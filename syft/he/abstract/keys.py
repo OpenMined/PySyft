5f3e08bb62bfbefb61de758d48c67a2fbce4db91
Andrew Trask <liamtrask@gmail.com>
2017-08-19 18:01:39 +0100

    pause for now

---
+++ syft/he/abstract/keys.py
@@ -0,0 +1,37 @@
+import phe as paillier
+import numpy as np
+import pickle
+from .basic import Integer
+
+class AbstractSecretKey():
+
+    def __init__(self,sk):
+        return NotImplemented
+
+    def decrypt(self,x):
+        return NotImplemented
+
+    def serialize(self):
+        return NotImplemented
+
+class AbstractPublicKey():
+
+    def __init__(self,pk):
+        return NotImplemented
+
+    def encrypt(self,x):
+        return NotImplemented
+
+    def serialize(self):
+        return NotImplemented
+
+class AbstractKeyPair():
+
+    def __init__(self):
+        ""
+
+    def deserialize(self,pubkey,seckey):
+        return NotImplemented
+
+    def generate(self,n_length=1024):
+        return NotImplemented
