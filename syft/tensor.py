edbf67f888b3eae8450680e31a12da071da8fc60
sajalsubodh22 <sajal.subodh10297@gmail.com>
2017-08-24 16:28:14 +0530

    Error Fix

--- syft/tensor.py
+++ syft/tensor.py
@@ -470,9 +470,3 @@ class TensorBase(object):
         if self.encrypted:
             return NotImplemented
         self.data = 1 / np.sqrt(self.data)
-
-    def to_numpy(self):
-        """Returns the tensor as numpy.ndarray"""
-        if self.encrypted:
-            return NotImplemented
-        return np.array(self.data)
