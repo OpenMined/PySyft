28127470e7b84caad19bea4c0b8cf51f41a7e46d
Edward Dixon <edward.dixon@intel.com>
2017-09-03 00:26:46 +0100

    Added max method to tensorbase

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -86,6 +86,19 @@ class SubTests(unittest.TestCase):
         self.assertTrue(syft.equal(t - 1, [0, 1, 2]))
 
 
+class MaxTests(unittest.TestCase):
+    def testNoDim(self):
+        t = TensorBase(np.array([[ 0.77937768,  0.51022484,  0.49155195,  0.02769902],[ 0.03777148,  0.13020167,  0.02155692,  0.69574893]]))
+        self.assertTrue(t.max() ==  0.77937768)
+
+    def testDim(self):
+        t = TensorBase(np.array([[0.77937768, 0.51022484, 0.49155195, 0.02769902], [0.03777148, 0.13020167, 0.02155692, 0.69574893]]))
+        result = t.max(dim = 1)
+        self.assertTrue(syft.equal(result, [0.77937768, 0.69574893]))
+        result = t.max(dim=0)
+        self.assertTrue(syft.equal(result, [0.77937768, 0.51022484, 0.49155195, 0.69574893]))
+        
+
 class MultTests(unittest.TestCase):
     def testSimple(self):
         t = TensorBase(np.array([1, 2, 3]))
