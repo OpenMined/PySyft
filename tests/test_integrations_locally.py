f05544080880f5f2293f29642dd122f42a9a0e8b
Andrew Trask <liamtrask@gmail.com>
2017-09-12 17:53:13 +0100

    fixed style issues

--- tests/test_integrations_locally.py
+++ tests/test_integrations_locally.py
@@ -1,18 +1,17 @@
 import unittest
 
-import numpy as np
 from syft.he.paillier import KeyPair
 from syft.nn.linear import LinearClassifier
 
-import syft
-from syft import TensorBase
 
-
-class PySonarTests(unittest.TestCase):
+class PySonarNotebooks(unittest.TestCase):
     def hydrogeDemoNotebook(self):
+        """If this method fails, you probably broke the demo notebook located at
+        PySonar/notebooks/Sonar - Decentralized Model Training Simulation
+        (local blockchain).ipynb """
 
-        pubkey,prikey = KeyPair().generate(n_length=1024)
-        diabetes_classifier = LinearClassifier(desc="DiabetesClassifier",n_inputs=10,n_labels=1)
-        diabetes_classifier.encrypt(pubkey)
+        pubkey, prikey = KeyPair().generate(n_length=1024)
+        d = LinearClassifier(desc="DiabetesClassifier", n_inputs=10, n_labels=1)
+        d.encrypt(pubkey)
 
         self.assertTrue(True)
