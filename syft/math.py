c087b202bc3dd4415c48c204a0577f816f0229a4
Gautam <gautamrbharadwaj@gmail.com>
2017-09-21 19:30:14 +0530

    Update math.py
    adding relu function

--- syft/math.py
+++ syft/math.py
@@ -170,6 +170,14 @@ def tanh(tensor):
         return NotImplemented
     return TensorBase(np.tanh(np.array(tensor.data)))
 
+def relu(tensor):
+        """ Returns a new tensor holding element wise values of relu function
+    """
+    tensor = _ensure_tensorbase(tensor)
+    if tensor.encrypted is True:
+        return NotImplemented
+    return TensorBase(np.maximum(tensor.data,0,tensor.data))
+
 
 def addmm(tensor1, tensor2, mat, beta=1, alpha=1):
     """Performs ((Mat*Beta)+((Tensor1.Tensor2)*Alpha)) and  returns the
