4066df6789bed5b0020dbbdaace053c14df9301f
Gautam <gautamrbharadwaj@gmail.com>
2017-09-26 10:57:23 +0530

    relu6

--- syft/math.py
+++ syft/math.py
@@ -174,7 +174,7 @@ def relu(tensor):
     tensor = _ensure_tensorbase(tensor)
     if tensor.encrypted is True:
         return NotImplemented
-    return Tensorbase(np.maximum(0,tensor.data))
+    return TensorBase(np.maximum(0,tensor.data))
 
 def addmm(tensor1, tensor2, mat, beta=1, alpha=1):
     """Performs ((Mat*Beta)+((Tensor1.Tensor2)*Alpha)) and  returns the
