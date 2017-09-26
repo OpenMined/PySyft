fixes broadcasting bug

syft/tensor.py
@@ -1299,6 +1299,6 @@ def masked_select(tensor, mask):
     tensor = _ensure_tensorbase(tensor)
     if tensor.encrypted or mask.encrypted:
         raise NotImplemented
-    mask_broadcasted = np.broadcast_to(mask.data, tensor.data.shape)
+    mask_broadcasted, data_broadcasted = np.broadcast_arrays(mask.data, tensor.data)
     indices = np.where(mask_broadcasted)
-    return tensor.data[indices]
+    return data_broadcasted[indices]

tests/test_tensor.py
@@ -947,10 +947,15 @@ class masked_select_Tests(unittest.TestCase):
         mask = TensorBase([[0, 0, 0], [1, 1, 0]])
         self.assertTrue(np.array_equal(tensor.masked_select(t, mask), TensorBase([3, 4])))
 
-    def testMasked_select_broadcasting(self):
+    def testMasked_select_broadcasting1(self):
         t = TensorBase(np.arange(0, 6).reshape(2, 3))
         mask = TensorBase([[1, 1, 0]])
         self.assertTrue(np.array_equal(tensor.masked_select(t, mask), TensorBase([0, 1, 3, 4])))
+
+    def testMasked_select_broadcasting2(self):
+        t = TensorBase([2.0])
+        mask = TensorBase([[1, 1, 0]])
+        self.assertTrue(np.array_equal(tensor.masked_select(t, mask), TensorBase([2.0, 2.0])))
 
     def testTensorBase_Masked_select(self):
         t = TensorBase(np.arange(0, 6).reshape(2, 3))