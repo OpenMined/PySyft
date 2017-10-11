customized methods replace 's built-in ones to control and define fixed-point precision configuraion

syft/he/paillier/basic.py
@@ -1,23 +1,32 @@
 import numpy as np
 from ...tensor import TensorBase
-from .FixedPoint import FXfamily
 
 
 class PaillierTensor(TensorBase):
 
     def __init__(self, public_key, data=None, input_is_decrypted=True, fixed_point_conf=None):
+        """
+        Initializes a Tensor. If `data` is encrypted, `fixed_point_conf` needs to match that of `data`.
+        That is `fixed_point_conf` should be the same one that was used for encryption of `data`.
+        If no `fixed_point_conf` is provided, it creates its own default fixed-point configuration.
+        :param public_key: Public key to encrypt the data by
+        :param data: Array-like data
+        :param input_is_decrypted: To indicate whether `data` is encrypted
+        :param fixed_point_conf: Precision configuration of type `FXfamily`
+        """
         self.encrypted = True
         if fixed_point_conf is None:
-            self.fixed_point_conf = FXfamily()
+            # creates default fixed point configuration
+            self.fixed_point_conf = PrecisionConf(32)
         else:
             self.fixed_point_conf = fixed_point_conf
 
         self.public_key = public_key
         if(type(data) == np.ndarray or type(data) == TensorBase) and input_is_decrypted:
             if type(data) == np.ndarray:
-                self.data = public_key.encrypt(data, True)
+                self.data = public_key.encrypt(data, True, precision_conf=self.fixed_point_conf)
             else:
-                self.data = public_key.encrypt(data.data, True)
+                self.data = public_key.encrypt(data.data, True, precision_conf=self.fixed_point_conf)
         else:
             self.data = data
 
@@ -26,21 +35,21 @@ class PaillierTensor(TensorBase):
         return self
 
     def __getitem__(self, i):
-        return PaillierTensor(self.public_key, self.data[i], False)
+        return PaillierTensor(self.public_key, self.data[i], False, fixed_point_conf=self.fixed_point_conf)
 
     def __add__(self, tensor):
         """Performs element-wise addition between two tensors"""
 
         if(not isinstance(tensor, TensorBase)):
             # try encrypting it
-            tensor = PaillierTensor(self.public_key, np.array([tensor]).astype('float'))
-            return PaillierTensor(self.public_key, self.data + tensor.data, False)
+            tensor = PaillierTensor(self.public_key, np.array([tensor]).astype('float'), fixed_point_conf=self.fixed_point_conf)
+            return PaillierTensor(self.public_key, self.data + tensor.data, False, fixed_point_conf=self.fixed_point_conf)
 
         if(type(tensor) == TensorBase):
-            tensor = PaillierTensor(self.public_key, tensor.data)
+            tensor = PaillierTensor(self.public_key, tensor.data, fixed_point_conf=self.fixed_point_conf)
 
         result_data = self.data + tensor.data
-        ptensor = PaillierTensor(self.public_key, result_data, False)
+        ptensor = PaillierTensor(self.public_key, result_data, False, fixed_point_conf=self.fixed_point_conf)
         ptensor._calc_add_depth(self, tensor)
         return ptensor
 
@@ -48,14 +57,14 @@ class PaillierTensor(TensorBase):
         """Performs element-wise subtraction between two tensors"""
         if(not isinstance(tensor, TensorBase)):
             # try encrypting it
-            tensor = PaillierTensor(self.public_key, np.array([tensor]).astype('float'))
-            return PaillierTensor(self.public_key, self.data - tensor.data, False)
+            tensor = PaillierTensor(self.public_key, np.array([tensor]).astype('float'), fixed_point_conf=self.fixed_point_conf)
+            return PaillierTensor(self.public_key, self.data - tensor.data, False, fixed_point_conf=self.fixed_point_conf)
 
         if(type(tensor) == TensorBase):
-            tensor = PaillierTensor(self.public_key, tensor.data)
+            tensor = PaillierTensor(self.public_key, tensor.data, fixed_point_conf=self.fixed_point_conf)
 
         result_data = self.data - tensor.data
-        ptensor = PaillierTensor(self.public_key, result_data, False)
+        ptensor = PaillierTensor(self.public_key, result_data, False, fixed_point_conf=self.fixed_point_conf)
         return ptensor
 
     def __isub__(self, tensor):
@@ -69,16 +78,19 @@ class PaillierTensor(TensorBase):
         if(isinstance(tensor, TensorBase)):
             if(not tensor.encrypted):
                 result = self.data * tensor.data
-                o = PaillierTensor(self.public_key, result, False)
+                o = PaillierTensor(self.public_key, result, False, fixed_point_conf=self.fixed_point_conf)
                 o._calc_mul_depth(self, tensor)
                 return o
             else:
                 return NotImplemented
-        else:
-            op = self.data * float(tensor)
-            ptensor = PaillierTensor(self.public_key, op, False)
+        elif np.isscalar(tensor):
+            # scalar is encode to match the precision of self before multiplication.
+            op = self.data * tensor
+            ptensor = PaillierTensor(self.public_key, op, False, fixed_point_conf=self.fixed_point_conf)
             ptensor._calc_mul_depth(self, tensor)
             return ptensor
+        else:
+            return NotImplemented
 
     def __truediv__(self, tensor):
         """Performs element-wise division between two tensors"""
@@ -86,13 +98,23 @@ class PaillierTensor(TensorBase):
         if(isinstance(tensor, TensorBase)):
             if(not tensor.encrypted):
                 result = self.data * (1 / tensor.data)
-                o = PaillierTensor(self.public_key, result, False)
+                o = PaillierTensor(self.public_key, result, False, fixed_point_conf=self.fixed_point_conf)
                 return o
             else:
                 return NotImplemented
+        elif np.isscalar(tensor):
+            op = self.data * (1 / tensor)
+            return PaillierTensor(self.public_key, op, False, fixed_point_conf=self.fixed_point_conf)
         else:
-            op = self.data * (1 / float(tensor))
-            return PaillierTensor(self.public_key, op, False)
+            return NotImplemented
+
+    def decrypt(self, seckey):
+        """
+        Returns a new decrypted tensor
+        :param seckey: secret key to use for decryption
+        :return: A new decrypted TensorBase object
+        """
+        return seckey.decrypt(self, self.fixed_point_conf)
 
     def sum(self, dim=None):
         """Returns the sum of all elements in the input array."""
@@ -100,10 +122,10 @@ class PaillierTensor(TensorBase):
             return NotImplemented
 
         if dim is None:
-            return PaillierTensor(self.public_key, self.data.sum(), False)
+            return PaillierTensor(self.public_key, self.data.sum(), False, fixed_point_conf=self.fixed_point_conf)
         else:
             op = self.data.sum(axis=dim)
-            return PaillierTensor(self.public_key, op, False)
+            return PaillierTensor(self.public_key, op, False, fixed_point_conf=self.fixed_point_conf)
 
     def dot(self, plaintext_x):
         if(not plaintext_x.encrypted):
@@ -116,3 +138,11 @@ class PaillierTensor(TensorBase):
 
     def __repr__(self):
         return "PaillierTensor: " + repr(self.data)
+
+
+class PrecisionConf:
+    def __init__(self, fraction_bits=None):
+        self.fraction_bits = fraction_bits
+
+    def __eq__(self, other):
+        return self.franction_bits == other.franction_bits

syft/he/paillier/keys.py
@@ -3,10 +3,10 @@ import numpy as np
 import json
 import syft
 from .basic import PaillierTensor
-from .FixedPoint import FXnum as FixedPoint
 from ...tensor import TensorBase
 from ..abstract.keys import AbstractSecretKey, AbstractPublicKey
 from ..abstract.keys import AbstractKeyPair
+import math
 
 
 class SecretKey(AbstractSecretKey):
@@ -14,15 +14,13 @@ class SecretKey(AbstractSecretKey):
     def __init__(self, sk):
         self.sk = sk
 
-    def decrypt(self, x):
+    def decrypt(self, x, precision_conf=None):
         """Decrypts x. X can be either an encrypted int or a numpy
         vector/matrix/tensor."""
 
         if(type(x) == paillier.EncryptedNumber):
-            FP_num = FixedPoint()
-            FP_num.scaledval = int(self.sk.decrypt(x))
             # casts the fixed point data type to python numeric type
-            return float(FP_num) if FP_num.family.fraction_bits > 1 else int(FP_num)
+            return self.sk.decrypt(x)
         elif(type(x) == TensorBase or type(x) == PaillierTensor):
             if(x.encrypted):
                 return TensorBase(self.decrypt(x.data), encrypted=False)
@@ -33,9 +31,7 @@ class SecretKey(AbstractSecretKey):
             x_ = x.reshape(-1)
             out = list()
             for v in x_:
-                FP_num = FixedPoint()
-                FP_num.scaledval = int(self.sk.decrypt(v))
-                python_numeric = float(FP_num) if FP_num.family.fraction_bits > 1 else int(FP_num)
+                python_numeric = self.sk.decrypt(v)
                 out.append(python_numeric)
             return np.array(out).reshape(sh)
         else:
@@ -83,14 +79,13 @@ class PublicKey(AbstractPublicKey):
         standard normal distribution"""
         return PaillierTensor(self, syft.randn(dim))
 
-    def encrypt(self, x, same_type=False):
+    def encrypt(self, x, same_type=False, precision_conf=None):
         """Encrypts x. X can be either an encrypted int or a numpy
         vector/matrix/tensor."""
         if(type(x) == int or type(x) == float or type(x) == np.float64):
             if(same_type):
                 return NotImplemented
-            FP_num = FixedPoint(x)
-            return self.pk.encrypt(FP_num.scaledval)
+            return self.pk.encrypt(x, precision_conf)
         elif(type(x) == TensorBase):
             if(x.encrypted or same_type):
                 return NotImplemented
@@ -100,8 +95,7 @@ class PublicKey(AbstractPublicKey):
             x_ = x.reshape(-1)
             out = list()
             for v in x_:
-                num = FixedPoint(v)
-                out.append(self.pk.encrypt(num.scaledval))
+                out.append(self.pk.encrypt(v, precision_conf))
             if(same_type):
                 return np.array(out).reshape(sh)
             else:
@@ -142,3 +136,89 @@ class KeyPair(AbstractKeyPair):
         self.secret_key = SecretKey(prikey)
 
         return (self.public_key, self.secret_key)
+
+
+# This is our custom implementations of `encode` and `decode`. It replaces those of `phe`'s EncodedNumber.
+# This gives us control over the precision of numbers.
+@classmethod
+def encode(cls, public_key, scalar, precision=None, max_exponent=None):
+
+    # Calculate the maximum exponent for desired precision
+    if precision is None:
+        if isinstance(scalar, int):
+            exponent = 0
+        elif isinstance(scalar, float):
+            # Encode with *at least* as much precision as the python float
+            # What's the base-2 exponent on the float?
+            bin_flt_exponent = math.frexp(scalar)[1]
+
+            # What's the base-2 exponent of the least significant bit?
+            # The least significant bit has value 2 ** bin_lsb_exponent
+            bin_lsb_exponent = bin_flt_exponent - cls.FLOAT_MANTISSA_BITS
+
+            # What's the corresponding base BASE exponent? Round that down.
+            exponent = math.floor(bin_lsb_exponent / cls.LOG2_BASE)
+        else:
+            raise TypeError("Don't know the precision of type %s."
+                            % type(scalar))
+    else:
+        exponent = -precision.fraction_bits
+
+    int_rep = int(scalar * pow(cls.BASE, -exponent))
+    if abs(int_rep) > public_key.max_int:
+        raise ValueError('Integer needs to be within +/- %d but got %d'
+                         % (public_key.max_int, int_rep))
+    # Wrap negative numbers by adding n
+    return cls(public_key, int_rep % public_key.n, exponent)
+
+
+def decode(self):
+    """Decode fixed-point plaintext and return the result.
+
+    Returns:
+      returns an int if `exponent` is < 1. Returns a float otherwise.
+
+    Raises:
+      OverflowError: if overflow is detected in the decrypted number.
+    """
+    if self.encoding >= self.public_key.n:
+        # Should be mod n
+        raise ValueError('Attempted to decode corrupted number')
+    elif self.encoding <= self.public_key.max_int:
+        # Positive
+        mantissa = self.encoding
+    elif self.encoding >= self.public_key.n - self.public_key.max_int:
+        # Negative
+        mantissa = self.encoding - self.public_key.n
+    else:
+        raise OverflowError('Overflow detected in decrypted number')
+    if self.exponent <= 1:
+        return int(mantissa * pow(self.BASE, self.exponent))
+    else:
+        return float(mantissa * pow(self.BASE, self.exponent))
+
+
+def my__mul__(self, other):
+    """Multiply by an int, float, or EncodedNumber.
+    If `other` is a scalar, it is first encoded to fixed-point precision of `self`"""
+    if isinstance(other, paillier.EncryptedNumber):
+        raise NotImplementedError('Good luck with that...')
+    if isinstance(other, paillier.EncodedNumber):
+        encoding = other
+    else:
+        int_rep = int(other * pow(paillier.EncodedNumber.BASE, -self.exponent))
+        if abs(int_rep) > self.public_key.max_int:
+            raise ValueError('Integer needs to be within +/- %d but got %d'
+                             % (self.public_key.max_int, int_rep))
+        # Wrap negative numbers by adding n
+        encoding = paillier.EncodedNumber(self.public_key, int_rep % self.public_key.n, self.exponent)
+    product = self._raw_mul(encoding.encoding)
+    exponent = self.exponent + encoding.exponent
+
+    return paillier.EncryptedNumber(self.public_key, product, exponent)
+
+
+paillier.EncodedNumber.encode = encode
+paillier.EncodedNumber.decode = decode
+paillier.EncodedNumber.BASE = 2
+paillier.EncryptedNumber.__mul__ = my__mul__