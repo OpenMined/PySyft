baa5843b5711bbe4d2568bd01bc237f1818af528
Andrew Trask <atrask@atrask-macbookpro.roam.corp.google.com>
2017-10-12 20:33:01 +0100

    added arbitrary nonlinearity approximator

--- syft/nonlin/__init__.py
+++ syft/nonlin/__init__.py
@@ -1,4 +1,7 @@
-from .sigmoid import SigmoidInterpolated10
+from .sigmoid import SigmoidInterpolated10, sigmoid
+from .arbitrary import PolyApproximator
 
 s = str(SigmoidInterpolated10)
 s += str(SigmoidInterpolated10)
+s += str(sigmoid)
+s += str(PolyApproximator)
