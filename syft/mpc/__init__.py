d5887bbd989930c84bee65d9b1a2c1eb6efd7340
Andrew Trask <atrask@atrask-macbookpro.roam.corp.google.com>
2017-10-14 14:38:13 +0100

    split spdz and rss MPC implementations into two directories

--- syft/mpc/__init__.py
+++ syft/mpc/__init__.py
@@ -1,5 +1,7 @@
 from .rss import PrecisionConfig, MPCRepo, RSSMPCTensor
+from .spdz import SPDZMPCTensor
 
 s = str(PrecisionConfig)
 s += str(MPCRepo)
 s += str(RSSMPCTensor)
+s += str(SPDZMPCTensor)
