021e26775fe214830675c5f8eb86459111025dc4
Marco <mark.sg97@gmail.com>
2017-10-15 21:23:21 +0200

    Add Polynomials and nonlinear activations

---
+++ syft/nonlin/polyfunction.py
@@ -0,0 +1,147 @@
+# coding=utf-8
+"""
+    Module polyfunction implements polynomial primitives for activation functions.
+    Note:The Documentation in this file follows the NumPy Doc. Style;
+         Hence, it is mandatory that future docs added here
+         strictly follow the same, to maintain readability and consistency
+         of the codebase.
+    NumPy Documentation Style-
+        http://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_numpy.html
+"""
+
+import numpy as np
+
+class PolyFunction:
+    """
+    Represents a polynomial and its derivative.
+    """
+    def __init__(self, coefs, derivative_coefs = None):
+        """
+        Builds a polynomial function with the given coefficients.
+        Parameters
+        ----------
+        coefs : numpy array
+            Polynomial coefficients in descending order.
+        
+        derivative_coefs : numpy array, optional
+            Polynomial derivative coefficients in descending order.
+            If None, derivative is calculated from the polynomial coefficients.
+        """
+        coefs = np.asarray(coefs)
+        self.degree = coefs.size - 1
+        
+        self.coefs = coefs
+        self.function = np.poly1d(self.coefs)
+        
+        if derivative_coefs is None:
+            derivative_coefs = np.polyder(coefs)
+            
+        self.derivative_coefs = np.asarray(derivative_coefs)
+        self.derivative = np.poly1d(self.derivative_coefs)
+        
+    def __call__(self, x):
+        """
+        Evaluates the polynomial in the given points.
+        Parameters
+        ----------
+        x : numpy array
+            The points to evaluate the polynomial at.
+        Returns
+        -------
+        numpy array:
+            Polynomial values in x.
+        """
+        return self.function(x)
+
+    def fit_function(func, degree, precision, nodes):
+        """
+        Performs polynomial approximation of the given function.
+        Parameters
+        ----------
+        func : function
+            The function to be approximated.
+        
+        degree : int
+            Polynomial degree used for interpolation.
+            
+        precision : int
+            Numerical precision of coefficients.
+           
+        nodes : numpy array
+            The interpolation points.
+            
+        Returns
+        -------
+        numpy array:
+            Coefficients of the interpolated polynomial.
+        """
+        # interpolate polynomial of given max degree
+        coefs = np.polyfit(nodes, func(nodes), degree)
+
+        # reduce precision of interpolated coefficients
+        coefs = np.asarray([int(x * 10**precision) / 10**precision for x in coefs])
+        
+        return coefs
+        
+    @classmethod
+    def from_approximation(cls, f_real, f_derivative=None, degree=10, precision=10, min_range=-5, max_range=5, num=100, distribution = 'uniform'):
+        """
+        Builds a PolyFunction by approximating a given function.
+        Parameters
+        ----------
+        f_real : function
+            The function to be interpolated.
+        
+        f_derivative : function, optional
+            The function derivative to be interpolated. 
+            If None, it will be calculated from the interpolating polynomial.
+            
+        degree : int, optional
+            Polynomial degree used for interpolation.
+           
+        precision : int, optional
+            Numerical precision of coefficients.
+            
+        min_range : int, optional
+            Lower bound of the interpolation nodes.
+            
+        max_range : int, optional
+            Upper bound of the interpolation nodes.
+            
+        num : int, optional
+            Number of interpolation nodes.
+            
+        distribution : string:function, optional
+            Node distribution to be used.
+            uniform, cheby and function accepting min/max range and number of nodes.
+            
+        Returns
+        -------
+        PolyFunction:
+            Fitted polynomial function with derivative.
+        """
+        if distribution == 'uniform':
+            nodes = np.linspace(min_range, max_range, num)
+        elif distribution == 'cheby':
+            nodes = np.cos( (np.pi/(2*num))*np.arange(1,2*num,2) ) * (max_range-min_range)/2
+            nodes = nodes + (max_range+min_range)/2
+        elif callable(distribution):
+            nodes = distribution(min_range, max_range, num)
+        else:
+            raise exception.ValueError('Invalid distribution type.')
+        
+        coefs = cls.fit_function(f_real, degree, precision, nodes)
+        
+        derivative_coefs = None
+        if f_derivative is not None:
+            derivative_coefs = cls.fit_function(f_derivative, degree, precision, nodes)
+        
+        poly = cls(coefs, derivative_coefs)
+        poly.is_approx = True
+        poly.precision = precision
+        poly.min_range = min_range
+        poly.max_range = max_range
+        poly.distribution = distribution
+        poly.nodes = nodes
+        
+        return poly
--- tests/test_nonlin.py
+++ tests/test_nonlin.py
@@ -1,24 +1,98 @@
 from syft import TensorBase
 import numpy as np
-from syft.nonlin import sigmoid, PolyApproximator
+from syft.nonlin import SigmoidActivation, SquareActivation, LinearSquareActivation
+from syft.nonlin import PolyFunction, CubicActivation, LinearCubicActivation
 import unittest
 
 
 # Here's our "unit tests".
-class NonlinTests(unittest.TestCase):
+class ActivationTests(unittest.TestCase):
     def test_sigmoid(self):
-
+        
         a = TensorBase(np.array([1, 2, 3]))
+        sigmoid = SigmoidActivation()
         approx = sigmoid(a)
         self.assertEqual(approx[0], 0.70788285770000015)
         self.assertEqual(approx[1], 0.87170293820000011)
         self.assertEqual(approx[2], 0.96626517229999997)
+        
+    def test_square(self):
+        
+        a = TensorBase(np.array([1, 2, 3]))
+        square = SquareActivation()
+        approx = square(a)
+        self.assertEqual(approx[0], 1)
+        self.assertEqual(approx[1], 4)
+        self.assertEqual(approx[2], 9)
+        
+    def test_square_derivative(self):
+        
+        a = TensorBase(np.array([1, 2, 3]))
+        square = SquareActivation()
+        approx = square.derivative(a)
+        self.assertEqual(approx[0], 2)
+        self.assertEqual(approx[1], 4)
+        self.assertEqual(approx[2], 6)
+        
+    def test_linear_square(self):
+        
+        a = TensorBase(np.array([1, 2, 3]))
+        linear_square = LinearSquareActivation()
+        approx = linear_square(a)
+        self.assertEqual(approx[0], 2)
+        self.assertEqual(approx[1], 6)
+        self.assertEqual(approx[2], 12)
+        
+    def test_linear_square_derivative(self):
+        
+        a = TensorBase(np.array([1, 2, 3]))
+        linear_square = LinearSquareActivation()
+        approx = linear_square.derivative(a)
+        self.assertEqual(approx[0], 3)
+        self.assertEqual(approx[1], 5)
+        self.assertEqual(approx[2], 7)    
+        
+    def test_cubic(self):
+        
+        a = TensorBase(np.array([1, 2, 3]))
+        cubic = CubicActivation()
+        approx = cubic(a)
+        self.assertEqual(approx[0], 1)
+        self.assertEqual(approx[1], 8)
+        self.assertEqual(approx[2], 27)
+        
+    def test_cubic_derivative(self):
+        
+        a = TensorBase(np.array([1, 2, 3]))
+        cubic = CubicActivation()
+        approx = cubic.derivative(a)
+        self.assertEqual(approx[0], 3)
+        self.assertEqual(approx[1], 12)
+        self.assertEqual(approx[2], 27)
+        
+    def test_linear_cubic(self):
+        
+        a = TensorBase(np.array([1, 2, 3]))
+        linear_cubic = LinearCubicActivation()
+        approx = linear_cubic(a)
+        self.assertEqual(approx[0], 2)
+        self.assertEqual(approx[1], 10)
+        self.assertEqual(approx[2], 30)
+        
+    def test_linear_cubic_derivative(self):
+        
+        a = TensorBase(np.array([1, 2, 3]))
+        linear_cubic = LinearCubicActivation()
+        approx = linear_cubic.derivative(a)
+        self.assertEqual(approx[0], 4)
+        self.assertEqual(approx[1], 13)
+        self.assertEqual(approx[2], 28)  
 
-
+        
 class TestPolyApproximators(unittest.TestCase):
     def test_poly_approx_sigmoid(self):
 
-        sigmoid = PolyApproximator(lambda x: 1 / (1 + np.exp(-x))).output
+        sigmoid = PolyFunction.from_approximation(lambda x: 1 / (1 + np.exp(-x)))
 
         a = TensorBase(np.array([.1, .2, .3, .4]))
         b = TensorBase(np.ones(4))
@@ -34,7 +108,7 @@ class TestPolyApproximators(unittest.TestCase):
 
     def test_poly_approx_tanh(self):
 
-        tanh = PolyApproximator(np.tanh).output
+        tanh = PolyFunction.from_approximation(np.tanh)
 
         a = TensorBase(np.array([.1, .2, .3, .4]))
         b = TensorBase(np.ones(4))
@@ -46,3 +120,22 @@ class TestPolyApproximators(unittest.TestCase):
         self.assertTrue(np.abs(tana[1] - 0.11527286) < 0.0001)
 
         self.assertTrue(np.abs(tanb[1] - 0.54896505) < 0.0001)
+
+    def test_function_fitting(self):
+        square = lambda x: x**2
+        square_approx = PolyFunction.fit_function(square,2,1,np.asarray([-1,0,1]))
+        self.assertTrue(np.abs(square_approx[0]-1) < 0.0001)
+        self.assertTrue(np.abs(square_approx[1]) < 0.0001)
+        self.assertTrue(np.abs(square_approx[2]) < 0.0001)
+        
+    def test_function_approx(self):
+        square = lambda x: x**2
+        square_approx = PolyFunction.from_approximation(square,degree=2,precision=1,num=3)
+        coefs = square_approx.coefs
+        self.assertTrue(np.abs(coeffs[0]-1) < 0.0001)
+        self.assertTrue(np.abs(coeffs[1]) < 0.0001)
+        self.assertTrue(np.abs(coeffs[2]) < 0.0001)
+        
+        derivative_coefs = square_approx.derivative_coefs
+        self.assertTrue(np.abs(derivative_coefs[0]-2) < 0.0001)
+        self.assertTrue(np.abs(derivative_coefs[1]) < 0.0001)
