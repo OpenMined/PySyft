cc093b0ec7c070efd14dd5d8d71e045e85911958
Adrian Cosma <adrian.cosma@scuola365.com>
2017-10-19 11:22:54 +0300

    fixed indentation

--- syft/nn/abstract_model.py
+++ syft/nn/abstract_model.py
@@ -1,29 +1,29 @@
-
 class AbstractModel(object):
-	"""
-		This is the interface used in all deep-learning models.
-	"""
+    """
+        This is the interface used in all deep-learning models.
+    """
 
-	def encrypt(self):
-		return NotImplemented
 
-	def decrypt(self):
-		return NotImplemented
+    def encrypt(self):
+        return NotImplemented
 
-	def forward(self):
-		return NotImplemented
+    def decrypt(self):
+        return NotImplemented
 
-	def learn(self, input, target, alpha=0.5, batchsize=32, encrypt_interval=16):
-		return NotImplemented
+    def forward(self):
+        return NotImplemented
 
-	def batch_update(self, minibatch, alpha):
-		return NotImplemented
+    def learn(self, input, target, alpha=0.5, batchsize=32, encrypt_interval=16):
+        return NotImplemented
 
-	def evaluate(self, inputs, targets):
-		return NotImplemented
+    def batch_update(self, minibatch, alpha):
+        return NotImplemented
 
-	def generate_gradient(self, input, target):
-		return NotImplemented
+    def evaluate(self, inputs, targets):
+        return NotImplemented
 
-	def __repr__(self):
-		return self.__str__()
+    def generate_gradient(self, input, target):
+        return NotImplemented
+
+    def __repr__(self):
+        return self.__str__()
