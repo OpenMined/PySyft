96cc9575de962c3b32844158e4cdbebcdcc25915
tim blazina <tim@felfel.ch>
2017-10-26 22:28:36 +0200

    Remove Float class that is currently not on master

--- syft/he/paillier/basic.py
+++ syft/he/paillier/basic.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pickle
 from ...tensor import TensorBase
 
 
@@ -129,86 +128,6 @@ class PaillierTensor(TensorBase):
         else:
             op = self.data.sum(axis=dim)
             return PaillierTensor(self.public_key, op, False, fixed_point_conf=self.fixed_point_conf)
-
-
-class Float():
-
-    def __init__(self, public_key, data=None):
-        """Wraps pointer to encrypted Float with an interface that numpy
-        can use."""
-
-        self.public_key = public_key
-        if(data is not None):
-            self.data = self.public_key.pk.encrypt(data)
-        else:
-            self.data = None
-
-    def __add__(self, y):
-        """Adds two encrypted Floats together."""
-
-        out = Float(self.public_key, None)
-        out.data = self.data + y.data
-        return out
-
-    def __mul__(self, y):
-        """Multiplies two Floats. y may be encrypted or a simple Float."""
-
-        if(type(y) == type(self)):
-            out = Float(self.public_key, None)
-            out.data = self.data * y.data
-            return out
-        elif(type(y) == int or type(y) == float):
-            out = Float(self.public_key, None)
-            out.data = self.data * y
-            return out
-        else:
-            return None
-
-    def __repr__(self):
-        """This is kindof a boring/uninformative __repr__"""
-
-        return 'e'
-
-    def __str__(self):
-        """This is kindof a boring/uninformative __str__"""
-
-        return 'e'
-
-    def __sub__(self, y):
-        """Subtracts two encrypted Floats."""
-
-        out = Float(self.public_key, None)
-        out.data = self.data - y.data
-        return out
-
-    def __truediv__(self, y):
-        """Divides two Floats. y may be encrypted or a simple Float."""
-
-        if(type(y) == type(self)):
-            out = Float(self.public_key, None)
-            out.data = self.data / y.data
-            return out
-        elif(type(y) == int):
-            out = Float(self.public_key, None)
-            out.data = self.data / y
-            return out
-        else:
-            return None
-
-    def decrypt(self, secret_key):
-        return secret_key.decrypt(self)
-
-    def deserialize(b):
-        return pickle.loads(b)
-
-    def dot(self, plaintext_x):
-        if(not plaintext_x.encrypted):
-            return (self * plaintext_x).sum(plaintext_x.dim() - 1)
-        else:
-            return NotImplemented
-
-    def serialize(self):
-        return pickle.dumps(self)
 
 
 class PrecisionConf:
