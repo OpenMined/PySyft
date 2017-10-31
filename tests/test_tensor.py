ba4db5c04ae12f2bc9b6028ce2fbc52f7269177d
Divyansh Jha <divyanshj.16@gmail.com>
2017-10-31 14:28:55 +0530

    modify wrong tests

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -1627,17 +1627,19 @@ class UnfoldTest(unittest.TestCase):
         self.assertTrue(np.array_equal(t1_unfolded_expected_2,
                                        t1_unfolded_actual_2))
 
-class SplitTests(unittest.Testcase):
+class SplitTests(unittest.TestCase):
     def test_split(self):
         t = TensorBase(np.random.rand(10,5))
         split_size = 3
         axis = 0
-        target_shapes = [(3,5),(3,5),(3,5),(1,5)]
+        target_shapes = [(4,5),(4,5),(2,5)]
         splits = syft.math.split(t,split_size,axis)
-        np_splits = np.array_split(t.data,split_size,axis)
-        for target_shape, split, np_split in zip(target_shapes, splits, np_splits):
+        start = 0
+        for target_shape, split in zip(target_shapes, splits):
+            print(split.shape())
             self.assertTrue(syft.equal(split.shape(),target_shape))
-            self.assertTrue(syft.equal(TensorBase(np_split),split))
+            self.assertTrue(syft.equal(t.narrow(axis,start,target_shape[axis]),split))
+            start += target_shape[axis]
 
 
 if __name__ == "__main__":
