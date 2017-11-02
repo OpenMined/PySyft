0447165ac271c55c93882324b29c71bb5c74c7d3
siavash <sia@siarez.com>
2017-11-02 11:56:23 -0700

    rolled back to using phe's floating point, until we have a better solution.

--- syft/he/paillier/basic.py
+++ syft/he/paillier/basic.py
@@ -4,29 +4,21 @@ from ...tensor import TensorBase
 
 class PaillierTensor(TensorBase):
 
-    def __init__(self, public_key, data=None, input_is_decrypted=True, fixed_point_conf=None):
+    def __init__(self, public_key, data=None, input_is_decrypted=True):
         """
-        Initializes a Tensor. If `data` is encrypted, `fixed_point_conf` needs to match that of `data`.
-        That is `fixed_point_conf` should be the same one that was used for encryption of `data`.
-        If no `fixed_point_conf` is provided, it creates its own default fixed-point configuration.
+        Initializes a Tensor.
         :param public_key: Public key to encrypt the data by
         :param data: Array-like data
         :param input_is_decrypted: To indicate whether `data` is encrypted
-        :param fixed_point_conf: Precision configuration of type `FXfamily`
         """
         self.encrypted = True
-        if fixed_point_conf is None:
-            # creates default fixed point configuration
-            self.fixed_point_conf = PrecisionConf(32)
-        else:
-            self.fixed_point_conf = fixed_point_conf
 
         self.public_key = public_key
         if(type(data) == np.ndarray or type(data) == TensorBase) and input_is_decrypted:
             if type(data) == np.ndarray:
-                self.data = public_key.encrypt(data, True, precision_conf=self.fixed_point_conf)
+                self.data = public_key.encrypt(data, True)
             else:
-                self.data = public_key.encrypt(data.data, True, precision_conf=self.fixed_point_conf)
+                self.data = public_key.encrypt(data.data, True)
         else:
             self.data = data
 
@@ -35,14 +27,14 @@ class PaillierTensor(TensorBase):
 
         if(not isinstance(tensor, TensorBase)):
             # try encrypting it
-            tensor = PaillierTensor(self.public_key, np.array([tensor]).astype('float'), fixed_point_conf=self.fixed_point_conf)
-            return PaillierTensor(self.public_key, self.data + tensor.data, False, fixed_point_conf=self.fixed_point_conf)
+            tensor = PaillierTensor(self.public_key, np.array([tensor]).astype('float'))
+            return PaillierTensor(self.public_key, self.data + tensor.data, False)
 
         if(type(tensor) == TensorBase):
-            tensor = PaillierTensor(self.public_key, tensor.data, fixed_point_conf=self.fixed_point_conf)
+            tensor = PaillierTensor(self.public_key, tensor.data)
 
         result_data = self.data + tensor.data
-        ptensor = PaillierTensor(self.public_key, result_data, False, fixed_point_conf=self.fixed_point_conf)
+        ptensor = PaillierTensor(self.public_key, result_data, False)
         ptensor._calc_add_depth(self, tensor)
         return ptensor
 
@@ -60,7 +52,7 @@ class PaillierTensor(TensorBase):
         if(isinstance(tensor, TensorBase)):
             if(not tensor.encrypted):
                 result = self.data * tensor.data
-                o = PaillierTensor(self.public_key, result, False, fixed_point_conf=self.fixed_point_conf)
+                o = PaillierTensor(self.public_key, result, False)
                 o._calc_mul_depth(self, tensor)
                 return o
             else:
@@ -68,7 +60,7 @@ class PaillierTensor(TensorBase):
         elif np.isscalar(tensor):
             # scalar is encode to match the precision of self before multiplication.
             op = self.data * tensor
-            ptensor = PaillierTensor(self.public_key, op, False, fixed_point_conf=self.fixed_point_conf)
+            ptensor = PaillierTensor(self.public_key, op, False)
             ptensor._calc_mul_depth(self, tensor)
             return ptensor
         else:
@@ -102,13 +94,13 @@ class PaillierTensor(TensorBase):
         if(isinstance(tensor, TensorBase)):
             if(not tensor.encrypted):
                 result = self.data * (1 / tensor.data)
-                o = PaillierTensor(self.public_key, result, False, fixed_point_conf=self.fixed_point_conf)
+                o = PaillierTensor(self.public_key, result, False)
                 return o
             else:
                 return NotImplemented
         elif np.isscalar(tensor):
             op = self.data * (1 / tensor)
-            return PaillierTensor(self.public_key, op, False, fixed_point_conf=self.fixed_point_conf)
+            return PaillierTensor(self.public_key, op, False)
         else:
             return NotImplemented
 
@@ -124,15 +116,7 @@ class PaillierTensor(TensorBase):
             return NotImplemented
 
         if dim is None:
-            return PaillierTensor(self.public_key, self.data.sum(), False, fixed_point_conf=self.fixed_point_conf)
+            return PaillierTensor(self.public_key, self.data.sum(), False)
         else:
             op = self.data.sum(axis=dim)
-            return PaillierTensor(self.public_key, op, False, fixed_point_conf=self.fixed_point_conf)
-
-
-class PrecisionConf:
-    def __init__(self, fraction_bits=None):
-        self.fraction_bits = fraction_bits
-
-    def __eq__(self, other):
-        return self.franction_bits == other.franction_bits
+            return PaillierTensor(self.public_key, op, False)
