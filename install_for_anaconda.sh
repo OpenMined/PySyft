0c2068f80205734245d6c5cc11dcbdf07d1559b1
Floev <florian.evingar@gmail.com>
2017-11-25 15:24:43 +0000

    Allow user to chose env name
    Haven't tested this yet.

--- install_for_anaconda.sh
+++ install_for_anaconda.sh
@@ -1,4 +1,5 @@
-conda create -n openmined python=3.6
-source activate openmined
+env_name=${1:-openmined}
+conda create -n $env_name python=3.6
+source activate $env_name
 pip install -r requirements.txt
 python setup.py install
