e4054de73ed1da5be0cc343dd19fd87cbb4299e0
Floev <Florian.Evingar@gmail.com>
2017-11-26 08:28:18 +0000

    Order syft.py, expose copy, make notebook work

--- syft/syft.py
+++ syft/syft.py
@@ -1,42 +1,74 @@
-import numpy as np
 import zmq
-from random import choice
 
 class FloatTensor():
-
     
     def __init__(self, controller, data, data_is_pointer = False, verbose=False):
         self.verbose = verbose
         self.controller = controller
         if(data is not None and not data_is_pointer):
-            
-            controller.socket.send_json({"functionCall":"createTensor", "data": list(data.flatten()), "shape": data.shape})
-            
+            data = data.astype('float')
+            controller.socket.send_json({"objectType"   :"tensor",
+                                         "functionCall" :"create",
+                                         "data"         : list(data.flatten()),
+                                         "shape"        : data.shape})
             self.id = int(controller.socket.recv_string())
-        
             print("FloatTensor.__init__: " +  str(self.id))
 
         elif(data_is_pointer):
             self.id = int(data)
 
-
     def __add__(self,x):
-
         self.controller.socket.send_json(self.cmd("add",[x.id])) # sends the command
         return FloatTensor(self.controller,int(self.controller.socket.recv_string()),True)
 
+    def __repr__(self):
+        return self.no_params_func("print",True,False)
+
+    def __str__(self):
+        return self.no_params_func("print",True,False)
     
     def abs(self):
-        return self.no_params_func("abs")
+        return self.no_params_func("abs",return_response=True)
+
+    def abs_(self):
+        return self.no_params_func("abs_")
+
+    def addmm_(self, x,y):
+        return self.params_func("addmm_",[x.id,y.id])
+
+    def addmm(self, x,y):
+        copy = self.copy()
+        copy.params_func("addmm_",[x.id,y.id])
+        return copy
+
+    def cmd(self,functionCall,tensorIndexParams=[]):
+        cmd = {
+            'functionCall'      :functionCall,
+            'objectType'        :'tensor',
+            'objectIndex'       :self.id,
+            'tensorIndexParams' :tensorIndexParams}
+        return cmd
+
+    def copy(self):
+        return self.no_params_func("copy", return_response=True)
+
+    def cpu(self):
+        return self.no_params_func("cpu")
+
+    def delete_tensor(self):
+        if(self.id is not None):
+            self.no_params_func("delete")
+        self.verbose = None
+        self.controller = None
+        self.id = None
+
+    def gpu(self):
+        return self.no_params_func("gpu")
 
     def neg(self):
-        return self.no_params_func("neg")
-
-    def scalar_multiply(self, scalar):
-        return self.params_func("scalar_multiply",[scalar])
+        return self.no_params_func("neg",return_response=True)
     
-    def params_func(self, name, params, return_response=False):
-        
+    def params_func(self, name, params, return_response=False,return_as_tensor=True):        
         # send the command
         self.controller.socket.send_json(self.cmd(name,tensorIndexParams=params))
         # receive output from command
@@ -46,26 +78,19 @@ class FloatTensor():
             print(res)
 
         if(return_response):
-            return res
+            if(return_as_tensor):
+                print("FloatTensor.__init__: " +  res)
+                return FloatTensor(self.controller,int(res),True)
+            else:
+                return res
         return self
 
-    def no_params_func(self, name, return_response=False):
-        return( self.params_func(name,[],return_response) )
+    def no_params_func(self, name, return_response=False,return_as_tensor=True):
+        return( self.params_func(name,[],return_response,return_as_tensor) )
+
+    def scalar_multiply(self, scalar):
+        return self.params_func("scalar_multiply",[scalar],return_response=True)
     
-    def __repr__(self):
-        return self.no_params_func("print",True)
-
-    def __str__(self):
-        return self.no_params_func("print",True)
-
-    def cmd(self,functionCall,tensorIndexParams=[]):
-        cmd = {}
-        cmd['functionCall'] = functionCall
-        cmd['objectType'] = 'tensor'
-        cmd['objectIndex'] = self.id
-        cmd['tensorIndexParams'] = tensorIndexParams
-        return cmd
-
 
 class SyftController():
 
