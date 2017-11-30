7dde48aa46338f76fa0b2c791a3d69d100c937e9
Martin Uribe <clamytoe@users.noreply.github.com>
2017-11-29 18:05:34 -0600

    Modified the installation scripts so that they would be more alike and so that they would install packages from Anaconda and only pip install zmq.

--- install_for_anaconda.sh
+++ install_for_anaconda.sh
@@ -1,5 +1,5 @@
 env_name=${1:-openmined}
-conda create -n $env_name python=3.6
+conda create -n $env_name python=3.6 numpy jupyter
 source activate $env_name
-pip install -r requirements.txt
+pip install zmq
 python setup.py install
