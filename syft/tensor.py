c70d5ed7155f4c249b43c5d5aea774578756498c
yanndupis <yann.dupis@gmail.com>
2018-01-01 14:13:04 -0800

    doc: add doc to clamp

--- syft/tensor.py
+++ syft/tensor.py
@@ -366,7 +366,19 @@ class FloatTensor():
         return self.no_params_func("ceil_")
 
     def clamp(self, min = 'None', max = 'None'):
-        
+        """
+        Clamp all elements in input into the range [min, max]
+        Parameters
+        ----------
+        min : float
+            lower-bound of the range to be clamped to
+        mas : float
+            upper-bound of the range to be clamped to
+        Returns
+        -------
+        FloatTensor
+            Output tensor
+        """
         return self.params_func("clamp", [min,max], return_response=True)
 
     def contiguous(self):
