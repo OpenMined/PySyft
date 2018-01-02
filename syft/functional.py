9954e812ee224a75a48cf9b200c8e3c4c99375ae
David de Meij <daviddemeij@gmail.com>
2018-01-02 16:40:51 +0100

    add randn function

--- syft/functional.py
+++ syft/functional.py
@@ -19,6 +19,19 @@ def ones(*args):
     assert type(dims[0]) == int
     return params_func(cmd, "ones", params=dims, return_type='FloatTensor')
 
+def randn(*args):
+    """
+    Returns a tensor filled with random numbers from a normal distribution with mean=0 and std=1
+    The shape of the tensor is defined by the varargs sizes.
+    ----------
+    Returns
+    -------
+    FloatTensor
+    """
+    dims = list(args)
+    assert type(dims[0]) == int
+    return params_func(cmd, "randn", params=dims, return_type='FloatTensor')
+
 def random(*args):
     """
     Returns a tensor filled with random numbers from a uniform distribution on the interval [0,1)
