d5411825cf3490d520b5b9f5387f994cd25cff6e
Kabir Khan <kakh@microsoft.com>
2018-01-02 17:13:29 -0800

    Adding momentum and lr decay

--- syft/optim.py
+++ syft/optim.py
@@ -2,7 +2,7 @@ import syft.controller as controller
 
 class SGD(object):
 
-	def __init__(self,params,alpha):
+	def __init__(self, params, lr=0.01, momentum=0., decay=0.):
 
 		param_ids = list()
 		for p in params:
@@ -10,13 +10,13 @@ class SGD(object):
 		self.type = 'Optimizer'
 		self.sc = controller
 		self.id = -1
-		self.id = int(self.sc.send_json(self.cmd("create",[alpha] + param_ids)))
+		self.id = int(self.sc.send_json(self.cmd("create", [lr, momentum, decay] + param_ids)))
 
 	def zero_grad(self):
 		return self.sc.no_params_func(self.cmd,"zero_grad",return_type='string')
 
-	def step(self):
-		return self.sc.no_params_func(self.cmd,"step",return_type='string')		
+	def step(self, batch_size, iteration):
+		return self.sc.params_func(self.cmd, "step", params=[batch_size, iteration], return_type='string')		
 
 	def cmd(self,function_call, params = []):
 		cmd = {
