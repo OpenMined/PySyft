30d3400d5365be77c1a0bb1f5d02910d10146cb9
Morgan Giraud <morgan@explee.com>
2018-01-17 17:52:18 +0100

    Add load function

--- syft/onnx/__init__.py
+++ syft/onnx/__init__.py
@@ -1,4 +1,5 @@
 import syft.controller
+import onnx
 
 def export(model, input_tensor, filename, verbose=False):
     return syft.controller.params_func(
@@ -6,4 +7,12 @@ def export(model, input_tensor, filename, verbose=False):
         "to_proto", 
         [model.id, input_tensor.id, filename], 
         return_type='string'
+    )
+
+def load(filename):
+    return syft.controller.params_func(
+        syft.controller.cmd,
+        "from_proto",
+        [filename],
+        return_type='Model'
     )
