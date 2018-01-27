69e0b3d842048316b630d0ff695e3c02f17cb438
Ben DeCoste <bendecoste@gmail.com>
2018-01-27 08:41:19 -0400

    load required params to run a job

--- grid/server/grid.py
+++ grid/server/grid.py
@@ -1,4 +1,47 @@
+import requests
+import json
+import keras
+import os
+import numpy as np
+
+## Base IPFS Address
+IPFS_ADDR = 'https://ipfs.infura.io/ipfs'
+
 class Server():
 
-    def runExperiment(self, ipfs_address):
-        print("running experiment")
+    def run_experiment(self, ipfs_address):
+        r = requests.get(f'{IPFS_ADDR}/{ipfs_address}')
+        json_response = json.loads(r.text)
+
+        print(f'got resp {json_response}')
+
+        model = self.load_model(json_response["model"])
+        input = self.load_tensor(json_response["input"])
+        target = self.load_tensor(json_response["target"])
+
+        batch_size = json_response["batch_size"]
+        epochs = json_response["epochs"]
+
+
+    def load_model(self, ipfs_address):
+        r = requests.get(f'{IPFS_ADDR}/{ipfs_address}')
+
+        # keras only loads models from files
+        with open('job-model.h5', 'wb') as model_file:
+            model_file.write(r.content)
+
+        model = keras.models.load_model('job-model.h5')
+        os.remove('job-model.h5')
+
+        return model
+
+    def load_tensor(self, ipfs_address):
+        r = requests.get(f'{IPFS_ADDR}/{ipfs_address}')
+
+        with open('job-tensor.npy', 'wb') as tensor_file:
+            tensor_file.write(r.content)
+
+        tensor = np.load('job-tensor.npy')
+        os.remove('job-tensor.npy')
+
+        return tensor
