4789fa487d3d74437cf6c839a3c34361aa096dc1
Robert (Bobby) Wagner <raw141@case.edu>
2018-01-29 17:22:20 -0500

    added ability to create mpc tensor from float tensor

--- syft/tensor/mpc_tensor.py
+++ syft/tensor/mpc_tensor.py
@@ -3,8 +3,11 @@ import syft.controller
 from .float_tensor import FloatTensor
 
 class MPCTensor():
-    def __init__(self, data,):
-        self.data = FloatTensor(self,data)
+    def __init__(self, data, data_as_tensor = False):
+        if data_as_tensor:
+            self.data = data
+        else:
+            self.data = FloatTensor(data)
     def shard(self):
         rand_tensor = self.data.random_()
         self.data = self.data-rand_tensor
