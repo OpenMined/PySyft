fe36ab5294a36cf03c360a0af93bc87b080df991
Robert (Bobby) Wagner <raw141@case.edu>
2018-01-29 15:47:45 -0500

    init, shard and addition functionality for int tensor

---
+++ syft/tensor/mpc_tensor.py
@@ -0,0 +1,13 @@
+import numpy as np
+import syft.controller
+from .float_tensor import FloatTensor
+
+class MPCTensor():
+    def __init__(self, data,):
+        self.data = FloatTensor(self,data, autograd=False, data_is_pointer=False, delete_after_use=True)
+    def shard(self):
+        rand_tensor = self.data.random_()
+        self.data = self.data-rand_tensor
+        return rand_tensor
+    def __add__(self,x):
+        return self.data+x.data
