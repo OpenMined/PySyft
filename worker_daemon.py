640d70de7393365bf0d789f0f8dc8611a61ee2ab
Ben DeCoste <bendecoste@gmail.com>
2018-01-29 10:44:28 -0400

    fix stack overflow in poll function
    also, you can specify poll interval with `--interval`

--- worker_daemon.py
+++ worker_daemon.py
@@ -1,4 +1,13 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Poll Openmined for Jobs.')
+parser.add_argument('--interval', dest='interval', action='store',
+                   default=10, type=int,
+                   help='How often to poll for jobs in seconds (default: 10)')
+
+args = parser.parse_args()
+
 from grid.server.grid import Server as GridWorker
 
 w = GridWorker()
-w.poll()
+w.poll(args.interval)
