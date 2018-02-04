1e6358f95feecca5f762eefb2038627910226c51
Andrew Trask <liamtrask@gmail.com>
2018-02-04 13:25:57 +0000

    initial linux install script

--- ubuntu_installation.sh
+++ ubuntu_installation.sh
@@ -3,3 +3,9 @@ sudo apt-get install python3-pip python3-dev
 sudo apt-get install libssl-dev 
 sudo pip3 install -r requirements.txt
 
+
+sudo apt install golang-go -y
+echo "export GOPATH=$HOME/go
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin" >> ~/.bashrc
+source ~/.bashrc
+
