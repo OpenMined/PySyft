c5053b90e3bf1ef38d1dd46e62f2f38c894fa42c
Andrew Trask <liamtrask@gmail.com>
2018-02-04 13:31:13 +0000

    fixed -y

--- ubuntu_installation.sh
+++ ubuntu_installation.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
-sudo apt-get install python3-pip python3-dev
-sudo apt-get install libssl-dev 
+sudo apt-get install python3-pip python3-dev -y
+sudo apt-get install libssl-dev  -y
 sudo pip3 install -r requirements.txt
 
 
