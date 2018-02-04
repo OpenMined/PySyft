ea1de3963773a7ce50172c8b8600a0041d1fefed
Andrew Trask <liamtrask@gmail.com>
2018-02-04 15:43:05 +0000

    added nohup

--- work.sh
+++ work.sh
@@ -1,4 +1,4 @@
-python3 ipfs_grid_worker_daemon.py &
+nohup python3 ipfs_grid_worker_daemon.py &
 
 while true
 do
@@ -8,7 +8,7 @@ do
         echo "Pulled, rebuilding and restarting worker"
         sudo pkill python3
         sudo python3 setup.py install
-        python3 ipfs_grid_worker_daemon.py &  
+        nohup python3 ipfs_grid_worker_daemon.py &  
     elif [[ $PULL_MSG = *"Aborting"* ]]; then
         echo "Not able to pull, you probably neeed to commit"
         exit 1
