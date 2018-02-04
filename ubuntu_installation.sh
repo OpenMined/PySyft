2c58903e5d314ac429b050eb00e0f61f3d385e33
Andrew Trask <liamtrask@gmail.com>
2018-02-04 13:34:10 +0000

    fix

--- ubuntu_installation.sh
+++ ubuntu_installation.sh
@@ -5,8 +5,9 @@ sudo pip3 install -r requirements.txt
 
 
 sudo apt-get install golang-1.9-go -y
-echo "export GOPATH=/usr/lib/go-1.9/bin
+echo "export GOPATH=/usr/lib/go-1.9
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin" >> ~/.bashrc
+
 source ~/.bashrc
 
 
