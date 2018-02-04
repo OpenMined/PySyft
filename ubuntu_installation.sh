2f738dc40ebe92462ebe04cff744a7618230a3b8
Andrew Trask <liamtrask@gmail.com>
2018-02-04 13:28:47 +0000

    removed line

--- ubuntu_installation.sh
+++ ubuntu_installation.sh
@@ -4,8 +4,10 @@ sudo apt-get install libssl-dev
 sudo pip3 install -r requirements.txt
 
 
-sudo apt install golang-go -y
-echo "export GOPATH=$HOME/go
+sudo apt-get install golang-1.9-go -y
+echo "export GOPATH=/usr/lib/go-1.9/bin
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin" >> ~/.bashrc
 source ~/.bashrc
+
+
 
