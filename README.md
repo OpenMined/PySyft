1904f41294f8cd29c57ea32ffb80f7a2729a4ce1
Benjamin DeCoste <bendecoste@gmail.com>
2018-02-05 16:24:50 -0400

    added some more info for getting started. (#17)
    * added some more info for getting started.
    
    * added more instructions to get started in readme

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Grid
 
 Proof of concept for python based GRID edge nodes.
+
+## Running
+
+```sh
+python setup.py install
+```
+
+# Running with PUBSUB
+### Running `ipfs_grid_worker_daemon.py`
+
+Grid worker daemon requires an IPFS daemon to be running with pubsub support
+turned on.
+
+```sh
+ipfs daemon --enable-pubsub-experiment
+```
+
+You can then run the worker daemon
+```sh
+python ipfs_gif_worker_daemon.py
+```
+
+Start Jupyter
+```sh
+jupyter notebook
+```
+
+navigate to `notebooks/pubsub/` and open `Keras Grid Client and Worker.ipynb` and
+follow along in that notebook.
--- grid/grid.py
+++ grid/grid.py
@@ -3,38 +3,49 @@ import base64
 import random
 import keras
 import json
-import numpy as np 
-
+import numpy as np
+import sys
+from colorama import Fore, Back, Style
 
 class Grid(object):
-    
-    def __init__(self,ipfs_addr='127.0.0.1',port=5001):
-        
-        self.api = ipfsapi.connect(ipfs_addr, port)
+
+    def __init__(self, ipfs_addr='127.0.0.1', port=5001):
+
+        try:
+            self.api = ipfsapi.connect(ipfs_addr, port)
+        except:
+            print(f'\n{Fore.RED}ERROR: {Style.RESET_ALL}could not connect to IPFS.  Is your daemon running with pubsub support at {ipfs_addr} on port {port}')
+            sys.exit()
+
         self.encoded_id = self.get_encoded_id()
         self.id = self.api.config_show()['Identity']['PeerID']
-        
+
     def get_encoded_id(self):
-        
-        """Currently a workaround because we can't figure out how to decode the 'from' 
+
+        """Currently a workaround because we can't figure out how to decode the 'from'
         side of messages sent across the wire. However, we can check to see if two messages
         are equal. Thus, by sending a random message to ourselves we can figure out what
         our own encoded id is. TODO: figure out how to decode it."""
-        
+
         rand_channel = random.randint(0,1000000)
-        temp_channel = self.api.pubsub_sub(topic=rand_channel,stream=True)
+        try:
+            temp_channel = self.api.pubsub_sub(topic=rand_channel,stream=True)
+        except:
+            print(f'\n{Fore.RED}ERROR: {Style.RESET_ALL}could not connect to IPFS PUBSUB.  Did you run the daemon with {Fore.GREEN}--enable-pubsub-experiment{Style.RESET_ALL} ?')
+            sys.exit()
+
         secret = random.randint(0,1000000)
         self.api.pubsub_pub(topic=rand_channel,payload="id:" + str(secret))
-        
+
         for encoded in temp_channel:
 
             # decode message
             decoded = self.decode_message(encoded)
-            
+
             if(decoded is not None):
                 if(str(decoded['data'].split(":")[-1]) == str(secret)):
                     return str(decoded['from'])
-                
+
     def decode_message(self,encoded):
         if('from' in encoded):
             decoded = {}
@@ -46,32 +57,32 @@ class Grid(object):
             return decoded
         else:
             return None
-        
+
     def serialize_keras_model(self,model):
         model.save('temp_model.h5')
         f = open('temp_model.h5','rb')
         model_bin = f.read()
         f.close()
         return model_bin
-    
+
     def deserialize_keras_model(self,model_bin):
         g = open('temp_model2.h5','wb')
         g.write(model_bin)
         g.close()
         model = keras.models.load_model('temp_model2.h5')
         return model
-    
+
     def serialize_numpy(self, tensor):
         return json.dumps(tensor.tolist()) # nested lists with same data, indices
-    
+
     def deserialize_numpy(self,json_array):
         return np.array(json.loads(json_array)).astype('float')
-    
+
     def publish(self,channel,dict_message):
         self.api.pubsub_pub(topic=channel,payload=json.dumps(dict_message))
-    
+
     def generate_fit_spec(self, model,input,target,valid_input=None,valid_target=None,batch_size=1,epochs=1,log_interval=1):
-        
+
         model_bin = self.serialize_keras_model(model)
         model_addr = self.api.add_bytes(model_bin)
 
@@ -101,7 +112,7 @@ class Grid(object):
         spec['framework'] = 'keras'
         spec['train_channel'] = 'openmined_train_'+str(model_addr)
         return spec
-    
+
     def listen_to_channel(self,handle_message,channel):
         new_models = self.api.pubsub_sub(topic=channel,stream=True)
 
@@ -112,16 +123,16 @@ class Grid(object):
                 out = handle_message(message)
                 if(out is not None):
                     return out
-                
+
     def keras2ipfs(self,model):
         return self.api.add_bytes(self.serialize_keras_model(model))
-    
+
     def ipfs2keras(self,model_addr):
         model_bin = self.api.cat(model_addr)
         return self.deserialize_keras_model(model_bin)
-    
+
     def receive_model(self,message, verbose=True):
-    
+
         msg = json.loads(message['data'])
         if(msg is not None):
             if(msg['type'] == 'transact'):
@@ -132,9 +143,9 @@ class Grid(object):
                     output += " - Epoch " + str(msg['epoch_id']) + " of " + str(msg['num_epochs'])
                     output += " - Valid Loss: " + str(msg['eval_loss'])[0:8]
                     print(output)
-    
+
     def fit_worker(self,message):
-    
+
         decoded = json.loads(message['data'])
 
         if(decoded['framework'] == 'keras'):
@@ -148,7 +159,7 @@ class Grid(object):
                 assert(False)
 
             input,target,valid_input,valid_target = list(map(lambda x:self.deserialize_numpy(x),np_strings))
-            
+
             for e in range(0,decoded['epochs'],decoded['log_interval']):
                 model.fit(input, target, batch_size=decoded['batch_size'], epochs=decoded['log_interval'], verbose=0)
                 eval_loss = model.evaluate(valid_input,valid_target,verbose=0)
@@ -174,16 +185,16 @@ class Grid(object):
             output += " - Valid Loss: " + str(spec['eval_loss'])[0:8]
             print(output)
 
-            
+
     def fit(self, model,input,target,valid_input=None,valid_target=None,batch_size=1,epochs=1,log_interval=1,message_handler=None):
-    
+
         if(message_handler is None):
             message_handler = self.receive_model
         spec = self.generate_fit_spec(model,input,target,valid_input,valid_target,batch_size,epochs,log_interval)
         self.publish('openmined',spec)
-        
+
         trained = self.listen_to_channel(message_handler,spec['train_channel'])
         return trained
-    
+
     def work(self):
         self.listen_to_channel(self.fit_worker,'openmined')
