7ba271fde9357c193e62d322e3a5c7fa09fc03e0
Justin Patriquin <justinpatriquin@gmail.com>
2018-02-13 19:51:42 -0400

    add task (#44)

--- grid/pubsub/client.py
+++ grid/pubsub/client.py
@@ -1,11 +1,10 @@
-from grid import ipfsapi
 from grid.lib import utils
 from grid.pubsub.base import PubSub
 
 import json
 
-class Client(PubSub):
 
+class Client(PubSub):
     def __init__(self):
         super().__init__()
         self.progress = {}
@@ -16,7 +15,9 @@ class Client(PubSub):
         self.spec = self.generate_fit_spec(model,input,target,valid_input,valid_target,batch_size,epochs,log_interval)
         self.publish('openmined', self.spec)
 
-        trained = self.listen_to_channel(self.spec['train_channel'], message_handler)
+
+        trained = self.listen_to_channel(message_handler,
+                                         self.spec['train_channel'])
         return trained
 
     def update_progress(self, parent_model, worker_id, num_epochs, epoch_id):
@@ -31,7 +32,6 @@ class Client(PubSub):
 
         return p
 
-
     def max_progress(self, parent_model):
         if parent_model not in self.progress:
             return 0
@@ -44,19 +44,18 @@ class Client(PubSub):
         return max_progress
 
     # TODO: torch
-    def receive_model(self,message, verbose=True):
+    def receive_model(self, message, verbose=True):
         msg = json.loads(message['data'])
 
         if(msg is not None):
             if(msg['type'] == 'transact'):
-                return utils.ipfs2keras(msg['model_addr']),msg
+                return utils.ipfs2keras(msg['model_addr']), msg
             elif(msg['type'] == 'log'):
                 if(verbose):
-                    output =  "Worker:" + msg['worker_id'][-5:]
+                    output = "Worker:" + msg['worker_id'][-5:]
                     output += " - Epoch " + str(msg['epoch_id']) + " of " + str(msg['num_epochs'])
                     output += " - Valid Loss: " + str(msg['eval_loss'])[0:8]
                     print(output)
-
 
                 # Figure out of we should tell this worker to quit.
                 parent_model = msg['parent_model']
@@ -64,13 +63,15 @@ class Client(PubSub):
                 num_epochs = msg['num_epochs']
                 epoch_id = msg['epoch_id']
 
-                progress = self.update_progress(parent_model, worker_id, num_epochs, epoch_id)
+                progress = self.update_progress(parent_model, worker_id,
+                                                num_epochs, epoch_id)
                 max_progress = self.max_progress(parent_model)
 
                 if progress < max_progress * 0.75:
                     quit = {}
                     quit['op_code'] = 'quit'
-                    self.publish(self.spec['train_channel'] + ':' + worker_id, quit)
+                    self.publish(self.spec['train_channel'] + ':' + worker_id,
+                                 quit)
 
     # TODO: framework = 'torch'
     def generate_fit_spec(self, model,input,target,valid_input=None,valid_target=None,batch_size=1,epochs=1,log_interval=1, framework = 'keras', model_class = None):
@@ -94,7 +95,7 @@ class Client(PubSub):
         else:
             valid_target = self.serialize_numpy(valid_target)
 
-        datasets = [train_input,train_target,valid_input,valid_target]
+        datasets = [train_input, train_target, valid_input, valid_target]
         data_json = json.dumps(datasets)
         data_addr = self.api.add_str(data_json)
 
@@ -105,5 +106,14 @@ class Client(PubSub):
         spec['epochs'] = epochs
         spec['log_interval'] = log_interval
         spec['framework'] = framework
-        spec['train_channel'] = 'openmined_train_'+str(model_addr)
+        spec['train_channel'] = 'openmined_train_' + str(model_addr)
         return spec
+
+    def add_task(self, name):
+        json = {"name": name}
+
+        addr = self.api.add_json(json)
+        print(addr)
+        data = "add_task:" + str(addr)
+
+        self.publish('openmined', data)
