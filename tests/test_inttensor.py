d9d7f8a53ca45deb8485f89abcc9212b7452d6c4
Ankit Goyal <goyalankit40@gmail.com>
2018-02-16 11:21:13 +0530

    Add unittest for step size of 2, resolves #1266

--- tests/test_inttensor.py
+++ tests/test_inttensor.py
@@ -44,12 +44,19 @@ def test_view():
 
 def test_unfold():
     a = IntTensor(np.array([[-1, 2, 3, 5], [0, 4, 6, 7], [10, 3, 2, -5]], dtype=np.int32))
+
+    # Test1
     expected_a = IntTensor(np.array([[[-1, 2, 3, 5], [0, 4, 6, 7]], [[0, 4, 6, 7], [10, 3, 2, -5]]], dtype=np.int32))
     actual_a = a.unfold(0, 2, 1)
     assert(actual_a.equal(expected_a))
 
+    # Test2
     expected_a = IntTensor(np.array([[[-1, 2, 3], [0, 4, 6], [10, 3, 2]],
         [[2, 3, 5], [4, 6, 7], [3, 2, -5]]], dtype=np.int32))
-
     actual_a = a.unfold(1, 3, 1)
+    assert(actual_a.equal(expected_a))
+
+    # Test3
+    expected_a = IntTensor(np.array([[[-1, 2], [0, 4], [10, 3]], [[3, 5], [6, 7], [2, -5]]], dtype=np.int32))
+    actual_a = a.unfold(1, 2, 2)
     assert(actual_a.equal(expected_a))
