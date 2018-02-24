83350ad9194ec734ece1f4aa93e8b3085775d34c
Andrew Trask <liamtrask@gmail.com>
2018-02-24 18:02:52 +0000

    updated install script

--- install_scripts/ubuntu_installation.sh
+++ install_scripts/ubuntu_installation.sh
@@ -4,16 +4,15 @@ bash Anaconda3-5.1.0-Linux-x86_64.sh
 rm Anaconda3-5.1.0-Linux-x86_64.sh
 /home/ubuntu/anaconda3/bin/conda install pytorch-cpu torchvision -c pytorch
 
-pip3 install -r requirements.txt
 # pip install -r requirements.txt
 
-# sudo add-apt-repository -y ppa:jonathonf/python-3.6
+sudo add-apt-repository -y ppa:jonathonf/python-3.6
 
 sudo apt-get update
-# sudo apt-get -y upgrade
-# sudo apt-get install python3.6 -y
-# sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
-# sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
+sudo apt-get -y upgrade
+sudo apt-get install python3.6 -y
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
 
 sudo apt-get install python3-pip python3.6-dev -y
 sudo apt-get install libssl-dev  -y
@@ -40,3 +39,5 @@ curl https://raw.githubusercontent.com/OpenMined/BootstrapNodes/master/bootstrap
 cat bootstrap_nodes | xargs ipfs bootstrap add
 
 ipfs daemon --enable-pubsub-experiment  > ipfs.log 2> ipfs.err &
+
+pip3 install -r requirements.txt
