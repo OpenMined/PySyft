45aa4e9c620cb3463811d6de3a38019fca2e2ebf
Andrew Trask <liamtrask@gmail.com>
2018-02-24 18:03:14 +0000

    updated install script

--- install_scripts/ubuntu_installation.sh
+++ install_scripts/ubuntu_installation.sh
@@ -6,15 +6,6 @@ rm Anaconda3-5.1.0-Linux-x86_64.sh
 
 # pip install -r requirements.txt
 
-sudo add-apt-repository -y ppa:jonathonf/python-3.6
-
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get install python3.6 -y
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
-
-sudo apt-get install python3-pip python3.6-dev -y
 sudo apt-get install libssl-dev  -y
 
 curl -O https://storage.googleapis.com/golang/go1.9.1.linux-amd64.tar.gz
@@ -40,4 +31,14 @@ cat bootstrap_nodes | xargs ipfs bootstrap add
 
 ipfs daemon --enable-pubsub-experiment  > ipfs.log 2> ipfs.err &
 
-pip3 install -r requirements.txt
+sudo add-apt-repository -y ppa:jonathonf/python-3.6
+
+# sudo apt-get update
+# sudo apt-get -y upgrade
+# sudo apt-get install python3.6 -y
+# sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+# sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
+
+# sudo apt-get install python3-pip python3.6-dev -y
+
+# pip3 install -r requirements.txt
