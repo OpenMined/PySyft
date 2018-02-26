0efdd1387677201b8cd8b99a13898fefd81744c0
Justin Patriquin <justinpatriquin@gmail.com>
2018-02-26 17:38:00 -0400

    Pip2 (#78)
    * install updates
    
    * update readme
    
    * make sure install dependencies

--- setup.py
+++ setup.py
@@ -8,24 +8,25 @@ import subprocess
 
 class PostDevelopCommand(develop):
     def run(self):
+        develop.run(self)
+
         # TODO windows
         if platform == 'Darwin':
             subprocess.call('install_scripts/osx_installation.sh', shell=True)
         elif platform == 'Linux':
             subprocess.call('install_scripts/ubuntu_installation.sh', shell=True)
 
-        #develop.run(self)
 
 
 class PostInstallCommand(install):
     def run(self):
+        install.do_egg_install(self)
+
         # TODO windows
         if platform == 'Darwin':
             subprocess.call('install_scripts/osx_installation.sh', shell=True)
         elif platform == 'Linux':
             subprocess.call('install_scripts/ubuntu_installation.sh', shell=True)
-
-        install.run(self)
 
 
 # Utility function to read the README file.
