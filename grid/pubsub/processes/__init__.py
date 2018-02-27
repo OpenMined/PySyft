e14bc2b19abec46351e0c4a019048f5ec6de9fd6
iamtrask <liamtrask@gmail.com>
2018-02-27 12:41:04 +0000

    removed redundant import

--- grid/pubsub/processes/__init__.py
+++ grid/pubsub/processes/__init__.py
@@ -1,3 +1,4 @@
 from . import *
 from . import base
 from . import fit_worker
+from . import listen_for_openmined_nodes
--- grid/pubsub/processes/base.py
+++ grid/pubsub/processes/base.py
@@ -1,5 +1,4 @@
 
-from .base import BaseProcess
 
 class BaseProcess(object):
 	
