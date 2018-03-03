4085b8a6d65070a0ad1eaca667e40b60d765cd5d
iamtrask <liamtrask@gmail.com>
2018-03-03 15:15:37 +0000

    Pretty Print Network Stats (#97)
    * revamped keras interface into an initial client
    
    * updated notebook
    
    * added initial torch service
    
    * initial service for torch
    
    * initial service for torch
    
    * improved logging a bit
    
    * typo
    
    * typo
    
    * missing numpy import
    
    * another bugfix in fit
    
    * testing a theory
    
    * testing a theory
    
    * testing a theory
    
    * testing a theory
    
    * testing a theory
    
    * added init.py
    
    * added init.py
    
    * added init.py
    
    * added init.py
    
    * added first object request logic
    
    * added print statement to help me introspect
    
    * bugfix
    
    * added missing json conversion
    
    * added support for when a tensor is asked for that a worker doesnt have
    
    * forgot parantheses
    
    * bugfix - publish() is in worker not service
    
    * attempt at adding the ability to send objects - before i was just asking for htem
    
    * added logging so i can see what's going on
    
    * bugfix in register_obj - was pointing to the service as the owner isntead of the worker as the owner
    
    * bugfix - tried to json read a dictionary
    
    * thought i already fixed this but
    
    * wrong channel
    
    * got tensor to send over IPFS - trying to stitch up with overriden torch api now
    
    * accidentally pointed to the worker when it should have pointed to the service
    
    * small bugfix
    
    * receive_obj() had a return value - which by default causes the listener to die - which we don't want to do
    
    * updated notebook
    
    * synchronous tensor .get() request on random channel
    
    * synchronous tensor .get() request on random channel
    
    * strange new bug - adding logging to investigate
    
    * strange new bug - adding logging to investigate
    
    * strange new bug - adding logging to investigate
    
    * strange new bug - adding logging to investigate
    
    * strange new bug - adding logging to investigate
    
    * id wasn't being configured correctly
    
    * cleaned up print statements
    
    * it looks like i can now send and receive tensors from a foreign node using pytorch and ipfs
    
    * better __repr__ for remote tensors
    
    * removed redundant listener
    
    * added listen() function and cleaned up notebook
    
    * cleaend notebook
    
    * better backend cleanup with some commenting
    
    * Added logging
    
    * cleaned logging
    
    * prints id when connects to ipfs
    
    * whoami service
    
    * bug in empty function
    
    * forgot to run the service
    
    * forgot to add the file
    
    * bug
    
    * trying out a theory
    
    * trying out a theory
    
    * added response logic for whoami service
    
    * added print statement
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * added a space so that copy pasting is easier
    
    * raw whoami service seems to be working
    
    * cleaned up demo notebook a bit
    
    * updated logging
    
    * fix get stats
    
    * get_network_stats()

--- grid/clients/base.py
+++ grid/clients/base.py
@@ -6,6 +6,7 @@ from ..services.passively_broadcast_membership import PassivelyBroadcastMembersh
 from ..services.listen_for_openmined_nodes import ListenForOpenMinedNodesService
 
 import json
+import time
 
 class BaseClient(base_worker.GridWorker):
 
@@ -25,6 +26,59 @@ class BaseClient(base_worker.GridWorker):
 
         def ret(msg):
             return json.loads(msg['data'])
+
+        return self.request_response(channel=channels.whoami_listener_callback(worker_id),message=[],response_handler=ret)
+
+    def __len__(self):
+        return len(self.get_openmined_nodes())
+
+    def get_network_stats(self,print_stats=True):
+        om_nodes = self.get_openmined_nodes()
+        om_nodes
+
+        stats = list()
+        for n in self.get_openmined_nodes():
+            start = time.time()
+            stat = self.get_stats(n)
+            end = time.time()
+            stat['ping_time'] = end-start
+            stats.append(stat)
+            if(print_stats):
+                print(self.pretty_print_node(stat))
+
+        return stats
+
+    def pretty_print_gpu(self,gpu):
+        return str(gpu['index']) + " : " + gpu['name'] + " : " + str(gpu['memory.used']) + "/" + str(gpu['memory.total'])
+
+    def pretty_print_compute(self,stat):
+
+        wtype = stat['worker_type']
+        ncpu = stat['cpu_num_logical_cores']
+        cpu_load = stat['cpu_processor_percent_utilization']
+        ngpu = len(stat['gpus'])
+        dp = stat['disk_percent']
+        rp = str(100-stat['cpu_ram_percent_available'])[0:4]
+        
+        if(ngpu == 0):
+            gpus = "[]"
+        else:
+            gpus = "["
+            for g in stat['gpus']:
+                gpus += self.pretty_print_gpu(g) + ", "
+            gpus = gpus[:-2] + "]"
+
+        ping = str(stat['ping_time']).split(".")
+        ping = ping[0] + "." + ping[1][0:2]
+
+        
+        return wtype + " - Ping:" + str(ping) + "sec  CPUs:" + str(ncpu) + "  CPU Load:" + str(cpu_load) + "  Disk-util:" + str(dp) + "%" + "  RAM-util:" + str(rp) + "%  GPUs:" + gpus
+
+    def pretty_print_node(self,node):
+
+        if(node['worker_type'] == 'ANCHOR'):
+            node['worker_type'] = ' ANCHOR'
+        return self.pretty_print_compute(node)
 
     def __len__(self):
         return len(self.get_openmined_nodes())
