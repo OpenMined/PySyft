2267ad181cca7fae86177a2000955b37ab394e8f
Justin Patriquin <justinpatriquin@gmail.com>
2018-03-09 11:48:11 -0400

    conda for ubuntu instead of pip (#125)
    * conda for ubuntu instead of pip
    
    * remove pytorch from ubuntu install for now
    
    * permissions
    
    * really permissions
    
    * remove torch vision

---
+++ install_scripts/integration_ubuntu_install.sh
@@ -0,0 +1,3 @@
+sh install_scripts/ubuntu_installation.sh
+
+conda install pytorch-cpu -c pytorch
--- install_scripts/ubuntu_installation.sh
+++ install_scripts/ubuntu_installation.sh
@@ -1,18 +1,18 @@
 sudo apt-get update
 sudo apt-get install curl libssl-dev  -y
 
-curl -O https://storage.googleapis.com/golang/go1.9.1.linux-amd64.tar.gz
-tar -xvf go1.9.1.linux-amd64.tar.gz
-rm go1.9.1.linux-amd64.tar.gz
-sudo mv go /usr/local
+if [ ! -d "$HOME/.ipfs" ]; then
+  curl -O https://storage.googleapis.com/golang/go1.9.1.linux-amd64.tar.gz
+  tar -xvf go1.9.1.linux-amd64.tar.gz
+  rm go1.9.1.linux-amd64.tar.gz
+  sudo mv go /usr/local
 
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
-export PATH=$PATH:/usr/local/go/bin
+  echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
+  export PATH=$PATH:/usr/local/go/bin
 
-echo "export GOPATH=$HOME" >> ~/.profile
-export GOPATH=$HOME
+  echo "export GOPATH=$HOME" >> ~/.profile
+  export GOPATH=$HOME
 
-if [ ! -d "~/.ipfs" ]; then
   go get -u github.com/ipfs/ipfs-update
   ipfs-update install latest
   ipfs init
@@ -21,6 +21,3 @@ fi
 # sudo apt-get install python3-dev # didn't seem to help
 sudo apt-get install build-essential automake pkg-config libtool libffi-dev libgmp-dev -y
 pip install -r requirements.txt
-
-pip3 install http://download.pytorch.org/whl/cpu/torch-0.3.1-cp36-cp36m-linux_x86_64.whl
-pip3 install torchvision
