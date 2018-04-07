fd37083815a067e2e4a201ded3935a9a141c1f92
iamtrask <liamtrask@gmail.com>
2018-04-07 22:53:51 +0100

    init test suite and examples

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+import unittest
+from setuptools import setup, find_packages
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def my_test_suite():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('test', pattern='test_*.py')
+    return test_suite
+
+requirements = read('requirements.txt').split()
+
+setup(
+    name="syft",
+    version="0.1.0",
+    author="Andrew Trask",
+    author_email="contact@openmined.org",
+    description=("A Library for Private, Secure Deep Learning"),
+    license="Apache-2.0",
+    keywords="deep learning artificial intelligence homomorphic encryption",
+    packages=find_packages(exclude=['docs','examples', 'dist']),
+    include_package_data=True,
+    long_description=read('README.md'),
+    url='github.com/OpenMined/PySyft',
+    classifiers=[
+        "Development Status :: 1 - Alpha",
+    ],
+    install_requires=requirements,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'pytest-flake8']
+)
---
+++ test/sample_test.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+class TestSample(TestCase):
+    def test_true(self):
+        assert True
