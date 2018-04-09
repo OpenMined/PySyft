ad6883681eb6875a83bc2873ccc99612a3f9b170
Yann Dupis <yann.dupis@gmail.com>
2018-04-09 15:22:32 -0700

    Version tree node (#177)
    * feat: broadcast and listen existence of children
    
    * bugs fixed, packages, input missing
    
    * Input parameters fixed
    
    * feat: add get_children to model version control demo

--- grid/ipfsapi/version_tree.py
+++ grid/ipfsapi/version_tree.py
@@ -2,8 +2,16 @@
 version-control system, which is structured as a directed in-tree with nodes
 represented by the bytes representation of the VersionTreeNode class. """
 from typing import Optional, Iterator
+from datetime import datetime
+import json
+import numpy as np
+import pickle
+import base64
+from bitcoin import base58
+import threading 
 
 from grid import ipfsapi
+from grid.lib import utils
 
 # TODO: Unit tests.
 # TODO: Do we want to store the hash on the node after it's been committed?
@@ -22,13 +30,24 @@ class VersionTreeNode:
         this node is the root of a version tree. """
         self.contents = contents
         # Convert empty string to None to minimize typing bugs.
+        self.id_hash =  None
         self.parent_hash = parent_hash or None
         self.ipfs_client = ipfs_client
 
-    def commit(self, ipfs_client: ipfsapi.Client = None) -> str:
-        """ Commits the node to the version tree, and returns the
-        UTF-8 multihash representing its IPFS ID"""
-        return (ipfs_client or self.ipfs_client).add_bytes(self.to_bytes())
+    def commit(self, ipfs_client: ipfsapi.Client = None,
+                broadcast = True, 
+                broadcast_period = 1) -> str:
+        """ Commits the node to the version tree,if broadcast set to true, 
+        broadcast child periodically on pubsub and 
+        returns the UTF-8 multihash representing its IPFS ID"""
+            
+        self.id_hash = (ipfs_client or self.ipfs_client).add_bytes(self.to_bytes())
+        # If there parent_hash is set to None it means it's the root of the tree
+        # therefore don't need to broadcast
+        if self.parent_hash is not None and broadcast:
+            self.broadcast_child_periodically(ipfs_client, broadcast_period)
+            
+        return self.id_hash
 
     @classmethod
     def get_node_by_hash(cls,
@@ -86,3 +105,96 @@ class VersionTreeNode:
     def __eq__(self, other):
         return self.contents == other.contents and \
             self.parent_hash == other.parent_hash
+
+    def to_json(self) -> str:
+        """Jsonify attributes of the child that will be 
+        broadcasted on the channel openmined:children_of_<parent_hash>"""
+        child = {'id_hash': self.id_hash, 'parent_hash': self.parent_hash}
+        return json.dumps(child)
+    
+    def broadcast_child_periodically(self, ipfs_client: ipfsapi.Client, 
+                                     broadcast_period = 1):
+        """Broadcast child periodically on openmined:children_of_<parent_hash>
+        to indicate to the parent the existence of the child"""
+        channel = 'openmined:children_of_' + str(self.parent_hash)
+        ipfs_client.pubsub_pub(topic = channel, 
+                               payload=self.to_json(),
+                               stream = True)
+        
+        _args = (ipfs_client,)
+        threading.Timer(broadcast_period, 
+                        self.broadcast_child_periodically, 
+                        args=_args).start()
+    
+    def get_children(
+            self, 
+            parent_hash,
+            ipfs_client: ipfsapi.Client,
+            timeout = 5) -> Iterator["VersionTreeNode"]:
+        """Listen to openmined:children_of_<parent_hash> and 
+        return list of children (VersionTreeNode)"""
+        channel = 'openmined:children_of_' + str(parent_hash)
+        child_messages =  self.listen_to_channel_impl(ipfs_client, 
+                                           channel, 
+                                           self.receive_child, 
+                                           timeout)
+        
+        children_list = [self.get_node_by_hash(child['id_hash'],ipfs_client) 
+                        for child in child_messages]
+    
+        return children_list
+    
+    def receive_child(self, message: str) -> dict:
+        """Extract child attributes from messages
+        published on the channel openmined:children_of_<parent_hash> """
+        msg = utils.unpack(message)
+        child = {}
+        child['id_hash'] = msg['id_hash']
+        child['parent_hash'] = msg['parent_hash']
+        return child
+
+    def listen_to_channel_impl(self,
+                               ipfs_client: ipfsapi.Client,
+                               channel: str,
+                               handle_message: dict,
+                               timeout: int = 5) -> list:
+        """Listen to channel for a certain period of time 
+        and return a list of messages (info extracted based
+        on handle message)"""
+        start_time = datetime.now()
+        time_delta = 0
+        out_list = list()
+
+        new_messages = ipfs_client.pubsub_sub(topic=channel, stream = True)
+
+        # new_messages is a generator which will keep yield new messages until
+        # you return from the loop. If you do return from the loop, we will no
+        # longer be subscribed.
+        for m in new_messages:
+            message = self.decode_message(m)
+            if message is not None:
+                out = handle_message(message)
+                if out is not None and out not in out_list:
+                    out_list.append(out)
+                    
+            time_delta = datetime.now() - start_time   
+            
+            if time_delta.seconds >= timeout:
+                break
+                
+        return out_list
+
+                    
+    def decode_message(self, encoded: dict) -> dict:
+        """Decode message published on pubsub"""
+        if ('from' in encoded):
+            decoded = {}
+            decoded['from'] = base64.standard_b64decode(encoded['from'])
+            decoded['data'] = base64.standard_b64decode(
+                encoded['data']).decode('ascii')
+            decoded['seqno'] = base64.standard_b64decode(encoded['seqno'])
+            decoded['topicIDs'] = encoded['topicIDs']
+            decoded['encoded'] = encoded
+            return decoded
+        else:
+            return None
