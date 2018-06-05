d7e4174f9efded817ea7048699fd18237385aac9
iamtrask <liamtrask@gmail.com>
2018-06-05 15:21:48 +0100

    hook edits

--- docs/conf.py
+++ docs/conf.py
@@ -32,12 +32,12 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.autosummary']
+              'sphinx.ext.todo',
+              'sphinx.ext.coverage',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.githubpages',
+              'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -160,6 +160,3 @@ texinfo_documents = [
      author, 'PySyft', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
