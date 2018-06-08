60af1c34876e0aeda273be97a326dd3c20d97f2c
iamtrask <liamtrask@gmail.com>
2018-06-08 21:56:18 +0100

    syncing so that Jason can help out...

---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+rm -rf build
+python setup.py clean install
+flake8 . --count --exit-zero --max-complexity=11 --max-line-length=100 --statistics
+cd docs
+rm -rf ./_modules
+rm -rf ./_autosummary
+sphinx-apidoc -o ./_modules ../syft
+rm -rf _build
+make html
+cd ../
+
+# uncomment if you want to open the documentation in a webpage
+# open ./docs/_build/html/index.html
