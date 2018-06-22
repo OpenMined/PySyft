c4d2ab44e40e281f1dde6dadbc764e44157769f5
Kevin Mader <kmader@users.noreply.github.com>
2018-06-22 10:11:46 +0200

    Update execute_notebooks.sh
    fixing path

--- scripts/execute_notebooks.sh
+++ scripts/execute_notebooks.sh
@@ -1,5 +1,5 @@
 # Generating notebooks
-for nb in notebooks/*ipynb; do
+for nb in examples/*ipynb; do
     jupyter nbconvert --ExecutePreprocessor.timeout=3600 --execute "$nb" --to markdown |& tee nb_to_md.txt
     traceback=$(grep "Traceback (most recent call last):" nb_to_md.txt)
     if [[ $traceback ]]; then
