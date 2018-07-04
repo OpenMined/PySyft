208d4237c1f5c2d24bb8fb64eb4520679d884efa
jvmancuso <jason@manc.us>
2018-07-04 11:55:50 -0400

    proper abstract base classes

--- syft/core/utils.py
+++ syft/core/utils.py
@@ -2,16 +2,16 @@
 import functools
 
 
-def map_tuple(service, args, func):
-    if service:
-        return tuple(func(service, x) for x in args)
+def map_tuple(hook, args, func):
+    if hook:
+        return tuple(func(hook, x) for x in args)
     else:
         return tuple(func(x) for x in args)
 
 
-def map_dict(service, kwargs, func):
-    if service:
-        return {key: func(service, val) for key, val in kwargs.items()}
+def map_dict(hook, kwargs, func):
+    if hook:
+        return {key: func(hook, val) for key, val in kwargs.items()}
     else:
         return {key: func(val) for key, val in kwargs.items()}
 
