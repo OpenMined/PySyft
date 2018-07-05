fixed precision methods added to method list
Conditions for method_router were changed to be compatible with remote fixed precision tensors and fixed precision ops were added to tensorvar_methods

syft/core/hooks/torch/hook.py
@@ -101,6 +101,7 @@ class TorchHook(BaseHook):
                              torch.IntTensor,
                              torch.LongTensor]
 
+
         # this is the list of torch tensor VARIABLE types that we will override for remote execution
         # Variables are simply tensors that are differentiable (support gradients)
         # Parameters are Variables that are also weights in a neural model
@@ -110,6 +111,12 @@ class TorchHook(BaseHook):
         self.tensorvar_types = self.tensor_types + \
                                [torch.autograd.variable.Variable]
         self.tensorvar_types_strs = [x.__name__ for x in self.tensorvar_types]
+
+        # a list of all methods in fixed precision type which will be overridden for remote execution
+        self.fixed_prec_var_methods = ['fixed_prec_add', 'fixed_prec_mul', 'fixed_prec_sub', 'fixed_prec_div',
+                                   'set_precision', 'fixed_prec_trudiv', 'free_precision',
+                                   '_execute_fixed_precision_call', '_conversion']
+
         self.tensorvar_methods = list(
             set(
                 [method
@@ -117,6 +124,9 @@ class TorchHook(BaseHook):
                  for method in dir(tensorvar)]
             )
         )
+
+        # adding fixed precision methods to the list of overriding methods for remote execution
+        self.tensorvar_methods.extend(self.fixed_prec_var_methods)
 
         # Methods that caused infinite recursion during testing
         # TODO: May want to handle the ones in "exclude" manually at
@@ -168,16 +178,22 @@ class TorchHook(BaseHook):
 
             _method = method(self, *args, **kwargs)
 
-            if hasattr(self, 'is_pointer') and self.is_pointer:
+            if hasattr(self, 'is_pointer') and self.is_pointer and not self.fixed_precision:
 
                 return hook_self._execute_remote_call(_method,
                                                       has_self=True)[0]
-            elif (hasattr(self, 'fixed_precision') and self.fixed_precision):
+            elif (hasattr(self, 'fixed_precision') and self.fixed_precision)\
+                    and not hasattr(self, 'is_pointer'):
 
                 return hook_self._execute_fixed_precision_call(self, _method, args, kwargs)
 
-            else:
+            elif (hasattr(self, 'fixed_precision') and self.fixed_precision)\
+                    and hasattr(self, 'is_pointer'):
 
+                pass
+                #TODO: implementing _execute_remote_fixed_precision_call for remote fixed precision tensors
+
+            else:
                 return hook_self._execute_local_call(self, _method, args, kwargs)
 
         return method_router
@@ -201,6 +217,7 @@ class TorchHook(BaseHook):
             """
 
             part = func(*args, **kwargs)
+
 
             _res = hook_self._execute_remote_call(
                 part, has_self=False)
@@ -451,6 +468,7 @@ class TorchHook(BaseHook):
                                                              torch_type,
                                                              var_data,
                                                              var_grad)
+
                 return pointer, has_remote, multiple_owners
 
         elif (has_remote and multiple_owners):
@@ -461,6 +479,14 @@ class TorchHook(BaseHook):
         #     raise NotImplementedError("""SOMETHING WENT WRONG: This should be a local call""")
 
         return (None, has_remote, multiple_owners)
+
+    def _execute_remote_fixed_precision_call(self, _method):
+        '''
+        This function is responsible for overloading remote
+        fixed precision TENSORS and their methods.
+        '''
+        #TODO: writing a function resposible for overloading remote fixed precision tensors and their methods
+        pass
 
     def _get_tensorvars(self, command):
         """Returns all Tensors and Variables in the args/kwargs of the command"""
@@ -739,6 +765,8 @@ class TorchHook(BaseHook):
                 raise NotImplementedError('Only able to get_ tensors belonging \
                                             to a single worker right now.')
             if hook_self.local_worker.id in self.owners:
+
+
                 return self
 
             _out = hook_self.local_worker.request_obj(obj_id=self.id,
@@ -761,6 +789,7 @@ class TorchHook(BaseHook):
             self = hook_self.local_worker.register_object(hook_self.local_worker,
                                                           _os,
                                                           id=self.id, owners=[_id])
+
 
             return self
 
@@ -1068,3 +1097,4 @@ class TorchHook(BaseHook):
         torch.nn.Module.get = module_get_
 
     # ######## END torch.nn.Module hooking #########
+