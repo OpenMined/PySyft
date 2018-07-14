fd94ac479a6cec3d51afebad74e8b4d9845f06a8
bartimaeus12 <sharat12p@gmail.com>
2018-07-14 11:17:02 +0530

    changed optim.step test

--- test/torch_test.py
+++ test/torch_test.py
@@ -490,33 +490,36 @@ class TestTorchVariable(TestCase):
 
     def test_remote_optim_step(self):
 
+        torch.manual_seed(42)
         hook = TorchHook(verbose=False)
         local = hook.local_worker
         local.verbose = False
         remote = VirtualWorker(id=1, hook=hook, verbose=False)
         local.add_worker(remote)
+        param = []
 
         data = Var(torch.FloatTensor([[0, 0], [0, 1], [1, 0], [1, 1]])).send(remote)
         target = Var(torch.FloatTensor([[0], [0], [1], [1]])).send(remote)
 
-        param = []
+        model = nn.Linear(2, 1)
+        opt = optim.SGD(params=model.parameters(), lr=0.1)
 
-        for i in range(2):
-            torch.manual_seed(42)
-            model = nn.Linear(2, 1)
-            opt = optim.SGD(params=model.parameters(), lr=0.1)
-            model.send_(remote)
-            model.zero_grad()
-            pred = model(data)
-            loss = ((pred - target) ** 2).sum()
-            loss.backward()
-            opt.step()
-            for i in model.parameters():
-                param.append(i.get())
-            model.reset_parameters()
+        for i in model.parameters():
+            param.append(i[:])
 
-        assert (param[0] == param[2]).all()
-        assert (param[1] == param[3]).all()
+        model.send_(remote)
+        model.zero_grad()
+        pred = model(data)
+        loss = ((pred - target) ** 2).sum()
+        loss.backward()
+        opt.step()
+
+        model.get_()
+        for i in model.parameters():
+            param.append(i[:])
+
+        assert not ((param[0] == param[2]).all())
+        assert not ((param[1] == param[3]).all())
 
     def test_federated_learning(self):
 
