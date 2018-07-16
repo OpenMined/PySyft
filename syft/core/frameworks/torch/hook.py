6c20bbaaf28b93b3dd6b472d896d16ad04fedf62
iamtrask <liamtrask@gmail.com>
2018-07-16 16:29:52 +0100

    strange temporary fix

--- syft/core/frameworks/torch/hook.py
+++ syft/core/frameworks/torch/hook.py
@@ -307,6 +307,7 @@ class TorchHook(object):
             the call locally. If self is a remote tensor, it
             executes a call to a remote worker.
             """
+            print(attr)
             results = list()
             if(call_native):
                 result = getattr(self.child, "native_"+attr)(*args, **kwargs)
