69b6ed06aacdb42f9fa5edffc6ff9925c4508ac5
iamtrask <liamtrask@gmail.com>
2018-08-30 13:23:09 +0100

    more merging

--- syft/core/__init__.py
+++ syft/core/__init__.py
@@ -1,14 +1,7 @@
 """Some core imports..."""
-<<<<<<< HEAD
-from . import hooks
-from . import utils
-from . import workers
-from .hooks import torch
-=======
 
 from . import workers
 from . import utils
 from . import frameworks
->>>>>>> pointers_are_tensors
 
 __all__ = ['workers', 'utils', 'frameworks']
--- test/core/hooks_test.py
+++ test/core/hooks_test.py
@@ -1,39 +1,28 @@
-# from unittest import TestCase
-# import torch
-
-# from syft.core.hooks import TorchHook
-
-
-# class TestTorchHook(TestCase):
-
-#     def test_types_guard(self):
-#         hook = TorchHook(verbose=False)
-
-#         with self.assertRaises(Exception) as context:
-#             # can't serialize an int, so should raise a TypError
-#             obj_type = hook.types_guard(3)
-
-#         with self.assertRaises(Exception) as context:
-
-#             # can't serialize a random string as a type, so should raise a TypError
-#             obj_type = hook.guard.types_guard("asdf")
-
-#             assert obj_type == obj_type
-
-#             self.assertTrue('TypeError' in context.exception)
-
-<<<<<<< HEAD
-        tensor_types = {
-            'torch.FloatTensor': torch.FloatTensor,
-            'torch.DoubleTensor': torch.DoubleTensor,
-            'torch.HalfTensor': torch.HalfTensor,
-            'torch.ByteTensor': torch.ByteTensor,
-            'torch.CharTensor': torch.CharTensor,
-            'torch.ShortTensor': torch.ShortTensor,
-            'torch.IntTensor': torch.IntTensor,
-            'torch.LongTensor': torch.LongTensor,
-        }
-=======
+# # from unittest import TestCase
+# # import torch
+#
+# # from syft.core.hooks import TorchHook
+#
+#
+# # class TestTorchHook(TestCase):
+#
+# #     def test_types_guard(self):
+# #         hook = TorchHook(verbose=False)
+#
+# #         with self.assertRaises(Exception) as context:
+# #             # can't serialize an int, so should raise a TypError
+# #             obj_type = hook.types_guard(3)
+#
+# #         with self.assertRaises(Exception) as context:
+#
+# #             # can't serialize a random string as a type, so should raise a TypError
+# #             obj_type = hook.guard.types_guard("asdf")
+#
+# #             assert obj_type == obj_type
+#
+# #             self.assertTrue('TypeError' in context.exception)
+#
+# <<<<<<< HEAD
 #         tensor_types = {
 #             'torch.FloatTensor': torch.FloatTensor,
 #             'torch.DoubleTensor': torch.DoubleTensor,
@@ -42,9 +31,20 @@
 #             'torch.CharTensor': torch.CharTensor,
 #             'torch.ShortTensor': torch.ShortTensor,
 #             'torch.IntTensor': torch.IntTensor,
-#             'torch.LongTensor': torch.LongTensor
+#             'torch.LongTensor': torch.LongTensor,
 #         }
->>>>>>> pointers_are_tensors
-
-#         for k, v in tensor_types.items():
-#             assert hook.guard.types_guard(k) == v
+# =======
+# #         tensor_types = {
+# #             'torch.FloatTensor': torch.FloatTensor,
+# #             'torch.DoubleTensor': torch.DoubleTensor,
+# #             'torch.HalfTensor': torch.HalfTensor,
+# #             'torch.ByteTensor': torch.ByteTensor,
+# #             'torch.CharTensor': torch.CharTensor,
+# #             'torch.ShortTensor': torch.ShortTensor,
+# #             'torch.IntTensor': torch.IntTensor,
+# #             'torch.LongTensor': torch.LongTensor
+# #         }
+# >>>>>>> pointers_are_tensors
+#
+# #         for k, v in tensor_types.items():
+# #             assert hook.guard.types_guard(k) == v
