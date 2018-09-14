0a506107ff57dc2caff0cdfd20880d25446584eb
Theo <theo.leffyr@gmail.com>
2018-09-14 17:50:37 +0100

    Add tests for MPC sum and mult

--- test/torch_test.py
+++ test/torch_test.py
@@ -9,6 +9,9 @@ import syft as sy
 from syft.core import utils
 from syft.core.frameworks.torch import utils as torch_utils
 from syft.core.frameworks.torch import encode
+from syft.core.frameworks.torch.mpc_tensor import _MPCTensor
+from syft.core.frameworks.torch.tensor import _GeneralizedPointerTensor
+from syft.mpc import spdz
 import torch
 import torch.nn.functional as F
 from torch.autograd import Variable as Var
@@ -193,7 +196,6 @@ class TestChainTensor(TestCase):
 
         target = sy._PlusIsMinusTensor().on(torch.FloatTensor([1, 1]))
         assert torch.equal(x.grad.data, target)
-
 
 
 class TestTorchTensor(TestCase):
@@ -928,6 +930,54 @@ class TestTorchVariable(TestCase):
         assert (torch.equal(z.get(), sy.Variable(torch.ByteTensor([1, 1, 1]))))
         z = torch.ge(x, y)
         assert (torch.equal(z.get(), sy.Variable(torch.ByteTensor([1, 1, 1]))))
+
+
+class TestMPCTensor(TestCase):
+
+    def generate_mpc_number_pair(self, n1, n2):
+        x = torch.LongTensor([n1])
+        y = torch.LongTensor([n2])
+        x_enc = spdz.encode(x)
+        y_enc = spdz.encode(y)
+        x_alice, x_bob = spdz.share(x_enc)
+        y_alice, y_bob = spdz.share(y_enc)
+        x_alice.send(alice)
+        x_bob.send(bob)
+        y_alice.send(alice)
+        y_bob.send(bob)
+        x_pointer_tensor_dict = {alice: x_alice.child, bob: x_bob.child}
+        y_pointer_tensor_dict = {alice: y_alice.child, bob: y_bob.child}
+        x_gp = _GeneralizedPointerTensor(x_pointer_tensor_dict).on(x)
+        y_gp = _GeneralizedPointerTensor(y_pointer_tensor_dict).on(y)
+        x_mpc = _MPCTensor(x_gp)
+        y_mpc = _MPCTensor(y_gp)
+        return x_mpc, y_mpc
+
+    def mpc_sum(self, n1, n2):
+        x_mpc, y_mpc = self.generate_mpc_number_pair(n1, n2)
+        sum_mpc = x_mpc + y_mpc
+        sum_mpc = sum_mpc.get()
+        assert torch.eq(sum_mpc, torch.LongTensor([n1 + n2])).all()
+
+    def test_mpc_sum(self):
+        self.mpc_sum(3, 5)
+        self.mpc_sum(4, 0)
+        self.mpc_sum(5, -5)
+        # self.mpc_sum(3, -5) TODO: Future work: how to handle gracefully minus numbers
+        self.mpc_sum(2 ** 24, 2 ** 12)
+
+    def mpc_mul(self, n1, n2):
+        x_mpc, y_mpc = self.generate_mpc_number_pair(n1, n2)
+        mul_mpc = x_mpc * y_mpc
+        mul_mpc = mul_mpc.get()
+        assert torch.eq(mul_mpc, torch.LongTensor([n1 * n2])).all(), (mul_mpc, 'should be', torch.LongTensor([n1 * n2]))
+
+    def test_mpc_mul(self):
+        self.mpc_mul(3, 5)
+        self.mpc_mul(4, 0)
+        #self.mpc_mul(5, -5) TODO: Future work: how to handle gracefully minus numbers
+        self.mpc_mul(3, 5)
+        self.mpc_mul(2 ** 12, 2 ** 12)
 
 
 if __name__ == '__main__':
