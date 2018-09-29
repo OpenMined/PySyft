6c9d75425f4ef0b908595ed0584a410d6545e13b
iamtrask <liamtrask@gmail.com>
2018-09-29 22:19:10 +0100

    encrypted relu works

--- syft/mpc/securenn.py
+++ syft/mpc/securenn.py
@@ -55,9 +55,6 @@ def flip(x, dim):
 
 
 def private_compare(x, r, BETA, j, alice, bob):
-    l = Q_BITS
-
-    workers = (alice, bob)
 
     t = (r + 1) % 2 ** l
 
@@ -99,13 +96,12 @@ def private_compare(x, r, BETA, j, alice, bob):
     return result
 
 
-def msb(a, alice, bob):
-    a = a + 0
-    a_sh = a.share(bob, alice)
+def msb(a_sh, alice, bob):
+    # a_sh = a.share(bob, alice)
 
     # 1)
 
-    x = torch.LongTensor(a.get_shape()).random_(L - 1)
+    x = torch.LongTensor(a_sh.get_shape()).random_(L - 1)
     x_bit = decompose(x)
     x_sh = x.share(bob, alice)
     x_bit_0 = x_bit[..., -1:]  # pretty sure decompose is backwards...
@@ -127,7 +123,7 @@ def msb(a, alice, bob):
     j_0 = j[..., -1]
 
     # 4)
-    BETA = (torch.rand(a.get_shape()) > 0.5).long().send(bob, alice)
+    BETA = (torch.rand(a_sh.get_shape()) > 0.5).long().send(bob, alice)
     BETA_prime = private_compare(x_bit_sh,
                                  r,
                                  BETA=BETA,
@@ -152,121 +148,12 @@ def msb(a, alice, bob):
 
     return a
 
-# def msb(x, workers):
-#     """
-#     computes the most significant bit of a shared input tensor
-#     uses the fact that msb(x) = lsb(2x) in an odd ring,
-#     so coerces the shares to an odd ring if needed (FIXME)
-#     """
-#     if L % 2 != 1:
-#         x = share_convert(x)
-#     return lsb(2 * x, workers)
+def relu_deriv(a_sh):
+    return msb(a_sh, *list(a_sh.child.shares.child.pointer_tensor_dict.keys()))
 
-
-def lsb(y, workers):
-    """
-    computes the least significant bit of a shared input tensor
-    """
-    # happens on crypto producer
-    xcrypt = random_as(y)
-    xbits = decompose(xcrypt)
-    xlsb = xbits[..., 0]
-    beta = random_as(y, mod=2)
-    # share x, xlsb, and xbits here, send to the 2 parties
-
-
-    r = y + x
-    r.get()
-    rbits = decompose(r)
-    rlsb = rbits[..., 0]
-    r = r.share(*workers)  # TODO: remove this line when public-private addition works
-    beta_prime = private_compare(xbits, r, beta)
-
-    gamma = xor(beta, beta_prime)
-    delta = xor(xlsb, rlsb)
-    alpha = xor(gamma, delta)
-    u = generate_zero_shares_communication(*workers, *alpha.get_shape())
-    return alpha + u
-
-
-def relu(x, workers):
-    return x * nonnegative(x, workers)
-
-
-def nonnegative(a, workers):
-    c = 2 * a
-    alpha = msb(c)
-    gamma = nope(alpha)
-    u = generate_zero_shares_communication(*workers, *gamma.get_shape())
-    return gamma + u
-
+def relu(a):
+    return a * relu_deriv(a)
 
 def random_as(tensor, mod=L):
     r = torch.LongTensor(tensor.shape).random_(mod)
     return r.type_as(tensor)
-
-
-def xor(x, y):
-    return x + y - 2 * x * y
-
-
-def nope(x):
-    return 1 - x
-
-
-def get_wrap(x, y, mod=L):
-    # FIXME: the conditional on the right should include a plaintext add of x and y,
-    # not an MPC add since the > test needs to be done without a modulus
-    # needed for share_convert
-    return x + y, x + y > mod
-
-
-def share_convert(x):
-    # FIXME: implement share convert protocol
-    raise NotImplementedError("Share convert protocol unfinished -- Ring modulus needs to be odd.")
-
-
-def _pc_beta0(x, r):
-    # note x and r are both binary tensors,
-    # and dim -1 contains their bits
-    # x should be shared, r should be public
-    w = xor(x, r)
-    z = r  - (x - 1)
-    w_sum = torch.zeros(*w.get_shape()).type_as(w)
-    for i in range(Q_BITS - 2, -1, -1):
-        w_sum[..., i] = w[..., (i + 1):].sum(dim=-1, keepdim=False)[0]
-    c = z + w_sum
-    return c
-
-
-def _pc_beta1(x, t):
-    w = xor(x, t)
-    z = (x + 1) - t
-    w_sum = torch.zeros(*w.get_shape()).type_as(w)
-    for i in range(Q_BITS - 2, -1, -1):
-        w_sum[..., i] = w[..., (i + 1):].sum(dim=-1, keepdim=False)[0]
-    c = z + w_sum
-    return c
-
-
-def _pc_else(workers, *sizes):
-    print('pcelse sz',*sizes)
-    u = generate_zero_shares_communication(*workers, *sizes)
-    print('beepbpp')
-    chain_print(u)
-    u_ptrdict = get_ptrdict(u)
-    (w0, u0), (w1, u1) = u_ptrdict.items()
-    print('BOOOP')
-    chain_print(u0)
-    c0 = torch.zeros(*u.get_shape()).long() # c0 = u0 * 0
-    c1 = torch.zeros(*u.get_shape()).long()
-    for i in range(Q_BITS - 2, -1, -1):
-        if i == 0:
-            c0[..., i] = -1 * u0
-            c1[..., i] = u1
-        c0[..., i] = u0 + 1
-        c1[..., i] = -1 * u1
-    ptr_dict = {w0:c0, w1:c1}
-    c_gp = _GeneralizedPointerTensor(ptr_dict, torch_type='syft.LongTensor').wrap(True)
-    c = _SPDZTensor(x_gp, torch_type='syft.LongTensor').wrap(True)
-    return c
