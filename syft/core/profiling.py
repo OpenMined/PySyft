71628c215a01775ca570c2c5c48b79e0816ddc38
Lukasz Kozuchowski <Lukasz.Kozuchowski@gmail.com>
2018-10-18 15:19:04 +0200

    Reformat with black

--- syft/core/profiling.py
+++ syft/core/profiling.py
@@ -5,8 +5,8 @@ from functools import wraps
 
 PROFILE_MODE = True
 
-SEND_MSG_STATS_LOG = 'send_msg_profiling.log'
-LOGFILE_LINE_FORMAT = '''{}\tFrom: {}\tTo: {}\ttype: {}\t{:.2f} ms\ttotal calls: {}\n'''
+SEND_MSG_STATS_LOG = "send_msg_profiling.log"
+LOGFILE_LINE_FORMAT = "{}\tFrom: {}\tTo: {}\ttype: {}\t{:.2f} ms\ttotal calls: {}\n"
 
 # how many milliseconds there are in one second
 MS_IN_S = 1000
@@ -17,21 +17,24 @@ def save_send_msg_stats(prof, *args, **kwargs):
     Saves _send_msg profiling information in SEND_MSG_STATS_LOG file.
     """
 
-    sender = kwargs.get('self', args[0])
-    encoded_message_wrapper = kwargs.get('message_json', args[1])
-    recipient = kwargs.get('recipient', args[2])
+    sender = kwargs.get("self", args[0])
+    encoded_message_wrapper = kwargs.get("message_json", args[1])
+    recipient = kwargs.get("recipient", args[2])
 
-    timestamp = datetime.now().strftime('%Y-%m-%d %T')
+    timestamp = datetime.now().strftime("%Y-%m-%d %T")
     message_wrapper = sender.decode_msg(encoded_message_wrapper)
     stats = pstats.Stats(prof)
 
-    new_logfile_line = LOGFILE_LINE_FORMAT.format(timestamp,
-                                                  sender.id, recipient.id,
-                                                  message_wrapper['type'],
-                                                  stats.total_tt * MS_IN_S,
-                                                  stats.total_calls)
+    new_logfile_line = LOGFILE_LINE_FORMAT.format(
+        timestamp,
+        sender.id,
+        recipient.id,
+        message_wrapper["type"],
+        stats.total_tt * MS_IN_S,
+        stats.total_calls,
+    )
 
-    with open(SEND_MSG_STATS_LOG, 'a+') as logfile:
+    with open(SEND_MSG_STATS_LOG, "a+") as logfile:
         logfile.write(new_logfile_line)
 
 
@@ -56,6 +59,7 @@ def profile(profile_processing_fun):
             prof.disable()
             profile_processing_fun(prof, *args, **kwargs)
             return res
+
         return wrapper
 
     return decorator
