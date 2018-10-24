7b482f7b903d66c90cff632fbff98ac26acacded
NiVeK92 <Ztear@web.de>
2018-10-24 12:18:45 +0200

    Wrong import path fixed.

--- syft/core/frameworks/__init__.py
+++ syft/core/frameworks/__init__.py
@@ -1,3 +1,3 @@
-from syft.core import torch, tensorflow, numpy, encode, pandas
+from syft.core.frameworks import torch, tensorflow, numpy, encode, pandas
 
 __all__ = ["torch", "tensorflow", "numpy", "encode", "pandas"]
