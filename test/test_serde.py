9e02532bfb17dff351bb96f71e6bfd099f9139bb
Bobby Wagner <raw141@case.edu>
2018-11-13 16:28:14 -0500

    updated tests for new serde logic

--- test/test_serde.py
+++ test/test_serde.py
@@ -1,10 +1,9 @@
 import syft
 from unittest import TestCase
 
+
 class TupleSerde(TestCase):
-
     def test_tuple_serialize(self):
-        input = ('hello', 'world')
-        target = ['hello', 'world']
-        assert syft.serde.simplify(input) == target
-
+        input = ("hello", "world")
+        target = ("hello", "world")
+        assert syft.serde._simplify(input) == target
