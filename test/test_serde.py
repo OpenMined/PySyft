23eea4e25f1f74d9fd625a423fab4634b5e7ec33
Bobby Wagner <raw141@case.edu>
2018-11-13 17:08:45 -0500

    added string test and renamed tests to simplify

--- test/test_serde.py
+++ test/test_serde.py
@@ -1,29 +1,34 @@
-import syft
+from syft.serde import _simplify
 from unittest import TestCase
 
 
 class TupleSerde(TestCase):
-    def test_tuple_serialize(self):
+    def test_tuple_simplify(self):
         input = ("hello", "world")
         target = ("hello", "world")
-        assert syft.serde._simplify(input) == target
+        assert _simplify(input) == target
 
-    def test_list_serialize(self):
+    def test_list_simplify(self):
         input = ["hello", "world"]
         target = ["hello", "world"]
-        assert syft.serde._simplify(input) == target
+        assert _simplify(input) == target
 
-    def test_set_serialize(self):
+    def test_set_simplify(self):
         input = set(["hello", "world"])
         target = set(["hello", "world"])
-        assert syft.serde._simplify(input) == target
+        assert _simplify(input) == target
 
-    def test_float_serialize(self):
+    def test_float_simplify(self):
         input = 5.6
         target = 5.6
-        assert syft.serde._simplify(input) == target
+        assert _simplify(input) == target
 
-    def test_int_serialize(self):
+    def test_int_simplify(self):
         input = 5
         target = 5
-        assert syft.serde._simplify(input) == target
+        assert _simplify(input) == target
+
+    def test_string_simplify(self):
+        input = "hello"
+        target = "hello"
+        assert _simplify(input) == target
