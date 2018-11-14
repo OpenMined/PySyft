8fea67032d82a55ab4e09e37aca099caf28fde1b
Philipp Schelske <philipp.schelske@gmail.com>
2018-11-14 18:56:09 +0100

    Issue #1666

--- test/test_serde.py
+++ test/test_serde.py
@@ -1,21 +1,24 @@
-from syft.serde import _simplify
+from syft.serde import _simplify, serialize, deserialize
 from unittest import TestCase
+from torch import tensor
+import numpy
+import msgpack
 
 
 class TupleSerde(TestCase):
     def test_tuple_simplify(self):
         input = ("hello", "world")
-        target = (1, ("hello", "world"))
+        target = [1, ("hello", "world")]
         assert _simplify(input) == target
 
     def test_list_simplify(self):
         input = ["hello", "world"]
-        target = (2, ["hello", "world"])
+        target = [2, ["hello", "world"]]
         assert _simplify(input) == target
 
     def test_set_simplify(self):
         input = set(["hello", "world"])
-        target = (3, set(["hello", "world"]))
+        target = [3, set(["hello", "world"])]
         assert _simplify(input) == target
 
     def test_float_simplify(self):
@@ -35,5 +38,17 @@ class TupleSerde(TestCase):
 
     def test_dict_simplify(self):
         input = {"hello": "world"}
-        target = (4, {"hello": "world"})
+        target = [4, {"hello": "world"}]
         assert _simplify(input) == target
+
+    def test_torch_tensor_serde(self):
+        t = tensor(numpy.random.random((100, 100)))
+        t_serialized = serialize(t, compress=False)
+        t_serialized_deserialized = deserialize(t_serialized, compressed=False)
+        assert (t == t_serialized_deserialized).all()
+
+    def test_tuple_serde(self):
+        tuple = (1,2)
+        tuple_serialized = serialize(tuple,compress=False)
+        tuple_deserialized = deserialize(tuple_serialized, compressed=False)
+        assert tuple == tuple_deserialized
