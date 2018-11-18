328e843be1d16f2b00498635f2a06518f80f63b7
Theo <theo.leffyr@gmail.com>
2018-11-18 18:34:45 +0100

    Add PointerTensor and create_pointer method

---
+++ syft/frameworks/torch/tensors/native.py
@@ -0,0 +1,58 @@
+import random
+
+from syft.frameworks.torch.tensors import PointerTensor
+
+
+class TorchTensor:
+    """
+    This tensor is simply a more convenient way to add custom functions to
+    all Torch tensor types.
+    """
+
+    def __init__(self):
+        self.id = None
+        self.owner = None
+
+    def create_pointer(
+        self,
+        location=None,
+        id_at_location=None,
+        register=False,
+        owner=None,
+        ptr_id=None,
+    ):
+
+        if owner is None:
+            owner = self.owner
+
+        if location is None:
+            location = self.owner.id
+
+        owner = self.owner.get_worker(owner)
+        location = self.owner.get_worker(location)
+
+        if id_at_location is None:
+            id_at_location = self.id
+
+        if ptr_id is None:
+            if location != self.owner.id:
+                ptr_id = self.id
+            else:
+                ptr_id = int(10e10 * random.random())
+
+        # previous_pointer = owner.get_pointer_to(location, id_at_location)
+        previous_pointer = None
+
+        if previous_pointer is None:
+            ptr = PointerTensor(
+                parent=self,
+                location=location,
+                id_at_location=id_at_location,
+                register=register,
+                owner=owner,
+                id=ptr_id,
+            )
+        else:
+            ptr = previous_pointer
+
+        return ptr
---
+++ syft/frameworks/torch/tensors/pointer.py
@@ -0,0 +1,29 @@
+from .abstract import AbstractTensor
+
+
+class PointerTensor(AbstractTensor):
+    def __init__(
+        self,
+        parent=None,
+        location=None,
+        id_at_location=None,
+        register=None,
+        owner=None,
+        id=None,
+    ):
+        self.location = location
+        self.id_at_location = id_at_location
+        self.owner = owner
+        self.id = id
+
+    def __str__(self):
+        type_name = type(self).__name__
+        return (
+            f"["
+            f"{type_name} - "
+            f"id:{self.id} "
+            f"owner:{self.owner.id} "
+            f"loc:{self.location.id} "
+            f"id@loc:{self.id_at_location}"
+            f"]"
+        )
