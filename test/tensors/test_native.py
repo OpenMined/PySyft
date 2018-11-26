8305db60ab8ec756d939b3f492f3065783bf5c24
NiVeK92 <Ztear@web.de>
2018-11-26 17:22:31 +0100

    Replaced TorchTensor initialization with torch tensor and refactored shared variables into setUp method

--- test/tensors/test_native.py
+++ test/tensors/test_native.py
@@ -1,5 +1,4 @@
 from syft.frameworks.torch.hook import TorchHook
-from syft.frameworks.torch.tensors import TorchTensor
 from syft.frameworks.torch.tensors import PointerTensor
 from syft.workers.virtual import VirtualWorker
 from unittest import TestCase
@@ -7,57 +6,53 @@ import torch
 
 
 class TestTorchTensor(TestCase):
-    def test_owner_default(self):
-        hook = TorchHook()
-        tensor = TorchTensor()
+    def setUp(self):
+        self.hook = TorchHook(torch)
+        self.bob = VirtualWorker()
 
-        assert tensor.owner == hook.local_worker
+    def test_owner_default(self):
+        tensor = torch.Tensor([1, 2, 3, 4, 5])
+
+        assert tensor.owner == self.hook.local_worker
 
     def test_create_pointer(self):
-        hook = TorchHook()
-        tensor = TorchTensor()
-        bob = VirtualWorker()
+        tensor = torch.Tensor([1, 2, 3, 4, 5])
 
         ptr = tensor.create_pointer(
-            location=bob, id_at_location=1, register=False, owner=hook.local_worker, ptr_id=2
+            location=self.bob,
+            id_at_location=1,
+            register=False,
+            owner=self.hook.local_worker,
+            ptr_id=2,
         )
 
-        assert ptr.owner == hook.local_worker
-        assert ptr.location == bob
+        assert ptr.owner == self.hook.local_worker
+        assert ptr.location == self.bob
         assert ptr.id_at_location == 1
-        assert ptr.ptr_id == 2
+        assert ptr.id == 2
 
     def test_create_pointer_defaults(self):
-        tensor = TorchTensor()
-        bob = VirtualWorker()
+        tensor = torch.Tensor([1, 2, 3, 4, 5])
 
-        ptr = tensor.create_pointer(location=bob)
+        ptr = tensor.create_pointer(location=self.bob)
 
         assert ptr.owner == tensor.owner
-        assert ptr.location == bob
+        assert ptr.location == self.bob
 
     def test_send(self):
-        hook = TorchHook()  # noqa
-
-        bob = VirtualWorker()
-
         tensor = torch.rand(5, 3)
         tensor.id = 1
 
-        pointer = tensor.send(bob)
+        pointer = tensor.send(self.bob)
 
         assert type(pointer) == PointerTensor
-        assert bob.get_obj(1) == tensor
+        assert self.bob.get_obj(1) == tensor
 
     def test_get(self):
-        hook = TorchHook()  # noqa
-
-        bob = VirtualWorker()
-
         tensor = torch.rand(5, 3)
         tensor.id = 1
 
-        pointer = tensor.send(bob)
+        pointer = tensor.send(self.bob)
 
         assert type(pointer) == PointerTensor
         assert pointer.get() == tensor
