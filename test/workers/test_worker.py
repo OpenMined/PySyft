4a8ec3e9853ac6969321777a38aa67e1e5ea4d6f
Bobby Wagner <raw141@case.edu>
2018-12-24 16:12:28 -0500

    Refactored tests to use pytest style and only create hooks and workers once

--- test/workers/test_worker.py
+++ test/workers/test_worker.py
@@ -1,18 +1,15 @@
-from unittest import TestCase
-
 from syft.workers import VirtualWorker
 
 
-class TestHook(TestCase):
-    def test___init__(self):
-        alice = VirtualWorker(id="alice")
-        bob = VirtualWorker(id="bob", known_workers={alice.id: alice})
-        charlie = VirtualWorker(id="charlie")
+def test___init__():
+    alice = VirtualWorker(id="alice")
+    bob = VirtualWorker(id="bob", known_workers={alice.id: alice})
+    charlie = VirtualWorker(id="charlie")
 
-        assert bob.get_worker("alice").id == alice.id
-        assert bob.get_worker(alice).id == alice.id
-        assert bob.get_worker(charlie).id == charlie.id
+    assert bob.get_worker("alice").id == alice.id
+    assert bob.get_worker(alice).id == alice.id
+    assert bob.get_worker(charlie).id == charlie.id
 
-        bob.get_worker("the_unknown_worker")
+    bob.get_worker("the_unknown_worker")
 
-        bob.add_worker(alice)
+    bob.add_worker(alice)
