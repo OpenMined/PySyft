0c8e668f34cd293af4f76f76856847907f29c3c3
akki2825 <akhilesh.rocking@gmail.com>
2019-02-01 23:46:29 +0530

    edit(test/torch/tensors/test_variable.py): modified to not use class structure

--- test/torch/tensors/test_variable.py
+++ test/torch/tensors/test_variable.py
@@ -7,49 +7,26 @@ import random
 
 import torch
 import syft as sy
+
 from syft.frameworks.torch.tensors import LoggingTensor
 
 
-class TestVariables(object):
-    def setUp(self):
-        hook = sy.TorchHook(torch, verbose=True)
+def test_gradient_serde():
 
-        self.me = hook.local_worker
-        self.me.is_client_worker = True
+    # create a tensor
+    x = torch.tensor([1, 2, 3, 4.0], requires_grad=True)
 
-        instance_id = str(int(10e10 * random.random()))
-        bob = sy.VirtualWorker(id=f"bob{instance_id}", hook=hook, is_client_worker=False)
-        alice = sy.VirtualWorker(id=f"alice{instance_id}", hook=hook, is_client_worker=False)
-        james = sy.VirtualWorker(id=f"james{instance_id}", hook=hook, is_client_worker=False)
+    # create gradient on tensor
+    x.sum().backward(torch.ones(1))
 
-        bob.add_workers([alice, james])
-        alice.add_workers([bob, james])
-        james.add_workers([bob, alice])
+    # save gradient
+    orig_grad = x.grad
 
-        self.hook = hook
+    # serialize
+    blob = sy.serde.serialize(x)
 
-        self.bob = bob
-        self.alice = alice
-        self.james = james
+    # deserialize
+    t = sy.serde.deserialize(blob)
 
-    def test_gradient_serde(self):
-
-        self.setUp()
-
-        # create a tensor
-        x = torch.tensor([1, 2, 3, 4.0], requires_grad=True)
-
-        # create gradient on tensor
-        x.sum().backward(torch.ones(1))
-
-        # save gradient
-        orig_grad = x.grad
-
-        # serialize
-        blob = sy.serde.serialize(x)
-
-        # deserialize
-        t = sy.serde.deserialize(blob)
-
-        # check that gradient was properly serde
-        assert (t.grad == orig_grad).all()
+    # check that gradient was properly serde
+    assert (t.grad == orig_grad).all()
