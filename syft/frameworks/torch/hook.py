fa92b2e87dbad23aedfedf7ad3bb00d4d53ef6ea
iamtrask <liamtrask@gmail.com>
2019-02-01 19:36:11 +0000

    fixed todo

--- syft/frameworks/torch/hook.py
+++ syft/frameworks/torch/hook.py
@@ -308,9 +308,6 @@ class TorchHook:
                 setattr(torch_module, func, new_func)
 
         torch_modules = syft.torch.torch_modules
-        # torch_modules = {"torch.nn.functional": self.torch.nn.functional,
-                         # "torch": self.torch}
-        # TODO Replace with syft.torch.torch_modules when hooking 'torch' will not break msgpack
 
         for module_name, torch_module in torch_modules.items():
             for func in dir(torch_module):
