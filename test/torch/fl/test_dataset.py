a8ae9e82da7820c34df00ee3cfd1ce0811e7b1e4
iamtrask <liamtrask@gmail.com>
2019-02-04 03:41:34 +0000

    black

--- test/torch/fl/test_dataset.py
+++ test/torch/fl/test_dataset.py
@@ -9,11 +9,11 @@ def test_federated_dataset():
     boston_data, _ = grid.search("#boston", "#data", verbose=False)
     boston_target, _ = grid.search("#boston", "#target", verbose=False)
 
-    n_inputs = boston_data['alice'][0].shape[1]
+    n_inputs = boston_data["alice"][0].shape[1]
     n_targets = 1
 
-    model = th.nn.Linear(n_inputs,n_targets, bias=False)
-    opt = sy.optim.SGD(params=model.parameters(),lr=0.0000001)
+    model = th.nn.Linear(n_inputs, n_targets, bias=False)
+    opt = sy.optim.SGD(params=model.parameters(), lr=0.000_000_1)
 
     fd = sy.FederatedDataset(boston_data, boston_target, batch_size=32, num_iterators=1)
 
@@ -21,8 +21,7 @@ def test_federated_dataset():
         fd.reset()
         i = 0
         loss_accum = 0
-        while(fd.keep_going()):
+        while fd.keep_going():
             i += 1
 
             data, target = fd.step()
-            
