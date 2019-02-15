9dc930bb16efcdc6071adf41a6f4bdc09f7b7cb5
kamathhrishi@gmail.com <kamathhrishi@gmail.com>
2019-02-15 21:58:06 +0530

    Polynomial Tensor

---
+++ syft/frameworks/torch/tensors/interpreters/Polynomial.py
@@ -0,0 +1,58 @@
+from syft.frameworks.torch.tensors.abstract import AbstractTensor
+
+
+class PolynomialTensor(AbstractTensor):
+
+    """Tensor type which provides polynomial approximation functionalities using Taylor Series expansion
+       since computing exact functions could impose a overhead on computation
+    """
+
+    def sigmoid(self, x: float) -> float:
+
+        """Parameters:
+            
+           x: A float value
+           
+           return: 
+               
+           approximation of the sigmoid function as a float"""
+
+        return (1 / 2) + (x / 4) - (x ** 3 / 48) + (x ** 5 / 480)
+
+    def exp(self, x: float) -> float:
+
+        """Parameters:
+            
+           x: (float) value for which approximation must be found
+           
+           return: 
+               
+           value: (float) approximation of the exponential function """
+
+
+        return (1 + x + (x ** 2 / 2) + (x ** 3 / 6) + (x ** 4 / 24))
+
+    def tanh(self, x: float) -> float:
+
+        """Parameters:
+            
+            x: A float value
+           
+            return: 
+               
+            approximation of the tanh function as a float"""
+
+        return (x) + ((x ** 3) / 3) + (((x ** 5) / 120)) + ((x ** 7) / 5040)
+
+    def log(self, x: float) -> float:
+
+        """Parameters:
+            
+            x: A float value
+           
+            return: 
+               
+            approximation of the log function as a float"""
+
+        return (x) - ((x ** 2) / 2) + ((x ** 3) / 3) - ((x ** 4) / 4)
+
---
+++ test/torch/tensors/test_poly.py
@@ -0,0 +1,105 @@
+
+from syft.frameworks.torch.tensors.Polynomial import PolynomialTensor
+import torch 
+
+
+""" Test cases to ensure working of Polynomial Tensor. The tests under these ensure that the error between actual funcion values and approximations do not deviate too much"""
+
+# Maximum permissible error as calculated by EvalRelative under PolynomialTensor
+threshold = 0.25
+
+def EvalRelative(x_true, x_pred):
+
+        """The function is used to measure the error between actual function value and approximated function value. The error is evaluated with respect to actual value.
+           
+        
+            Parameters: 
+            
+            x_true: Value of true function 
+            x_pred: Value of function approximation 
+            
+        """
+        
+        error = torch.div(torch.abs(x_true - x_pred),x_true)
+
+        return round(torch.max(error).item(),2)
+    
+
+def test_EvalRelative():
+    
+    " Ensure that EvalRelative function does what it has to. For the purpose we take a tensor of ones and subtract and add an amount of error "
+    
+    ones=torch.ones((2,3))
+    pred=ones-0.1
+    assert(EvalRelative(ones,pred)==0.1)
+    
+    ones=torch.ones((2,3))
+    pred=ones-0.2
+    assert(EvalRelative(ones,pred)==0.2)
+    
+    ones=torch.ones((2,3))
+    pred=ones+0.2
+    assert(EvalRelative(ones,pred)==0.2)
+    
+    ones=torch.ones((2,3))
+    pred=ones+0.1
+    assert(EvalRelative(ones,pred)==0.1)
+    
+    
+def testSigmoid():
+
+    Ptensor = PolynomialTensor()  
+    
+    x = torch.randn(100)*1.5
+    m = torch.nn.Sigmoid()
+    ten=m(x)
+    
+    print(ten)
+    print("0.1215==0.1036")
+    print(Ptensor.sigmoid(x))
+    print((EvalRelative(ten,Ptensor.sigmoid(x))))
+    assert((EvalRelative(ten,Ptensor.sigmoid(x)))<threshold)
+    
+
+def testExpTest():
+
+    Ptensor = PolynomialTensor()    
+    in_tensor=torch.randn(2)
+    
+    m = torch.nn.Sigmoid()
+    ten=m(in_tensor)
+    
+    assert (
+            EvalRelative(ten,Ptensor.sigmoid(in_tensor))< threshold
+         )
+
+def testtanh():
+
+    Ptensor = PolynomialTensor()    
+    in_tensor=torch.randn(2)
+    
+    m = torch.nn.Sigmoid()
+    ten=m(in_tensor)
+    
+    assert (
+            EvalRelative(ten,Ptensor.sigmoid(in_tensor)).all()
+            < threshold
+         )
+
+def testLog():
+
+    Ptensor = PolynomialTensor()    
+    in_tensor=torch.randn(2)
+    
+    m = torch.nn.Sigmoid()
+    ten=m(in_tensor)
+    
+    assert (
+            EvalRelative(ten,Ptensor.sigmoid(in_tensor)).all()
+            < threshold
+         )
+
+
+test_EvalRelative()
+testSigmoid()
+
