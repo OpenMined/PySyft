86b49adb938147a716659577f218b3f95e909af3
kamathhrishi@gmail.com <kamathhrishi@gmail.com>
2019-02-17 11:32:47 +0530

    Added documentation

--- test/torch/tensors/test_poly.py
+++ test/torch/tensors/test_poly.py
@@ -7,24 +7,33 @@ import torch
 # Maximum permissible error as calculated by EvalRelative under PolynomialTensor
 
 DATA_THRESHOLD = 2.9
-ERROR_THRESHOLD = 0.15
+ERROR_THRESHOLD = 0.05
 
 
-def EvalRelative(x_true, x_pred):
+def EvalError(x_true, x_pred):
 
-    """The function is used to measure the error between actual function value and approximated function value. The error is evaluated with respect to actual value.
-           
+    """ The function is used to measure the error between actual function value and approximated function value. It is evaluated as follows.
+        Given a tensor of 1000 random numbers, the function approximation of individual numbers in the tensor must be within atleast +- 5% error of actual value.
+        This threshold is given by ERROR_THRESHOLD.
+        But , a certain percentage of numbers in the tensor can exceed the ERROR_THRESHOLD. This threshold is described by DATA_THRESHOLD.
+        The function returns true if the percentage of individual approximations are within the DATA_THRESHOLD.
         
             Parameters: 
             
             x_true: Value of true function 
             x_pred: Value of function approximation 
             
+            Returns:
+                
+            within_limits: If function approximation error of given tensor is within DATA_THRESHOLD
+            
         """
-
-    error = torch.div(torch.abs(x_true - x_pred), x_true)
-
-    error = error < ERROR_THRESHOLD
+    
+    #Relative absolute error give by abs(true_value-prediction_value)/(true_value)
+    error_rel = torch.div(torch.abs(x_true - x_pred), x_true)
+    
+    #Check if individual approximations are within the threshold
+    error = error_rel < ERROR_THRESHOLD
 
     count = 0
 
@@ -34,6 +43,7 @@ def EvalRelative(x_true, x_pred):
             count += 1
 
     data_error = (count / len(x_true)) * 100
+    print(data_error)
     within_limits = data_error < DATA_THRESHOLD
 
     return within_limits
@@ -45,69 +55,82 @@ def test_EvalRelative():
 
     ones = torch.ones((2, 3))
     pred = ones - 0.1
-    assert EvalRelative(ones, pred) == 0.1
+    assert EvalError(ones, pred) == 0.1
 
     ones = torch.ones((2, 3))
     pred = ones - 0.2
-    assert EvalRelative(ones, pred) == 0.2
+    assert EvalError(ones, pred) == 0.2
 
     ones = torch.ones((2, 3))
     pred = ones + 0.2
-    assert EvalRelative(ones, pred) == 0.2
+    assert EvalError(ones, pred) == 0.2
 
     ones = torch.ones((2, 3))
     pred = ones + 0.1
-    assert EvalRelative(ones, pred) == 0.1
+    assert EvalError(ones, pred) == 0.1
 
     ones = torch.ones((2, 3)) * 2
     pred = ones + 0.2
-    assert EvalRelative(ones, pred) == 0.1
+    assert EvalError(ones, pred) == 0.1
 
     ones = torch.ones((2, 3)) * 2
     pred = ones - 0.2
-    assert EvalRelative(ones, pred) == 0.1
+    assert EvalError(ones, pred) == 0.1
 
 
 def testSigmoid():
 
     Ptensor = PolynomialTensor()
 
-    x = torch.randn(100_000) * (-1)
+    x = torch.randn(50000) 
+    
+    print(x)
 
     m = torch.nn.Sigmoid()
     ten = m(x)
 
-    assert (EvalRelative(ten, Ptensor.sigmoid(x))) == True
+    assert (EvalError(ten, Ptensor.sigmoid(x))) == True
 
 
-def testExpTest():
+def testExp():
 
     Ptensor = PolynomialTensor()
-    in_tensor = torch.randn(2)
 
-    m = torch.nn.Sigmoid()
-    ten = m(in_tensor)
+    x = torch.randn(50000) 
+    
+    print(x)
 
-    assert EvalRelative(ten, Ptensor.sigmoid(in_tensor)) < threshold
+    ten = torch.exp(x)
+
+    assert (EvalError(ten, Ptensor.exp(x))) == True
 
 
 def testtanh():
 
     Ptensor = PolynomialTensor()
-    in_tensor = torch.randn(2)
 
-    m = torch.nn.Sigmoid()
-    ten = m(in_tensor)
+    x = torch.randn(50000) 
+    
+    print(x)
 
-    assert EvalRelative(ten, Ptensor.sigmoid(in_tensor)).all() < threshold
+    m = torch.nn.tanh()
+    ten = m(x)
+
+    assert (EvalError(ten, Ptensor.tanh(x))) == True
 
 
 def testLog():
 
     Ptensor = PolynomialTensor()
-    in_tensor = torch.randn(2)
 
-    m = torch.nn.Sigmoid()
-    ten = m(in_tensor)
+    x = torch.randn(50000) 
+    
+    print(x)
 
-    assert EvalRelative(ten, Ptensor.sigmoid(in_tensor)).all() < threshold
+    m = torch.nn.tanh()
+    ten = m(x)
+
+    assert (EvalError(ten, Ptensor.log(x))) == True
+    
+testExp()
+
