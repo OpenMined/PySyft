b8f9a5dfe945c040a199db47714f5e93d44c1fcc
Theo <theo.leffyr@gmail.com>
2019-02-20 22:23:04 +0100

    Upgrade FederatedDataset to match the PyTorch api:

--- syft/frameworks/torch/federated/__init__.py
+++ syft/frameworks/torch/federated/__init__.py
@@ -1,2 +1,3 @@
+from .dataset import FederatedData
 from .dataset import FederatedDataset
 from .dataloader import FederatedDataLoader
--- syft/frameworks/torch/federated/dataset.py
+++ syft/frameworks/torch/federated/dataset.py
@@ -1,59 +1,101 @@
-class FederatedDataset:
-    def __init__(self, inputs: dict, targets: dict):
-        """This class takes two dictionaries of datapoints and forms
-        them into batches ready for training. The keys to both inputs
-        and target are worker IDs, and the values are tensors. The class
-        will use these collections of pointers to datasets and form them
-        into one cohesive dataset (with some statistics on how many data-
-        points had to be thrown away, if any).
+class FederatedData:
+    """
+    A piece of data which is distributed across workers
+    """
 
-        Args:
-            inputs (dict): dict of dataset tensors or pointers to dataset tensors
-            targets (dict): dict of target/label tensors or pointers to target/label tensors
-            batch_size (int): size of each batch
-            num_iterators (int): number of iterators to use for parallel computations
-        """
+    def __init__(self, data_dict):
+        self.worker_ids = set()
+        for worker_id, worker_data in data_dict.items():
+            # Allow worker_data to be a list of size 1
+            if isinstance(worker_data, list):
+                if len(worker_data) == 1:
+                    worker_data = worker_data[0]
+                else:
+                    raise ReferenceError(
+                        f"There should be exactly one dataset provided per worker, found{len(worker_data)}."
+                    )
 
-        # first check to see which people line up
-        input_workers = set(inputs.keys())
-        target_workers = set(targets.keys())
-        intersection = input_workers.intersection(target_workers)
-        diff = input_workers.difference(target_workers)
+            self.worker_ids.add(worker_id)
+            setattr(self, worker_id, worker_data)
 
-        if len(diff) > 0:
-            print("Data from some workers ignored:" + str(diff))
-
-        self.workers = list(intersection)
-
-        self.worker2num_rows = {}
-        self.worker2inputs = {}
-        self.worker2targets = {}
-
-        self.num_rows = 0
-
-        for worker in self.workers:
-
-            if (len(inputs[worker]) != 1) or (len(targets[worker]) != 1):
-                raise Exception(
-                    "There must be only one input and one target"
-                    + "tensor per worker. Otherwise it's not clear"
-                    + "which rows in the input correspond to which"
-                    + "rows in the output"
-                )
-
-            worker_inputs = inputs[worker][0]
-            worker_targets = targets[worker][0]
-
-            if worker_inputs.shape[0] != worker_targets.shape[0]:
-                raise Exception(
-                    "On each worker, the input and target must have" + "the same number of rows."
-                )
-
-            self.worker2num_rows[worker] = worker_inputs.shape[0]
-            self.worker2inputs[worker] = worker_inputs
-            self.worker2targets[worker] = worker_targets
-
-            self.num_rows += worker_inputs.shape[0]
+    def __getitem__(self, indices):
+        if isinstance(indices, tuple):
+            worker_id, item_id = indices
+            return getattr(self, worker_id)[item_id]
+        else:
+            worker_id = indices
+            return getattr(self, worker_id)
 
     def __len__(self):
-        return self.num_rows
+        data_length = 0
+        for worker_id in self.worker_ids:
+            data_length += self[worker_id].shape[0]
+        return data_length
+
+    @property
+    def workers(self):
+        return self.worker_ids
+
+    def drop_worker(self, worker_id):
+        if worker_id in self.worker_ids:
+            delattr(self, worker_id)
+            self.worker_ids.remove(worker_id)
+
+    def __repr__(self):
+        fmt_str = "FederatedData\n"
+        fmt_str += "    Distributed accross: {}\n".format(", ".join(self.workers))
+        fmt_str += "    Number of datapoints: {}\n".format(self.__len__())
+        return fmt_str
+
+
+class FederatedDataset:
+    def __init__(self, federated_inputs, federated_targets):
+        """This class takes two dictionaries of data points, the keys
+        of which are worker IDs, and their values are tensors or pointers
+        to tensors. One can also provide direct FederatedData elements.
+        The class will use these collections of datasets or pointers
+        to datasets and form them into one cohesive dataset.
+        Args:
+            federated_inputs (dict or FederatedData): dict of dataset tensors
+            or pointers to dataset tensors
+            federated_targets (dict or FederatedData): dict of target/label
+            tensors or pointers to target/label tensors
+        """
+        # Convert in the Federated format if needed
+        if isinstance(federated_inputs, dict):
+            federated_inputs = FederatedData(federated_inputs)
+        self.data = federated_inputs
+        if isinstance(federated_targets, dict):
+            federated_targets = FederatedData(federated_targets)
+        self.targets = federated_targets
+
+        # Check to see which people line up
+        input_workers = federated_inputs.workers
+        target_workers = federated_targets.workers
+        skewed_workers = input_workers.difference(target_workers)
+
+        # Keep only workers that appear in both the data and the targets
+        if len(skewed_workers) > 0:
+            print("Data from some workers ignored:", skewed_workers)
+            for worker_id in skewed_workers:
+                self.data.drop_worker(worker_id)
+                self.targets.drop_worker(worker_id)
+
+        # Check that data and targets for a worker are consistent
+        for worker in self.workers:
+            assert len(self.data[worker]) == len(self.targets[worker]), (
+                "On each worker, the input and target must have" + "the same number of rows."
+            )
+
+    @property
+    def workers(self):
+        return self.data.workers
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        fmt_str = "FederatedDataset\n"
+        fmt_str += "    Distributed accross: {}\n".format(", ".join(self.workers))
+        fmt_str += "    Number of datapoints: {}\n".format(self.__len__())
+        return fmt_str
