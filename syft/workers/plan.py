141a17e48aeb47204156d7ba1259961c92c858fa
iamtrask <liamtrask@gmail.com>
2019-02-23 21:25:21 +0000

    moved plan execution to its own method

--- syft/workers/plan.py
+++ syft/workers/plan.py
@@ -4,6 +4,11 @@ from syft import serde
 
 
 class Plan(BaseWorker):
+    """This worker does not send messages or execute any commands. Instead,
+    it simply records messages that are sent to it such that message batches
+    (called 'Plans') can be created and sent once."""
+
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.plan = list()
@@ -17,16 +22,22 @@ class Plan(BaseWorker):
 
         self.plan.append(bin_message)
         self.readable_plan.append((msg_type, contents))
-
+        
+        # we can't receive the results of a plan without
+        # executing it. So, execute the plan.
         if msg_type == MSGTYPE.OBJ_REQ:
-            print("Execute Plan")
-            response = None
-            for bin_message, message in zip(self.plan, self.readable_plan):
-                print(message)
-                response = self.recv_msg(bin_message)
-
-            self.plan = []
-            self.readable_plan = []
-            return response
+            return self.execute_plan()
 
         return serde.serialize(None)
+
+    def execute_plan(self):
+
+        print("Execute Plan")
+        response = None
+        for bin_message, message in zip(self.plan, self.readable_plan):
+            print(message)
+            response = self.recv_msg(bin_message)
+
+        self.plan = []
+        self.readable_plan = []
+        return response
