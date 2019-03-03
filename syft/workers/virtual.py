d567a0a45ad20180533ebe597ee09aee5da9aef3
Ala ABOUDIB <agabudeeb@gmail.com>
2019-03-03 08:58:53 +0100

    added return type to _recv_msg() in syft/workers/virutal.py

--- syft/workers/virtual.py
+++ syft/workers/virtual.py
@@ -5,5 +5,5 @@ class VirtualWorker(BaseWorker):
     def _send_msg(self, message: bin, location: BaseWorker) -> bin:
         return location._recv_msg(message)
 
-    def _recv_msg(self, message: bin):
+    def _recv_msg(self, message: bin) -> bin:
         return self.recv_msg(message)
