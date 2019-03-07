69ca3780b96f0fd6048a0de3dea380016b68c76f
Bobby Wagner <raw141@case.edu>
2019-03-06 19:34:00 -0500

    added worker discovery to flask server

--- grid/__init__.py
+++ grid/__init__.py
@@ -10,48 +10,47 @@ from .grid import Grid
 from .client import GridClient
 
 
-
-
-
-
 host = "localhost"
+
 
 def run_command(cmd="sleep 100"):
     _id = "process" + str(random.randint(0, 1e10))
     cmd = "screen -d -m -S " + str(_id) + " " + cmd
 
-    process = subprocess.Popen(cmd, shell=True,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
+    process = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
 
     cmd = "screen -ls " + str(_id)
-    process = subprocess.Popen(cmd, shell=True,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
+    process = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
 
     # wait for the process to terminate
     out, err = process.communicate()
     errcode = process.returncode
 
-    pid = int(out.decode('ascii').split("\t")[1].split(".")[0]) + 1
+    pid = int(out.decode("ascii").split("\t")[1].split(".")[0]) + 1
     return pid
 
 
 def kill_command(pid):
     cmd = "kill " + pid
-    process = subprocess.Popen(cmd, shell=True,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
+    process = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
 
     # wait for the process to terminate
     out, err = process.communicate()
     errcode = process.returncode
 
+
 def find_free_port():
     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-        s.bind(('', 0))
+        s.bind(("", 0))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         return s.getsockname()[1]
+
 
 def create_app(test_config=None):
     # create and configure the app
@@ -82,15 +81,24 @@ def create_app(test_config=None):
         print("PID:" + str(pid))
 
         output = {}
-        output['host'] = host
-        output['port'] = port
-        output['pid'] = pid
+        output["host"] = host
+        output["port"] = port
+        output["pid"] = pid
 
         return json.dumps(output)
 
     @app.route("/kill_connection/<pid>")
     def kill_connection(pid):
         kill_command(pid)
+        return "Done!"
+
+    @app.route("/get_known_workers/")
+    def get_know_workers():
+        return json.dumps({"known_workers": session["known_workers"]})
+
+    @app.route("/add_worker/<hostname>/<port>")
+    def add_worker(hostname, port):
+        session["known_workers"].append((hostname, port))
         return "Done!"
 
     return app
