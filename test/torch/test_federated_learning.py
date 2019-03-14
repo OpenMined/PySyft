dc849169cfc18aa1b1c70c76fe3d9f7676f69a66
Theo <theo.leffyr@gmail.com>
2019-03-14 17:14:23 +0100

    Clean test_FL and add test for LSTM

--- test/torch/test_federated_learning.py
+++ test/torch/test_federated_learning.py
@@ -6,86 +6,81 @@ import torch
 hook = sy.TorchHook(torch)
 from torch import nn
 from torch import optim
-import random
 
 
-class TestFederatedLearning(object):
-    def setUp(self):
-        hook = sy.TorchHook(torch, verbose=True)
+def test_toy_federated_learning(workers):
 
-        self.me = hook.local_worker
-        self.me.is_client_worker = True
+    bob, alice = workers["bob"], workers["alice"]
 
-        instance_id = str(int(10e10 * random.random()))
-        bob = sy.VirtualWorker(id=f"bob{instance_id}", hook=hook, is_client_worker=False)
-        alice = sy.VirtualWorker(id=f"alice{instance_id}", hook=hook, is_client_worker=False)
-        james = sy.VirtualWorker(id=f"james{instance_id}", hook=hook, is_client_worker=False)
+    # A Toy Dataset
+    data = torch.tensor([[0, 0], [0, 1], [1, 0], [1, 1.0]], requires_grad=True)
+    target = torch.tensor([[0], [0], [1], [1.0]], requires_grad=True)
 
-        bob.add_workers([alice, james])
-        alice.add_workers([bob, james])
-        james.add_workers([bob, alice])
+    # get pointers to training data on each worker by
+    # sending some training data to bob and alice
+    data_bob = data[0:2]
+    target_bob = target[0:2]
 
-        self.hook = hook
+    data_alice = data[2:]
+    target_alice = target[2:]
 
-        self.bob = bob
-        self.alice = alice
-        self.james = james
+    data_bob = data_bob.send(bob)
+    data_alice = data_alice.send(alice)
+    target_bob = target_bob.send(bob)
+    target_alice = target_alice.send(alice)
 
-        # A Toy Dataset
-        data = torch.tensor([[0, 0], [0, 1], [1, 0], [1, 1.0]], requires_grad=True)
-        target = torch.tensor([[0], [0], [1], [1.0]], requires_grad=True)
+    # organize pointers into a list
+    datasets = [(data_bob, target_bob), (data_alice, target_alice)]
 
-        # get pointers to training data on each worker by
-        # sending some training data to bob and alice
-        data_bob = data[0:2]
-        target_bob = target[0:2]
+    # Initialize A Toy Model
+    model = nn.Linear(2, 1)
 
-        data_alice = data[2:]
-        target_alice = target[2:]
+    # Training Logic
+    opt = optim.SGD(params=model.parameters(), lr=0.1)
+    for iter in range(20):
 
-        data_bob = data_bob.send(bob)
-        data_alice = data_alice.send(alice)
-        target_bob = target_bob.send(bob)
-        target_alice = target_alice.send(alice)
+        # NEW) iterate through each worker's dataset
+        for data, target in datasets:
+            # NEW) send model to correct worker
+            model.send(data.location)
 
-        # organize pointers into a list
-        self.datasets = [(data_bob, target_bob), (data_alice, target_alice)]
+            # 1) erase previous gradients (if they exist)
+            opt.zero_grad()
 
-    # POINTERS
+            # 2) make a prediction
+            pred = model(data)
 
-    def test_toy_federated_learning(self):
+            # 3) calculate how much we missed
+            loss = ((pred - target) ** 2).sum()
 
-        self.setUp()
+            # 4) figure out which weights caused us to miss
+            loss.backward()
 
-        # Initialize A Toy Model
-        model = nn.Linear(2, 1)
+            # 5) change those weights
+            opt.step()
 
-        # Training Logic
-        opt = optim.SGD(params=model.parameters(), lr=0.1)
-        for iter in range(20):
+            # get model (with gradients)
+            model.get()
 
-            # NEW) iterate through each worker's dataset
-            for data, target in self.datasets:
-                # NEW) send model to correct worker
-                model.send(data.location)
+            # 6) print our progress
+            print(loss.get())  # NEW) slight edit... need to call .get() on loss
 
-                # 1) erase previous gradients (if they exist)
-                opt.zero_grad()
 
-                # 2) make a prediction
-                pred = model(data)
+def test_lstm(workers):
+    bob, alice = workers["bob"], workers["alice"]
 
-                # 3) calculate how much we missed
-                loss = ((pred - target) ** 2).sum()
+    lstm = nn.LSTM(3, 3)
+    inputs = torch.randn(5, 1, 3)
+    hidden = (torch.randn(1, 1, 3), torch.randn(1, 1, 3))  # clean out hidden state
+    out, hidden = lstm(inputs, hidden)
+    assert out.shape == torch.Size([5, 1, 3])
 
-                # 4) figure out which weights caused us to miss
-                loss.backward()
-
-                # 5) change those weights
-                opt.step()
-
-                # get model (with gradients)
-                model.get()
-
-                # 6) print our progress
-                print(loss.get())  # NEW) slight edit... need to call .get() on loss
+    lstm = nn.LSTM(3, 3)
+    lstm.send(bob)
+    inputs = torch.randn(5, 1, 3).send(bob)
+    hidden = (
+        torch.randn(1, 1, 3).send(bob),
+        torch.randn(1, 1, 3).send(bob),
+    )  # clean out hidden state
+    out, hidden = lstm(inputs, hidden)
+    assert out.shape == torch.Size([5, 1, 3])
