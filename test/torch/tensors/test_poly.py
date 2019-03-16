e741a593cd56a3bb7a4689dc5f74181dddb03aa5
kamathhrishi@gmail.com <kamathhrishi@gmail.com>
2019-03-16 19:55:05 +0530

    Add comments and remove function calls in test

--- test/torch/tensors/test_poly.py
+++ test/torch/tensors/test_poly.py
@@ -14,7 +14,8 @@ def testSigmoid():
     expected = torch.tensor(
         [-0.1938, 0.0372, 0.1530, 0.2688, 0.4174, 0.5826, 0.7313, 0.8470, 0.9628, 1.1938]
     )
-
+    
+    #allclose function to compare the expected values and approximations with fixed precision
     assert torch.allclose(expected, Ptensor.sigmoid(x),1e-03)
 
 
@@ -26,7 +27,8 @@ def testExp():
     expected = torch.tensor(
         [0.2179, 0.1224, 0.1905, 0.3679, 0.7165, 1.3956, 2.7179, 5.2813, 10.1764, 19.2679]
     )
-
+    
+    #allclose function to compare the expected values and approximations with fixed precision
     assert torch.allclose(expected, Ptensor.exp(x),1e-03)
 
 
@@ -49,9 +51,7 @@ def testtanh():
             3.3883e02,
         ]
     )
-
+    
+    #allclose function to compare the expected values and approximations with fixed precision
     assert torch.allclose(expected, Ptensor.tanh(x),1e-03)
 
-testSigmoid()
-testExp()
-testtanh()
