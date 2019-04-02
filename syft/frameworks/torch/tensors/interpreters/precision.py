c954a59f2449ab0ff3ac796aa7a8b9a9480ea47a
Bobby Wagner <raw141@case.edu>
2019-04-02 12:54:22 -0400

    had get preserve fixed precision attributes

--- syft/frameworks/torch/tensors/interpreters/precision.py
+++ syft/frameworks/torch/tensors/interpreters/precision.py
@@ -291,7 +291,17 @@ class FixedPrecisionTensor(AbstractTensor):
     def get(self):
         """Just a pass through. This is most commonly used when calling .get() on a
         FixedPrecisionTensor which has also been shared."""
-        return FixedPrecisionTensor().on(self.child.get())
+        return FixedPrecisionTensor(
+            parent=self.parent,
+            owner=self.owner,
+            id=self.id,
+            field=self.field,
+            base=self.base,
+            precision_fractional=self.precision_fractional,
+            kappa=self.kappa,
+            tags=self.tags,
+            description=self.description,
+        ).on(self.child.get())
 
     def share(self, *owners, field=None, crypto_provider=None):
         self.child = self.child.share(*owners, field=field, crypto_provider=crypto_provider)
