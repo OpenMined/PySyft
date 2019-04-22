316f8e5aa3b67d9616b14c8a5b8d6440a7622d8c
Daniele Gadler <daniele.gadler@yahoo.it>
2019-04-22 08:33:37 +0200

    Fixed constant name

--- syft/workers/websocket_client.py
+++ syft/workers/websocket_client.py
@@ -55,7 +55,7 @@ class WebsocketClientWorker(BaseWorker):
         return sy.serde.deserialize(response)
 
     def _send_msg(self, message: bin, location) -> bin:
-        self.ws.settimeout(TIME_OUT_AMOUNT)
+        self.ws.settimeout(TIMEOUT_INTERVAL)
         raise RuntimeError(
             "_send_msg should never get called on a ",
             "WebsocketClientWorker. Did you accidentally "
