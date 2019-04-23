cd5de8800732b9e06c3dd97bba6772cc252bb923
Marianne Linhares <mariannelinharesm@gmail.com>
2019-04-23 04:16:27 +0100

    Create draft of TrainConfig abstraction

--- syft/federated/__init__.py
+++ syft/federated/__init__.py
@@ -3,5 +3,6 @@ from syft.federated.plan import Plan
 from syft.federated.plan import func2plan
 from syft.federated.plan import method2plan
 from syft.federated.plan import make_plan
+from syft.federated.train_config import TrainConfig
 
-__all__ = ["Plan", "func2plan", "method2plan", "make_plan"]
+__all__ = ["plan", "func2plan", "method2plan", "make_plan", "TrainConfig"]
