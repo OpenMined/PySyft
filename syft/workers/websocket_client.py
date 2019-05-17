db96478b2e99776158d2dfda04473f8611ef97d7
Marianne Monteiro <mariannelinharesm@gmail.com>
2019-05-17 13:31:09 -0300

    (Backprop not working) Fit implementation and datasets support

--- syft/workers/websocket_client.py
+++ syft/workers/websocket_client.py
@@ -97,9 +97,9 @@ class WebsocketClientWorker(BaseWorker):
                 )
         return response
 
-    def _send_msg_and_deserialize(self, command_name: str):
+    def _send_msg_and_deserialize(self, command_name: str, *args, **kwargs):
         message = self.create_message_execute_command(
-            command_name=command_name, command_owner="self"
+            command_name=command_name, command_owner="self", *args, **kwargs
         )
 
         # Send the message and return the deserialized response.
@@ -109,6 +109,9 @@ class WebsocketClientWorker(BaseWorker):
 
     def list_objects_remote(self):
         return self._send_msg_and_deserialize("list_objects")
+
+    def fit(self, dataset_key):
+        return self._send_msg_and_deserialize("fit", dataset=dataset_key)
 
     def fit_batch_remote(self):
         return self._send_msg_and_deserialize("fit_batch")
