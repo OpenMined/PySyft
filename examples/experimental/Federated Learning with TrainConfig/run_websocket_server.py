ecc4fd04781755ed6302463e447638cab08ab1be
Marianne Monteiro <mariannelinharesm@gmail.com>
2019-05-17 13:31:09 -0300

    (Backprop not working) Fit implementation and datasets support

--- examples/experimental/Federated Learning with TrainConfig/run_websocket_server.py
+++ examples/experimental/Federated Learning with TrainConfig/run_websocket_server.py
@@ -1,28 +1,42 @@
 import argparse
 from multiprocessing import Process
-import sys
+import logging
 
 import torch as th
 
 import syft as sy
+from syft.frameworks.torch.tensors.interpreters import AutogradTensor
 from syft.workers import WebsocketServerWorker
 
+logger = logging.getLogger("run_websocket_server")
+FORMAT = "%(asctime)s %(levelname)s %(filename)s(l:%(lineno)d) - %(message)s"
+logging.basicConfig(format=FORMAT)
+logger.setLevel(level=logging.INFO)
+
 hook = sy.TorchHook(th)
-hook.local_worker = sy.VirtualWorker(id="me, port: 8777", is_client_worker=True, hook=hook)
+hook.local_worker = sy.VirtualWorker(id="local_worker_at_remote", is_client_worker=True, hook=hook)
 
 
 def start_proc(participant, kwargs):  # pragma: no cover
     """ helper function for spinning up a websocket participant """
 
     def target():
+
         server = participant(**kwargs)
 
-        _ = th.tensor([[-1, 2.0], [0, 1.1]], id=("#data")).send(server)
-        _ = th.tensor([[1.0], [0.0]], id=("#target")).send(server)
+        # data = AutogradTensor().on(th.tensor([[-1, 2.0], [0, 1.1], [-1, 2.1], [0, 1.2]], requires_grad=True))
+        # target = AutogradTensor().on(th.tensor([[1.0], [0.0], [1.0], [0.0]], requires_grad=True))
+        data = th.tensor([[-1, 2.0], [0, 1.1], [-1, 2.1], [0, 1.2]], requires_grad=True)
+        target = th.tensor([[1.0], [0.0], [1.0], [0.0]], requires_grad=True)
 
-        # dataset = sy.BaseDataset(data, target_)
-        # dataset.send(server)
-        # server.dataset = dataset
+        logger.info("data: %s", data)
+        logger.info("target: %s", target)
+
+        dataset = sy.BaseDataset(data, target)
+        server.add_dataset(dataset, key="vectors")
+
+        logger.info("datasets: %s", server.datasets)
+        logger.info("server._objects: %s", server._objects)
 
         server.start()
 
--- syft/federated/federated_client.py
+++ syft/federated/federated_client.py
@@ -1,21 +1,33 @@
+import logging
 from typing import Union
 
 import torch as th
+from torch.utils.data import BatchSampler, RandomSampler, SequentialSampler
 from torch import nn
 
 from syft.generic import ObjectStorage
 from syft.federated.train_config import TrainConfig
 
+logger = logging.getLogger(__name__)
+
 
 class FederatedClient(ObjectStorage):
     """A Client able to execute federated learning in a defined dataset."""
 
-    def __init__(self):
+    def __init__(self, datasets=None, verbose=True):
         super().__init__()
-        self.dataset = None
+        self.datasets = datasets if datasets is not None else dict()
         self.optimizer = None
         self.train_config = None
         self.parameters = []
+        self.verbose = verbose
+
+    def add_dataset(self, dataset, key: str):
+        self.datasets[key] = dataset
+
+    def remove_dataset(self, key: str):
+        if key in self.datasets:
+            del self.datasets[key]
 
     def set_obj(self, obj: object):
         """Registers objects checking if which objects it should cache.
@@ -50,28 +62,59 @@ class FederatedClient(ObjectStorage):
             raise ValueError("Unknown optimizer: {}".format(optimizer_name))
         return self.optimizer
 
-    def fit_batch(self, *args, **kwargs):
-        model_prediction = self.train_config.forward_plan(self._objects[b"#data"])
-        model_prediction.owner.register_obj(model_prediction)
-        loss = self.train_config.loss_plan(model_prediction, self._objects[b"#target"])
+    def fit_batch(self, data, target, *args, **kwargs):
+        if self.verbose:
+            logger.debug("data = %s", data)
+            logger.debug("target = %s", target)
+        self.register_obj(data)
+        self.register_obj(target)
+        self.optimizer.zero_grad()
+        output = self.train_config.forward_plan(data)
+        self.register_obj(output)
+        loss = self.train_config.loss_plan(output, target)
+        loss.backward()
+        self.optimizer.step()
+        self.de_register_obj(output)
+        self.de_register_obj(data)
+        self.de_register_obj(target)
         return loss
 
     def fit(self, *args, **kwargs):
         if self.train_config is None:
             raise ValueError("TrainConfig not defined.")
+        if self.datasets is None:
+            logger.error("No dataset available on worker")
+            return None, -1
+        try:
+            key = kwargs["dataset"]
+        except KeyError:
+            logger.warning("Missing keyword argument 'dataset'.")
+            if len(self.datasets) == 1:
+                logger.warning("Only one dataset available, using this one.")
+                key = list(self.datasets.keys())[0]
+            else:
+                logger.warning("Available datasets: %s", list(self.datasets.keys()))
+                return None, -1
 
         self._build_optimizer(self.train_config.optimizer, self.train_config.lr)
-        self._fit()
+        return self._fit(key)
 
-    def _fit(self):
-        # TODO: how to get the actual model?
-        # self.model.train()
-        # TODO: how to create/set a dataset?
-        for data, target in self.dataset:
-            self.optimizer.zero_grad()
-            output = self.train_config.forward_plan(data)
-            loss = self.train_config.loss_plan(output, target)
-            loss.backward()
-            self.optimizer.step()
-        # TODO: check if multiple returns are supported.
-        return self.model.get(), loss.get()
+    def _create_batch_sampler(self, ds_key: str, shuffle: bool = False, drop_last: bool = True):
+        data_range = range(len(self.datasets[ds_key]))
+        if shuffle:
+            sampler = RandomSampler(data_range)
+        else:
+            sampler = SequentialSampler(data_range)
+
+        batch_sampler = BatchSampler(sampler, self.train_config.batch_size, drop_last)
+        return batch_sampler
+
+    def _fit(self, ds_key: str):
+        loss = -1.0
+        batch_sampler = self._create_batch_sampler(ds_key)
+        for data_indices in batch_sampler:
+            data, target = self.datasets[ds_key][data_indices]
+            loss = self.fit_batch(data, target)
+            if self.verbose:
+                logger.info("loss: %s", loss)
+        return loss
