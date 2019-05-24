d0f20f5bb7c56a40461968e2f679b580b3b26e21
Silvia Gandy <silvia@midokura.com>
2019-05-24 17:46:33 +0200

    Working example with jit traced models and train config

--- examples/experimental/Federated Learning with TrainConfig/run_training_train_config.py
+++ examples/experimental/Federated Learning with TrainConfig/run_training_train_config.py
@@ -3,24 +3,19 @@ import torch.nn.functional as F
 from torch import nn
 
 import syft as sy
-from syft.workers import WebsocketClientWorker
+from syft import workers
+from syft.frameworks.torch import pointers
 
 use_cuda = th.cuda.is_available()
 th.manual_seed(1)
 device = th.device("cuda" if use_cuda else "cpu")
 
 hook = sy.TorchHook(th)
-hook.local_worker.is_client_worker = False
 me = hook.local_worker
 
-
 kwargs_websocket = {"host": "localhost", "hook": hook, "verbose": False}
-alice = WebsocketClientWorker(id="alice", port=8777, **kwargs_websocket)
+alice = workers.WebsocketClientWorker(id="alice", port=8777, **kwargs_websocket)
 
-
-# TODO: maybe by default is_client_worker could be False?
-# for plans the client_worker needs to be able to register tensors
-# since it needs to build the plan.
 # Loss function
 @th.jit.script
 def loss_fn(real, pred):
@@ -28,14 +23,13 @@ def loss_fn(real, pred):
 
 
 # Model
-class Net(th.jit.ScriptModule):
+class Net(th.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(2, 3)
         self.fc2 = nn.Linear(3, 2)
         self.fc3 = nn.Linear(2, 1)
 
-    @th.jit.script_method
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -44,13 +38,15 @@ class Net(th.jit.ScriptModule):
 
 
 model = Net()
-model.id = sy.ID_PROVIDER.pop()
+data = th.tensor(th.tensor([[-10, -2.0], [1, 1.1], [11, 22.1], [-10, 1.2]]))
 
-loss_fn.id = sy.ID_PROVIDER.pop()
+traced_model = th.jit.trace(model, data)
 
-model_ptr = me.send(model, alice)
-# forward_fn_ptr = me.send_obj(model.forward, alice)
-loss_fn_ptr = me.send(loss_fn, alice)
+model_with_id = pointers.ObjectWrapper(id=sy.ID_PROVIDER.pop(), obj=traced_model)
+loss_fn_with_id = pointers.ObjectWrapper(id=sy.ID_PROVIDER.pop(), obj=loss_fn)
+
+model_ptr = me.send(model_with_id, alice)
+loss_fn_ptr = me.send(loss_fn_with_id, alice)
 
 # Create and send train config
 train_config = sy.TrainConfig(
@@ -58,25 +54,21 @@ train_config = sy.TrainConfig(
 )
 train_config.send(alice)
 
-# TODO: Returns a tensor when it should actually return a Pointer to the result Tensor.
-
 for epoch in range(5):
-    loss = alice.fit(dataset_key="vectors")
+    loss = alice.fit(dataset_key="vectors", return_id=88 + epoch)
     print("-" * 50)
     print("Iteration %s: alice's loss: %s" % (epoch, loss))
 
-print(alice)
 new_model = model_ptr.get()
 data = th.tensor([[-1, 2.0], [0, 1.1], [-1, 2.1], [0, 1.2]], requires_grad=True)
 target = th.tensor([[1.0], [0.0], [1.0], [0.0]], requires_grad=True)
 
-
-print("Evaluation before training")
+print("\nEvaluation before training")
 pred = model(data)
 loss = loss_fn(real=target, pred=pred)
 print("Loss: {}".format(loss))
 
-print("Evaluation after training:")
+print("\nEvaluation after training:")
 pred = new_model(data)
 loss = loss_fn(real=target, pred=pred)
 print("Loss: {}".format(loss))
