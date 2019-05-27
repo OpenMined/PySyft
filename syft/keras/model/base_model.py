e55529bb562bca23918b458a4f5f5ebdddea4c93
Morten Dahl <mortendahlcs@gmail.com>
2019-05-27 12:58:21 +0200

    convert into specific graph

--- syft/keras/model/base_model.py
+++ syft/keras/model/base_model.py
@@ -19,41 +19,51 @@ class Sequential(Sequential):
         super(Sequential, self).__init__(*args, **kwargs)
 
 
-    def share(self, prot=None):
+    def share(self, prot=None, target_graph=None):
 
         # Store Keras weights before loading them in the TFE layers.
         stored_keras_weights = dict()
 
+        # TODO(Morten) we could optimize runtime by running a single model.get_weights instead
         for keras_layer in self._layers:
             stored_keras_weights[keras_layer.name] = keras_layer.get_weights()
         
-        # TODO[Morten, Yann] instead of reseting the graph, we should create 
-        # a sperate graph with the TFE model different from the plaintext graph
-        tf.reset_default_graph()
+        if prot is None:
+            # If no protocol is specified we use the default
+            prot = tfe.get_protocol()
 
-        tfe_model = tfe_Sequential()
+        if target_graph is None:
+            # By default we create a new graph for the shared model
+            target_graph = tf.Graph()
 
-        with prot:
-            for keras_layer in self._layers:
-                # Don't need to instantiate 'InputLayer'. Will be automatically
-                # created when `mode.predict()`.
-                if _get_layer_name(keras_layer) == 'InputLayer':
-                    batch_input_shape = keras_layer._batch_input_shape
-                    supply_next = True
-                    if not any(batch_input_shape):
-                        raise NotImplementedError("Please provide batch_size or batch_input_shape "
-                                                  "keyword arguments when creating your syft.keras "
-                                                  "model")
-                    continue
-                if supply_next:
-                    tfe_layer  = _instantiate_tfe_layer(keras_layer, batch_input_shape, stored_keras_weights)
-                    supply_next = False
-                else:
-                    tfe_layer  = _instantiate_tfe_layer(keras_layer, None, stored_keras_weights)
+        with target_graph.as_default():
 
-                tfe_model.add(tfe_layer)
+            with prot:
 
-        return tfe_model
+                tfe_model = tfe_Sequential()
+
+                for keras_layer in self._layers:
+                    # Don't need to instantiate 'InputLayer'. Will be automatically
+                    # created when `mode.predict()`.
+                    print(keras_layer.name, _get_layer_type(keras_layer))
+                    if _get_layer_type(keras_layer) == 'InputLayer':
+                        batch_input_shape = keras_layer._batch_input_shape
+                        supply_next = True
+                        if not any(batch_input_shape):
+                            raise NotImplementedError("Please provide batch_size or batch_input_shape "
+                                                    "keyword arguments when creating your syft.keras "
+                                                    "model")
+                        continue
+                    if supply_next:
+                        tfe_layer  = _instantiate_tfe_layer(keras_layer, batch_input_shape, stored_keras_weights)
+                        supply_next = False
+                    else:
+                        tfe_layer  = _instantiate_tfe_layer(keras_layer, None, stored_keras_weights)
+
+                    tfe_model.add(tfe_layer)
+
+        # TODO(Morten) we should keep a reference to the graph instead of returning it
+        return target_graph, tfe_model
 
 
 def _instantiate_tfe_layer(keras_layer, batch_input_shape, stored_keras_weights):
@@ -61,7 +71,7 @@ def _instantiate_tfe_layer(keras_layer, batch_input_shape, stored_keras_weights)
     # Get dictionary with layer attributes
     keras_layer_attr = keras_layer.__dict__
 
-    keras_layer_name = _get_layer_name(keras_layer)
+    keras_layer_name = _get_layer_type(keras_layer)
 
     # Identify the TFE layer corresponding to the Keras layer
     tfe_layer_cls = getattr(tfe.keras.layers, keras_layer_name)
@@ -99,7 +109,7 @@ def _instantiate_tfe_layer(keras_layer, batch_input_shape, stored_keras_weights)
     return tfe_layer_cls(**tfe_kwargs)
 
 
-def _get_layer_name(keras_layer_cls):
+def _get_layer_type(keras_layer_cls):
     return keras_layer_cls.__class__.__name__
 
 def trim_tfe_args(tfe_args, not_supported_args):
