d3e95c44c81c8d9b828d7e359f2f8547b307eb41
LaRiffle <theo.leffyr@gmail.com>
2019-06-04 16:30:37 +0200

    Reorder simplifier and detailers

--- syft/serde.py
+++ syft/serde.py
@@ -1177,44 +1177,6 @@ def _detail_worker(worker: AbstractWorker, worker_tuple: tuple) -> pointers.Poin
     return referenced_worker
 
 
-def _simplify_exception(e):
-    """
-    Serialize information about an Exception which was raised to forward it
-    """
-    # Get information about the exception: type of error,  traceback
-    tp = type(e)
-    tb = e.__traceback__
-    # Serialize the traceback
-    traceback_str = "Traceback (most recent call last):\n" + "".join(traceback.format_tb(tb))
-    # Include special attributes if relevant
-    try:
-        attributes = e.get_attributes()
-    except AttributeError:
-        attributes = {}
-    return tp.__name__, traceback_str, _simplify(attributes)
-
-
-def _detail_exception(worker: AbstractWorker, error_tuple: Tuple[str, str, dict]):
-    """
-    Detail and re-raise an Exception forwarded by another worker
-    """
-    error_name, traceback_str, attributes = error_tuple
-    error_name, traceback_str = error_name.decode("utf-8"), traceback_str.decode("utf-8")
-    attributes = _detail(worker, attributes)
-    # De-serialize the traceback
-    tb = Traceback.from_string(traceback_str)
-    # Check that the error belongs to a valid set of Exceptions
-    if error_name in dir(sy.exceptions):
-        error_type = getattr(sy.exceptions, error_name)
-        error = error_type()
-        # Include special attributes if any
-        for attr_name, attr in attributes.items():
-            setattr(error, attr_name, attr)
-        reraise(error_type, error, tb.as_traceback())
-    else:
-        raise ValueError(f"Invalid Exception returned:\n{traceback_str}")
-
-
 def _force_full_simplify_worker(worker: AbstractWorker) -> tuple:
     """
 
@@ -1258,6 +1220,44 @@ def _detail_object_wrapper(
         id=obj_wrapper_tuple[0], obj=_detail(worker, obj_wrapper_tuple[1])
     )
     return obj_wrapper
+
+
+def _simplify_exception(e):
+    """
+    Serialize information about an Exception which was raised to forward it
+    """
+    # Get information about the exception: type of error,  traceback
+    tp = type(e)
+    tb = e.__traceback__
+    # Serialize the traceback
+    traceback_str = "Traceback (most recent call last):\n" + "".join(traceback.format_tb(tb))
+    # Include special attributes if relevant
+    try:
+        attributes = e.get_attributes()
+    except AttributeError:
+        attributes = {}
+    return tp.__name__, traceback_str, _simplify(attributes)
+
+
+def _detail_exception(worker: AbstractWorker, error_tuple: Tuple[str, str, dict]):
+    """
+    Detail and re-raise an Exception forwarded by another worker
+    """
+    error_name, traceback_str, attributes = error_tuple
+    error_name, traceback_str = error_name.decode("utf-8"), traceback_str.decode("utf-8")
+    attributes = _detail(worker, attributes)
+    # De-serialize the traceback
+    tb = Traceback.from_string(traceback_str)
+    # Check that the error belongs to a valid set of Exceptions
+    if error_name in dir(sy.exceptions):
+        error_type = getattr(sy.exceptions, error_name)
+        error = error_type()
+        # Include special attributes if any
+        for attr_name, attr in attributes.items():
+            setattr(error, attr_name, attr)
+        reraise(error_type, error, tb.as_traceback())
+    else:
+        raise ValueError(f"Invalid Exception returned:\n{traceback_str}")
 
 
 def _simplify_script_module(obj: torch.jit.ScriptModule) -> str:
