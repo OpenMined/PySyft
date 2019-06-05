ef4f49f8b326418c5f7ef20b8107e63ce96d24d5
Marianne Monteiro <mariannelinharesm@gmail.com>
2019-06-05 12:18:15 -0300

    Clean up federated_client

--- syft/federated/federated_client.py
+++ syft/federated/federated_client.py
@@ -10,15 +10,13 @@ logger = logging.getLogger(__name__)
 
 
 class FederatedClient(ObjectStorage):
-    """A Client able to execute federated learning in a defined dataset."""
+    """A Client able to execute federated learning in local datasets."""
 
-    def __init__(self, datasets=None, verbose=True):
+    def __init__(self, datasets=None):
         super().__init__()
         self.datasets = datasets if datasets is not None else dict()
         self.optimizer = None
         self.train_config = None
-        self.parameters = []
-        self.verbose = verbose
 
     def add_dataset(self, dataset, key: str):
         self.datasets[key] = dataset
@@ -36,9 +34,6 @@ class FederatedClient(ObjectStorage):
         if isinstance(obj, TrainConfig):
             self.train_config = obj
             self.optimizer = None
-        elif isinstance(obj, nn.parameter.Parameter):
-            self.parameters.append(obj)
-            super().set_obj(obj)
         else:
             super().set_obj(obj)
 
@@ -56,33 +51,21 @@ class FederatedClient(ObjectStorage):
 
         optimizer_name = optimizer_name.lower()
         if optimizer_name == "sgd":
-            logger.debug("sgd parameters = %s, lr = %s", model.parameters(), lr)
             self.optimizer = th.optim.SGD(model.parameters(), lr=lr)
         else:
             raise ValueError("Unknown optimizer: {}".format(optimizer_name))
         return self.optimizer
 
-    def fit(self, **kwargs):
+    def fit(self, dataset_key, **kwargs):
         if self.train_config is None:
             raise ValueError("TrainConfig not defined.")
-        if self.datasets is None:
-            logger.error("No dataset available on worker")
-            return None, -1
-        try:
-            key = kwargs["dataset"]
-        except KeyError:
-            logger.warning("Missing keyword argument 'dataset'.")
-            if len(self.datasets) == 1:
-                logger.warning("Only one dataset available, using this one.")
-                key = list(self.datasets.keys())[0]
-            else:
-                logger.warning("Available datasets: %s", list(self.datasets.keys()))
-                return None, -1
 
         model = self.get_obj(self.train_config._model_id).obj
+        loss_fn = self.get_obj(self.train_config._loss_fn_id).obj
 
         self._build_optimizer(self.train_config.optimizer, model, self.train_config.lr)
-        return self._fit(model=model, key=key)
+
+        return self._fit(model=model, dataset_key=dataset_key, loss_fn=loss_fn)
 
     def _create_batch_sampler(self, ds_key: str, shuffle: bool = False, drop_last: bool = True):
         data_range = range(len(self.datasets[ds_key]))
@@ -94,19 +77,18 @@ class FederatedClient(ObjectStorage):
         batch_sampler = BatchSampler(sampler, self.train_config.batch_size, drop_last)
         return batch_sampler
 
-    def _fit(self, model, key):
-        loss_fn = self.get_obj(self.train_config._loss_fn_id).obj
+    def _fit(self, model, dataset_key, loss_fn):
         model.train()
-        batch_sampler = self._create_batch_sampler(key)
-        loss = -1.0
-        for data_indices in batch_sampler:
-            data, target = self.datasets[key][data_indices]
-            self.optimizer.zero_grad()
-            output = model.forward(data)
-            loss = loss_fn(output, target)
-            loss.backward()
-            self.optimizer.step()
+        loss = None
+        batch_sampler = self._create_batch_sampler(dataset_key)
+
+        for epoch in range(self.train_config.epochs):
+            for data_indices in batch_sampler:
+                data, target = self.datasets[dataset_key][data_indices]
+                self.optimizer.zero_grad()
+                output = model.forward(data)
+                loss = loss_fn(output, target)
+                loss.backward()
+                self.optimizer.step()
+
         return loss
-
-
-#
