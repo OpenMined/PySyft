3e9f9b8c8722eee06682f36371794b5dd3d7e696
Silvia Gandy <silvia@midokura.com>
2019-06-07 14:06:22 +0200

    Add condition to skip tests based on pytorch version

--- test/test_serde.py
+++ test/test_serde.py
@@ -617,7 +617,10 @@ def test_serde_object_wrapper_int():
     assert obj_wrapper.id == obj_wrapper_received.id
 
 
-@pytest.mark.skip(reason="bug in pytorch version 1.1.0, jit.trace returns raw C function")
+@pytest.mark.skipif(
+    torch.__version__ >= "1.1",
+    reason="bug in pytorch version 1.1.0, jit.trace returns raw C function",
+)
 def test_serialize_and_deserialize_torch_scriptmodule():  # pragma: no cover
     @torch.jit.script
     def foo(x):
@@ -629,7 +632,10 @@ def test_serialize_and_deserialize_torch_scriptmodule():  # pragma: no cover
     assert foo.code == foo_loaded.code
 
 
-@pytest.mark.skip(reason="bug in pytorch version 1.1.0, jit.trace returns raw C function")
+@pytest.mark.skipif(
+    torch.__version__ >= "1.1",
+    reason="bug in pytorch version 1.1.0, jit.trace returns raw C function",
+)
 def test_torch_jit_script_module_serde():  # pragma: no cover
     @torch.jit.script
     def foo(x):
