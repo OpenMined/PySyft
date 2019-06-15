b42e5015f2b3e87ae00ac06efe3f381eed406b43
Marianne Linhares <mariannelinharesm@gmail.com>
2019-06-15 21:22:28 +0100

    Force MNIST download before execution

--- examples/tutorials/advanced/websockets-example-MNIST-parallel/start_websocket_servers.py
+++ examples/tutorials/advanced/websockets-example-MNIST-parallel/start_websocket_servers.py
@@ -1,5 +1,20 @@
 import subprocess
 
+
+from torchvision import datasets
+from torchvision import transforms
+
+
+# Downloads MNIST dataset
+mnist_trainset = datasets.MNIST(
+    root="./data",
+    train=True,
+    download=True,
+    transform=transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+    ),
+)
+
 call_alice = ["python", "run_websocket_server.py", "--port", "8777", "--id", "alice"]
 
 call_bob = ["python", "run_websocket_server.py", "--port", "8778", "--id", "bob"]
