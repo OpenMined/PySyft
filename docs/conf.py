d43ac308a60f1b3ac1dda4a7ec1e649ddc0f60c9
Bobby Wagner <bobby.wagner@augustusai.com>
2019-06-18 11:09:02 -0400

    bumpversion 0.1.18a1 -> 0.1.19a1

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = "2019, OpenMinedContributors"
 author = "Andrew Trask"
 
 # The short X.Y version
-version = "0.1.18"
+version = "0.1.19a1"
 # The full version, including alpha/beta/rc tags
-release = "0.1.18"
+release = "0.1.19a1"
 
 
 # -- General configuration ---------------------------------------------------
