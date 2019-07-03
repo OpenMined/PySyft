37cc8d061f1f28762e85dc274a66a9fd1c61a594
paumier_jas <jason.paumier@supelec.fr>
2019-07-03 09:43:00 +0200

    compute coeffs to solve system only once

--- syft/frameworks/torch/tensors/interpreters/chinese_remainder_theorem.py
+++ syft/frameworks/torch/tensors/interpreters/chinese_remainder_theorem.py
@@ -25,6 +25,7 @@ class CRTTensor(AbstractTensor):
         residues: dict = None,
         base=None,
         precision_fractional=None,
+        solve_system_coeffs=None,
         owner=None,
         id=None,
         tags: set = None,
@@ -68,8 +69,19 @@ class CRTTensor(AbstractTensor):
                     r.shape == res_shape
                 ), "All residue tensors of CRTTensor must have the same shape"
 
+        # In order to avoid computing the coefficients to solve the modular system several times,
+        # we can store them once computed. See the docstring of the solve_system method for more details.
+        if residues is not None and solve_system_coeffs is None:
+            solve_system_coeffs = self.compute_solve_system_coeffs()
+        self.solve_system_coeffs = solve_system_coeffs
+
     def get_class_attributes(self):
-        return {"base": self.base, "precision_fractional": self.precision_fractional}
+        return {
+            "base": self.base,
+            "precision_fractional": self.precision_fractional,
+            # Solving coefficients can be reused in results of operations (see docstring of solve_system method)
+            "solve_system_coeffs": self.solve_system_coeffs,
+        }
 
     def __str__(self):
         type_name = type(self).__name__
@@ -99,9 +111,9 @@ class CRTTensor(AbstractTensor):
 
         dict_result = {}
         for mod in self_.keys():
-            result.field = mod
-            dict_result[mod] = result
-
+            # This is a bit ugly, I want the result to be stored in a CRT tensor with same modulos
+            result.child.field = mod
+            dict_result[mod] = result.copy()
         return dict_result
 
     __eq__ = eq
@@ -154,7 +166,7 @@ class CRTTensor(AbstractTensor):
         result = {}
 
         for mod in self_.keys():
-            result[mod] = self_[mod].sum() % mod  # TODO why % mod is needed?
+            result[mod] = self_[mod].sum() % mod  # FIXME why % mod is needed?
 
         return result
 
@@ -164,16 +176,34 @@ class CRTTensor(AbstractTensor):
         The algorithm consists in:
         1) Compute N = prod(self.child.keys())
         2) yi = N / ni where the ni's are modulos in the system
-        3) zi = yi^(-1) mod ni (we know zi exists because all the original modulos are pairwise coprime
+        3) zi = yi^(-1) mod ni (we know zi exists because all the original modulos are pairwise coprime)
         4) The result is x = sum(ai * yi * zi) where ai is the residue modulo ni in the system
+        
+        We can see that this boils down to a linear combination of ai's with coefficients yi * zi.
+        These coefficients depend only on the modulos used to represent the tensor so we don't need
+        to compute them several times for the same tensor. We can also reuse them for a tensor produced
+        by operations on self because 1) the operations can only be done between tensors with the same modulos
+        and 2) the output tensor will have the same modulos.
+        This is why we store them after having computed them once.
         """
+        res = 0
         N = 1
-        for mod in self.child.keys():
+        # We need to sort the dict to be sure that the coeffs are retrieved in the same order they were stored
+        for i, (mod, ai) in enumerate(sorted(self.child.items())):
+            ai = (ai.float_prec() % mod).long()
+            res += ai * self.solve_system_coeffs[i]
             N *= mod
 
+        return FixedPrecisionTensor(
+            field=N, base=self.base, precision_fractional=self.precision_fractional
+        ).on(res % N)
+
+    def compute_solve_system_coeffs(self):
         def modular_inverse(a, b):
             """ Computes the modular inverse x = a^(-1) mod b
             with Euclid's extended algorithm.
+            The coefficient are computed and stored in a list in ascending order of modulos.
+            The list is then turned to a tuple to be hashable and passed via the get_class_attributes method.
             """
             b0 = b
             x0, x1 = 0, 1
@@ -187,16 +217,17 @@ class CRTTensor(AbstractTensor):
                 x1 += b0
             return x1
 
-        res = 0
-        for mod, ai in self.child.items():
-            ai = (ai.float_prec() % mod).long()
+        N = 1
+        for mod in self.child.keys():
+            N *= mod
+
+        coeffs = []
+        for mod, ai in sorted(self.child.items()):
             yi = N // mod
             zi = modular_inverse(yi, mod)
-            res += ai * yi * zi
+            coeffs.append(yi * zi)
 
-        return FixedPrecisionTensor(
-            field=N, base=self.base, precision_fractional=self.precision_fractional
-        ).on(res % N)
+        return tuple(coeffs)
 
     @staticmethod
     @overloaded.module
