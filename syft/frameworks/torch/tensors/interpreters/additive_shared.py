0af0d062694042a05416e686fb2ffe1b6c3dce43
LaRiffle <theo.leffyr@gmail.com>
2019-07-12 11:07:12 +0200

    Disable gc-ing shared when simplying an AdditiveSharedTensor

--- syft/frameworks/torch/tensors/interpreters/additive_shared.py
+++ syft/frameworks/torch/tensors/interpreters/additive_shared.py
@@ -842,6 +842,10 @@ class AdditiveSharingTensor(AbstractTensor):
         chain = None
         if hasattr(tensor, "child"):
             chain = sy.serde._simplify(tensor.child)
+
+        # Don't delete the remote values of the shares at simplification
+        tensor.set_garbage_collect_data(False)
+
         return (tensor.id, tensor.field, tensor.crypto_provider.id, chain)
 
     @staticmethod
