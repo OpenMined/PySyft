a7853f70b19b13dc94d512627687579f7b142186
paumier_jas <jason.paumier@supelec.fr>
2019-07-18 09:31:50 +0200

    more interesting tests

--- test/torch/tensors/test_chinese_remainder.py
+++ test/torch/tensors/test_chinese_remainder.py
@@ -4,7 +4,7 @@ import syft
 
 def test__str__():
     t = torch.tensor([[3, 9], [4, 1]])
-    crt = t.fix_precision(field_type="int100", precision_fractional=0, storage="crt")
+    crt = t.fix_precision(field_type="int100", precision_fractional=2, storage="crt")
 
     assert isinstance(crt.__str__(), str)
 
@@ -14,9 +14,9 @@ def test_eq():
     t_b = torch.tensor([[3, 9], [4, 1]])
     t_c = torch.tensor([[2, 9], [4, 2]])
 
-    crt_a = t_a.fix_precision(field_type="int100", precision_fractional=0, storage="crt")
-    crt_b = t_b.fix_precision(field_type="int100", precision_fractional=0, storage="crt")
-    crt_c = t_c.fix_precision(field_type="int100", precision_fractional=0, storage="crt")
+    crt_a = t_a.fix_precision(field_type="int100", precision_fractional=2, storage="crt")
+    crt_b = t_b.fix_precision(field_type="int100", precision_fractional=2, storage="crt")
+    crt_c = t_c.fix_precision(field_type="int100", precision_fractional=2, storage="crt")
 
     eq_ab = (crt_a == crt_b).float_precision()
     eq_ac = (crt_a == crt_c).float_precision()
@@ -27,7 +27,7 @@ def test_eq():
 
 def test__neg__():
     t = torch.tensor([[1, -3], [3, -2]])
-    crt = t.fix_precision(field_type="int100", precision_fractional=0, storage="crt")
+    crt = t.fix_precision(field_type="int100", precision_fractional=2, storage="crt")
 
     neg = -crt
 
@@ -38,8 +38,8 @@ def test_add():
     t_a = torch.tensor([[1, 2], [3, -4]])
     t_b = torch.tensor([[1, -3], [3, -2]])
 
-    crt_a = t_a.fix_precision(field_type="int100", precision_fractional=0, storage="crt")
-    crt_b = t_b.fix_precision(field_type="int100", precision_fractional=0, storage="crt")
+    crt_a = t_a.fix_precision(field_type="int100", precision_fractional=2, storage="crt")
+    crt_b = t_b.fix_precision(field_type="int100", precision_fractional=2, storage="crt")
 
     result = crt_a + crt_b
 
@@ -55,8 +55,8 @@ def test_sub():
     t_a = torch.tensor([[1, 2], [5, -4]])
     t_b = torch.tensor([[1, -3], [3, -2]])
 
-    crt_a = t_a.fix_precision(field_type="int100", precision_fractional=0, storage="crt")
-    crt_b = t_b.fix_precision(field_type="int100", precision_fractional=0, storage="crt")
+    crt_a = t_a.fix_precision(field_type="int100", precision_fractional=2, storage="crt")
+    crt_b = t_b.fix_precision(field_type="int100", precision_fractional=2, storage="crt")
 
     result = crt_a - crt_b
 
@@ -74,8 +74,8 @@ def test_mul():
     t_a = torch.tensor([[1, -2], [3, -2]])
     t_b = torch.tensor([[1, 2], [3, -2]])
 
-    crt_a = t_a.fix_precision(field_type="int100", precision_fractional=0, storage="crt")
-    crt_b = t_b.fix_precision(field_type="int100", precision_fractional=0, storage="crt")
+    crt_a = t_a.fix_precision(field_type="int100", precision_fractional=2, storage="crt")
+    crt_b = t_b.fix_precision(field_type="int100", precision_fractional=2, storage="crt")
 
     result = crt_a * crt_b
 
@@ -91,7 +91,7 @@ def test_send_and_get(workers):
     alice, bob = (workers["alice"], workers["bob"])
 
     t = torch.tensor([1, 2])
-    crt = t.fix_precision(field_type="int100", precision_fractional=0, storage="crt")
+    crt = t.fix_precision(field_type="int100", precision_fractional=2, storage="crt")
 
     to_alice = crt.send(alice)
     to_alice_id = to_alice.id_at_location
@@ -121,8 +121,8 @@ def test_share_and_get(workers):
     alice, bob, james = (workers["alice"], workers["bob"], workers["james"])
 
     t = torch.tensor([1, 2])
-    crt = t.fix_precision(field_type="int100", precision_fractional=0, storage="crt")
-    copy = t.fix_precision(field_type="int100", precision_fractional=0, storage="crt")
+    crt = t.fix_precision(field_type="int100", precision_fractional=2, storage="crt")
+    copy = t.fix_precision(field_type="int100", precision_fractional=2, storage="crt")
 
     shared = crt.share(alice, bob, crypto_provider=james)
     back = shared.get()
