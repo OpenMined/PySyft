4d8cb0de436d7335bba6eb0a4a18402698ad3964
1000ping <wajahat102@ymail.com>
2019-07-31 00:13:59 +0500

    black

--- examples/tutorials/advanced/websockets-example-MNIST/run_websocket_server.py
+++ examples/tutorials/advanced/websockets-example-MNIST/run_websocket_server.py
@@ -23,15 +23,10 @@ def start_proc(participant, kwargs):  # pragma: no cover
 parser = argparse.ArgumentParser(description="Run websocket server worker.")
 
 parser.add_argument(
-    "--port",
-    "-p",
-    type=int,
-    help="port number of the websocket server worker, e.g. --port 8777",
+    "--port", "-p", type=int, help="port number of the websocket server worker, e.g. --port 8777"
 )
 
-parser.add_argument(
-    "--host", type=str, default="localhost", help="host for the connection"
-)
+parser.add_argument("--host", type=str, default="localhost", help="host for the connection")
 
 parser.add_argument(
     "--id", type=str, help="name (id) of the websocket server worker, e.g. --id alice"
