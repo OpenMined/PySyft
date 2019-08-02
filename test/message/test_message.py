9317ac18bf31880d542e943f9722d1cee5a1ae19
iamtrask <liamtrask@gmail.com>
2019-08-02 14:24:41 +0100

    rmeoved extraneous imports

--- test/message/test_message.py
+++ test/message/test_message.py
@@ -1,20 +1,4 @@
 import syft as sy
-import syft
-
-import torch as th
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-
-from syft.serde.serde import deserialize
-from syft.serde.serde import serialize
-import time
-
-import pytest
-import unittest.mock as mock
-
-from syft.workers import WebsocketClientWorker
-from syft.workers import WebsocketServerWorker
 
 
 def test_message_serde(hook):
