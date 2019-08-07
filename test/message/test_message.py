34cef68bda14455603d08f883e283c1f1a1043ea
iamtrask <liamtrask@gmail.com>
2019-08-07 19:46:26 +0100

    Added unit tests for CommandMessage, ObjectMessage, and ObjectRequsetMessage

--- test/message/test_message.py
+++ test/message/test_message.py
@@ -1,7 +1,8 @@
 import syft as sy
+import torch as th
 
 
-def test_message_serde(hook):
+def test_message_serde():
 
     x = sy.Message(0, [1, 2, 3])
     x_bin = sy.serde.serialize(x)
@@ -9,3 +10,40 @@ def test_message_serde(hook):
 
     assert x.contents == y.contents
     assert x.msg_type == y.msg_type
+
+def test_cmd_message(workers):
+
+    bob = workers["bob"]
+
+    x = th.tensor([1,2,3,4]).send(bob)
+
+    y = (x + x) # this is the test
+
+    y = y.get()
+
+    assert (y==th.tensor([2,4,6,8])).all()
+
+def test_obj_message(workers):
+
+    bob = workers["bob"]
+
+    x = th.tensor([1,2,3,4]).send(bob) # this is the test
+
+    y = (x + x)
+
+    y = y.get()
+
+    assert (y==th.tensor([2,4,6,8])).all()
+
+def test_obj_req_message(workers):
+
+    bob = workers["bob"]
+
+    x = th.tensor([1,2,3,4]).send(bob)
+
+    y = (x + x)
+
+    y = y.get() # this is the test
+
+    assert (y==th.tensor([2,4,6,8])).all()
+
