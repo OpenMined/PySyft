accad6dad4ed9569bbb790fd695ef8a99571f002
Daniele Gadler <daniele.gadler@yahoo.it>
2019-08-10 09:16:03 +0200

    Added hooked remote version of the torch/nn/utils/clip_grad.py method

--- syft/frameworks/torch/hook/hook.py
+++ syft/frameworks/torch/hook/hook.py
@@ -27,6 +27,8 @@ from syft.frameworks.torch.tensors.interpreters.abstract import _apply_args
 
 from syft.exceptions import route_method_exception
 from syft.exceptions import TensorsNotCollocatedException
+from torch._six import inf
+
 
 
 class TorchHook:
@@ -424,6 +426,9 @@ class TorchHook:
             return self
 
         torch.nn.Parameter.grad = grad
+        
+        
+        
 
     def _hook_torch_module(self):
         """Overloads functions in the main torch modules.
@@ -481,6 +486,8 @@ class TorchHook:
                     continue
 
                 perform_overloading(torch_module, func)
+                
+                
 
     def get_hooked_pointer_method(hook_self, attr):
         """
@@ -1134,3 +1141,49 @@ class TorchHook:
             return optim_self
 
         self.torch.optim.Optimizer.float_precision = optim_float_precision_
+        
+        
+        #Modification of torch/nn/utils/clip_grad.py
+        def clip_grad_norm_remote_(parameters, max_norm, worker=None, norm_type=2):
+            r"""Clips gradient norm of an iterable of parameters stored over a remote model
+        
+            The norm is computed over all gradients together, as if they were
+            concatenated into a single vector. Gradients are modified in-place.
+        
+            Arguments:
+                - parameters (Iterable[Tensor] or Tensor): an iterable of PySyft remote
+                Tensors or a single Tensor that will have gradients normalized
+                - max_norm (float or int): max norm of the gradients
+                - worker: The worker where the parameters are hosted and where the gradient clipping
+                will be performed
+                - norm_type (float or int): type of the used p-norm. Can be ``'inf'`` for
+                    infinity norm.
+        
+            Returns:
+                Total norm of the parameters (viewed as a single vector).
+            """
+            print("In remote method")
+            if isinstance(parameters, torch.Tensor):
+                parameters = [parameters]
+            parameters = list(filter(lambda p: p.grad is not None, parameters))
+            max_norm = float(max_norm)
+            norm_type = float(norm_type)
+            if norm_type == inf:
+                total_norm = max(p.grad.data.abs().max() for p in parameters)
+            else:
+                 #Init tensor with value 0 and dimension 1 (plain scalar)  
+                total_norm = torch.Tensor(1)   
+                total_norm = total_norm.send(worker)
+                for param in parameters:
+                    param_norm = param.grad.data.norm(norm_type)
+                    total_norm += param_norm ** norm_type 
+        
+                total_norm = total_norm ** (1. / norm_type)
+                
+            clip_coef = max_norm / (total_norm + 1e-6)
+            if clip_coef < 1:
+                for param in parameters:
+                    param.grad.data.mul_(clip_coef)
+            return total_norm
+        
+        self.torch.torch.nn.utils.clip_grad = clip_grad_norm_remote_
