1abefdfd8910109c00daad089187d7e88b3bc423
paumier_jas <jason.paumier@supelec.fr>
2019-08-13 18:48:13 +0200

    changed error to warning

--- syft/frameworks/torch/tensors/interpreters/native.py
+++ syft/frameworks/torch/tensors/interpreters/native.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 import math
+import warnings
 
 from typing import List
 from typing import Union
@@ -683,9 +684,10 @@ class TorchTensor(AbstractTensor):
         else:
             assert not need_large_prec, "This tensor needs large precision to be correctly stored"
             if "internal_type" in kwargs:
-                raise TypeError(
-                    "internal_type not needed if data does not need LargePrecisionTensor to be stored"
+                warnings.warn(
+                    "do not provide internal_type if data does not need LargePrecisionTensor to be stored"
                 )
+                del kwargs["internal_type"]
             return syft.FixedPrecisionTensor(*args, **kwargs).on(self).enc_fix_prec().wrap()
 
     fix_precision = fix_prec
