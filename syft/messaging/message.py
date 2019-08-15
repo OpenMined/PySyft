fe6888cd4b9c81884157adab7fd72c3563ad3439
iamtrask <liamtrask@gmail.com>
2019-08-15 12:57:17 +0100

    Add documentation for ObjectRequestMessage

--- syft/messaging/message.py
+++ syft/messaging/message.py
@@ -190,13 +190,35 @@ class ObjectMessage(Message):
 
 
 class ObjectRequestMessage(Message):
+    """Request another worker to send one of its objects
+
+    If ObjectMessage pushes an object to another worker, this Message type pulls an
+    object from another worker. It also assumes that the other worker will delete it's
+    local copy of the object after sending it to you."""
+
     # TODO: add more efficient detalier and simplifier custom for this type
 
     def __init__(self, contents):
+        """Initialize the message using default Message constructor.
+
+        See Message.__init__ for details."""
         super().__init__(codes.MSGTYPE.OBJ_REQ, contents)
 
     @staticmethod
-    def detail(worker: AbstractWorker, tensor_tuple: tuple) -> "Message":
+    def detail(worker: AbstractWorker, tensor_tuple: tuple) -> "ObjectRequestMessage":
+        """
+        This function takes the simplified tuple version of this message and converts
+        it into an ObjectRequestMessage. The simplify() method runs the inverse of this method.
+
+        Args:
+            worker (AbstractWorker): a reference to the worker necessary for detailing. Read
+                syft/serde/serde.py for more information on why this is necessary.
+            msg_tuple (Tuple): the raw information being detailed.
+        Returns:
+            ptr (ObjectRequestMessage): a ObjectRequestMessage.
+        Examples:
+            message = detail(sy.local_worker, msg_tuple)
+        """
         return ObjectRequestMessage(sy.serde._detail(worker, tensor_tuple[1]))
 
 
