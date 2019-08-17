432b0cea999e6aa15ea06b797509503e2a45863f
Yugandhartripathi <tripathi.yugandhar@gmail.com>
2019-08-18 00:04:36 +0530

    moved generate_triple code to request_triple

--- syft/workers/base.py
+++ syft/workers/base.py
@@ -818,29 +818,6 @@ class BaseWorker(AbstractWorker, ObjectStorage):
         """
         return self.search(*query_items)
 
-    def generate_triple(
-        self, cmd: Callable, field: int, a_size: tuple, b_size: tuple, locations: list
-    ):
-        """Generates a multiplication triple and sends it to all locations.
-
-        Args:
-            cmd: An equation in einsum notation.
-            field: An integer representing the field size.
-            a_size: A tuple which is the size that a should be.
-            b_size: A tuple which is the size that b should be.
-            locations: A list of workers where the triple should be shared between.
-
-        Returns:
-            A triple of AdditiveSharedTensors such that c_shared = cmd(a_shared, b_shared).
-        """
-        a = self.torch.randint(field, a_size)
-        b = self.torch.randint(field, b_size)
-        c = cmd(a, b)
-        a_shared = a.share(*locations, field=field, crypto_provider=self).child
-        b_shared = b.share(*locations, field=field, crypto_provider=self).child
-        c_shared = c.share(*locations, field=field, crypto_provider=self).child
-        return a_shared, b_shared, c_shared
-
     def _get_msg(self, index):
         """Returns a decrypted message from msg_history. Mostly useful for testing.
 
