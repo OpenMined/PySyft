5212fad6823707e8cb96b9c5cb8e875eae8c21ca
Andre Farias <andre.lmfarias@gmail.com>
2019-08-20 00:03:40 +0200

    test FL with RNNs

--- test/torch/nn/test_rnn.py
+++ test/torch/nn/test_rnn.py
@@ -1,5 +1,7 @@
 import pytest
 import torch as th
+from torch import nn
+from torch import optim
 import syft as sy
 from syft.frameworks.torch.nn import RNN
 from syft.frameworks.torch.nn import GRU
@@ -7,7 +9,7 @@ from syft.frameworks.torch.nn import LSTM
 
 
 @pytest.mark.parametrize("module", ["lstm", "gru", "rnn_tanh", "rnn_relu"])
-def test_rnn(module, hook, workers):
+def test_rnn_mpc(module, hook, workers):
     """
     Testing RNN modules with MPC
     """
@@ -125,3 +127,86 @@ def test_rnn(module, hook, workers):
         assert h.get().float_precision().shape == th.Size(
             [batch_size, directions * num_layers, hidden_size]
         )
+
+
+@pytest.mark.parametrize("module", ["lstm", "gru", "rnn_tanh", "rnn_relu"])
+def test_rnn_federated(module, hook, workers):
+    """
+    Testing Federated Learning with RNN modules
+    """
+
+    bob = workers["bob"]
+    alice = workers["alice"]
+
+    # model hyperparameters
+    data_size = 8
+    input_size = 3
+    hidden_size = 5
+    seq_len = 4
+    num_layers = 1
+    bidirectional = True
+
+    # Create toy dataset and federated loader
+    x = th.randn(data_size, seq_len, input_size)
+    y = th.rand(data_size)
+
+    idx = int(data_size / 2)
+    bob_dataset = sy.BaseDataset(x[:idx], y[:idx]).send(bob)
+    alice_dataset = sy.BaseDataset(x[idx:], y[idx:]).send(alice)
+    federated_dataset = sy.FederatedDataset([bob_dataset, alice_dataset])
+    federated_loader = sy.FederatedDataLoader(federated_dataset, shuffle=True, batch_size=2)
+
+    # Build Model
+    if module == "lstm":
+        model = LSTM(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            bidirectional=bidirectional,
+            batch_first=True,
+        )
+    if module == "gru":
+        model = GRU(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            bidirectional=bidirectional,
+            batch_first=True,
+        )
+    if module == "rnn_tanh":
+        model = RNN(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            nonlinearity="tanh",
+            bidirectional=bidirectional,
+            batch_first=True,
+        )
+    if module == "rnn_relu":
+        model = RNN(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            nonlinearity="relu",
+            bidirectional=bidirectional,
+            batch_first=True,
+        )
+
+    # Training
+    opt = optim.SGD(model.parameters(), lr=0.01)
+    losses = []
+    for e in range(10):
+        current_losses = []
+        for data, target in federated_loader:
+            model.send(data.location)
+            opt.zero_grad()
+            output, _ = model(data)
+            pred = output.sum(dim=1).sum(dim=1)
+            loss = ((pred - target) ** 2).sum()
+            loss.backward()
+            opt.step()
+            current_losses.append(loss.get())
+            model = model.get()
+        losses.append(sum(current_losses))
+
+    assert losses[0] > losses[-1]
