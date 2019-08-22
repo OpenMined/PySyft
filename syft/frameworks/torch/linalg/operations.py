94d27bf3f72caa0bfc9a02d19d1c08cd53c6e632
Andre Farias <andre.lmfarias@gmail.com>
2019-08-22 09:04:17 +0200

    black

--- syft/frameworks/torch/linalg/operations.py
+++ syft/frameworks/torch/linalg/operations.py
@@ -4,6 +4,13 @@ from typing import List
 from syft.workers import BaseWorker
 
 
+def qr_remote(a):
+    # TODO
+    pass
+
+
 def qr_mpc(a, workers: List, crypto_provider: BaseWorker):
+    # TODO
     # a = a.float_precision().share(*workers, crypto_provider=crypto_provider)
-    m, n = a.shape
+    # m, n = a.shape
+    pass
