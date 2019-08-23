3e57cac8040728d6c04b609d5e6dc0b0c9e407f9
Andre Farias <andre.lmfarias@gmail.com>
2019-08-23 18:22:02 +0200

    implem draft of ldl decomposition

--- syft/frameworks/torch/linalg/operations.py
+++ syft/frameworks/torch/linalg/operations.py
@@ -4,6 +4,26 @@ from typing import List
 from syft.workers import BaseWorker
 
 
+def inv_sym(a):  # , workers: List, crypto_provider: BaseWorker):
+    # TODO
+    pass
+
+
+def ldl(a):
+    n = a.shape[0]
+    l = th.zeros_like(a)
+    d = th.diag(l).copy()
+    inv_d = d.copy()
+
+    for i in range(n):
+        d[i] = a[i, i] - (l[i, :i] ** 2 * d[:i]).sum()
+        inv_d[i] = 1.0 / d[i]
+        for j in range(i, n):
+            l[j, i] = (a[j, i] - (l[j, :i] * l[i, :i] * d[:i]).sum()) * inv_d[i]
+
+    return l, d, inv_d
+
+
 def qr_remote(a):
     # TODO
     pass
@@ -11,6 +31,4 @@ def qr_remote(a):
 
 def qr_mpc(a, workers: List, crypto_provider: BaseWorker):
     # TODO
-    # a = a.float_precision().share(*workers, crypto_provider=crypto_provider)
-    # m, n = a.shape
     pass
