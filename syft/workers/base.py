998993cdf3e2579eb32cfb21f28f59b6c0556a41
Marianne Monteiro <mariannelinharesm@gmail.com>
2019-08-26 13:49:41 -0300

    Move serde from VirtualWorker to BaseWorker

--- syft/workers/base.py
+++ syft/workers/base.py
@@ -551,7 +551,7 @@ class BaseWorker(AbstractWorker, ObjectStorage):
 
     def get_worker(
         self, id_or_worker: Union[str, int, "BaseWorker"], fail_hard: bool = False
-    ) -> Union[str, int]:
+    ) -> Union[str, int, AbstractWorker]:
         """Returns the worker id or instance.
 
         Allows for resolution of worker ids to workers to happen automatically
@@ -876,3 +876,44 @@ class BaseWorker(AbstractWorker, ObjectStorage):
         self._known_workers[remote_worker_id] = sy.workers.WebsocketClientWorker(
             id=remote_worker_id, port=port, **kwargs
         )
+
+    @staticmethod
+    def simplify(worker: AbstractWorker) -> tuple:
+        return (sy.serde._simplify(worker.id),)
+
+    @staticmethod
+    def detail(worker: AbstractWorker, worker_tuple: tuple) -> Union["AbstractWorker", int, str]:
+        """
+        This function reconstructs a PlanPointer given it's attributes in form of a tuple.
+
+        Args:
+            worker: the worker doing the deserialization
+            plan_pointer_tuple: a tuple holding the attributes of the PlanPointer
+        Returns:
+            A worker id or worker instance.
+        """
+        worker_id = sy.serde._detail(worker, worker_tuple[0])
+
+        referenced_worker = worker.get_worker(worker_id)
+
+        return referenced_worker
+
+    @staticmethod
+    def force_simplify(worker: AbstractWorker) -> tuple:
+        return (sy.serde._simplify(worker.id), sy.serde._simplify(worker._objects), worker.auto_add)
+
+    @staticmethod
+    def force_detail(worker: AbstractWorker, worker_tuple: tuple) -> tuple:
+        worker_id, _objects, auto_add = worker_tuple
+        worker_id = sy.serde._detail(worker, worker_id)
+
+        result = sy.VirtualWorker(sy.hook, worker_id, auto_add=auto_add)
+        _objects = sy.serde._detail(worker, _objects)
+        result._objects = _objects
+
+        # make sure they weren't accidentally double registered
+        for _, obj in _objects.items():
+            if obj.id in worker._objects:
+                del worker._objects[obj.id]
+
+        return result
