f81b4d2905eff18ce295755f79292862d3d36c4d
Marianne Monteiro <mariannelinharesm@gmail.com>
2019-08-28 13:11:22 -0300

    Style correction

--- syft/workers/base.py
+++ syft/workers/base.py
@@ -610,14 +610,13 @@ class BaseWorker(AbstractWorker, ObjectStorage):
         if worker_id == self.id:
             return self
 
-        worker = self._known_workers.get(worker_id)
+        worker = self._known_workers.get(worker_id, d=worker_id)
 
-        if worker is None:
+        if worker == worker_id:
             if fail_hard:
                 raise WorkerNotFoundException
-            logger.warning("Worker %s couldn't recognize worker %s", self.id, worker_id)
-            return worker_id
 
+        logger.warning("Worker %s couldn't recognize worker %s", self.id, worker_id)
         return worker
 
     def add_worker(self, worker: "BaseWorker"):
