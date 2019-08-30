adc2ece9c24ccf4b4004e84a48bb9ebe37ce2a14
Marianne Monteiro <mariannelinharesm@gmail.com>
2019-08-30 10:17:50 -0300

    Remove unsupported arguments

--- syft/frameworks/torch/crypto/securenn.py
+++ syft/frameworks/torch/crypto/securenn.py
@@ -5,8 +5,6 @@ https://eprint.iacr.org/2018/442.pdf
 Note that there is a difference here in that our shares can be
 negative numbers while they are always positive in the paper
 """
-import math
-
 import torch
 import syft as sy
 
@@ -586,22 +584,13 @@ def maxpool_deriv(x_sh):
     return maxpool_d_sh.view(n1, n2)
 
 
-def maxpool2d(
-    a_sh,
-    kernel_size: int = 1,
-    stride: int = 1,
-    padding: int = 0,
-    dilation: int = 1,
-    ceil_mode: bool = False,
-):
+def maxpool2d(a_sh, kernel_size: int = 1, stride: int = 1, padding: int = 0):
     """Applies a 2D max pooling over an input signal composed of several input planes.
     This interface is similar to torch.nn.MaxPool2D.
     Args:
         kernel_size: the size of the window to take a max over
         stride: the stride of the window
         padding: implicit zero padding to be added on both sides
-        dilation: a parameter that controls the stride of elements in the window
-        ceil_mode: when True, will use ceil instead of floor to compute the output shape
     """
     assert len(a_sh.shape) == 4
 
@@ -609,17 +598,18 @@ def maxpool2d(
     kernel = torch.nn.modules.utils._pair(kernel_size)
     stride = torch.nn.modules.utils._pair(stride)
     padding = torch.nn.modules.utils._pair(padding)
-    dilation = torch.nn.modules.utils._pair(dilation)
+
+    # TODO: support dilation.
+    dilation = torch.nn.modules.utils._pair(1)
 
     # Extract a few useful values
-    batch_size, nb_channels_in, nb_rows_in, nb_cols_in = a_sh.shape
+    batch_size, nb_channels, nb_rows_in, nb_cols_in = a_sh.shape
 
-    # ########## Calculate output shapes ###############
-    round_op = math.ceil if ceil_mode else math.floor
-    nb_rows_out = round_op(
+    # Calculate output shapes
+    nb_rows_out = int(
         (nb_rows_in + 2 * padding[0] - dilation[0] * (kernel[0] - 1) - 1) / stride[0] + 1
     )
-    nb_cols_out = round_op(
+    nb_cols_out = int(
         (nb_cols_in + 2 * padding[1] - dilation[1] * (kernel[1] - 1) - 1) / stride[1] + 1
     )
 
@@ -634,13 +624,13 @@ def maxpool2d(
 
     # TODO: use array instead of a sharing zeros
     res = _shares_of_zero(
-        (batch_size, nb_channels_in, nb_rows_out, nb_cols_out),
+        (batch_size, nb_channels, nb_rows_out, nb_cols_out),
         a_sh.child.child.field,
         a_sh.child.child.crypto_provider,
         *a_sh.child.child.locations,
     )
     for batch in range(batch_size):
-        for channel in range(nb_channels_in):
+        for channel in range(nb_channels):
             r_out = 0
             for r_in in range(0, nb_rows_in - (kernel[0] - 1), stride[0]):
                 c_out = 0
