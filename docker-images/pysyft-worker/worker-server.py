69ece8898d245891483c84c7963b11861f4e4eb0
youben11 <ayouben9@gmail.com>
2019-09-02 17:24:02 +0100

    running black

--- docker-images/pysyft-worker/worker-server.py
+++ docker-images/pysyft-worker/worker-server.py
@@ -11,31 +11,24 @@ def get_args():
         "-p",
         type=int,
         default=8777,
-        help="port number of the websocket server worker, e.g. --port 8777"
+        help="port number of the websocket server worker, e.g. --port 8777",
     )
+    parser.add_argument("--host", type=str, default="0.0.0.0", help="host for the connection")
     parser.add_argument(
-        "--host",
-        type=str,
-        default="0.0.0.0",
-        help="host for the connection"
-    )
-    parser.add_argument(
-        "--id",
-        type=str,
-        help="name (id) of the websocket server worker, e.g. --id alice"
+        "--id", type=str, help="name (id) of the websocket server worker, e.g. --id alice"
     )
     parser.add_argument(
         "--verbose",
         "-v",
         action="store_true",
-        help="if set, websocket server worker will be started in verbose mode"
+        help="if set, websocket server worker will be started in verbose mode",
     )
 
     args = parser.parse_args()
     return args
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     hook = sy.TorchHook(torch)
     args = get_args()
     kwargs = {
