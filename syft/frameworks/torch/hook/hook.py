8c1bb2313333b3e36d75f37577295e772d469cbb
Andre Farias <andre.lmfarias@gmail.com>
2019-09-03 12:03:29 +0200

    added __module__ to excluded native methods to be added

--- syft/frameworks/torch/hook/hook.py
+++ syft/frameworks/torch/hook/hook.py
@@ -510,6 +510,7 @@ class TorchHook(FrameworkHook):
             "__init__",
             "__init_subclass__",
             "__weakref__",
+            "__module__",
             "__ne__",
             "__new__",
             "__reduce__",
