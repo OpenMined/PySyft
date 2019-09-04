a118b10e0d921fca762da2421b2912ad01ab0e26
LaRiffle <theo.leffyr@gmail.com>
2019-09-04 11:50:52 +0200

    Fix .get() for Autograd tensor and shared(requires_grad=True) tensors

--- syft/frameworks/torch/tensors/interpreters/autograd.py
+++ syft/frameworks/torch/tensors/interpreters/autograd.py
@@ -259,10 +259,19 @@ class AutogradTensor(AbstractTensor):
     def get(self):
         """Just a pass through. This is most commonly used when calling .get() on a
         AutogradTensor which has also been shared."""
-        self.child = self.child.get()
-        # Remove the autograd node if a simple tensor is received
-        if isinstance(self.child, torch.Tensor) and not self.child.is_wrapper:
-            return self.child
+        tensor = self.child.get()
+
+        if isinstance(tensor, torch.Tensor):
+            # Remove the autograd node if a simple tensor is received
+            if not tensor.is_wrapper:
+                return tensor
+            # If it's a wrapper, then insert the autograd under the wrapper
+            else:
+                self.child = tensor.child
+                tensor.child = self
+                return tensor
+
+        self.child = tensor
         return self
 
     def float_precision(self):
