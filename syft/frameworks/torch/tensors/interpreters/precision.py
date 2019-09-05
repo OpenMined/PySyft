28cc0c24dccff80e0f9e4a79e0e331bd1c1eed0e
kamathhrishi@gmail.com <kamathhrishi@gmail.com>
2019-09-05 18:51:21 +0530

    Fix merge conflict

--- syft/frameworks/torch/tensors/interpreters/precision.py
+++ syft/frameworks/torch/tensors/interpreters/precision.py
@@ -493,11 +493,13 @@ class FixedPrecisionTensor(AbstractTensor):
 
         def sigmoid(tensor):
 
-            """Overloads torch.sigmoid to be able to use MPC
-            Approximation with polynomial interpolation of degree 10 over [-10,10]
-            Ref: https://mortendahl.github.io/2017/04/17/private-deep-learning-with-mpc/#approximating-sigmoid"""
+            """
+            Overloads torch.sigmoid to be able to use MPC
+            Approximation with polynomial interpolation of degree 5 over [-8,8]
+            Ref: https://mortendahl.github.io/2017/04/17/private-deep-learning-with-mpc/#approximating-sigmoid
+            """
 
-            weights = [0.5, 1.912_047_79e-01, -4.586_673_07e-03, 4.206_908_03e-05]
+            weights = [0.5, 1.91204779e-01, -4.58667307e-03, 4.20690803e-05]
             degrees = [0, 1, 3, 5]
 
             # TODO: change to max_degree == degrees[-1] once MPC computations with high exponentials
