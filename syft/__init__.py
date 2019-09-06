bd6ae557a252749747735460b2acb072be026a16
jvmancuso <jason@manc.us>
2019-09-06 16:24:58 -0400

    clean up syft/__init__.py

--- syft/__init__.py
+++ syft/__init__.py
@@ -3,12 +3,11 @@ PySyft is a Python library for secure, private Deep Learning.
 PySyft decouples private data from model training, using Federated Learning,
 Differential Privacy, and Multi-Party Computation (MPC) within PyTorch.
 """
-# We load this module first so that syft knows which frameworks are available
+# We load these modules first so that syft knows which are available
 from syft import dependency_check
+from syft import frameworks  # Triggers registration of any available frameworks
 
 # Major imports
-from syft import frameworks
-from syft import codes
 from syft.version import __version__
 
 # This import statement is strictly here to trigger registration of syft
@@ -30,22 +29,15 @@ if dependency_check.tfe_available:
     from syft.frameworks.keras import KerasHook
     from syft.workers.tfe import TFECluster
     from syft.workers.tfe import TFEWorker
+
+    __all__ = ["KerasHook", "TFECluster", "TFEWorker"]
 else:
     logger.info("TF Encrypted Keras not available.")
+    __all__ = []
 
 # Pytorch dependencies
 # Import Hook
 from syft.frameworks.torch.hook.hook import TorchHook
-
-# Import Tensor Types
-from syft.frameworks.torch.tensors.decorators.logging import LoggingTensor
-from syft.frameworks.torch.tensors.interpreters.additive_shared import AdditiveSharingTensor
-from syft.frameworks.torch.tensors.interpreters.autograd import AutogradTensor
-from syft.generic.pointers.multi_pointer import MultiPointerTensor
-from syft.generic.pointers.pointer_tensor import PointerTensor
-
-# import other useful classes
-from syft.frameworks.torch.federated import FederatedDataset, FederatedDataLoader, BaseDataset
 
 # Import grids
 from syft.grid import VirtualGrid
@@ -54,10 +46,10 @@ from syft.grid import VirtualGrid
 from syft.sandbox import create_sandbox
 
 # Import federate learning objects
+from syft.frameworks.torch.federated import FederatedDataset, FederatedDataLoader, BaseDataset
 from syft.federated.train_config import TrainConfig
 
 # Import messaging objects
-from syft.messaging.message import Message
 from syft.messaging.plan import Plan
 from syft.messaging.plan import func2plan
 from syft.messaging.plan import method2plan
@@ -65,46 +57,58 @@ from syft.messaging.plan import make_plan
 
 # Import Worker Types
 from syft.workers.virtual import VirtualWorker
+from syft.workers.websocket_client import WebsocketClientWorker
+from syft.workers.websocket_server import WebsocketServerWorker
 
-
-# Import Tensor Types
+# Import Syft's Public Tensor Types
 from syft.frameworks.torch.tensors.decorators.logging import LoggingTensor
 from syft.frameworks.torch.tensors.interpreters.additive_shared import AdditiveSharingTensor
 from syft.frameworks.torch.tensors.interpreters.crt_precision import CRTPrecisionTensor
 from syft.frameworks.torch.tensors.interpreters.autograd import AutogradTensor
 from syft.frameworks.torch.tensors.interpreters.precision import FixedPrecisionTensor
 from syft.frameworks.torch.tensors.interpreters.large_precision import LargePrecisionTensor
-
-from syft.generic.pointers.object_pointer import ObjectPointer
-from syft.generic.pointers.callable_pointer import CallablePointer
-from syft.generic.pointers.object_wrapper import ObjectWrapper
+from syft.generic.pointers.pointer_tensor import PointerTensor
+from syft.generic.pointers.multi_pointer import MultiPointerTensor
 
 # Import serialization tools
 from syft import serde
-from syft.serde import torch_serde
 
 # import functions
 from syft.frameworks.torch.functions import combine_pointers
 
-__all__ = [
-    "frameworks",
-    "workers",
-    "serde",
-    "torch_serde",
-    "TorchHook",
-    "VirtualWorker",
-    "Plan",
-    "codes",
-    "LoggingTensor",
-    "PointerTensor",
-    "VirtualGrid",
-    "ObjectWrapper",
-    "LargePrecisionTensor",
-    "create_sandbox",
-]
+__all__.extend(
+    [
+        "frameworks",
+        "serde",
+        "TorchHook",
+        "VirtualWorker",
+        "WebsocketClientWorker",
+        "WebsocketServerWorker",
+        "Plan",
+        "func2plan",
+        "method2plan",
+        "make_plan",
+        "LoggingTensor",
+        "AdditiveSharingTensor",
+        "CRTPrecisionTensor",
+        "AutogradTensor",
+        "FixedPrecisionTensor",
+        "LargePrecisionTensor",
+        "PointerTensor",
+        "MultiPointerTensor",
+        "VirtualGrid",
+        "create_sandbox",
+        "combine_pointers",
+        "FederatedDataset",
+        "FederatedDataLoader",
+        "BaseDataset",
+        "TrainConfig",
+    ]
+)
 
 local_worker = None
 torch = None
+framework = None
 
 if "ID_PROVIDER" not in globals():
     from syft.generic.id_provider import IdProvider
