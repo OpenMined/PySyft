3255ce254cfcafd2d36125946741d9550e32d08e
jvmancuso <jason@manc.us>
2019-10-09 14:28:03 -0400

    func_api_name in _get_hooked_func

--- syft/frameworks/torch/hook/hook.py
+++ syft/frameworks/torch/hook/hook.py
@@ -415,12 +415,12 @@ class TorchHook(FrameworkHook):
                 self._perform_function_overloading(module_name, torch_module, func)
 
     @classmethod
-    def _get_hooked_func(cls, public_module_name, attr):
+    def _get_hooked_func(cls, public_module_name, func_api_name, attr):
         """Torch-specific implementation. See the subclass for more."""
         if attr.__module__ is None:
             attr.__module__ = "torch"
 
-        return super()._get_hooked_func(public_module_name, attr)
+        return super()._get_hooked_func(public_module_name, func_api_name, attr)
 
     def _get_hooked_additive_shared_method(hook_self, attr):
         """
