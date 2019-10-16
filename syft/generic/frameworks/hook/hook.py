c58c80be1f64f5eca2d6c5f09050d9fa811e740a
Ben DeCoste <bendecoste@gmail.com>
2019-10-16 16:24:04 -0300

    reformat code

--- syft/generic/frameworks/hook/hook.py
+++ syft/generic/frameworks/hook/hook.py
@@ -40,7 +40,8 @@ class FrameworkHook(ABC):
     def create_wrapper(cls, wrapper_type, *args, **kwargs):
         """Factory method for creating a generic wrapper of type wrapper_type."""
         if wrapper_type is None:
-          wrapper_type = syft.framework.Tensor
+            wrapper_type = syft.framework.Tensor
+
         return wrapper_type(*args, **kwargs)
 
     ### Standardized, framework-specific methods ###
