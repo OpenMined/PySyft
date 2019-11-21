c50a8258285c62a8caf1b9de2ddd7a990195568a
Bobby Wagner <raw141@case.edu>
2019-11-21 08:55:27 -0500

    fix version number

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = "2019, OpenMinedContributors"
 author = "Andrew Trask"
 
 # The short X.Y version
-version = "0.2.1a2"
+version = "0.2.1a1"
 # The full version, including alpha/beta/rc tags
-release = "0.2.1a2"
+release = "0.2.1a1"
 
 
 # -- General configuration ---------------------------------------------------
