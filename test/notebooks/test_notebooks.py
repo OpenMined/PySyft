cfb476c7e92199607a5983f713150be844e20b75
Florian Roessler <roessler.fd@gmail.com>
2019-11-24 19:09:36 +0100

    adding advanced notebooks

--- test/notebooks/test_notebooks.py
+++ test/notebooks/test_notebooks.py
@@ -4,26 +4,35 @@ import glob
 from pathlib import Path
 
 # buggy notebooks
-exclusion_list = [
+exclusion_list_basic = [
     "Part 10 - Federated Learning with Secure Aggregation.ipynb",
     "Part 13b - Secure Classification with Syft Keras and TFE - Secure Model Serving.ipynb",
     "Part 13c - Secure Classification with Syft Keras and TFE - Private Prediction Client.ipynb",
 ]
 
-# Data Dependent notebooks
-# Part 06 - Federated Learning on MNIST using a CNN (MNIST in '../data')
-# Part 10 ('../data/BostonHousing/boston_housing.pickle')
-# Part 11 (MNIST in '../data')
-# Part 12 bis - Encrypted Training on MNIST (MNIST in '../data')
+exclusion_list_advanced = [
+    "Build your own tensor type (advanced).ipynb",
+    "Federated Recurrent Neural Network.ipynb",
+]
 
 
-def test_notebooks(isolated_filesystem):
+def test_notebooks_basic(isolated_filesystem):
     notebooks = glob.glob("*.ipynb")
     for notebook in notebooks:
-        if Path(notebook).name in exclusion_list:
+        if Path(notebook).name in exclusion_list_basic:
             continue
         print(notebook)
         res = pm.execute_notebook(
             notebook, "/dev/null", parameters={"epochs": 1, "n_test_batches": 5}, timeout=300
         )
+        assert isinstance(res, nbformat.notebooknode.NotebookNode)
+
+
+def test_notebooks_advanced(isolated_filesystem):
+    notebooks = glob.glob("advanced/*.ipynb")
+    for notebook in notebooks:
+        if Path(notebook).name in exclusion_list_advanced:
+            continue
+        print(notebook)
+        res = pm.execute_notebook(notebook, "/dev/null", parameters={"epochs": 1}, timeout=300)
         assert isinstance(res, nbformat.notebooknode.NotebookNode)
