244e236abaf51e85d4c2bc4b0046adcde0b06018
IonesioJunior <ionesiojr@gmail.com>
2019-11-28 12:59:30 -0300

    Moving PyGrid Source Files to PySyft - WebsocketGridClient -> NodeClient 	- Connecting with Grid Nodes. 	- Connecting between each other (allow SMPC operations) 	- Hosting Models (Create, Run inferences, Update, Delete) 	- Authentication (TODO) - Adding Grid codes

--- syft/codes.py
+++ syft/codes.py
@@ -8,3 +8,22 @@ class TENSOR_SERIALIZATION(object):
     NUMPY = "numpy"
     TF = "tf"
     ALL = "all"
+
+
+class REQUEST_MSG(object):
+    TYPE_FIELD = "type"
+    GET_ID = "get-id"
+    CONNECT_NODE = "connect-node"
+    HOST_MODEL = "host-model"
+    RUN_INFERENCE = "run-inference"
+    LIST_MODELS = "list-models"
+    DELETE_MODEL = "delete-model"
+    RUN_INFERENCE = "run-inference"
+
+
+class RESPONSE_MSG(object):
+    NODE_ID = "id"
+    ERROR = "error"
+    SUCCESS = "success"
+    MODELS = "models"
+    INFERENCE_RESULT = "prediction"
