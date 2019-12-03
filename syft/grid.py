93027886c52a61b64c66f7e540043eff4f218765
IonesioJunior <ionesiojr@gmail.com>
2019-12-03 09:55:06 -0300

    Development of GridNetwork - REFACTORY (Rename) VirtualGrid -> GridNetwork. - Connect known remote nodes to each other. - Search to remote grid nodes. - Host models using the grid network. - Query models using the grid network. - Run model's inference using the grid network.

--- syft/grid.py
+++ syft/grid.py
@@ -1,14 +1,31 @@
+import random
+import torch
 from collections import Counter
 from typing import Any
 from typing import Tuple
 from typing import Counter as CounterType
 from typing import Dict
 from typing import Union
+from syft.workers.node_client import NodeClient
 
 
-class VirtualGrid:
+class GridNetwork:
     def __init__(self, *workers):
-        self.workers = workers
+        self.workers = list(workers)
+        self._connect_all_nodes(self.workers)
+
+    def _connect_all_nodes(self, nodes: list):
+        """Connect all nodes to each other.
+
+        Args:
+            nodes: A tuple of grid clients.
+        """
+        if all(isinstance(NodeClient, type(node)) for node in nodes):
+            for i in range(len(nodes)):
+                for j in range(i):
+                    node_i, node_j = nodes[i], nodes[j]
+                    node_i.connect_nodes(node_j)
+                    node_j.connect_nodes(node_i)
 
     def search(
         self, *query, verbose: bool = True, return_counter: bool = True
@@ -51,3 +68,65 @@ class VirtualGrid:
             return results, tag_counter
         else:
             return results
+
+    def serve_model(
+        self,
+        model,
+        id,
+        allow_remote_inference: bool = False,
+        allow_download: bool = False,
+        n_replica: int = 1,
+    ):
+
+        """ Choose some node(s) on grid network to host a unencrypted model.
+            Args:
+                model: Model to be hosted.
+                id: Model's ID.
+                allow_remote_inference: Allow to run inference remotely.
+                allow_download: Allow to copy the model and run it locally.
+                n_replica: Number of copies distributed through grid network.
+            Raises:
+                RuntimeError: If grid network doesn't have enough nodes to replicate the model.
+        """
+        if n_replica > len(self.workers):
+            raise RuntimeError("Not enough nodes!")
+        else:
+            nodes = random.sample(self.workers, n_replica)
+
+        for node in nodes:
+            # Host model
+            node.serve_model(
+                model,
+                model_id=id,
+                allow_download=allow_download,
+                allow_remote_inference=allow_remote_inference,
+            )
+
+    def run_remote_inference(self, id, data):
+        """ Search for a specific model registered on grid network, if found,
+            It will run inference.
+            Args:
+                id : Model's ID.
+                dataset : Data used to run inference.
+            Returns:
+                Tensor : Inference's result.
+        """
+        node = self.query_model(id)
+        if node:
+            response = node.run_remote_inference(model_id=id, data=data)
+            return torch.tensor(response)
+        else:
+            raise RuntimeError("Model not found on Grid Network!")
+
+    def query_model(self, id):
+        """ Search for a specific model registered on grid network, if found,
+            It will return all grid nodes that contains the desired model.
+            Args:
+                id : Model's ID.
+                data : Data used to run inference.
+            Returns:
+                workers : List of workers that contains the desired model.
+        """
+        for node in self.workers:
+            if id in node.models:
+                return node
