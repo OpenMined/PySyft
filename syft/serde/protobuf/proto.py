7a429437999f096fc73fb8b2bdd14bfcad212784
Karl Higley <kmhigley@gmail.com>
2019-12-18 19:00:31 -0500

    Add a mapping between Python and Protobuf classes

---
+++ syft/serde/protobuf/proto.py
@@ -0,0 +1,30 @@
+"""
+This file exists to translate python classes to and from Protobuf messages.
+The reason for this is to have stable serialization protocol that can be used
+not only by PySyft but also in other languages.
+
+https://github.com/OpenMined/syft-proto (`syft_proto` module) is included as
+a dependency in setup.py.
+"""
+import torch
+
+from syft.messaging.message import ObjectMessage
+from syft.frameworks.torch.tensors.interpreters.additive_shared import AdditiveSharingTensor
+
+from syft_proto.frameworks.torch.tensors.interpreters.v1.additive_shared_pb2 import (
+    AdditiveSharingTensor as AdditiveSharingTensorPB,
+)
+from syft_proto.messaging.v1.message_pb2 import ObjectMessage as ObjectMessagePB
+from syft_proto.generic.v1.tensor_pb2 import Tensor as TensorPB
+
+
+MAP_PYTHON_TO_PROTOBUF_CLASSES = {
+    ObjectMessage: ObjectMessagePB,
+    torch.Tensor: TensorPB,
+    AdditiveSharingTensor: AdditiveSharingTensorPB,
+}
+
+MAP_PROTOBUF_TO_PYTHON_CLASSES = {}
+
+for key, value in MAP_PYTHON_TO_PROTOBUF_CLASSES.items():
+    MAP_PROTOBUF_TO_PYTHON_CLASSES[value] = key
