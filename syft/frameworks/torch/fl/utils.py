cadb9307f5944d5b51bb56004e6a3dcd5d6bf959
ganxin <ganxin@sensetime.com>
2020-01-10 18:22:40 +0800

    fedavg

--- syft/frameworks/torch/fl/utils.py
+++ syft/frameworks/torch/fl/utils.py
@@ -29,36 +29,35 @@ def extract_batches_per_worker(federated_train_loader: sy.FederatedDataLoader):
     return batches
 
 
-def add_model(dst_model, src_model):
+def add_model(dst_model, src_model,dst_no_data,src_no_data):
     """Add the parameters of two models.
-
         Args:
             dst_model (torch.nn.Module): the model to which the src_model will be added.
             src_model (torch.nn.Module): the model to be added to dst_model.
+            dst_no_data: the number of data within dst_model.
+            src_no_data: the number of data within src_model.
         Returns:
             torch.nn.Module: the resulting model of the addition.
-
         """
-
+    if (dst_model==None):
+        return src_model
     params1 = src_model.named_parameters()
     params2 = dst_model.named_parameters()
     dict_params2 = dict(params2)
     with torch.no_grad():
         for name1, param1 in params1:
             if name1 in dict_params2:
-                dict_params2[name1].set_(param1.data + dict_params2[name1].data)
+                dict_params2[name1].set_(param1.data*src_no_data + dict_params2[name1].data*dst_no_data)
     return dst_model
 
 
 def scale_model(model, scale):
     """Scale the parameters of a model.
-
     Args:
         model (torch.nn.Module): the models whose parameters will be scaled.
         scale (float): the scaling factor.
     Returns:
         torch.nn.Module: the module with scaled parameters.
-
     """
     params = model.named_parameters()
     dict_params = dict(params)
@@ -68,21 +67,21 @@ def scale_model(model, scale):
     return model
 
 
-def federated_avg(models: List[torch.nn.Module]) -> torch.nn.Module:
+def federated_avg(models,data_num):
     """Calculate the federated average of a list of models.
-
     Args:
-        models (List[torch.nn.Module]): the models of which the federated average is calculated.
-
+        models: the list of models of which the federated average is calculated.
+        data_num: the list of number of data within corresponding model.
     Returns:
         torch.nn.Module: the module with averaged parameters.
     """
-    nr_models = len(models)
-    model_list = list(models.values())
-    model = model_list[0]
-    for i in range(1, nr_models):
-        model = add_model(model, model_list[i])
-    model = scale_model(model, 1.0 / nr_models)
+
+    total_no_data=0
+    model=None
+    for i in range(len(models)):
+        model = add_model(model, models[i],total_no_data,data_num[i])
+        model = scale_model(model, 1.0 / (total_no_data+data_num[i]))
+        total_no_data=total_no_data+data_num[i]
     return model
 
 
