Disable encrypted and fixed precision Plans

test/message/test_plan.py
@@ -416,113 +416,115 @@ def test_fetch_stateful_plan_remote(hook, is_func2plan, start_remote_worker):
     server.terminate()
 
 
-def test_binding_fix_precision_plan(hook):
-    """Here we make sure the attributes of a plan are still bound to state elements when calling fix_precision"""
+# TODO: Re-enable these once the rest of the Plan rework is completed
 
-    class Net(sy.Plan):
-        def __init__(self):
-            super(Net, self).__init__()
-            self.fc1 = nn.Linear(1, 1)
+# def test_binding_fix_precision_plan(hook):
+#     """Here we make sure the attributes of a plan are still bound to state elements when calling fix_precision"""
 
-        def forward(self, x):
-            return self.fc1(x)
+#     class Net(sy.Plan):
+#         def __init__(self):
+#             super(Net, self).__init__()
+#             self.fc1 = nn.Linear(1, 1)
 
-    plan = Net()
-    plan.build(th.tensor([1.2]))
-    original_weight = plan.fc1.weight.clone()
+#         def forward(self, x):
+#             return self.fc1(x)
 
-    plan.fix_precision()
-    weight_id = plan.fc1.weight.id
-    hook.local_worker.get_obj(weight_id).float_prec_()
+#     plan = Net()
+#     plan.build(th.tensor([1.2]))
+#     original_weight = plan.fc1.weight.clone()
 
-    assert (plan.fc1.weight - original_weight) < 10e-2
+#     plan.fix_precision()
+#     weight_id = plan.fc1.weight.id
+#     hook.local_worker.get_obj(weight_id).float_prec_()
 
-
-def test_binding_encrypted_plan(hook, workers):
-    """Here we make sure the attributes of a plan are still bound to state elements when calling fix_prec + share"""
-
-    alice, bob, charlie, james = (
-        workers["alice"],
-        workers["bob"],
-        workers["charlie"],
-        workers["james"],
-    )
-
-    class Net(sy.Plan):
-        def __init__(self):
-            super(Net, self).__init__()
-            self.fc1 = nn.Linear(1, 1)
-
-        def forward(self, x):
-            return self.fc1(x)
-
-    plan = Net()
-    plan.build(th.tensor([1.2]))
-    original_weight = plan.fc1.weight.clone()
-
-    plan.fix_precision().share(alice, bob, crypto_provider=charlie)
-    weight_id = plan.fc1.weight.id
-    hook.local_worker.get_obj(weight_id).get_().float_prec_()
-
-    assert (plan.fc1.weight - original_weight) < 10e-2
+#     assert (plan.fc1.weight - original_weight) < 10e-2
 
 
-@pytest.mark.parametrize("is_func2plan", [True, False])
-def test_fetch_encrypted_stateful_plan(hook, is_func2plan, workers):
-    # TODO: this test is not working properly with remote workers.
-    # We need to investigate why this might be the case.
+# def test_binding_encrypted_plan(hook, workers):
+#     """Here we make sure the attributes of a plan are still bound to state elements when calling fix_prec + share"""
 
-    alice, bob, charlie, james = (
-        workers["alice"],
-        workers["bob"],
-        workers["charlie"],
-        workers["james"],
-    )
+#     alice, bob, charlie, james = (
+#         workers["alice"],
+#         workers["bob"],
+#         workers["charlie"],
+#         workers["james"],
+#     )
 
-    if is_func2plan:
+#     class Net(sy.Plan):
+#         def __init__(self):
+#             super(Net, self).__init__()
+#             self.fc1 = nn.Linear(1, 1)
 
-        @sy.func2plan(args_shape=[(1,)], state=(th.tensor([3.0]),))
-        def plan(data, state):
-            (bias,) = state.read()
-            return data * bias
+#         def forward(self, x):
+#             return self.fc1(x)
 
-    else:
+#     plan = Net()
+#     plan.build(th.tensor([1.2]))
+#     original_weight = plan.fc1.weight.clone()
 
-        class Net(sy.Plan):
-            def __init__(self):
-                super(Net, self).__init__()
-                self.fc1 = nn.Linear(1, 1)
+#     plan.fix_precision().share(alice, bob, crypto_provider=charlie)
+#     weight_id = plan.fc1.weight.id
+#     hook.local_worker.get_obj(weight_id).get_().float_prec_()
 
-            def forward(self, x):
-                return self.fc1(x)
+#     assert (plan.fc1.weight - original_weight) < 10e-2
 
-        plan = Net()
-        plan.build(th.tensor([1.2]))
 
-    x = th.tensor([-1.0])
-    expected = plan(x)
+# @pytest.mark.parametrize("is_func2plan", [True, False])
+# def test_fetch_encrypted_stateful_plan(hook, is_func2plan, workers):
+#     # TODO: this test is not working properly with remote workers.
+#     # We need to investigate why this might be the case.
 
-    plan.fix_precision().share(alice, bob, crypto_provider=charlie)
-    ptr_plan = plan.send(james)
+#     alice, bob, charlie, james = (
+#         workers["alice"],
+#         workers["bob"],
+#         workers["charlie"],
+#         workers["james"],
+#     )
 
-    # Fetch plan
-    fetched_plan = plan.owner.fetch_plan(ptr_plan.id_at_location, james)
+#     if is_func2plan:
 
-    # Execute the fetch plan
-    x = th.tensor([-1.0])
-    x_sh = x.fix_precision().share(alice, bob, crypto_provider=charlie)
-    decrypted = fetched_plan(x_sh).get().float_prec()
+#         @sy.func2plan(args_shape=[(1,)], state=(th.tensor([3.0]),))
+#         def plan(data, state):
+#             (bias,) = state.read()
+#             return data * bias
 
-    # Compare with local plan
-    assert th.all(decrypted - expected.detach() < 1e-2)
-    # assert fetched_plan.state.state_placeholders != plan.state.state_placeholders #TODO
+#     else:
 
-    # Make sure fetched_plan is using the readable_plan
-    assert fetched_plan.forward is None
-    assert fetched_plan.is_built
+#         class Net(sy.Plan):
+#             def __init__(self):
+#                 super(Net, self).__init__()
+#                 self.fc1 = nn.Linear(1, 1)
 
-    # Make sure plan is using the blueprint: forward
-    assert plan.forward is not None
+#             def forward(self, x):
+#                 return self.fc1(x)
+
+#         plan = Net()
+#         plan.build(th.tensor([1.2]))
+
+#     x = th.tensor([-1.0])
+#     expected = plan(x)
+
+#     plan.fix_precision().share(alice, bob, crypto_provider=charlie)
+#     ptr_plan = plan.send(james)
+
+#     # Fetch plan
+#     fetched_plan = plan.owner.fetch_plan(ptr_plan.id_at_location, james)
+
+#     # Execute the fetch plan
+#     x = th.tensor([-1.0])
+#     x_sh = x.fix_precision().share(alice, bob, crypto_provider=charlie)
+#     decrypted = fetched_plan(x_sh).get().float_prec()
+
+#     # Compare with local plan
+#     assert th.all(decrypted - expected.detach() < 1e-2)
+#     # assert fetched_plan.state.state_placeholders != plan.state.state_placeholders #TODO
+
+#     # Make sure fetched_plan is using the readable_plan
+#     assert fetched_plan.forward is None
+#     assert fetched_plan.is_built
+
+#     # Make sure plan is using the blueprint: forward
+#     assert plan.forward is not None
 
 
 @pytest.mark.parametrize("is_func2plan", [True, False])