eec0dc7c4a56958b60ff48c3bba2dd40579f52ca
youben11 <ayouben9@gmail.com>
2020-01-27 18:28:53 +0100

    test serde of CryptenInit message

--- test/serde/serde_helpers.py
+++ test/serde/serde_helpers.py
@@ -1318,6 +1318,33 @@ def make_message(**kwargs):
     ]
 
 
+# syft.messaging.message.CryptenInit
+def make_crypteninit(**kwargs):
+    def compare(detailed, original):
+        assert type(detailed) == syft.messaging.message.CryptenInit
+        assert detailed.contents == original.contents
+        return True
+
+    return [
+        {
+            "value": syft.messaging.message.CryptenInit([0, 2, "127.0.01", 8080]),
+            "simplified": (
+                CODE[syft.messaging.message.CryptenInit],
+                ((CODE[list], (0, 2, "127.0.01", 8080)),),  # (Any) simplified content
+            ),
+            "cmp_detailed": compare,
+        },
+        {
+            "value": syft.messaging.message.CryptenInit((0, 2, "127.0.01", 8080)),
+            "simplified": (
+                CODE[syft.messaging.message.CryptenInit],
+                ((CODE[tuple], (0, 2, "127.0.01", 8080)),),  # (Any) simplified content
+            ),
+            "cmp_detailed": compare,
+        },
+    ]
+
+
 # syft.messaging.message.Operation
 def make_operation(**kwargs):
     bob = kwargs["workers"]["bob"]
