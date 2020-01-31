ee55d412a6382d27dbc6298456aaa2febfb8abe2
Karl Higley <kmhigley@gmail.com>
2020-01-31 13:11:01 -0500

    Add a failing test for JIT tracing a complicated Syft plan

--- test/message/test_plan.py
+++ test/message/test_plan.py
@@ -1011,3 +1011,28 @@ def test_plan_can_be_jit_traced(hook, workers):
     y = torchscript_plan(t)
 
     assert (y == th.tensor([3.0, 5])).all()
+
+
+def test_jit_trace_complicated_plan(hook, workers):
+    class Net(sy.Plan):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.conv1 = nn.Conv2d(1, 20, 5, 1)
+            self.conv2 = nn.Conv2d(20, 50, 5, 1)
+            self.fc1 = nn.Linear(4 * 4 * 50, 500)
+            self.fc2 = nn.Linear(500, 10)
+
+        def forward(self, x):
+            x = F.relu(self.conv1(x))
+            x = F.relu(self.conv2(x))
+            x = x.view(-1, 4 * 4 * 50)
+            x = F.relu(self.fc1(x))
+            x = self.fc2(x)
+            return x
+
+    data = th.zeros((1, 1, 28, 28))
+    plan = Net()
+    plan.build(data)
+
+    # Error happens here
+    torchscript_plan = th.jit.trace(plan, data)
