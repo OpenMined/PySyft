02b79b3d75b4a1c566e4d72781687eb8cf1e222e
Benardi Nunes <benardinunes@gmail.com>
2020-02-05 15:48:24 -0300

    Configure sphinx to ignore "_static"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,12 +52,13 @@ html_theme = "alabaster"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []
 
 # -- Extension configuration -------------------------------------------------
 
 # AutoApi
 autoapi_root = "api"
+autoapi_type = "python"
 autoapi_dirs = ["../../syft"]
 
 # Napoleon settings
