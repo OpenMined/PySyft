ecb71bcd06a619105f80769b4d09da036a1bf756
Benardi Nunes <benardinunes@gmail.com>
2020-02-05 16:33:20 -0300

    Update theme to sphinx-rtd-theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,8 +13,10 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../.."))
+import sphinx_rtd_theme
 
+
+sys.path.insert(0, os.path.abspath("../.."))
 
 # -- Project information -----------------------------------------------------
 
@@ -31,7 +33,7 @@ release = "0.2.3a1"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.napoleon", "autoapi.extension"]
+extensions = ["sphinx.ext.napoleon", "autoapi.extension", "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -47,12 +49,17 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
+
+def setup(app):
+    app.add_stylesheet("css/theme.css")
+
 
 # -- Extension configuration -------------------------------------------------
 
