b8ecfbf123e10a8ba4ac58f6944f6c5750b461f1
Benardi Nunes <benardinunes@gmail.com>
2020-02-05 14:50:38 -0300

    Fix formatting via black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,17 +12,18 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+
+sys.path.insert(0, os.path.abspath("../.."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'PySyft'
-copyright = '2020, Andrew Trask'
-author = 'Andrew Trask'
+project = "PySyft"
+copyright = "2020, Andrew Trask"
+author = "Andrew Trask"
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.3a1'
+release = "0.2.3a1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,10 +31,10 @@ release = '0.2.3a1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ "sphinx.ext.napoleon", "autoapi.extension" ]
+extensions = ["sphinx.ext.napoleon", "autoapi.extension"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -46,12 +47,12 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # -- Extension configuration -------------------------------------------------
 
