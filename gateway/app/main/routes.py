ab3d00ceaffb442e35cbf10dd8c7e17af0bf5866
Madeleine Shang <shang.madeleine@gmail.com>
2020-02-26 06:11:27 -0500

    pr for feedback fl_impFLcycles - use server config (#479)
    * WIP: fl_download_model_endpoint
    
    * mock up use of server_config params for #438-2
    
    * mock up use of server_config params for #438-2
    
    * math stuff
    
    * wip: model in place
    
    * wip: model in place
    
    * wip: model in place
    
    * wip: model in place
    
    * wip: model in place
    
    * comment code
    
    * comment code
    
    * comment code
    
    Co-authored-by: Héricles Emanuel <hericles.me@gmail.com>

--- gateway/app/main/routes.py
+++ gateway/app/main/routes.py
@@ -3,16 +3,22 @@
 """
 
 from flask import render_template, Response, request, current_app
+from math import floor
+import numpy as np
+from scipy.stats import poisson
 from . import main
 import json
 import random
 import os
 import requests
+import logging
 
 from .persistence.manager import register_new_node, connected_nodes, delete_node
 from .processes import processes
+from .events import handler
 from .auth import workers
 from .events.fl_events import report
+
 
 # All grid nodes registered at grid network will be stored here
 grid_nodes = {}
@@ -399,3 +405,189 @@ def download_model():
         return Response(
             json.dumps(response_body), status=status_code, mimetype="application/json"
         )
+
+
+@main.route("/req_join", methods=["GET"])
+def fl_cycle_application_decision():
+    """
+        use the temporary req_join endpoint to mockup:
+        - reject if worker does not satisfy 'minimum_upload_speed' and/or 'minimum_download_speed'
+        - is a part of current or recent cycle according to 'do_not_reuse_workers_until_cycle'
+        - selects according to pool_selection
+        - is under max worker (with some padding to account for expected percent of workers so do not report successfully)
+    """
+
+    # parse query strings (for now), evetually this will be parsed from the request body
+    model_id = request.args.get("model_id")
+    up_speed = request.args.get("up_speed")
+    down_speed = request.args.get("down_speed")
+    worker_id = request.args.get("worker_id")
+    worker_ping = request.args.get("ping")
+    _cycle = processes.get_cycle(model_id)
+    _accept = False
+    """
+    MVP variable stubs:
+        we will stub these with hard coded numbers first, then make functions to dynaically query/update in subsquent PRs
+    """
+    # this will be replaced with a function that check for the same (model_id, version_#) tuple when the worker last participated
+    last_participation = 1
+    # how late is too late into the cycle time to give a worker "new work", if only 5 seconds left probably don't bother, set this intelligently later
+    MINIMUM_CYCLE_TIME_LEFT = 500
+    # the historical amount of workers that fail to report (out of time, offline, too slow etc...),
+    # could be modified to be worker/model specific later, track across overall pygrid instance for now
+    EXPECTED_FAILURE_RATE = 0.2
+
+    dummy_server_config = {
+        "max_workers": 100,
+        "pool_selection": "random",  # or "iterate"
+        "num_cycles": 5,
+        "do_not_reuse_workers_until_cycle": 4,
+        "cycle_length": 8 * 60 * 60,  # 8 hours
+        "minimum_upload_speed": 2000,  # 2 mbps
+        "minimum_download_speed": 4000,  # 4 mbps
+    }
+
+    """  end of variable stubs """
+
+    _server_config = dummy_server_config
+
+    up_speed_check = up_speed > _server_config["minimum_upload_speed"]
+    down_speed_check = down_speed > _server_config["minimum_download_speed"]
+    cycle_valid_check = (
+        (
+            last_participation + _server_config["do_not_reuse_workers_until_cycle"]
+            >= _cycle.get(
+                "cycle_sequence", 99999
+            )  # this should reuturn current cycle sequence number
+        )
+        * (_cycle.get("cycle_sequence", 99999) <= _server_config["num_cycles"])
+        * (_cycle.cycle_time > MINIMUM_CYCLE_TIME_LEFT)
+        * (worker_id not in _cycle._workers)
+    )
+
+    if up_speed_check * down_speed_check * cycle_valid_check:
+        if _server_config["pool_selection"] == "iterate" and len(
+            _cycle._workers
+        ) < _server_config["max_workers"] * (1 + EXPECTED_FAILURE_RATE):
+            """ first come first serve selection mode """
+            _accept = True
+        elif _server_config["pool_selection"] == "random":
+            """
+                probabilistic model for rejction rate:
+                    - model the rate of worker's request to join as lambda in a poisson process
+                    - set probabilistic reject rate such that we can expect enough workers will request to join and be accepted
+                        - between now and ETA till end of _server_config['cycle_length']
+                        - such that we can expect (,say with 95% confidence) successful completion of the cycle
+                        - while accounting for EXPECTED_FAILURE_RATE (% of workers that join cycle but never successfully report diff)
+
+                EXPECTED_FAILURE_RATE = moving average with exponential decay based on historical data (maybe: noised up weights for security)
+
+                k' = max_workers * (1+EXPECTED_FAILURE_RATE) # expected failure adjusted max_workers = var: k_prime
+
+                T_left = T_cycle_end - T_now # how much time is left (in the same unit as below)
+
+                normalized_lambda_actual = (recent) historical rate of request / unit time
+
+                lambda' = number of requests / unit of time that would satisfy the below equation
+
+                probability of receiving at least k' requests per unit time:
+                    P(K>=k') = 0.95 = e ^ ( - lambda' * T_left) * ( lambda' * T_left) ^ k' / k'! = 1 - P(K<k')
+
+                var: lambda_approx = lambda' * T_left
+
+                solve for lambda':
+                    use numerical approximation (newton's method) or just repeatedly call prob = poisson.sf(x, lambda') via scipy
+
+                reject_probability = 1 - lambda_approx / (normalized_lambda_actual * T_left)
+            """
+
+            # time base units = 1 hr, assumes lambda_actual and lambda_approx have the same unit as T_left
+            k_prime = _server_config["max_workers"] * (1 + EXPECTED_FAILURE_RATE)
+            T_left = _cycle.get("cycle_time", 0)
+
+            # TODO: remove magic number = 5 below... see block comment above re: how
+            normalized_lambda_actual = 5
+            lambda_actual = (
+                normalized_lambda_actual * T_left
+            )  # makes lambda_actual have same unit as lambda_approx
+            # @hyperparam: valid_range => (0, 1) | (+) => more certainty to have completed cycle, (-) => more efficient use of worker as computational resource
+            confidence = 0.95  # P(K>=k')
+            pois = lambda l: poisson.sf(k_prime, l) - confidence
+
+            """
+            _bisect_approximator because:
+                - solving for lambda given P(K>=k') has no algebraic solution (that I know of) => need approxmiation
+                - scipy's optimizers are not stable for this problem (I tested a few) => need custom approxmiation
+                - at this MVP stage we are not likely to experince performance problems, binary search is log(N)
+            refactor notes:
+                - implmenting a smarter approximiator using lambert's W or newton's methods will take more time
+                - if we do need to scale then we can refactor to the above ^
+            """
+            # @hyperparam: valid_range => (0, 1) | (+) => get a faster but lower quality approximation
+            _search_tolerance = 0.01
+
+            def _bisect_approximator(arr, search_tolerance=_search_tolerance):
+                """ uses binary search to find lambda_actual within search_tolerance"""
+                n = len(arr)
+                L = 0
+                R = n - 1
+
+                while L <= R:
+                    mid = floor((L + R) / 2)
+                    if pois(arr[mid]) > 0 and pois(arr[mid]) < search_tolerance:
+                        return mid
+                    elif pois(arr[mid]) > 0 and pois(arr[mid]) > search_tolerance:
+                        R = mid - 1
+                    else:
+                        L = mid + 1
+                return None
+
+            """
+            if the number of workers is relatively small:
+                - approximiation methods is not neccessary / we can find exact solution fast
+                - and search_tolerance is not guaranteed because lambda has to be int()
+            """
+            if k_prime < 50:
+                lambda_approx = np.argmin(
+                    [abs(pois(x)) for x in range(floor(k_prime * 3))]
+                )
+            else:
+                lambda_approx = _bisect_approximator(range(floor(k_prime * 3)))
+
+            rej_prob = (
+                (1 - lambda_approx / lambda_actual)
+                if lambda_actual > lambda_approx
+                else 0  # don't reject if we expect to be short on worker requests
+            )
+
+            # additional security:
+            if (
+                k_prime > 50
+                and abs(poisson.sf(k_prime, lambda_approx) - confidence)
+                > _search_tolerance
+            ):
+                """something went wrong, fall back to safe default"""
+                rej_prob = 0.1
+                WARN = "_bisect_approximator failed unexpectedly, reset rej_prob to default"
+                logging.exception(WARN)  # log error
+
+            if random.random_sample() < rej_prob:
+                _accept = True
+
+    if _accept:
+        return Response(
+            json.dumps(
+                {"status": "accepted"}
+            ),  # leave out other accpet keys/values for now
+            status=200,
+            mimetype="application/json",
+        )
+
+    # reject by default
+    return Response(
+        json.dumps(
+            {"status": "rejected"}
+        ),  # leave out other accpet keys/values for now
+        status=400,
+        mimetype="application/json",
+    )
