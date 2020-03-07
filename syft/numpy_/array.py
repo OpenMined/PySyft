dea17ecc1de5a78d526da1d702fc713be38ac83d
iamtrask <liamtrask@gmail.com>
2020-03-07 19:40:27 +0000

    Bug identified; Numpy id checking complains because of tensorlfow related implications

--- syft/numpy_/array.py
+++ syft/numpy_/array.py
@@ -1,4 +1,6 @@
 from syft.generic import ObjectConstructor
+from syft import check
+
 import numpy as np
 
 
@@ -12,6 +14,24 @@ class ArrayConstructor(ObjectConstructor):
     # self.install_inside_library()
     constructor_location = np
 
+    @check.type_hints
+    def post_init(self, obj: object, *args, **kwargs):
+        """Execute functionality after object has been created.
+
+        This method executes functionality which can only be run after an object has been initailized. It is
+        particularly useful for logic which registers the created object into an appropriate registry. It is
+        also useful for adding arbitrary attributes to the object after initialization.
+
+        Args:
+            *args (tuple): the arguments being used to initialize the object
+            **kwargs (dict): the kwarguments eeing used to initialize the object
+        Returns:
+            out (SyftObject): returns the underlying syft object.
+        """
+
+        # obj = self.assign_id(obj=obj)
+
+        return obj
 
 # Step 3: create constructor and install it in the library
 ArrayConstructor().install_inside_library()
