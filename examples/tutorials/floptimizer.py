b393df8de26c5ed8ad56e521a0d8578885c031d2
rimijoker <anz19999999999@gmail.com>
2020-03-11 22:21:56 +0530

    reformat

--- examples/tutorials/floptimizer.py
+++ examples/tutorials/floptimizer.py
@@ -1,19 +1,24 @@
 """to maintain a list of optimizer objects,
 one for each worker and use them in the appropriate context"""
 import copy
+
+
 class Optims:
     """to create a list of optimizer objects"""
+
     def __init__(self, workers, optim):
         self.optim = optim
         self.workers = workers
         self.optimizers = {}
         for worker in workers:
             self.optimizers[str(worker)] = copy.copy(self.optim)
-            self.optimizers[str(worker)].load_state_dict((self.optim).state_dict())
+            self.optimizers[str(worker)].load_state_dict(
+                (self.optim).state_dict())
 
     def get_optim(self, worker):
         """returns optimizer for worker"""
         return self.optimizers[str(worker)]
+
     def count(self):
         """returns the number of optimizer objects"""
         return len(self.workers)
