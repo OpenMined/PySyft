642166c388afa366f3d185b4bfc1510b9d3336db
Muhammed Abogazia <abogaziah@users.noreply.github.com>
2020-03-13 14:26:08 +0200

    formatting

--- syft/frameworks/torch/fl/dataset.py
+++ syft/frameworks/torch/fl/dataset.py
@@ -121,9 +121,9 @@ class BaseDataset(AbstractObject):
         return self
 
     def create_pointer(
-            self, owner, garbage_collect_data, location=None, id_at_location=None, **kwargs
+        self, owner, garbage_collect_data, location=None, id_at_location=None, **kwargs
     ):
-        """creats a pointer to the self dataset."""
+        """creats a pointer to the self dataset"""
         if owner is None:
             owner = self.owner
 
