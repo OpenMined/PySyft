f77938ab01fee388bcd52d9748ebc91138a2dd77
Muhammed Abogazia <33666625+abogaziah@users.noreply.github.com>
2020-03-16 14:48:23 +0200

    Send & Get Datasets (#2960)
    * dataset inherits abstractobject
    
    * added abstract dataset test
    
    * formatting
    
    * fix
    
    * removed owner annotation
    
    * formatting
    
    * Added simplify& detail
    
    * Edit simplify& detail
    
    * Added PointerDataset type
    
    * Added send()
    
    * Added send(), create_pointer()
    
    * Added get(), pointer.data(), pointer.target()
    
    * Added wrap(), edited base.search(), edit dataset.__init__()
    
    * Formatting
    
    * Added test file, __repr__(), removed get() override
    
    * Added tags, description to create_pointer()
    
    * formatting
    
    * changed repr of dataset ptr
    
    * replaced == with torch.equal()
    
    * adapt dataloader
    
    * formatting
    
    * added serede test!
    
    * added serede test!
    
    * test fixes
    
    * test fixes
    
    * serde test fix
    
    * formatting
    
    * hook_args fix
    
    * Update test_dataset_pointer.py
    
    * pointer location fix
    
    * formatting
    
    * formatting
    
    * formatting
    
    * formatting
    
    * fedrated check pass
    
    * formatting
    
    Co-authored-by: Muhammed Abogazia <abogaziah@users.noreply.github.com>
    Co-authored-by: Théo Ryffel <theo.leffyr@gmail.com>

--- syft/serde/msgpack/serde.py
+++ syft/serde/msgpack/serde.py
@@ -72,6 +72,7 @@ from syft.serde import compression
 from syft.serde.msgpack.native_serde import MAP_NATIVE_SIMPLIFIERS_AND_DETAILERS
 from syft.workers.abstract import AbstractWorker
 from syft.workers.base import BaseWorker
+from syft.frameworks.torch.fl import BaseDataset
 
 from syft.exceptions import GetNotPermittedError
 from syft.exceptions import ResponseSignatureError
@@ -133,6 +134,7 @@ OBJ_SIMPLIFIER_AND_DETAILERS = [
     PlanCommandMessage,
     GradFunc,
     String,
+    BaseDataset,
     ExecuteWorkerFunctionMessage,
 ]
 
