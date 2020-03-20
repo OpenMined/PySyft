d1836955e15cd9ad9e80dbc179655949b693206b
vineet jain <vineetj869@gmail.com>
2020-03-20 23:43:23 +0530

    Update examples/tutorials/advanced/websockets-example-MNIST/start_websocket_servers.py
    Co-Authored-By: Karl Higley <kmhigley@gmail.com>

--- examples/tutorials/advanced/websockets-example-MNIST/start_websocket_servers.py
+++ examples/tutorials/advanced/websockets-example-MNIST/start_websocket_servers.py
@@ -9,7 +9,7 @@ else:
     python = "python" + sys.version[0:3]
 
 FILE_PATH = (
-    Path(__file__).resolve().parent.parent.parent.parent.parent.joinpath("run_websocket_server.py")
+    Path(__file__).resolve().parents[5].joinpath("run_websocket_server.py")
 )
 
 call_alice = [python, FILE_PATH, "--port", "8777", "--id", "alice"]
