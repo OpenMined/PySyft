df6b0c48af5aca585c9f337414f9e7dcbc586fa4
Vineeth R <50334073+vineeth14@users.noreply.github.com>
2020-03-27 18:38:39 +0530

    Add documentation to test files (#3177)
    * Added docstrings to test_object_storage.py
    
    * Added inline comments
    
    * Reformatted
    
    * changes to  docstring
    
    Co-authored-by: Vineeth Rajesh <vineethrajesh14@gmail.com>

--- test/generic/test_object_storage.py
+++ test/generic/test_object_storage.py
@@ -4,17 +4,22 @@ from syft.generic import object_storage
 
 
 def test_clear_objects():
-    obj_storage = object_storage.ObjectStorage()
+    """
+    Checks the clear_objects method
+    """
+    obj_storage = (
+        object_storage.ObjectStorage()
+    )  #  obj_storage is a wrapper object to a collection of objects
 
     x = torch.tensor(1)
     obj_storage.set_obj(x)
 
-    objs = obj_storage.current_objects()
+    objs = obj_storage.current_objects()  # Returns a copy of the objects in obj_storage(here:x)
 
     assert len(objs) == 1
     assert objs[x.id] == x
 
-    ret_val = obj_storage.clear_objects()
+    ret_val = obj_storage.clear_objects()  # Completely removes all objects from obj_storage
 
     objs = obj_storage.current_objects()
     assert len(objs) == 0
@@ -22,6 +27,9 @@ def test_clear_objects():
 
 
 def test_clear_objects_return_None():
+    """
+    Checks the clear_objects method when no return is required
+    """
     obj_storage = object_storage.ObjectStorage()
 
     x = torch.tensor(1)
