f5d1a789f74bfea35b49b5af8e0579b23d6403b3
Yugandhar Tripathi <tripathi.yugandhar@gmail.com>
2020-03-27 06:53:47 +0530

    Update syft/serde/msgpack/serde.py
    Co-Authored-By: Karl Higley <kmhigley@gmail.com>

--- syft/serde/msgpack/serde.py
+++ syft/serde/msgpack/serde.py
@@ -68,7 +68,8 @@ from syft.messaging.message import ForceObjectDeleteMessage
 from syft.messaging.message import SearchMessage
 from syft.messaging.message import PlanCommandMessage
 from syft.messaging.message import WorkerCommandMessage
-from syft.serde import compression, msgpack
+from syft.serde import compression
+from syft.serde import msgpack
 from syft.serde.msgpack.native_serde import MAP_NATIVE_SIMPLIFIERS_AND_DETAILERS
 from syft.workers.abstract import AbstractWorker
 from syft.workers.base import BaseWorker
