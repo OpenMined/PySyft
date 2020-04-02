6874e4d23ab33ab08dbf4700f7dc70018e59dc4f
Vineet Jain <vineetj869@gmail.com>
2020-04-02 15:23:50 +0530

    merging websockets-example-MNIST and websockets_mnist file

--- examples/tutorials/advanced/websockets-example-MNIST/start_websocket_servers.py
+++ examples/tutorials/advanced/websockets-example-MNIST/start_websocket_servers.py
@@ -4,20 +4,12 @@ from pathlib import Path
 
 python = Path(sys.executable).name
 
-FILE_PATH = Path(__file__).resolve().parents[5].joinpath("run_websocket_server.py")
+FILE_PATH = (
+    Path(__file__)
+    .resolve()
+    .parents[1]
+    .joinpath("websockets_mnist")
+    .joinpath("run_websocket_server.py")
+)
 
-call_alice = [python, FILE_PATH, "--port", "8777", "--id", "alice"]
-
-call_bob = [python, FILE_PATH, "--port", "8778", "--id", "bob"]
-
-call_charlie = [python, FILE_PATH, "--port", "8779", "--id", "charlie"]
-
-
-print("Starting server for Alice")
-subprocess.Popen(call_alice)
-
-print("Starting server for Bob")
-subprocess.Popen(call_bob)
-
-print("Starting server for Charlie")
-subprocess.Popen(call_charlie)
+subprocess.Popen([python, FILE_PATH])
