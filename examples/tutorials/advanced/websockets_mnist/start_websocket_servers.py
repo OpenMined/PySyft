d5cde074b77a195bfb64ce2b59720da5a7b8676d
vineet jain <vineetj869@gmail.com>
2020-04-05 01:12:45 +0530

    Merging tests run_websocket_server.py file  (#3265)
    * removing websockets-example-MNIST/run_websocket_server.py and changing websockets-example-MNIST-parallel/start_websocket_servers.py so it can be readable and run in python3 verson specifically
    
    * testing
    
    * removing websockets-example-MNIST/run_websocket_server.py
    
    * removing websockets-example-MNIST-parallel/run_websocket_server.py
    
    * removed run_websocket_server.py from all tutorials and made one single file in master
    
    * removed run_websocket_server.py from all tutorials and made one single file in master
    
    * minor changes
    
    * reformatting code
    
    * import module changes
    
    * getting to previous commit
    
    * adding FILE_PATH variable
    
    * change to intial version
    
    * using pathlib
    
    * removing os part
    
    * Update examples/tutorials/advanced/websockets-example-MNIST-parallel/start_websocket_servers.py
    
    Co-Authored-By: Karl Higley <kmhigley@gmail.com>
    
    * Update examples/tutorials/advanced/websockets-example-MNIST/start_websocket_servers.py
    
    Co-Authored-By: Karl Higley <kmhigley@gmail.com>
    
    * Update examples/tutorials/websocket/pen_testing/steal_data_over_sockets/start_websocket_servers.py
    
    Co-Authored-By: Karl Higley <kmhigley@gmail.com>
    
    * Update examples/tutorials/websocket/pen_testing/steal_data_over_sockets/start_websocket_servers.py
    
    Co-Authored-By: Karl Higley <kmhigley@gmail.com>
    
    * Update examples/tutorials/advanced/websockets-example-MNIST/start_websocket_servers.py
    
    Co-Authored-By: Karl Higley <kmhigley@gmail.com>
    
    * Update examples/tutorials/websocket/pen_testing/steal_data_over_sockets/start_websocket_servers.py
    
    Co-Authored-By: Karl Higley <kmhigley@gmail.com>
    
    * Update examples/tutorials/advanced/websockets-example-MNIST-parallel/start_websocket_servers.py
    
    Co-Authored-By: Karl Higley <kmhigley@gmail.com>
    
    * Update examples/tutorials/advanced/websockets-example-MNIST/start_websocket_servers.py
    
    Co-Authored-By: Karl Higley <kmhigley@gmail.com>
    
    * Update examples/tutorials/advanced/websockets-example-MNIST-parallel/start_websocket_servers.py
    
    Co-Authored-By: Karl Higley <kmhigley@gmail.com>
    
    * reformatting
    
    * making codebase short
    
    * making codebase short
    
    * changing test files
    
    * changing test files
    
    * changes
    
    * changes
    
    * using arguments in test files
    
    * correcting indentation
    
    * changing FILE_PATH variable
    
    * changing federated to fl
    
    * merging websockets-example-MNIST and websockets_mnist file
    
    * FIle is not used.
    
    * correcting file path
    
    Co-authored-by: Karl Higley <kmhigley@gmail.com>

--- examples/tutorials/advanced/websockets_mnist/start_websocket_servers.py
+++ examples/tutorials/advanced/websockets_mnist/start_websocket_servers.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 python = Path(sys.executable).name
 
-FILE_PATH = Path(__file__).resolve().parents[5].joinpath("run_websocket_server.py")
+FILE_PATH = Path(__file__).resolve().parents[4].joinpath("run_websocket_server.py")
 
 call_alice = [python, FILE_PATH, "--port", "8777", "--id", "alice"]
 
