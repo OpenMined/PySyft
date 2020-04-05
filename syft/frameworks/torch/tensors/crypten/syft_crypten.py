0774fd8c36157f16ebe142e43494c0c4cd4e19ce
Ajay Singh <ajaysimple91@gmail.com>
2020-04-04 20:38:53 -0500

    clone,from_share,get_share,get_share,set_encoder and get_encoder function added

--- syft/frameworks/torch/tensors/crypten/syft_crypten.py
+++ syft/frameworks/torch/tensors/crypten/syft_crypten.py
@@ -1,4 +1,5 @@
 from crypten.mpc import MPCTensor
+from crypten.mpc.ptype import ptype as Ptype
 
 from syft.generic.frameworks.hook import hook_args
 from syft.generic.tensor import AbstractTensor
@@ -31,6 +32,22 @@ class SyftCrypTensor(AbstractTensor):
         self.child = new_data.child
         return self
 
+    @staticmethod
+    def new(*args, **kwargs):
+        """
+        Creates a new MPCTensor, passing all args and kwargs into the constructor.
+        """
+        return MPCTensor(*args, **kwargs)
+
+    def from_shares(self, share, precision=None, src=0, ptype=Ptype.arithmetic):
+        return self.tensor.from_share(share, precision=None, src=0, ptype=Ptype.arithmetic)
+
+    def clone(self):
+        return self.tensor.clone()
+
+    def shallow_copy(self):
+        return self.tensor.shallow_copy()
+
     def get_plain_text(self, dst=None):
         """Decrypts the tensor."""
         return self.tensor.get_plain_text(dst=dst)
@@ -62,6 +79,18 @@ class SyftCrypTensor(AbstractTensor):
 
     def __setitem__(self, index, value):
         self.tensor.__setitem__(index, value)
+
+    def get_share(self):
+        return self.tensor.share()
+
+    def set_share(self, value):
+        self.tensor = value
+
+    def get_encoder(self):
+        return self.tensor.encoder()
+
+    def set_encoder(self, value):
+        self.tensor.encoder = value
 
 
 ### Register the tensor with hook_args.py ###
