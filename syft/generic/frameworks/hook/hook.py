ca6813aedf10348a756bdc67fbcd83520e570a59
Karl Higley <kmhigley@gmail.com>
2020-04-12 14:34:51 -0400

    Don't call arguments or locals `args` or `kwargs` (unless they're `*args` or `**kwargs`) (#3314)
    * Don't call arguments or locals `kwargs` (if they're not `**kwargs`)
    
    * Don't call arguments or locals `args` (unless they're `*args`)
    
    * Fix a few errors from the merge

--- syft/generic/frameworks/hook/hook.py
+++ syft/generic/frameworks/hook/hook.py
@@ -639,8 +639,8 @@ class FrameworkHook(ABC):
             the hooked method
         """
 
-        def dispatch(args, k):
-            return map(lambda x: x[k] if isinstance(x, dict) else x, args)
+        def dispatch(args_, k):
+            return map(lambda x: x[k] if isinstance(x, dict) else x, args_)
 
         @wraps(attr)
         def overloaded_attr(self, *args, **kwargs):
@@ -667,7 +667,7 @@ class FrameworkHook(ABC):
         return overloaded_attr
 
     @classmethod
-    def _string_input_args_adaptor(cls, args: Tuple[object]):
+    def _string_input_args_adaptor(cls, args_: Tuple[object]):
         """
            This method is used when hooking String methods.
 
@@ -683,7 +683,7 @@ class FrameworkHook(ABC):
            be peeled down to 'str'
 
            Args:
-               args: A tuple or positional arguments of the method
+               args_: A tuple or positional arguments of the method
                      being hooked to the String class.
 
            Return:
@@ -693,7 +693,7 @@ class FrameworkHook(ABC):
 
         new_args = []
 
-        for arg in args:
+        for arg in args_:
 
             # If 'arg' is an object of type String
             # replace it by and 'str' object
