7160a6ace3ae5317a9949ec66f5e9eb9e011da41
Syzygianinfern0 <spsharan2000@gmail.com>
2020-04-14 14:37:02 +0530

    iadd Test

--- syft/frameworks/torch/hook/hook.py
+++ syft/frameworks/torch/hook/hook.py
@@ -812,7 +812,9 @@ class TorchHook(FrameworkHook):
                     param_norm = p.grad.data.norm(norm_type)
                     # Remote PySyft tensor
                     if param_is_pointer_tensor(p):
-                        total_norm += param_norm ** norm_type
+                        total_norm = (
+                            total_norm + param_norm ** norm_type
+                        )  # TODO: @Syzygianinfern0 iadd buggy
                     # Local PySyft tensor
                     else:
                         total_norm += param_norm.item() ** norm_type
