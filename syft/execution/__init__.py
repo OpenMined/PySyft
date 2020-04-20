ecd626a38cd8b03b33adde3ba7874cc804e86975
Karl Higley <kmhigley@gmail.com>
2020-04-20 11:28:06 -0400

    Revert changes to `__init__.py` (those were intentional apparently)

--- syft/execution/__init__.py
+++ syft/execution/__init__.py
@@ -1,6 +1,1 @@
-from syft.execution.plan import func2plan
-from syft.execution.plan import method2plan
-from syft.execution.plan import Plan
 
-from syft.execution.translation.torchscript import PlanTranslatorTorchscript
-from syft.execution.translation.default import PlanTranslatorDefault
