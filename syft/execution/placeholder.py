884aa15ff6ed540bc1e356599af173b9958af008
Karl Higley <kmhigley@gmail.com>
2020-04-21 11:18:24 -0400

    Set `requires_grad` to `True` on `Plan` building temporary args

--- syft/execution/placeholder.py
+++ syft/execution/placeholder.py
@@ -79,7 +79,9 @@ class PlaceHolder(AbstractTensor):
                 raise ValueError(f"Invalid shape {shape}")
             mapped_shapes.append(tuple(map(lambda y: 1 if y == -1 else y, shape)))
 
-        return [syft.framework.hook.create_zeros(shape) for shape in mapped_shapes]
+        return [
+            syft.framework.hook.create_zeros(shape, requires_grad=True) for shape in mapped_shapes
+        ]
 
     @staticmethod
     def instantiate_placeholders(obj, response):
