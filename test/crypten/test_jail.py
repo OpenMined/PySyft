88dc0e31f01b825527c0cc6c2ad15b4e9e24441b
Ayoub Benaissa <ayouben9@gmail.com>
2020-04-22 15:45:35 +0100

    JailRunner (#3367)
    * JailRunner class
    
    * serialization
    
    * add required packages
    
    * fallback to inspect if dill doesn't work
    
    * set _is_buit at the end of build
    
    * use isinstance
    
    * typo
    
    Co-Authored-By: Théo Ryffel <theo.leffyr@gmail.com>
    
    * typo
    
    Co-Authored-By: Théo Ryffel <theo.leffyr@gmail.com>
    
    * specify RestricedPython version
    
    * use pytest.raises
    
    Co-authored-by: Théo Ryffel <theo.leffyr@gmail.com>

---
+++ test/crypten/test_jail.py
@@ -0,0 +1,36 @@
+import pytest
+import torch as th
+from syft.frameworks.crypten import jail
+
+
+def add_tensors(tensor):
+    # torch here is from the jail
+    t = torch.tensor(5)  # noqa
+    return t + tensor
+
+
+def test_simple_func():
+    func = jail.JailRunner(func=add_tensors)
+    t = th.tensor(2)
+    expected = th.tensor(7)
+    assert expected == func(t)
+
+
+def import_socket():
+    import socket
+
+
+def test_import():
+    func = jail.JailRunner(func=import_socket)
+    with pytest.raises(ImportError):
+        func()
+
+
+def test_ser_deser():
+    func = jail.JailRunner(func=add_tensors, modules=[th])
+    func_ser = jail.JailRunner.simplify(func)
+    func_deser = jail.JailRunner.detail(func_ser)
+
+    t = th.tensor(2)
+    expected = th.tensor(7)
+    assert expected == func_deser(t)
