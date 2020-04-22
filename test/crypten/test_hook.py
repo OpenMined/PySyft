3704466156d5272d14a78caa089ac40c6f121ab1
Ayoub Benaissa <ayouben9@gmail.com>
2020-04-22 15:54:08 +0100

    Hooking crypten.nn.Module (#3374)
    * hook crypten.nn.Module
    
    * test crypten module hook
    
    * typos
    
    * test copy

---
+++ test/crypten/test_hook.py
@@ -0,0 +1,55 @@
+import pytest
+import crypten
+import torch
+import syft
+
+
+@pytest.fixture(scope="function")
+def model():
+    l_in, l_h, l_out = 32, 16, 2
+    model = crypten.nn.Sequential(
+        [crypten.nn.Linear(l_in, l_h), crypten.nn.ReLU(), crypten.nn.Linear(l_h, l_out)]
+    )
+    return model
+
+
+def test_send_module(workers, model):
+    alice = workers["alice"]
+    model_cmp = model.copy()
+    params = [p.data for p in model_cmp.parameters()]
+
+    assert model.location is None
+    assert model.owner == syft.local_worker
+    model.send(alice)
+    assert model.location == alice
+    assert model.owner == syft.local_worker
+    model.get()
+    assert model.location is None
+    assert model.owner == syft.local_worker
+
+    for p, p_cmp in zip(model.parameters(), params):
+        assert torch.all(p.data == p_cmp)
+
+
+def test_move_module(workers, model):
+    alice = workers["alice"]
+    bob = workers["bob"]
+
+    assert model.location is None
+    model.send(alice)
+    assert model.location == alice
+    model.move(bob)
+    assert model.location == bob
+    model.get()
+    assert model.location is None
+
+
+def test_copy(model):
+    copy_model = model.copy()
+    with torch.no_grad():
+        for p in model.parameters():
+            assert isinstance(p, torch.Tensor)
+            p.set_(torch.zeros_like(p))
+
+        for p in copy_model.parameters():
+            assert not torch.all(p == 0)
