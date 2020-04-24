d6b77f76d1792e97da7d3b93607e2f61226b617d
George-Cristian Muraru <murarugeorgec@gmail.com>
2020-04-24 15:36:18 +0300

    Add Crypten changes to master (#3377)
    * Add Crypten changes to master
    
    * Add test placeholder
    
    * Do not check
    
    * Format test

---
+++ test/execution/test_placeholder.py
@@ -0,0 +1,26 @@
+import pytest
+import torch as th
+
+from syft.execution.placeholder import PlaceHolder
+
+
+def test_placeholder_forwarding():
+    class TestClass(object):
+        def child_only(self):
+            return "Method 1"
+
+        def copy(self):
+            return "Method 2"  # noqa: F821
+
+    placeholder = PlaceHolder()
+    placeholder.instantiate(TestClass())
+
+    # Should be forwarded to the child
+    assert placeholder.child_only() == "Method 1"
+
+    # Should be found in placeholder -- should not be forwarded
+    assert placeholder.copy() != "Method 2"
+
+    # Not found in placeholder or child
+    with pytest.raises(AttributeError):
+        placeholder.dummy_method()
