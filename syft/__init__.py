0e72ae925e941142fb54c0e29ebd9b04defa763e
Karl Higley <kmhigley@gmail.com>
2020-04-26 10:58:39 -0400

    Set up `func2protocol` on `syft` module

--- syft/__init__.py
+++ syft/__init__.py
@@ -52,6 +52,7 @@ from syft.federated.train_config import TrainConfig
 
 # Import messaging objects
 from syft.execution.protocol import Protocol
+from syft.execution.protocol import func2protocol
 from syft.execution.plan import Plan
 from syft.execution.plan import func2plan
 
@@ -100,6 +101,8 @@ __all__.extend(
         "VirtualWorker",
         "WebsocketClientWorker",
         "WebsocketServerWorker",
+        "Protocol",
+        "func2protocol",
         "Plan",
         "func2plan",
         "make_plan",
