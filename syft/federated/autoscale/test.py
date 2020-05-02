31b37ed3bd144dd23e6d7e6b5008b08943a3c835
rimijoker <anz19999999999@gmail.com>
2020-05-02 10:52:51 +0530

    reformatted

--- syft/federated/autoscale/test.py
+++ syft/federated/autoscale/test.py
@@ -3,16 +3,11 @@ import gcloud
 
 
 NEW = gcloud.GoogleCloud(
-    credentials="GCP Login/terraf.json", 
-    project_id="terraform", 
-    region="us-central1"
+    credentials="GCP Login/terraf.json", project_id="terraform", region="us-central1"
 )
 
 NEW.compute_instance(
-    name="new-12345",
-    machine_type="f1-micro",
-    zone="us-central1-a",
-    image_family="debian-9",
+    name="new-12345", machine_type="f1-micro", zone="us-central1-a", image_family="debian-9",
 )
 
 NEW.destroy()
