9952ed6e5491144e9ad878e485f5fb5d87d5424a
unknown <daniel.hurtadoramirez314@gmail.com>
2020-05-03 17:56:02 +0200

    ran black, again

--- test/torch/tensors/test_multiparty_nn.py
+++ test/torch/tensors/test_multiparty_nn.py
@@ -10,9 +10,15 @@ def test_select_share(workers):
         workers["charlie"],
         workers["james"],
     )
-    alpha_1 = torch.tensor([[0, 1]]).share(alice, bob, charlie, crypto_provider=james, dtype="long").child
-    ones = torch.tensor([[1, 1]]).share(alice, bob, charlie, crypto_provider=james, dtype="long").child
-    twos = torch.tensor([[2, 2]]).share(alice, bob, charlie, crypto_provider=james, dtype="long").child
+    alpha_1 = (
+        torch.tensor([[0, 1]]).share(alice, bob, charlie, crypto_provider=james, dtype="long").child
+    )
+    ones = (
+        torch.tensor([[1, 1]]).share(alice, bob, charlie, crypto_provider=james, dtype="long").child
+    )
+    twos = (
+        torch.tensor([[2, 2]]).share(alice, bob, charlie, crypto_provider=james, dtype="long").child
+    )
     selected = securenn.select_share(alpha_1, ones, twos)
     assert (selected.get() == torch.tensor([[1, 2]])).all()
 
@@ -25,13 +31,13 @@ def test_private_compare(workers):
         workers["james"],
     )
     x_bits = (
-        securenn.decompose(torch.tensor([3, 3]), 2**64)
+        securenn.decompose(torch.tensor([3, 3]), 2 ** 64)
         .share(alice, bob, charlie, dtype="custom", field=67, crypto_provider=james)
         .child
     )
     r = torch.tensor([1, 5]).send(alice, bob, charlie).child
     b = torch.tensor([0]).send(alice, bob, charlie).child
-    compare = securenn.private_compare(x_bits, r, b, 2**64)
+    compare = securenn.private_compare(x_bits, r, b, 2 ** 64)
     assert (compare == torch.tensor([1, 0])).all()
 
 
@@ -105,10 +111,14 @@ def test_division(workers):
         workers["james"],
     )
     tensorA = (
-        torch.tensor([[0, 10, 10, 20, 20]]).share(alice, bob, charlie, crypto_provider=james, dtype="long").child
+        torch.tensor([[0, 10, 10, 20, 20]])
+        .share(alice, bob, charlie, crypto_provider=james, dtype="long")
+        .child
     )
     tensorB = (
-        torch.tensor([[1, 2, 3, 4, 5]]).share(alice, bob, charlie, crypto_provider=james, dtype="long").child
+        torch.tensor([[1, 2, 3, 4, 5]])
+        .share(alice, bob, charlie, crypto_provider=james, dtype="long")
+        .child
     )
     division = securenn.division(tensorA, tensorB)
     assert (division.get() == torch.tensor([[0, 5, 3, 5, 4]])).all()
@@ -122,7 +132,9 @@ def test_maxpool(workers):
         workers["james"],
     )
     tensorA = (
-        torch.tensor([[0, 1, 8, 3, 4]]).share(alice, bob, charlie, crypto_provider=james, dtype="long").child
+        torch.tensor([[0, 1, 8, 3, 4]])
+        .share(alice, bob, charlie, crypto_provider=james, dtype="long")
+        .child
     )
     v, i = securenn.maxpool(tensorA)
     assert (v.get() == torch.tensor([[8]])).all()
