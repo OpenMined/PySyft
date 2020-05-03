204a5bf09865476138d8db730c16c0f5748f8dae
unknown <daniel.hurtadoramirez314@gmail.com>
2020-05-03 17:47:02 +0200

    fixed bug on tests

--- test/torch/tensors/test_multiparty_nn.py
+++ test/torch/tensors/test_multiparty_nn.py
@@ -10,9 +10,9 @@ def test_select_share(workers):
         workers["charlie"],
         workers["james"],
     )
-    alpha_1 = torch.tensor([[0, 1]]).share(alice, bob, charlie, crypto_provider=james).child
-    ones = torch.tensor([[1, 1]]).share(alice, bob, charlie, crypto_provider=james).child
-    twos = torch.tensor([[2, 2]]).share(alice, bob, charlie, crypto_provider=james).child
+    alpha_1 = torch.tensor([[0, 1]]).share(alice, bob, charlie, crypto_provider=james, dtype="long").child
+    ones = torch.tensor([[1, 1]]).share(alice, bob, charlie, crypto_provider=james, dtype="long").child
+    twos = torch.tensor([[2, 2]]).share(alice, bob, charlie, crypto_provider=james, dtype="long").child
     selected = securenn.select_share(alpha_1, ones, twos)
     assert (selected.get() == torch.tensor([[1, 2]])).all()
 
@@ -25,14 +25,13 @@ def test_private_compare(workers):
         workers["james"],
     )
     x_bits = (
-        securenn.decompose(torch.tensor([3, 3]))
-        .share(alice, bob, charlie, crypto_provider=james)
+        securenn.decompose(torch.tensor([3, 3]), 2**64)
+        .share(alice, bob, charlie, dtype="custom", field=67, crypto_provider=james)
         .child
     )
     r = torch.tensor([1, 5]).send(alice, bob, charlie).child
     b = torch.tensor([0]).send(alice, bob, charlie).child
-    compare = securenn.private_compare(x_bits, r, b)
-    print("compare", compare)
+    compare = securenn.private_compare(x_bits, r, b, 2**64)
     assert (compare == torch.tensor([1, 0])).all()
 
 
@@ -45,7 +44,7 @@ def test_share_convert(workers):
     )
     tensorA = (
         torch.tensor([10, 20, 30])
-        .share(alice, bob, charlie, crypto_provider=james, field=2 ** 60)
+        .share(alice, bob, charlie, crypto_provider=james, dtype="long")
         .child
     )
     tensorB = securenn.share_convert(tensorA)
@@ -62,7 +61,7 @@ def test_msb(workers):
     )
     tensorA = (
         torch.tensor([-10, 0, 10])
-        .share(alice, bob, charlie, crypto_provider=james, field=2 ** 60 - 1)
+        .share(alice, bob, charlie, crypto_provider=james, dtype="custom", field=2 ** 60 - 1)
         .child
     )
     assert (securenn.msb(tensorA).get() == torch.tensor([1, 0, 0])).all()
@@ -77,7 +76,7 @@ def test_relu_deriv(workers):
     )
     tensorA = (
         torch.tensor([-10, 0, 10])
-        .share(alice, bob, charlie, crypto_provider=james, field=2 ** 60)
+        .share(alice, bob, charlie, crypto_provider=james, dtype="long")
         .child
     )
     assert (securenn.relu_deriv(tensorA).get() == torch.tensor([0, 1, 1])).all()
@@ -92,7 +91,7 @@ def test_relu(workers):
     )
     tensorA = (
         torch.tensor([-10, 0, 10])
-        .share(alice, bob, charlie, crypto_provider=james, field=2 ** 60)
+        .share(alice, bob, charlie, crypto_provider=james, dtype="long")
         .child
     )
     assert (securenn.relu(tensorA).get() == torch.tensor([0, 0, 10])).all()
@@ -106,10 +105,10 @@ def test_division(workers):
         workers["james"],
     )
     tensorA = (
-        torch.tensor([[0, 10, 10, 20, 20]]).share(alice, bob, charlie, crypto_provider=james).child
+        torch.tensor([[0, 10, 10, 20, 20]]).share(alice, bob, charlie, crypto_provider=james, dtype="long").child
     )
     tensorB = (
-        torch.tensor([[1, 2, 3, 4, 5]]).share(alice, bob, charlie, crypto_provider=james).child
+        torch.tensor([[1, 2, 3, 4, 5]]).share(alice, bob, charlie, crypto_provider=james, dtype="long").child
     )
     division = securenn.division(tensorA, tensorB)
     assert (division.get() == torch.tensor([[0, 5, 3, 5, 4]])).all()
@@ -123,7 +122,7 @@ def test_maxpool(workers):
         workers["james"],
     )
     tensorA = (
-        torch.tensor([[0, 1, 8, 3, 4]]).share(alice, bob, charlie, crypto_provider=james).child
+        torch.tensor([[0, 1, 8, 3, 4]]).share(alice, bob, charlie, crypto_provider=james, dtype="long").child
     )
     v, i = securenn.maxpool(tensorA)
     assert (v.get() == torch.tensor([[8]])).all()
@@ -139,7 +138,7 @@ def test_maxpool_deriv(workers):
     )
     tensorA = (
         torch.tensor([[0, 1, 8, 3]])
-        .share(alice, bob, charlie, crypto_provider=james, field=2 ** 60)
+        .share(alice, bob, charlie, crypto_provider=james, dtype="long")
         .child
     )
     deriv = securenn.maxpool_deriv(tensorA)
