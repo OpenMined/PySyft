1ef911605d8242f138a02662a9f58f4525cf06a8
Karl Higley <kmhigley@gmail.com>
2020-05-07 13:12:41 -0400

    Add a test for registering a `CommunicationAction`

--- test/execution/test_role.py
+++ test/execution/test_role.py
@@ -6,6 +6,7 @@ import syft as sy
 from syft.execution.role import Role
 from syft.execution.placeholder import PlaceHolder
 from syft.execution.computation import ComputationAction
+from syft.execution.communication import CommunicationAction
 
 
 def test_register_computation_action():
@@ -22,6 +23,26 @@ def test_register_computation_action():
     registered = role.actions[0]
 
     assert isinstance(registered, ComputationAction)
+    assert registered.name == "method_name"
+    assert registered.target == target
+    assert registered.args == ()
+    assert registered.kwargs == {}
+
+
+def test_register_communication_action():
+    role = Role()
+    placeholder = PlaceHolder()
+    target = torch.ones([1])
+
+    action = ("method_name", target, (), {})
+
+    role.register_action((action, placeholder), CommunicationAction)
+
+    assert len(role.actions) == 1
+
+    registered = role.actions[0]
+
+    assert isinstance(registered, CommunicationAction)
     assert registered.name == "method_name"
     assert registered.target == target
     assert registered.args == ()
