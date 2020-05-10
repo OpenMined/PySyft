4e9e55782db951faa8b44d2c7b596c926667669f
maddie_shang <mshang@tableau.com>
2020-05-09 23:12:16 -0400

    remote_get working

--- test/execution/test_protocol.py
+++ test/execution/test_protocol.py
@@ -140,18 +140,18 @@ def test_trace_communication_actions_mid_get(workers):
     assert "mid_get" in [action.name for action in traced_actions]
 
 
-# def test_trace_communication_actions_remote_get(workers):
-#     alice, bob = workers["alice"], workers["bob"]
+def test_trace_communication_actions_remote_get(workers):
+    alice, bob = workers["alice"], workers["bob"]
 
-#     @sy.func2protocol(args_shape=[(1,)])
-#     def protocol(tensor):
-#         ptr = tensor.send(bob)
-#         res = ptr.remote_get()
-#         return res
+    @sy.func2protocol(args_shape=[(1,)])
+    def protocol(tensor):
+        ptr = tensor.send(bob).send(alice)
+        res = ptr.remote_get()
+        return res
 
-#     traced_actions = protocol.roles["me"].actions
+    traced_actions = protocol.roles["me"].actions
 
-#     assert protocol.is_built
-#     assert len(traced_actions) == 2
-#     print([action.name for action in traced_actions])
-#     assert "remote_get" in [action.name for action in traced_actions]
+    assert protocol.is_built
+    assert len(traced_actions) == 3
+    print([action.name for action in traced_actions])
+    assert "remote_get" in [action.name for action in traced_actions]
