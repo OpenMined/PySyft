cc5276be537924bbf53785e9094d9ad0365a69e2
Karl Higley <kmhigley@gmail.com>
2020-05-14 08:49:40 -0400

    Update test/torch/tensors/test_additive_shared.py
    Co-authored-by: Théo Ryffel <theo.leffyr@gmail.com>

--- test/torch/tensors/test_additive_shared.py
+++ test/torch/tensors/test_additive_shared.py
@@ -712,7 +712,7 @@ def test_comp(workers, protocol):
     )
 
     if protocol == "fss":
-        for worker in workers:
+        for worker in workers.values():
             syft.frameworks.torch.mpc.fss.initialize_crypto_plans(worker)
         me.crypto_store.provide_primitives(
             ["xor_add_couple", "fss_eq", "fss_comp"], [alice, bob], n_instances=50
