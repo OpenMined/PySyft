6140d08c3d35160c2d57ef6d2421b6c97663044f
Ionésio Junior <ionesiojr@gmail.com>
2020-05-20 10:20:14 -0300

    WebRTC connection peers (#3548)
    * ADD new event/message codes
    
    * ADD webrtc Connection class
    - Class used to process messages sended/received by two different peers.
    
    * Add Nodes Manager class
    - Node connections controller
    
    * ADD webrtc.grid
    - Add the GridNetwork peer class used to connect with the grid network service.
    
    * ADD grid_routes
    - Set of methods used to process grid network requests
    
    * Add asyncio import
    
    * Fix code style
    
    * ADD webrtc_connection docstring
    
    * ADD aiortc dependency
    
    * [REFACTORY] rename grid.py to network.py
    
    * [REFACTORY] rename grid_routes.py -> peer_events.py
    
    * [REFACTORY] rename webrtc_connections.py -> webrtc_connection.py
    
    * Fix import reference
    
    * Create grid.register()
    
    * Add psutil dependency
    
    * Change DEFAULT_NETWORK_URL value

---
+++ syft/grid/webrtc/nodes_manager.py
@@ -0,0 +1,48 @@
+from syft.grid.webrtc.webrtc_connection import WebRTCConnection
+from syft.workers.base import BaseWorker
+import asyncio
+
+
+class WebRTCManager(BaseWorker):
+    """ Class used to manage multiple webrtc peer connections in different threads. """
+
+    def __init__(self, grid_descriptor, syft_worker):
+        self._connections = {}
+        self._grid = grid_descriptor
+        self.worker = syft_worker
+
+    @property
+    def nodes(self):
+        """ Return all the peer nodes connected directly with this peer."""
+        return list(self._connections.keys())
+
+    def _send_msg(self, message: bin, location):
+        """ Forward a local syft request to the proper destination. """
+        return asyncio.run(self._connection[location.id].send(message))
+
+    def _recv_msg(self, message):
+        """ Overwrite BaseWorker's abstract method. But it's not used. """
+        raise NotImplementedError
+
+    def get(self, node_id: str):
+        """ Return a peer connection reference by its ID. """
+        return self._connections.get(node_id, None)
+
+    def process_answer(self, destination: str, content: str):
+        """ Set the webrtc connection answer message. """
+        self._connections[destination].set_msg(content)
+
+    def process_offer(self, destination: str, content: str):
+        """ Create a thread to process a webrtc offer connection. """
+        self._connections[destination] = WebRTCConnection(
+            self._grid, self.worker, destination, self._connections, WebRTCConnection.ANSWER,
+        )
+        self._connections[destination].set_msg(content)
+        self._connections[destination].start()
+
+    def start_offer(self, destination: str):
+        """ Create a new thread to offer a webrtc connection. """
+        self._connections[destination] = WebRTCConnection(
+            self._grid, self.worker, destination, self._connections, WebRTCConnection.OFFER,
+        )
+        self._connections[destination].start()
