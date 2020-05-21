81a3f8a2a36740e8b32125b47a524e8245df4166
jeferson <jeferson.silva@ccc.ufcg.edu.br>
2020-05-21 19:00:45 -0300

    registers a grid node only once in each execution environment

--- syft/grid/__init__.py
+++ syft/grid/__init__.py
@@ -3,6 +3,8 @@ import uuid
 
 DEFAULT_NETWORK_URL = "ws://ec2-13-59-45-128.us-east-2.compute.amazonaws.com"
 
+_registered_peer = None
+
 
 def register(**kwargs):
     """ Add this process as a new peer registering it in the grid network.
@@ -10,13 +12,16 @@ def register(**kwargs):
         Returns:
             peer: Peer Network instance.
     """
-    if not kwargs:
-        args = args = {"max_size": None, "timeout": 444, "url": DEFAULT_NETWORK_URL}
-    else:
-        args = kwargs
+    global _registered_peer
 
-    peer_id = str(uuid.uuid4())
-    peer = Network(peer_id, **args)
-    peer.start()
+    if _registered_peer is None:
+        if not kwargs:
+            args = args = {"max_size": None, "timeout": 444, "url": DEFAULT_NETWORK_URL}
+        else:
+            args = kwargs
 
-    return peer
+        peer_id = str(uuid.uuid4())
+        _registered_peer = Network(peer_id, **args)
+        _registered_peer.start()
+
+    return _registered_peer
