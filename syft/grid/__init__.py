93f356b6f18260e66eeeb004b66d4f58ed101d1e
jeferson <jeferson.silva@ccc.ufcg.edu.br>
2020-05-21 19:33:21 -0300

    add user warning on multiple registration

--- syft/grid/__init__.py
+++ syft/grid/__init__.py
@@ -3,20 +3,27 @@ import uuid
 
 DEFAULT_NETWORK_URL = "ws://ec2-13-59-45-128.us-east-2.compute.amazonaws.com"
 
+_registered_peer = None
+
 
 def register(**kwargs):
     """ Add this process as a new peer registering it in the grid network.
-        
+
         Returns:
             peer: Peer Network instance.
     """
-    if not kwargs:
-        args = args = {"max_size": None, "timeout": 444, "url": DEFAULT_NETWORK_URL}
+    global _registered_peer
+
+    if _registered_peer is None:
+        if not kwargs:
+            args = args = {"max_size": None, "timeout": 444, "url": DEFAULT_NETWORK_URL}
+        else:
+            args = kwargs
+
+        peer_id = str(uuid.uuid4())
+        _registered_peer = Network(peer_id, **args)
+        _registered_peer.start()
     else:
-        args = kwargs
+        raise UserWarning(f"You're already a registered peer {_registered_peer}.")
 
-    peer_id = str(uuid.uuid4())
-    peer = Network(peer_id, **args)
-    peer.start()
-
-    return peer
+    return _registered_peer
