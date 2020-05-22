f97e562e1a56c4b37752838490474471e63aa85f
Ionésio Junior <ionesiojr@gmail.com>
2020-05-22 11:00:28 -0300

    Handle with grid unexpected errors properly (#3593)
    * Handle with unexpected errors properly
    - Handle with unexpected exceptions during syft.grid.register function.
    - Handle with unexpected behavior due notebook async constraints.
    
    * Fix print logs

--- syft/grid/__init__.py
+++ syft/grid/__init__.py
@@ -11,38 +11,42 @@ def register(**kwargs):
         Returns:
             peer: Peer Network instance.
     """
-    if not kwargs:
-        args = args = {"max_size": None, "timeout": 444, "url": DEFAULT_NETWORK_URL}
-    else:
-        args = kwargs
+    try:
+        if not kwargs:
+            args = {"max_size": None, "timeout": 444, "url": DEFAULT_NETWORK_URL}
+        else:
+            args = kwargs
 
-    peer_id = str(uuid.uuid4())
-    sys.stdout.write(
-        "Connecting to OpenGrid (" + "\033[94m" + DEFAULT_NETWORK_URL + "\033[0m" + ") ... "
-    )
-    peer = Network(peer_id, **args)
+        peer_id = str(uuid.uuid4())
+        sys.stdout.write(
+            "Connecting to OpenGrid (" + "\033[94m" + args["url"] + "\033[0m" + ") ... "
+        )
 
-    sys.stdout.write("\033[92m" + "OK" + "\033[0m" + "\n")
-    sys.stdout.write("Peer ID: " + peer_id + "\n")
+        peer = Network(peer_id, **args)
 
-    sys.stdout.write(
-        "\033[93m" + "DISCLAIMER" + "\033[0m"
-        ":"
-        + "\033[1m"
-        + " OpenGrid is an experimental feature currently in alpha. Do not use this to protect real-world data.\n"
-        + "\033[0m"
-    )
+        sys.stdout.write("\033[92m" + "OK" + "\033[0m" + "\n")
+        sys.stdout.write("Peer ID: " + peer_id + "\n")
 
-    sys.stdout.write("Where to get help: \n")
-    sys.stdout.write(
-        " - Join our slack  (https://slack.openmined.org) and ask for help in the #lib_syft channel.\n"
-    )
-    sys.stdout.write(
-        " - File a Github Issue: https://github.com/OpenMined/PySyft and add the string '#opengrid' in the issue title.\n"
-    )
-    sys.stdout.write(
-        " - Want to join in our development team? Apply here: https://forms.gle/wcH1vxzvPyDSbSVW6\n"
-    )
-    peer.start()
+        sys.stdout.write(
+            "\033[93m" + "DISCLAIMER" + "\033[0m"
+            ":"
+            + "\033[1m"
+            + " OpenGrid is an experimental feature currently in alpha. Do not use this to protect real-world data.\n"
+            + "\033[0m"
+        )
 
-    return peer
+        sys.stdout.write("Where to get help: \n")
+        sys.stdout.write(
+            " - Join our slack  (https://slack.openmined.org) and ask for help in the #lib_syft channel.\n"
+        )
+        sys.stdout.write(
+            " - File a Github Issue: https://github.com/OpenMined/PySyft and add the string '#opengrid' in the issue title.\n"
+        )
+        sys.stdout.write(
+            " - Want to join in our development team? Apply here: https://forms.gle/wcH1vxzvPyDSbSVW6\n"
+        )
+        peer.start()
+        return peer
+    except Exception as e:
+        sys.stdout.write("\033[91m" + "FAIL" + "\033[0m" + "\n")
+        sys.stdout.write("You were not able to register your node.\n")
