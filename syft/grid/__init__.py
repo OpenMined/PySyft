eb0b87da9747e921643366491f5f8e0aa2cbc4ce
Jeferson Ferreira <jefersonnpn@gmail.com>
2020-05-22 12:14:16 -0300

    Avoids multiple peers registration in the same grid environment. (#3591)
    * add translation to Portuguese of the tutorial Part 02
    
    * (enhance) translate example toy expression literally
    
    * (minor fixes) change 'portugues' to 'português' and 'nível superior' to 'parte superior'
    
    * add translation to Portuguese of the tutorial Part 03
    
    * (fix) remove old folder name
    
    * add translation to Portuguese of the tutorial Part 04
    
    * (update) rename 'portugues' to 'português' and remove old folder
    
    * add translation to Portuguese of the tutorial Part 05
    
    * add translation to Portuguese of the tutorial Part 06
    
    * (minor fixes) typo and verb correction
    
    * add translation to Portuguese of the tutorial Part 09
    
    * add translation to Portuguese of the tutorial Part 10
    
    * add translation to Portuguese of the tutorial Part 11
    
    * (fix) a cell has been removed by mistake :expressionless:
    
    * (minor fixes) :crossed_fingers: :crossed_fingers: :crossed_fingers:
    
    * (minor fix) full stop added
    
    * (fixes) remove repeated paragraphs left in tutorials part 06 and 11
    
    * registers a grid node only once in each execution environment
    
    * add user warning on multiple registration
    
    * (minor fix) lint warning
    
    Co-authored-by: Andrew Trask <liamtrask@gmail.com>
    Co-authored-by: George-Cristian Muraru <murarugeorgec@gmail.com>
    Co-authored-by: Karl Higley <kmhigley@gmail.com>

--- syft/grid/__init__.py
+++ syft/grid/__init__.py
@@ -4,13 +4,25 @@ import uuid
 
 DEFAULT_NETWORK_URL = "ws://ec2-13-59-45-128.us-east-2.compute.amazonaws.com"
 
+_registered_peer = None
+
 
 def register(**kwargs):
     """ Add this process as a new peer registering it in the grid network.
-        
+
         Returns:
             peer: Peer Network instance.
     """
+    global _registered_peer
+
+    if isinstance(_registered_peer, Network):
+        sys.stdout.write(
+            "\033[93m" + "WARNING" + "\033[0m"
+            ":" + f" You are already a registered peer!\n{_registered_peer}\n"
+        )
+
+        return _registered_peer
+
     try:
         if not kwargs:
             args = {"max_size": None, "timeout": 444, "url": DEFAULT_NETWORK_URL}
@@ -22,7 +34,7 @@ def register(**kwargs):
             "Connecting to OpenGrid (" + "\033[94m" + args["url"] + "\033[0m" + ") ... "
         )
 
-        peer = Network(peer_id, **args)
+        _registered_peer = Network(peer_id, **args)
 
         sys.stdout.write("\033[92m" + "OK" + "\033[0m" + "\n")
         sys.stdout.write("Peer ID: " + peer_id + "\n")
@@ -45,8 +57,11 @@ def register(**kwargs):
         sys.stdout.write(
             " - Want to join in our development team? Apply here: https://forms.gle/wcH1vxzvPyDSbSVW6\n"
         )
-        peer.start()
-        return peer
+
+        _registered_peer.start()
+
+        return _registered_peer
+
     except Exception as e:
         sys.stdout.write("\033[91m" + "FAIL" + "\033[0m" + "\n")
         sys.stdout.write("You were not able to register your node.\n")
