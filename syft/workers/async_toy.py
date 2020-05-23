9630d9fee154bb752c0e5991647129350c9a9797
Karl Higley <kmhigley@gmail.com>
2020-05-23 11:59:28 -0400

    Create toy workers, actions, and messages to experiment with async

---
+++ syft/workers/async_toy.py
@@ -0,0 +1,131 @@
+from dataclasses import dataclass
+from dataclasses import field
+
+import asyncio
+
+
+@dataclass
+class ToyAction:
+    command: str
+    value: str = field(default_factory=str)
+    dependencies: list = field(default_factory=list)
+
+
+@dataclass
+class ToyMessage:
+    destination: str
+    command: str
+    value: str
+
+    def to_action(self):
+        return ToyAction(dependencies=[], command=self.command, value=self.value)
+
+
+class AbstractToyWorker:
+    counter = 0
+
+    def __init__(self, name: str, actions: list = [], all_workers: dict = {}):
+        self.name = name
+        self.actions = actions
+        self.known_workers = all_workers
+        self.store = {}
+        self.next_action_index = 0
+        self.executed = []
+
+    @property
+    def _name_counter(self):
+        return str(self.name) + str(AbstractToyWorker.counter)
+
+    def receive_msg(self, message: ToyMessage):
+        self._execute_action(message.to_action())
+        # Try to execute anything that is now unlocked by received values
+        self.execute()
+
+    def send_msg(self, message: ToyMessage):
+        self.known_workers[message.destination].receive_msg(message)
+
+    def execute(self):
+        while not self.finished:
+            action = self.actions[self.next_action_index]
+
+            # Check dependencies and return if not satisfied
+            for dep in action.dependencies:
+                if dep not in self.store.keys():
+                    return
+
+            # Run the command if dependencies are satisfied
+            self.next_action_index += 1
+            self._execute_action(action)
+
+    def _execute_action(self, action):
+        # Record the action being executed
+        self.executed.append(action)
+
+        # Set a flag in the store and execute any further actions now unlocked
+        if action.command == "set" or action.command == "receive":
+            self.store[action.value] = True
+        # Send a message that sets a flag on a remote worker
+        elif action.command == "send":
+            msg = ToyMessage(destination=action.value, command="receive", value=self._name_counter)
+            return self.send_msg(msg)
+        # Do a local "computation"
+        elif action.command == "compute":
+            self.store[self._name_counter] = True
+        else:
+            raise ValueError("action.command value not supported.")
+
+        # Increment the global counter of actions executed
+        AbstractToyWorker.counter += 1
+
+        return None
+
+    @property
+    def finished(self):
+        return self.next_action_index >= len(self.actions)
+
+    @classmethod
+    def reset_counter(cls):
+        cls.counter = 0
+
+    @staticmethod
+    def introduce(*args):
+        # Build map of all the workers
+        workers = {}
+        for worker in args:
+            workers[worker.name] = worker
+
+        # Introduce them to each other
+        for worker in args:
+            worker.known_workers = workers
+
+
+class SingleThreadedSynchronousWorker(AbstractToyWorker):
+    pass
+
+
+class SingleThreadedAsynchronousWorker(AbstractToyWorker):
+    async def receive_msg(self, message: ToyMessage):
+        await self._execute_action(message.to_action())
+        # Try to execute anything that is now unlocked by received values
+        await self.execute()
+
+    async def send_msg(self, message: ToyMessage):
+        await self.known_workers[message.destination].receive_msg(message)
+
+    async def execute(self):
+        while not self.finished:
+            action = self.actions[self.next_action_index]
+
+            # Check dependencies and return if not satisfied
+            for dep in action.dependencies:
+                if dep not in self.store.keys():
+                    return
+
+            # Run the command if dependencies are satisfied
+            self.next_action_index += 1
+            await self._execute_action(action)
+
+    async def _execute_action(self, action):
+        awaitable = super()._execute_action(action)
+        if awaitable:
+            await awaitable
---
+++ test/workers/test_async_toy.py
@@ -0,0 +1,166 @@
+import pytest
+import asyncio
+
+from syft.workers.async_toy import AbstractToyWorker
+from syft.workers.async_toy import SingleThreadedSynchronousWorker
+from syft.workers.async_toy import SingleThreadedAsynchronousWorker
+from syft.workers.async_toy import ToyAction
+from syft.workers.async_toy import ToyMessage
+
+
+def test_set():
+    AbstractToyWorker.reset_counter()
+
+    alice = AbstractToyWorker(name="alice")
+    action = ToyAction("set", "test")
+
+    alice._execute_action(action)
+    assert alice.store["test"] is True
+
+
+def test_compute():
+    AbstractToyWorker.reset_counter()
+
+    alice = AbstractToyWorker(name="alice")
+    action = ToyAction("compute", "test")
+
+    alice._execute_action(action)
+
+    assert alice.store == {"alice0": True}
+
+
+def test_send():
+    AbstractToyWorker.reset_counter()
+
+    alice = AbstractToyWorker(name="alice")
+    bob = AbstractToyWorker(name="bob")
+    AbstractToyWorker.introduce(alice, bob)
+
+    action = ToyAction("send", "bob")
+
+    alice._execute_action(action)
+    assert bob.store["alice0"] is True
+
+
+def test_unknown_action():
+    AbstractToyWorker.reset_counter()
+
+    alice = AbstractToyWorker(name="alice")
+    action = ToyAction("flarble", "garb")
+
+    with pytest.raises(ValueError):
+        alice._execute_action(action)
+
+
+def test_action_counter():
+    AbstractToyWorker.reset_counter()
+
+    alice = AbstractToyWorker(name="alice")
+    bob = AbstractToyWorker(name="bob")
+    AbstractToyWorker.introduce(alice, bob)
+
+    # Counter increments by one every time an action is executed
+    action = ToyAction("set", "test1")
+    alice._execute_action(action)
+
+    assert alice.store["test1"] is True
+    assert AbstractToyWorker.counter == 1
+
+    action = ToyAction("set", "test2")
+    alice._execute_action(action)
+
+    assert alice.store["test2"] is True
+    assert AbstractToyWorker.counter == 2
+
+    # Counter increments regardless of which worker executes the action
+    action = ToyAction("set", "test3")
+    bob._execute_action(action)
+
+    assert bob.store["test3"] is True
+    assert AbstractToyWorker.counter == 3
+
+    # Send actions increment the counter only for send
+    action = ToyAction("send", "bob")
+    alice._execute_action(action)
+
+    assert bob.store["alice3"] is True
+    assert AbstractToyWorker.counter == 4
+
+
+def test_single_threaded_synchronous_comms():
+    AbstractToyWorker.reset_counter()
+
+    alice = SingleThreadedSynchronousWorker(name="alice")
+    bob = SingleThreadedSynchronousWorker(name="bob")
+    AbstractToyWorker.introduce(alice, bob)
+
+    # Create a potential deadlock
+    alice.actions = [ToyAction("send", "bob"), ToyAction("compute", dependencies=["bob2"])]
+
+    bob.actions = [ToyAction("compute", dependencies=["alice0"]), ToyAction("send", "alice")]
+
+    # Take turns executing synchronously
+    while not (alice.finished and bob.finished):
+        alice.execute()
+        bob.execute()
+
+    assert len(alice.executed) == 3
+    assert len(bob.executed) == 3
+
+    assert bob.store["alice0"] is True
+    assert bob.store["bob1"] is True
+    assert alice.store["bob2"] is True
+    assert alice.store["alice3"] is True
+
+
+@pytest.mark.asyncio
+async def test_single_threaded_async_comms():
+    AbstractToyWorker.reset_counter()
+
+    alice = SingleThreadedAsynchronousWorker(name="alice")
+    bob = SingleThreadedAsynchronousWorker(name="bob")
+    AbstractToyWorker.introduce(alice, bob)
+
+    # Create a potential deadlock
+    alice.actions = [ToyAction("send", "bob"), ToyAction("compute", dependencies=["bob2"])]
+
+    bob.actions = [ToyAction("compute", dependencies=["alice0"]), ToyAction("send", "alice")]
+
+    # Take turns executing asynchronously
+    while not (alice.finished and bob.finished):
+        await alice.execute()
+        await bob.execute()
+
+    assert len(alice.executed) == 3
+    assert len(bob.executed) == 3
+
+    assert bob.store["alice0"] is True
+    assert bob.store["bob1"] is True
+    assert alice.store["bob2"] is True
+    assert alice.store["alice3"] is True
+
+
+@pytest.mark.asyncio
+async def test_single_threaded_async_comms_concurrent():
+    AbstractToyWorker.reset_counter()
+
+    alice = SingleThreadedAsynchronousWorker(name="alice")
+    bob = SingleThreadedAsynchronousWorker(name="bob")
+    AbstractToyWorker.introduce(alice, bob)
+
+    # Create a potential deadlock
+    alice.actions = [ToyAction("send", "bob"), ToyAction("compute", dependencies=["bob2"])]
+
+    bob.actions = [ToyAction("compute", dependencies=["alice0"]), ToyAction("send", "alice")]
+
+    # Execute asynchronously AND concurrently
+    while not (alice.finished and bob.finished):
+        await asyncio.gather(alice.execute(), bob.execute())
+
+    assert len(alice.executed) == 3
+    assert len(bob.executed) == 3
+
+    assert bob.store["alice0"] is True
+    assert bob.store["bob1"] is True
+    assert alice.store["bob2"] is True
+    assert alice.store["alice3"] is True
