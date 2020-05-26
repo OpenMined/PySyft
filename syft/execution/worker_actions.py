972f7e9876353727bd129aa6889f1d10cdfae995
maddie_shang <mshang@tableau.com>
2020-05-25 20:19:48 -0400

    formatting

--- syft/execution/worker_actions.py
+++ syft/execution/worker_actions.py
@@ -1,13 +1,11 @@
-from typing import List, Union, Tuple
-
-import syft as sy
 from syft.execution.action import Action
 from syft.workers.abstract import AbstractWorker
 
-from syft_proto.execution.v1.communication_action_pb2 import (
-    CommunicationAction as CommunicationActionPB,  # HACK replace this with the correct syft proto objects and update proto version in reqs and update documentations
+from syft_proto.execution.v1.worker_action_pb2 import (
+    WorkerAction as CommunicationActionPB,
+    # HACK replace this with the correct syft proto objects and update proto version in reqs
+    # and update documentations
 )
-
 
 WORKER_ACTION_METHODS = ["store", "load"]
 
@@ -23,19 +21,21 @@ class WorkerAction(Action):
             target (Tensor): The object to invoke the method on
             args_ (Tuple): The arguments to the method call
             kwargs_ (Dictionary): The keyword arguments to the method call
-            return_ids (Tuple): primarily for our async infrastructure (Plan, Protocol, etc.), the id of
-                action results are set by the client. This allows the client to be able to predict where
-                the results will be ahead of time. Importantly, this allows the client to pre-initalize the
-                pointers to the future data, regardless of whether the action has yet executed. It also
+            return_ids (Tuple): primarily for our async infrastructure (Plan, Protocol, etc.), the
+                id of action results are set by the client. This allows the client to be able to
+                predict where the results will be ahead of time. Importantly, this allows the
+                client to pre-initalize the pointers to the future data, regardless of whether
+                the action has yet executed. It also
                 reduces the size of the response from the action (which is very often empty).
-            return_value (boolean): return the result or not. If true, the result is directly returned,
-                if not, the command sender will create a pointer to the remote result using the return_ids
-                and will need to do .get() later to get the result.
+            return_value (boolean): return the result or not. If true, the result is directly
+                returned, if not, the command sender will create a pointer to the remote result
+                using the return_ids and will need to do .get() later to get the result.
 
         """
         if name not in WORKER_ACTION_METHODS:
             raise ValueError(
-                f"Method `{name}` is not supported by WorkerActions. Consider using ComputationAction or CommunicationAction instead."
+                f"Method `{name}` is not supported by WorkerActions. Consider using \
+                ComputationAction or CommunicationAction instead."
             )
 
         super().__init__(name, target, args_, kwargs_, return_ids, return_value=return_value)
