749d3c25ed08f1793319c73760037b0e65dfc15d
joaolcaas <joao.felipe@ccc.ufcg.edu.br>
2020-05-26 14:37:29 -0300

    trying to delete side effect

--- syft/frameworks/torch/fl/utils.py
+++ syft/frameworks/torch/fl/utils.py
@@ -82,8 +82,11 @@ def federated_avg(models: Dict[Any, torch.nn.Module]) -> torch.nn.Module:
     """
     nr_models = len(models)
     model_list = list(models.values())
-    model = model_list[0]
-    for i in range(1, nr_models):
+    model = type(model_list[0])()
+
+    model.__dict__ = model_list[0].__dict__.copy()
+
+    for i in range(nr_models):
         model = add_model(model, model_list[i])
     model = scale_model(model, 1.0 / nr_models)
     return model
