53277cb88967fdfce26db94ca987e80f47ffce6f
Ariann Farias <chael.andrade@gmail.com>
2020-05-31 19:15:22 -0300

    Tests for crypto provider not found exception

--- test/test_exceptions.py
+++ test/test_exceptions.py
@@ -34,3 +34,23 @@ def test_tensors_not_collated_exception(workers):
 
     with pytest.raises(sy.exceptions.TensorsNotCollocatedException):
         b = x + y
+
+
+def test_crypto_provider_not_found_exception(workers):
+    """
+    Ensure that the CryptoProviderNotFoundError works as expected
+    raises when operations like Multiplication and Comparison are made
+    on shared tensors that doesn't have a crypto provider.
+    """
+
+    bob = workers["bob"]
+    alice = workers["alice"]
+
+    x = th.tensor([1, 2, 3, 4, 5]).send(bob)
+    y = th.tensor([1, 2, 3, 4, 5]).send(alice)
+
+    with pytest.raises(sy.exceptions.CryptoProviderNotFoundError):
+        b = x * y
+
+    with pytest.raises(sy.exceptions.CryptoProviderNotFoundError):
+        b = x == y
