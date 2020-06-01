77118d5c39508f9fc545b8a575ebe24063d880a0
Karl Higley <kmhigley@gmail.com>
2020-06-01 16:23:59 -0400

    Move `get`/`mid_get` methods from `AbstractSendable` to `AbstractTensor`
    These methods only make sense on tensors as it turns out.

--- syft/generic/abstract/sendable.py
+++ syft/generic/abstract/sendable.py
@@ -9,24 +9,3 @@ class AbstractSendable(AbstractObject, SyftSerializable):
 
     def send(self, destination):
         return self.owner.send_obj(self, destination)
-
-    def get(self):
-        """Just a pass through. This is most commonly used when calling .get() on a
-        Syft tensor which has a child which is a pointer, an additive shared tensor,
-        a multi-pointer, etc."""
-        class_attributes = self.get_class_attributes()
-        return type(self)(
-            **class_attributes,
-            owner=self.owner,
-            tags=self.tags,
-            description=self.description,
-            id=self.id,
-        ).on(self.child.get())
-
-    def mid_get(self):
-        """This method calls .get() on a child pointer and correctly registers the results"""
-
-        child_id = self.id
-        tensor = self.get()
-        tensor.id = child_id
-        self.owner.register_obj(tensor)
