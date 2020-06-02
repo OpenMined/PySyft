a008438e63863f8111c4811181d2769e603a7a8d
Karl Higley <kmhigley@gmail.com>
2020-06-02 13:35:48 -0400

    Add a dictionary for storing remote references to `AbstractSendable`

--- syft/generic/abstract/sendable.py
+++ syft/generic/abstract/sendable.py
@@ -1,4 +1,5 @@
 from syft.serde.syft_serializable import SyftSerializable
+from collections import defaultdict
 from syft.generic.abstract.object import AbstractObject
 
 
@@ -6,6 +7,12 @@ class AbstractSendable(AbstractObject, SyftSerializable):
     """
     This layers functionality for sending objects between workers on top of AbstractObject.
     """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # { worker_id -> set(object_ids) }
+        self.remote_refs = defaultdict(set)
 
     def send(self, destination):
         return self.owner.send_obj(self, destination)
