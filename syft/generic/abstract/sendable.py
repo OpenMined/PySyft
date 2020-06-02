0262b152c1e65d5e15896666ca73a952ca9e791a
Alan Aboudib <agabudeeb@gmail.com>
2020-06-03 01:18:43 +0200

    Update AbstractSendable's send() method

--- syft/generic/abstract/sendable.py
+++ syft/generic/abstract/sendable.py
@@ -1,11 +1,23 @@
 from syft.serde.syft_serializable import SyftSerializable
 from syft.generic.abstract.object import AbstractObject
+from syft.workers.base import BaseWorker
+from syft.generic.pointers.object_pointer import ObjectPointer
 
 
 class AbstractSendable(AbstractObject, SyftSerializable):
-    """
-    This layers functionality for sending objects between workers on top of AbstractObject.
+    """This layers functionality for sending objects between workers on top of AbstractObject.
     """
 
-    def send(self, destination):
-        return self.owner.send_obj(self, destination)
+    def send(self, destination: BaseWorker) -> ObjectPointer:
+        """Send the current object to the worker `destination`.
+
+        Args:
+            destination: The worker where the current object is sent.
+ 
+        Returns:
+            A pointer to the send object.
+        """
+
+        ptr = self.owner.send(self, destination)
+
+        return ptr
