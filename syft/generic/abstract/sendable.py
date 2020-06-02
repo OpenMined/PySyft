f13e7a87e42f16d614a43295fdf036f8215d54c6
Alan Aboudib <agabudeeb@gmail.com>
2020-06-03 01:40:52 +0200

    Updated type hints in AbstractSendable

--- syft/generic/abstract/sendable.py
+++ syft/generic/abstract/sendable.py
@@ -1,14 +1,12 @@
 from syft.serde.syft_serializable import SyftSerializable
 from syft.generic.abstract.object import AbstractObject
-from syft.workers.base import BaseWorker
-from syft.generic.pointers.object_pointer import ObjectPointer
 
 
 class AbstractSendable(AbstractObject, SyftSerializable):
     """This layers functionality for sending objects between workers on top of AbstractObject.
     """
 
-    def send(self, destination: BaseWorker) -> ObjectPointer:
+    def send(self, destination: "BaseWorker") -> "ObjectPointer":
         """Send the current object to the worker `destination`.
 
         Args:
