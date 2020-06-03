57639d876fc3c0e133346ab2059330a342c594fe
Karl Higley <kmhigley@gmail.com>
2020-06-03 09:34:25 -0400

    Allow modifying `args` and `kwargs` passed through `chain_call` methods
    This make it possible for `_before_method()` and `_after_method()` hooks
    to make modification to the arguments and keyword arguments that get
    passed through the tensor chain and into the method itself.

--- syft/generic/abstract/hookable.py
+++ syft/generic/abstract/hookable.py
@@ -1,28 +1,47 @@
 from functools import wraps
 
 
+def parse_return(raw_return):
+    if not isinstance(raw_return, tuple):
+        raw_return = (raw_return, [], {})
+    return_val, *modified_args_kwargs = raw_return
+    modified_args, *modified_kwargs = modified_args_kwargs
+    modified_kwargs = modified_kwargs[0] if len(modified_kwargs) > 0 else {}
+    return return_val, modified_args, modified_kwargs
+
+
 def map_chain_call(obj, method_name, *args, **kwargs):
     """Calls each hook method in sequence and creates a list of the return values"""
     results = []
     current = obj
+    args_ = args
+    kwargs_ = kwargs
     while current is not None:
         method = getattr(current, method_name, None)
         if method:
-            results.append(method(*args, **kwargs))
+            return_val, mod_args, mod_kwargs = parse_return(method(*args_, **kwargs_))
+            results.append(return_val)
+            args_ = mod_args if len(mod_args) == len(args_) else args_
+            kwargs_ = mod_kwargs if len(mod_kwargs) == len(kwargs_) else kwargs_
         current = getattr(current, "child", None)
-    return results
+    return (results, args_, kwargs_)
 
 
 def reduce_chain_call(obj, method_name, initial_val, *args, **kwargs):
     """Calls each hook method in sequence, passing return values from one to the next"""
     result = initial_val
     current = obj
+    args_ = args
+    kwargs_ = kwargs
     while current is not None:
         method = getattr(current, method_name, None)
         if method:
-            result = method(result, *args, **kwargs)
+            return_val, mod_args, mod_kwargs = parse_return(method(result, *args_, **kwargs_))
+            result = return_val
+            args_ = mod_args if len(mod_args) == len(args_) else args_
+            kwargs_ = mod_kwargs if len(mod_kwargs) == len(kwargs_) else kwargs_
         current = getattr(current, "child", None)
-    return result
+    return (result, args_, kwargs_)
 
 
 def hookable(hookable_method):
@@ -37,9 +56,11 @@ def hookable(hookable_method):
 
     @wraps(hookable_method)
     def hooked_method(self, *args, **kwargs):
-        map_chain_call(self, f"_before_{method_name}", *args, **kwargs)
-        return_val = hookable_method(self, *args, **kwargs)
-        return_val = reduce_chain_call(self, f"_after_{method_name}", return_val, *args, **kwargs)
+        _, args_, kwargs_ = map_chain_call(self, f"_before_{method_name}", *args, **kwargs)
+        return_val = hookable_method(self, *args_, **kwargs_)
+        return_val, args_, kwargs_ = reduce_chain_call(
+            self, f"_after_{method_name}", return_val, *args_, **kwargs_
+        )
         return return_val
 
     return hooked_method
--- test/generic/test_hookable.py
+++ test/generic/test_hookable.py
@@ -3,24 +3,6 @@ from syft.generic.abstract.hookable import reduce_chain_call
 from syft.generic.abstract.hookable import hookable
 
 
-def test_reduce_chain_call():
-    class Reduceable:
-        def __init__(self, value):
-            self.child = None
-            self.value = value
-
-        def reduceable(self, value):
-            return value + self.value
-
-    c1 = Reduceable(1)
-    c1.child = Reduceable(2)
-    c1.child.child = Reduceable(3)
-
-    return_val = reduce_chain_call(c1, "reduceable", 0)
-
-    assert return_val == 6
-
-
 def test_map_chain_call():
     class Mappable:
         def __init__(self, value):
@@ -34,9 +16,69 @@ def test_map_chain_call():
     c1.child = Mappable(2)
     c1.child.child = Mappable(3)
 
-    return_val = map_chain_call(c1, "mappable")
+    return_val, *_ = map_chain_call(c1, "mappable")
 
     assert return_val == [1, 2, 3]
+
+
+def test_map_chain_call_modifies_args():
+    class MappableMod:
+        def __init__(self, value):
+            self.child = None
+            self.value = value
+
+        def mappable(self, *args, **kwargs):
+            arg_value = args[0]
+            kwarg_key, kwarg_value = list(kwargs.items())[0]
+            result = self.value + arg_value + kwarg_value
+            return (result, [arg_value + 1], {kwarg_key: kwarg_value + 1})
+
+    c1 = MappableMod(1)
+    c1.child = MappableMod(1)
+    c1.child.child = MappableMod(1)
+
+    return_val, *_ = map_chain_call(c1, "mappable", 0, test=0)
+
+    assert return_val == [1, 3, 5]  # [ (1+0+0), (1+1+1), (1+2+2) ]
+
+
+def test_reduce_chain_call():
+    class Reduceable:
+        def __init__(self, value):
+            self.child = None
+            self.value = value
+
+        def reduceable(self, accumulator):
+            return accumulator + self.value
+
+    c1 = Reduceable(1)
+    c1.child = Reduceable(2)
+    c1.child.child = Reduceable(3)
+
+    return_val, *_ = reduce_chain_call(c1, "reduceable", 0)
+
+    assert return_val == 6  # 1 + 2 + 3
+
+
+def test_reduce_chain_call_modifies_args():
+    class ReduceableMod:
+        def __init__(self, value):
+            self.child = None
+            self.value = value
+
+        def reduceable(self, accumulator, *args, **kwargs):
+            arg_value = args[0]
+            kwarg_key, kwarg_value = list(kwargs.items())[0]
+            result = accumulator + self.value + arg_value + kwarg_value
+            return (result, [arg_value + 1], {kwarg_key: kwarg_value + 1})
+
+    c1 = ReduceableMod(1)
+    c1.child = ReduceableMod(1)
+    c1.child.child = ReduceableMod(1)
+
+    return_val, *_ = reduce_chain_call(c1, "reduceable", 0, 0, test=0)
+
+    assert return_val == 9  # (1+0+0) + (1+1+1) + (1+2+2)
 
 
 def test_hooks_get_called():
@@ -64,8 +106,31 @@ def test_hooks_get_called():
     assert h.flags["after_flag"] is True
 
 
-def test_hooks_propagate_return_val():
-    class Alterable:
+def test_hooks_can_modify_args():
+    class ArgMod:
+        def __init__(self, value):
+            self.child = None
+            self.value = value
+
+        def _before_concat(self, other):
+            reversed = other[::-1]
+            return (None, [reversed], {})
+
+        @hookable
+        def concat(self, other):
+            return self.value + other
+
+    a = ArgMod("first")
+    a.child = ArgMod("second")
+    a.child.child = ArgMod("third")
+
+    result = a.concat("string")
+
+    assert result == "firstgnirts"
+
+
+def test_hooks_can_modify_return_val():
+    class ReturnMod:
         def __init__(self, value, after):
             self.child = None
             self.value = value
@@ -80,10 +145,10 @@ def test_hooks_propagate_return_val():
             return_val = {**return_val, **self.after}
             return return_val
 
-    a = Alterable({"a": 1}, {"after_a": 1})
-    a.child = Alterable({"b": 2}, {"after_b": 2})
-    a.child.child = Alterable({"c": 3}, {"after_c": 3})
+    r = ReturnMod({"a": 1}, {"after_a": 1})
+    r.child = ReturnMod({"b": 2}, {"after_b": 2})
+    r.child.child = ReturnMod({"c": 3}, {"after_c": 3})
 
-    result = a.get_value()
+    result = r.get_value()
 
     assert result == {"a": 1, "after_a": 1, "after_b": 2, "after_c": 3}
