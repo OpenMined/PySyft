c628768f4b4af9fd3be80c5014d8e3683ca8f671
Ravikant <ravikantsingh2308@gmail.com>
2020-06-06 16:52:12 +0530

    Add FV Addition operation

---
+++ syft/frameworks/torch/he/fv/evaluator.py
@@ -0,0 +1,57 @@
+import copy
+
+from syft.frameworks.torch.he.fv.util.operations import poly_add_mod
+from syft.frameworks.torch.he.fv.util.operations import multiply_add_plain_with_delta
+from syft.frameworks.torch.he.fv.ciphertext import CipherText
+from syft.frameworks.torch.he.fv.plaintext import PlainText
+from syft.frameworks.torch.he.fv.integer_encoder import IntegerEncoder
+
+
+class Evaluator:
+    def __init__(self, context):
+        self.context = context
+        self.coeff_modulus = context.param.coeff_modulus
+
+    def add(self, op1, op2):
+        op1, op2 = copy.deepcopy(op1), copy.deepcopy(op2)
+
+        if isinstance(op1, CipherText) and isinstance(op2, CipherText):
+            return self._add_cipher_cipher(op1, op2)
+
+        elif isinstance(op1, PlainText) and isinstance(op2, PlainText):
+            return self._add_plain_plain(op1, op2)
+
+        elif isinstance(op1, PlainText) and isinstance(op2, CipherText):
+            return self._add_plain_cipher(op1, op2)
+
+        elif isinstance(op1, CipherText) and isinstance(op2, PlainText):
+            return self._add_plain_cipher(op2, op1)
+
+        else:
+            raise TypeError(f"Addition Operation not supported between {type(op1)} and {type(op2)}")
+
+    def _add_cipher_cipher(self, ct1, ct2):
+        """Adds two ciphertexts.
+        """
+        ct1, ct2 = copy.deepcopy(ct1.data), copy.deepcopy(ct2.data)
+        result = ct2 if len(ct2) > len(ct1) else ct1
+
+        for i in range(min(len(ct1), len(ct2))):
+            for j in range(len(self.coeff_modulus)):
+                result[i][j] = poly_add_mod(ct1[i][j], ct2[i][j], self.coeff_modulus[j])
+
+        return CipherText(result)
+
+    def _add_plain_cipher(self, pt, ct):
+        """Adds a ciphertext and a plaintext.
+        """
+        pt, ct = copy.deepcopy(pt), copy.deepcopy(ct)
+        return multiply_add_plain_with_delta(ct, pt, self.context)
+
+    def _add_plain_plain(self, pt1, pt2):
+        """Adds two plaintexts.
+        """
+        pt1, pt2 = copy.deepcopy(pt1), copy.deepcopy(pt2)
+        encoder = IntegerEncoder(self.context)
+
+        return encoder.encode(encoder.decode(pt1) + encoder.decode(pt2))
