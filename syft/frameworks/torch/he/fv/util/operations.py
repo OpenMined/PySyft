cc5c50dc5ab320a92af347c5e49dcbc95aed49b7
Ravikant <ravikantsingh2308@gmail.com>
2020-06-08 22:23:57 +0530

    Add inline comment

--- syft/frameworks/torch/he/fv/util/operations.py
+++ syft/frameworks/torch/he/fv/util/operations.py
@@ -60,11 +60,15 @@ def invert_mod(value, modulus):
 def poly_add_mod(op1, op2, modulus):
     """return addition of two polynomials with all coefficients of
     polynomial %q(coefficient modulus)"""
+
+    # For non same size polynomails we have to shift the polynomials because numpy consider right
+    # side as lower order of polynomial and we consider right side as heigher order.
     if len(op1) != len(op2):
         if len(op1) > len(op2):
             op2 = op2 + [0] * (len(op1) - len(op2))
         else:
             op1 = op1 + [0] * (len(op2) - len(op1))
+
     return np.mod(np.polyadd(op1, op2), modulus).tolist()
 
 
