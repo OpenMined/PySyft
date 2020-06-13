642a1971fc0f3c99fe0d79d829436bf111e33c65
Alan Aboudib <agabudeeb@gmail.com>
2020-06-13 23:06:55 +0200

    updated the wrapping condition in respond_to_search()

--- syft/workers/message_handler.py
+++ syft/workers/message_handler.py
@@ -249,7 +249,7 @@ class BaseMessageHandler(AbstractMessageHandler):
 
             # Wrap only if the pointer points to a tensor.
             # If it points to a generic object, do not wrap.
-            if isinstance(ptr, PointerTensor):
+            if isinstance(obj, FrameworkTensor):
                 ptr.wrap()
 
             results.append(ptr)
