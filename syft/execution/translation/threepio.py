ba29910cc89a632dee7ccdcf9bc9a066dad8dcca
Mike Nolan <me@michael-nolan.com>
2020-06-19 11:09:26 +0100

    Update to threepio 0.0.9

--- syft/execution/translation/threepio.py
+++ syft/execution/translation/threepio.py
@@ -1,5 +1,5 @@
 from pythreepio.threepio import Threepio
-from pythreepio.utils import Command
+from pythreepio.command import Command
 from syft.execution.action import Action
 from syft.execution.role import Role
 from syft.execution.translation import TranslationTarget
@@ -17,14 +17,17 @@ class PlanTranslatorThreepio(AbstractPlanTranslator):
         threepio = Threepio(self.plan.base_framework, to_framework, None)
         function_name = action.name.split(".")[-1]
         args = action.args if action.target is None else (action.target, *action.args)
-        cmd = threepio.translate(Command(function_name, args, action.kwargs))
+        translated_cmds = threepio.translate(Command(function_name, args, action.kwargs))
 
-        new_action = action.copy()
-        new_action.name = ".".join(cmd.attrs)
-        new_action.args = tuple(cmd.args)
-        new_action.kwargs = cmd.kwargs
-        new_action.target = None
-        return new_action
+        actions = []
+        for cmd in translated_cmds:
+            new_action = action.copy()
+            new_action.name = ".".join(cmd.attrs)
+            new_action.args = tuple(cmd.args)
+            new_action.kwargs = cmd.kwargs
+            new_action.target = None
+            actions.append(new_action)
+        return actions
 
     def translate_framework(self, to_framework: str) -> Role:
         """Translates current plan's Role to specified framework"""
@@ -37,7 +40,8 @@ class PlanTranslatorThreepio(AbstractPlanTranslator):
 
         new_actions = []
         for action in role.actions:
-            new_actions.append(self.translate_action(action, to_framework))
+            translated_actions = self.translate_action(action, to_framework)
+            new_actions = [*new_actions, *translated_actions]
         role.actions = tuple(new_actions)
         return role
 
