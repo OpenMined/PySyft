d8f9faf33ffb36e6ce6d0a17f6d81936439ae7a2
Tudor Cebere <tudorcebere@gmail.com>
2020-07-18 13:16:26 +0300

    black fixes

--- src/syft/core/nodes/vm/__init__.py
+++ src/syft/core/nodes/vm/__init__.py
@@ -1,6 +1,6 @@
-from ..abstract.service import WorkerService # noqa: F401
-from typing import Dict # noqa: F401
+from ..abstract.service import WorkerService  # noqa: F401
+from typing import Dict  # noqa: F401
 
 message_service_mapping: Dict[str, WorkerService] = {}
 
-from . import service # noqa: F401
+from . import service  # noqa: F401
