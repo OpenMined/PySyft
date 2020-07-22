c5d8ac95a80d3323855d0980153740061a8a7c8f
Rima <rima@.com>
2020-07-22 10:02:38 -0300

    Establish remote nodes on network

--- src/syft/message/syft_message.py
+++ src/syft/message/syft_message.py
@@ -7,8 +7,11 @@ class SyftMessage(AbstractMessage):
         self.route = route
         self.msg_id = msg_id
 
+class SignedMessage(SyftMessage):
+    def sign(self, my_route):
+        self.my_route = my_route
 
-class SyftMessageWithReply(SyftMessage):
+class SyftMessageWithReply(SignedMessage):
     def __init__(self, route: Route, reply_to: Route, msg_id: UID = None) -> None:
         super(self).__init__(route, msg_id)
         msg.reply_to = reply_to
