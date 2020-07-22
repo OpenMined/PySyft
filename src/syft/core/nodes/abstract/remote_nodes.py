fb8decd482fe7c6f43c7d0ac025d38c7dbf3597e
Rima <rima@.com>
2020-07-22 09:21:06 -0300

    Abstracted remote nodes

--- src/syft/core/nodes/abstract/remote_nodes.py
+++ src/syft/core/nodes/abstract/remote_nodes.py
@@ -9,34 +9,53 @@ class NodeTypes(enum):
     Device = 3
     VM = 4
 
-class MyRemoteNodes(object):
-    nodes = {}
+class Node(object):
+    def __init__(self, type, id = None, name = None, route = None, tags=[]):
+        self.id = id
+        self.name = name
+        self.type = type
+        self.route = route
+        self.tags = tags
+
+    def as_dict(self, on_key = 'id'):
+        key = getattr(self, on_key)
+        if on_key == 'tags':
+            key = self._flatten_tags_for_search(key)
+        {key: {
+            'id': self.id,
+            'name': self.name,
+            'type': self.type,
+            'route': self.route,
+            'tags': self.tags}}
+
+    def _flatten_tags_for_search(self):
+        return tags.join('-')
+
+class RemoteNodes(object):
+    nodes = []
 
     def __init__(self, my_type):
-        for type in NodeTypes:
-            self.nodes.update({type.name: []})
         self.iam = my_type
 
-    def register_node(self, route: Route, type: str) -> None:
-        if type not in self.nodes:
-            # log unknown type
-            return
+    def register_node(self, type: str, id: UID = None, name: str = None,
+        route: Route = None, tags: list = None) -> None:
+        self.nodes.append(Node(type, id, name, route, tags))
 
-        self.nodes[type].append(route)
+    def forget_node(self, key: str, value: str) -> None:
+        #as_dict = self.as_dict(key)
+        #del as_dict[value]
+        #self.nodes =
+        pass
 
-    def forget_node(self, route: Route, type: str) -> None:
-        self.nodes[type].pop(route)
+    def as_dict(self, on_key = 'id'):
+        all = {}
+        for node in nodes:
+            all.update(node.as_dict(on_key))
+        return all
 
-    def get_node(self, type: str, id_or_name:(str, UID)):
-        try:
-            return self.nodes[type][id_or_name]
-        except KeyError as e:
-            try:
-                id = self.vm_name2id[id_or_name]
-                return self.vms[id]
-            except KeyError as e:
-                raise KeyError("You must ask for a vm using either a name or ID.")
-
+    def get_node(self, key: str = 'id', value:(str, UID)):
+        nodes = self.as_dict(on_key = key)
+        return nodes.get(value)
 
     def route_message_to_relevant_nodes(self, message: SyftMessage) -> None:
         """
@@ -46,26 +65,8 @@ class MyRemoteNodes(object):
         Device: routes to VMs
         VM: doesn't route
         """
-        route = message.route
-        if self.iam == 'Network':
-            if route.domain != '*':
-                route.domain.client().send(message)
-            else:
-                for domain in self.nodes['domain']:
-                    domain.client().send(message)
-        elif self.iam == 'Domain':
-            if route.device != '*':
-                route.device.client().send(message)
-            else:
-                for device in self.nodes['device']:
-                    device.client().send(message)
-        elif self.iam == 'Device':
-            if route.vm != '*':
-                route.vm.client().send(message)
-            else:
-                for vm in self.nodes['vm']:
-                    vm.client().send(message)
+        raise NotImplementedError()
 
     def broadcast(self, route: Route):
         channel = route.broadcast_channel
-        route.client().send_msg
+        route.connection().send_msg
