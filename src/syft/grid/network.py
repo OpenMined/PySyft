79e3c6d8382d57614eff8eedd0813aff97c0b681
Andrew Trask <andrew@openmined.org>
2020-07-24 15:12:18 +0100

    init simplegrid

---
+++ src/syft/grid/network.py
@@ -0,0 +1,21 @@
+"""The purpose of this application is to allow us to dev and test PySyft
+functionality on an actual local network. This is NOT meant to be run in
+production (that's the *actual* grid's job)."""
+
+from flask import Flask
+app = Flask(__name__)
+
+from syft.core.nodes.network.network import Network
+import pickle
+
+network = Network(name="ucsf-net")
+
+@app.route('/')
+def get_client():
+
+    client = network.get_client()
+
+    return pickle.dumps(client).hex()
+
+def run():
+    app.run()
