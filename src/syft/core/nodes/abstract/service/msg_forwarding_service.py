72a32bfd3801c64863694daf25bd292cb0af82b3
Andrew Trask <andrew@openmined.org>
2020-07-26 02:18:13 +0100

    Split message_forwarding_service into with/without reply versions

--- src/syft/core/nodes/abstract/service/msg_forwarding_service.py
+++ src/syft/core/nodes/abstract/service/msg_forwarding_service.py
@@ -1,33 +1,65 @@
 from __future__ import annotations
 
 from .....decorators import syft_decorator
-from .node_service import NodeService
+from .node_service import NodeServiceWithoutReply
+from .node_service import NodeServiceWithReply
 from ...common.node import AbstractNode
-from .....common.message import AbstractMessage
+from ....message.syft_message import SyftMessageWithoutReply
+from ....message.syft_message import SyftMessageWithReply
 from typing import List
 
 
-class MessageForwardingService(NodeService):
+class MessageWithoutReplyForwardingService(NodeServiceWithoutReply):
     @syft_decorator(typechecking=True)
-    def process(self, node: AbstractNode, msg: AbstractMessage) -> AbstractMessage:
+    def process(self, node: AbstractNode, msg: SyftMessageWithoutReply) -> None:
+
         addr = msg.address
         pri_addr = addr.pri_address
         pub_addr = addr.pub_address
+
         if pri_addr.vm is not None and node.store.has_object(pri_addr.vm):
-            return node.store.get_object(pri_addr.vm).send_msg(msg=msg)
+            return node.store.get_object(pri_addr.vm).send_msg_without_reply(msg=msg)
 
         if pri_addr.device is not None and node.store.has_object(pri_addr.device):
-            return node.store.get_object(pri_addr.device).send_msg(msg=msg)
+            return node.store.get_object(pri_addr.device).send_msg_without_reply(msg=msg)
 
         if pub_addr.domain is not None and node.store.has_object(pub_addr.domain):
-            return node.store.get_object(pub_addr.domain).send_msg(msg=msg)
+            return node.store.get_object(pub_addr.domain).send_msg_without_reply(msg=msg)
 
         if pub_addr.network is not None and node.store.has_object(pub_addr.network):
-            return node.store.get_object(pub_addr.network).send_msg(msg=msg)
+            return node.store.get_object(pub_addr.network).send_msg_without_reply(msg=msg)
 
         raise Exception("Address unknown - cannot forward message. Throwing it away.")
 
     @staticmethod
     @syft_decorator(typechecking=True)
     def message_handler_types() -> List[type]:
-        return [AbstractMessage]
+        return [SyftMessageWithoutReply]
+
+
+class MessageWithReplyForwardingService(NodeServiceWithReply):
+    @syft_decorator(typechecking=True)
+    def process(self, node: AbstractNode, msg: SyftMessageWithReply) -> None:
+
+        addr = msg.address
+        pri_addr = addr.pri_address
+        pub_addr = addr.pub_address
+
+        if pri_addr.vm is not None and node.store.has_object(pri_addr.vm):
+            return node.store.get_object(pri_addr.vm).send_msg_with_reply(msg=msg)
+
+        if pri_addr.device is not None and node.store.has_object(pri_addr.device):
+            return node.store.get_object(pri_addr.device).send_msg_with_reply(msg=msg)
+
+        if pub_addr.domain is not None and node.store.has_object(pub_addr.domain):
+            return node.store.get_object(pub_addr.domain).send_msg_with_reply(msg=msg)
+
+        if pub_addr.network is not None and node.store.has_object(pub_addr.network):
+            return node.store.get_object(pub_addr.network).send_msg_with_reply(msg=msg)
+
+        raise Exception("Address unknown - cannot forward message. Throwing it away.")
+
+    @staticmethod
+    @syft_decorator(typechecking=True)
+    def message_handler_types() -> List[type]:
+        return [SyftMessageWithReply]
