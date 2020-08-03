3b48fdd730009a69559249fe9d9ceaba541defa2
Andrew Trask <andrew@openmined.org>
2020-08-03 16:57:28 +0100

    Stub to future tests which don't pass yet

--- tests/syft/core/common/object_test.py
+++ tests/syft/core/common/object_test.py
@@ -27,6 +27,28 @@ def test_basic_init():
     obj = ObjectWithID()
     assert isinstance(obj.id, UID)
 
+#
+# def test_immutability_of_id():
+#     """We shouldn't allow people to modify the id of an
+#     ObjectWithID because this can create all sorts of errors.
+#
+#     Put the other way around - blocking people from modifying
+#     the ID of an object means we can build a codebase which more
+#     firmly relies on the id being truthful. It also will avoid
+#     people initialising objects in weird ways (setting ids later).
+#     """
+#
+#
+# def test_compare():
+#
+#     obj = ObjectWithID()
+#     obj2 = ObjectWithID()
+#
+#     assert obj != obj2
+#
+#     obj.id = obj2.id
+#
+#     assert obj == obj2
 
 ################## CLASS METHODS #######################
 ###################### SERDE ##########################
