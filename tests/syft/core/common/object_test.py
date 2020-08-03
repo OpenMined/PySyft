bfe6c2f08f5f8e32e2fdf233ca398c6bd06e07f0
Andrew Trask <andrew@openmined.org>
2020-08-03 16:55:29 +0100

    Add test_basic_init test

--- tests/syft/core/common/object_test.py
+++ tests/syft/core/common/object_test.py
@@ -14,8 +14,41 @@ import pytest
 
 # syft imports
 import syft as sy
+from syft.core.common import UID
 from syft.core.common import ObjectWithID
 
 ################## INITIALIZATION ######################
+
+
+def test_basic_init():
+    """Test that creating ObjectWithID() does in fact create
+    an object with an id."""
+
+    obj = ObjectWithID()
+    assert isinstance(obj.id, UID)
+
+#
+# def test_immutability_of_id():
+#     """We shouldn't allow people to modify the id of an
+#     ObjectWithID because this can create all sorts of errors.
+#
+#     Put the other way around - blocking people from modifying
+#     the ID of an object means we can build a codebase which more
+#     firmly relies on the id being truthful. It also will avoid
+#     people initialising objects in weird ways (setting ids later).
+#     """
+#
+#
+# def test_compare():
+#
+#     obj = ObjectWithID()
+#     obj2 = ObjectWithID()
+#
+#     assert obj != obj2
+#
+#     obj.id = obj2.id
+#
+#     assert obj == obj2
+
 ################## CLASS METHODS #######################
 ###################### SERDE ##########################
