316f2749fe91b58f22e86c26009d9efa61c8bee9
Andrew Trask <andrew@openmined.org>
2020-08-03 18:59:56 +0100

    Adjustd docs styling

--- docs/conf.py
+++ docs/conf.py
@@ -183,8 +183,7 @@ html_theme = "alabaster"
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "sidebar_width": "300px",
-    "page_width": "1400px",
+    "sidebar_width": "300px"
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
