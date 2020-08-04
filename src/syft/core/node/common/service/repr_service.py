439f8f96c7e64954e6328f1a2e3a6ec7ce6f4c9f
Madhava Jay <me@madhavajay.com>
2020-08-04 15:47:42 +1000

    Support Python 3.6
    - Removed unused PEP 0563 calls to from __future__ import annotations
    - Added dataclasses backport to python 3.6

--- src/syft/core/node/common/service/repr_service.py
+++ src/syft/core/node/common/service/repr_service.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from typing import List
 from typing_extensions import final
 
--- src/syft/decorators/syft_decorator_impl.py
+++ src/syft/decorators/syft_decorator_impl.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 import inspect
 
 from .typecheck import type_hints
