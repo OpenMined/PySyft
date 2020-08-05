9260ecad610948fd928f3fbdf5cb8d6ac80964e5
Andrew Trask <andrew@openmined.org>
2020-08-05 21:03:52 +0100

    Add test for StorableObject serde

--- tests/syft/core/store/storable_object_test.py
+++ tests/syft/core/store/storable_object_test.py
@@ -1,11 +1,23 @@
+import syft as sy
 from syft.core.store import StorableObject
 from syft.core.common import UID
 from syft.core.common.serde import _deserialize
 
 
-def test_create_obj():
+def test_create_storable_obj():
     key = UID()
     data = UID()
     description = "This is a dummy test"
     tags = ["dummy", "test"]
     StorableObject(key=key, data=data, description=description, tags=tags)
+
+def test_serde_storable_obj():
+    key = UID()
+    data = UID()
+    description = "This is a dummy test"
+    tags = ["dummy", "test"]
+    obj = StorableObject(key=key, data=data, description=description, tags=tags)
+
+    blob = sy.serialize(obj=obj)
+
+    sy.deserialize(blob=blob)
