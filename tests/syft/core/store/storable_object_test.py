5516775f50c6506eba6ec3663f902d6659e2ed5b
Andrew Trask <andrew@openmined.org>
2020-08-05 21:45:43 +0100

    Init another test for StorableObjct wrapping numpy arrays

--- tests/syft/core/store/storable_object_test.py
+++ tests/syft/core/store/storable_object_test.py
@@ -1,7 +1,8 @@
 import syft as sy
+import numpy as np
+
 from syft.core.store import StorableObject
 from syft.core.common import UID
-from syft.core.common.serde import _deserialize
 
 
 def test_create_storable_obj():
@@ -15,6 +16,20 @@ def test_create_storable_obj():
 def test_serde_storable_obj():
     key = UID()
     data = UID()
+    description = "This is a dummy test"
+    tags = ["dummy", "test"]
+    obj = StorableObject(key=key, data=data, description=description, tags=tags)
+
+    blob = sy.serialize(obj=obj)
+
+    sy.deserialize(blob=blob)
+
+def test_serde_storable_obj_with_wrapped_class():
+    """Ensure that storable object serialization works wrapping non-syft classes (like np.ndarray)"""
+
+
+    key = UID()
+    data = np.array([1,2,3,4])
     description = "This is a dummy test"
     tags = ["dummy", "test"]
     obj = StorableObject(key=key, data=data, description=description, tags=tags)
