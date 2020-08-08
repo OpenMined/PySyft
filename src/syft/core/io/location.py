dfffe342870f82e4755c1cda8b61ed640c823d48
Andrew Trask <andrew@openmined.org>
2020-08-08 22:50:53 +0100

    Init SpecificLocation object

--- src/syft/core/io/location.py
+++ src/syft/core/io/location.py
@@ -12,6 +12,15 @@ class Location(ObjectWithID):
         super().__init__()
 
 
+class SpecificLocation(Location):
+    """This represents the location of a single Node object
+    represented by a single UID. It may not have any functionality
+    beyond Location but there is logic which interprets it differently."""
+
+    def __init__(self):
+        super().__init__()
+
+
 class LocationGroup(Location):
 
     """This represents a group of multiple locations. There
