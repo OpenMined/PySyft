df30609636a0eaec807a52b629a668d9ff2baf8b
Andrew Trask <andrew@openmined.org>
2020-08-08 16:51:19 +0100

    Remove type annotations not comments requirement

--- src/syft/lib/numpy/__init__.py
+++ src/syft/lib/numpy/__init__.py
@@ -3,6 +3,8 @@ from .tensor import NumpyTensorWrapper
 from .array import ArrayConstructor
 from .ndarray import NdArrayConstructor
 
+__all__ = ['NumpyTensorWrapper', 'ArrayConstructor', 'NdArrayConstructor']
+
 from syft.ast.globals import Globals
 
 whitelist = set()
