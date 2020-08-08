02f91cc6666318d1702084c56ecd03b23dc478db
Ravikant Singh <ravikantsingh2308@gmail.com>
2020-08-08 18:07:10 +0530

    Update context to hold context chain instead of one context object

--- syft/frameworks/torch/he/fv/context.py
+++ syft/frameworks/torch/he/fv/context.py
@@ -1,3 +1,4 @@
+import copy
 import math
 from functools import reduce
 
@@ -13,27 +14,88 @@ from syft.frameworks.torch.he.fv.util.global_variable import POLY_MOD_DEGREE_MIN
 
 
 class Context:
-    """A class used as for holding and easily supplying of all the general
-    parameters required throughout the implementation.
+    def __init__(self, params):
+        """A class used as for holding and easily supplying of all the general
+        parameters required throughout the implementation.
+
+        Args:
+            An EncryptionParams object with polynomial modulus, coefficient modulus and
+            plain modulus set.
+        """
+        # Validation of params provided for the encryption scheme.
+        self.key_param_id = params.param_id
+        self.context_data_map = {}
+        self.context_data_map[self.key_param_id] = ContextData(params)
+
+        if len(params.coeff_modulus) > 1:
+            self.first_param_id = self.create_next_context_data(self.key_param_id)
+        else:
+            self.first_param_id = self.key_param_id
+        self.last_param_id = self.first_param_id
+
+        prev_parms_id = self.first_param_id
+        while len(self.context_data_map[prev_parms_id].param.coeff_modulus) > 1:
+            prev_parms_id = self.create_next_context_data(prev_parms_id)
+            self.last_param_id = prev_parms_id
+
+    def create_next_context_data(self, prev_parms_id):
+        """Generate next context data in context chain by dropping the last coefficient modulus.
+
+        Args:
+            prev_params_id: Id of previous context data in context chain.
+        Returns:
+            Id of newly generated context data in context chain.
+        """
+        next_parms = copy.deepcopy(self.context_data_map[prev_parms_id].param)
+        next_coeff_modulus = next_parms.coeff_modulus[:-1]
+        next_parms.set_coeff_modulus(next_coeff_modulus)
+        next_param_id = next_parms.param_id
+
+        assert next_param_id != prev_parms_id
+
+        next_context_data = ContextData(next_parms)
+        self.context_data_map[next_param_id] = next_context_data
+        self.context_data_map[prev_parms_id].next_context_id = next_param_id
+        self.context_data_map[next_param_id].prev_context_id = prev_parms_id
+        return next_param_id
+
+
+class ContextData:
+    """A class used to hold parameters and perform parameters validation.
+
+    Args:
+        param: An EncryptionParams object with polynomial modulus, coefficient modulus and
+        plain modulus set.
+        prev_context_id: Id of the previous ContextData object in context chain.
+        next_context_id: Id of the next ContextData object in context chain.
 
     Attributes:
-        param: An EncryptionParams object.
+        prev_context_id: Id of the previous ContextData object in context chain.
+        next_context_id: Id of the next ContextData object in context chain.
         coeff_div_plain_modulus: A list of float values equal to (q[i]/t),
             In research papers denoted by delta.
         rns_tool: A RNSTool class instance.
     """
 
-    def __init__(self, params):
+    def __init__(self, param, prev_context_id=None, next_context_id=None):
+        self.validate(param)
+        self.param = param
+        self.prev_context_id = prev_context_id
+        self.next_context_id = next_context_id
 
-        # Validation of params provided for the encryption scheme.
-        self.validate(params)
+        # A list containing values of coeff_mod[i] / plain_mod for one time computation
+        # and useful in encryption process.
+        self.coeff_div_plain_modulus = [x / param.plain_modulus for x in param.coeff_modulus]
 
-        self.param = params
-
-        self.rns_tool = RNSTool(params)
+        self.rns_tool = RNSTool(param)
 
     def validate(self, params):
+        """Performs parameter validation on the provided encryption parameters.
 
+        Args:
+            params: An EncryptionParams object with polynomial modulus, coefficient modulus and
+            plain modulus set.
+        """
         # The number of coeff moduli is restricted to 62
         if (
             len(params.coeff_modulus) > COEFF_MOD_COUNT_MAX
@@ -93,7 +155,3 @@ class Context:
                 raise RuntimeError(
                     "Coefficient modulus are not relatively prime with plain modulus"
                 )
-
-        # A list containing values of coeff_mod[i] / plain_mod for one time computation
-        # and useful in encryption process.
-        self.coeff_div_plain_modulus = [x / params.plain_modulus for x in params.coeff_modulus]
