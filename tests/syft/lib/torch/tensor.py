0125ec6c770d5cc62229bc1b4c846462dd6fccf0
Andrew Trask <andrew@openmined.org>
2020-08-09 03:00:46 +0100

    ADd torch tensor tests

---
+++ tests/syft/lib/torch/tensor.py
@@ -0,0 +1,35 @@
+import syft as sy
+import torch as th
+
+
+def test_torch_vm_remote_operation():
+
+    alice = sy.VirtualMachine(name="alice")
+    alice_client = alice.get_client()
+
+    x = th.tensor([1, 2, 3, 4])
+
+    xp = x.send(alice_client)
+
+    y = xp + xp
+
+    assert len(alice.store._objects) == 2
+
+    y.get()
+
+    assert len(alice.store._objects) == 1
+
+    del xp
+
+    assert len(alice.store._objects) == 0
+
+
+def test_torch_serde():
+    
+    x = th.tensor([1, 2, 3, 4])
+
+    blob = x.serialize()
+
+    x2 = sy.deserialize(blob=blob)
+
+    assert (x == x2).all()
