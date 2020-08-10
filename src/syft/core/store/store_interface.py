ef2783a1df2dff540fde52a37a1e35d467041fb4
Tudor Cebere <tudorcebere@gmail.com>
2020-08-10 15:34:38 +0200

    Store cleanup (#3979)
    * store cleanup
    
    * removing proto
    
    * black
    
    * linting

--- src/syft/core/store/store_interface.py
+++ src/syft/core/store/store_interface.py
@@ -136,23 +136,3 @@ class ObjectStore(ABC):
         Clears all storage owned by the store.
         """
         raise NotImplementedError
-
-    @syft_decorator(typechecking=True)
-    def delete_object(self) -> None:
-        raise NotImplementedError
-
-    @syft_decorator(typechecking=True)
-    def store_object(self) -> None:
-        raise NotImplementedError
-
-    @syft_decorator(typechecking=True)
-    def get_object(self) -> None:
-        raise NotImplementedError
-
-    @syft_decorator(typechecking=True)
-    def has_object(self) -> None:
-        raise NotImplementedError
-
-    @syft_decorator(typechecking=True)
-    def get_objects_of_type(self) -> None:
-        raise NotImplementedError
