f9be4fb396aa7a8f79d9ae05c173dede35aaa619
Andrew Trask <andrew@openmined.org>
2020-08-10 15:34:46 +0100

    Add Serializable methods to SpecificLocation
    In order to deserialize, we need to overload the Serialiable methods
    in SpecificLocation to use the right protobuf. Interestingly, we
    probably could have not done this and modified ObjectWithID to
    deserialize correctly. However, instead of scope creep I chose to
    go ahead and do a custom class for SpecificLocation despite the
    fact that serialization actually already worked since SpecificLocation
    doesn't have any additional attributes beyond ObjectWithID. Serde
    actually worked, deserializatoin just returned an ObjectWithID
    object instead of SpecificLocation (the correct subclass).

--- src/syft/core/io/location/specific.py
+++ src/syft/core/io/location/specific.py
@@ -1,5 +1,12 @@
+# external class imports
+from google.protobuf.reflection import GeneratedProtocolMessageType
+
+# syft imports (sorted by length)
 from .location import Location
 from ...common.object import ObjectWithID
+from ...common.serde.deserialize import _deserialize
+from ....decorators.syft_decorator_impl import syft_decorator
+from ....proto.core.io.location_pb2 import SpecificLocation as SpecificLocation_PB
 
 
 class SpecificLocation(Location, ObjectWithID):
@@ -7,5 +14,44 @@ class SpecificLocation(Location, ObjectWithID):
     represented by a single UID. It may not have any functionality
     beyond Location but there is logic which interprets it differently."""
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, id=None):
+        ObjectWithID.__init__(self, id=id)
+
+    @syft_decorator(typechecking=True)
+    def _object2proto(self) -> SpecificLocation_PB:
+        """Returns a protobuf serialization of self.
+
+        As a requirement of all objects which inherit from Serializable,
+        this method transforms the current object into the corresponding
+        Protobuf object so that it can be further serialized.
+
+        :return: returns a protobuf object
+        :rtype: SpecificLocation_PB
+
+        .. note::
+            This method is purely an internal method. Please use object.serialize() or one of
+            the other public serialization methods if you wish to serialize an
+            object.
+        """
+        return SpecificLocation_PB(id=self.id.serialize())
+
+    @staticmethod
+    def _proto2object(proto: SpecificLocation_PB) -> "SpecificLocation":
+        """Creates a SpecificLocation from a protobuf
+
+        As a requirement of all objects which inherit from Serializable,
+        this method transforms a protobuf object into an instance of this class.
+
+        :return: returns an instance of SpecificLocation
+        :rtype: SpecificLocation
+
+        .. note::
+            This method is purely an internal method. Please use syft.deserialize()
+            if you wish to deserialize an object.
+        """
+
+        return SpecificLocation(id=_deserialize(blob=proto.id))
+
+    @staticmethod
+    def get_protobuf_schema() -> GeneratedProtocolMessageType:
+        return SpecificLocation_PB
