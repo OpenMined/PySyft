7b136731d21a2d9950d81ed96b9fb2f0614bb1fd
Vova Manannikov <vova@promail.spb.ru>
2020-08-11 21:35:43 +0300

    Torch tensor serialization to proto

--- src/syft/lib/torch/uppercase_tensor.py
+++ src/syft/lib/torch/uppercase_tensor.py
@@ -1,13 +1,34 @@
 from typing import Optional, Type
 
 from ..generic import ObjectConstructor
-from syft.proto.lib.numpy.tensor_pb2 import TensorProto
+from syft.proto.lib.torch.tensor_pb2 import TensorProto
 from syft.core.store.storeable_object import StorableObject
 from syft.lib.numpy.tensor_util import numpy_array_to_tensor
 from syft.lib.numpy.tensor_util import tensor_to_numpy_array
 from ...util import aggressive_set_attr
 
 import torch as th
+
+# Torch dtypes to string (and back) mappers
+TORCH_DTYPE_STR = {
+    th.uint8: "uint8",
+    th.int8: "int8",
+    th.int16: "int16",
+    th.int32: "int32",
+    th.int64: "int64",
+    th.float16: "float16",
+    th.float32: "float32",
+    th.float64: "float64",
+    th.complex32: "complex32",
+    th.complex64: "complex64",
+    th.complex128: "complex128",
+    th.bool: "bool",
+    th.qint8: "qint8",
+    th.quint8: "quint8",
+    th.qint32: "qint32",
+    th.bfloat16: "bfloat16",
+}
+TORCH_STR_DTYPE = {name: cls for cls, name in TORCH_DTYPE_STR.items()}
 
 
 class UppercaseTensorConstructor(ObjectConstructor):
@@ -43,15 +64,40 @@ class TorchTensorWrapper(StorableObject):
         self.value = value
 
     def _data_object2proto(self) -> TensorProto:
-        return numpy_array_to_tensor(self.value.numpy())
+        tensor = self.value
+        proto = TensorProto()
+
+        dtype = TORCH_DTYPE_STR[tensor.dtype]
+        if tensor.is_quantized:
+            proto.is_quantized = True
+            proto.scale = tensor.q_scale()
+            proto.zero_point = tensor.q_zero_point()
+            data = tensor.flatten().int_repr().tolist()
+        else:
+            data = tensor.flatten().tolist()
+
+        proto.dtype = dtype
+        proto.shape.extend(tensor.size())
+        getattr(proto, "contents_" + dtype).extend(data)
+
+        return proto
 
     @staticmethod
     def _data_proto2object(proto: TensorProto) -> int:
+        size = tuple(proto.shape)
+        data = getattr(proto, "contents_" + proto.dtype)
 
-        # proto -> original numpy type
-        data = tensor_to_numpy_array(proto)
-
-        return th.tensor(data)
+        if proto.is_quantized:
+            # Drop the 'q' from the beginning of the quantized dtype to get the int type
+            dtype = TORCH_STR_DTYPE[proto.dtype[1:]]
+            int_tensor = th.tensor(data, dtype=dtype).reshape(size)
+            # Automatically converts int types to quantized types
+            return th._make_per_tensor_quantized_tensor(
+                int_tensor, proto.scale, proto.zero_point
+            )
+        else:
+            dtype = TORCH_STR_DTYPE[proto.dtype]
+            return th.tensor(data, dtype=dtype).reshape(size)
 
     @staticmethod
     def get_data_protobuf_schema() -> Optional[Type]:
