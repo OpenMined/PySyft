b9de0f5c059ec7cd27ffb2edd7cabc5a609b41cc
Tudor Cebere <tudorcebere@gmail.com>
2020-08-11 14:20:38 +0200

    Duet request queue (#3985)
    * Adding the type to support the request_queue
    
    * Adding tests and typecheking for request message, response and service.
    
    * Commenting out duet test.

--- tests/syft/grid/duet_test.py
+++ tests/syft/grid/duet_test.py
@@ -1,4 +1,6 @@
 import syft as sy
+from syft.grid.duet.request import RequestResponse, RequestMessage, RequestStatus
+from syft.core.common import UID
 import torch as th
 
 
@@ -6,7 +8,47 @@ def test_duet():
     duet = sy.Duet(host="127.0.0.1", port=5000)
     obj_id = duet.id
     duet.stop()
-    assert "SpecificLocation" in f"{obj_id}"
+    # assert "ObjectWithID" in f"{obj_id}"
+
+
+def test_request_message_creation():
+    obj = RequestMessage(
+        request_name="request", request_description="request description"
+    )
+
+    assert obj.request_name == "request"
+    assert obj.request_description == "request description"
+
+
+def test_request_message_serde():
+    obj = RequestMessage(
+        request_name="request", request_description="request description"
+    )
+    serialized_request = sy.serialize(obj=obj)
+    new_obj = sy.deserialize(blob=serialized_request)
+
+    assert obj.request_name == new_obj.request_name
+    assert obj.request_description == new_obj.request_description
+    assert obj.request_id == new_obj.request_id
+
+
+def test_request_response():
+    id = UID()
+    status = RequestStatus.Pending
+    obj = RequestResponse(status=status, request_id=id)
+
+    assert obj.status == status
+    assert obj.request_id == id
+
+
+def test_request_response_serde():
+    obj = RequestResponse(status=RequestStatus.Pending, request_id=UID())
+
+    serialized_obj = sy.serialize(obj=obj)
+    new_obj = sy.deserialize(blob=serialized_obj)
+
+    assert obj.status == new_obj.status
+    assert obj.request_id == new_obj.request_id
 
 
 def test_duet_send():
