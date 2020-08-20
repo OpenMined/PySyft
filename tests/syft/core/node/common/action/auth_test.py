435a738d6d0d873f6c313f74d1ee375db8eee69e
Jasopaum <jason.paumier@gmail.com>
2020-08-20 16:32:41 +0200

    Add tests for auth decorator (#4052)
    * use raise instead of assert
    
    * test root only
    
    * test existing_users_only, guests_welcome, register_new_guests

---
+++ tests/syft/core/node/common/action/auth_test.py
@@ -0,0 +1,58 @@
+import pytest
+from nacl.signing import SigningKey
+from nacl.signing import VerifyKey
+
+import syft as sy
+
+from syft.core.node.common.action.auth import service_auth
+
+
+def test_service_auth():
+    node = sy.Device()
+    msg = sy.ReprMessage(address=node.address)
+
+    random_signing_key = SigningKey.generate()
+    random_verify_key = random_signing_key.verify_key
+
+    new_signing_key = SigningKey.generate()
+    new_verify_key = new_signing_key.verify_key
+
+    # root_only
+    @service_auth(root_only=True)
+    def process(node: sy.Device, msg: sy.ReprMessage, verify_key: VerifyKey) -> None:
+        pass
+
+    process(node=node, msg=msg, verify_key=node.root_verify_key)
+
+    with pytest.raises(Exception, match="User is not root."):
+        process(node=node, msg=msg, verify_key=random_verify_key)
+
+    # existing_users_only
+    @service_auth(existing_users_only=True)
+    def process(node: sy.Device, msg: sy.ReprMessage, verify_key: VerifyKey) -> None:
+        pass
+
+    with pytest.raises(Exception, match="User not known."):
+        process(node=node, msg=msg, verify_key=random_verify_key)
+
+    # NOTE didn't find a method to add a key to guest_verify_key_registry
+    node.guest_verify_key_registry.add(random_verify_key)
+    process(node=node, msg=msg, verify_key=random_verify_key)
+
+    # guests_welcome
+    @service_auth(guests_welcome=True)
+    def process(node: sy.Device, msg: sy.ReprMessage, verify_key: VerifyKey) -> None:
+        pass
+
+    process(node=node, msg=msg, verify_key=new_verify_key)
+
+    assert new_verify_key not in node.guest_verify_key_registry
+
+    # register_new_guests
+    @service_auth(guests_welcome=True, register_new_guests=True)
+    def process(node: sy.Device, msg: sy.ReprMessage, verify_key: VerifyKey) -> None:
+        pass
+
+    process(node=node, msg=msg, verify_key=new_verify_key)
+
+    assert new_verify_key in node.guest_verify_key_registry
