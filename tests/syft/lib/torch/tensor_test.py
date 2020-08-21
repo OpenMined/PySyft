9bdfbf1a428674e7e4870e8b40d1aa864a25477d
George Muraru <murarugeorgec@gmail.com>
2020-08-21 04:48:41 +0300

    Remove quant

--- tests/syft/lib/torch/tensor_test.py
+++ tests/syft/lib/torch/tensor_test.py
@@ -11,9 +11,16 @@ import torch as th
 import pytest
 from itertools import product
 
-TYPES_TEST = [e for e in TORCH_STR_DTYPE.keys() if not e.startswith("complex")]
+# Currently, we do not have constructors with torch.Tensor
+# for dtype in ["complex*", "q*"] (complex and quantized types)
+TYPES_EXCEPTIONS_PREFIX = ("complex", "q")
+
+TEST_TYPES = [
+    e for e in TORCH_STR_DTYPE.keys() if not e.startswith(TYPES_EXCEPTIONS_PREFIX)
+]
 BASIC_OPS = ["__add__", "add", "__sub__", "sub", "__mul__", "mul"]
-TEST_DATA = list(product(TYPES_TEST, BASIC_OPS))
+
+TEST_DATA = list(product(TEST_TYPES, BASIC_OPS))
 
 
 def test_torch_remote_tensor_register() -> None:
@@ -34,14 +41,14 @@ def test_torch_remote_tensor_register() -> None:
     assert len(alice.store) == 0  # Get removes the object
 
 
-@pytest.mark.parametrize("tensor_type_str, op_name", TEST_DATA)
-def test_torch_remote_remote_basic_ops(tensor_type_str, op_name):
+@pytest.mark.parametrize("tensor_type, op_name", TEST_DATA)
+def test_torch_remote_remote_basic_ops(tensor_type: str, op_name: str) -> None:
     """ Test basic operations on remote simple tensors """
 
     alice = sy.VirtualMachine(name="alice")
     alice_client = alice.get_client()
 
-    t_type = TORCH_STR_DTYPE[tensor_type_str]
+    t_type = TORCH_STR_DTYPE[tensor_type]
     x = th.tensor([-1, 0, 1, 2, 3, 4], dtype=t_type)
 
     xp = x.send(alice_client)
@@ -58,7 +65,7 @@ def test_torch_remote_remote_basic_ops(tensor_type_str, op_name):
     #  uncoment this.
     # del xp
     #
-    # assert len(alice.store._objects) == 0
+    # assert len(alice.store.) == 0
 
 
 def test_torch_serde() -> None:
@@ -77,25 +84,25 @@ def test_torch_serde() -> None:
     assert (x == x2).all()
 
 
-def test_torch_permissions() -> None:
+def test_torch_no_read_permissions() -> None:
 
     bob = sy.VirtualMachine(name="bob")
     root_bob = bob.get_root_client()
     guest_bob = bob.get_client()
-
-    import torch as th
 
     x = th.tensor([1, 2, 3, 4])
 
     # root user of Bob's machine sends a tensor
     ptr = x.send(root_bob)
 
-    # guest bob creates a pointer to that object (assuming he could guess/inpher the ID)
+    # guest creates a pointer to that object (assuming the client can guess/inpher the ID)
     ptr.client = guest_bob
 
     # this should trigger an exception
-    with pytest.raises(Exception):
-        ptr.get()
+    with pytest.raises(Exception) as exception:
+        local_x = ptr.get()
+
+    assert str(exception.value) == "You do not have permission to .get() this tensor. Please submit a request."
 
     x = th.tensor([1, 2, 3, 4])
 
