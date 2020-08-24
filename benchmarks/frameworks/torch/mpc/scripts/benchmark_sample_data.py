1868f268cefa53d21863e40f48bfb4415d1fc79a
arturomf94 <arturomf94@gmail.com>
2020-08-24 05:55:29 -0500

    Add benchmark graph and script for tanh function approximation methods (#4465)
    * Add benchmark graph for tanh function
    
    * Add sample data for tanh benchmarking
    
    * Add benchmark script for tanh function
    
    * Modify data format in sample data script
    
    * Use correct docstrings in tanh benchmark

--- benchmarks/frameworks/torch/mpc/scripts/benchmark_sample_data.py
+++ benchmarks/frameworks/torch/mpc/scripts/benchmark_sample_data.py
@@ -1,4 +1,9 @@
-# this is sample data for benchmark of sigmoid function
+"""
+Sample data for the benchmarking of FPT/AST with differnt types of approximations
+See: https://github.com/OpenMined/PySyft/issues/3997
 
-# data format ('method_name', precision value)
+data format: list[('method_name', precision value)]
+"""
+
 benchmark_data_sigmoid = [("chebyshev", 4), ("maclaurin", 4), ("exp", 4)]
+benchmark_data_tanh = [("chebyshev", 4), ("sigmoid", 4)]
