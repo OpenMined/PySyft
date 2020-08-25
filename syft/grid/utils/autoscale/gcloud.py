6c5647873127e3069c9366feb22861719a3f6cc5
rimijoker <anz19999999999@gmail.com>
2020-08-25 20:01:22 +0530

    Added HOST env variable

--- syft/grid/utils/autoscale/gcloud.py
+++ syft/grid/utils/autoscale/gcloud.py
@@ -166,6 +166,7 @@ class GoogleCloud:
                 docker pull openmined/grid-node:production;
                 docker run \
                 -e NODE_ID={name} \
+                -e HOST = "$(hostname -I)" \
                 -e PORT=80 \
                 -e NETWORK={pygrid_network_address} \
                 -e DATABASE_URL=sqlite:///databasenode.db \
@@ -242,6 +243,7 @@ class GoogleCloud:
                 docker pull openmined/grid-node:production;
                 docker run \
                 -e NODE_ID={name} \
+                -e HOST="$(curl ifcongif.co)" \
                 -e PORT=80 \
                 -e NETWORK={pygrid_network_address} \
                 -e DATABASE_URL=sqlite:///databasenode.db \
