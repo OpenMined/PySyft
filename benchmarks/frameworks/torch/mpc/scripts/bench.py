68be926bc652dcfcfc85c35f9e1408911b4fa893
Arturo Marquez <arturomf94@gmail.com>
2020-08-30 23:58:33 -0500

    Add module for benchmarking with pytest-benchmark

--- benchmarks/frameworks/torch/mpc/scripts/bench.py
+++ benchmarks/frameworks/torch/mpc/scripts/bench.py
@@ -1,7 +1,17 @@
-from benchmark_sigmoid import benchmark_sigmoid
+"""
+Define benchmark tests
+"""
+
 from workers_initialization import workers, hook
+from benchmark_functions import sigmoid
+
+# Initialize workers globally for the tests
+worker = workers(hook())
 
 
 def test_sigmoid_chebyshev_1(benchmark):
-    worker = workers(hook())
-    benchmark(benchmark_sigmoid, "chebyshev", 1, worker)
+    """
+    Test sigmoid aproximation with chebyshev method an
+    precision value of 1
+    """
+    benchmark(sigmoid, "chebyshev", 1, worker)
