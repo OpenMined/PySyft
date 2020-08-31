12e68cd6f3cb566b0e3dbe7b40bf9433512bb7f6
Andrew Trask <andrew@openmined.org>
2020-08-31 22:30:58 +0100

    Fix port bug in Duet unit test

--- tests/syft/grid/duet_test.py
+++ tests/syft/grid/duet_test.py
@@ -24,11 +24,14 @@ def test_duet_searchable_functionality() -> None:
     xp = th.tensor([1,2,3]).tag("some", "diabetes", "data").send(duet)
     xp2 = th.tensor([1, 2, 3]).tag("some", "diabetes", "data").send(duet, searchable=True)
 
-    guest = sy.Duet(domain_url="http://127.0.0.1:5000/")
+    guest = sy.Duet(domain_url="http://127.0.0.1:5001/")
 
     assert len(guest.store) == 1
     assert len(duet.store) == 2
 
     assert guest.store[0].id_at_location == xp2.id_at_location
+
+    del xp
+    del xp2
 
     duet.stop()
