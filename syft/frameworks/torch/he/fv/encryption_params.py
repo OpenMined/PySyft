4b2e72175f14bf01017f66e7d62e8871e9a59ad9
Ravikant Singh <ravikantsingh2308@gmail.com>
2020-08-31 22:51:25 +0530

    Add black2b hash to generate param_id for bfv scheme

--- syft/frameworks/torch/he/fv/encryption_params.py
+++ syft/frameworks/torch/he/fv/encryption_params.py
@@ -1,3 +1,6 @@
+from hashlib import blake2b
+
+
 class EncryptionParams:
     """A class to hold the encryption parameters for easy access by any component of scheme.
 
@@ -39,6 +42,7 @@ class EncryptionParams:
         self.param_id = self.compute_parms_id()
 
     def compute_parms_id(self):
-        # TODO: use hash function here.
         param_data = [self.poly_modulus, *self.coeff_modulus, self.plain_modulus]
-        return " ".join(str(x) for x in param_data)
+        param_str = " ".join(str(x) for x in param_data).encode()
+        hash_id = blake2b(param_str).hexdigest()
+        return hash_id
