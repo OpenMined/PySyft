ea450a6f1225f99b67c7c81b8e24f592a8ad363b
Andrew Trask <andrew@openmined.org>
2020-09-01 16:53:55 +0100

    Test request_access -> request

--- tests/syft/core/node/domain/domain_test.py
+++ tests/syft/core/node/domain/domain_test.py
@@ -19,7 +19,7 @@ def test_domain_serde() -> None:
     _ = tensor.send(domain_1_client)
 
 
-def test_domain_request_access_pending() -> None:
+def test_domain_request_pending() -> None:
     domain_1 = Domain(name="remote domain")
     tensor = th.tensor([1, 2, 3])
 
@@ -28,7 +28,7 @@ def test_domain_request_access_pending() -> None:
 
     domain_2 = Domain(name="my domain")
 
-    data_ptr_domain_1.request_access(
+    data_ptr_domain_1.request(
         request_name="My Request", reason="I'd lke to see this pointer"
     )
 
@@ -47,7 +47,7 @@ def test_domain_request_access_pending() -> None:
     assert RequestStatus.Pending == response
 
 
-def test_domain_request_access_denied() -> None:
+def test_domain_request_denied() -> None:
     domain_1 = Domain(name="remote domain")
     tensor = th.tensor([1, 2, 3])
 
@@ -56,7 +56,7 @@ def test_domain_request_access_denied() -> None:
 
     domain_2 = Domain(name="my domain")
 
-    data_ptr_domain_1.request_access(
+    data_ptr_domain_1.request(
         request_name="My Request", reason="I'd lke to see this pointer"
     )
 
@@ -79,7 +79,7 @@ def test_domain_request_access_denied() -> None:
     assert RequestStatus.Rejected == response
 
 
-def test_domain_request_access_accepted() -> None:
+def test_domain_request_accepted() -> None:
     domain_1 = Domain(name="remote domain")
     tensor = th.tensor([1, 2, 3])
 
@@ -88,7 +88,7 @@ def test_domain_request_access_accepted() -> None:
 
     domain_2 = Domain(name="my domain")
 
-    data_ptr_domain_1.request_access(
+    data_ptr_domain_1.request(
         request_name="My Request", reason="I'd lke to see this pointer"
     )
 
