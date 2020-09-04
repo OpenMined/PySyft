a1455c50da97558ed64615d7dc35a54538ab86e2
Ionesio Junior <ionesiojr@gmail.com>
2020-09-03 21:27:26 -0300

    Fix signaling service tests

--- tests/syft/grid/services/signaling_service_test.py
+++ tests/syft/grid/services/signaling_service_test.py
@@ -2,33 +2,79 @@
 import syft as sy
 from syft.grid.services.signaling_service import SignalingAnswerMessage
 from syft.grid.services.signaling_service import SignalingOfferMessage
+from syft.grid.services.signaling_service import OfferPullRequestMessage
+from syft.grid.services.signaling_service import AnswerPullRequestMessage
+
+from syft.core.io.address import Address
 
 
 def test_signaling_offer_message_serde() -> None:
     bob_vm = sy.VirtualMachine(name="Bob")
+    target = Address(name="Alice")
 
-    msg = SignalingOfferMessage(address=bob_vm.address, payload="Test Payload")
+    msg = SignalingOfferMessage(
+        address=target,
+        payload="Test Payload",
+        target_metadata=bob_vm.get_metadata_for_client(),
+        reply_to=bob_vm.address,
+    )
 
     blob = msg.serialize()
     msg2 = sy.deserialize(blob=blob)
 
     assert msg.id == msg2.id
-    assert msg.address == msg2.address
+    assert msg.address == target
     assert msg.payload == msg2.payload
     assert msg2.payload == "Test Payload"
+    assert msg2.target_metadata == bob_vm.get_metadata_for_client()
     assert msg == msg2
 
 
 def test_signaling_answer_message_serde() -> None:
     bob_vm = sy.VirtualMachine(name="Bob")
+    target = Address(name="Alice")
 
-    msg = SignalingAnswerMessage(address=bob_vm.address, payload="Test Payload")
+    msg = SignalingAnswerMessage(
+        address=target,
+        payload="Test Payload",
+        target_metadata=bob_vm.get_metadata_for_client(),
+        reply_to=bob_vm.address,
+    )
 
     blob = msg.serialize()
     msg2 = sy.deserialize(blob=blob)
 
     assert msg.id == msg2.id
-    assert msg.address == msg2.address
+    assert msg.address == target
     assert msg.payload == msg2.payload
     assert msg2.payload == "Test Payload"
+    assert msg2.target_metadata == bob_vm.get_metadata_for_client()
+    assert msg == msg2
+
+
+def test_signaling_answer_pull_request_message_serde() -> None:
+    bob_vm = sy.VirtualMachine(name="Bob")
+    target = Address(name="Alice")
+
+    msg = AnswerPullRequestMessage(address=target, reply_to=bob_vm.address)
+
+    blob = msg.serialize()
+    msg2 = sy.deserialize(blob=blob)
+
+    assert msg.id == msg2.id
+    assert msg.address == target
+    assert msg == msg2
+
+
+def test_signaling_offer_pull_request_message_serde() -> None:
+    bob_vm = sy.VirtualMachine(name="Bob")
+    target = Address(name="Alice")
+
+    msg = OfferPullRequestMessage(address=target, reply_to=bob_vm.address)
+
+    blob = msg.serialize()
+    msg2 = sy.deserialize(blob=blob)
+
+    assert msg.id == msg2.id
+    assert msg.address == target
     assert msg == msg2
