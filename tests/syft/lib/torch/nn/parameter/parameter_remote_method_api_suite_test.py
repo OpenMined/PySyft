f3632adc56f817909409e040680bce1036e46f0f
Jasopaum <jason.paumier@gmail.com>
2020-09-07 15:25:28 +0200

    black

--- tests/syft/lib/torch/nn/parameter/parameter_remote_method_api_suite_test.py
+++ tests/syft/lib/torch/nn/parameter/parameter_remote_method_api_suite_test.py
@@ -36,7 +36,9 @@ tensor_type = type(th.tensor([1, 2, 3]))
 # for dtype in ["complex*", "q*"] (complex and quantized types)
 TYPES_EXCEPTIONS_PREFIX = ("complex", "q")
 
-TEST_TYPES = [e for e in TORCH_STR_DTYPE.keys() if not e.startswith(TYPES_EXCEPTIONS_PREFIX)]
+TEST_TYPES = [
+    e for e in TORCH_STR_DTYPE.keys() if not e.startswith(TYPES_EXCEPTIONS_PREFIX)
+]
 
 # only float types
 TEST_TYPES = [ttype for ttype in TEST_TYPES if "float" in ttype]
@@ -245,7 +247,9 @@ def test_all_allowlisted_parameter_methods_work_remotely_on_all_types(
         # NOTE: send the copy we haven't mutated
         xp = self_tensor_copy.send(alice_client)
         if args is not None:
-            argsp = [arg.send(alice_client) if hasattr(arg, "send") else arg for arg in args]
+            argsp = [
+                arg.send(alice_client) if hasattr(arg, "send") else arg for arg in args
+            ]
         else:
             argsp = None  # type:ignore
 
@@ -301,7 +305,10 @@ def test_all_allowlisted_parameter_methods_work_remotely_on_all_types(
             assert type(local_result) == type(target_result)
 
             # make sure the return type matches the specified allowlist return type
-            assert full_name_with_qualname(type(local_result)) == BASIC_OPS_RETURN_TYPE[op_name]
+            assert (
+                full_name_with_qualname(type(local_result))
+                == BASIC_OPS_RETURN_TYPE[op_name]
+            )
 
         except RuntimeError as e:
             msg = repr(e)
