bb79b4de93aaa414cf89e9180ca6fce603f40def
Jasopaum <jason.paumier@gmail.com>
2020-09-09 02:58:38 +0200

    Enable serialization of Syft Primitives (#4542)
    * augment python ast programmatically
    
    * add serialization to Syft Bool
    
    * add serialization to Syft Float
    
    * add serialization to Syft List
    
    * lint
    
    * add serde tests for Int
    
    * add send test for List
    
    * add serde tests for Bool
    
    * add serde tests for Float
    
    * add __init__.py files in test folders

--- src/syft/lib/python/__init__.py
+++ src/syft/lib/python/__init__.py
@@ -1,82 +1,74 @@
 # stdlib
-from typing import Dict
-from typing import Union
+from typing import Any
+from typing import List as TypeList
+from typing import Tuple
 
 # syft relative
-from ... import ast as astlib
 from ...ast.globals import Globals
+from ...ast.klass import Class
+from ...ast.module import Module
+from .bool import Bool
+from .float import Float
 from .int import Int
+from .list import List
 
 
-def get_return_type(support_dict: Union[str, Dict[str, str]]) -> str:
-    if isinstance(support_dict, str):
-        return support_dict
-    else:
-        return support_dict["return_type"]
+def get_parent(path: str, root: Any) -> Module:
+    parent = root
+    for step in path.split(".")[:-1]:
+        parent = parent.attrs[step]
+    return parent
 
 
-def version_supported(support_dict: Union[str, Dict[str, str]]) -> bool:
-    return True
+def add_modules(ast: Globals, modules: TypeList[str]) -> None:
+    for module in modules:
+        parent = get_parent(module, ast)
+        attr_name = module.rsplit(".", 1)[-1]
+
+        parent.add_attr(
+            attr_name=attr_name,
+            attr=Module(
+                attr_name,
+                module,
+                None,
+                return_type_name="",
+            ),
+        )
+
+
+def add_primitives(ast: Globals, primitives: TypeList[Tuple[str, Any]]) -> None:
+    for primitive, ref in primitives:
+        parent = get_parent(primitive, ast)
+        attr_name = primitive.rsplit(".", 1)[-1]
+
+        parent.add_attr(
+            attr_name=attr_name,
+            attr=Class(
+                attr_name,
+                primitive,
+                ref,
+                return_type_name=primitive,
+            ),
+        )
 
 
 def create_python_ast() -> Globals:
     ast = Globals()
 
-    # build fake syft.lib.python module
-    syft = lambda: None  # noqa: E731
-    lib = lambda: None  # noqa: E731
-    python = lambda: None  # noqa: E731
-    setattr(lib, "python", python)
-    setattr(python, "Int", Int)
-    setattr(syft, "lib", lib)
+    modules = [
+        "syft",
+        "syft.lib",
+        "syft.lib.python",
+    ]
+    primitives = [
+        ("syft.lib.python.Bool", Bool),
+        ("syft.lib.python.Int", Int),
+        ("syft.lib.python.Float", Float),
+        ("syft.lib.python.List", List),
+    ]
 
-    ast.attrs["syft"] = astlib.module.Module(
-        name="syft",
-        path_and_name="syft",
-        ref=syft,
-        return_type_name="",
-    )
-
-    syft_node = ast.attrs["syft"]
-    add_attr = getattr(syft_node, "add_attr", None)
-    if add_attr is not None:
-        add_attr(
-            attr_name="lib",
-            attr=astlib.module.Module(
-                "lib",
-                "syft.lib",
-                syft,
-                return_type_name="",
-            ),
-        )
-
-    syft_node_attrs = getattr(syft_node, "attrs", None)
-    if syft_node_attrs is not None:
-        lib_node = syft_node_attrs["lib"]
-        add_attr = getattr(lib_node, "add_attr", None)
-        if add_attr is not None:
-            add_attr(
-                attr_name="python",
-                attr=astlib.module.Module(
-                    "python",
-                    "syft.lib.python",
-                    lib,
-                    return_type_name="",
-                ),
-            )
-
-    python_node = lib_node.attrs["python"]
-    add_attr = getattr(python_node, "add_attr", None)
-    if add_attr is not None:
-        add_attr(
-            attr_name="Int",
-            attr=astlib.klass.Class(
-                "Int",
-                "syft.lib.python.Int",
-                Int,
-                return_type_name="syft.lib.python.Int",
-            ),
-        )
+    add_modules(ast, modules)
+    add_primitives(ast, primitives)
 
     for klass in ast.classes:
         klass.create_pointer_class()
--- src/syft/lib/python/list.py
+++ src/syft/lib/python/list.py
@@ -1,6 +1,7 @@
 # stdlib
 from collections import UserList
 from typing import Any
+from typing import List as TypeList
 from typing import Optional
 
 # third party
@@ -10,8 +11,10 @@ from google.protobuf.reflection import GeneratedProtocolMessageType
 from ... import deserialize
 from ... import serialize
 from ...core.common import UID
+from ...core.store.storeable_object import StorableObject
 from ...decorators import syft_decorator
 from ...proto.lib.python.list_pb2 import List as List_PB
+from ...util import aggressive_set_attr
 from .primitive_interface import PyPrimitive
 
 
@@ -52,3 +55,46 @@ class List(UserList, PyPrimitive):
     @staticmethod
     def get_protobuf_schema() -> GeneratedProtocolMessageType:
         return List_PB
+
+
+class ListWrapper(StorableObject):
+    def __init__(self, value: object):
+        super().__init__(
+            data=value,
+            id=getattr(value, "id", UID()),
+            tags=getattr(value, "tags", []),
+            description=getattr(value, "description", ""),
+        )
+        self.value = value
+
+    def _data_object2proto(self) -> List_PB:
+        _object2proto = getattr(self.data, "_object2proto", None)
+        if _object2proto is not None:
+            return _object2proto()
+
+    @staticmethod
+    def _data_proto2object(proto: List_PB) -> "ListWrapper":
+        return List._proto2object(proto=proto)
+
+    @staticmethod
+    def get_data_protobuf_schema() -> GeneratedProtocolMessageType:
+        return List_PB
+
+    @staticmethod
+    def get_wrapped_type() -> type:
+        return List
+
+    @staticmethod
+    def construct_new_object(
+        id: UID,
+        data: StorableObject,
+        description: Optional[str],
+        tags: Optional[TypeList[str]],
+    ) -> StorableObject:
+        setattr(data, "_id", id)
+        data.tags = tags
+        data.description = description
+        return data
+
+
+aggressive_set_attr(obj=List, name="serializable_wrapper_type", attr=ListWrapper)
