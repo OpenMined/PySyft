8a674f2877c61cd572acf939391f6ac4c56cdb1c
Tudor Cebere <tudorcebere@gmail.com>
2020-09-09 02:34:12 -0700

    Adding more testing on Int (#4526)
    * operator coverage on syft.Int
    
    * removing redefinition
    
    * primitive factory

--- src/syft/lib/python/primitive_factory.py
+++ src/syft/lib/python/primitive_factory.py
@@ -1,17 +1,21 @@
 # stdlib
+from abc import ABC
+from typing import Optional
 from typing import Union
 
 # syft relative
+from ...core.common import UID
 from ...decorators import syft_decorator
 from .primitive_interface import PyPrimitive
 
 
-class PrimitiveFactory:
+class PrimitiveFactory(ABC):
     @staticmethod
     @syft_decorator(typechecking=True)
     def generate_primitive(
-        value: Union[int, float, bool, complex, None]
-    ) -> PyPrimitive:
+        value: Union[int, float, bool, complex, None, type(NotImplemented)],  # type: ignore
+        id: Optional[UID] = None,
+    ) -> Union[PyPrimitive, type(NotImplemented)]:  # type: ignore
         # syft relative
         from .bool import Bool
         from .complex import Complex
@@ -19,16 +23,20 @@ class PrimitiveFactory:
         from .int import Int
         from .none import SyNone
 
-        if isinstance(value, int):
-            return Int(value=value)
+        if type(value) is int:
+            return Int(value=value, id=id)
 
-        if isinstance(value, float):
-            return Float(value=value)
+        if type(value) is float:
+            return Float(value=value, id=id)
 
-        if isinstance(value, bool):
-            return Bool(value=value)
+        if type(value) is bool:
+            return Bool(value=value, id=id)
 
-        if isinstance(value, complex):
-            return Complex(value=value)
+        if type(value) is complex:
+            return Complex(value=value, id=id)
 
-        return SyNone()
+        if value is NotImplemented:
+            return value
+
+        none: SyNone = SyNone()
+        return none
--- src/syft/lib/python/primitive_interface.py
+++ src/syft/lib/python/primitive_interface.py
@@ -1,2 +1,7 @@
+# syft relative
+from ...core.common import UID
+
+
 class PyPrimitive:
-    pass
+    def __init__(self) -> None:
+        self._id: UID
