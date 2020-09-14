a595092d5d1621bb4c9897c517f3faf2b7c2b4a0
Ionesio Junior <ionesiojr@gmail.com>
2020-09-14 03:01:12 -0300

    Finish signaling service

--- src/syft/core/node/network/network.py
+++ src/syft/core/node/network/network.py
@@ -50,6 +50,8 @@ class Network(Node):
 
         # specific location with name
         self.network = SpecificLocation(name=self.name)
+        
+        self.signaling_msgs = dict()  # type: ignore
 
         self._register_services()
         self.post_init()
--- src/syft/grid/network.py
+++ src/syft/grid/network.py
@@ -2,25 +2,47 @@
 functionality on an actual local network. This is NOT meant to be run in
 production (that's the *actual* grid's job)."""
 
-# stdlib
-import pickle
 
 # third party
 from flask import Flask
+from flask import request
 
-# syft relative
-from ..core.node.network.network import Network
+# syft absolute
+from syft.core.common.message import SignedImmediateSyftMessageWithReply
+from syft.core.common.message import SignedImmediateSyftMessageWithoutReply
+from syft.core.common.serde.deserialize import _deserialize
+from syft.core.node.network.network import Network
+from syft.grid.services.signaling_service import PullSignalingService
+from syft.grid.services.signaling_service import PushSignalingService
+
 
 app = Flask(__name__)
 
-network = Network(name="ucsf-net")
+network = Network(name="om-net")
+
+network.immediate_services_without_reply.append(PushSignalingService)
+network.immediate_services_with_reply.append(PullSignalingService)
+network._register_services()  # re-register all services including SignalingService
 
 
-@app.route("/")
-def get_client() -> str:
-    client = network.get_client()
-    return pickle.dumps(client).hex()
+@app.route("/metadata")
+def get_metadata() -> str:
+    return network.get_metadata_for_client()
 
+
+@app.route("/", methods=["POST"])
+def process_network_msgs():
+    json_msg = request.get_json()
+    obj_msg = _deserialize(blob=json_msg, from_json=True)
+    if isinstance(obj_msg, SignedImmediateSyftMessageWithReply):
+        reply = network.recv_immediate_msg_with_reply(msg=obj_msg)
+        return reply.json()
+    elif isinstance(obj_msg, SignedImmediateSyftMessageWithoutReply):
+        network.recv_immediate_msg_without_reply(msg=obj_msg)
+    else:
+        network.recv_eventual_msg_without_reply(msg=obj_msg)
+    return ""
 
 def run() -> None:
     app.run()
+
