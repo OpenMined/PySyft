717e3f825f89b2883cab273af00c3271aa38bb3d
Arturo Marquez <arturomf94@gmail.com>
2020-09-16 14:41:10 -0500

    Add test for tanh approximation

--- benchmarks/frameworks/torch/mpc/pytest-benchmark/bench.py
+++ benchmarks/frameworks/torch/mpc/pytest-benchmark/bench.py
@@ -3,15 +3,23 @@ Define benchmark tests
 """
 
 from workers_initialization import workers, hook
-from pytest_benchmark_functions import sigmoid
+from benchmark_functions import sigmoid, tanh
 
 # Initialize workers globally for the tests
 worker = workers(hook())
 
 
-def test_sigmoid_chebyshev_1(benchmark):
+def test_sigmoid_chebyshev(benchmark):
     """
-    Test sigmoid aproximation with chebyshev method an
+    Test sigmoid aproximation with chebyshev method and
     precision value of 1
     """
     benchmark(sigmoid, "chebyshev", 1, worker)
+
+
+def test_tanh_chebyshev(benchmark):
+    """
+    Test tanh aproximation with chebyshev method and
+    precision value of 1
+    """
+    benchmark(tanh, "chebyshev", 1, worker)
