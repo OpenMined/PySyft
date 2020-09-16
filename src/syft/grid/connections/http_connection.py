1abf04b71891a2f31a5d55ca4685c85b66767207
Ionesio Junior <ionesiojr@gmail.com>
2020-09-16 09:41:25 -0300

    Fix HTTPConnection - ADD typechecks - ADD docstrings / comments - RENAME send_msg (public) to __send_msg (private)

--- src/syft/grid/connections/http_connection.py
+++ src/syft/grid/connections/http_connection.py
@@ -1,5 +1,3 @@
-# stdlib
-
 # third party
 import requests
 
@@ -8,36 +6,101 @@ import syft as sy
 from syft.core.common.message import SignedImmediateSyftMessageWithReply
 from syft.core.common.message import SignedImmediateSyftMessageWithoutReply
 from syft.core.common.message import SyftMessage
-from syft.core.common.serde.serializable import Serializable
 from syft.core.io.connection import ClientConnection
+
+# syft relative
+from ...decorators.syft_decorator_impl import syft_decorator
 
 
 class HTTPConnection(ClientConnection):
+    @syft_decorator(typechecking=True)
     def __init__(self, url: str) -> None:
         self.base_url = url
 
+    @syft_decorator(typechecking=True)
     def send_immediate_msg_with_reply(
         self, msg: SignedImmediateSyftMessageWithReply
-    ) -> Serializable:
-        blob = self.send_msg(msg=msg).text
+    ) -> SignedImmediateSyftMessageWithoutReply:
+        """Sends high priority messages and wait for their responses.
 
+        NOTE: Mandatory abstract method.
+
+        This method aims to implement
+        a HTTP version of the
+        ClientConnection.send_immediate_msg_with_reply
+
+        :return: returns an instance of SignedImmediateSyftMessageWithReply.
+        :rtype: SignedImmediateSyftMessageWithReply
+        """
+
+        # Serializes SignedImmediateSyftMessageWithReply in json format
+        # and send it using HTTP protocol
+        blob = self.__send_msg(msg=msg).text
+
+        # Deserialize node's response
         response = sy.deserialize(blob=blob, from_json=True)
+
+        # Return SignedImmediateSyftMessageWithoutReply
         return response
 
+    @syft_decorator(typechecking=True)
     def send_immediate_msg_without_reply(
         self, msg: SignedImmediateSyftMessageWithoutReply
     ) -> None:
-        self.send_msg(msg=msg)
+        """Sends high priority messages without waiting for their reply.
 
+        NOTE: Mandatory abstract method.
+
+        This method aims to implement
+        a HTTP version of the
+        ClientConnection.send_immediate_msg_without_reply
+
+        """
+        # Serializes SignedImmediateSyftMessageWithReply in json format
+        # and send it using HTTP protocol
+        self.__send_msg(msg=msg)
+
+    @syft_decorator(typechecking=True)
     def send_eventual_msg_without_reply(
         self, msg: SignedImmediateSyftMessageWithoutReply
     ) -> None:
-        self.send_msg(msg=msg)
+        """Sends low priority messages without waiting for their reply.
 
-    def send_msg(self, msg: SyftMessage) -> requests.Response:
+        NOTE: Mandatory abstract method.
+
+        This method aims to implement
+        a HTTP version of the
+        ClientConnection.send_eventual_msg_without_reply
+        """
+        # Serializes SignedImmediateSyftMessageWithReply in json format
+        # and send it using HTTP protocol
+        self.__send_msg(msg=msg)
+
+    @syft_decorator(typechecking=True)
+    def __send_msg(self, msg: SyftMessage) -> requests.Response:
+        """Serializes Syft messages in json format and send it using HTTP protocol.
+
+        NOTE: Auxiliar method to provide code modularity and reuse
+        This will avoid code duplication.
+
+        :return: returns response body in string format (serialized syft message).
+        :rtype: str
+        """
+        # Serialize SyftMessage object
         json_msg = msg.json()
+
+        # Perform HTTP request using base_url as a root address
         r = requests.post(url=self.base_url, json=json_msg)
+
+        # Return request's response body (serialized
+        # syft msg sent by the other peer)
         return r
 
+    @syft_decorator(typechecking=True)
     def _get_metadata(self) -> str:
+        """Request Node's metadata
+
+        :return: returns  node metadata.
+        :rtype: str
+        """
         return requests.get(self.base_url + "/metadata").text
