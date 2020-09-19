e9c630dda886f69119b9c0fa506d32495c0fcae8
Anubhav Raj Singh <anubhavraj.08@gmail.com>
2020-09-19 20:49:53 +0530

    Add conftest file with workers
    * converted benchmark into package
    
    * fixed typos
    
    * added  conftest
    
    - removed workers_initilization files

--- benchmarks/conftest.py
+++ benchmarks/conftest.py
@@ -1,18 +1,24 @@
+import pytest
 import torch
+
 import syft
 from syft import TorchHook
 from syft.generic.frameworks.hook import hook_args
 
 
+@pytest.fixture(scope="session", autouse=True)
 def hook():
     hook = TorchHook(torch)
     return hook
 
 
+@pytest.fixture(scope="function", autouse=True)
 def workers(hook):
-    """
-    This function defines virtual workers to be used in benchmarking functions.
-    """
+    # To run a plan locally the local worker can't be a client worker,
+    # since it needs to register objects
+    # LaRiffle edit: doing this increases the reference count on pointers and
+    # breaks the auto garbage collection for pointer of pointers, see #2150
+    # hook.local_worker.is_client_worker = False
 
     # Reset the hook and the local worker
     syft.local_worker.clear_objects()
@@ -21,11 +27,12 @@ def workers(hook):
     hook_args.register_response_functions = {}
     hook_args.get_tensor_type_functions = {}
 
-    # Define virtual workers
+    # Define 4 virtual workers
     alice = syft.VirtualWorker(id="alice", hook=hook, is_client_worker=False)
     bob = syft.VirtualWorker(id="bob", hook=hook, is_client_worker=False)
-    james = syft.VirtualWorker(id="james", hook=hook, is_client_worker=False)
     charlie = syft.VirtualWorker(id="charlie", hook=hook, is_client_worker=False)
+    james = syft.VirtualWorker(id="james", hook=hook, is_client_worker=False)
+
     workers = {
         "me": hook.local_worker,
         "alice": alice,
@@ -33,4 +40,10 @@ def workers(hook):
         "charlie": charlie,
         "james": james,
     }
-    return workers
+
+    yield workers
+
+    alice.remove_worker_from_local_worker_registry()
+    bob.remove_worker_from_local_worker_registry()
+    charlie.remove_worker_from_local_worker_registry()
+    james.remove_worker_from_local_worker_registry()
---
+++ benchmarks/frameworks/torch/mpc/pytestbenchmark/bench.py
@@ -0,0 +1,20 @@
+"""
+Define benchmark tests
+"""
+from benchmarks.frameworks.torch.mpc.pytestbenchmark.benchmark_functions import sigmoid, tanh
+
+
+def test_sigmoid_chebyshev(benchmark, workers):
+    """
+    Test sigmoid approximation with chebyshev method and
+    precision value of 1
+    """
+    benchmark(sigmoid, "chebyshev", 1, workers)
+
+
+def test_tanh_chebyshev(benchmark, workers):
+    """
+    Test tanh approximation with chebyshev method and
+    precision value of 1
+    """
+    benchmark(tanh, "chebyshev", 1, workers)
