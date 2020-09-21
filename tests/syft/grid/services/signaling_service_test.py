71db6b2d0bb3d9aa1f0170eecad6cea8abd9b839
Ionésio Junior <ionesiojr@gmail.com>
2020-09-21 15:45:21 -0300

    WebRTC Duet Demo (#4554)
    * Remove unused files
    
    * Finish signaling service
    
    * ADD HTTPClient connection
    
    * ADD WebRTC Bidirectional Connection
    
    * ADD Duet (WebRTC) class
    
    * ADD signaling service unit tests
    
    * ADD webrtc connection tests
    
    * ADD pytest-async as a requirement
    
    * Remove unused import
    
    * Replace signaling_msgs dict -> in_memory_client_registry dict
    
    - At this moment, self.store doesn't support store SyftMessage type (which is important for WebRTC signaling process).
    - For this moment, we can use in_memory_client_registry to supply it, but it should be changed after.
    
    * Move GridNetworkClient to grid.clients.network_client module
    
    * Minor Fixes
    - Remove prints
    - Fix type hints
    - Update imports
    
    * REMOVE unused module
    
    * Minor Fixes
    - Fix imports
    - Rename network_client -> signaling_client (Signaling can be a generic service in future)
    - Improve network print
    
    * Remove redundant unit test
    
    * UPDATE signaling protobuf messages
    
    * UPDATE setup.cfg
    - ADD new packages (aiortc, nest_asyncio)
    
    * RENAME http_client -> http_connection
    
    * ADD Notebooks
    
    * Minor fixes
    
    * Add pytest.skipif for non supported async in Python <= 3.7
    
    * Add pytest.skipif for non supported async in Python <= 3.7
    
    * Fix HTTPConnection
    - ADD typechecks
    - ADD docstrings / comments
    - RENAME send_msg (public) to __send_msg (private)
    
    * - FIX docstring
    
    * FIX relative import -> absolute import
    
    * Create signaling_msgs dict as a temporary workaround to store SignalingMessages
    
    - Declare signaling_msgs in abstract.node
    - Init empty signaling messages dict in common.node
    - Update Signaling service  in_memory_registry -> signaling_msgs
    - Update Signaling service tests in_memory_registry -> signaling_msgs
    
    * Webrtc duet fixes (#4578)
    
    * More comments and improvements mentioned in PR: #4554
    
    * General fixes to grid files
    
    - Added aiortc to requirements.txt
    - Updated bob and maria notebooks to explain how to run network.py
    - Moved executable node examples into folder
    - Changed executable examples to absolute imports
    - Changed grid library code to relative imports
    
    Co-authored-by: Madhava Jay <me@madhavajay.com>

--- tests/syft/grid/services/signaling_service_test.py
+++ tests/syft/grid/services/signaling_service_test.py
@@ -1,48 +1,226 @@
+# stdlib
+from typing import Tuple
+
 # syft absolute
 import syft as sy
+from syft.core.io.address import Address
+from syft.core.node.common.node import Node
+from syft.grid.services.signaling_service import AnswerPullRequestMessage
+from syft.grid.services.signaling_service import OfferPullRequestMessage
+from syft.grid.services.signaling_service import PullSignalingService
+from syft.grid.services.signaling_service import PushSignalingService
 from syft.grid.services.signaling_service import SignalingAnswerMessage
 from syft.grid.services.signaling_service import SignalingOfferMessage
-from syft.grid.services.signaling_service import SignalingService
+from syft.grid.services.signaling_service import SignalingRequestsNotFound
+
+
+def get_preset_nodes() -> Tuple[Node, Node, Node]:
+    om_network = sy.Network(name="OpenMined")
+    om_network.immediate_services_without_reply.append(PushSignalingService)
+    om_network.immediate_services_with_reply.append(PullSignalingService)
+    om_network._register_services()  # re-register all services including SignalingService
+    bob_vm = sy.VirtualMachine(name="Bob")
+    alice_vm = sy.VirtualMachine(name="Alice")
+    return om_network, bob_vm, alice_vm
 
 
 def test_signaling_offer_message_serde() -> None:
     bob_vm = sy.VirtualMachine(name="Bob")
+    target = Address(name="Alice")
 
-    msg = SignalingOfferMessage(address=bob_vm.address, payload="Test Payload")
+    msg = SignalingOfferMessage(
+        address=target,
+        payload="SDP",
+        host_metadata=bob_vm.get_metadata_for_client(),
+        target_peer=target,
+        host_peer=bob_vm.address,
+    )
 
     blob = msg.serialize()
     msg2 = sy.deserialize(blob=blob)
 
     assert msg.id == msg2.id
-    assert msg.address == msg2.address
+    assert msg.address == target
     assert msg.payload == msg2.payload
-    assert msg2.payload == "Test Payload"
+    assert msg2.payload == "SDP"
+    assert msg2.host_metadata == bob_vm.get_metadata_for_client()
     assert msg == msg2
 
 
 def test_signaling_answer_message_serde() -> None:
     bob_vm = sy.VirtualMachine(name="Bob")
+    target = Address(name="Alice")
 
-    msg = SignalingAnswerMessage(address=bob_vm.address, payload="Test Payload")
+    msg = SignalingAnswerMessage(
+        address=target,
+        payload="SDP",
+        host_metadata=bob_vm.get_metadata_for_client(),
+        target_peer=target,
+        host_peer=bob_vm.address,
+    )
 
     blob = msg.serialize()
     msg2 = sy.deserialize(blob=blob)
 
     assert msg.id == msg2.id
-    assert msg.address == msg2.address
+    assert msg.address == target
     assert msg.payload == msg2.payload
-    assert msg2.payload == "Test Payload"
+    assert msg2.payload == "SDP"
+    assert msg2.host_metadata == bob_vm.get_metadata_for_client()
     assert msg == msg2
 
 
-def test_signaling_service() -> None:
+def test_signaling_answer_pull_request_message_serde() -> None:
     bob_vm = sy.VirtualMachine(name="Bob")
-    bob_vm.immediate_services_without_reply.append(SignalingService)
-    bob_vm._register_services()  # re-register all services including SignalingService
-    bob_vm_client = bob_vm.get_root_client()
+    target = Address(name="Alice")
 
-    bob_vm_client.send_immediate_msg_without_reply(
-        msg=SignalingAnswerMessage(address=bob_vm.address, payload="Test Payload")
+    msg = AnswerPullRequestMessage(
+        address=target,
+        target_peer=target,
+        host_peer=bob_vm.address,
+        reply_to=bob_vm.address,
     )
 
-    # TODO add some asserts here that verify something happened
+    blob = msg.serialize()
+    msg2 = sy.deserialize(blob=blob)
+
+    assert msg.id == msg2.id
+    assert msg.address == target
+    assert msg == msg2
+
+
+def test_signaling_offer_pull_request_message_serde() -> None:
+    bob_vm = sy.VirtualMachine(name="Bob")
+    target = Address(name="Alice")
+
+    msg = OfferPullRequestMessage(
+        address=target,
+        target_peer=target,
+        host_peer=bob_vm.address,
+        reply_to=bob_vm.address,
+    )
+
+    blob = msg.serialize()
+    msg2 = sy.deserialize(blob=blob)
+
+    assert msg.id == msg2.id
+    assert msg.address == target
+    assert msg == msg2
+
+
+def test_push_offer_signaling_service() -> None:
+    om_network, bob_vm, alice_vm = get_preset_nodes()
+    om_network_client = om_network.get_root_client()
+
+    offer_msg = SignalingOfferMessage(
+        address=om_network.address,
+        payload="SDP",
+        host_metadata=alice_vm.get_metadata_for_client(),
+        target_peer=alice_vm.address,
+        host_peer=bob_vm.address,
+    )
+    om_network_client.send_immediate_msg_without_reply(msg=offer_msg)
+
+    assert om_network.signaling_msgs.pop(offer_msg.id) == offer_msg
+
+
+def test_push_answer_signaling_service() -> None:
+    om_network, bob_vm, alice_vm = get_preset_nodes()
+    om_network_client = om_network.get_root_client()
+
+    ans_msg = SignalingAnswerMessage(
+        address=om_network.address,
+        payload="SDP",
+        host_metadata=alice_vm.get_metadata_for_client(),
+        target_peer=alice_vm.address,
+        host_peer=bob_vm.address,
+    )
+    om_network_client.send_immediate_msg_without_reply(msg=ans_msg)
+
+    assert om_network.signaling_msgs.pop(ans_msg.id) == ans_msg
+
+
+def test_pull_offer_signaling_service() -> None:
+    om_network, bob_vm, alice_vm = get_preset_nodes()
+    om_network_client = om_network.get_root_client()
+
+    offer_msg = SignalingOfferMessage(
+        address=om_network.address,
+        payload="SDP",
+        host_metadata=alice_vm.get_metadata_for_client(),
+        target_peer=alice_vm.address,
+        host_peer=bob_vm.address,
+    )
+    om_network_client.send_immediate_msg_without_reply(msg=offer_msg)
+
+    assert om_network.signaling_msgs.get(offer_msg.id) == offer_msg
+
+    offer_pull_req = OfferPullRequestMessage(
+        address=om_network.address,
+        target_peer=bob_vm.address,
+        host_peer=alice_vm.address,
+        reply_to=om_network_client.address,
+    )
+
+    response = om_network_client.send_immediate_msg_with_reply(msg=offer_pull_req)
+
+    assert response == offer_msg
+
+
+def test_pull_answer_signaling_service() -> None:
+    om_network, bob_vm, alice_vm = get_preset_nodes()
+    om_network_client = om_network.get_root_client()
+
+    answer_msg = SignalingAnswerMessage(
+        address=om_network.address,
+        payload="SDP",
+        host_metadata=alice_vm.get_metadata_for_client(),
+        target_peer=alice_vm.address,
+        host_peer=bob_vm.address,
+    )
+    om_network_client.send_immediate_msg_without_reply(msg=answer_msg)
+
+    assert om_network.signaling_msgs.get(answer_msg.id) == answer_msg
+
+    ans_pull_req = AnswerPullRequestMessage(
+        address=om_network.address,
+        target_peer=bob_vm.address,
+        host_peer=alice_vm.address,
+        reply_to=om_network_client.address,
+    )
+
+    response = om_network_client.send_immediate_msg_with_reply(msg=ans_pull_req)
+
+    assert response == answer_msg
+
+
+def test_not_found_pull_offer_requests_signaling_service() -> None:
+    om_network, bob_vm, alice_vm = get_preset_nodes()
+    om_network_client = om_network.get_root_client()
+
+    offer_pull_req = OfferPullRequestMessage(
+        address=om_network.address,
+        target_peer=bob_vm.address,
+        host_peer=alice_vm.address,
+        reply_to=om_network_client.address,
+    )
+
+    response = om_network_client.send_immediate_msg_with_reply(msg=offer_pull_req)
+
+    assert isinstance(response, SignalingRequestsNotFound)
+
+
+def test_not_found_pull_ans_requests_signaling_service() -> None:
+    om_network, bob_vm, alice_vm = get_preset_nodes()
+    om_network_client = om_network.get_root_client()
+
+    ans_pull_req = AnswerPullRequestMessage(
+        address=om_network.address,
+        target_peer=bob_vm.address,
+        host_peer=alice_vm.address,
+        reply_to=om_network_client.address,
+    )
+
+    response = om_network_client.send_immediate_msg_with_reply(msg=ans_pull_req)
+
+    assert isinstance(response, SignalingRequestsNotFound)
