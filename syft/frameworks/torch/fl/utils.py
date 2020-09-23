46c660bb5de4c4ffb12be67838aa8ce9a6a258ad
Denny Mattern <dymat@users.noreply.github.com>
2020-09-23 21:42:06 +0200

    prevent federated_avg() from adding noise to model
    `model = type(model_list[0])()` instantiates a new model in most cases with random weights, which then will be added to the merged model. This is probably an unwanted behavior. Hence, we should overwrite all weights with zeros: 
    ```
    for p in model.parameters():
            nn.init.zeros_(p)
    ```

--- syft/frameworks/torch/fl/utils.py
+++ syft/frameworks/torch/fl/utils.py
@@ -83,6 +83,10 @@ def federated_avg(models: Dict[Any, torch.nn.Module]) -> torch.nn.Module:
     nr_models = len(models)
     model_list = list(models.values())
     model = type(model_list[0])()
+    
+    # setting all model parameters to zero
+    for p in model.parameters():
+        nn.init.zeros_(p)
 
     for i in range(nr_models):
         model = add_model(model, model_list[i])
