fd7a751f39641c679661fa81868275548d8d94b5
Nilansh Rajput <2017csb1092@iitrpr.ac.in>
2020-10-01 18:21:09 +0530

    add boto3 in setup.py for CLI (#735)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="OpenMined PyGrid CLI",
     version="0.0.1",
     packages=["apps.cli",],
-    install_requires=["click", "PyInquirer", "terrascript"],
+    install_requires=["click", "PyInquirer", "terrascript", "boto3"],
     entry_points="""
         [console_scripts]
         pygrid=apps.cli.cli:cli
