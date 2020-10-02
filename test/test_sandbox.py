a8375312bf6eaaad4bdc815ba0db688acaa98d32
Théo Ryffel <theo.leffyr@gmail.com>
2020-10-02 18:09:18 +0200

    Update test/test_sandbox.py

--- test/test_sandbox.py
+++ test/test_sandbox.py
@@ -41,7 +41,7 @@ def test_sandbox():
     assert bob.search(["#linnerrud_dataset"])  # noqa: F821
 
     try:
-        from tensorflow.keras import datasets  # pragma: no cover
+        from tensorflow.keras import datasets  # noqa: F401
         assert bob.search(["#fashion_mnist"])  # noqa: F821
         assert bob.search(["#cifar10"])  # noqa: F821
     except ImportError:
