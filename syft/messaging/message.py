ab689319f17d92216064438ab7dc01c804058000
Nilanshrajput <2017csb1092@iitrpr.ac.in>
2020-10-10 13:36:22 +0530

    Add get_copy argument in  get  and related methods
    for getting objects without  removing the remote object

--- syft/messaging/message.py
+++ syft/messaging/message.py
@@ -331,12 +331,13 @@ class ObjectRequestMessage(Message):
     # TODO: add more efficient detailer and simplifier custom for this type
     # https://github.com/OpenMined/PySyft/issues/2512
 
-    def __init__(self, obj_id, user, reason):
+    def __init__(self, obj_id, user, reason, get_copy):
         """Initialize the message."""
 
         self.object_id = obj_id
         self.user = user
         self.reason = reason
+        self.get_copy = get_copy
 
     def __str__(self):
         """Return a human readable version of this message"""
@@ -359,6 +360,7 @@ class ObjectRequestMessage(Message):
             sy.serde.msgpack.serde._simplify(worker, msg.object_id),
             sy.serde.msgpack.serde._simplify(worker, msg.user),
             sy.serde.msgpack.serde._simplify(worker, msg.reason),
+            sy.serde.msgpack.serde._simplify(worker, msg.get_copy),
         )
 
     @staticmethod
@@ -380,6 +382,7 @@ class ObjectRequestMessage(Message):
             sy.serde.msgpack.serde._detail(worker, msg_tuple[0]),
             sy.serde.msgpack.serde._detail(worker, msg_tuple[1]),
             sy.serde.msgpack.serde._detail(worker, msg_tuple[2]),
+            sy.serde.msgpack.serde._detail(worker, msg_tuple[3]),
         )
 
     @staticmethod
