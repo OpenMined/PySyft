92487eaeef608a1967a03a64be99ef11ff2a4720
Ionesio Junior <ionesiojr@gmail.com>
2020-10-11 21:50:24 -0300

    Remove unused io folder

--- apps/network/src/main/io/routes/__init__.py
+++
@@ -1,4 +0,0 @@
-from .data_centric import *
-from .model_centric import *
-from .common import *
-from .signaling import *
--- apps/network/src/main/io/routes/common/route.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request, Response
-from ....core import node
-
-common = Blueprint("common", __name__)
-
-
-@common.route("/metadata", methods=["GET"])
-def get_metadata():
-    return node.get_metadata_for_client()
