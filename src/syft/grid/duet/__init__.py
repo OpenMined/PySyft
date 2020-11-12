a80ef16921f241002549983b189f81a197e879ca
Madhava Jay <me@madhavajay.com>
2020-11-12 21:38:55 +1000

    Fixed path to logo and added it to setup.cfg for packaging
    - Ran image through pngcrush

--- src/syft/grid/duet/__init__.py
+++ src/syft/grid/duet/__init__.py
@@ -28,7 +28,7 @@ ADDR_REPOSITORY = (
     "https://raw.githubusercontent.com/OpenMined/OpenGridNodes/master/network_address"
 )
 
-LOGO_URL = Path("../../../docs/img") / "logo.png"
+LOGO_URL = os.path.abspath(Path(__file__) / "../../../img/logo.png")
 
 
 try:
