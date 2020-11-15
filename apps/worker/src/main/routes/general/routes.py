d40e011655eae4418cf6700c9fe4b8f6404f09f3
Ionesio Junior <ionesiojr@gmail.com>
2020-11-15 17:41:00 -0300

    UPDATE worker routes

--- apps/worker/src/main/routes/general/routes.py
+++ apps/worker/src/main/routes/general/routes.py
@@ -1,5 +1,7 @@
 from .blueprint import root_blueprint as root_route
 from ...core.node import node
+from nacl.encoding import HexEncoder
+import json
 
 # syft absolute
 from syft.core.common.message import SignedImmediateSyftMessageWithReply
@@ -11,14 +13,28 @@ import json
 
 
 @root_route.route("/pysyft", methods=["POST"])
-def root_route():
-    json_msg = request.get_json()
-    obj_msg = _deserialize(blob=json_msg, from_json=True)
+def pysyft_route():
+    data = request.get_data()
+    obj_msg = _deserialize(blob=data, from_bytes=True)
     if isinstance(obj_msg, SignedImmediateSyftMessageWithReply):
         reply = node.recv_immediate_msg_with_reply(msg=obj_msg)
-        return reply.json()
+        r = Response(response=reply.serialize(to_bytes=True), status=200)
+        r.headers["Content-Type"] = "application/octet-stream"
+        return r
     elif isinstance(obj_msg, SignedImmediateSyftMessageWithoutReply):
         node.recv_immediate_msg_without_reply(msg=obj_msg)
     else:
         node.recv_eventual_msg_without_reply(msg=obj_msg)
     return ""
+
+
+@root_route.route("/metadata", methods=["GET"])
+def metadata_route():
+    response_body = {
+        "key": node.signing_key.encode(encoder=HexEncoder).decode("utf-8"),
+        "metadata": node.get_metadata_for_client()
+        .serialize()
+        .SerializeToString()
+        .decode("ISO-8859-1"),
+    }
+    return Response(json.dumps(response_body), status=200, mimetype="application/json")
