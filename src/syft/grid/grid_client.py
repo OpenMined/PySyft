57f392ad2fa3d2bf5258e322af28bfe70f6e1420
Ionesio Junior <ionesiojr@gmail.com>
2020-11-15 17:08:09 -0300

    UPDATE Grid Client

--- src/syft/grid/grid_client.py
+++ src/syft/grid/grid_client.py
@@ -2,12 +2,20 @@
 from typing import Any
 from typing import Dict
 from typing import Optional
+from typing import Union
 
 # third party
-from nacl.signing import SigningKey
 from nacl.encoding import HexEncoder
+from nacl.signing import SigningKey
 
 # syft relative
+from ..core.common.message import EventualSyftMessageWithoutReply
+from ..core.common.message import ImmediateSyftMessageWithReply
+from ..core.common.message import ImmediateSyftMessageWithoutReply
+from ..core.common.message import SignedEventualSyftMessageWithoutReply
+from ..core.common.message import SignedImmediateSyftMessageWithReply
+from ..core.common.message import SignedImmediateSyftMessageWithoutReply
+from ..core.common.message import SyftMessage
 from ..core.io.connection import ClientConnection
 from ..core.io.location.specific import SpecificLocation
 from ..core.io.route import SoloRoute
@@ -40,21 +48,26 @@ def connect(
             # Use Server metadata
             # to build client route
             conn = conn_type(url=url)  # type: ignore
-            metadata, user_key = conn.login(credentials=credentials)
+            if not issubclass(client_type, VirtualMachineClient):
+                metadata, user_key = conn.login(credentials=credentials)
+            else:
+                metadata, user_key = conn._get_metadata()
+
             user_key = SigningKey(user_key.encode("utf-8"), encoder=HexEncoder)
-            
             (
                 spec_location,
                 name,
                 client_id,
             ) = client_type.deserialize_client_metadata_from_node(metadata=metadata)
-            
+
             # Create a new Solo Route using the selected connection type
             route = SoloRoute(destination=spec_location, connection=conn)
 
             location_args = self.__route_client_location(
                 client_type=client_type, location=spec_location
             )
+
+            self.proxy_mode = False
 
             # Create a new client using the selected client type
             super().__init__(
@@ -65,6 +78,41 @@ def connect(
                 name=name,
                 routes=[route],
                 signing_key=user_key,
+            )
+
+        @syft_decorator(typechecking=True)
+        def send_immediate_msg_with_reply(
+            self,
+            msg: Union[
+                SignedImmediateSyftMessageWithReply, ImmediateSyftMessageWithReply
+            ],
+            route_index: int = 0,
+        ) -> SyftMessage:
+            msg.address = None
+            return super(GridClient, self).send_immediate_msg_with_reply(
+                msg=msg, route_index=route_index
+            )
+
+        @syft_decorator(typechecking=True)
+        def send_immediate_msg_without_reply(
+            self,
+            msg: Union[
+                SignedImmediateSyftMessageWithoutReply, ImmediateSyftMessageWithoutReply
+            ],
+            route_index: int = 0,
+        ) -> None:
+            msg.address = None
+            return super(GridClient, self).send_immediate_msg_without_reply(
+                msg=msg, route_index=route_index
+            )
+
+        @syft_decorator(typechecking=True)
+        def send_eventual_msg_without_reply(
+            self, msg: EventualSyftMessageWithoutReply, route_index: int = 0
+        ) -> None:
+            msg.address = None
+            return super(GridClient, self).send_eventual_msg_without_reply(
+                msg=msg, route_index=route_index
             )
 
         @syft_decorator(typechecking=True)
