68b20d50111f44f4df2e6d25273c511532c4955c
Ionesio Junior <ionesiojr@gmail.com>
2020-11-15 22:27:17 -0300

    ADD new Domain Message types

---
+++ src/syft/core/node/domain/service/worker_management_message.py
@@ -0,0 +1,184 @@
+# DOs and Don's of this class:
+# - Do NOT use absolute syft imports (i.e. import syft.core...) Use relative ones.
+# - Do NOT put multiple imports on the same line (i.e. from <x> import a, b, c). Use separate lines
+# - Do sort imports by length
+# - Do group imports by where they come from
+
+# stdlib
+import json
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Type
+
+# third party
+from google.protobuf.reflection import GeneratedProtocolMessageType
+from nacl.signing import VerifyKey
+from typing_extensions import final
+
+# syft relative
+from .....decorators.syft_decorator_impl import syft_decorator
+from .....proto.core.node.common.service.repr_service_pb2 import (
+    CreateWorkerMessage as CreateWorkerMessage_PB,
+)
+from .....proto.core.node.common.service.repr_service_pb2 import (
+    WorkerCreatedMessage as WorkerCreatedMessage_PB,
+)
+from ....common.message import ImmediateSyftMessageWithReply
+from ....common.message import ImmediateSyftMessageWithoutReply
+from ....common.serde.deserialize import _deserialize
+from ....common.uid import UID
+from ....io.address import Address
+from ...abstract.node import AbstractNode
+
+
+@final
+class CreateWorkerMessage(ImmediateSyftMessageWithReply):
+    def __init__(
+        self,
+        address: Address,
+        settings: Dict,
+        reply_to: Address,
+        msg_id: Optional[UID] = None,
+    ):
+        super().__init__(address=address, msg_id=msg_id, reply_to=reply_to)
+        self.settings = settings
+
+    @syft_decorator(typechecking=True)
+    def _object2proto(self) -> CreateWorkerMessage_PB:
+        """Returns a protobuf serialization of self.
+
+        As a requirement of all objects which inherit from Serializable,
+        this method transforms the current object into the corresponding
+        Protobuf object so that it can be further serialized.
+
+        :return: returns a protobuf object
+        :rtype: CreateWorkerMessage_PB
+
+        .. note::
+            This method is purely an internal method. Please use object.serialize() or one of
+            the other public serialization methods if you wish to serialize an
+            object.
+        """
+
+        return CreateWorkerMessage_PB(
+            msg_id=self.id.serialize(),
+            address=self.address.serialize(),
+            settings=json.dumps(self.settings),
+            reply_to=self.reply_to.serialize(),
+        )
+
+    @staticmethod
+    def _proto2object(proto: CreateWorkerMessage_PB) -> "CreateWorkerMessage":
+        """Creates a CreateWorkerMessage from a protobuf
+
+        As a requirement of all objects which inherit from Serializable,
+        this method transforms a protobuf object into an instance of this class.
+
+        :return: returns an instance of CreateWorkerMessage
+        :rtype: CreateWorkerMessage
+
+        .. note::
+            This method is purely an internal method. Please use syft.deserialize()
+            if you wish to deserialize an object.
+        """
+
+        return CreateWorkerMessage(
+            msg_id=_deserialize(blob=proto.msg_id),
+            address=_deserialize(blob=proto.address),
+            settings=json.loads(proto.settings),
+            reply_to=_deserialize(blob=proto.reply_to),
+        )
+
+    @staticmethod
+    def get_protobuf_schema() -> GeneratedProtocolMessageType:
+        """Return the type of protobuf object which stores a class of this type
+
+        As a part of serialization and deserialization, we need the ability to
+        lookup the protobuf object type directly from the object type. This
+        static method allows us to do this.
+
+        Importantly, this method is also used to create the reverse lookup ability within
+        the metaclass of Serializable. In the metaclass, it calls this method and then
+        it takes whatever type is returned from this method and adds an attribute to it
+        with the type of this class attached to it. See the MetaSerializable class for details.
+
+        :return: the type of protobuf object which corresponds to this class.
+        :rtype: GeneratedProtocolMessageType
+
+        """
+
+        return CreateWorkerMessage_PB
+
+
+@final
+class WorkerCreatedMessage(ImmediateSyftMessageWithoutReply):
+    def __init__(
+        self, address: Address, vm_address: Address, msg_id: Optional[UID] = None
+    ):
+        super().__init__(address=address, msg_id=msg_id)
+        self.vm_address = vm_address
+
+    @syft_decorator(typechecking=True)
+    def _object2proto(self) -> WorkerCreatedMessage_PB:
+        """Returns a protobuf serialization of self.
+
+        As a requirement of all objects which inherit from Serializable,
+        this method transforms the current object into the corresponding
+        Protobuf object so that it can be further serialized.
+
+        :return: returns a protobuf object
+        :rtype: WorkerCreatedMessage_PB
+
+        .. note::
+            This method is purely an internal method. Please use object.serialize() or one of
+            the other public serialization methods if you wish to serialize an
+            object.
+        """
+
+        return WorkerCreatedMessage_PB(
+            msg_id=self.id.serialize(),
+            address=self.address.serialize(),
+            vm_address=self.vm_address.serialize(),
+        )
+
+    @staticmethod
+    def _proto2object(proto: WorkerCreatedMessage_PB) -> "WorkerCreatedMessage":
+        """Creates a WorkerCreatedMessage from a protobuf
+
+        As a requirement of all objects which inherit from Serializable,
+        this method transforms a protobuf object into an instance of this class.
+
+        :return: returns an instance of WorkerCreatedMessage
+        :rtype: WorkerCreatedMessage
+
+        .. note::
+            This method is purely an internal method. Please use syft.deserialize()
+            if you wish to deserialize an object.
+        """
+
+        return WorkerCreatedMessage(
+            msg_id=_deserialize(blob=proto.msg_id),
+            address=_deserialize(blob=proto.address),
+            vm_address=_deserialize(blob=proto.vm_address),
+        )
+
+    @staticmethod
+    def get_protobuf_schema() -> GeneratedProtocolMessageType:
+        """Return the type of protobuf object which stores a class of this type
+
+        As a part of serialization and deserialization, we need the ability to
+        lookup the protobuf object type directly from the object type. This
+        static method allows us to do this.
+
+        Importantly, this method is also used to create the reverse lookup ability within
+        the metaclass of Serializable. In the metaclass, it calls this method and then
+        it takes whatever type is returned from this method and adds an attribute to it
+        with the type of this class attached to it. See the MetaSerializable class for details.
+
+        :return: the type of protobuf object which corresponds to this class.
+        :rtype: GeneratedProtocolMessageType
+
+        """
+
+        return WorkerCreatedMessage_PB
