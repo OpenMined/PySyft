a5c02c497eab124a20e307c939ecbbaa5d485de2
wip-abramson <wip.abramson@gmail.com>
2020-11-20 12:18:37 +0000

    working docker config

---
+++ examples/duet/ssi/scripts/get_URLS.sh
@@ -0,0 +1,2 @@
+#script to get notebook URLs
+for i in `docker ps --format "table {{.Names}}" | grep notebook`; do port=$(docker port $i 8888/tcp | cut -d":" -f2);token=$(docker logs $i 2>&1 | grep "127.0.0.1):8888/" | cut -d"=" -f2 | head -1); echo "$i - http://127.0.0.1:$port/?token=$token"; done
