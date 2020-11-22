b7514ff97118f3bd0a22d620659d307226e0d1fd
Ionesio Junior <ionesiojr@gmail.com>
2020-11-22 14:25:08 -0300

    ADD a new worker connection

--- apps/domain/src/main/core/node.py
+++ apps/domain/src/main/core/node.py
@@ -1,3 +1,19 @@
+from syft.grid.grid_client import connect
 from syft.core.node.domain.domain import Domain
+from syft.core.node.device.client import DeviceClient
+from syft.grid.connections.http_connection import HTTPConnection
+from syft.grid.services.worker_management_service import CreateWorkerService
 
 node = Domain(name="om-domain")
+node.immediate_services_with_reply.append(CreateWorkerService)
+node._register_services()  # re-register all services including SignalingService
+
+try:
+    node.private_device = connect(
+        url="http://localhost:5000",  # Domain Address
+        conn_type=HTTPConnection,  # HTTP Connection Protocol
+        client_type=DeviceClient,
+    )  # Device Client type
+    node.in_memory_client_registry[node.private_device.device_id] = node.private_device
+except:
+    pass
