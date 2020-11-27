2e3dd7efc6c672809e0119e6ae24a6399a8b4ed9
Théo Ryffel <theo.leffyr@gmail.com>
2020-11-27 18:49:00 +0100

    Update examples/ariann/main.py

--- examples/ariann/main.py
+++ examples/ariann/main.py
@@ -150,7 +150,7 @@ if __name__ == "__main__":
                 stdout=subprocess.PIPE,
                 shell=True,
                 preexec_fn=os.setsid,
-                executable="/bin/zsh",
+                executable="/bin/bash",
             )
             for worker in ["alice", "bob", "crypto_provider"]
         ]
