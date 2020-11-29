e664790b7ae4f66907e011c78940b2c9f628dcdf
Théo Ryffel <theo.leffyr@gmail.com>
2020-11-29 18:23:55 +0100

    Move cuda SPDZ out of multiprocessing

--- syft/frameworks/torch/mpc/spdz.py
+++ syft/frameworks/torch/mpc/spdz.py
@@ -51,25 +51,10 @@ def slice(x, j, slice_size):
 
 
 def triple_mat_mul(core_id, op, delta, epsilon, a, b, kwargs_):
-    if th.cuda.is_available():
-        cmd = getattr(CUDALongTensor, op)
-        delta = CUDALongTensor(delta)
-        epsilon = CUDALongTensor(epsilon)
-        a = CUDALongTensor(a)
-        b = CUDALongTensor(b)
-        delta_b = cmd(delta, b, **kwargs_)
-        a_epsilon = cmd(a, epsilon, **kwargs_)
-        delta_epsilon = cmd(delta, epsilon, **kwargs_)
-        delta_b, a_epsilon, delta_epsilon = (
-            delta_b._tensor,
-            a_epsilon._tensor,
-            delta_epsilon._tensor,
-        )
-    else:
-        cmd = getattr(th, op)
-        delta_b = cmd(delta, b, **kwargs_)
-        a_epsilon = cmd(a, epsilon, **kwargs_)
-        delta_epsilon = cmd(delta, epsilon, **kwargs_)
+    cmd = getattr(th, op)
+    delta_b = cmd(delta, b, **kwargs_)
+    a_epsilon = cmd(a, epsilon, **kwargs_)
+    delta_epsilon = cmd(delta, epsilon, **kwargs_)
 
     return core_id, delta_b, a_epsilon, delta_epsilon
 
@@ -106,28 +91,43 @@ def spdz_compute(
     )
 
     if op in {"matmul", "conv2d"}:
-        batch_size = delta.shape[0]
-
-        multiprocessing_args = []
-        slice_size = math.ceil(batch_size / N_CORES)
-        for core_id in range(N_CORES):
-            process_args = (
-                core_id,
-                op,
-                slice(delta, core_id, slice_size),
-                epsilon,
-                slice(a, core_id, slice_size),
-                b,
-                kwargs_,
+        if th.cuda.is_available():
+            cmd = getattr(CUDALongTensor, op)
+            delta = CUDALongTensor(delta)
+            epsilon = CUDALongTensor(epsilon)
+            a = CUDALongTensor(a)
+            b = CUDALongTensor(b)
+            delta_b = cmd(delta, b, **kwargs_)
+            a_epsilon = cmd(a, epsilon, **kwargs_)
+            delta_epsilon = cmd(delta, epsilon, **kwargs_)
+            delta_b, a_epsilon, delta_epsilon = (
+                delta_b._tensor,
+                a_epsilon._tensor,
+                delta_epsilon._tensor,
             )
-            multiprocessing_args.append(process_args)
-        p = multiprocessing.Pool()
-        partitions = p.starmap(triple_mat_mul, multiprocessing_args)
-        p.close()
-        partitions = sorted(partitions, key=lambda k: k[0])
-        delta_b = th.cat([partition[1] for partition in partitions])
-        a_epsilon = th.cat([partition[2] for partition in partitions])
-        delta_epsilon = th.cat([partition[3] for partition in partitions])
+        else:
+            batch_size = delta.shape[0]
+
+            multiprocessing_args = []
+            slice_size = math.ceil(batch_size / N_CORES)
+            for core_id in range(N_CORES):
+                process_args = (
+                    core_id,
+                    op,
+                    slice(delta, core_id, slice_size),
+                    epsilon,
+                    slice(a, core_id, slice_size),
+                    b,
+                    kwargs_,
+                )
+                multiprocessing_args.append(process_args)
+            p = multiprocessing.Pool()
+            partitions = p.starmap(triple_mat_mul, multiprocessing_args)
+            p.close()
+            partitions = sorted(partitions, key=lambda k: k[0])
+            delta_b = th.cat([partition[1] for partition in partitions])
+            a_epsilon = th.cat([partition[2] for partition in partitions])
+            delta_epsilon = th.cat([partition[3] for partition in partitions])
     else:
         cmd = getattr(th, op)
 
