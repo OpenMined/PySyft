3d05830ae82359548ed3a345edf9d70b60ccc34a
Madhava Jay <me@madhavajay.com>
2020-11-30 16:02:51 +1000

    Updated create_tenseal_ast to use new cleaner refactored methods
    - Commented out import tenseal for the lint checker

--- src/syft/lib/tenseal/__init__.py
+++ src/syft/lib/tenseal/__init__.py
@@ -4,60 +4,13 @@ from typing import List as TypeList
 from typing import Tuple as TypeTuple
 
 # third party
-import tenseal
+# import tenseal
 
 # syft relative
 from ...ast.globals import Globals
-from ...ast.klass import Class
-from ...ast.module import Module
-
-
-def get_parent(path: str, root: TypeAny) -> Module:
-    parent = root
-    for step in path.split(".")[:-1]:
-        parent = parent.attrs[step]
-    return parent
-
-
-def add_modules(ast: Globals, modules: TypeList[str]) -> None:
-    for module in modules:
-        parent = get_parent(module, ast)
-        attr_name = module.rsplit(".", 1)[-1]
-
-        parent.add_attr(
-            attr_name=attr_name,
-            attr=Module(
-                attr_name,
-                module,
-                None,
-                return_type_name="",
-            ),
-        )
-
-
-def add_classes(ast: Globals, paths: TypeList[TypeTuple[str, str, TypeAny]]) -> None:
-    for path, return_type, ref in paths:
-        parent = get_parent(path, ast)
-        attr_name = path.rsplit(".", 1)[-1]
-
-        parent.add_attr(
-            attr_name=attr_name,
-            attr=Class(
-                attr_name,
-                path,
-                ref,
-                return_type_name=return_type,
-            ),
-        )
-
-
-def add_methods(ast: Globals, paths: TypeList[TypeTuple[str, str, TypeAny]]) -> None:
-    for path, return_type, _ in paths:
-        parent = get_parent(path, ast)
-        path_list = path.split(".")
-        parent.add_path(
-            path=path_list, index=len(path_list) - 1, return_type_name=return_type
-        )
+from ...ast import add_classes
+from ...ast import add_methods
+from ...ast import add_modules
 
 
 def create_tenseal_ast() -> Globals:
@@ -67,15 +20,11 @@ def create_tenseal_ast() -> Globals:
         "tenseal",
         "tenseal.version",
     ]
-    classes = [
-    ]
+
+    classes: TypeList[TypeTuple[str, str, TypeAny]] = []
 
     methods = [
-        (
-            "tenseal.version.__version__",
-            "syft.lib.python.String",
-            tenseal.version.__version__,
-        ),
+        ("tenseal.version.__version__", "syft.lib.python.String"),
     ]
 
     add_modules(ast, modules)
