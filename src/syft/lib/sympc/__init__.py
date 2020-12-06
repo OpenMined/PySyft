7803081b9489a6f4e269ee12043df70e30423e36
George Muraru <murarugeorgec@gmail.com>
2020-12-06 15:21:15 +0200

    Add tests and remove allowlist

--- src/syft/lib/sympc/__init__.py
+++ src/syft/lib/sympc/__init__.py
@@ -1,5 +1,5 @@
 # stdlib
-from typing import Union
+from typing import Any as TypeAny
 from typing import List as TypeList
 from typing import Tuple as TypeTuple
 
@@ -8,27 +8,66 @@ import sympc
 
 # syft relative
 from . import session  # noqa: 401
+from . import share  # noqa: 401
 from ...ast import add_classes
 from ...ast import add_methods
 from ...ast import add_modules
 from ...ast.globals import Globals
 
 
-def get_return_type(support_dict: Union[str, Dict[str, str]]) -> str:
-    if isinstance(support_dict, str):
-        return support_dict
-    else:
-        return support_dict["return_type"]
-
-
 def create_sympc_ast() -> Globals:
-
     ast = Globals()
 
-    for method, return_type_name_or_dict in allowlist.items():
-        return_type = get_return_type(support_dict=return_type_name_or_dict)
-        ast.add_path(
-            path=method, framework_reference=sympc, return_type_name=return_type
-        )
+    modules: TypeList[TypeTuple[str, TypeAny]] = [
+        ("sympc", sympc),
+        ("sympc.session", sympc.session),
+        ("sympc.tensor", sympc.tensor),
+        ("sympc.protocol", sympc.protocol),
+        ("sympc.protocol.spdz", sympc.protocol.spdz),
+        ("sympc.protocol.spdz.spdz", sympc.protocol.spdz.spdz),
+    ]
+
+    classes: TypeList[TypeTuple[str, str, TypeAny]] = [
+        ("sympc.session.Session", "sympc.session.Session", sympc.session.Session),
+        (
+            "sympc.tensor.ShareTensor",
+            "sympc.tensor.ShareTensor",
+            sympc.tensor.ShareTensor,
+        ),
+    ]
+
+    methods: TypeList[TypeTuple[str, str]] = [
+        ("sympc.protocol.spdz.spdz.mul_parties", "sympc.tensor.ShareTensor"),
+        (
+            "sympc.session.Session.przs_generate_random_share",
+            "sympc.tensor.ShareTensor",
+        ),
+        (
+            "sympc.session.get_generator",
+            "torch.Generator",
+        ),
+        (
+            "sympc.tensor.ShareTensor.__add__",
+            "sympc.tensor.ShareTensor",
+        ),
+        (
+            "sympc.tensor.ShareTensor.__sub__",
+            "sympc.tensor.ShareTensor",
+        ),
+        (
+            "sympc.tensor.ShareTensor.__mul__",
+            "sympc.tensor.ShareTensor",
+        ),
+    ]
+
+    add_modules(ast, modules)
+    add_classes(ast, classes)
+    add_methods(ast, methods)
+
+    for klass in ast.classes:
+        klass.create_pointer_class()
+        klass.create_send_method()
+        klass.create_serialization_methods()
+        klass.create_storable_object_attr_convenience_methods()
 
     return ast
