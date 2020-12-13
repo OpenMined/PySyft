ae3d70b51043f76fdbabe175c9f000d9132fdcf4
Ionesio Junior <ionesiojr@gmail.com>
2020-12-13 19:00:51 -0300

    Remove unused import

--- apps/worker/src/main/core/node.py
+++ apps/worker/src/main/core/node.py
@@ -1,6 +1,7 @@
 from syft.core.node.device.device import Device
-from syft.grid.services.vm_management_service import CreateVMService
+
+# from syft.grid.services.vm_management_service import CreateVMService
 
 node = Device(name="om-device")
-node.immediate_services_with_reply.append(CreateVMService)
-node._register_services()  # re-register all services including SignalingService
+# node.immediate_services_with_reply.append(CreateVMService)
+# node._register_services()  # re-register all services including SignalingService
