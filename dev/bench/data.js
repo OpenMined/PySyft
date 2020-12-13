c93bf79760a1053784810e475b884641dd440070
github-action-benchmark <github@users.noreply.github.com>
2020-12-13 05:21:31 +0000

    add Pytest-benchmarks (pytest) benchmark result for fe71a27a745d205981e44177d092d326f7e7c1af

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1607836891913,
+  "repoUrl": "https://github.com/OpenMined/PySyft",
+  "entries": {
+    "Pytest-benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "anubhavraj.08@gmail.com",
+            "name": "Anubhav Raj Singh",
+            "username": "aanurraj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fe71a27a745d205981e44177d092d326f7e7c1af",
+          "message": "add github benchmark-actions support (#4905)\n\n* added benchmark_actions support\r\n\r\n* added requirements\r\n\r\n* fixes\r\n\r\n* fixes\r\n\r\n* yml fixes\r\n\r\n* Trying different github secrets\r\n\r\n* Trying BENCHMARK_TOKEN\r\n\r\n* Changing token path\r\n\r\n* Trying different token setting\r\n\r\n* Back to secrets.GITHUB_TOKEN\r\n\r\nCo-authored-by: Madhava Jay <me@madhavajay.com>",
+          "timestamp": "2020-12-13T15:19:24+10:00",
+          "tree_id": "4959ab2de2e0399c8254a9e7c2e571e60235e71c",
+          "url": "https://github.com/OpenMined/PySyft/commit/fe71a27a745d205981e44177d092d326f7e7c1af"
+        },
+        "date": 1607836891206,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/benchmarks/pytest_benchmarks/bench_test.py::test_string_serde",
+            "value": 1897.024760214044,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00007056849149450316",
+            "extra": "mean: 527.141248218167 usec\nrounds: 842"
+          }
+        ]
+      }
+    ]
+  }
+}
