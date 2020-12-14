baa59c12e41e3ce68c3bb1b8510b4aed0e465a77
Théo Ryffel <theo.leffyr@gmail.com>
2020-12-14 13:45:37 +0100

    Fix reqs and rm glitch

--- syft/serde/msgpack/proto.py
+++ syft/serde/msgpack/proto.py
@@ -72,18 +72,12 @@ def proto_type_info(cls):
     """
     type_name = fullname(cls)
 
-    class EmptyCode:
-        code = None
-
-    try:
-        if type_name in proto_info["TYPES"]:
-            return TypeInfo(name=type_name, obj=proto_info["TYPES"][type_name])
-        elif cls.get_msgpack_code.__qualname__.startswith(cls.__name__):
-            return TypeInfo(name=type_name, obj=cls.get_msgpack_code())
-        else:
-            raise UndefinedProtocolTypeError(
-                f"{type_name} is not defined in the protocol file and it does not provide a code by"
-                f" implementing 'get_msgpack_code'."
-            )
-    except AttributeError:
-        return EmptyCode()
+    if type_name in proto_info["TYPES"]:
+        return TypeInfo(name=type_name, obj=proto_info["TYPES"][type_name])
+    elif cls.get_msgpack_code.__qualname__.startswith(cls.__name__):
+        return TypeInfo(name=type_name, obj=cls.get_msgpack_code())
+    else:
+        raise UndefinedProtocolTypeError(
+            f"{type_name} is not defined in the protocol file and it does not provide a code by"
+            f" implementing 'get_msgpack_code'."
+        )
