b44d7aa5fcdda2a4a54e04b7bb381e99674f2c62
Ionesio Junior <ionesiojr@gmail.com>
2020-12-15 03:09:02 -0300

    Update Association request routes

--- apps/domain/src/main/routes/association_requests/routes.py
+++ apps/domain/src/main/routes/association_requests/routes.py
@@ -3,114 +3,89 @@ from flask import request, Response
 import json
 
 from syft.core.node.common.service.repr_service import ReprMessage
+from syft.grid.messages.association_messages import (
+    SendAssociationRequestMessage,
+    ReceiveAssociationRequestMessage,
+    GetAssociationRequestMessage,
+    DeleteAssociationRequestMessage,
+)
+
 from ..auth import error_handler, token_required
 
 from ...core.node import node
+from ...core.task_handler import task_handler, process_as_syft_message
+from ...core.exceptions import (
+    PyGridError,
+    UserNotFoundError,
+    RoleNotFoundError,
+    GroupNotFoundError,
+    AuthorizationError,
+    MissingRequestKeyError,
+    InvalidCredentialsError,
+)
+
+
+def route_logic(message_class):
+    # Get request body
+    content = request.get_json()
+    content = {
+        "address": node.address,
+        "content": content,
+        "reply_to": node.address,
+    }
+
+    syft_message = {}
+    syft_message["message_class"] = message_class
+    syft_message["message_content"] = content
+    syft_message[
+        "sign_key"
+    ] = node.signing_key  # TODO: Method to map token into sign-key
+
+    # Execute task
+    response_msg = task_handler(
+        route_function=process_as_syft_message,
+        data=syft_message,
+        mandatory={
+            "message_class": MissingRequestKeyError,
+            "message_content": MissingRequestKeyError,
+            "sign_key": MissingRequestKeyError,
+        },
+    )
+    return response_msg
 
 
 @association_request_route.route("/request", methods=["POST"])
 # @token_required
 def send_association_request():
-    def route_logic():
-        # Get request body
-        content = loads(request.data)
-
-        syft_message = {}
-        syft_message[
-            "message_class"
-        ] = ReprMessage  # TODO: Send Association Request Message
-        syft_message["message_content"] = content
-        syft_message[
-            "sign_key"
-        ] = node.signing_key  # TODO: Method to map token into sign-key
-
-        # Execute task
-        status_code, response_body = task_handler(
-            route_function=process_as_syft_message,
-            data=syft_message,
-            mandatory={
-                "message_class": MissingRequestKeyError,
-                "message_content": MissingRequestKeyError,
-                "sign_key": MissingRequestKeyError,
-            },
-        )
-        return response_body
-
-    # status_code, response_body = error_handler(process_as_syft_message)
-
-    status_code, response_body = 200, {"msg": "Association request sent!"}
-
+    status_code, response_msg = error_handler(
+        route_logic, SendAssociationRequestMessage
+    )
     return Response(
-        json.dumps(response_body), status=status_code, mimetype="application/json"
+        json.dumps(response_msg.content),
+        status=status_code,
+        mimetype="application/json",
     )
 
 
 @association_request_route.route("/receive", methods=["POST"])
 # @token_required
 def recv_association_request():
-    def route_logic():
-        # Get request body
-        content = loads(request.data)
-
-        syft_message = {}
-        syft_message[
-            "message_class"
-        ] = ReprMessage  # TODO: Retrieve Association Request Messages
-        syft_message["message_content"] = content
-        syft_message[
-            "sign_key"
-        ] = node.signing_key  # TODO: Method to map token into sign-key
-
-        # Execute task
-        status_code, response_body = task_handler(
-            route_function=process_as_syft_message,
-            data=syft_message,
-            mandatory={
-                "message_class": MissingRequestKeyError,
-                "message_content": MissingRequestKeyError,
-                "sign_key": MissingRequestKeyError,
-            },
-        )
-        return response_body
-
-    # status_code, response_body = error_handler(process_as_syft_message)
-
-    status_code, response_body = 200, {"msg": "Association request received!"}
-
+    status_code, response_msg = error_handler(
+        route_logic, ReceiveAssociationRequestMessage
+    )
     return Response(
-        json.dumps(response_body), status=status_code, mimetype="application/json"
+        json.dumps(response_msg.content),
+        status=status_code,
+        mimetype="application/json",
     )
 
 
 @association_request_route.route("/respond", methods=["POST"])
 # @token_required
 def reply_association_request():
-    def route_logic():
-        # Get request body
-        content = loads(request.data)
-
-        syft_message = {}
-        syft_message[
-            "message_class"
-        ] = ReprMessage  # TODO: ReplyAssociationRequestMessage
-        syft_message["message_content"] = content
-        syft_message[
-            "sign_key"
-        ] = node.signing_key  # TODO: Method to map token into sign-key
-
-        # Execute task
-        status_code, response_body = task_handler(
-            route_function=process_as_syft_message,
-            data=syft_message,
-            mandatory={
-                "message_class": MissingRequestKeyError,
-                "message_content": MissingRequestKeyError,
-                "sign_key": MissingRequestKeyError,
-            },
-        )
-        return response_body
-
-    # status_code, response_body = error_handler(process_as_syft_message)
+    # status_code, response_body = error_handler(
+    #   route_logic, ReplyAssociationRequestMessage
+    # )
 
     status_code, response_body = 200, {"msg": "Association request was replied!"}
 
@@ -122,32 +97,9 @@ def reply_association_request():
 @association_request_route.route("/", methods=["GET"])
 # @token_required
 def get_all_association_requests():
-    def route_logic():
-        # Get request body
-        content = loads(request.data)
-
-        syft_message = {}
-        syft_message[
-            "message_class"
-        ] = ReprMessage  # TODO: Get All Association Request Message
-        syft_message["message_content"] = content
-        syft_message[
-            "sign_key"
-        ] = node.signing_key  # TODO: Method to map token into sign-key
-
-        # Execute task
-        status_code, response_body = task_handler(
-            route_function=process_as_syft_message,
-            data=syft_message,
-            mandatory={
-                "message_class": MissingRequestKeyError,
-                "message_content": MissingRequestKeyError,
-                "sign_key": MissingRequestKeyError,
-            },
-        )
-        return response_body
-
-    # status_code, response_body = error_handler(process_as_syft_message)
+    # status_code, response_body = error_handler(
+    #   route_login, GetAllAssociationRequestsMessage
+    # )
 
     status_code, response_body = 200, {
         "association-requests": ["Network A", "Network B", "Network C"]
@@ -161,39 +113,9 @@ def get_all_association_requests():
 @association_request_route.route("/<association_request_id>", methods=["GET"])
 # @token_required
 def get_specific_association_requests(association_request_id):
-    def route_logic():
-        # Get request body
-        content = loads(request.data)
-
-        syft_message = {}
-        syft_message[
-            "message_class"
-        ] = ReprMessage  # TODO: Get Specific Association Request Message
-        syft_message["message_content"] = content
-        syft_message[
-            "sign_key"
-        ] = node.signing_key  # TODO: Method to map token into sign-key
-
-        # Execute task
-        status_code, response_body = task_handler(
-            route_function=process_as_syft_message,
-            data=syft_message,
-            mandatory={
-                "message_class": MissingRequestKeyError,
-                "message_content": MissingRequestKeyError,
-                "sign_key": MissingRequestKeyError,
-            },
-        )
-        return response_body
-
-    # status_code, response_body = error_handler(process_as_syft_message)
-
-    status_code, response_body = 200, {
-        "association-request": {
-            "ID": "51613546",
-            "address": "156.89.33.200",
-        }
-    }
+    status_code, response_body = error_handler(
+        route_logic, GetAssociationRequestMessage
+    )
 
     return Response(
         json.dumps(response_body), status=status_code, mimetype="application/json"
@@ -203,34 +125,9 @@ def get_specific_association_requests(association_request_id):
 @association_request_route.route("/<association_request_id>", methods=["DELETE"])
 # @token_required
 def delete_association_requests(association_request_id):
-    def route_logic():
-        # Get request body
-        content = loads(request.data)
-
-        syft_message = {}
-        syft_message[
-            "message_class"
-        ] = ReprMessage  # TODO: Delete Association Request Messages
-        syft_message["message_content"] = content
-        syft_message[
-            "sign_key"
-        ] = node.signing_key  # TODO: Method to map token into sign-key
-
-        # Execute task
-        status_code, response_body = task_handler(
-            route_function=process_as_syft_message,
-            data=syft_message,
-            mandatory={
-                "message_class": MissingRequestKeyError,
-                "message_content": MissingRequestKeyError,
-                "sign_key": MissingRequestKeyError,
-            },
-        )
-        return response_body
-
-    # status_code, response_body = error_handler(process_as_syft_message)
-
-    status_code, response_body = 200, {"msg": "Association request deleted!"}
+    status_code, response_body = error_handler(
+        route_logic, DeleteAssociationRequestMessage
+    )
 
     return Response(
         json.dumps(response_body), status=status_code, mimetype="application/json"
