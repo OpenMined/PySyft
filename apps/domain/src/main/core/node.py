073d18c567ed26cdeb6b6f81bea23dc521a15293
Ionesio Junior <ionesiojr@gmail.com>
2020-12-15 03:09:48 -0300

    Create an abstraction level over Domain

--- apps/domain/src/main/core/node.py
+++ apps/domain/src/main/core/node.py
@@ -1,18 +1,43 @@
-from syft.grid.grid_client import connect
+from typing import Optional
+
+# from syft.grid.grid_client import connect
 from syft.core.node.domain.domain import Domain
 from syft.core.node.device.client import DeviceClient
 from syft.grid.connections.http_connection import HTTPConnection
+from syft.core.io.location import SpecificLocation
+from syft.core.io.location import Location
+from .services.association_request import AssociationRequestService
 
-node = Domain(name="om-domain")
-# node.immediate_services_with_reply.append(CreateWorkerService)
-# node._register_services()  # re-register all services including SignalingService
+from nacl.signing import SigningKey
+from nacl.signing import VerifyKey
 
-try:
-    node.private_device = connect(
-        url="http://localhost:5000",  # Domain Address
-        conn_type=HTTPConnection,  # HTTP Connection Protocol
-        client_type=DeviceClient,
-    )  # Device Client type
-    node.in_memory_client_registry[node.private_device.device_id] = node.private_device
-except:
-    pass
+
+class GridDomain(Domain):
+    def __init__(
+        self,
+        name: Optional[str],
+        network: Optional[Location] = None,
+        domain: SpecificLocation = SpecificLocation(),
+        device: Optional[Location] = None,
+        vm: Optional[Location] = None,
+        signing_key: Optional[SigningKey] = None,
+        verify_key: Optional[VerifyKey] = None,
+        root_key: Optional[VerifyKey] = None,
+        db_path: Optional[str] = None,
+    ):
+        super().__init__(
+            name=name,
+            network=network,
+            domain=domain,
+            device=device,
+            vm=vm,
+            signing_key=signing_key,
+            verify_key=verify_key,
+            db_path=db_path,
+        )
+
+        self.immediate_services_with_reply.append(AssociationRequestService)
+        self._register_services()
+
+
+node = GridDomain(name="om-domain")
