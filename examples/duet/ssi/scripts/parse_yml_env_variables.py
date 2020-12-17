cdd37b887bc6b89f7ec2800f75f453a5d5415f83
Madhava Jay <me@madhavajay.com>
2020-12-17 15:03:57 +1000

    Fixed a few small issues
    - conda install jupyter was failing with a dependency conflict
      swapped for installing with pip since Jupyter 5.x and Duet have issues
    - Fixed typo in DS notebook saying to copy invite to DS instead of DO
    - Fixed issue with get_URLS.sh where extraneous ) broken token grep
      added some formatting to the code and output to make it easier to read
    - .py and yaml formatting
    - '!ENV' is breaking the yaml check

--- examples/duet/ssi/scripts/parse_yml_env_variables.py
+++ examples/duet/ssi/scripts/parse_yml_env_variables.py
@@ -9,15 +9,20 @@ import argparse
 import yaml
 from yml_env_variables import parse_config
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='YAML environment variables substitution')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="YAML environment variables substitution"
+    )
     parser.add_argument(
-        "-c", "--conf", action="store", dest="conf_file",
-        help="Path to YAML config file"
+        "-c",
+        "--conf",
+        action="store",
+        dest="conf_file",
+        help="Path to YAML config file",
     )
     args = parser.parse_args()
     print(args)
     conf = parse_config(path=args.conf_file)
     print(conf)
-    with open(r'/tmp/agent_conf.yml', 'w') as file:
+    with open(r"/tmp/agent_conf.yml", "w") as file:
         documents = yaml.dump(conf, file)
