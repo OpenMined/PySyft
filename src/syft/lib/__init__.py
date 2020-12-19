4bbc9aa89926e5faedf27a13ddf6b20ee749bce3
Alan Aboudib <agabudeeb@gmail.com>
2020-12-19 15:55:51 +0200

    Added experiment to support a toy tokenizer

--- src/syft/lib/__init__.py
+++ src/syft/lib/__init__.py
@@ -3,6 +3,7 @@ from ..ast.globals import Globals
 from ..lib.python import create_python_ast
 from ..lib.torch import create_torch_ast
 from ..lib.torchvision import create_torchvision_ast
+from ..lib.syfertext import create_syfertext_ast
 
 
 # now we need to load the relevant frameworks onto the node
@@ -11,12 +12,15 @@ def create_lib_ast() -> Globals:
     python_ast = create_python_ast()
     torch_ast = create_torch_ast()
     torchvision_ast = create_torchvision_ast()
+    syfertext_ast = create_syfertext_ast()    
     # numpy_ast = create_numpy_ast()
 
     lib_ast = Globals()
+ 
     lib_ast.add_attr(attr_name="syft", attr=python_ast.attrs["syft"])
     lib_ast.add_attr(attr_name="torch", attr=torch_ast.attrs["torch"])
     lib_ast.add_attr(attr_name="torchvision", attr=torchvision_ast.attrs["torchvision"])
+    lib_ast.add_attr(attr_name="syfertext", attr=syfertext_ast.attrs["syfertext"])    
     # lib_ast.add_attr(attr_name="numpy", attr=numpy_ast.attrs["numpy"])
 
     return lib_ast
