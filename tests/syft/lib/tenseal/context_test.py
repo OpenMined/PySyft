cf1ed21ce6a5a35a99ffc32f4682182655271622
Ayoub Benaissa <ayouben9@gmail.com>
2020-12-22 08:11:32 +0100

    TenSEAL integration (#4853)
    * accessing the version of tenseal
    
    * Updated create_tenseal_ast to use new cleaner refactored methods
    
    - Commented out import tenseal for the lint checker
    
    * Updated TenSEAL allowlist file
    
    - Added Duet Notebooks
    - Added TODOs for required support
    
    * Added ContextWrapper and changes to TenSEAL ast
    
    - Added updated TenSEAL version to requirements.txt for CI testing
    - Added generic VendorBytes protobuf
    
    * Refactored code with new TenSEAL Python API
    
    - Updated TenSEAL package version
    - Added workaround for overloading vendor .serialize()
    - Hit problem with CKKSVector deserialization requiring context
    
    * more methods for Context
    
    * test send context
    
    * test file name
    
    * Added CKKSVector proto which holds both Vector and Context
    
    - Added tests from hello world example
    - Updated notebooks
    
    * Formatting and Linting
    
    - Added example notebook output for easy reference
    
    * Updated Jupyter Notebooks with better comments
    
    - Moved computation from DO to DS side
    - WIP: finish the other examples
    
    * Added dynamic library loading for supported libraries
    
    - Syntax is: sy.load_lib("tenseal")
    - Added supported library requirements/supported_libs.txt file
    - Added tests for supported libraries
    - Refactored TenSEAL test to be a vendor supported lib test
    - WIP: Issue with: got Future attached to a different loop
    
    * Added 0.4 to the GitHub Actions CI
    
    * Nit Cleanup
    
    Co-authored-by: Madhava Jay <me@madhavajay.com>

---
+++ tests/syft/lib/tenseal/context_test.py
@@ -0,0 +1,27 @@
+# third party
+import pytest
+
+# syft absolute
+import syft as sy
+
+
+@pytest.mark.vendor(lib="tenseal")
+def test_context_send() -> None:
+    # third party
+    import tenseal as ts
+
+    """Test sending a TenSEAL context"""
+
+    alice = sy.VirtualMachine(name="alice")
+    alice_client = alice.get_client()
+
+    assert len(alice.store) == 0
+
+    ctx = ts.context(ts.SCHEME_TYPE.CKKS, 8192, 0, [40, 20, 40])
+    ctx.global_scale = 2 ** 40
+
+    sy.load_lib("tenseal")
+
+    _ = ctx.tag("context").send(alice_client)
+
+    assert len(alice.store) == 1
