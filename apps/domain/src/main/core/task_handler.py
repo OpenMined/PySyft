8f591c883abc0e334b41255cdbd07ea1114c76b3
Ionesio Junior <ionesiojr@gmail.com>
2021-01-04 09:04:27 -0300

    Move generic route_logic function to task_handler module

--- apps/domain/src/main/core/task_handler.py
+++ apps/domain/src/main/core/task_handler.py
@@ -1,10 +1,14 @@
 from .codes import RESPONSE_MSG
 from json.decoder import JSONDecodeError
 from flask_executor import Executor
-
+from flask import request
 
 from syft.core.common.message import SignedImmediateSyftMessageWithReply
 from syft.core.common.message import SignedImmediateSyftMessageWithoutReply
+
+# third party
+from nacl.encoding import HexEncoder
+from nacl.signing import SigningKey
 
 from .node import node
 from .exceptions import (
@@ -64,3 +68,30 @@ def task_handler(route_function, data, mandatory, optional=[]):
     response_body = route_function(**args)
 
     return response_body
+
+
+def route_logic(message_class, current_user, content):
+    user_key = SigningKey(current_user.private_key.encode("utf-8"), encoder=HexEncoder)
+
+    content = {
+        "address": node.address,
+        "content": content,
+        "reply_to": node.address,
+    }
+
+    syft_message = {}
+    syft_message["message_class"] = message_class
+    syft_message["message_content"] = content
+    syft_message["sign_key"] = user_key
+
+    # Execute task
+    response_msg = task_handler(
+        route_function=process_as_syft_message,
+        data=syft_message,
+        mandatory={
+            "message_class": MissingRequestKeyError,
+            "message_content": MissingRequestKeyError,
+            "sign_key": MissingRequestKeyError,
+        },
+    )
+    return response_msg
