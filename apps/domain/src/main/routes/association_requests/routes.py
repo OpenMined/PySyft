9c14e7589e5df1c17626b96ed14227ab8b8ad68a
Ionesio Junior <ionesiojr@gmail.com>
2021-01-04 09:05:06 -0300

    Remote unused imports / Move route logic to task_handler module

--- apps/domain/src/main/routes/association_requests/routes.py
+++ apps/domain/src/main/routes/association_requests/routes.py
@@ -2,7 +2,6 @@ from .blueprint import association_requests_blueprint as association_request_rou
 from flask import request, Response
 import json
 
-from syft.core.node.common.service.repr_service import ReprMessage
 from syft.grid.messages.association_messages import (
     SendAssociationRequestMessage,
     ReceiveAssociationRequestMessage,
@@ -13,60 +12,23 @@ from syft.grid.messages.association_messages import (
 )
 
 from ..auth import error_handler, token_required
-
-# third party
-from nacl.encoding import HexEncoder
-from nacl.signing import SigningKey
-
-from ...core.node import node
-from ...core.task_handler import task_handler, process_as_syft_message
-from ...core.exceptions import (
-    PyGridError,
-    UserNotFoundError,
-    RoleNotFoundError,
-    GroupNotFoundError,
-    AuthorizationError,
-    MissingRequestKeyError,
-    InvalidCredentialsError,
-)
-
-
-def route_logic(message_class, current_user):
-    user_key = SigningKey(current_user.private_key.encode("utf-8"), encoder=HexEncoder)
-
-    # Get request body
-    content = request.get_json()
-    content = {
-        "address": node.address,
-        "content": content,
-        "reply_to": node.address,
-    }
-
-    syft_message = {}
-    syft_message["message_class"] = message_class
-    syft_message["message_content"] = content
-    syft_message["sign_key"] = user_key
-
-    # Execute task
-    response_msg = task_handler(
-        route_function=process_as_syft_message,
-        data=syft_message,
-        mandatory={
-            "message_class": MissingRequestKeyError,
-            "message_content": MissingRequestKeyError,
-            "sign_key": MissingRequestKeyError,
-        },
-    )
-    return response_msg
+from ...core.task_handler import route_logic
 
 
 @association_request_route.route("/request", methods=["POST"])
 @token_required
 def send_association_request(current_user):
+    # Get request body
+    content = request.get_json()
+    if not content:
+        content = {}
+
     status_code, response_msg = error_handler(
-        route_logic, SendAssociationRequestMessage, current_user
+        route_logic, SendAssociationRequestMessage, current_user, content
     )
+
     response = response_msg if isinstance(response_msg, dict) else response_msg.content
+
     return Response(
         json.dumps(response),
         status=status_code,
@@ -77,8 +39,13 @@ def send_association_request(current_user):
 @association_request_route.route("/receive", methods=["POST"])
 @token_required
 def recv_association_request(current_user):
+    # Get request body
+    content = request.get_json()
+    if not content:
+        content = {}
+
     status_code, response_msg = error_handler(
-        route_logic, ReceiveAssociationRequestMessage, current_user
+        route_logic, ReceiveAssociationRequestMessage, current_user, content
     )
 
     response = response_msg if isinstance(response_msg, dict) else response_msg.content
@@ -93,8 +60,13 @@ def recv_association_request(current_user):
 @association_request_route.route("/respond", methods=["POST"])
 @token_required
 def reply_association_request(current_user):
+    # Get request body
+    content = request.get_json()
+    if not content:
+        content = {}
+
     status_code, response_msg = error_handler(
-        route_logic, RespondAssociationRequestMessage, current_user
+        route_logic, RespondAssociationRequestMessage, current_user, content
     )
 
     response = response_msg if isinstance(response_msg, dict) else response_msg.content
@@ -109,8 +81,13 @@ def reply_association_request(current_user):
 @association_request_route.route("/", methods=["GET"])
 @token_required
 def get_all_association_requests(current_user):
+    # Get request body
+    content = request.get_json()
+    if not content:
+        content = {}
+
     status_code, response_msg = error_handler(
-        route_logic, GetAllAssociationRequestMessage, current_user
+        route_logic, GetAllAssociationRequestMessage, current_user, content
     )
 
     response = response_msg if isinstance(response_msg, dict) else response_msg.content
@@ -125,8 +102,16 @@ def get_all_association_requests(current_user):
 @association_request_route.route("/<association_request_id>", methods=["GET"])
 @token_required
 def get_specific_association_requests(current_user, association_request_id):
+    # Get request body
+    content = request.get_json()
+    if not content:
+        content = {}
+
+    # ADD path parameters
+    content["association_request_id"] = association_request_id
+
     status_code, response_msg = error_handler(
-        route_logic, GetAssociationRequestMessage, current_user
+        route_logic, GetAssociationRequestMessage, current_user, content
     )
 
     response = response_msg if isinstance(response_msg, dict) else response_msg.content
@@ -141,8 +126,16 @@ def get_specific_association_requests(current_user, association_request_id):
 @association_request_route.route("/<association_request_id>", methods=["DELETE"])
 @token_required
 def delete_association_requests(current_user, association_request_id):
+    # Get request body
+    content = request.get_json()
+    if not content:
+        content = {}
+
+    # ADD path parameters
+    content["association_request_id"] = association_request_id
+
     status_code, response_msg = error_handler(
-        route_logic, DeleteAssociationRequestMessage, current_user
+        route_logic, DeleteAssociationRequestMessage, current_user, content
     )
 
     response = response_msg if isinstance(response_msg, dict) else response_msg.content
