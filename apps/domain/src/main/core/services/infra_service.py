3a5304c3e2df9472b7555f283930af7ed959482b
Ionesio Junior <ionesiojr@gmail.com>
2021-01-04 10:46:31 -0300

    ADD TODO / Guide comments

--- apps/domain/src/main/core/services/infra_service.py
+++ apps/domain/src/main/core/services/infra_service.py
@@ -35,59 +35,117 @@ from syft.grid.messages.infra_messages import (
 @syft_decorator(typechecking=True)
 def create_worker_msg(
     msg: CreateWorkerMessage,
+    node: AbstractNode,
 ) -> CreateWorkerResponse:
-    return CreateWorkerResponse(
-        address=msg.reply_to,
-        success=True,
-        content={"msg": "Worker created succesfully!"},
-    )
+    try:
+        # TODO:
+        # 1 - Deploy a Worker into the cloud using the parameters in msg.content
+        # 2 - Save worker adress/metadata at node.workers
+
+        final_msg = "Worker created succesfully!"
+        success = True
+
+        return CreateWorkerResponse(
+            address=msg.reply_to,
+            success=success,
+            content={"msg": final_msg},
+        )
+    except Exception as e:
+        return CreateWorkerResponse(
+            address=msg.reply_to,
+            success=False,
+            content={"error": str(e)},
+        )
 
 
 @syft_decorator(typechecking=True)
 def check_worker_deployment_msg(
     msg: CheckWorkerDeploymentMessage,
+    node: AbstractNode,
 ) -> CheckWorkerDeploymentResponse:
-    return CheckWorkerDeploymentMessage(
-        address=msg.reply_to,
-        success=True,
-        content={"msg": "Association request received!"},
-    )
+    try:
+        # TODO:
+        # Check Cloud Deployment progress
+        # PS: msg.content is optional, but can be used to map different cloud deployment processes
+        final_msg = {}  # All data about deployment progress.
+        final_status = True
+
+        return CheckWorkerDeploymentMessage(
+            address=msg.reply_to,
+            success=final_status,
+            content={"deployment_status": final_msg},
+        )
+    except Exception as e:
+        return CheckWorkerDeploymentMessage(
+            address=msg.reply_to,
+            success=False,
+            content={"error": str(e)},
+        )
 
 
 @syft_decorator(typechecking=True)
 def get_worker_msg(
     msg: GetWorkerMessage,
+    node: AbstractNode,
 ) -> GetWorkerResponse:
-    return GetWorkerResponse(
-        address=msg.reply_to,
-        success=True,
-        content={
+    try:
+        # TODO:
+        # final_msg = node.workers[msg.content["worker_id"]]
+
+        final_msg = {
             "worker": {"id": "9846165", "address": "159.156.128.165", "datasets": 25320}
-        },
-    )
+        }
+        final_status = True
+
+        return GetWorkerResponse(
+            address=msg.reply_to,
+            success=final_status,
+            content=final_msg,
+        )
+    except Exception as e:
+        return CheckWorkerDeploymentMessage(
+            address=msg.reply_to,
+            success=False,
+            content={"error": str(e)},
+        )
 
 
 @syft_decorator(typechecking=True)
 def get_workers_msg(
     msg: GetWorkersMessage,
+    node: AbstractNode,
 ) -> GetWorkersResponse:
-    return GetWorkersResponse(
-        address=msg.reply_to,
-        success=True,
-        content={
+    try:
+        # TODO:
+        # final_msg = node.workers
+
+        final_msg = {
             "workers": [
                 {"id": "546513231a", "address": "159.156.128.165", "datasets": 25320},
                 {"id": "asfa16f5aa", "address": "138.142.125.125", "datasets": 2530},
                 {"id": "af61ea3a3f", "address": "19.16.98.146", "datasets": 2320},
                 {"id": "af4a51adas", "address": "15.59.18.165", "datasets": 5320},
             ]
-        },
-    )
+        }
+        final_status = True
+
+        return GetWorkersResponse(
+            address=msg.reply_to,
+            success=final_status,
+            content=final_msg,
+        )
+    except Exception as e:
+        return GetWorkersResponse(
+            address=msg.reply_to,
+            success=False,
+            content={"error": str(e)},
+        )
 
 
 @syft_decorator(typechecking=True)
 def del_worker_msg(
     msg: DeleteWorkerMessage,
+    node: AbstractNode,
 ) -> DeleteWorkerResponse:
     return DeleteWorkerResponse(
         address=msg.reply_to,
@@ -99,6 +157,7 @@ def del_worker_msg(
 @syft_decorator(typechecking=True)
 def update_worker_msg(
     msg: UpdateWorkerMessage,
+    node: AbstractNode,
 ) -> UpdateWorkerResponse:
     return UpdateWorkerResponse(
         address=msg.reply_to,
@@ -139,7 +198,9 @@ class DomainInfrastructureService(ImmediateNodeServiceWithReply):
         GetWorkersResponse,
         DeleteWorkerResponse,
     ]:
-        return DomainInfrastructureService.msg_handler_map[type(msg)](msg=msg)
+        return DomainInfrastructureService.msg_handler_map[type(msg)](
+            msg=msg, node=node
+        )
 
     @staticmethod
     def message_handler_types() -> List[Type[ImmediateSyftMessageWithReply]]:
