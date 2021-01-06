7a2fe47edc033dfa2232a58edc710b0c339db061
Madhava Jay <me@madhavajay.com>
2021-01-05 19:23:11 -0800

    Opacus DP Support (#4935)
    * Fixed Sample Code (#4923)
    
    * removed actions for pr (#4928)
    
    * WIP: Added initial support for Opacus
    
    - Added create_ast for Opacus
    - Added dp opacus MNIST example notebooks
    - Added grad_sample to Parameters proto and serde
    - Modified run_class_method_action to resolve methods at execution
      WIP: This only works for some methods, we need to investigate
    
    * [WIP] opacus dynamic loading
    
    - a test function is added to verify that the dynamic loading is good
    - TO FIX: some tests/syft/lib/python/xxx/pointer_test.py fail
    
    * Refactored Opacus work around to be cleaner
    
    - Added UnionGenerator with common types to Iterator.__next__
    - Fixed issue in Iterator with self._index existing for StopIteration
    
    * Added torch={"min_version": "1.6.0"} to opacus test
    
    * Added python={"min_version": (3, 7)} to opacus
    
    * Fixed opacus minimum requirement
    
    - Created PR to support windows 3.6.8 https://github.com/pytorch/opacus/pull/108
    - Updated pip cache in GitHub Actions
    
    * Re-added torch==1.6.0 opacus requirement until bug fix on Opacus side
    
    Co-authored-by: rajatrc1705 <52173002+rajatrc1705@users.noreply.github.com>
    Co-authored-by: Anubhav Raj Singh <anubhavraj.08@gmail.com>
    Co-authored-by: xutongye <xvtongye1986@163.com>

--- src/syft/__init__.py
+++ src/syft/__init__.py
@@ -64,6 +64,7 @@ from syft.core.node.vm.vm import VirtualMachineClient  # noqa: F401
 
 # Convenience Functions
 from syft.decorators import type_hints  # noqa: F401
+from syft.grid.duet import bcolors  # noqa: F401
 from syft.grid.duet import duet  # noqa: F401
 from syft.grid.duet import join_duet  # noqa: F401
 from syft.grid.duet import launch_duet  # noqa: F401
@@ -72,7 +73,6 @@ from syft.grid.duet import launch_duet  # noqa: F401
 from syft.lib import lib_ast  # noqa: F401
 from syft.lib import load_lib  # noqa: F401
 from syft.lib.torch.module import Module  # noqa: F401
-from syft.grid.duet import bcolors  # noqa: F401
 
 # syft relative
 # Package Imports
