34e1ab97ccb8d98e88162dfa3b7d322b826e4d3f
Cebere Bogdan <bogdan.cebere@gmail.com>
2021-01-12 16:49:16 +0200

    debug windows use pathos.multiprocess

--- tests/syft/grid/duet/duet_test.py
+++ tests/syft/grid/duet/duet_test.py
@@ -1,50 +1,27 @@
 # stdlib
-from multiprocessing import Barrier
-from multiprocessing import Pipe
-from multiprocessing import Process
+from multiprocessing import Manager
+from pathos.multiprocessing import ProcessPool
 import socket
 from time import sleep
-import traceback
-from typing import Any
 from typing import Callable
 from typing import List
-from typing import Optional
 from typing import Tuple
 
 # syft relative
 from .duet_scenarios_tests import register_duet_scenarios
 from .signaling_server_test import run
 
+pool = ProcessPool(nodes=4)
+
 port = 21000
-grid_proc = Process(target=run, args=(port,))
-grid_proc.start()
+pool.amap(run, [port])
 
 registered_tests: List[Tuple[Callable, Callable]] = []
 register_duet_scenarios(registered_tests)
 
 
-class SyftTestProcess(Process):
-    def __init__(self, *args: Any, **kwargs: Any):
-        Process.__init__(self, *args, **kwargs)
-        self._pconn, self._cconn = Pipe()
-        self._exception = None
-
-    def run(self) -> None:
-        try:
-            Process.run(self)
-            self._cconn.send(None)
-        except Exception as e:
-            tb = traceback.format_exc()
-            self._cconn.send((e, tb))
-
-    @property
-    def exception(self) -> Optional[tuple]:
-        if self._pconn.poll():
-            self._exception = self._pconn.recv()
-        return self._exception
-
-
 def test_duet() -> None:
+    global pool
     # let the flask server init:
     sleep(5)
 
@@ -52,24 +29,15 @@ def test_duet() -> None:
         assert s.connect_ex(("localhost", port)) == 0
 
     for do, ds in registered_tests:
-        barrier = Barrier(2, action=lambda: barrier.reset())
+        mgr = Manager()
+        barrier = mgr.Barrier(2)  # type: ignore
 
-        do_proc = SyftTestProcess(target=do, args=(barrier, port))
-        do_proc.start()
+        do_proc = pool.apipe(do, barrier, port)
+        ds_proc = pool.apipe(ds, barrier, port)
 
-        ds_proc = SyftTestProcess(target=ds, args=(barrier, port))
-        ds_proc.start()
+        do_proc.get()
+        ds_proc.get()
 
-        do_proc.join()
-        ds_proc.join()
-
-        if do_proc.exception:
-            exception, tb = do_proc.exception
-            raise Exception(tb) from exception
-
-        if ds_proc.exception:
-            exception, tb = ds_proc.exception
-            raise Exception(tb) from exception
-
-    grid_proc.terminate()
-    grid_proc.join()
+    pool.close()
+    pool.terminate()
+    pool.join()
