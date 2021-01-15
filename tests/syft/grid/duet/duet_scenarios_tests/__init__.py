195bfc915760c9d08761739971dbb43c91faf8b4
Cebere Bogdan <bogdan.cebere@gmail.com>
2021-01-15 21:12:10 +0200

    drop sanity test until Duet works

--- tests/syft/grid/duet/duet_scenarios_tests/__init__.py
+++ tests/syft/grid/duet/duet_scenarios_tests/__init__.py
@@ -6,9 +6,6 @@ from typing import Tuple
 # syft relative
 from .duet_init_test import test_scenario_init
 
-# from .duet_sanity_test import test_scenario_sanity
-
 
 def register_duet_scenarios(registered_tests: List[Tuple[Callable, Callable]]) -> None:
     registered_tests.append(test_scenario_init)
-    # registered_tests.append(test_scenario_sanity)
