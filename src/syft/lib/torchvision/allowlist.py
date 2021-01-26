0277dfc59a53705a499005d0bd897a9a2010634b
syumeikoukyo <kankankan0818@hotmail.com>
2021-01-26 09:15:32 +0900

    modify allowlist.py from #5032

--- src/syft/lib/torchvision/allowlist.py
+++ src/syft/lib/torchvision/allowlist.py
@@ -11,7 +11,6 @@ allowlist["torchvision.transforms.Compose"] = "torchvision.transforms.Compose"
 # allowlist["torchvision.transforms.Compose.transforms"] = "syft.lib.python.List"
 allowlist["torchvision.transforms.ToTensor"] = "torchvision.transforms.ToTensor"
 allowlist["torchvision.transforms.Normalize"] = "torchvision.transforms.Normalize"
-# TODO: Normalize properties only exists on the object not on the class?
 # allowlist["torchvision.transforms.Normalize.inplace"] = "syft.lib.python.Bool"
 # TODO: mean and std are actually tuples
 # allowlist["torchvision.transforms.Normalize.mean"] = "syft.lib.python.List"
