3ea1cae5ca66643b00767e51ea57d3bd8cf640e6
Ionesio Junior <ionesiojr@gmail.com>
2021-01-27 04:10:34 -0300

    ADD PyGrid's Infrastructure Message tests

---
+++ tests/syft/grid/messages/infra_msg_test.py
@@ -0,0 +1,245 @@
+# syft absolute
+import syft as sy
+from syft.core.io.address import Address
+from syft.grid.messages.infra_messages import CreateWorkerMessage
+from syft.grid.messages.infra_messages import CreateWorkerResponse
+from syft.grid.messages.infra_messages import DeleteWorkerMessage
+from syft.grid.messages.infra_messages import DeleteWorkerResponse
+from syft.grid.messages.infra_messages import GetWorkerMessage
+from syft.grid.messages.infra_messages import GetWorkerResponse
+from syft.grid.messages.infra_messages import GetWorkersMessage
+from syft.grid.messages.infra_messages import GetWorkersResponse
+from syft.grid.messages.infra_messages import UpdateWorkerMessage
+from syft.grid.messages.infra_messages import UpdateWorkerResponse
+
+
+def test_create_worker_message_serde() -> None:
+    bob_vm = sy.VirtualMachine(name="Bob")
+    target = Address(name="Alice")
+
+    request_content = { 
+            "settings": { 
+                "instance-size" : "t4g.medium",
+                "vCPU": "2",
+                "network-bandwith": "5Gbps",
+                "vGPU": True, 
+            }
+    }
+    msg = CreateWorkerMessage(
+        address=target,
+        content= request_content,
+        reply_to=bob_vm.address,
+    )
+
+    blob = msg.serialize()
+    msg2 = sy.deserialize(blob=blob)
+
+    assert msg.id == msg2.id
+    assert msg.address == target
+    assert msg.content == msg2.content
+    assert msg == msg2
+
+def test_create_worker_response_serde() -> None:
+    target = Address(name="Alice")
+
+    request_content = {"msg": "Worker Environment Created Successfully!"}
+    msg = CreateWorkerResponse(
+        address=target,
+        success=True,
+        content= request_content,
+    )
+
+    blob = msg.serialize()
+    msg2 = sy.deserialize(blob=blob)
+
+    assert msg.id == msg2.id
+    assert msg.address == target
+    assert msg.content == msg2.content
+    assert msg == msg2
+
+def test_delete_worker_message_serde() -> None:
+    bob_vm = sy.VirtualMachine(name="Bob")
+    target = Address(name="Alice")
+
+    request_content = {"worker_id": "f2a6as5d16fasd"}
+    msg = DeleteWorkerMessage(
+        address=target,
+        content=request_content,
+        reply_to=bob_vm.address,
+    )
+
+    blob = msg.serialize()
+    msg2 = sy.deserialize(blob=blob)
+
+    assert msg.id == msg2.id
+    assert msg.address == target
+    assert msg.content == msg2.content
+    assert msg == msg2
+
+def test_delete_worker_response_serde() -> None:
+    target = Address(name="Alice")
+
+    content = {"msg": "Worker Environment deleted Successfully!"}
+    msg = DeleteWorkerResponse(
+        success=True,
+        address=target,
+        content=content,
+    )
+
+    blob = msg.serialize()
+    msg2 = sy.deserialize(blob=blob)
+
+    assert msg.id == msg2.id
+    assert msg.address == target
+    assert msg.content == msg2.content
+    assert msg == msg2
+
+
+def test_update_worker_message_serde() -> None:
+    bob_vm = sy.VirtualMachine(name="Bob")
+    target = Address(name="Alice")
+
+    content = {
+            "worker-id" : "eqw9e4a5d846",
+            "settings": { 
+                "instance-size" : "t4g.large",
+                "vCPU": "2",
+                "network-bandwith": "5Gbps",
+                "vGPU": True, 
+            }
+    }
+    msg = UpdateWorkerMessage(
+        address=target,
+        content=content,
+        reply_to=bob_vm.address,
+    )
+
+    blob = msg.serialize()
+    msg2 = sy.deserialize(blob=blob)
+
+    assert msg.id == msg2.id
+    assert msg.address == target
+    assert msg.content == msg2.content
+    assert msg == msg2
+
+
+def test_update_worker_response_serde() -> None:
+    target = Address(name="Alice")
+
+
+    request_content = {"msg": "Worker Environment updated successfully!"}
+    msg = UpdateWorkerResponse(
+        address=target,
+        success=True,
+        content=request_content,
+    )
+
+    blob = msg.serialize()
+    msg2 = sy.deserialize(blob=blob)
+
+    assert msg.id == msg2.id
+    assert msg.address == target
+    assert msg.content == msg2.content
+    assert msg == msg2
+
+
+def test_get_worker_message_serde() -> None:
+    bob_vm = sy.VirtualMachine(name="Bob")
+    target = Address(name="Alice")
+
+
+    content = { "worker-id" : "eqw9e4a5d846" }
+    msg = GetWorkerMessage(
+        address=target,
+        content=content,
+        reply_to=bob_vm.address,
+    )
+
+    blob = msg.serialize()
+    msg2 = sy.deserialize(blob=blob)
+
+    assert msg.id == msg2.id
+    assert msg.address == target
+    assert msg.content == msg2.content
+    assert msg == msg2
+
+
+def test_get_worker_response_serde() -> None:
+    target = Address(name="Alice")
+
+
+    content = {
+            "worker-id" : "eqw9e4a5d846",
+            "environment-name": "Heart Diseases Environment", 
+            "owner": "user-id7",
+            "deployment-date": "05/12/2021",
+    }
+    
+    msg = GetWorkerResponse(
+        address=target,
+        success=True,
+        content=content,
+    )
+
+    blob = msg.serialize()
+    msg2 = sy.deserialize(blob=blob)
+
+    assert msg.id == msg2.id
+    assert msg.address == target
+    assert msg.content == msg2.content
+    assert msg == msg2
+
+def test_get_all_workers_message_serde() -> None:
+    bob_vm = sy.VirtualMachine(name="Bob")
+    target = Address(name="Alice")
+
+
+    content = {}
+    msg = GetWorkersMessage(
+        address=target,
+        content=content,
+        reply_to=bob_vm.address,
+    )
+
+    blob = msg.serialize()
+    msg2 = sy.deserialize(blob=blob)
+
+    assert msg.id == msg2.id
+    assert msg.address == target
+    assert msg.content == msg2.content
+    assert msg == msg2
+
+
+def test_get_all_workers_response_serde() -> None:
+    target = Address(name="Alice")
+
+
+    request_content = { "workers": 
+        {
+            "626sadaf631": {
+                "environment-name": "Heart Diseases Environment", 
+                "owner": "user-id7",
+                "deployment-date": "05/12/2021",
+            },
+            "a84ew64wq6e": {
+                "worker-id" : "eqw9e4a5d846",
+                "environment-name": "Brain Diseases Environment", 
+                "owner": "user-id8",
+                "deployment-date": "15/12/2021",
+            },
+        }
+    }
+
+    msg = GetWorkersResponse(
+        address=target,
+        success=True,
+        content=request_content,
+    )
+
+    blob = msg.serialize()
+    msg2 = sy.deserialize(blob=blob)
+
+    assert msg.id == msg2.id
+    assert msg.address == target
+    assert msg.content == msg2.content
+    assert msg == msg2
