2e45291ddf229487ffdfed869c3b9bf80dc0c4ca
Cebere Bogdan <bogdan.cebere@gmail.com>
2021-01-30 19:20:54 +0200

    reactivate iadd test

--- tests/syft/lib/tenseal/tenseal_ckkstensor_test.py
+++ tests/syft/lib/tenseal/tenseal_ckkstensor_test.py
@@ -139,7 +139,6 @@ def test_tenseal_ckkstensor_mul(context: Any, duet: sy.VirtualMachine) -> None:
     _almost_equal(result, expected)
 
 
-@pytest.mark.skip
 @pytest.mark.vendor(lib="tenseal")
 def test_tenseal_ckkstensor_iadd(context: Any, duet: sy.VirtualMachine) -> None:
     v1 = [0, 1, 2, 3, 4]
