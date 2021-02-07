e2af7133d1a021e05e7a7c4d377015fe57f9ab71
George Muraru <murarugeorgec@gmail.com>
2021-02-07 19:24:47 +0200

    Only raise AttributeError in __getattribute__

--- src/syft/ast/klass.py
+++ src/syft/ast/klass.py
@@ -27,6 +27,7 @@ from ..core.node.common.action.save_object_action import SaveObjectAction
 from ..core.pointer.pointer import Pointer
 from ..logger import critical
 from ..logger import traceback_and_raise
+from ..logger import warning
 from ..util import aggressive_set_attr
 
 
@@ -457,10 +458,17 @@ class Class(Callable):
                 return target_object_ptr
 
             return target_object
+        except AttributeError as e:
+            warning(
+                "__getattribute__ failed. If you are trying to access an EnumAttribute or a "
+                "StaticAttribute, be sure they have been added to the AST. Falling back on "
+                "__getattr__ to search in self.attrs for the requested field."
+            )
+            raise e
         except Exception as e:
             critical(
                 "__getattribute__ failed. If you are trying to access an EnumAttribute or a "
-                "StaticAttribute, be sure they have been added to the AST. Falling back on"
+                "StaticAttribute, be sure they have been added to the AST. Falling back on "
                 "__getattr__ to search in self.attrs for the requested field."
             )
             traceback_and_raise(e)
