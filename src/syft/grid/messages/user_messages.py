4e1e1279ad013b3cceec507c51490984dbd5fe30
Ionesio Junior <ionesiojr@gmail.com>
2021-02-09 11:31:14 -0300

    fix code style

--- src/syft/grid/messages/user_messages.py
+++ src/syft/grid/messages/user_messages.py
@@ -766,6 +766,7 @@ class DeleteUserResponse(ImmediateSyftMessageWithoutReply):
 
         return DeleteUserResponse_PB
 
+
 @final
 class SearchUsersMessage(ImmediateSyftMessageWithReply):
     def __init__(
