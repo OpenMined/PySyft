52712074865d476a6632a9a1865a21c00fd05cfa
Madhava Jay <me@madhavajay.com>
2021-02-20 13:08:06 +1000

    Fixed adjust_torch_versions
    - Added torchcsprng skip for python 3.9
    - Added +cpu for Linux and Windows
    - Added -f torch_stable.html repo

--- .github/adjust_torch_versions.py
+++ .github/adjust_torch_versions.py
@@ -1,19 +1,22 @@
 # stdlib
 import os
+import platform
 import re
 import sys
 from typing import Any
 from typing import Dict
 
-VERSIONS_NONE: Dict[str, Any] = dict(torchvision=None, torchsprng=None)
+VERSIONS_NONE: Dict[str, Any] = dict(torchvision=None, torchcsprng=None)
 VERSIONS_LUT: Dict[str, Dict[str, Any]] = {
-    "1.4.0": dict(torchvision="0.5.0", torchsprng=None),
-    "1.5.0": dict(torchvision="0.6.0", torchsprng=None),
-    "1.5.1": dict(torchvision="0.6.1", torchsprng=None),
-    "1.6.0": dict(torchvision="0.7", torchsprng="0.1.2"),
-    "1.7.0": dict(torchvision="0.8.1", torchsprng="0.1.3"),
-    "1.7.1": dict(torchvision="0.8.2", torchsprng="0.1.4"),
+    "1.4.0": dict(torchvision="0.5.0", torchcsprng=None),
+    "1.5.0": dict(torchvision="0.6.0", torchcsprng=None),
+    "1.5.1": dict(torchvision="0.6.1", torchcsprng=None),
+    "1.6.0": dict(torchvision="0.7", torchcsprng="0.1.2"),
+    "1.7.0": dict(torchvision="0.8.1", torchcsprng="0.1.3"),
+    "1.7.1": dict(torchvision="0.8.2", torchcsprng="0.1.4"),
 }
+
+system = platform.system()
 
 
 def main(path_req: str, torch_version: str) -> None:
@@ -22,15 +25,22 @@ def main(path_req: str, torch_version: str) -> None:
 
     dep_versions = VERSIONS_LUT.get(torch_version, VERSIONS_NONE)
     dep_versions["torch"] = torch_version
+    if system != "Darwin":
+        dep_versions["torch"] += "+cpu"  # linux and windows require +cpu
     for lib in dep_versions:
+        replace = ""
         if dep_versions[lib]:
-            req = re.sub(
-                rf"{lib}[>=]*[\d\.]*{os.linesep}",
-                f"{lib}=={dep_versions[lib]}{os.linesep}",
-                req,
-            )
+            replace = f"{lib}=={dep_versions[lib]}{os.linesep}"
 
-    print(torch_version)
+        if lib == "torchcsprng" and sys.version_info >= (3, 9):
+            replace = ""  # no torchcsprng for python 3.9 yet
+
+        req = re.sub(
+            rf"{lib}[>=]*[\d\.]*{os.linesep}",
+            replace,
+            req,
+        )
+
     with open(path_req, "w") as fp:
         fp.write(req)
 
