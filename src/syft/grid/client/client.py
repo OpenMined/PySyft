8ce9df88727f9a9796d82ccab27b0cfca1cbf3b0
Ionesio Junior <ionesiojr@gmail.com>
2021-03-01 05:34:47 -0300

    Update Grid Client - ADD typing hints - Remove unused imports - Remove unused parameters

--- src/syft/grid/client/client.py
+++ src/syft/grid/client/client.py
@@ -98,12 +98,12 @@ def connect(
         def unproxy(self) -> None:
             self.proxy_address = None
 
-        def initial_setup(self, **kwargs) -> Any:
+        def initial_setup(self, **kwargs: Any) -> Any:
             return self.__perform_grid_request(
                 grid_msg=CreateInitialSetUpMessage, content=kwargs
             )
 
-        def get_setup(self, **kwargs) -> Any:
+        def get_setup(self, **kwargs: Any) -> Any:
             return self.__perform_grid_request(grid_msg=GetSetUpMessage, content=kwargs)
 
         def send_immediate_msg_with_reply(
@@ -112,10 +112,9 @@ def connect(
                 SignedImmediateSyftMessageWithReply, ImmediateSyftMessageWithReply
             ],
             route_index: int = 0,
-            address: Address = None,
         ) -> SyftMessage:
 
-            if address:
+            if self.proxy_address:
                 msg.address = self.proxy_address
 
             return super(GridClient, self).send_immediate_msg_with_reply(
@@ -128,9 +127,8 @@ def connect(
                 SignedImmediateSyftMessageWithoutReply, ImmediateSyftMessageWithoutReply
             ],
             route_index: int = 0,
-            address: Address = None,
         ) -> None:
-            if address:
+            if self.proxy_address:
                 msg.address = self.proxy_address
 
             return super(GridClient, self).send_immediate_msg_without_reply(
@@ -141,9 +139,8 @@ def connect(
             self,
             msg: EventualSyftMessageWithoutReply,
             route_index: int = 0,
-            address: Address = None,
         ) -> None:
-            if address:
+            if self.proxy_address:
                 msg.address = self.proxy_address
 
             return super(GridClient, self).send_eventual_msg_without_reply(
@@ -163,13 +160,13 @@ def connect(
             return locations
 
         def __perform_grid_request(
-            self, grid_msg: Any, content: Dict[Any, Any] = None
+            self, grid_msg: Any, content: Dict[Any, Any] = {}
         ) -> Dict[Any, Any]:
             signed_msg = self.__build_msg(grid_msg=grid_msg, content=content)
             response = self.send_immediate_msg_with_reply(msg=signed_msg)
             return self.__process_response(response=response)
 
-        def __build_msg(self, grid_msg: Any, content=Dict[Any, Any]) -> Any:
+        def __build_msg(self, grid_msg: Any, content: Dict[Any, Any]) -> Any:
             args = {
                 "address": self.address,
                 "content": content,
@@ -177,11 +174,11 @@ def connect(
             }
             return grid_msg(**args).sign(signing_key=self.signing_key)
 
-        def __process_response(self, response=SyftMessage) -> Dict[Any, Any]:
-            if response.status_code == 200:
-                return response.content
+        def __process_response(self, response: SyftMessage) -> Dict[Any, Any]:
+            if response.status_code == 200:  # type: ignore
+                return response.content  # type: ignore
             else:
-                return Exception(response.content["error"])
+                raise Exception(response.content["error"])  # type: ignore
 
     return GridClient(
         credentials=credentials, url=url, conn_type=conn_type, client_type=client_type
