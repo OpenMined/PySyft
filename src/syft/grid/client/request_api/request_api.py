ab992eb0bad829ba5b632d005e969b8ecab69cd5
Ionesio Junior <ionesiojr@gmail.com>
2021-03-01 05:38:39 -0300

    Update Request API - ADD type hints - Remove unused imports - Remove unused property

--- src/syft/grid/client/request_api/request_api.py
+++ src/syft/grid/client/request_api/request_api.py
@@ -1,8 +1,9 @@
 # stdlib
 from typing import Any
+from typing import Callable
 from typing import Dict
 from typing import Optional
-from typing import Union
+from typing import Type
 
 # third party
 from pandas import DataFrame
@@ -14,13 +15,13 @@ from ....core.common.message import SyftMessage
 class GridRequestAPI:
     def __init__(
         self,
-        create_msg,
-        get_msg,
-        get_all_msg,
-        update_msg,
-        delete_msg,
-        send,
-        response_key=None,
+        send: Callable,
+        create_msg: Optional[Type[SyftMessage]] = None,
+        get_msg: Optional[Type[SyftMessage]] = None,
+        get_all_msg: Optional[Type[SyftMessage]] = None,
+        update_msg: Optional[Type[SyftMessage]] = None,
+        delete_msg: Optional[Type[SyftMessage]] = None,
+        response_key: str = "",
     ):
         self.__create_message = create_msg
         self.__get_message = get_msg
@@ -30,30 +31,26 @@ class GridRequestAPI:
         self.__send = send
         self.__response_key = response_key
 
-    @property
-    def send_method(self):
-        return self.__send
-
-    def create(self, **kwargs):
+    def create(self, **kwargs: Any) -> Dict[str, str]:
         return self.__send(grid_msg=self.__create_message, content=kwargs)
 
-    def get(self, **kwargs):
+    def get(self, **kwargs: Any) -> Any:
         return self.to_obj(self.__send(grid_msg=self.__get_message, content=kwargs))
 
-    def all(self, pandas: bool = False):
+    def all(self, pandas: bool = False) -> Dict[str, Any]:
         result = self.__send(grid_msg=self.__get_all_message)
         if pandas:
             return DataFrame(result)
         else:
             return result
 
-    def update(self, **kwargs):
+    def update(self, **kwargs: Any) -> Dict[str, Any]:
         return self.__send(grid_msg=self.__update_message, content=kwargs)
 
-    def delete(self, **kwargs):
+    def delete(self, **kwargs: Any) -> Dict[str, Any]:
         return self.__send(grid_msg=self.__delete_message, content=kwargs)
 
-    def to_obj(self, result: Dict[Any, Any]):
+    def to_obj(self, result: Any) -> Any:
         if result:
             _class_name = self.__response_key.capitalize()
             result = type(_class_name, (object,), result)()
