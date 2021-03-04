13565af41c375a0908c7be2aa376d5103f0d6b08
Arturo Marquez <arturomf94@gmail.com>
2021-03-04 10:08:39 -0600

    Replace pupulate_crypto_store with crypto_store.populate_store

--- src/syft/lib/sympc/__init__.py
+++ src/syft/lib/sympc/__init__.py
@@ -57,7 +57,7 @@ def create_ast(client: TypeAny = None) -> Globals:
             "sympc.tensor.ShareTensor",
         ),
         (
-            "sympc.session.Session.populate_crypto_store",
+            "sympc.session.crypto_store.populate_store",
             "syft.lib.python._SyNone",
         ),
         (
