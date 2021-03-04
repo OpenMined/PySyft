6082a10fdeb8c879b44632e677f2a4e18efcbe02
Koen van der Veen <koenlennartvanderveen@gmail.com>
2021-03-04 18:25:30 +0100

    mlp test

--- tests/syft/core/node/common/plan/plan_test.py
+++ tests/syft/core/node/common/plan/plan_test.py
@@ -29,8 +29,10 @@ from syft.core.node.common.action.get_or_set_static_attribute_action import (
 )
 from syft.core.node.common.action.run_class_method_action import RunClassMethodAction
 from syft.core.node.common.action.save_object_action import SaveObjectAction
+from syft.core.plan.plan_builder import ROOT_CLIENT
 from syft.core.plan.plan_builder import make_plan
 from syft.core.store.storeable_object import StorableObject
+from syft.lib.python.list import List
 
 
 def test_plan_serialization() -> None:
@@ -254,3 +256,70 @@ def test_make_plan_error_no_kwargs() -> None:
 
     # we can only define a plan with *kwargs* when using the @make_plan decorator, not with args
     assertRaises(ValueError, test_define_plan, "__call__")
+
+
+def test_mlp_plan() -> None:
+    class MLP(sy.Module):
+        def __init__(self, torch_ref):  # type: ignore
+            super().__init__(torch_ref=torch_ref)
+            self.l1 = self.torch_ref.nn.Linear(784, 100)
+            self.a1 = self.torch_ref.nn.ReLU()
+            self.l2 = self.torch_ref.nn.Linear(100, 10)
+
+        def forward(self, x):  # type: ignore
+            x_reshaped = x.view(-1, 28 * 28)
+            l1_out = self.a1(self.l1(x_reshaped))
+            l2_out = self.l2(l1_out)
+            return l2_out
+
+    def set_params(model, params):  # type: ignore
+        """happens outside of plan"""
+        for p, p_new in zip(model.parameters(), params):
+            p.data = p_new.data
+
+    def cross_entropy_loss(logits, targets, batch_size):  # type: ignore
+        norm_logits = logits - logits.max()
+        log_probs = norm_logits - norm_logits.exp().sum(dim=1, keepdim=True).log()
+        return -(targets * log_probs).sum() / batch_size
+
+    def sgd_step(model, lr=0.1):  # type: ignore
+        with ROOT_CLIENT.torch.no_grad():
+            for p in model.parameters():
+                p.data = p.data - lr * p.grad
+                p.grad = th.zeros_like(p.grad.get())
+
+    local_model = MLP(th)  # type: ignore
+
+    @make_plan
+    def train(  # type: ignore
+        xs=th.rand([64 * 3, 1, 28, 28]),
+        ys=th.randint(0, 10, [64 * 3, 10]),
+        params=List(local_model.parameters()),
+    ):
+
+        model = local_model.send(ROOT_CLIENT)
+        set_params(model, params)
+        for i in range(1):
+            indices = th.tensor(range(64 * i, 64 * (i + 1)))
+            x, y = xs.index_select(0, indices), ys.index_select(0, indices)
+            out = model(x)
+            loss = cross_entropy_loss(out, y, 64)
+            loss.backward()
+            sgd_step(model)
+
+        return model.parameters()
+
+    alice_client = sy.VirtualMachine(name="alice").get_client()
+    train_ptr = train.send(alice_client)
+
+    old_params = local_model.parameters().copy()
+
+    res_ptr = train_ptr(
+        xs=th.rand([64 * 3, 1, 28, 28]),
+        ys=th.randint(0, 10, [64 * 3, 10]),
+        params=local_model.parameters(),
+    )
+
+    (new_params,) = res_ptr.get()
+
+    assert not (old_params[0] == new_params[0]).all()
