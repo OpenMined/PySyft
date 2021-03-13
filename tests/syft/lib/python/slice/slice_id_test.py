713eda271c55358f577e6f682036049448340975
dnabanita7 <dashnabanita@gmail.com>
2021-03-14 00:51:31 +0530

    added a test

--- tests/syft/lib/python/slice/slice_id_test.py
+++ tests/syft/lib/python/slice/slice_id_test.py
@@ -1,16 +1,28 @@
 # syft absolute
-from syft.lib.python.int import Int
+from syft.lib.python.list import List
 from syft.lib.python.slice import Slice
+from syft.lib.python.string import String
+from syft.lib.python.tuple import Tuple
 
-sy_slice = Slice(1, 3)
-py_slice = slice(1, 3)
-other_slice = Slice(Int(4))
-other = Int(4)
-py_list = [1, 2, 3]
+py_string = "Python"
+py_list = ["P", "y", "t", "h", "o", "n"]
+py_tuple = ("P", "y", "t", "h", "o", "n")
+sy_string = String(py_string)
+sy_list = List(py_list)
+sy_tuple = Tuple(py_tuple)
+sy_string_slice = sy_string[Slice(3)]
+sy_list_slice = sy_list[Slice(3)]
+sy_tuple_slice = sy_tuple[Slice(3)]
+py_string_slice = py_string[slice(3)]
+py_list_slice = py_list[slice(3)]
+py_tuple_slice = py_tuple[slice(3)]
+other_string_slice = sy_string[Slice(1)]
+other_list_slice = sy_list[Slice(1)]
+other_tuple_slice = sy_tuple[Slice(1)]
 
 
 def test_id_eq():
-    sy_res = sy_slice == other_slice
-    py_res = sy_slice == other_slice
-    assert py_res == sy_res
-    assert sy_res.id != sy_slice.id
+    sy_list_res = sy_list_slice == other_list_slice
+    py_list_res = sy_list_slice == other_list_slice
+    assert py_list_res == sy_list_res
+    assert sy_list_res.id != sy_list_slice.id
