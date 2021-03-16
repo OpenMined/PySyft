3680106e7879b2a34a740a9a3f981974fe4c4ea9
George-Cristian Muraru <murarugeorgec@gmail.com>
2021-03-16 13:43:18 +0200

    Add view

--- src/syft/lib/sympc/__init__.py
+++ src/syft/lib/sympc/__init__.py
@@ -109,6 +109,14 @@ def create_ast(client: TypeAny = None) -> Globals:
             "sympc.tensor.ShareTensor.T",
             "sympc.tensor.ShareTensor",
         ),
+        (
+            "sympc.tensor.ShareTensor.unsqueeze",
+            "sympc.tensor.ShareTensor"
+        ),
+        (
+            "sympc.tensor.ShareTensor.view",
+            "sympc.tensor.ShareTensor"
+        )
     ]
 
     add_modules(ast, modules)
