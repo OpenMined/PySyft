adfd99e6b707ceab1c9f46372aa2035d00d3b342
xutongye <xvtongye1986@163.com>
2021-03-21 13:13:08 +0800

    debug test

--- src/syft/core/node/common/service/obj_search_service.py
+++ src/syft/core/node/common/service/obj_search_service.py
@@ -209,7 +209,7 @@ class ImmediateObjectSearchService(ImmediateNodeServiceWithReply):
                 # if this tensor allows anyone to search for it, then one of its keys
                 # has a VerifyAll in it.
                 contains_all_in_permissions = any(
-                    key is VerifyAll for key in obj.search_permissions.keys()
+                    isinstance(key, VerifyAll) for key in obj.search_permissions.keys()
                 )
                 if (
                     verify_key in obj.search_permissions.keys()
