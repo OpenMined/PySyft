ebf375b14a7ade786a2416366ebd84688b76d88f
Syumei <koukyosyumei@hotmail.com>
2021-03-23 15:57:44 +0900

    move numpy calls to inside the test

--- tests/syft/lib/numpy/array_test.py
+++ tests/syft/lib/numpy/array_test.py
@@ -1,30 +1,32 @@
 # third party
-import numpy as np
 import pytest
 
 # syft absolute
 import syft as sy
 
-ExampleArray = [
-    np.array([1, 2, -3], dtype=np.int8),
-    np.array([1, 2, -3], dtype=np.int16),
-    np.array([1, 2, -3], dtype=np.int32),
-    np.array([1, 2, -3], dtype=np.int64),
-    np.array([1, 2, 3], dtype=np.uint8),
-    # np.array([1, 2, 3], dtype=np.uint16),
-    # np.array([1, 2, 3], dtype=np.uint32),
-    # np.array([1, 2, 3], dtype=np.uint64),
-    np.array([1.2, 2.2, 3.0], dtype=np.float16),
-    np.array([1.2, 2.2, 3.0], dtype=np.float32),
-    np.array([1.2, 2.2, 3.0], dtype=np.float64),
-    # np.array([1 + 2j, 3 + 4j, 5 + 0j], dtype=np.complex64),
-    # np.array([1 + 2j, 3 + 4j, 5 + 0j], dtype=np.complex128),
-    np.array([True, False, True], dtype=np.bool_),
-]
-
 
 @pytest.mark.vendor(lib="numpy")
 def test_remote_numpy_array() -> None:
+    # third party
+    import numpy as np
+
+    ExampleArray = [
+        np.array([1, 2, -3], dtype=np.int8),
+        np.array([1, 2, -3], dtype=np.int16),
+        np.array([1, 2, -3], dtype=np.int32),
+        np.array([1, 2, -3], dtype=np.int64),
+        np.array([1, 2, 3], dtype=np.uint8),
+        # np.array([1, 2, 3], dtype=np.uint16),
+        # np.array([1, 2, 3], dtype=np.uint32),
+        # np.array([1, 2, 3], dtype=np.uint64),
+        np.array([1.2, 2.2, 3.0], dtype=np.float16),
+        np.array([1.2, 2.2, 3.0], dtype=np.float32),
+        np.array([1.2, 2.2, 3.0], dtype=np.float64),
+        # np.array([1 + 2j, 3 + 4j, 5 + 0j], dtype=np.complex64),
+        # np.array([1 + 2j, 3 + 4j, 5 + 0j], dtype=np.complex128),
+        np.array([True, False, True], dtype=np.bool_),
+    ]
+
     sy.load("numpy")
 
     vm = sy.VirtualMachine()
