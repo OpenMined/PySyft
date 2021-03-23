8ab2740a842b55f5947c2b07234fb8a0a8f6fa1a
dnabanita7 <dashnabanita@gmail.com>
2021-03-23 20:48:00 +0530

    refactored the deserialization part

--- tests/syft/lib/python/slice/slice_serde_test.py
+++ tests/syft/lib/python/slice/slice_serde_test.py
@@ -1,12 +1,12 @@
 # syft absolute
 import syft as sy
+from syft.lib.python.int import Int
 from syft.lib.python.slice import Slice
 from syft.proto.lib.python.slice_pb2 import Slice as Slice_PB
 
 
 def test_slice_serde() -> None:
-    syft_slice = Slice(1, 3)
-
+    syft_slice = Slice(Int(1), Int(3))
     serialized = syft_slice._object2proto()
 
     assert isinstance(serialized, Slice_PB)
@@ -15,15 +15,15 @@ def test_slice_serde() -> None:
 
     assert isinstance(deserialized, Slice)
     assert deserialized.id == syft_slice.id
-    assert deserialized == syft_slice
+    assert deserialized.start == syft_slice.start
+    assert deserialized.stop == syft_slice.stop
 
 
 def test_slice_send() -> None:
     alice = sy.VirtualMachine(name="alice")
     alice_client = alice.get_client()
 
-
-    syft_slice = Slice(1, 3)
+    syft_slice = Slice(Int(1), Int(3))
     ptr = syft_slice.send(alice_client)
 
     # Check pointer type
@@ -31,4 +31,5 @@ def test_slice_send() -> None:
 
     # Check that we can get back the object
     res = ptr.get()
-    assert res == syft_slice
+    assert res.start == syft_slice.start
+    assert res.stop == syft_slice.stop
