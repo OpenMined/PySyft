29eaf9432087889c8f6b31adc1890b853e905b2a
Syumei <koukyosyumei@hotmail.com>
2021-03-24 08:48:49 +0900

    add remote construct

--- tests/syft/lib/PIL/pil_test.py
+++ tests/syft/lib/PIL/pil_test.py
@@ -1,6 +1,3 @@
-# stdlib
-import io
-
 # third party
 import pytest
 
@@ -9,7 +6,10 @@ import syft as sy
 
 
 @pytest.mark.vendor(lib="PIL")
-def test_remote_engine_simple() -> None:
+def test_send_and_get() -> None:
+    # stdlib
+    import io
+
     # third party
     import PIL
     import requests
@@ -21,6 +21,33 @@ def test_remote_engine_simple() -> None:
     im_url = "https://www.python.org/static/community_logos/python-logo.png"
     im = PIL.Image.open(io.BytesIO(requests.get(im_url).content))
     remote_im = im.send(data_owner)
+    received_im = remote_im.get()
+
+    assert PIL.ImageChops.difference(im, received_im).getbbox() is None
+
+
+@pytest.mark.vendor(lib="PIL")
+def test_remote_create() -> None:
+    # stdlib
+    import io
+
+    # third party
+    import PIL
+    import numpy as np
+    import requests
+    import torch
+
+    sy.load("PIL")
+
+    data_owner = sy.VirtualMachine().get_root_client()
+    remote_torchvision = data_owner.torchvision
+
+    im_url = "https://www.python.org/static/community_logos/python-logo.png"
+    im = PIL.Image.open(io.BytesIO(requests.get(im_url).content))
+    im_array = np.array(im)
+    im_tensor = torch.Tensor(im_array).permute(2, 0, 1)
+    remote_tensor = im_tensor.send(data_owner)
+    remote_im = remote_torchvision.transforms.functional.to_pil_image(remote_tensor)
     received_im = remote_im.get()
 
     assert PIL.ImageChops.difference(im, received_im).getbbox() is None
