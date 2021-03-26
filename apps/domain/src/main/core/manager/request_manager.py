f659b022a942acdeef8adf75d32f1e77ee292b88
Ionesio Junior <ionesiojr@gmail.com>
2021-03-26 14:50:56 -0300

    Update create_request method / Create status method

--- apps/domain/src/main/core/manager/request_manager.py
+++ apps/domain/src/main/core/manager/request_manager.py
@@ -6,6 +6,8 @@ from .database_manager import DatabaseManager
 from ..database.requests.request import Request
 from .role_manager import RoleManager
 from ..exceptions import RequestError
+from syft.core.node.domain.service import RequestStatus
+from syft.core.common.uid import UID
 
 
 class RequestManager(DatabaseManager):
@@ -23,17 +25,40 @@ class RequestManager(DatabaseManager):
 
         return result
 
-    def create_request(self, user_id, user_name, object_id, reason, request_type):
+    def create_request(
+        self,
+        user_id,
+        user_name,
+        object_id,
+        reason,
+        request_type,
+        verify_key=None,
+        tags=[],
+        object_type="",
+    ):
         date = datetime.now()
 
         return self.register(
+            id=str(UID().value),
             user_id=user_id,
             user_name=user_name,
             object_id=object_id,
             date=date,
             reason=reason,
             request_type=request_type,
+            verify_key=verify_key,
+            tags=tags,
+            object_type=object_type,
         )
+
+    def status(self, request_id):
+        _req = self.first(id=request_id)
+        if _req.status == "pending":
+            return RequestStatus.pending
+        elif _req.status == "accepted":
+            return RequestStatus.Accepted
+        else:
+            return RequestStatus.Rejected
 
     def set(self, request_id, status):
         self.modify({"id": request_id}, {"status": status})
