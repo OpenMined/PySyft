3fe63e89ef7ed8449fea8c7f161579b9834d15d7
Ionesio Junior <ionesiojr@gmail.com>
2021-03-26 14:50:31 -0300

    ADD modified Syft Services

--- apps/domain/src/main/core/services/request_service.py
+++ apps/domain/src/main/core/services/request_service.py
@@ -2,6 +2,8 @@
 from typing import List
 from typing import Type
 from typing import Union
+from typing import Optional
+import time
 
 # third party
 from nacl.signing import VerifyKey
@@ -10,9 +12,37 @@ from nacl.encoding import HexEncoder
 # syft relative
 from syft.core.node.abstract.node import AbstractNode
 from syft.core.node.common.service.auth import service_auth
+from syft.core.node.common.node import DuplicateRequestException
 from syft.core.node.common.service.node_service import ImmediateNodeServiceWithReply
 from syft.core.node.common.service.node_service import ImmediateNodeServiceWithoutReply
 from syft.core.common.message import ImmediateSyftMessageWithReply
+from syft.core.common.message import ImmediateSyftMessageWithoutReply
+from syft.core.common.uid import UID
+from syft.util import validate_type
+
+# Syft Request services with reply
+from syft.core.node.domain.service.request_answer_message import (
+    RequestAnswerMessage,
+    RequestAnswerResponse,
+)
+from syft.core.node.domain.service.get_all_requests_service import (
+    GetAllRequestsMessage,
+    GetAllRequestsResponseMessage,
+)
+from syft.core.node.domain.service.request_handler_service import (
+    GetAllRequestHandlersMessage,
+    GetAllRequestHandlersResponseMessage,
+)
+
+# Syft Request services without reply
+from syft.core.node.domain.service.request_message import RequestMessage
+from syft.core.node.domain.service.accept_or_deny_request_service import (
+    AcceptOrDenyRequestMessage,
+)
+from syft.core.node.domain.service.request_handler_service import (
+    UpdateRequestHandlerMessage,
+)
+
 
 from syft.grid.messages.request_messages import (
     CreateRequestMessage,
@@ -253,6 +283,85 @@ def del_request_msg(
     )
 
 
+# PySyft Services
+
+
+def request_answer_msg(
+    msg: RequestAnswerMessage, node: AbstractNode, verify_key: VerifyKey
+):
+    if verify_key is None:
+        raise ValueError(
+            "Can't process Request service without a given " "verification key"
+        )
+
+    status = node.data_requests.status(id=str(msg.request_id.value))  # type: ignore
+    address = msg.reply_to
+    return RequestAnswerResponse(
+        request_id=msg.request_id, address=address, status=status
+    )
+
+
+# TODO: Check if this method/message should really be a service_without_reply message
+def get_all_requests(
+    msg: GetAllRequestsMessage, node: AbstractNode, verify_key: VerifyKey
+):
+    if verify_key is None:
+        raise ValueError(
+            "Can't process Request service without a given " "verification key"
+        )
+
+    current_user = node.users.first(
+        verify_key=verify_key.encode(encoder=HexEncoder).decode("utf-8")
+    )
+    _can_triage_request = node.users.can_triage_requests(user_id=current_user.id)
+
+    _requests = node.data_requests.all()
+
+    if _can_triage_request:
+        _requests = node.data_requests.all()
+    else:
+        _requests = node.data_requests.query(
+            verify_key=verify_key.encode(encoder=HexEncoder).decode("utf-8")
+        )
+
+    data_requests = [
+        RequestMessage(
+            request_id=UID.from_string(req.id),
+            request_description=req.reason,
+            address=node.address,
+            owner_address=node.address,
+            object_id=UID.from_string(req.object_id),
+            object_type=req.object_type,
+            object_tags=req.tags,
+            requester_verify_key=VerifyKey(
+                req.verify_key.encode("utf-8"), encoder=HexEncoder
+            ),
+            timeout_secs=None,
+        )
+        for req in _requests
+    ]
+
+    return GetAllRequestsResponseMessage(requests=data_requests, address=msg.reply_to)
+
+
+def get_all_request_handlers(
+    msg: GetAllRequestHandlersMessage, node: AbstractNode, verify_key: VerifyKey
+):
+
+    if verify_key is None:
+        raise ValueError(
+            "Can't process Request service without a given " "verification key"
+        )
+
+    handlers = []
+    if verify_key == node.root_verify_key:
+        existing_handlers = getattr(node, "request_handlers", None)
+        if existing_handlers is not None:
+            handlers = existing_handlers
+
+    return GetAllRequestHandlersResponseMessage(handlers=handlers, address=msg.reply_to)
+
+
 class RequestService(ImmediateNodeServiceWithReply):
 
     msg_handler_map = {
@@ -261,6 +370,9 @@ class RequestService(ImmediateNodeServiceWithReply):
         GetRequestsMessage: get_all_request_msg,
         UpdateRequestMessage: update_request_msg,
         DeleteRequestMessage: del_request_msg,
+        RequestAnswerMessage: request_answer_msg,
+        GetAllRequestsMessage: get_all_requests,
+        GetAllRequestHandlersMessage: get_all_request_handlers,
     }
 
     @staticmethod
@@ -273,6 +385,9 @@ class RequestService(ImmediateNodeServiceWithReply):
             GetRequestsMessage,
             UpdateRequestMessage,
             DeleteRequestMessage,
+            RequestAnswerMessage,
+            GetAllRequestsMessage,
+            GetAllRequestHandlersMessage,
         ],
         verify_key: VerifyKey,
     ) -> Union[
@@ -281,11 +396,13 @@ class RequestService(ImmediateNodeServiceWithReply):
         GetRequestsResponse,
         UpdateRequestResponse,
         DeleteRequestResponse,
+        RequestAnswerResponse,
+        GetAllRequestsResponseMessage,
+        GetAllRequestHandlersResponseMessage,
     ]:
-
         return RequestService.msg_handler_map[type(msg)](
-            msg=msg, node=node, verify_key=verify_key
-        )
+                msg=msg, node=node, verify_key=verify_key
+            )
 
     @staticmethod
     def message_handler_types() -> List[Type[ImmediateSyftMessageWithReply]]:
@@ -296,4 +413,168 @@ class RequestService(ImmediateNodeServiceWithReply):
             GetRequestsMessage,
             UpdateRequestMessage,
             DeleteRequestMessage,
+            RequestAnswerMessage,
+            GetAllRequestsMessage,
+            GetAllRequestHandlersMessage,
         ]
+
+
+def build_request_message(
+    msg: RequestMessage, node: AbstractNode, verify_key: VerifyKey
+):
+    if verify_key is None:
+        raise ValueError(
+            "Can't process Request service without a given " "verification key"
+        )
+
+    if msg.requester_verify_key != verify_key:
+        raise Exception(
+            "You tried to request access for a key that is not yours!"
+            "You cannot do this! Whatever key you want to request access"
+            "for must be the verify key that also verifies the message"
+            "containing the request."
+        )
+
+    # since we reject/accept requests based on the ID, we don't want there to be
+    # multiple requests with the same ID because this could cause security problems.
+    _duplicate_request = node.data_requests.contain(
+        object_id=str(msg.object_id.value),
+        verify_key=verify_key.encode(encoder=HexEncoder).decode("utf-8"),
+    )
+
+    if _duplicate_request:
+        raise DuplicateRequestException(
+            f"You have already requested {msg.object_id} ",
+            node.data_requests.all(),
+            "My Requests",
+        )
+
+    current_user = node.users.first(
+        verify_key=verify_key.encode(encoder=HexEncoder).decode("utf-8")
+    )
+
+    node.data_requests.create_request(
+        user_id=current_user.id,
+        user_name=current_user.email,
+        object_id=str(msg.object_id.value),
+        reason=msg.request_description,
+        request_type="permissions",
+        verify_key=verify_key.encode(encoder=HexEncoder).decode("utf-8"),
+        object_type=msg.object_type,
+        tags=node.store[msg.object_id]._tags,
+    )
+
+
+def accept_or_deny_request(
+    msg: AcceptOrDenyRequestMessage, node: AbstractNode, verify_key: VerifyKey
+):
+    if verify_key is None:
+        raise ValueError(
+            "Can't process AcceptOrDenyRequestService without a specified verification key"
+        )
+
+    _msg: AcceptOrDenyRequestMessage = validate_type(msg, AcceptOrDenyRequestMessage)
+
+    current_user = node.users.first(
+        verify_key=verify_key.encode(encoder=HexEncoder).decode("utf-8")
+    )
+
+    _req = node.data_requests.first(id=str(_msg.request_id.value))
+    _can_triage_request = node.users.can_triage_requests(user_id=current_user.id)
+    if _msg.accept:
+        if _req and _can_triage_request:
+            tmp_obj = node.store[UID.from_string(_req.object_id)]
+            tmp_obj.read_permissions[
+                VerifyKey(_req.verify_key.encode("utf-8"), encoder=HexEncoder)
+            ] = _req.id
+            node.store[UID.from_string(_req.object_id)] = tmp_obj
+            node.data_requests.set(request_id=_req.id, status="accepted")
+    else:
+        _req_owner = current_user.verify_key == _req.verify_key
+        if _req and (_can_triage_request or _req_owner):
+            node.data_requests.set(request_id=_req.id, status="denied")
+
+
+def update_req_handler(
+    node: AbstractNode,
+    msg: UpdateRequestHandlerMessage,
+    verify_key: Optional[VerifyKey] = None,
+) -> None:
+    if verify_key is None:
+        raise ValueError(
+            "Can't process Request service without a given " "verification key"
+        )
+
+    if verify_key == node.root_verify_key:
+        replacement_handlers = []
+
+        # find if there exists a handler match the handler passed in
+        existing_handlers = getattr(node, "request_handlers", None)
+        # debug(f"> Updating Request Handlers with existing: {existing_handlers}")
+        if existing_handlers is not None:
+            matched = None
+            for existing_handler in existing_handlers:
+                # we match two handlers according to their tags
+                if existing_handler["tags"] == msg.handler["tags"]:
+                    matched = existing_handler
+                    # if an existing_handler match the passed in handler,
+                    # we ignore it in for loop
+                    continue
+                else:
+                    # if an existing_handler does not match the passed in
+                    # handler, we keep it
+                    replacement_handlers.append(existing_handler)
+
+            if msg.keep:
+                msg.handler["created_time"] = time.time()
+                replacement_handlers.append(msg.handler)
+                if matched is not None:
+                    print(
+                        f"> Replacing a Request Handler {matched} with: {msg.handler}"
+                    )
+                    # debug(
+                    #    f"> Replacing a Request Handler {matched} with: {msg.handler}"
+                    # )
+                else:
+                    print(f"> Adding a Request Handler {msg.handler}")
+                    # debug(f"> Adding a Request Handler {msg.handler}")
+            else:
+                print(f"> Removing a Request Handler with: {msg.handler}")
+                # debug(f"> Removing a Request Handler with: {msg.handler}")
+
+            setattr(node, "request_handlers", replacement_handlers)
+            # debug(f"> Finished Updating Request Handlers with: {existing_handlers}")
+        else:
+            # TODO: Replace line below
+            print(f"> Node has no Request Handlers attribute: {type(node)}")
+            # error(f"> Node has no Request Handlers attribute: {type(node)}")
+
+    return
+
+
+class RequestServiceWithoutReply(ImmediateNodeServiceWithoutReply):
+
+    msg_handler_map = {
+        RequestMessage: build_request_message,
+        AcceptOrDenyRequestMessage: accept_or_deny_request,
+        UpdateRequestHandlerMessage: update_req_handler,
+    }
+
+    @staticmethod
+    @service_auth(guests_welcome=True)
+    def process(
+        node: AbstractNode,
+        msg: Union[
+            RequestMessage,
+            AcceptOrDenyRequestMessage,
+            UpdateRequestHandlerMessage,
+        ],
+        verify_key: VerifyKey,
+    ) -> None:
+        RequestServiceWithoutReply.msg_handler_map[type(msg)](
+            msg=msg, node=node, verify_key=verify_key
+        )
+
+    @staticmethod
+    def message_handler_types() -> List[Type[ImmediateSyftMessageWithoutReply]]:
+        return [RequestMessage, AcceptOrDenyRequestMessage, UpdateRequestHandlerMessage]
