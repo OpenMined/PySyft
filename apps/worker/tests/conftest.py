430b9bfbf463207427524b971896c2611c535ab5
Ionesio Junior <ionesiojr@gmail.com>
2021-03-31 19:14:56 -0300

    Update Worker tests

--- apps/worker/tests/conftest.py
+++ apps/worker/tests/conftest.py
@@ -5,7 +5,6 @@ import sys, os
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(myPath + "/../src/")
 from app import create_app
-from main.core.database import db
 import main.core.node
 from main.core.node import GridDomain
 
@@ -25,17 +24,3 @@ def client(app):
 @pytest.fixture
 def domain():
     return GridDomain(name="testing")
-
-
-@pytest.fixture(scope="function")
-def database(app):
-    # TODO: Testing db should be used
-    # but right now changes do not propagate
-    # outside test suite
-
-    # test_db = db
-    # test_db.init_app(app)
-    # app.app_context().push()
-    # test_db.create_all()
-    # return test_db
-    return db
