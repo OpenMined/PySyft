f8ae32c2c58d072d166210940d81f3210e4ef4d1
dnabanita7 <dashnabanita@gmail.com>
2021-03-31 17:42:49 +0530

    deleted the legacy docs in util.py

--- src/syft/ast/util.py
+++ src/syft/ast/util.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
-"""This is a skeleton file that can serve as a starting point for Python console script.
-To run this script uncomment the following lines in [options.entry_points] section in setup.cfg.
-console_scripts =
-     fibonacci = syft.skeleton:run
-Then run `python setup.py install` which will install the command `fibonacci` inside your current environment.
-Besides console scripts, header (i.e. until _logger...) of this file can also be used as template for Python modules.
-Note: This skeleton file can be safely removed if not needed!
-"""
 # stdlib
 from typing import List as TypeList
 
