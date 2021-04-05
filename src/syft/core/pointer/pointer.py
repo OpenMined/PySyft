da0a8be600dabec5e8ee37ab507e44fe39562576
Chitresh13 <goelchitresh13@gmail.com>
2021-04-05 15:16:39 +0530

    first commit

--- src/syft/core/pointer/pointer.py
+++ src/syft/core/pointer/pointer.py
@@ -592,3 +592,5 @@ class Pointer(AbstractPointer):
 
             # Send the message
             self.client.send_eventual_msg_without_reply(msg=msg)
+
+    #Methods of klass.py are listed here (Pointer related)
