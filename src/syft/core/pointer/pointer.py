991765108ba0e1a4d1ced865ceba9833b2ea1aee
Chitresh13 <goelchitresh13@gmail.com>
2021-04-07 02:35:41 +0530

    Added _resolve_pointer_type method to Pointer class

--- src/syft/core/pointer/pointer.py
+++ src/syft/core/pointer/pointer.py
@@ -114,6 +114,7 @@ from ..node.common.service.get_repr_service import GetReprMessage
 from ..node.common.service.obj_search_permission_service import (
     ObjectSearchPermissionUpdateMessage,
 )
+from ..node.common.service.resolve_pointer_type_service import ResolvePointerTypeMessage
 from ..store.storeable_object import StorableObject
 
 
@@ -584,3 +585,44 @@ class Pointer(AbstractPointer):
 
         if self.gc_enabled:
             self.client.gc.apply(self)
+
+    def _resolve_pointer_type(self):
+        """
+        Creates a request on a pointer to validate and regenerate the current pointer type. This method
+        is useful when deadling with AnyPointer or Union<types>Pointers, to retrieve the real pointer.
+
+        The existing pointer will be deleted and a new one will be generated. The remote data won't
+        be touched.
+
+        Args:
+            self: The pointer which will be validated.
+
+        Returns:
+            The new pointer, validated from the remote object.
+        """
+
+        # id_at_location has to be preserved
+        id_at_location = getattr(self, "id_at_location", None)
+
+        if None:
+            traceback_and_raise(
+                ValueError("Can't resolve a pointer that has no underlying object.")
+            )
+
+        cmd = ResolvePointerTypeMessage(
+            id_at_location=id_at_location,
+            address=self.client.address,
+            reply_to=self.client.address,
+        )
+
+        # the path to the underlying type. It has to live in the AST
+        real_type_path = self.client.send_immediate_msg_with_reply(msg=cmd).type_path
+        new_pointer = self.client.lib_ast.query(real_type_path).pointer_type(
+            client=self.client, id_at_location=id_at_location
+        )
+
+        # we disable the garbage collection message and then we delete the existing message.
+        self.gc_enabled = False
+        del self
+
+        return new_pointer
