71132a754e1ce1620eb13bf58514caa140d07f48
Param Mirani <param.mirani1999@gmail.com>
2021-04-14 18:21:10 +0530

    initial commit

---
+++ src/syft/lib/xgboost/__init__.py
@@ -0,0 +1,54 @@
+# stdlib
+import functools
+from typing import Any as TypeAny
+from typing import List as TypeList
+from typing import Tuple as TypeTuple
+
+# third party
+import xgboost as xgb
+
+# syft relative
+from ...ast import add_classes
+from ...ast import add_methods
+from ...ast import add_modules
+from ...ast.globals import Globals
+from ..util import generic_update_ast
+
+LIB_NAME = "xgboost"
+PACKAGE_SUPPORT = {
+    "lib": LIB_NAME,
+}
+
+
+def create_ast(client: TypeAny) -> Globals:
+    ast = Globals(client=client)
+
+    modules: TypeList[TypeTuple[str, TypeAny]] = [
+        ("xgboost", xgb),
+        ("xgboost.core", xgb.core),
+    ]
+
+    classes: TypeList[TypeTuple[str, str, TypeAny]] = [
+        ("xgboost.DMatrix", "xgboost.DMatrix", xgb.core.DMatrix),
+        ("xgboost.core.DMatrix", "xgboost.core.DMatrix", xgb.core.DMatrix),
+        ("xgboost.core.Booster", "xgboost.core.Booster", xgb.core.Booster),
+    ]
+
+    methods = [
+        ("xgboost.train", "xgboost.core.Booster"),
+        ("xgboost.core.Booster.predict", "numpy.ndarray"),
+    ]
+
+    add_modules(ast, modules)
+    add_classes(ast, classes)
+    add_methods(ast, methods)
+
+    for klass in ast.classes:
+        klass.create_pointer_class()
+        klass.create_send_method()
+        klass.create_storable_object_attr_convenience_methods()
+
+    return ast
+
+
+update_ast = functools.partial(generic_update_ast, LIB_NAME, create_ast)
---
+++ tests/syft/lib/xgboost/model_test.py
@@ -0,0 +1,38 @@
+# third party
+import pytest
+
+# syft absolute
+import syft as sy
+
+
+@pytest.mark.vendor(lib="xgboost")
+@pytest.mark.vendor(lib="numpy")
+def test_pandas(root_client: sy.VirtualMachineClient) -> None:
+    sy.load("xgboost")
+    sy.load("numpy")
+    # import xgboost
+
+    # third party
+    import numpy as np
+    import xgboost as xgb
+
+    xgb_remote = root_client.xgboost
+
+    # import xgboost as xgb
+
+    X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
+    y = np.array([1, 1, 2, 2])
+
+    param = {"eta": 0.3, "max_depth": 3, "num_class": 3}
+
+    steps = 20
+
+    D_train = xgb.DMatrix(X, label=y)
+    model = xgb.train(param, D_train, steps)
+    preds = model.predict(D_train)
+
+    D_train = xgb_remote.DMatrix(X, label=y)
+    model = xgb_remote.train(param, D_train, steps)
+    preds_remote = model.predict(D_train)
+
+    assert preds_remote.get(root_client).all() == preds.all()
