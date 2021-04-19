80b0eb5f0d2a9bbb488c17b3e9cc7ce01c005ef4
Chitresh13 <goelchitresh13@gmail.com>
2021-04-20 01:25:28 +0530

    commented methods which hinders privacy

--- src/syft/lib/numpy/__init__.py
+++ src/syft/lib/numpy/__init__.py
@@ -42,7 +42,11 @@ def create_ast(client: TypeAny = None) -> Globals:
         ("numpy.ndarray.itemsize", "numpy.ndarray"),
         ("numpy.ndarray.nbytes", "numpy.ndarray"),
         ("numpy.ndarray.base", "numpy.ndarray"),
+<<<<<<< HEAD
         # ("numpy.ndarray.dtype", "numpy.ndarray"), # SECURITY WARNING: DO NOT ADD TO ALLOW LIST YET
+=======
+        # ("numpy.ndarray.dtype", "numpy.ndarray"), SECURITY WARNING: DO NOT ADD TO ALLOW LIST YET 
+>>>>>>> commented methods which hinders privacy
         ("numpy.ndarray.T", "numpy.ndarray"),
         # ("numpy.ndarray.real", "numpy.ndarray"), # requires dtype complex
         # ("numpy.ndarray.imag", "numpy.ndarray"), # requires dtype complex
