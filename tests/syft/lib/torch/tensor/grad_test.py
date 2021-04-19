2e621ee2b20d9b7ff1987de61ad989d6f5228a03
Tudor Cebere <tudorcebere@gmail.com>
2021-04-19 11:53:52 +0300

    Test to prevent regression of .grad and .data on torch.Tensor

---
+++ tests/syft/lib/torch/tensor/grad_test.py
@@ -0,0 +1,18 @@
+import syft as sy
+import torch
+
+def torch_grad_test(client: sy.VirtualMachineClient):
+    x = client.torch.Tensor([[1, 1], [1, 1]])
+    x.requires_grad = True
+    gt = client.torch.Tensor([[1, 1], [1, 1]]) * 16 - 0.5
+
+    loss_fn = client.torch.nn.MSELoss()
+
+    v = x + 2
+    y = v ** 2
+
+    loss = loss_fn(y, gt)
+    loss.backward()
+
+    assert x.grad.get().equal(torch.Tensor([[-19.5, -19.5], [-19.5, -19.5]]))
+    assert x.data.get().equal(torch.Tensor([[1, 1], [1, 1]]))
