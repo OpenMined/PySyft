02415e97e5d044047c6e45273c2b748b2d6f6013
Tudor Cebere <tudorcebere@gmail.com>
2021-04-21 16:46:49 +0300

    make signaling server global in the test suite

--- scripts/nb_duet_test.py
+++ scripts/nb_duet_test.py
@@ -24,207 +24,245 @@ import os
 from pathlib import Path
 import re
 import shutil
-from typing import Dict
+from typing import List
 
 # third party
 from nbconvert import PythonExporter
 from nbconvert.writers import FilesWriter
 import nbformat
 
-tests = defaultdict(list)
-output_dir = Path("tests/syft/notebooks")
-checkpoint_dir = Path("tests/syft/notebooks/checkpoints")
 
-SLEEP_TIME = 360
-PORTS: Dict[str, int] = {}
+def generate_targets() -> List[Path]:
+    result = []
 
-try:
-    os.makedirs(output_dir, exist_ok=True)
-except BaseException as e:
-    print("os.makedirs failed ", e)
+    for path in PATHS:
+        opt_notebooks = list(Path(path).rglob("*.ipynb"))
+        for _path in opt_notebooks:
+            if ".ipynb_checkpoints" in str(_path):
+                continue
+            result.append(_path)
 
-try:
-    shutil.rmtree(checkpoint_dir)
-except BaseException as e:
-    print("rmtree failed ", e)
+    result.sort()
+    return result
 
-try:
-    os.makedirs(checkpoint_dir, exist_ok=True)
-except BaseException as e:
-    print("os.makedirs failed ", e)
 
-testcase_lib = {}
-
-for path in (
-    list(Path("examples/homomorphic-encryption").rglob("*.ipynb"))
-    + list(Path("examples/duet/dcgan").rglob("*.ipynb"))
-    + list(Path("examples/duet/super_resolution").rglob("*.ipynb"))
-    + list(Path("examples/private-ai-series/duet_basics").rglob("*.ipynb"))
-    + list(Path("examples/private-ai-series/duet_iris_classifier").rglob("*.ipynb"))
-    + list(Path("examples/differential-privacy/opacus").rglob("*.ipynb"))
-    + list(Path("examples/duet/mnist").rglob("*.ipynb"))
-    + list(Path("examples/duet/mnist_lightning").rglob("*.ipynb"))
-    + list(Path("examples/duet/reinforcement_learning").rglob("*.ipynb"))
-):
-    if ".ipynb_checkpoints" in str(path):
-        continue
-
-    testname = re.sub("[^0-9a-zA-Z]+", "_", str(path))
-    output = output_dir / testname
-
-    file_name = str(path.stem)
-    is_do = False
-    is_ds = False
-
-    if file_name.endswith("_Data_Scientist"):
-        testcase = file_name.replace("_Data_Scientist", "")
-        tests[testcase].append(testname)
-        is_ds = True
-    elif file_name.endswith("_Data_Owner"):
-        testcase = file_name.replace("_Data_Owner", "")
-        tests[testcase].append(testname)
-        is_do = True
-    else:
-        continue
-
-    # generate a unique port for this testcase
-    PORTS[testcase] = 20157
-    load_lib_search = r"load_lib\(\W+([a-z_-]+)\W+\)"
-
-    with open(path, "r") as f:
-        load_lib_results = re.search(load_lib_search, str(f.read()), re.IGNORECASE)
-        if load_lib_results:
-            lib_name = load_lib_results.group(1)
-            testcase_lib[testcase] = lib_name
-
-    notebook_nodes = nbformat.read(path, as_version=4)
-
-    custom_cell = nbformat.v4.new_code_cell(
-        source="""
+asyncio_event_loop = """
 import os
 import time
 import asyncio
 from pathlib import Path
-
+from . import get_global_var
 loop = asyncio.get_event_loop()
 """
-    )
-    notebook_nodes["cells"].insert(0, custom_cell)
 
-    for idx, cell in enumerate(notebook_nodes["cells"]):
-        if cell["cell_type"] == "code" and "loopback=True" in cell["source"]:
-            network_url = f"http://127.0.0.1:{PORTS[testcase]}"
-            notebook_nodes["cells"][idx]["source"] = cell["source"].replace(
-                "loopback=True", f"loopback=True, network_url='{network_url}'"
-            )
-        if cell["cell_type"] == "markdown" and "Checkpoint" in cell["source"]:
-            checkpoint = (
-                cell["source"]
-                .lower()
-                .split("checkpoint")[1]
-                .strip()
-                .split(":")[0]
-                .strip()
-            )
 
-            testcase_checkpoint_dir = f"checkpoints/{testcase}/"
-
-            # For DO, we wait until DS gets to the same checkpoint
-            if is_do:
-                ck_file = testcase_checkpoint_dir + (
-                    testcase + "_DO_checkpoint_" + str(checkpoint)
-                )
-                wait_file = testcase_checkpoint_dir + (
-                    testcase + "_DS_checkpoint_" + str(checkpoint)
-                )
-                checkpoint_cell = nbformat.v4.new_code_cell(
-                    source=f"""
-Path(\"{ck_file}\").touch()
-for retry in range({SLEEP_TIME}):
-    if Path(\"{wait_file}\").exists():
+checkpoint_template = """
+Path(\"{}\").touch()
+for retry in range({}):
+    if Path(\"{}\").exists():
         break
     task = loop.create_task(asyncio.sleep(1))
     loop.run_until_complete(task)
-                                                            """
-                )
+"""
 
-            # For DS, we wait until DO gets to the next checkpoint
-            elif is_ds:
-                ck_file = testcase_checkpoint_dir + (
-                    testcase + "_DS_checkpoint_" + str(checkpoint)
-                )
-                wait_file = testcase_checkpoint_dir + (
-                    testcase + "_DO_checkpoint_" + str(int(checkpoint) + 1)
-                )
-                checkpoint_cell = nbformat.v4.new_code_cell(
-                    source=f"""
-Path(\"{ck_file}\").touch()
-for retry in range({SLEEP_TIME}):
-    if Path(\"{wait_file}\").exists():
+
+checkpoint_ack_template = """
+Path(\"{}\").touch()
+for retry in range({}):
+    if Path(\"{}\").exists():
         break
     task = loop.create_task(asyncio.sleep(1))
     loop.run_until_complete(task)
-assert Path(\"{wait_file}\").exists()
-                                                            """
-                )
-            notebook_nodes["cells"][idx] = checkpoint_cell
+assert Path(\"{}\").exists()
+"""
+
+PATHS = [
+    "examples/homomorphic-encryption",
+    "examples/duet/dcgan",
+    "examples/duet/super_resolution",
+    "examples/private-ai-series/duet_basics",
+    "examples/private-ai-series/duet_iris_classifier",
+    "examples/differential-privacy/opacus" "examples/duet/mnist",
+    "examples/duet/mnist_lightning",
+    "examples/duet/reinforcement_learning",
+]
+
+NOTEBOOK_TESTS_PATH = Path("tests/syft/notebooks")
+CHECKPOINT_PATH = Path("tests/syft/notebooks/checkpoints")
+TARGETS = generate_targets()
+
+
+def generate_duet_notebooks() -> None:
+    tests = defaultdict(list)
+
+    SLEEP_TIME = 500
 
     try:
-        exporter = PythonExporter()
+        os.makedirs(NOTEBOOK_TESTS_PATH, exist_ok=True)
+    except BaseException as e:
+        print("os.makedirs failed ", e)
 
-        (body, resources) = exporter.from_notebook_node(notebook_nodes)
-        write_file = FilesWriter()
+    try:
+        shutil.rmtree(CHECKPOINT_PATH)
+    except BaseException as e:
+        print("rmtree failed ", e)
 
-        # replace empty cells with print statements for easy debugging
-        empty_cell = "# In[ ]:"
-        counter = 1
-        cell_type = "DO" if is_do else "DS"
-        while empty_cell in body:
-            body = body.replace(empty_cell, f"print('{cell_type} Cell: {counter}')", 1)
-            counter += 1
+    try:
+        os.makedirs(CHECKPOINT_PATH, exist_ok=True)
+    except BaseException as e:
+        print("os.makedirs failed ", e)
 
-        # replace any test variables / lines to make things faster in test mode
-        json_file = f"{path}.json"
-        if os.path.exists(json_file):
-            with open(json_file, "r") as f:
-                json_rules = json.loads(f.read())
+    testcase_lib = {}
 
-                for rules in json_rules["replace_lines"]:
-                    try:
-                        body = re.sub(
-                            rules["match"], rules["replace"], body, flags=re.MULTILINE
+    for path in TARGETS:
+        testname = re.sub("[^0-9a-zA-Z]+", "_", str(path))
+        output = NOTEBOOK_TESTS_PATH / testname
+
+        file_name = str(path.stem)
+        is_do = False
+        is_ds = False
+
+        if file_name.endswith("_Data_Scientist"):
+            testcase = file_name.replace("_Data_Scientist", "")
+            tests[testcase].append(testname)
+            is_ds = True
+        elif file_name.endswith("_Data_Owner"):
+            testcase = file_name.replace("_Data_Owner", "")
+            tests[testcase].append(testname)
+            is_do = True
+        else:
+            continue
+
+        load_lib_search = r"load_lib\(\W+([a-z_-]+)\W+\)"
+
+        with open(path, "r") as f:
+            load_lib_results = re.search(load_lib_search, str(f.read()), re.IGNORECASE)
+            if load_lib_results:
+                lib_name = load_lib_results.group(1)
+                testcase_lib[testcase] = lib_name
+
+        notebook_nodes = nbformat.read(path, as_version=4)
+
+        custom_cell = nbformat.v4.new_code_cell(source=asyncio_event_loop)
+        notebook_nodes["cells"].insert(0, custom_cell)
+
+        for idx, cell in enumerate(notebook_nodes["cells"]):
+            if cell["cell_type"] == "code" and "loopback=True" in cell["source"]:
+                network_url = "f'http://0.0.0.0:{get_global_var()}'"
+                notebook_nodes["cells"][idx]["source"] = cell["source"].replace(
+                    "loopback=True", f"loopback=True, network_url={network_url}"
+                )
+            if cell["cell_type"] == "markdown" and "Checkpoint" in cell["source"]:
+                checkpoint = (
+                    cell["source"]
+                    .lower()
+                    .split("checkpoint")[1]
+                    .strip()
+                    .split(":")[0]
+                    .strip()
+                )
+
+                testcase_checkpoint_dir = f"checkpoints/{testcase}/"
+
+                # For DO, we wait until DS gets to the same checkpoint
+                if is_do:
+                    ck_file = testcase_checkpoint_dir + (
+                        testcase + "_DO_checkpoint_" + str(checkpoint)
+                    )
+                    wait_file = testcase_checkpoint_dir + (
+                        testcase + "_DS_checkpoint_" + str(checkpoint)
+                    )
+                    checkpoint_cell = nbformat.v4.new_code_cell(
+                        source=checkpoint_template.format(
+                            ck_file, SLEEP_TIME, wait_file
                         )
-                    except Exception as e:
-                        print(f"Failed to replace rule {rules} for test: {path}. {e}")
+                    )
 
-        write_file.write(output=body, resources=resources, notebook_name=str(output))
-    except Exception as e:
-        print(f"There was a problem exporting the file(s): {e}")
+                # For DS, we wait until DO gets to the next checkpoint
+                elif is_ds:
+                    ck_file = testcase_checkpoint_dir + (
+                        testcase + "_DS_checkpoint_" + str(checkpoint)
+                    )
+                    wait_file = testcase_checkpoint_dir + (
+                        testcase + "_DO_checkpoint_" + str(int(checkpoint) + 1)
+                    )
+                    checkpoint_cell = nbformat.v4.new_code_cell(
+                        source=checkpoint_ack_template.format(
+                            ck_file, SLEEP_TIME, wait_file, wait_file
+                        )
+                    )
+                notebook_nodes["cells"][idx] = checkpoint_cell
 
-for case in tests:
-    test = tests[case]
-    if len(test) != 2:
-        print("invalid testcase ", test)
+        try:
+            exporter = PythonExporter()
 
-    print(case, test)
+            (body, resources) = exporter.from_notebook_node(notebook_nodes)
+            write_file = FilesWriter()
 
-    template = open(output_dir / "duet_test.py.template").read()
+            # replace empty cells with print statements for easy debugging
+            empty_cell = "# In[ ]:"
+            counter = 1
+            cell_type = "DO" if is_do else "DS"
+            while empty_cell in body:
+                body = body.replace(
+                    empty_cell, f"print('{cell_type} Cell: {counter}')", 1
+                )
+                counter += 1
 
-    output_py = template.replace("{{TESTCASE}}", str(case))
-    output_py = output_py.replace("21000", f"{PORTS[case]}")
-    output_py = output_py.replace("checkpoints", f"checkpoints/{case}")
+            # replace any test variables / lines to make things faster in test mode
+            json_file = f"{path}.json"
+            if os.path.exists(json_file):
+                with open(json_file, "r") as f:
+                    json_rules = json.loads(f.read())
 
-    for script in test:
-        if "Data_Owner" in script:
-            output_py = output_py.replace("{{DO_SCRIPT}}", script)
-        elif "Data_Scientist" in script:
-            output_py = output_py.replace("{{DS_SCRIPT}}", script)
+                    for rules in json_rules["replace_lines"]:
+                        try:
+                            body = re.sub(
+                                rules["match"],
+                                rules["replace"],
+                                body,
+                                flags=re.MULTILINE,
+                            )
+                        except Exception as e:
+                            print(
+                                f"Failed to replace rule {rules} for test: {path}. {e}"
+                            )
 
-    decorator = ""
-    if case in testcase_lib:
-        lib_name = testcase_lib[case]
-        decorator = f"@pytest.mark.vendor(lib='{lib_name}')"
-    output_py = output_py.replace("{{LIB_DECORATOR}}", decorator)
+            write_file.write(
+                output=body, resources=resources, notebook_name=str(output)
+            )
+        except Exception as e:
+            print(f"There was a problem exporting the file(s): {e}")
 
-    with open(output_dir / f"duet_{case}_test.py", "w") as out_py:
-        out_py.write(output_py)
+    for case in tests:
+        test = tests[case]
+        if len(test) != 2:
+            print("invalid testcase ", test)
+
+        print(case, test)
+
+        template = open(NOTEBOOK_TESTS_PATH / "duet_test.py.template").read()
+
+        output_py = template.replace("{{TESTCASE}}", str(case))
+        output_py = output_py.replace("checkpoints", f"checkpoints/{case}")
+
+        for script in test:
+            if "Data_Owner" in script:
+                output_py = output_py.replace("{{DO_SCRIPT}}", script)
+            elif "Data_Scientist" in script:
+                output_py = output_py.replace("{{DS_SCRIPT}}", script)
+
+        decorator = ""
+        if case in testcase_lib:
+            lib_name = testcase_lib[case]
+            decorator = f"@pytest.mark.vendor(lib='{lib_name}')"
+        output_py = output_py.replace(
+            "{{LIB_DECORATOR}}", decorator + "\n@pytest.mark.duet"
+        )
+
+        with open(NOTEBOOK_TESTS_PATH / f"duet_{case}_test.py", "w") as out_py:
+            out_py.write(output_py)
+
+
+if __name__ == "__main__":
+    generate_duet_notebooks()
--- tests/beta_test.py
+++
@@ -1,6 +0,0 @@
-from .conftest import SIGNALING_SERVER_PORT
-import socket
-
-def test_signaling_server(signaling_server):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        assert s.connect_ex(("127.0.0.1", SIGNALING_SERVER_PORT)) == 0
