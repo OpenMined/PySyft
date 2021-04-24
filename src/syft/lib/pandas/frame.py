42a94f72650379f7c53689c11a95cfda9eb91796
Koukyosyumei <koukyosyumei@hotmail.com>
2021-04-24 21:05:44 +0900

    use pyarrow as serde method of pandas.dataframe

--- src/syft/lib/pandas/frame.py
+++ src/syft/lib/pandas/frame.py
@@ -1,26 +1,20 @@
 # third party
 import pandas as pd
+import pyarrow as pa
 
 # syft relative
 from ...generate_wrapper import GenerateWrapper
-from ...lib.python.dict import Dict
-from ...lib.python.primitive_factory import PrimitiveFactory
 from ...proto.lib.pandas.frame_pb2 import PandasDataFrame as PandasDataFrame_PB
 
 
 def object2proto(obj: pd.DataFrame) -> PandasDataFrame_PB:
-    pd_dict = PrimitiveFactory.generate_primitive(value=obj.to_dict())
-    dict_proto = pd_dict._object2proto()
-
-    return PandasDataFrame_PB(
-        id=dict_proto.id,
-        dataframe=dict_proto,
-    )
+    table = pa.Table.from_pandas(obj)
+    return PandasDataFrame_PB(dataframe=pa.serialize(table).to_buffer().to_pybytes())
 
 
 def proto2object(proto: PandasDataFrame_PB) -> pd.DataFrame:
-    dataframe_dict = Dict._proto2object(proto=proto.dataframe)
-    return pd.DataFrame.from_dict(dataframe_dict.upcast())
+    reconstructed_buf = pa.py_buffer(proto.dataframe)
+    return pa.deserialize(reconstructed_buf).to_pandas()
 
 
 GenerateWrapper(
