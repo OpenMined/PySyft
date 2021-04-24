9334f8a6754850cfb6d7ddc6ece10e376136205e
Koukyosyumei <koukyosyumei@hotmail.com>
2021-04-24 21:57:51 +0900

    make docstrings end with periods

--- src/syft/lib/pandas/frame.py
+++ src/syft/lib/pandas/frame.py
@@ -8,13 +8,13 @@ from ...proto.lib.pandas.frame_pb2 import PandasDataFrame as PandasDataFrame_PB
 
 
 def object2proto(obj: pd.DataFrame) -> PandasDataFrame_PB:
-    """convert pd.DataFrame to PandasDataFrame_PB with pyarrow"""
+    """convert pd.DataFrame to PandasDataFrame_PB with pyarrow."""
     table = pa.Table.from_pandas(obj)
     return PandasDataFrame_PB(dataframe=pa.serialize(table).to_buffer().to_pybytes())
 
 
 def proto2object(proto: PandasDataFrame_PB) -> pd.DataFrame:
-    """convert PandasDataFrame_PB to pd.DataFrame with pyarrow"""
+    """convert PandasDataFrame_PB to pd.DataFrame with pyarrow."""
     reconstructed_buf = pa.py_buffer(proto.dataframe)
     return pa.deserialize(reconstructed_buf).to_pandas()
 
