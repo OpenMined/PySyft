7192fc990d2882262f1208e0c416f8cc81bf54c3
Tudor Cebere <tudorcebere@gmail.com>
2021-05-05 14:56:42 +0300

    entity cleaned up and added into syft/lib/ast

---
+++ tests/syft/lib/adp/entity_test.py
@@ -0,0 +1,13 @@
+# syft absolute
+from syft import deserialize
+from syft import serialize
+from syft.lib.adp.entity import Entity
+
+
+def test_serde() -> None:
+    ent = Entity(name="test")
+    serialized = serialize(ent, to_bytes=True)
+    deserialized = deserialize(serialized, from_bytes=True)
+
+    assert ent.name == deserialized.name
+    assert ent.id == deserialized.id
