2e855992c6f691038474284018860af6d4810d3e
Tudor Cebere <tudorcebere@gmail.com>
2021-05-13 14:55:56 +0300

    comment test

--- scripts/parse_setup.py
+++ scripts/parse_setup.py
@@ -1,6 +1,11 @@
 lines = open("setup.cfg").readlines()
 mark = "#install-custom-dependency"
+trigger = False
 
 for line in lines:
-    if line.startswith(";") and line.endswith("#install-custom-dependency\n"):
+    if "[lib]" in line:
+        trigger = True
+        continue
+
+    if trigger:
         print(line[1:].rsplit(mark)[0])
