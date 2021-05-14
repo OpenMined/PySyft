88e66f0d255128d15968f1af053e492c8931ca43
Madhava Jay <me@madhavajay.com>
2021-05-14 18:37:09 +1000

    Fixed issue where MNIST changed in torchvision==0.9.1 again
    - Using updated scripts/mnist.py

--- packages/syft/tests/syft/core/fl/model-centric/mcfl_create_execute_plan_test.py
+++ packages/syft/tests/syft/core/fl/model-centric/mcfl_create_execute_plan_test.py
@@ -567,30 +567,6 @@ def train_with_hosted_training_plan(
     model_name = name
     model_version = "1.0"
 
-    # syft absolute
-    # TorchVision hotfix https://github.com/pytorch/vision/issues/3549
-
-    datasets.MNIST.resources = [
-        (
-            "https://ossci-datasets.s3.amazonaws.com/mnist/train-images-idx3-ubyte.gz",
-            "f68b3c2dcbeaaa9fbdd348bbdeb94873",
-        ),
-        (
-            "https://ossci-datasets.s3.amazonaws.com/mnist/train-labels-idx1-ubyte.gz",
-            "d53e105ee54ea40749a09fcbcd1e9432",
-        ),
-        (
-            "https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images-idx3-ubyte.gz",
-            "9fb629c4189551a2d022fa330f9573f3",
-        ),
-        (
-            "https://ossci-datasets.s3.amazonaws.com/mnist/t10k-labels-idx1-ubyte.gz",
-            "ec29112dd5afa0611ce80d1b7f02629c",
-        ),
-    ]
-    datasets.MNIST(get_root_data_path(), train=True, download=True)
-    datasets.MNIST(get_root_data_path(), train=False, download=True)
-
     tfs = transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
     )
