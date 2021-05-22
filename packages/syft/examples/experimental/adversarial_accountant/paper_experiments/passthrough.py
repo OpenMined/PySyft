a3561489f76eb7444828c6aa526b6a5e935c5acb
Andrew Liam Trask <andrewliamtrask@Ambers-iMac-Pro.local>
2021-05-22 03:03:54 +0100

    Make progress on tensor/scalar chaining, SingleEntityPhiTensor, and AutogradTensor

--- packages/syft/examples/experimental/adversarial_accountant/paper_experiments/passthrough.py
+++ packages/syft/examples/experimental/adversarial_accountant/paper_experiments/passthrough.py
@@ -1,65 +1,76 @@
-# third party
 import numpy as np
 
 HANDLED_FUNCTIONS = {}
 
-
 def inputs2child(*args, **kwargs):
     args = [x.child if isinstance(x, PassthroughTensor) else x for x in args]
-    kwargs = {
-        x.key: x.value.child if isinstance(x.value, PassthroughTensor) else x.value
-        for x in kwargs.items()
-    }
-    return args, kwargs
-
+    kwargs = {x[0] : x[1].child if isinstance(x[1], PassthroughTensor) else x[1] for x in kwargs.items()}
+    return args,kwargs
 
 class PassthroughTensor(np.lib.mixins.NDArrayOperatorsMixin):
     """A simple tensor class which passes method/function calls to self.child"""
-
+    
     def __init__(self, child):
         self.child = child
 
     def __len__(self):
         return len(self.child)
-
+        
     @property
     def shape(self):
-        return self.child.shape
+        return self.child.shape        
+    
+    def __add__(self, other):
+        return self.__class__(self.child + other.child)
+    
+    def manual_dot(self, other):
+        
+        expanded_self = self.repeat(other.shape[1]).reshape(self.shape[0], self.shape[1], other.shape[1])
+        expanded_other = other.repeat(self.shape[0]).reshape(other.shape[0], other.shape[1], self.shape[0]).transpose(2,0,1)
+        
+        prod = expanded_self * expanded_other
+        result = prod.sum(1)
+        
+        return result    
+    
+    def dot(self, other):
+        
+        if isinstance(other, self.__class__):
+            return self.__class__(self.child.dot(other.child))
 
-    def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
-        if method == "__call__":
-
-            inputs, kwargs = inputs2child(*inputs, **kwargs)
-
-            return self.__class__(ufunc(*inputs, **kwargs))
-        else:
-            return NotImplemented
-
+        return self.__class__(self.child.dot(other))
+    
+    def repeat(self, n):
+        return self.__class__(self.child.repeat(n))
+    
+    def transpose(self, *args, **kwargs):
+        return self.child.transpose(*args, **kwargs)
+        
     def __array_function__(self, func, types, args, kwargs):
-        args, kwargs = inputs2child(*args, **kwargs)
-
+#         args, kwargs = inputs2child(*args, **kwargs)
+            
         # Note: this allows subclasses that don't override
         # __array_function__ to handle PassthroughTensor objects.
         if not all(issubclass(t, self.__class__) for t in types):
             return NotImplemented
-
-        if func in HANDLED_FUNCTIONS:
-            return self.__class__(HANDLED_FUNCTIONS[func](*args, **kwargs))
+        
+        if func in HANDLED_FUNCTIONS[self.__class__]:
+            return HANDLED_FUNCTIONS[self.__class__][func](*args, **kwargs)
         else:
-            return self.__class__(func(*args, **kwargs))
-
+            return self.__class__(func(*args, **kwargs))        
+        
     def __repr__(self):
         return f"{self.__class__.__name__}(child={self.child})"
 
 
+    
+    
 def implements(tensor_type, np_function):
-    "Register an __array_function__ implementation for DiagonalArray objects."
-
-    def decorator(func):
-        if tensor_type not in HANDLED_FUNCTIONS:
-            HANDLED_FUNCTIONS[tensor_type] = {}
-
-        HANDLED_FUNCTIONS[tensor_type][np_function] = func
-        return func
-
-    return decorator
+        "Register an __array_function__ implementation for DiagonalArray objects."
+        def decorator(func):
+            if tensor_type not in HANDLED_FUNCTIONS:
+                HANDLED_FUNCTIONS[tensor_type] = {}
+                
+            HANDLED_FUNCTIONS[tensor_type][np_function] = func
+            return func
+        return decorator
