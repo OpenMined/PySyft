5cf0a70115b81ae2e8d95e8ea51fa8aa9f9219f7
kamathhrishi@gmail.com <kamathhrishi@gmail.com>
2021-05-22 21:43:26 +0530

    Initialize RSTensor proto

--- packages/syft/src/syft/lib/sympc/share.py
+++ packages/syft/src/syft/lib/sympc/share.py
@@ -2,12 +2,14 @@
 
 # third party
 from sympc.tensor import ShareTensor
+from symc.tensor import ReplicatedSharedTensor
 
 # syft relative
 from ...generate_wrapper import GenerateWrapper
 from ...lib.torch.tensor_util import protobuf_tensor_deserializer
 from ...lib.torch.tensor_util import protobuf_tensor_serializer
 from ...proto.lib.sympc.share_tensor_pb2 import ShareTensor as ShareTensor_PB
+from ...proto.lib.sympc.replicatedshared_tensor_pb2 import ReplicatedSharedTensor as ReplicatedSharedTensor_PB
 from .session_util import protobuf_session_deserializer
 from .session_util import protobuf_session_serializer
 
@@ -42,6 +44,40 @@ GenerateWrapper(
     wrapped_type=ShareTensor,
     import_path="sympc.tensor.ShareTensor",
     protobuf_scheme=ShareTensor_PB,
+    type_object2proto=object2proto,
+    type_proto2object=proto2object,
+)
+
+def object2proto(obj: object) -> ReplicatedSharedTensor_PB:
+
+    share: ShareTensor = obj
+
+    session = protobuf_session_serializer(share.session)
+    proto = ReplicatedSharedTensor_PB(session=session)
+
+    tensor_data = getattr(share.tensor, "data", None)
+    if tensor_data is not None:
+        proto.tensor.tensor.CopyFrom(protobuf_tensor_serializer(tensor_data))
+
+    return proto
+
+
+def proto2object(proto: ReplicatedSharedTensor_PB) -> ReplicatedSharedTensor:
+    session = protobuf_session_deserializer(proto=proto.session)
+
+    data = protobuf_tensor_deserializer(proto.tensor.tensor)
+    share = ReplicatedSharedTensor(data=None, session=session)
+
+    # Manually put the tensor since we do not want to re-encode it
+    share.tensor = data.type(session.tensor_type)
+
+    return share
+
+
+GenerateWrapper(
+    wrapped_type=ReplicatedSharedTensor_PB,
+    import_path="sympc.tensor.ReplicatedSharedTensor",
+    protobuf_scheme=ReplicatedSharedTensor_PB,
     type_object2proto=object2proto,
     type_proto2object=proto2object,
 )
