9265eac48202bc1b2e749c5f40f3ecdf766fcf33
kamathhrishi@gmail.com <kamathhrishi@gmail.com>
2021-05-23 10:11:38 +0530

    Fixed function names for RST sympc lib

--- packages/syft/src/syft/lib/sympc/share.py
+++ packages/syft/src/syft/lib/sympc/share.py
@@ -1,15 +1,17 @@
 # stdlib
 
 # third party
-from sympc.tensor import ShareTensor
 from symc.tensor import ReplicatedSharedTensor
+from sympc.tensor import ShareTensor
 
 # syft relative
 from ...generate_wrapper import GenerateWrapper
 from ...lib.torch.tensor_util import protobuf_tensor_deserializer
 from ...lib.torch.tensor_util import protobuf_tensor_serializer
+from ...proto.lib.sympc.replicatedshared_tensor_pb2 import (
+    ReplicatedSharedTensor as ReplicatedSharedTensor_PB,
+)
 from ...proto.lib.sympc.share_tensor_pb2 import ShareTensor as ShareTensor_PB
-from ...proto.lib.sympc.replicatedshared_tensor_pb2 import ReplicatedSharedTensor as ReplicatedSharedTensor_PB
 from .session_util import protobuf_session_deserializer
 from .session_util import protobuf_session_serializer
 
@@ -48,7 +50,8 @@ GenerateWrapper(
     type_proto2object=proto2object,
 )
 
-def object2proto(obj: object) -> ReplicatedSharedTensor_PB:
+
+def object2proto_rst(obj: object) -> ReplicatedSharedTensor_PB:
 
     share: ShareTensor = obj
 
@@ -62,7 +65,7 @@ def object2proto(obj: object) -> ReplicatedSharedTensor_PB:
     return proto
 
 
-def proto2object(proto: ReplicatedSharedTensor_PB) -> ReplicatedSharedTensor:
+def proto2object_rst(proto: ReplicatedSharedTensor_PB) -> ReplicatedSharedTensor:
     session = protobuf_session_deserializer(proto=proto.session)
 
     data = protobuf_tensor_deserializer(proto.tensor.tensor)
@@ -78,6 +81,6 @@ GenerateWrapper(
     wrapped_type=ReplicatedSharedTensor_PB,
     import_path="sympc.tensor.ReplicatedSharedTensor",
     protobuf_scheme=ReplicatedSharedTensor_PB,
-    type_object2proto=object2proto,
-    type_proto2object=proto2object,
+    type_object2proto=object2proto_rst,
+    type_proto2object=proto2object_rst,
 )
