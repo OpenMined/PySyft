0e8cc2feabfcc5af4f5acdd5f71a681d37c49b86
Madhava Jay <me@madhavajay.com>
2021-05-25 00:48:59 +1000

    Added a bunch of ops to PassthroughTensor
    - Created Op Tests notebook test suite for comparing to np and torch

--- packages/syft/examples/experimental/adversarial_accountant/paper_experiments/passthrough.py
+++ packages/syft/examples/experimental/adversarial_accountant/paper_experiments/passthrough.py
@@ -1,146 +1,439 @@
+# future
+from __future__ import annotations
+
+# stdlib
+from typing import Optional
+from typing import Union
+from typing import Tuple as TypeTuple
+
+# third party
 import numpy as np
 
 HANDLED_FUNCTIONS = {}
 
+AcceptableSimpleType = Union[int, bool, float, np.ndarray]
+
+
 def inputs2child(*args, **kwargs):
     args = [x.child if isinstance(x, PassthroughTensor) else x for x in args]
-    kwargs = {x[0] : x[1].child if isinstance(x[1], PassthroughTensor) else x[1] for x in kwargs.items()}
-    return args,kwargs
+    kwargs = {
+        x[0]: x[1].child if isinstance(x[1], PassthroughTensor) else x[1]
+        for x in kwargs.items()
+    }
+    return args, kwargs
+
 
 def is_acceptable_simple_type(obj):
-    return isinstance(obj, (int,bool,float, np.ndarray))
+    return isinstance(obj, (int, bool, float, np.ndarray))
+
 
 class PassthroughTensor(np.lib.mixins.NDArrayOperatorsMixin):
     """A simple tensor class which passes method/function calls to self.child"""
-    
+
     def __init__(self, child):
         self.child = child
 
     def __len__(self):
         return len(self.child)
-        
+
     @property
     def shape(self):
-        return self.child.shape        
-    
+        return self.child.shape
+
+    def __abs__(self) -> PassthroughTensor:
+        return self.__class__(self.child.__abs__())
+
     def __add__(self, other):
+        if is_acceptable_simple_type(other):
+            return self.__class__(self.child + other)
         return self.__class__(self.child + other.child)
-    
+
+    def __radd__(self, other):
+        return self.__class__(self + other)
+
     def __sub__(self, other):
-        return self.__class__(self.child - other.child)    
-    
-    def __neg__(self):
+        if is_acceptable_simple_type(other):
+            return self.__class__(self.child - other)
+        return self.__class__(self.child - other.child)
+
+    def __rsub__(self, other):
+        return self.__class__(self.__neg__() + other)
+
+    def __gt__(
+        self,
+        other: Union[PassthroughTensor, AcceptableSimpleType],
+    ) -> PassthroughTensor:
+        if is_acceptable_simple_type(other):
+            return self.__class__(self.child > other)
+
+        return self.__class__(self.child > other.child)
+
+    def __ge__(
+        self,
+        other: Union[PassthroughTensor, AcceptableSimpleType],
+    ) -> PassthroughTensor:
+        if is_acceptable_simple_type(other):
+            return self.__class__(self.child >= other)
+
+        return self.__class__(self.child >= other.child)
+
+    def __lt__(
+        self,
+        other: Union[PassthroughTensor, AcceptableSimpleType],
+    ) -> PassthroughTensor:
+        if is_acceptable_simple_type(other):
+            return self.__class__(self.child < other)
+
+        return self.__class__(self.child < other.child)
+
+    def __le__(
+        self,
+        other: Union[PassthroughTensor, AcceptableSimpleType],
+    ) -> PassthroughTensor:
+        if is_acceptable_simple_type(other):
+            return self.__class__(self.child <= other)
+
+        return self.__class__(self.child <= other.child)
+
+    def __ne__(
+        self,
+        other: Union[PassthroughTensor, AcceptableSimpleType],
+    ) -> PassthroughTensor:
+        if is_acceptable_simple_type(other):
+            return self.__class__(self.child != other)
+
+        return self.__class__(self.child != other.child)
+
+    def __eq__(
+        self,
+        other: Union[PassthroughTensor, AcceptableSimpleType],
+    ) -> PassthroughTensor:
+        if is_acceptable_simple_type(other):
+            return self.__class__(self.child == other)
+
+        return self.__class__(self.child == other.child)
+
+    def __floordiv__(
+        self,
+        other: Union[PassthroughTensor, AcceptableSimpleType],
+    ) -> PassthroughTensor:
+        if is_acceptable_simple_type(other):
+            return self.__class__(self.child.__floordiv__(other))
+
+        return self.__class__(self.child.__floordiv__(other.child))
+
+    def __rfloordiv__(
+        self,
+        other: Union[PassthroughTensor, AcceptableSimpleType],
+    ) -> PassthroughTensor:
+        if is_acceptable_simple_type(other):
+            return self.__class__(other.__floordiv__(self.child))
+
+        return self.__class__(other.child.__floordiv__(self.child))
+
+    def __lshift__(
+        self,
+        other: Union[PassthroughTensor, AcceptableSimpleType],
+    ) -> PassthroughTensor:
+        if is_acceptable_simple_type(other):
+            return self.__class__(self.child.__lshift__(other))
+
+        return self.__class__(self.child.__lshift__(other.child))
+
+    def __rlshift__(
+        self,
+        other: Union[PassthroughTensor, AcceptableSimpleType],
+    ) -> PassthroughTensor:
+        if is_acceptable_simple_type(other):
+            return self.__class__(other.__lshift__(self.child))
+
+        return self.__class__(other.child.__lshift__(self.child))
+
+    def __rshift__(
+        self,
+        other: Union[PassthroughTensor, AcceptableSimpleType],
+    ) -> PassthroughTensor:
+        if is_acceptable_simple_type(other):
+            return self.__class__(self.child.__rshift__(other))
+
+        return self.__class__(self.child.__rshift__(other.child))
+
+    def __rrshift__(
+        self,
+        other: Union[PassthroughTensor, AcceptableSimpleType],
+    ) -> PassthroughTensor:
+        if is_acceptable_simple_type(other):
+            return self.__class__(other.__rshift__(self.child))
+
+        return self.__class__(other.child.__rshift__(self.child))
+
+    def __pow__(
+        self,
+        other: Union[PassthroughTensor, AcceptableSimpleType],
+    ) -> PassthroughTensor:
+        if is_acceptable_simple_type(other):
+            return self.__class__(self.child.__pow__(other))
+
+        return self.__class__(self.child.__pow__(other.child))
+
+    def __divmod__(
+        self,
+        other: Union[PassthroughTensor, AcceptableSimpleType],
+    ) -> PassthroughTensor:
+        if is_acceptable_simple_type(other):
+            return self.__class__(self.child.__divmod__(other))
+
+        return self.__class__(self.child.__divmod__(other.child))
+
+    def __neg__(self) -> PassthroughTensor:
         return self * -1
-    
+
+    def __index__(self) -> int:
+        return self.child.__index__()
+
+    def __invert__(self) -> PassthroughTensor:
+        return self.child.__invert__()
+
     def copy(self):
         return self.__class__(self.child.copy())
-    
+
     def __mul__(self, other):
-        
         if is_acceptable_simple_type(other):
             return self.__class__(self.child * other)
-        
-        return self.__class__(self.child * other.child)        
-    
+
+        return self.__class__(self.child * other.child)
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __matmul__(
+        self, other: Union[PassthroughTensor, np.ndarray]
+    ) -> PassthroughTensor:
+        if isinstance(other, np.ndarray):
+            return self.__class__(self.child @ other)
+        return self.__class__(self.child @ other.child)
+
+    def __rmatmul__(
+        self, other: Union[PassthroughTensor, np.ndarray]
+    ) -> PassthroughTensor:
+        return other @ self
+
     def __truediv__(self, other):
-        
         if is_acceptable_simple_type(other):
             return self.__class__(self.child / other)
-        
-        return self.__class__(self.child / other.child)        
-    
+
+        return self.__class__(self.child / other.child)
+
+    def __rtruediv__(self, other):
+        return other.__truediv__(self)
+
     def manual_dot(self, other):
-        
-        expanded_self = self.repeat(other.shape[1]).reshape(self.shape[0], self.shape[1], other.shape[1])
-        expanded_other = other.repeat(self.shape[0]).reshape(other.shape[0], other.shape[1], self.shape[0]).transpose(2,0,1)
-        
+
+        expanded_self = self.repeat(other.shape[1]).reshape(
+            self.shape[0], self.shape[1], other.shape[1]
+        )
+        expanded_other = (
+            other.repeat(self.shape[0])
+            .reshape(other.shape[0], other.shape[1], self.shape[0])
+            .transpose(2, 0, 1)
+        )
+
         prod = expanded_self * expanded_other
         result = prod.sum(1)
-        
-        return result    
-    
+
+        return result
+
     def dot(self, other):
         return self.manual_dot(other)
-#         if isinstance(other, self.__class__):
-#             return self.__class__(self.child.dot(other.child))
+        # if isinstance(other, self.__class__):
+        #     return self.__class__(self.child.dot(other.child))
+        # return self.__class__(self.child.dot(other))
 
-#         return self.__class__(self.child.dot(other))
-
-    def __ge__(self, other):
-        return self.__class__(self.child >= other.child)    
-    
-    def __le__(self, other):
-        return self.__class__(self.child <= other.child)
-    
     def reshape(self, *dims):
         return self.__class__(self.child.reshape(*dims))
-    
+
     def repeat(self, *args, **kwargs):
         return self.__class__(self.child.repeat(*args, **kwargs))
-    
+
+    # numpy.resize(a, new_shape)
+    def resize(self, new_shape: Union[int, TypeTuple[int, ...]]) -> PassthroughTensor:
+        return self.__class__(self.child.resize(new_shape))
+
     def sum(self, dim):
         return self.__class__(self.child.sum(dim))
-    
+
+    @property
+    def T(self) -> PassthroughTensor:
+        return self.transpose()
+
     def transpose(self, *args, **kwargs):
         return self.__class__(self.child.transpose(*args, **kwargs))
-        
+
+    def __getitem__(
+        self, other: Union[int, bool, np.array, PassthroughTensor, slice, ellipsis]
+    ) -> Union[PassthroughTensor, AcceptableSimpleType]:
+        if isinstance(other, PassthroughTensor):
+            return self.__class__(self.child.__getitem__(other.child))
+
+        return self.__class__(self.child.__getitem__(other))
+
+    # numpy.argmax(a, axis=None, out=None)
+    def argmax(self, axis: Optional[int]) -> Union[PassthroughTensor, int]:
+        return self.__class__(self.child.argmax(axis))
+
+    def argmin(self, axis: Optional[int]) -> Union[PassthroughTensor, int]:
+        return self.__class__(self.child.argmin(axis))
+
+    # numpy.argsort(a, axis=-1, kind=None, order=None)
+    def argsort(self, axis: Optional[int] = -1) -> PassthroughTensor:
+        return self.__class__(self.child.argsort(axis))
+
+    # ndarray.sort(axis=-1, kind=None, order=None)
+    def sort(self, axis: int = -1, kind: Optional[str] = None) -> PassthroughTensor:
+        return self.__class__(self.child.sort(axis=axis, kind=kind))
+
+    # numpy.clip(a, a_min, a_max, out=None, **kwargs)
+    def clip(
+        self,
+        min: Optional[AcceptableSimpleType] = None,
+        max: Optional[AcceptableSimpleType] = None,
+    ) -> PassthroughTensor:
+        return self.__class__(self.child.clip(min, max))
+
+    # numpy.cumprod(a, axis=None, dtype=None, out=None)
+    def cumprod(self, axis: Optional[int] = None) -> PassthroughTensor:
+        return self.__class__(self.child.cumprod(axis=axis))
+
+    # numpy.cumsum(a, axis=None, dtype=None, out=None)
+    def cumsum(self, axis: Optional[int] = None) -> PassthroughTensor:
+        return self.__class__(self.child.cumsum(axis=axis))
+
+    # numpy.diagonal(a, offset=0, axis1=0, axis2=1)
+    def diagonal(
+        self, offset: int = 0, axis1: int = 0, axis2: int = 1
+    ) -> PassthroughTensor:
+        return self.__class__(self.child.diagonal(offset, axis1, axis2))
+
+    # ndarray.flatten(order='C')
+    def flatten(self, order: str = "C") -> PassthroughTensor:
+        return self.__class__(self.child.flatten(order))
+
+    # numpy.mean(a, axis=None, dtype=None, out=None, keepdims=<no value>, *, where=<no value>)
+    def mean(
+        self, axis: Optional[Union[int, TypeTuple[int, ...]]] = None
+    ) -> Union[PassthroughTensor, np.floating]:
+        return self.__class__(self.child.mean(axis=axis))
+
+    # ndarray.max(axis=None, out=None, keepdims=False, initial=<no value>, where=True)
+    def max(
+        self, axis: Optional[Union[int, TypeTuple[int, ...]]] = None
+    ) -> Union[PassthroughTensor, np.number]:
+        return self.__class__(self.child.max(axis=axis))
+
+    # ndarray.min(axis=None, out=None, keepdims=False, initial=<no value>, where=True)
+    def min(
+        self, axis: Optional[Union[int, TypeTuple[int, ...]]] = None
+    ) -> Union[PassthroughTensor, np.number]:
+        return self.__class__(self.child.min(axis=axis))
+
+    @property
+    def ndim(self) -> np.unsignedinteger:
+        return self.child.ndim
+
+    # ndarray.prod(axis=None, dtype=None, out=None, keepdims=False, initial=1, where=True)
+    def prod(
+        self, axis: Optional[Union[int, TypeTuple[int, ...]]] = None
+    ) -> Union[PassthroughTensor, np.number]:
+        return self.child.prod(axis=axis)
+
+    # numpy.squeeze(a, axis=None)
+    def squeeze(
+        self, axis: Optional[Union[int, TypeTuple[int, ...]]] = None
+    ) -> PassthroughTensor:
+        return self.__class__(self.child.squeeze(axis=axis))
+
+    # numpy.std(a, axis=None, dtype=None, out=None, ddof=0, keepdims=<no value>, *, where=<no value>)
+    def std(
+        self, axis: Optional[Union[int, TypeTuple[int, ...]]] = None
+    ) -> Union[PassthroughTensor, np.number]:
+        return self.child.std(axis=axis)
+
+    # numpy.sum(a, axis=None, dtype=None, out=None, keepdims=<no value>, initial=<no value>, where=<no value>)
+    def sum(
+        self, axis: Optional[Union[int, TypeTuple[int, ...]]] = None
+    ) -> Union[PassthroughTensor, np.number]:
+        return self.child.sum(axis=axis)
+
+    # numpy.take(a, indices, axis=None, out=None, mode='raise')
+    def take(
+        self, indices: Optional[Union[int, TypeTuple[int, ...]]] = None
+    ) -> Union[PassthroughTensor, np.number]:
+        return self.child.take(indices=indices)
+
+    # numpy.repeat(a, repeats, axis=None)
+    # def repeat(
+    #     self, repeats: Union[int, TypeTuple[int, ...]], axis: Optional[int] = None
+    # ) -> PassthroughTensor:
+    #     return self.__class__(self.child.repeat(repeats=repeats, axis=axis))
+
     def __array_function__(self, func, types, args, kwargs):
-#         args, kwargs = inputs2child(*args, **kwargs)
-        
+        #         args, kwargs = inputs2child(*args, **kwargs)
+
         print(func)
-    
+
         # Note: this allows subclasses that don't override
         # __array_function__ to handle PassthroughTensor objects.
         if not all(issubclass(t, self.__class__) for t in types):
             return NotImplemented
-        
+
         if func in HANDLED_FUNCTIONS[self.__class__]:
             return HANDLED_FUNCTIONS[self.__class__][func](*args, **kwargs)
         else:
-            return self.__class__(func(*args, **kwargs))        
+            return self.__class__(func(*args, **kwargs))
 
-#     def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
-#         print(ufunc)
-#         print(method)
-#         print(inputs)
-#         print(kwargs)
-#         return NotImplemented
-#         print()
-#         if method == '__call__':
-#             N = None
-#             scalars = []
-#             for input in inputs:
-#                 if isinstance(input, Number):
-#                     scalars.append(input)
-#                 elif isinstance(input, self.__class__):
-#                     scalars.append(input._i)
-#                     if N is not None:
-#                         if N != self._N:
-#                             raise TypeError("inconsistent sizes")
-#                     else:
-#                         N = self._N
-#                 else:
-#                     return NotImplemented
-#             return self.__class__(N, ufunc(*scalars, **kwargs))
-#         else:
-#             return NotImplemented
-        
+    #     def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
+    #         print(ufunc)
+    #         print(method)
+    #         print(inputs)
+    #         print(kwargs)
+    #         return NotImplemented
+    #         print()
+    #         if method == '__call__':
+    #             N = None
+    #             scalars = []
+    #             for input in inputs:
+    #                 if isinstance(input, Number):
+    #                     scalars.append(input)
+    #                 elif isinstance(input, self.__class__):
+    #                     scalars.append(input._i)
+    #                     if N is not None:
+    #                         if N != self._N:
+    #                             raise TypeError("inconsistent sizes")
+    #                     else:
+    #                         N = self._N
+    #                 else:
+    #                     return NotImplemented
+    #             return self.__class__(N, ufunc(*scalars, **kwargs))
+    #         else:
+    #             return NotImplemented
+
     def __repr__(self):
         return f"{self.__class__.__name__}(child={self.child})"
 
-    
+
 def implements(tensor_type, np_function):
-        "Register an __array_function__ implementation for DiagonalArray objects."
-        def decorator(func):
-            if tensor_type not in HANDLED_FUNCTIONS:
-                HANDLED_FUNCTIONS[tensor_type] = {}
-                
-            HANDLED_FUNCTIONS[tensor_type][np_function] = func
-            return func
-        return decorator
-    
-    
+    "Register an __array_function__ implementation for DiagonalArray objects."
+
+    def decorator(func):
+        if tensor_type not in HANDLED_FUNCTIONS:
+            HANDLED_FUNCTIONS[tensor_type] = {}
+
+        HANDLED_FUNCTIONS[tensor_type][np_function] = func
+        return func
+
+    return decorator
+
+
 @implements(PassthroughTensor, np.square)
 def square(x):
-    return x*x
-
+    return x * x
--- packages/syft/examples/experimental/adversarial_accountant/paper_experiments/tensor.py
+++ packages/syft/examples/experimental/adversarial_accountant/paper_experiments/tensor.py
@@ -6,28 +6,31 @@ from passthrough import inputs2child
 from ancestors import AutogradTensorAncestor
 from ancestors import SingleEntityPhiTensorAncestor
 
-class Tensor(PassthroughTensor, AutogradTensorAncestor, SingleEntityPhiTensorAncestor):
 
+class Tensor(PassthroughTensor, AutogradTensorAncestor, SingleEntityPhiTensorAncestor):
     def __init__(self, child):
         """data must be a list of numpy array"""
-        
+
         if isinstance(child, list):
             child = np.array(child)
-        
+
         if isinstance(child, th.Tensor):
             child = child.numpy()
-            
-        if not isinstance(child, PassthroughTensor) and not isinstance(child, np.ndarray):
+
+        if not isinstance(child, PassthroughTensor) and not isinstance(
+            child, np.ndarray
+        ):
             raise Exception("Data must be list or nd.array")
-        
+
         super().__init__(child=child)
 
-        
+
 @implements(Tensor, np.mean)
 def mean(*args, **kwargs):
     print("meaning")
     args, kwargs = inputs2child(*args, **kwargs)
     return np.mean(*args, **kwargs)
+
 
 @implements(Tensor, np.max)
 def npmax(*args, **kwargs):
@@ -35,15 +38,18 @@ def npmax(*args, **kwargs):
     args, kwargs = inputs2child(*args, **kwargs)
     return np.max(*args, **kwargs)
 
+
 @implements(Tensor, np.min)
 def npmin(*args, **kwargs):
     print("mining1")
     args, kwargs = inputs2child(*args, **kwargs)
     return np.min(*args, **kwargs)
 
+
 @implements(Tensor, np.square)
 def square(x):
-    return x*x
+    return x * x
+
 
 @implements(Tensor, np.expand_dims)
 def expand_dims(*args, **kwargs):
