fd6aef63c6b5e97b3219f494fdfa7e668b34322b
kamathhrishi@gmail.com <kamathhrishi@gmail.com>
2021-05-27 18:48:56 +0530

    Replicate ShareTensor behaviour with RSTensor

--- packages/syft/src/syft/lib/sympc/rst_share.py
+++ packages/syft/src/syft/lib/sympc/rst_share.py
@@ -13,19 +13,14 @@ from .session_util import protobuf_session_serializer
 
 
 def object2proto(obj: object) -> ReplicatedSharedTensor_PB:
-
     share: ReplicatedSharedTensor = obj
 
     session = protobuf_session_serializer(share.session)
     proto = ReplicatedSharedTensor_PB(session=session)
 
-    tensor_list = []
-
-    for share_tensor in share.shares:
-        tensor_list.append(getattr(share_tensor, "data", None))
-
-    if tensor_list is not None:
-        proto.tensor.tensor.CopyFrom(protobuf_tensor_serializer(tensor_list))
+    tensor_data = getattr(share.shares, "data", None)
+    if tensor_data is not None:
+        proto.tensor.tensor.CopyFrom(protobuf_tensor_serializer(tensor_data))
 
     return proto
 
@@ -34,7 +29,7 @@ def proto2object(proto: ReplicatedSharedTensor_PB) -> ReplicatedSharedTensor:
     session = protobuf_session_deserializer(proto=proto.session)
 
     data = protobuf_tensor_deserializer(proto.tensor.tensor)
-    share = ReplicatedSharedTensor(data=None, session=session)
+    share = ReplicatedSharedTensor(shares=None, session=session)
 
     # Manually put the tensor since we do not want to re-encode it
     share.shares = data.type(session.tensor_type)
