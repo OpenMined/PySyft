26628ee667c1461597a23ee757cd83525a44c218
Andrew Trask <atrask@google.com>
2021-06-12 01:55:03 +0100

    Eliminate extraneous Netwowrk codee

--- packages/grid/apps/domain/src/main/core/node.py
+++ packages/grid/apps/domain/src/main/core/node.py
@@ -16,7 +16,6 @@ from ..routes import setup_blueprint
 from ..routes import users_blueprint
 from ..utils.executor import executor
 from .nodes.domain import GridDomain
-from .nodes.network import GridNetwork
 from .nodes.worker import GridWorker
 from .sleepy_until_configured import SleepyUntilConfigured
 
@@ -44,62 +43,6 @@ def create_worker_app(app, args):
     executor.init_app(app)
 
     return app
-
-
-def create_network_app(app, args, testing=False):
-    test_config = None
-    if args.start_local_db:
-        test_config = {"SQLALCHEMY_DATABASE_URI": "sqlite:///nodedatabase.db"}
-
-    app.register_blueprint(roles_blueprint, url_prefix=r"/roles")
-    app.register_blueprint(users_blueprint, url_prefix=r"/users")
-    app.register_blueprint(setup_blueprint, url_prefix=r"/setup")
-    app.register_blueprint(root_blueprint, url_prefix=r"/")
-    app.register_blueprint(search_blueprint, url_prefix=r"/search")
-    app.register_blueprint(
-        association_requests_blueprint, url_prefix=r"/association-requests/"
-    )
-
-    # Register WebSocket blueprints
-    # Here you should add all the blueprints related to WebSocket routes.
-
-    # grid relative
-    from .database import Role
-    from .database import User
-    from .database import db
-    from .database import seed_db
-    from .database import set_database_config
-
-    global node
-    node = GridNetwork(name=args.name)
-
-    # Set SQLAlchemy configs
-    set_database_config(app, test_config=test_config)
-    s = app.app_context().push()
-
-    db.create_all()
-
-    if not testing:
-        if len(db.session.query(Role).all()) == 0:
-            seed_db()
-
-        role = db.session.query(Role.id).filter_by(name="Owner").first()
-        user = User.query.filter_by(role=role.id).first()
-        if user:
-            signing_key = SigningKey(
-                user.private_key.encode("utf-8"), encoder=HexEncoder
-            )
-            node.signing_key = signing_key
-            node.verify_key = node.signing_key.verify_key
-            node.root_verify_key = node.verify_key
-    db.session.commit()
-
-    app.config["EXECUTOR_PROPAGATE_EXCEPTIONS"] = True
-    app.config["EXECUTOR_TYPE"] = "thread"
-    executor.init_app(app)
-
-    return app
-
 
 def create_domain_app(app, args, testing=False):
     test_config = None
