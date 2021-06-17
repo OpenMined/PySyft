24b1184c9929da8896453358c37c5b5c1a1b95f5
iamtrask <liamtrask@gmail.com>
2021-06-17 23:49:51 +0100

    Switch Syft object store from key-value to SQLAlchemy

---
+++ packages/grid/apps/domain/src/domain.py
@@ -0,0 +1,1 @@
+# this only needed to be here for the conversion from poetry to setup.py.
---
+++ packages/syft/src/syft/core/node/common/tables/bin_obj_metadata.py
@@ -0,0 +1,21 @@
+# third party
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import JSON
+from sqlalchemy import String
+from sqlalchemy.orm.decl_api import DeclarativeMeta
+
+
+def BinObjectMetadata(TableBase: DeclarativeMeta) -> DeclarativeMeta:
+    class ObjectMetadata(TableBase):  # type: ignore
+        __tablename__ = "obj_metadata"
+
+        id = Column(Integer(), primary_key=True, autoincrement=True)
+        obj = Column(Integer, ForeignKey("bin_object.id"))
+        tags = Column(JSON())
+        description = Column(String())
+        read_permissions = Column(JSON())
+        search_permissions = Column(JSON())
+
+    return ObjectMetadata
