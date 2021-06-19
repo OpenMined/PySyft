305edaf97186340737fd81bc8affcbd3609e03e4
jmaunon <juanmixp@gmail.com>
2021-06-19 16:00:37 +0200

    Deleting foo function

--- packages/syft/src/syft/__init__.py
+++ packages/syft/src/syft/__init__.py
@@ -104,7 +104,3 @@ logger.add(sink=sys.stderr, level="CRITICAL")
 
 # TODO: remove this requirement in pytorch lightning
 client_cache: Dict[str, Any] = {}
-
-def foo(a:str):
-    "wrong docstring"
-    pass
