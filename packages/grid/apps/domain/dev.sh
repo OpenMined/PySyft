d4c1ed6e39b159e1139b0b2b61bb06d253e1e40a
Madhava Jay <me@madhavajay.com>
2021-06-22 15:27:21 +1000

    Fixed tox domain.grid and hot reloading

--- packages/grid/apps/domain/dev.sh
+++ packages/grid/apps/domain/dev.sh
@@ -4,10 +4,14 @@
 export FLASK_ENV=development
 
 # use this function as the entry point
-export FLASK_APP=src/app.py:create_app
+APP_SRC=$(pwd)/src
+export FLASK_APP=${APP_SRC}/app.py:create_app
 
 # --start_local_db
 export LOCAL_DATABASE=True
+
+# allow domain imports from the site-packages
+export PYTHONPATH="${PYTHONPATH}:${APP_SRC}"
 
 # run
 flask run
