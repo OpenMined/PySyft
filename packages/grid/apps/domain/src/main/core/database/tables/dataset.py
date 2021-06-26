9577f6b4ef9d11f11a802981554d6f61fb897f2e
iamtrask <liamtrask@gmail.com>
2021-06-26 18:21:24 +0100

    Move dataset to its own file

---
+++ packages/grid/apps/domain/src/main/core/database/tables/dataset.py
@@ -0,0 +1,12 @@
+# grid relative
+from .. import BaseModel
+from .. import db
+
+
+class Dataset(BaseModel):
+    __tablename__ = "dataset"
+
+    id = db.Column(db.String(256), primary_key=True)
+    manifest = db.Column(db.String(2048))
+    description = db.Column(db.String(2048))
+    tags = db.Column(db.JSON())
