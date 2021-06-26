994d7e2c1127ba169eacf2aa98db3b358092089a
iamtrask <liamtrask@gmail.com>
2021-06-26 18:22:07 +0100

    Move bin obj dataset to its own file and fix some broken imports

---
+++ packages/grid/apps/domain/src/main/core/database/tables/bin_obj_dataset.py
@@ -0,0 +1,14 @@
+# grid relative
+from .. import BaseModel
+from .. import db
+
+
+class BinObjDataset(BaseModel):
+    __tablename__ = "bin_obj_dataset"
+
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    name = db.Column(db.String(256))
+    obj = db.Column(db.String(256), db.ForeignKey("bin_object.id"))
+    dataset = db.Column(db.String(256), db.ForeignKey("dataset.id"))
+    dtype = db.Column(db.String(256))
+    shape = db.Column(db.String(256))
