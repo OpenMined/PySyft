17ef792ae5f4194fc21129b8172657364c3bc5b7
Madhava Jay <me@madhavajay.com>
2021-06-26 11:34:24 +1000

    bump ci

--- packages/syft/src/syft/__init__.py
+++ packages/syft/src/syft/__init__.py
@@ -102,5 +102,5 @@ sys.path.append(str(Path(__file__)))
 
 logger.add(sink=sys.stderr, level="CRITICAL")
 
-# TODO: remove this requirement in pytorch lightning, BUMP CI
+# TODO: remove this requirement in pytorch lightning
 client_cache: Dict[str, Any] = {}
