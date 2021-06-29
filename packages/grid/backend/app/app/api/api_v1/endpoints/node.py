c9528919d11803baddbcf60423d296d27470347a
Ionesio Junior <ionesiojr@gmail.com>
2021-06-29 09:29:25 -0300

    UPDATE /syft routes - uncomment /metadata - change /msg to /pysyft to keep compatibility with pysyft client - comment fast api default authentication

--- packages/grid/backend/app/app/api/api_v1/endpoints/node.py
+++ packages/grid/backend/app/app/api/api_v1/endpoints/node.py
@@ -2,6 +2,7 @@ import json
 from typing import Any, List
 
 from fastapi import APIRouter, Depends, HTTPException, Request, Response
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
 from app import crud, models, schemas
@@ -35,47 +36,37 @@ def read_items(
         items = crud.item.get_multi_by_owner(
             db=db, owner_id=current_user.id, skip=skip, limit=limit
         )
-    print("SOME ITEMS")
-    print(items)
     return "hello world"
 
 
-# @router.get("/metadata", response_model=str)
-# def metadata_route():
-#     response_body = {
-#         "metadata": sy.serialize(domain.get_metadata_for_client())
-#         .SerializeToString()
-#         .decode("ISO-8859-1")
-#     }
-#     return json.dumps(response_body)
+@router.get("/metadata", response_model=str)
+def syft_metadata():
+    response_body = {
+         "metadata": sy.serialize(domain.get_metadata_for_client())
+         .SerializeToString()
+         .decode("ISO-8859-1")
+     }
+    return json.dumps(response_body)
 
 
-@router.post("/msg", response_model=str)
-async def msg(
+
+@router.post("/pysyft", response_model=str)
+async def syft(
     request: Request,
-    db: Session = Depends(deps.get_db),
-    skip: int = 0,
-    limit: int = 100,
-    current_user: models.User = Depends(deps.get_current_active_user),
+#    db: Session = Depends(deps.get_db),
+#    skip: int = 0,
+#    limit: int = 100,
+#    current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
 
     data = await request.body()
     obj_msg = sy.deserialize(blob=data, from_bytes=True)
     if isinstance(obj_msg, SignedImmediateSyftMessageWithReply):
         reply = domain.recv_immediate_msg_with_reply(msg=obj_msg)
-        r = Response(response=sy.serialize(obj=reply, to_bytes=True), status=200)
-        r.headers["Content-Type"] = "application/octet-stream"
+        r = Response(sy.serialize(obj=reply, to_bytes=True), media_type="application/octet-stream")
         return r
     elif isinstance(obj_msg, SignedImmediateSyftMessageWithoutReply):
         domain.recv_immediate_msg_without_reply(msg=obj_msg)
     else:
         domain.recv_eventual_msg_without_reply(msg=obj_msg)
-
-    #
-    # if crud.user.is_superuser(current_user):
-    #     items = crud.item.get_multi(db, skip=skip, limit=limit)
-    # else:
-    #     items = crud.item.get_multi_by_owner(
-    #         db=db, owner_id=current_user.id, skip=skip, limit=limit
-
-    return "sending message to domain"
+    return ""
