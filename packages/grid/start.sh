62c2b762654a110eedaf5c6120c944d7adf6754e
iamtrask <liamtrask@gmail.com>
2021-06-30 00:17:34 +0100

    Added some files

---
+++ packages/grid/start.sh
@@ -0,0 +1,2 @@
+docker-compose --file "docker-compose.yml" --project-name "grid" --project-directory "/Users/andrewliamtrask/Dropbox/Laboratory/openmined/PySyft/PySyft/packages/grid" down
+docker-compose up
---
+++ packages/grid/start_notebook.sh
@@ -0,0 +1,1 @@
+docker-compose exec backend bash -c \$JUPYTER
