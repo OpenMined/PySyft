0d3e0d66fdd49c0a0e33658dd666b13cfe61f66c
Ionesio Junior <ionesiojr@gmail.com>
2021-06-30 12:12:57 -0300

    CREATE POST/GET methods to /setup

--- packages/grid/backend/app/app/api/api_v1/endpoints/setup.py
+++ packages/grid/backend/app/app/api/api_v1/endpoints/setup.py
@@ -4,18 +4,24 @@ from typing import Any
 from fastapi import APIRouter
 from fastapi import Request
 from fastapi import Response
+from fastapi import Depends
 import json
+from nacl.encoding import HexEncoder
+from nacl.signing import SigningKey
 
 # syft
 from syft.grid.messages.setup_messages import CreateInitialSetUpMessage
+from syft.grid.messages.setup_messages import GetSetUpMessage
 
 
 from app.core.node import domain
+from app.api import deps
+
 router = APIRouter()
 
 
 @router.post("", response_model=str)
-async def syft_metadata( 
+async def create_setup( 
     request: Request,
 ) -> Any:
     data = json.loads( await request.body())
@@ -25,6 +31,29 @@ async def syft_metadata(
         content=data,
         reply_to=domain.address
     ).sign(signing_key=domain.signing_key)
+
+    reply = domain.recv_immediate_msg_with_reply(msg=msg)
+
+    return Response(
+            json.dumps({"message": reply.message.content}),
+            media_type="application/json",
+    )
+
+
+@router.get("", response_model=str)
+def get_setup( 
+    request: Request,
+    current_user: Any = Depends(deps.get_current_user)
+) -> Any:
+    user_key =  SigningKey(
+        current_user.private_key.encode(),
+        encoder=HexEncoder
+        )
+    
+    msg = GetSetUpMessage(
+        address=domain.address,
+        reply_to=domain.address
+    ).sign(signing_key=user_key)
 
     reply = domain.recv_immediate_msg_with_reply(msg=msg)
 
