db63fbe6f64675c26c421cb84e40800f08b90bf4
Ionesio Junior <ionesiojr@gmail.com>
2021-06-30 09:04:17 -0300

    UPDATE PyGrid routes

--- packages/grid/backend/app/app/api/api_v1/api.py
+++ packages/grid/backend/app/app/api/api_v1/api.py
@@ -2,15 +2,13 @@
 from fastapi import APIRouter
 
 # grid absolute
-from app.api.api_v1.endpoints import items
 from app.api.api_v1.endpoints import login
-from app.api.api_v1.endpoints import node
+from app.api.api_v1.endpoints import syft
 from app.api.api_v1.endpoints import users
-from app.api.api_v1.endpoints import utils
+from app.api.api_v1.endpoints import setup
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
-# api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
-# api_router.include_router(items.router, prefix="/items", tags=["items"])
-api_router.include_router(node.router, prefix="/syft", tags=["syfts"])
+api_router.include_router(syft.router, prefix="/syft", tags=["syfts"])
+api_router.include_router(setup.router, prefix="/setup", tags=["setup"])
