975ec3989856a94f14086b2fbc9ff52d5eafe550
Ionesio Junior <ionesiojr@gmail.com>
2021-06-30 09:09:09 -0300

    ADD /setup route

---
+++ packages/grid/backend/app/app/api/api_v1/endpoints/setup.py
@@ -0,0 +1,20 @@
+from typing import Any 
+
+# third party
+from fastapi import APIRouter
+from fastapi import Request
+from fastapi import Response
+import json
+
+
+from app.core.node import domain
+router = APIRouter()
+
+
+@router.get("", response_model=str)
+async def syft_metadata( request: Request ) -> Any:
+    data = await request.body()
+    return Response(
+            json.dumps({}),
+            media_type="application/json",
+    )
