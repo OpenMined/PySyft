521147bf27e561be551628e7755255b02eda5747
Ionesio Junior <ionesiojr@gmail.com>
2021-06-30 11:47:22 -0300

    FIX /setup endpoint

--- packages/grid/backend/app/app/api/api_v1/endpoints/setup.py
+++ packages/grid/backend/app/app/api/api_v1/endpoints/setup.py
@@ -6,15 +6,29 @@ from fastapi import Request
 from fastapi import Response
 import json
 
+# syft
+from syft.grid.messages.setup_messages import CreateInitialSetUpMessage
+
 
 from app.core.node import domain
 router = APIRouter()
 
 
-@router.get("", response_model=str)
-async def syft_metadata( request: Request ) -> Any:
-    data = await request.body()
+@router.post("", response_model=str)
+async def syft_metadata( 
+    request: Request,
+) -> Any:
+    data = json.loads( await request.body())
+
+    msg = CreateInitialSetUpMessage(
+        address=domain.address,
+        content=data,
+        reply_to=domain.address
+    ).sign(signing_key=domain.signing_key)
+
+    reply = domain.recv_immediate_msg_with_reply(msg=msg)
+
     return Response(
-            json.dumps({}),
+            json.dumps({"message": reply.message.content}),
             media_type="application/json",
     )
