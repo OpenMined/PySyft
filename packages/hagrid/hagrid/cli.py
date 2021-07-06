1b43d2f141eb4d33ad6db3b469a2621007213e4e
iamtrask <andrew@openmined.org>
2021-07-06 17:06:26 +0100

    Added first docker check

--- packages/hagrid/hagrid/cli.py
+++ packages/hagrid/hagrid/cli.py
@@ -1,16 +1,35 @@
 import click
 from .lib import some_function
+from .lib import motorcycle
 
 @click.group()
 def cli():
     pass
 
 @click.command(help="Have your new program say Hi to you!")
-@click.argument('name')
-def hello(name):
+@click.argument('type', type=click.Choice(['domain', 'network']))
+@click.option('--name', default="", required=False, type=str)
+@click.option('--port', default=8081, required=False, type=int)
+@click.option('--tag', default="", required=False, type=str)
+def launch(type, name, port, tag):
+    import os
 
-    print(f'Hi, {name}!')
-    some_function()
+    if  not (type == "domain" or type=="network"):
+        raise Exception("Must specify either a domain or network.")
+
+    docker, compose, word, version = os.popen('docker compose version', 'r').read().split()
+
+    motorcycle()
+    print("Launching a " + str(type) + " PyGrid node on port " + str(port) + "!\n")
+    print("  - TYPE: " + str(type))
+    print("  - NAME: " + str(name))
+    print("  - TAG: " + str(tag))
+    print("  - PORT: " + str(port))
+    print("  - DOCKER: " + version)
+
+    print("\n")
+
+    # some_function()
 
 
-cli.add_command(hello)
+cli.add_command(launch)
--- packages/hagrid/hagrid/lib.py
+++ packages/hagrid/hagrid/lib.py
@@ -1,3 +1,68 @@
+import os
 
 def some_function():
     print('Running lib.some_function')
+
+def motorcycle():
+    print("""
+                                             `
+                                         `.+yys/.`
+                                       ``/NMMMNNs`
+                                    `./shNMMMMMMNs``    `..`
+                                  `-smNMMNNMMMMMMN/.``......`
+                                `.yNMMMMNmmmmNNMMm/.`....`
+                              `:sdNMMMMMMNNNNddddds-`.`` `--. `
+                           `.+dNNNNMMMMMMMMMNNNNmddohmh//hddy/.```..`
+                          `-hNMMMMMMMMMMMMNNdmNNMNNdNNd:sdyoo+/++:..`
+                        ../mMMMMMMMMMMMMMMNNmmmmNMNmNNmdmd/hNNNd+:`
+                        `:mMNNMMMMMMMMMMMMNMNNmmmNNNNNdNNd/NMMMMm::
+                       `:mMNNNMMMMMMMMMMMMMMMNNNNdNMNNmmNd:smMMmh//
+                     ``/mMMMMMMMMMMMMMMMMMMMMMMNmdmNNMMNNNy/osoo/-`
+                    `-sNMMMMMMMMMMMMMMMMMMMMMMMMNNmmMMMMNh-....`
+                   `/dNMMMMMMMMMMMMMMMMMMMMMMMMMMMNNMMMNy.`
+                ``.omNNMMMMMMMMMMMMNMMMMMMMNmmmmNNMMMMN+`
+                `:hmNNMMMMMMMMMMMNo/ohNNNNho+os+-+hNys/`
+                -mNNNNNNMMMMMMMMm+``-yNdd+/mMMMms.-:`
+                .+dmNNNNMMMMMMNd:``:dNN+y`oMMMMMm-.`
+                `+dmmmNNNmmmmy+.   `-+m/s/+MMMMm/--
+               `+mmmhNy/-...```     ``-.-sosyys+/-`
+            ``.smmmsoo``               .oh+-:/:.
+          `.:odmdh/````             `.+d+``````
+     ```/sydNdhy+.`              ``-sds.
+    `:hdmhs::-````               `oNs.`
+```.sdmh/``                    `-ym+`
+ ``ssy+`                     `-yms.`
+   ``                      `:hNy-``
+   `                     `-yMN/```
+                       `-yNhy-
+                     `/yNd/`
+                   `:dNMs``
+                 `.+mNy/.`
+              `.+hNMMs``
+             `:dMMMMh.`""")
+
+    rows, columns = os.popen('stty size', 'r').read().split()
+
+    if int(columns) >= 91:
+
+        print("""
+ _   _       _     _                 _   _       _     _     _   _                       _ 
+| | | |     | |   | |               | | (_)     | |   | |   | | | |                     | |
+| |_| | ___ | | __| |   ___  _ __   | |_ _  __ _| |__ | |_  | |_| | __ _ _ __ _ __ _   _| |
+|  _  |/ _ \| |/ _` |  / _ \| '_ \  | __| |/ _` | '_ \| __| |  _  |/ _` | '__| '__| | | | |
+| | | | (_) | | (_| | | (_) | | | | | |_| | (_| | | | | |_  | | | | (_| | |  | |  | |_| |_|
+\_| |_/\___/|_|\__,_|  \___/|_| |_|  \__|_|\__, |_| |_|\__| \_| |_/\__,_|_|  |_|   \__, (_)
+                                            __/ |                                   __/ |  
+                                           |___/                                   |___/     
+            """)
+    else:
+        print("""
+ _   _       _     _                 _   _                       _ 
+| | | |     | |   | |               | | | |                     | |
+| |_| | ___ | | __| |   ___  _ __   | |_| | __ _ _ __ _ __ _   _| |
+|  _  |/ _ \| |/ _` |  / _ \| '_ \  |  _  |/ _` | '__| '__| | | | |
+| | | | (_) | | (_| | | (_) | | | | | | | | (_| | |  | |  | |_| |_|
+\_| |_/\___/|_|\__,_|  \___/|_| |_| \_| |_/\__,_|_|  |_|   \__, (_)
+                                                            __/ |  
+                                                           |___/    
+        """)
