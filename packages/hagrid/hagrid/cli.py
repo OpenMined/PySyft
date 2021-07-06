128326bb9b4f01b041732ca62c34fd4038cc08dd
iamtrask <andrew@openmined.org>
2021-07-06 21:19:49 +0100

    New updates

--- packages/hagrid/hagrid/cli.py
+++ packages/hagrid/hagrid/cli.py
@@ -1,6 +1,9 @@
 import click
-from .lib import some_function
 from .lib import motorcycle
+import os
+import subprocess
+
+install_path = os.path.dirname(os.path.realpath(__file__))
 
 @click.group()
 def cli():
@@ -12,14 +15,17 @@ def cli():
 @click.option('--port', default=8081, required=False, type=int)
 @click.option('--tag', default="", required=False, type=str)
 def launch(type, name, port, tag):
-    import os
 
-    if  not (type == "domain" or type=="network"):
-        raise Exception("Must specify either a domain or network.")
+    if tag != "":
+        if ' ' in tag:
+            raise Exception("Can't have spaces in --tag. Try something without spaces.")
 
     docker, compose, word, version = os.popen('docker compose version', 'r').read().split()
 
+
+
     motorcycle()
+
     print("Launching a " + str(type) + " PyGrid node on port " + str(port) + "!\n")
     print("  - TYPE: " + str(type))
     print("  - NAME: " + str(name))
@@ -29,7 +35,17 @@ def launch(type, name, port, tag):
 
     print("\n")
 
-    # some_function()
+    cmd = "DOMAIN_PORT="+str(port)
+    cmd += " TRAEFIK_TAG="+tag
+    cmd += " DOMAIN_NAME=\""+name+"\""
+    cmd += " NODE_TYPE="+type
+    cmd += " docker compose -p " + tag
+    cmd += " up"
 
+    install_path =os.path.abspath(os.path.join(os.path.realpath(__file__), '../../../grid/'))
+
+    cmd = "cd " + install_path + ";" + cmd
+    print(cmd)
+    subprocess.call(cmd, shell=True)
 
 cli.add_command(launch)
