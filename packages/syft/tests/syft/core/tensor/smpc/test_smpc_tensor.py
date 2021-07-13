6484244d8de188de8d3ec4a93a3d4f0d42e501a6
George Muraru <murarugeorgec@gmail.com>
2021-07-14 00:10:06 +0100

    Add WIP test

--- packages/syft/tests/syft/core/tensor/smpc/test_smpc_tensor.py
+++ packages/syft/tests/syft/core/tensor/smpc/test_smpc_tensor.py
@@ -3,6 +3,7 @@ import sys
 
 # third party
 import numpy as np
+import pytest
 import torch
 
 # syft absolute
@@ -29,3 +30,27 @@ def test_remote_sharing():
     shares = [share.get_copy() for share in mpc_tensor.child]
     assert all([isinstance(share, ShareTensor) for share in shares])
     assert (mpc_tensor.reconstruct() == value).all()
+
+
+@pytest.mark.parametrize("op_str", ["add", "sub"])
+def test_mpc_private_op(op_str):
+    value_1 = np.array([[1, 2, 3, 4, -5]], dtype=np.int64)
+    value_2 = np.array([10], dtype=np.int64)
+
+    remote_value_1 = clients[0].syft.core.tensor.tensor.Tensor(value)
+    remote_value_2 = clients[4].syft.core.tensor.tensor.Tensor(value)
+
+    mpc_tensor_1 = MPCTensor(
+        parties=clients, secret=remote_value_1, shape=(1, 5), seed_shares=52
+    )
+
+    mpc_tensor_2 = MPCTensor(
+        parties=clients, secret=remote_value_2, shape=(1,), seed_shares=42
+    )
+
+    op = getattr(operator, op_str)
+
+    res = op(mpc_tensor_1, mpc_tensor_1)
+    expected = op(value_1, value_2)
+
+    assert (res == expected).all()
