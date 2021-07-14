7468bf85c3b08106367dd9b6aaf44c41174b8233
arpitvaghela <arpitvaghela9210@gmail.com>
2021-07-14 23:01:48 +0530

    meta-packging

--- packages/syft-libs/pyscaffoldext-syft-support/src/pyscaffoldext/syft_support/extension.py
+++ packages/syft-libs/pyscaffoldext-syft-support/src/pyscaffoldext/syft_support/extension.py
@@ -1,25 +1,25 @@
 # stdlib
-from argparse import ArgumentParser
 import importlib
+from argparse import ArgumentParser
 from pathlib import Path
 from typing import List
 
 # third party
+import pyscaffold.dependencies as deps
 from configupdater.configupdater import ConfigUpdater
 from pyscaffold import structure
 from pyscaffold.actions import Action
 from pyscaffold.actions import ActionParams
 from pyscaffold.actions import ScaffoldOpts
 from pyscaffold.actions import Structure
-import pyscaffold.dependencies as deps
 from pyscaffold.extensions import Extension
 from pyscaffold.extensions import include
 from pyscaffold.extensions.no_skeleton import NoSkeleton
-from pyscaffold.extensions.pre_commit import PreCommit
 from pyscaffold.operations import no_overwrite
 from pyscaffold.structure import merge
 from pyscaffold.templates import add_pyscaffold
 from pyscaffold.templates import get_template
+from pyscaffoldext.markdown.extension import Markdown
 
 # syft relative
 from . import templates
@@ -44,7 +44,7 @@ class SyftSupport(Extension):
             self.flag,
             help=self.help_text,
             nargs=0,
-            action=include(NoSkeleton(), PreCommit(), self),
+            action=include(NoSkeleton(), Markdown(), self),
         )
         return self
 
@@ -71,14 +71,14 @@ def default_opts(opts: ScaffoldOpts) -> ScaffoldOpts:
 def set_pkg_opts(struct: Structure, opts: ScaffoldOpts) -> ActionParams:
 
     path: Path = opts["project_path"]
-    if not str(path).startswith("syft-"):
-        opts["project_path"] = Path("syft-" + str(path))
+    if not str(path).startswith("syft."):
+        opts["project_path"] = Path("syft." + str(path))
 
-    if not opts["package"].startswith("syft-"):
-        opts["package"] = "syft_" + opts["package"]
+    if not opts["package"].startswith("syft."):
+        opts["package"] = "syft." + opts["package"]
 
-    if not opts["name"].startswith("syft-"):
-        opts["name"] = "syft-" + opts["name"]
+    if not opts["name"].startswith("syft."):
+        opts["name"] = "syft." + opts["name"]
 
     module = opts["name"][5:]
     try:
@@ -121,9 +121,6 @@ def add_files(struct: Structure, opts: ScaffoldOpts) -> ActionParams:
     conftest_py = get_template("conftest_py", relative_to=templates.__name__)
     VERSION = get_template("VERSION", relative_to=templates.__name__)
     pyproject_toml = get_template("pyproject_toml", relative_to=templates.__name__)
-    precommit_config = get_template(
-        "pre-commit-config_yaml", relative_to=templates.__name__
-    )
 
     module = importlib.import_module(opts["name"][5:])
     package_support = generate_package_support(module)
@@ -131,7 +128,6 @@ def add_files(struct: Structure, opts: ScaffoldOpts) -> ActionParams:
     files: Structure = {
         "setup.cfg": (setup_cfg, no_overwrite()),
         "pyproject.toml": (pyproject_toml, no_overwrite()),
-        ".pre-commit-config.yaml": precommit_config,
         "proto": {"sample.proto": (proto, no_overwrite())},
         "src": {
             opts["package"]: {
---
+++ packages/syft-libs/setup.py
@@ -0,0 +1,26 @@
+# stdlib
+from pathlib import Path
+
+# third party
+import setuptools
+from setuptools import find_namespace_packages
+
+package_dir = {}
+packages = []
+libs = ["syft"]
+
+for p in Path().glob("*/src/syft/*"):
+    packages += [
+        pkg for pkg in find_namespace_packages(str(p.parent.parent)) if pkg != "syft"
+    ]
+    libs.append(p.stem)
+    package_dir[f"syft.{p.stem}"] = str(p)
+
+setuptools.setup(
+    name="syft.libs",
+    version="0.0.1",
+    package_dir=package_dir,
+    packages=packages,
+    package_data={"": ["package-support.json"]},
+    install_requires=libs,
+)
