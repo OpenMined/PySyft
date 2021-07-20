7df27ff5158ff7e8aa4e7d62ec66d61145dc3f61
Madhava Jay <me@madhavajay.com>
2021-07-20 18:45:03 +1000

    Added ability to pip install and update hagrid syft src from source
    - Added deps output to hagrid
    - Cleaned up setup.py
    - Added dev mode check
    - Added rich output
    - Added ability to fetch git repo in prod
    - Removed including actual syft + grid code in wheel
    - Silenced waitforit check on backend_stream and celeryworker containers

--- packages/hagrid/setup.py
+++ packages/hagrid/setup.py
@@ -1,48 +1,13 @@
 # stdlib
-import os
-import subprocess
+from glob import glob
 
 # third party
 from setuptools import find_packages
 from setuptools import setup
 
-hagrid_path = os.path.abspath(os.path.join(os.path.realpath(__file__), "../../hagrid/"))
-
-syft_path = os.path.abspath(os.path.join(os.path.realpath(__file__), "../../syft/"))
-
-grid_path = os.path.abspath(os.path.join(os.path.realpath(__file__), "../../grid/"))
-
-
-print("\n\n")
-print("PREPARING INSTALLATION:")
-print("\n")
-cmd = "rm -rf build; rm -rf dist; rm -rf grid; rm -rf syft;"
-print(cmd)
-subprocess.call(cmd, shell=True)
-cmd = "cp -r " + syft_path + " " + hagrid_path + "/"
-print(cmd)
-subprocess.call(cmd, shell=True)
-
-cmd = "cp -r " + grid_path + " " + hagrid_path + "/"
-print(cmd)
-subprocess.call(cmd, shell=True)
-print("\n\n")
-
-
-def package_files(directory):
-    paths = []
-    for (path, directories, filenames) in os.walk(directory):
-        for filename in filenames:
-            path = os.path.join("..", path, filename)
-            _path = str(path)
-            if ".pyc" not in _path and "build/" not in _path:
-                paths.append(path)
-    return paths
-
-
-extra_files = package_files("./syft")
-extra_files = package_files("./grid")
-
+DATA_FILES = [
+    ("img", glob("hagrid/img/*.png")),
+]
 
 setup(
     name="hagrid",
@@ -50,6 +15,7 @@ setup(
     version="0.1",
     author="Andrew Trask <andrew@openmined.org>",
     packages=find_packages(),
+    data_files=DATA_FILES,
     install_requires=[
         "click",
         "names",
@@ -58,9 +24,10 @@ setup(
         "ansible",
         "ansible-core",
         "paramiko",
+        "rich",
         "ascii_magic",
+        "gitpython",
     ],
-    package_data={"": extra_files},
     include_package_data=True,
     entry_points={"console_scripts": ["hagrid = hagrid.cli:cli"]},
 )
