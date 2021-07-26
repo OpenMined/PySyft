1291e53f7b9de7faa42f0fd17d63378ffdb4260f
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-07-26 11:35:13 +0100

    feat: add privacy budget to user type

--- packages/grid/grid-ui/src/types/grid-types.ts
+++ packages/grid/grid-ui/src/types/grid-types.ts
@@ -46,6 +46,7 @@ export interface User {
   email: string
   id: number
   role: string
+  epsilon: number
 }
 
 export type UserPermissions =
@@ -77,6 +78,7 @@ export interface Me {
   email: string
   permissions: GridPermissions
   role: Pick<Role, 'id' | 'name'>
+  epsilon: number
 }
 
 export interface Settings {
