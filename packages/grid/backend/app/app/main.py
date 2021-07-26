e6e7433073b2c53bb842eedaab05b4e1f2f09d72
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-07-26 10:41:41 +0100

    feat: init log on startup [ch218]

--- packages/grid/backend/app/app/main.py
+++ packages/grid/backend/app/app/main.py
@@ -5,6 +5,7 @@ from starlette.middleware.cors import CORSMiddleware
 # grid absolute
 from app.api.api_v1.api import api_router
 from app.core.config import settings
+from app.logger.log import LogHandler
 
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
@@ -20,4 +21,7 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_headers=["*"],
     )
 
+logHandler = LogHandler()
+
 app.include_router(api_router, prefix=settings.API_V1_STR)
+app.add_event_handler("startup", logHandler.init)
