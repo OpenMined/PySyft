1ded64c0727d2aa3b7967f39f5590f1b60bc0eeb
Andrew Trask <liamtrask@gmail.com>
2021-08-03 20:38:40 +0100

    Fix bug'

--- packages/syft/src/syft/core/node/domain/client.py
+++ packages/syft/src/syft/core/node/domain/client.py
@@ -443,7 +443,7 @@ class DomainClient(Client):
         metadata['description'] = bytes(description, 'utf-8')
 
         for k,v in metadata.items():
-            if isinstance(v, 'str'):
+            if isinstance(v, str):
                 metadata[k] = bytes(v, 'utf-8')
 
         assets = downcast(assets)
