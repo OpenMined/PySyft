768f62667c49a25031b63a1b5fc9147b5afca7f8
IshanMi <mishra.ishan@gmail.com>
2021-08-03 12:55:15 -0400

    Fix Tensor AST after merging branches

--- packages/syft/src/syft/core/tensor/tensor.py
+++ packages/syft/src/syft/core/tensor/tensor.py
@@ -15,11 +15,16 @@ from ..common.serde.serializable import bind_protobuf
 from .ancestors import AutogradTensorAncestor
 from .ancestors import PhiTensorAncestor
 from .passthrough import PassthroughTensor  # type: ignore
+from .fixed_precision_tensor_ancestor import FixedPrecisionTensorAncestor
+from .smpc.mpc_tensor_ancestor import MPCTensorAncestor
+# from .smpc.share_tensor import ShareTensor
 
 
+# TODO: Need to double check to see if smpc.ShareTensor operations are working correctly here since it's not inherited
 @bind_protobuf
 class Tensor(
-    PassthroughTensor, AutogradTensorAncestor, PhiTensorAncestor, RecursiveSerde
+    PassthroughTensor, AutogradTensorAncestor, PhiTensorAncestor, FixedPrecisionTensorAncestor, MPCTensorAncestor,
+    RecursiveSerde
 ):
 
     __attr_allowlist__ = ["child"]
