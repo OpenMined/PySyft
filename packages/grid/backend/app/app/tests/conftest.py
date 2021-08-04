6ad5d53837ec33bcf0fa7c7fe55bf4ed5eed5cd5
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-08-04 16:43:21 +0100

    test: moved unnecessary pytest.fixtures to utils/auth and utils/common

--- packages/grid/backend/app/app/tests/conftest.py
+++ packages/grid/backend/app/app/tests/conftest.py
@@ -1,13 +1,7 @@
-# stdlib
-from typing import Dict
-
 # third party
 from asgi_lifespan import LifespanManager
 from httpx import AsyncClient
 import pytest
-
-# grid absolute
-from app.users.models import UserCreate
 
 
 @pytest.fixture
@@ -26,14 +20,4 @@ async def client(app) -> AsyncClient:
     ) as client:
         yield client
 
-@pytest.fixture
-async def test_user_creation() -> UserCreate:
-    # TODO: Add randomness
-    return UserCreate(email="test@openmined.org", name="Container Tester", password="changethis", role="Administrator")
-
-@pytest.fixture()
-async def logged_in_owner_auth_token_header(app, client) -> Dict[str, str]:
-    res = await client.post(app.url_path_for('login'), json={"email": "info@openmined.org", "password": "changethis"})
-    res = res.json()
-    return {"Authorization": f"Bearer {res['access_token']}"}
 
---
+++ packages/grid/backend/app/app/tests/utils/common.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+def random_lower_string() -> str:
+    return "".join(random.choices(string.ascii_lowercase, k=32))
+
+
+def random_email() -> str:
+    return f"{random_lower_string()}@{random_lower_string()}.com"
+
+
