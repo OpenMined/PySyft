08f557fdee96de9bfe5fa3183a897391a64924ba
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-08-04 13:36:53 +0100

    style: lint pass

--- packages/grid/backend/app/app/tests/conftest.py
+++ packages/grid/backend/app/app/tests/conftest.py
@@ -1,24 +1,24 @@
 # stdlib
+import logging
 from typing import Dict
 from typing import Generator
 
 # third party
 from fastapi.testclient import TestClient
+from loguru import logger
 import pytest
 from sqlalchemy.orm import Session
 
 # grid absolute
 from app.core.config import settings
 from app.db.session import SessionLocal
+from app.logger.handler import get_log_handler
 from app.main import app
 from app.tests.utils.user import authentication_token_from_email
 from app.tests.utils.utils import get_superuser_token_headers
-from app.logger.handler import get_log_handler
-
-from loguru import logger
-import logging 
 
 log_handler = get_log_handler()
+
 
 @pytest.fixture(scope="session")
 def db() -> Generator:
@@ -42,12 +42,13 @@ def normal_user_token_headers(client: TestClient, db: Session) -> Dict[str, str]
         client=client, email=settings.EMAIL_TEST_USER, db=db
     )
 
+
 @pytest.fixture(autouse=True)
 def caplog(caplog):
     class PropagateHandler(logging.Handler):
         def emit(self, record):
             logging.getLogger(record.name).handle(record)
+
     sink_handler_id = logger.add(PropagateHandler(), format=log_handler.format_record)
     yield caplog
     logger.remove(sink_handler_id)
-
