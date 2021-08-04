55e7c3728a3aee5911a154274d13dc42adf7ca40
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-08-04 20:19:27 +0100

    test: wrap up utils.auth

--- packages/grid/backend/app/app/tests/utils/auth.py
+++ packages/grid/backend/app/app/tests/utils/auth.py
@@ -9,11 +9,11 @@ from httpx import AsyncClient
 async def authenticate_user(
     app: FastAPI, client: AsyncClient, email: str, password: str
 ) -> Dict[str, str]:
-    res = await client.post(app.url_path_for("login"), json={email, password})
+    user_login = {"email": email, "password": password}
+    res = await client.post(app.url_path_for("login"), json=user_login)
     res = res.json()
     auth_token = res["access_token"]
-    headers = {"Authorization": f"Bearer {auth_token}"}
-    return headers
+    return {"Authorization": f"Bearer {auth_token}"}
 
 
 async def authenticate_owner(app: FastAPI, client: AsyncClient) -> Dict[str, str]:
