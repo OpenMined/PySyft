f4c2cc4ba0818af706456374380d5bcf95dc2874
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-08-04 19:08:18 +0100

    test: use tests.utils.auth and tests.utils.user for auth and creating users

--- packages/grid/backend/app/app/tests/utils/user.py
+++ packages/grid/backend/app/app/tests/utils/user.py
@@ -1,9 +1,19 @@
-from pydantic import EmailStr
-from app.users.models import UserCreate
+# stdlib
 from typing import Optional
-from .common import random_email, random_lower_string
+
+# grid absolute
+from app.users.models import UserCreate
+
+# relative
+from .common import random_email
+from .common import random_lower_string
 
 
-def create_user(email: Optional[EmailStr] = random_email(), password: Optional[str] = random_lower_string(), name: Optional[str] = random_lower_string(), role: Optional[str] = "Administrator") -> UserCreate:
-    return UserCreate(email, password, name, role)
-
+def create_user(
+    email: Optional[str] = random_email(),
+    password: Optional[str] = random_lower_string(),
+    name: Optional[str] = random_lower_string(),
+    role: Optional[str] = "Administrator",
+) -> UserCreate:
+    user = {"email": email, "password": password, "name": name, "role": role}
+    return UserCreate(**user)
