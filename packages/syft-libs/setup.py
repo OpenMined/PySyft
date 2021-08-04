634112f69cb88a735d7b5353f891bc8e6b05defc
arpitvaghela <arpitvaghela9210@gmail.com>
2021-08-04 12:49:37 +0530

    updated setup.py

--- packages/syft-libs/setup.py
+++ packages/syft-libs/setup.py
@@ -1,5 +1,6 @@
 # stdlib
 import os
+import platform
 
 # third party
 from setuptools import find_packages
@@ -10,15 +11,23 @@ packages = ["syft"]
 here = os.path.abspath(os.path.dirname(__file__))
 local = os.path.exists(f"{here}/pyscaffoldext-syft-support")
 
+def check_petlib_windows(package:str)->bool:
+    p = platform.system().lower()
+    if p == "windows" and package.endswith("petlib"):
+        return True 
+    return False
+
 if local:
     for package in os.listdir(here):
-        if package.startswith("syft-"):
+        if package.startswith("syft-") and not check_petlib_windows(package):
             local_package = (
                 f"{package} @ file://localhost{here}/{package}#egg={package}"
             )
             packages.append(local_package)
 else:
-    packages += ["syft-xgboost", "syft-statsmodels"]
+    packages += ["syft-xgboost", "syft-statsmodels", "syft-opacus", "syft-petlib"]
+    if platform.system().lower() == "windows":
+        packages.remove("syft-petlib")
 
 packages = sorted(list(packages))
 
