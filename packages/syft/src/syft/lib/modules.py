3a256d75689d5d969dee5c595aeff2cea6a38e08
Aditi <aditi.verma9988@gmail.com>
2021-08-09 11:34:57 +0530

    Clean up

--- packages/syft/src/syft/lib/modules.py
+++ packages/syft/src/syft/lib/modules.py
@@ -9,11 +9,11 @@ import wrapt
 modules_imported = set()
 old_import = __import__
 
-id(__builtins__.__import__)
+# id(__builtins__.__import__)
 
 queue=[]
 
-def my_import(module, *args, **kwargs):
+def syft_lib_import(module, *args, **kwargs):
     if module not in modules_imported and not module.startswith("syft") and not module.startswith("_") and "." not in module:
     # if not module.startswith("syft") and "." not in module:
         queue.append(f"syft_{module}")
@@ -25,28 +25,23 @@ def my_import(module, *args, **kwargs):
 def post_import_hook(module):
     print("hook called")
     print(queue)
-    while queue:
-        syft_module=queue[0]
-        try:
-            importlib.import_module(syft_module)
-            globals()[syft_module]=sys.modules[syft_module]
-        except Exception as e:
-            print(f"Failed to load {syft_module}\n {e}")
-        queue.pop(0)
+    with open("sy_libs_log.txt", "w+") as f:
+        while queue:
+            syft_module=queue[0]
+            try:
+                importlib.import_module(syft_module)
+                globals()[syft_module]=sys.modules[syft_module]
+            except Exception as e:
+                f.write(f"Failed to load {syft_module}\n {e}")
+            queue.pop(0)
+    
+__builtins__.__import__ = syft_lib_import
 
-__builtins__.__import__ = my_import
+# id(__builtins__.__import__)
 
-id(__builtins__.__import__)
-
-# def my_import(module, *args, **kwargs):
-#     modules_imported.add(module)
-#     #     print(module, 'loaded successfully')
-#     if module == "xgboost":
-#         return old_import(module, *args, **kwargs)
-#     return old_import(module, *args, **kwargs)
 
 import xgboost
 
 print(xgboost)
 print(syft_xgboost)
-print(sys.path)
+# print(sys.path)
