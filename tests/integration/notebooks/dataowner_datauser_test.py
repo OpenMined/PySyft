2000bdd79205ae55bb4f5a4bc36b44ab44deba0f
cyberdroidmann <cyberlexix@gmail.com>
2021-08-09 11:07:50 +0100

    notebook integration  tests

---
+++ tests/integration/notebooks/dataowner_datauser_test.py
@@ -0,0 +1,27 @@
+import os
+from nbclient.client import execute
+
+# third party
+import pytest
+from testbook import testbook
+
+ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+NOTEBOOK_PATH = ROOT_PATH + '/notebooks'
+
+
+@pytest.fixture(scope='module')
+def notebook_run():
+    with testbook(NOTEBOOK_PATH + '/nb_for_integration_tests.ipynb', execute=True) as nb:
+        yield nb
+
+
+@testbook(NOTEBOOK_PATH + '/nb_for_integration_tests.ipynb', execute=True)
+def test_dataowner_can_connect_to_node(notebook_run):
+    domain_name = notebook_run.ref("do_domain_node.name")
+    assert domain_name == 'test_node'
+
+
+def test_dataowner_can_create_data_user(notebook_run):
+    domain_name = notebook_run.ref("ds_domain_node.name")
+    assert domain_name == 'test_node'
+
