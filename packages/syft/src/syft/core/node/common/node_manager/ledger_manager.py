60f83b8b0bd085e55a6e90203b698486f03843af
Kritikalcoder <krpr96@gmail.com>
2021-08-11 21:46:47 +0530

    Fix ledger mypy

--- packages/syft/src/syft/core/node/common/node_manager/ledger_manager.py
+++ packages/syft/src/syft/core/node/common/node_manager/ledger_manager.py
@@ -2,6 +2,7 @@
 from typing import Any
 
 # third party
+from sqlalchemy import String
 from sqlalchemy.engine import Engine
 
 # relative
@@ -18,12 +19,12 @@ class LedgerManager(DatabaseManager):
     def __init__(self, database: Engine) -> None:
         super().__init__(db=database, schema=LedgerManager.schema)
 
-    def __setitem__(self, key, value) -> None:
+    def __setitem__(self, key: String(256), value: String(256)) -> None:
         if super().contain(entity_name=key):
             super().delete(entity_name=key)
         super().register(value)
 
-    def __getitem__(self, key) -> Any:
+    def __getitem__(self, key: String(256)) -> Any:
         if super().contain(entity_name=key):
             super().first(entity_name=key)
         else:
