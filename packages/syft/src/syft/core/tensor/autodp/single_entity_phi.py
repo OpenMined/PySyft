f417330f5cfb64f2eb9cc257f7d0ea1397ce84c2
Kritikalcoder <krpr96@gmail.com>
2021-08-11 20:59:36 +0530

    Fix mypy errors in single entity phi tensor

--- packages/syft/src/syft/core/tensor/autodp/single_entity_phi.py
+++ packages/syft/src/syft/core/tensor/autodp/single_entity_phi.py
@@ -17,6 +17,7 @@ from ...adp.entity import Entity
 from ...adp.vm_private_scalar_manager import VirtualMachinePrivateScalarManager
 from ...common.serde.serializable import bind_protobuf
 from ..ancestors import AutogradTensorAncestor
+from ..ancestors import AcceptableSimpleType
 from ..passthrough import PassthroughTensor
 from ..passthrough import SupportedChainType
 from ..passthrough import implements
@@ -84,7 +85,7 @@ class SingleEntityPhiTensor(PassthroughTensor, AutogradTensorAncestor, Recursive
             scalar_manager=scalar_manager,
         )
 
-    def publish(self, acc: Any, sigma: float):
+    def publish(self, acc: Any, sigma: float) -> AcceptableSimpleType:
         return self.__class__(self.child.publish(acc=acc, sigma=sigma))
 
     @property
