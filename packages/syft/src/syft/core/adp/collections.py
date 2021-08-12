e20bea2bf3f4107dcdf7b7fa27335d5dc7bd30c9
Kritikalcoder <krpr96@gmail.com>
2021-08-12 17:44:14 +0530

    Fix more mypy errors

--- packages/syft/src/syft/core/adp/collections.py
+++ packages/syft/src/syft/core/adp/collections.py
@@ -1,6 +1,8 @@
 # stdlib
 from collections import Counter
 from collections import defaultdict
+from typing import Any
+from typing import Optional
 
 # relative
 from ..common.serde.recursive import RecursiveSerde
@@ -11,16 +13,16 @@ from ..common.serde.serializable import bind_protobuf
 class DefaultDict(defaultdict, RecursiveSerde):
     """Aim: build a class that inherits all the methods from collections.defaultdict but is serializable"""
 
-    __attr_allowlist__ = []
+    __attr_allowlist__ = []  # type: ignore
 
-    def __init__(self, factory=None, **kwargs):
+    def __init__(self, factory: Optional[Any] = None, **kwargs: Any) -> None:
         super().__init__(default_factory=factory, **kwargs)
 
 
 @bind_protobuf
 class SerializableCounter(Counter, RecursiveSerde):
 
-    __attr_allowlist__ = []
+    __attr_allowlist__ = []  # type: ignore
 
-    def __init__(self, iterable=None, **kwargs):
+    def __init__(self, iterable: Optional[Any] = None, **kwargs: Any) -> None:
         super().__init__(iterable, **kwargs)
