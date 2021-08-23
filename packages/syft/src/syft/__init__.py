cc18bc0b8f202e3b2beaf7838a86d2d7491a1967
Andrew Trask <liamtrask@gmail.com>
2021-08-23 21:24:11 +0100

    Add entity to syft.__init__

--- packages/syft/src/syft/__init__.py
+++ packages/syft/src/syft/__init__.py
@@ -82,6 +82,7 @@ from syft.lib import load_lib  # noqa: F401
 from syft.lib.torch.module import Module  # noqa: F401
 from syft.lib.torch.module import SyModule  # noqa: F401
 from syft.lib.torch.module import SySequential  # noqa: F401
+from syft.core.adp.entity import Entity
 
 # relative
 # Package Imports
