0cafd0fc08f34938d20ef0b98a7b806781bbc046
Andrew Trask <liamtrask@gmail.com>
2021-08-24 18:58:45 +0100

    Remove incorrect comment

--- packages/grid/backend/app/app/db/init_db.py
+++ packages/grid/backend/app/app/db/init_db.py
@@ -16,10 +16,6 @@ from app.core.node import node
 
 
 def init_db(db: Session) -> None:
-    # Tables should be created with Alembic migrations
-    # But if you don't want to use migrations, create
-    # the tables un-commenting the next line
-    # Base.metadata.create_all(bind=engine)z§
 
     # Build Syft Message
     msg = CreateInitialSetUpMessage(
