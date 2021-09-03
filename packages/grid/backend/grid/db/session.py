b3e8d898e5fa80f3c5375a30258bde813fca4a19
Madhava Jay <me@madhavajay.com>
2021-09-03 17:48:41 +1000

    Fixing issue with backend tests and in-memory db not having schema

--- packages/grid/backend/grid/db/session.py
+++ packages/grid/backend/grid/db/session.py
@@ -6,11 +6,17 @@ from sqlalchemy.orm import sessionmaker
 
 # grid absolute
 from grid.core.config import settings
+from grid.db.base import Base
 
 
 def get_db_engine(db_uri: str = str(settings.SQLALCHEMY_DATABASE_URI)) -> Engine:
     if db_uri.startswith("sqlite://"):
-        return create_engine(db_uri, echo=False)
+
+        db_engine = create_engine(db_uri, echo=False)
+        # TODO change to use alembic properly with the sqlite memory store:
+        # https://stackoverflow.com/questions/31406359/use-alembic-to-upgrade-in-memory-sqlite3-database
+        Base.metadata.create_all(db_engine)
+        return db_engine
     return create_engine(db_uri, pool_pre_ping=True)
 
 
