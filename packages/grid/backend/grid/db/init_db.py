7b2719e7ef9d1975463b08a620648022805c6c1e
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-09-03 07:54:28 +0100

    fix: remove comments

--- packages/grid/backend/grid/db/init_db.py
+++ packages/grid/backend/grid/db/init_db.py
@@ -11,11 +11,6 @@ from grid.core.config import settings
 from grid.core.node import node
 from grid.db import base  # noqa: F401
 
-# make sure all SQL Alchemy models are imported (app.db.base) before initializing DB
-# otherwise, SQL Alchemy might fail to initialize relationships properly
-# for more details: https://github.com/tiangolo/full-stack-fastapi-postgresql/issues/28
-
-
 def init_db(db: Session) -> None:
 
     # Build Syft Message
