4ae095d19642592e26bc3b6a368807ca8275dac0
Andrew Trask <liamtrask@gmail.com>
2021-09-06 02:18:29 +0100

    Slight ux polish

--- packages/syft/src/syft/core/node/common/client_manager/dataset_api.py
+++ packages/syft/src/syft/core/node/common/client_manager/dataset_api.py
@@ -33,6 +33,78 @@ from ....node.domain.enums import RequestAPIFields
 from ....node.domain.enums import ResponseObjectEnum
 from ...common.client_manager.request_api import RequestAPI
 
+initial_boilerplate = """<style>
+        #myInput {
+          background-position: 10px 12px; /* Position the search icon */
+          background-repeat: no-repeat; /* Do not repeat the icon image */
+          background-color: #bbb;
+          width: 98%; /* Full-width */
+          font-size: 14px; /* Increase font-size */
+          padding: 12px 20px 12px 40px; /* Add some padding */
+          border: 1px solid #ddd; /* Add a grey border */
+          margin-bottom: 12px; /* Add some space below the input */
+        }
+
+        #myTable {
+          border-collapse: collapse; /* Collapse borders */
+          width: 50%; /* Full-width */
+          border: 1px solid #ddd; /* Add a grey border */
+          font-size: 14px; /* Increase font-size */
+        }
+
+        #myTable th, #myTable td {
+          text-align: left; /* Left-align text */
+          padding: 10px; /* Add padding */
+        }
+
+        #myTable tr {
+          /* Add a bottom border to all table rows */
+          border-bottom: 1px solid #ddd;
+        }
+
+        #myTable tr.header, #myTable tr:hover {
+          /* Add a grey background color to the table header and on hover */
+          background-color: #777;
+        }
+        </style>
+
+        <table id="myTable">
+          <tr class="header">
+            <th style="width:15%;">Asset Key</th>
+            <th style="width:20%;">Type</th>
+            <th style="width:10%;">Shape</th>
+          </tr>
+        """
+
+end_boilerplate = """
+        </table>
+
+        <script>
+        function myFunction() {
+          // Declare variables
+          var input, filter, table, tr, td, i, txtValue;
+          input = document.getElementById("myInput");
+          filter = input.value.toUpperCase();
+          table = document.getElementById("myTable");
+          tr = table.getElementsByTagName("tr");
+
+          // Loop through all table rows, and hide those who don't match the search query
+          for (i = 0; i < tr.length; i++) {
+            name_td = tr[i].getElementsByTagName("td")[1];
+            desc_td = tr[i].getElementsByTagName("td")[2];
+            if (name_td || desc_td) {
+              name_txtValue = name_td.textContent || name_td.innerText;
+              desc_txtValue = desc_td.textContent || name_td.innerText;
+              if (name_txtValue.toUpperCase().indexOf(filter) > -1 || desc_txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+              } else {
+                tr[i].style.display = "none";
+              }
+            }
+          }
+        }
+        </script>"""
+
 
 class DatasetRequestAPI(RequestAPI):
     def __init__(self, client: AbstractNodeClient):
@@ -107,56 +179,59 @@ class DatasetRequestAPI(RequestAPI):
         self.delete(dataset_id=key)
 
     def _repr_html_(self) -> str:
-        return self.dataset_list_to_html(self.all_as_datasets())
+        if len(self) > 0:
+            return self.dataset_list_to_html(self.all_as_datasets())
+        return "(no datasets found)Z"
 
     @staticmethod
     def dataset_list_to_html(dataset_iterable):
+
         initial_boilerplate = """<style>
-        #myInput {
-          background-position: 10px 12px; /* Position the search icon */
-          background-repeat: no-repeat; /* Do not repeat the icon image */
-          background-color: #bbb;
-          width: 98%; /* Full-width */
-          font-size: 14px; /* Increase font-size */
-          padding: 12px 20px 12px 40px; /* Add some padding */
-          border: 1px solid #ddd; /* Add a grey border */
-          margin-bottom: 12px; /* Add some space below the input */
-        }
+                #myInput {
+                  background-position: 10px 12px; /* Position the search icon */
+                  background-repeat: no-repeat; /* Do not repeat the icon image */
+                  background-color: #bbb;
+                  width: 98%; /* Full-width */
+                  font-size: 14px; /* Increase font-size */
+                  padding: 12px 20px 12px 40px; /* Add some padding */
+                  border: 1px solid #ddd; /* Add a grey border */
+                  margin-bottom: 12px; /* Add some space below the input */
+                }
 
-        #myTable {
-          border-collapse: collapse; /* Collapse borders */
-          width: 100%; /* Full-width */
-          border: 1px solid #ddd; /* Add a grey border */
-          font-size: 14px; /* Increase font-size */
-        }
+                #myTable {
+                  border-collapse: collapse; /* Collapse borders */
+                  width: 100%; /* Full-width */
+                  border: 1px solid #ddd; /* Add a grey border */
+                  font-size: 14px; /* Increase font-size */
+                }
 
-        #myTable th, #myTable td {
-          text-align: left; /* Left-align text */
-          padding: 10px; /* Add padding */
-        }
+                #myTable th, #myTable td {
+                  text-align: left; /* Left-align text */
+                  padding: 10px; /* Add padding */
+                }
 
-        #myTable tr {
-          /* Add a bottom border to all table rows */
-          border-bottom: 1px solid #ddd;
-        }
+                #myTable tr {
+                  /* Add a bottom border to all table rows */
+                  border-bottom: 1px solid #ddd;
+                }
 
-        #myTable tr.header, #myTable tr:hover {
-          /* Add a grey background color to the table header and on hover */
-          background-color: #777;
-        }
-        </style>
+                #myTable tr.header, #myTable tr:hover {
+                  /* Add a grey background color to the table header and on hover */
+                  background-color: #777;
+                }
+                </style>
 
-        <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for datasets..">
+                <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for datasets..">
 
-        <table id="myTable">
-          <tr class="header">
-            <th style="width:30px">Idx</th>
-            <th style="width:20%;">Name</th>
-            <th style="width:45%;">Description</th>
-            <th style="width:15%;">Assets</th>
-            <th style="width:300px;">Id</th>
-          </tr>
-        """
+                <table id="myTable">
+                  <tr class="header">
+                    <th style="width:30px">Idx</th>
+                    <th style="width:20%;">Name</th>
+                    <th style="width:45%;">Description</th>
+                    <th style="width:15%;">Assets</th>
+                    <th style="width:300px;">Id</th>
+                  </tr>
+                """
 
         rows = ""
         for row_i, d in enumerate(dataset_iterable):
@@ -185,54 +260,8 @@ class DatasetRequestAPI(RequestAPI):
                 + """</td>
           </tr>"""
             )
-        end_boilerplate = """
-        </table>
-
-        <script>
-        function myFunction() {
-          // Declare variables
-          var input, filter, table, tr, td, i, txtValue;
-          input = document.getElementById("myInput");
-          filter = input.value.toUpperCase();
-          table = document.getElementById("myTable");
-          tr = table.getElementsByTagName("tr");
-
-          // Loop through all table rows, and hide those who don't match the search query
-          for (i = 0; i < tr.length; i++) {
-            name_td = tr[i].getElementsByTagName("td")[1];
-            desc_td = tr[i].getElementsByTagName("td")[2];
-            if (name_td || desc_td) {
-              name_txtValue = name_td.textContent || name_td.innerText;
-              desc_txtValue = desc_td.textContent || name_td.innerText;
-              if (name_txtValue.toUpperCase().indexOf(filter) > -1 || desc_txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-              } else {
-                tr[i].style.display = "none";
-              }
-            }
-          }
-        }
-        </script>"""
 
         return initial_boilerplate + rows + end_boilerplate
-
-    # def to_obj(self, result: Any) -> Any:
-    #     dataset_obj = super().to_obj(result)
-    #     dataset_obj.pandas = DataFrame(dataset_obj.data)
-    #     datasets = []
-    #
-    #     pointers = self.client.store
-    #     for data in dataset_obj.data:
-    #         _class_name = ResponseObjectEnum.DATA.capitalize()
-    #         data_obj = type(_class_name, (object,), data)()
-    #         data_obj.shape = ast.literal_eval(data_obj.shape)
-    #         data_obj.pointer = pointers[data_obj.id.replace("-", "")]
-    #         datasets.append(data_obj)
-    #
-    #     dataset_obj.files = datasets
-    #     type(dataset_obj).__getitem__ = lambda x, i: x.data[i]
-    #     dataset_obj.node = self.client
-    #     return Dataset(dataset_obj)
 
 
 class Dataset:
@@ -272,48 +301,10 @@ class Dataset:
         )
 
     def _repr_html_(self):
-        initial_boilerplate = """<style>
-        #myInput {
-          background-position: 10px 12px; /* Position the search icon */
-          background-repeat: no-repeat; /* Do not repeat the icon image */
-          background-color: #bbb;
-          width: 98%; /* Full-width */
-          font-size: 14px; /* Increase font-size */
-          padding: 12px 20px 12px 40px; /* Add some padding */
-          border: 1px solid #ddd; /* Add a grey border */
-          margin-bottom: 12px; /* Add some space below the input */
-        }
 
-        #myTable {
-          border-collapse: collapse; /* Collapse borders */
-          width: 50%; /* Full-width */
-          border: 1px solid #ddd; /* Add a grey border */
-          font-size: 14px; /* Increase font-size */
-        }
-
-        #myTable th, #myTable td {
-          text-align: left; /* Left-align text */
-          padding: 10px; /* Add padding */
-        }
-
-        #myTable tr {
-          /* Add a bottom border to all table rows */
-          border-bottom: 1px solid #ddd;
-        }
-
-        #myTable tr.header, #myTable tr:hover {
-          /* Add a grey background color to the table header and on hover */
-          background-color: #777;
-        }
-        </style>
-
-        <table id="myTable">
-          <tr class="header">
-            <th style="width:15%;">Asset Key</th>          
-            <th style="width:20%;">Type</th>
-            <th style="width:10%;">Shape</th>
-          </tr>
-        """
+        print("Dataset: " + self.name)
+        print("Description: " + self.description)
+        print()
 
         rows = ""
 
@@ -323,16 +314,16 @@ class Dataset:
 
             rows += (
                 """
-    
+
               <tr>
             <td>[\""""
-                + a['name']
+                + a["name"]
                 + """\"]</td>
             <td>"""
-                + a['dtype']
+                + a["dtype"]
                 + """</td>
             <td>"""
-                + a['shape']
+                + a["shape"]
                 + """</td>
           </tr>"""
             )
@@ -342,37 +333,3 @@ class Dataset:
         """
 
         return initial_boilerplate + rows + end_boilerplate
-
-
-# class Dataset:
-#     def __init__(self, dataset_metadata: Any) -> None:
-#         self.dataset_metadata = dataset_metadata
-#         self.id = self.dataset_metadata.id
-#         self.description = self.dataset_metadata.description
-#         self.tags = self.dataset_metadata.tags
-#         self.manifest = self.dataset_metadata.manifest
-#         self.pandas = self.dataset_metadata.pandas
-#
-#         for key, value in self.dataset_metadata.str_metadata.items():
-#             setattr(self, key, value)
-#
-#         for key, value in self.dataset_metadata.blob_metadata.items():
-#             setattr(self, key, deserialize(b"".fromhex(value), from_bytes=True))
-#
-#     def __getitem__(self, key: Union[str, int]) -> Any:
-#         if isinstance(key, int):
-#             obj_id = self.dataset_metadata.data[key]["id"].replace("-", "")
-#             return self.dataset_metadata.node.store[obj_id]
-#         elif isinstance(key, str):
-#             id = self.dataset_metadata.pandas[
-#                 self.dataset_metadata.pandas["name"] == key
-#             ].id.values[0]
-#             return self.dataset_metadata.node.store[id.replace("-", "")]
-#
-#     def _repr_html_(self) -> str:
-#         id = "<b>Id: </b>" + str(self.id) + "<br />"
-#         tags = "<b>Tags: </b>" + str(self.tags) + "<br />"
-#         manifest = "<b>Manifest: </b>" + str(self.manifest) + "<br /><br />"
-#         table = self.pandas._repr_html_()
-#
-#         return self.__repr__() + "<br /><br />" + id + tags + manifest + table
