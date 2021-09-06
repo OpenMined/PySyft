b83fb201f00efedcf1b6b8c1f850af72ecb06163
Andrew Trask <liamtrask@gmail.com>
2021-09-06 21:35:59 +0100

    Bugfix

--- packages/grid/backend/alembic/env.py
+++ packages/grid/backend/alembic/env.py
@@ -41,7 +41,7 @@ def get_url() -> str:
         return url
     user = os.getenv("POSTGRES_USER", "postgres")
     password = os.getenv("POSTGRES_PASSWORD", "")
-    server = os.getenv("POSTGRES_SERVER", "localhost:5432")
+    server = os.getenv("POSTGRES_SERVER", "db")
     db = os.getenv("POSTGRES_DB", "app")
     return f"postgresql://{user}:{password}@{server}/{db}"
 
