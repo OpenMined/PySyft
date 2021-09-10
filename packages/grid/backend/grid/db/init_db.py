3d192a55a7eeade742cf19568f3e269f7bdb6089
Ionesio Junior <ionesiojr@gmail.com>
2021-09-10 07:00:47 -0300

    Fix init_db to sync the node/owner keys

--- packages/grid/backend/grid/db/init_db.py
+++ packages/grid/backend/grid/db/init_db.py
@@ -1,4 +1,6 @@
 # third party
+from nacl.encoding import HexEncoder
+from nacl.signing import SigningKey
 from sqlalchemy.orm import Session
 
 # syft absolute
@@ -13,17 +15,24 @@ from grid.db import base  # noqa: F401
 
 
 def init_db(db: Session) -> None:
+    if not len(node.setup):  # Check if setup was defined previously
+        # Build Syft Message
+        msg = CreateInitialSetUpMessage(
+            address=node.address,
+            name="Jane Doe",
+            email=settings.FIRST_SUPERUSER,
+            password=settings.FIRST_SUPERUSER_PASSWORD,
+            domain_name=settings.DOMAIN_NAME,
+            budget=55.55,
+            reply_to=node.address,
+        ).sign(signing_key=node.signing_key)
 
-    # Build Syft Message
-    msg = CreateInitialSetUpMessage(
-        address=node.address,
-        name="Jane Doe",
-        email=settings.FIRST_SUPERUSER,
-        password=settings.FIRST_SUPERUSER_PASSWORD,
-        domain_name=settings.DOMAIN_NAME,
-        budget=55.55,
-        reply_to=node.address,
-    ).sign(signing_key=node.signing_key)
+        # Process syft message
+        _ = node.recv_immediate_msg_with_reply(msg=msg).message
 
-    # Process syft message
-    _ = node.recv_immediate_msg_with_reply(msg=msg).message
+    else:  # Uses the Owner root key to update node keys
+        owner = node.users.first(role=node.roles.owner_role.id)
+        root_key = SigningKey(owner.private_key.encode("utf-8"), encoder=HexEncoder)
+        node.signing_key = root_key
+        node.verify_key = root_key.verify_key
+        node.root_verify_key = root_key.verify_key
