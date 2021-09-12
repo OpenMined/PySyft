59b747d58cdc59cbda3d42aa15cc2decdc9ab064
Ionesio Junior <ionesiojr@gmail.com>
2021-09-12 19:11:08 -0300

    minor fix

--- packages/grid/backend/grid/core/config.py
+++ packages/grid/backend/grid/core/config.py
@@ -21,7 +21,7 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     SERVER_NAME: str = "unconfigured"
-    SERVER_HOST: AnyHttpUrl = AnyHttpUrl("https://localhost")
+    SERVER_HOST: str = "https://localhost"
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
