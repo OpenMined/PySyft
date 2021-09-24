ce9f22e673ba20943a0e40e9147311c57a30818f
IshanMi <mishra.ishan@gmail.com>
2021-09-24 10:31:53 -0400

    Added Patch for Flaky Tests

--- packages/syft/tests/syft/core/tensor/autodp/bug_catcher.sh
+++ packages/syft/tests/syft/core/tensor/autodp/bug_catcher.sh
@@ -1,6 +1,11 @@
-pytest -v single_entity_phi_test.py > results.txt
-if grep -q FAILED results.txt; then
-  echo search is over
-fi
+for iteration in {1..100}
+do
+  echo $iteration
+  pytest -v single_entity_phi_test.py > results.txt
 
-tail results.txt | grep "FAILED"
+  if grep -q FAILED results.txt; then
+    echo search is over
+    cp results.txt error_log
+    break
+  fi
+done
