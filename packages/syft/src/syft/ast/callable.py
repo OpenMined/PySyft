bacb62a0f19f121dc838008a770da4e0ee7bbf74
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2021-10-08 21:27:05 +0530

    set gc enable as false in callable

--- packages/syft/src/syft/ast/callable.py
+++ packages/syft/src/syft/ast/callable.py
@@ -90,7 +90,10 @@ class Callable(ast.attribute.Attribute):
 
             # then we convert anything which isn't a pointer into a pointer
             pointer_args, pointer_kwargs = ast.klass.pointerize_args_and_kwargs(
-                args=downcast_args, kwargs=downcast_kwargs, client=self.client
+                args=downcast_args,
+                kwargs=downcast_kwargs,
+                client=self.client,
+                gc_enabled=False,
             )
 
             if self.path_and_name is not None:
