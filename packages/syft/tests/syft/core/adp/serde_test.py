925bdc7c28a4567a5a0a2152dcb679a0ccf4d666
IshanMi <mishra.ishan@gmail.com>
2021-10-09 00:35:34 -0400

    Fix linting error

--- packages/syft/tests/syft/core/adp/serde_test.py
+++ packages/syft/tests/syft/core/adp/serde_test.py
@@ -7,7 +7,8 @@ from syft import serialize
 from syft.core.adp.entity import Entity
 from syft.core.adp.scalar.abstract.base_scalar import BaseScalar
 from syft.core.adp.scalar.abstract.intermediate_scalar import IntermediateScalar
-from syft.core.adp.scalar.gamma_scalar import GammaScalar
+
+# from syft.core.adp.scalar.gamma_scalar import GammaScalar
 from syft.core.adp.scalar.intermediate_phi_scalar import IntermediatePhiScalar
 from syft.core.adp.scalar.phi_scalar import PhiScalar
 
