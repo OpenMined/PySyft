Fixed issue with row entity phi tests and broadcasting entities
- In some instances the product of the tensor shape was returning a float
  which would fail to scale the entity array size to match

packages/syft/src/syft/core/tensor/autodp/row_entity_phi.py
@@ -111,8 +111,11 @@ class RowEntityPhiTensor(PassthroughTensor, ADPTensor):
 
     @property
     def entities(self) -> np.ndarray:
+        # we must cast the result of x.shape prod to an int because sometimes
+        # it can be a float depending on the original type which fails to
+        # multiply the [x.entity] array
         return np.array(
-            [[x.entity] * np.array(x.shape).prod() for x in self.child]
+            [[x.entity] * int(np.array(x.shape).prod()) for x in self.child]
         ).reshape(self.shape)
 
     @property
@@ -687,12 +690,12 @@ class RowEntityPhiTensor(PassthroughTensor, ADPTensor):
             new_list.append(row.diagonal(offset=offset, axis1=axis1, axis2=axis2))
 
         return RowEntityPhiTensor(rows=new_list, check_shape=False)
-            
+
     def round(self, decimals: int = 0) -> RowEntityPhiTensor:
         new_list = list()
         for tensor in self.child:
             new_list.append(tensor.round(decimals))
-            
+
         return RowEntityPhiTensor(rows=new_list, check_shape=False)
 
 