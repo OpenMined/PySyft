baf63a6438567bb0b37efacebf83c7fb3e5d6b0b
Andrew Trask <liamtrask@gmail.com>
2021-10-28 22:15:01 +0100

    Add extra block check in test.

--- packages/syft/src/syft/core/node/common/node_service/simple/obj_exists.py
+++ packages/syft/src/syft/core/node/common/node_service/simple/obj_exists.py
@@ -10,18 +10,20 @@ from ... import UID
 from ....abstract.node import AbstractNode
 from .simple_messages import NodeRunnableMessageWithReply
 
+from ......logger import info
 
 class DoesObjectExistMessage(NodeRunnableMessageWithReply):
 
     __attr_allowlist__ = ["obj_id"]
 
-    def __init__(self, obj_id: UID):
+    def __init__(self, obj_id: UID) -> None:
         self.obj_id = obj_id
 
-    def run(self, node: AbstractNode, verify_key: Optional[VerifyKey] = None) -> Any:
+    def run(self, node: AbstractNode, verify_key: Optional[VerifyKey] = None) -> bool:
 
         try:
             node.store[self.obj_id]  # type: ignore
             return True
-        except Exception:
+        except Exception as e:
+            info("Exception in DoesObjectExistMessage:" + str(e))
             return False
