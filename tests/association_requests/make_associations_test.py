b0a3a033d77f80f6aebfefeb72aa8e61aca79b91
Madhava Jay <me@madhavajay.com>
2021-10-28 17:22:43 +1000

    Re-enabled missing domain2 vpn connect and association tests

--- tests/association_requests/make_associations_test.py
+++ tests/association_requests/make_associations_test.py
@@ -51,28 +51,28 @@ def test_domain1_association_network1() -> None:
     assert domain.association.all()[0]["status"] == "ACCEPTED"
 
 
-# @pytest.mark.integration
-# def test_domain2_association_network1() -> None:
-#     domain = sy.login(
-#         email="info@openmined.org", password="changethis", port=DOMAIN2_PORT
-#     )
+@pytest.mark.integration
+def test_domain2_association_network1() -> None:
+    domain = sy.login(
+        email="info@openmined.org", password="changethis", port=DOMAIN2_PORT
+    )
 
-#     # get the vpn ips
-#     vpn_status = domain.vpn_status()
-#     DOMAIN_VPN_IP, NETWORK_VPN_IP = get_vpn_ips(vpn_status=vpn_status)
+    # get the vpn ips
+    vpn_status = domain.vpn_status()
+    DOMAIN_VPN_IP, NETWORK_VPN_IP = get_vpn_ips(vpn_status=vpn_status)
 
-#     domain.apply_to_network(
-#         domain_vpn_ip=DOMAIN_VPN_IP,
-#         network_vpn_ip=NETWORK_VPN_IP,
-#     )
+    domain.apply_to_network(
+        domain_vpn_ip=DOMAIN_VPN_IP,
+        network_vpn_ip=NETWORK_VPN_IP,
+    )
 
-#     network = sy.login(
-#         email="info@openmined.org", password="changethis", port=NETWORK_PORT
-#     )
-#     associations = network.association.all()
-#     for association in associations:
-#         if association["node_address"] == domain.target_id.id.no_dash:
-#             request_id = int(association["association_id"])
+    network = sy.login(
+        email="info@openmined.org", password="changethis", port=NETWORK_PORT
+    )
+    associations = network.association.all()
+    for association in associations:
+        if association["node_address"] == domain.target_id.id.no_dash:
+            request_id = int(association["association_id"])
 
-#     network.association[request_id].accept()
-#     assert domain.association.all()[0]["status"] == "ACCEPTED"
+    network.association[request_id].accept()
+    assert domain.association.all()[0]["status"] == "ACCEPTED"
