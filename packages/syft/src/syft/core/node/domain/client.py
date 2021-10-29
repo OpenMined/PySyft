b863b8fddb32d7e291a9d54130c598a1f490c813
Ionesio Junior <ionesiojr@gmail.com>
2021-10-28 22:24:20 -0300

    Fix mypy issues

--- packages/syft/src/syft/core/node/domain/client.py
+++ packages/syft/src/syft/core/node/domain/client.py
@@ -444,7 +444,7 @@ class DomainClient(Client):
             source=domain_vpn_ip, target=network_vpn_ip, metadata=metadata
         )
 
-        print('Application submitted.')
+        print("Application submitted.")
 
     @property
     def id(self) -> UID:
@@ -519,7 +519,7 @@ class DomainClient(Client):
         return self.store.pandas
 
     def join_network(self, client: AbstractNodeClient) -> None:
-        return self.vpn.join_network(host_or_ip=client.routes[0].connection.host)
+        return self.vpn.join_network(host_or_ip=client.routes[0].connection.host)  # type: ignore
 
     def vpn_status(self) -> Dict[str, Any]:
         return self.vpn.get_status()
