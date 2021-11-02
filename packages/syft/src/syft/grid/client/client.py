2f93f4594958050c2e0263d94749a5b5251d3082
Andrew Trask <liamtrask@gmail.com>
2021-11-02 21:58:00 +0000

    request secret password terminal

--- packages/syft/src/syft/grid/client/client.py
+++ packages/syft/src/syft/grid/client/client.py
@@ -100,7 +100,7 @@ def login(
             )
 
     # TRASK: please keep this so that people will stop putting their passwords in notebooks.
-    if password is None:
+    if password == "secret":
         print("Welcome " + str(email) + "!")
         password = getpass(prompt="Please enter you password:")
 
