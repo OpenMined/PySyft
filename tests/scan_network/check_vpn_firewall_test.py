66fc297a2befc3c89c3a8c1f25dd34fa797d8a50
Madhava Jay <me@madhavajay.com>
2021-11-02 17:27:04 +1000

    Nearly forgot to re-add the actual vpn scan test 😱

--- tests/scan_network/check_vpn_firewall_test.py
+++ tests/scan_network/check_vpn_firewall_test.py
@@ -1,4 +1,5 @@
 # stdlib
+import re
 import subprocess
 
 
@@ -24,41 +25,41 @@ def docker_network_connect(cmd: str = "connect") -> None:
                 print(f"Exception running: {cmd}. {e}")
 
 
-# def test_create_overlay_networks_docker() -> None:
-#     docker_network_connect(cmd="connect")
+def test_create_overlay_networks_docker() -> None:
+    docker_network_connect(cmd="connect")
 
 
-# def test_vpn_scan() -> None:
-#     # we need to scan two containers so that the first container itself gets scanned
-#     # externally by another container to make sure all of the containers on the VPN
-#     # are externally scanned and their firewall rules verified
-#     containers = [
-#         "test_network_1-tailscale-1",
-#         "test_domain_1-tailscale-1",
-#     ]
+def test_vpn_scan() -> None:
+    # we need to scan two containers so that the first container itself gets scanned
+    # externally by another container to make sure all of the containers on the VPN
+    # are externally scanned and their firewall rules verified
+    containers = [
+        "test_network_1-tailscale-1",
+        "test_domain_1-tailscale-1",
+    ]
 
-#     allowed_ports = [80]
-#     blocked_ports = [21, 4000, 8001, 8011, 8080, 5050, 5432, 5555, 5672, 15672]
+    allowed_ports = [80]
+    blocked_ports = [21, 4000, 8001, 8011, 8080, 5050, 5432, 5555, 5672, 15672]
 
-#     # run in two containers so that all IPs are scanned externally
-#     for container in containers:
-#         try:
-#             cmd = f"cat scripts/vpn_scan.sh | docker exec -i {container} ash"
-#             print(f"Scanning {container}")
-#             output = subprocess.check_output(cmd, shell=True)
-#             output = output.decode("utf-8")
-#         except Exception as e:
-#             print(f"Exception running: {cmd}. {e}")
+    # run in two containers so that all IPs are scanned externally
+    for container in containers:
+        try:
+            cmd = f"cat scripts/vpn_scan.sh | docker exec -i {container} ash"
+            print(f"Scanning {container}")
+            output = subprocess.check_output(cmd, shell=True)
+            output = output.decode("utf-8")
+        except Exception as e:
+            print(f"Exception running: {cmd}. {e}")
 
-#         for port in allowed_ports:
-#             matcher = re.compile(f" {port} port .+succeeded")
-#             lines = re.findall(matcher, output)
-#             assert len(lines) > 0
+        for port in allowed_ports:
+            matcher = re.compile(f" {port} port .+succeeded")
+            lines = re.findall(matcher, output)
+            assert len(lines) > 0
 
-#         for port in blocked_ports:
-#             matcher = re.compile(f" {port} port .+succeeded")
-#             lines = re.findall(matcher, output)
-#             assert len(lines) == 0
+        for port in blocked_ports:
+            matcher = re.compile(f" {port} port .+succeeded")
+            lines = re.findall(matcher, output)
+            assert len(lines) == 0
 
 
 def test_remove_overlay_networks_docker() -> None:
