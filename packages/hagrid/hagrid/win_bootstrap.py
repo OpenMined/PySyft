78b5cfc1381b7b205ea050f528c202280d51d77d
Madhava Jay <me@madhavajay.com>
2021-11-12 16:42:28 +1000

    Refactored Windows Bootstrap script
    - Added wait on choco prompts
    - Improved user wizard output
    - Combined choco installs into a single command
    - Added advanced where.exe search to find installed applications

--- packages/hagrid/hagrid/win_bootstrap.py
+++ packages/hagrid/hagrid/win_bootstrap.py
@@ -1,111 +1,252 @@
 # stdlib
 import subprocess
+from typing import Callable
+from typing import List
 
 # one liner to use bootstrap script:
-# PS: $r = Invoke-WebRequest "https://raw.githubusercontent.com/madhavajay/PySyft/madhava/win_bootstrap/packages/hagrid/hagrid/win_bootstrap.py" -UseBasicParsing; echo $r.Content | python
+# PS: $r = Invoke-WebRequest "https://raw.githubusercontent.com/madhavajay/PySyft/madhava/win_bootstrap/packages/hagrid/hagrid/win_bootstrap.py" -UseBasicParsing; echo $r.Content | python # noqa
+
+
+class Requirement:
+    def __init__(
+        self, full_name: str, choco_name: str, detect: Callable, extras: str = ""
+    ) -> None:
+        self.full_name = full_name
+        self.choco_name = choco_name
+        self.detect = detect
+        self.extras = extras
+
+    def __repr__(self) -> str:
+        return self.full_name
 
 
 install_choco_pwsh = """
-Start-Process PowerShell -Verb RunAs -ArgumentList "Set-ExecutionPolicy Bypass -Scope Process -Force;
+Start-Process PowerShell -Wait -Verb RunAs -ArgumentList "Set-ExecutionPolicy Bypass -Scope Process -Force;
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'));
-Read-Host -Prompt string"
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'));"
 """
+
+# add this to block powershell from existing for debugging
+# Read-Host -Prompt string
 
 
 def make_admin_cmd(admin_cmd: str) -> str:
     return (
-        f"Start-Process PowerShell -Verb RunAs -ArgumentList "
+        f"Start-Process PowerShell -Wait -Verb RunAs -ArgumentList "
         '"'
         "Set-ExecutionPolicy Bypass -Scope Process -Force; "
         f"{admin_cmd}; "
-        "Read-Host -Prompt string"
         '"'
     )
 
 
-def has_binary(binary: str) -> bool:
+def where_is(binary: str, req: Requirement) -> bool:
+    print(f"{req.full_name} - {binary}", end="")
+    found = path_where_is(binary)
+    if not found:
+        found = full_where_is(binary)
+    if found:
+        print("√")
+    else:
+        print("×")
+    return found
+
+
+def path_where_is(binary: str) -> bool:
     try:
-        subprocess.check_call(["where.exe", binary])
-        return True
-    except Exception:
+        cmds = ["where.exe", binary]
+        output = subprocess.run(cmds, capture_output=True, cwd="C:\\")
+        out = str(output.stdout.decode("utf-8")).split("\r\n")
+        if binary in out[0]:
+            return True
+    except Exception as e:
+        print("error", e)
         pass
     return False
 
 
-def install_choco() -> None:
-    if not has_binary("choco.exe"):
-        try:
-            powershell_cmds = ["-command", install_choco_pwsh]
-            val = input(
-                "Do you want to install choco.exe (Administrator Required) (Y/n): "
-            )
-            print(val)
-            if "y" in val.lower():
-                print("running pwsh", powershell_cmds)
-                output = subprocess.check_call(["powershell.exe"] + powershell_cmds)
-                print("output", output)
-            else:
-                print("ok no problem")
-        except Exception as e:
-            print("failed", e)
-    else:
-        print("choco.exe already installed")
+def full_where_is(binary: str) -> bool:
+    try:
+        powershell_cmd = f"where.exe /R C:\ *.exe | findstr \\{binary}$"  # noqa: W605
+        cmds = ["powershell.exe", "-Command", powershell_cmd]
+        output = subprocess.run(cmds, capture_output=True, cwd="C:\\")
+        out = str(output.stdout.decode("utf-8")).split("\r\n")
+        if binary in out[0]:
+            return True
+    except Exception as e:
+        print("error", e)
+        pass
+    return False
 
 
-def install_package(package: str, binary: str, admin: bool = False) -> None:
-    if not has_binary(binary):
-        try:
-            choco_args = f"choco.exe install {package} -y"
-            val = input(f"Do you want to install {package} (Y/n): ")
-            print(val)
-            if "y" in val.lower():
-                powershell_cmds = ["-command", make_admin_cmd(choco_args)]
-                print("running pwsh", powershell_cmds)
-                output = subprocess.check_call(["powershell.exe"] + powershell_cmds)
-                print("output", output)
-            else:
-                print("ok no problem")
-        except Exception as e:
-            print("failed", e)
-    else:
-        print(f"{binary} already installed")
+def exe(binary: str) -> Callable:
+    def call(req: Requirement) -> bool:
+        return where_is(binary=binary, req=req)
+
+    return call
 
 
-def install_docker_desktop() -> None:
-    return install_package(package="docker-desktop", binary="docker.exe", admin=True)
+def detect_wsl2(req: Requirement) -> bool:
+    print(f"{req.full_name} - wsl.exe ", end="")
+    try:
+        powershell_cmd = "wsl.exe --status"
+        cmds = ["powershell.exe", "-Command", powershell_cmd]
+        output = subprocess.run(cmds, capture_output=True)
+        out = output.stdout.decode("utf-16")
+        if "Default Distribution: Ubuntu" in out:
+            pass
+        if "Default Version: 2" in out:
+            print("√")
+            return True
+    except Exception as e:
+        print("error", e)
+        pass
+    print("×")
+    return False
 
 
-def install_wsl2() -> None:
-    # there is no wsl2.exe so we need to check for Default Version: 2 in 'wsl --status'
-    # and then set it if its not set
-    return install_package(package="wsl2", binary="wsl2.exe", admin=True)
-
-
-def install_ubuntu_wsl2() -> None:
-    return install_package(
-        package="wsl-ubuntu-2004", binary="ubuntu2004.exe", admin=True
+requirements = []
+requirements.append(
+    Requirement(
+        full_name="Chocolatey Package Manager",
+        choco_name="choco",
+        detect=exe("choco.exe"),
     )
+)
+requirements.append(
+    Requirement(
+        full_name="Anaconda Individual Edition",
+        choco_name="anaconda3",
+        detect=exe("conda.exe"),
+    )
+)
+requirements.append(
+    Requirement(
+        full_name="Git Version Control",
+        choco_name="git",
+        detect=exe("git.exe"),
+    )
+)
+requirements.append(
+    Requirement(
+        full_name="Windows Subsystem for Linux 2",
+        choco_name="wsl2",
+        detect=detect_wsl2,
+    )
+)
+requirements.append(
+    Requirement(
+        full_name="Ubuntu 20.04 WSL2",
+        choco_name="wsl-ubuntu-2004",
+        detect=exe("ubuntu2004.exe"),
+    )
+)
+requirements.append(
+    Requirement(
+        full_name="Docker Desktop",
+        choco_name="docker-desktop",
+        detect=exe("docker.exe"),
+    )
+)
 
 
-def install_git() -> None:
-    return install_package(package="git", binary="git.exe", admin=True)
+def install_choco() -> None:
+    try:
+        input(
+            "\nInstalling Chocolatey requires Administrator.\n"
+            "When the UAC dialogue appears click Yes on the left.\n\n"
+            "Press enter to start..."
+        )
+        powershell_cmds = ["-command", install_choco_pwsh]
+        output = subprocess.run(
+            ["powershell.exe"] + powershell_cmds, capture_output=True
+        )
+        _ = output.stdout.decode("utf-8")
+    except Exception as e:
+        print("failed", e)
 
 
-def install_anaconda3() -> None:
-    return install_package(package="anaconda3", binary="conda.exe", admin=True)
+def install_deps(requirements: List[Requirement]) -> None:
+    package_names = []
+    for req in requirements:
+        package_names.append(req.choco_name)
+
+    try:
+        input(
+            "\nInstalling packages requires Administrator.\n"
+            "When the UAC dialogue appears click Yes on the left.\n\n"
+            "Press enter to start..."
+        )
+        choco_args = f"choco.exe install {' '.join(package_names)} -y"
+        powershell_cmds = ["-command", make_admin_cmd(choco_args)]
+        output = subprocess.run(
+            ["powershell.exe"] + powershell_cmds, capture_output=True
+        )
+        _ = str(output.stdout.decode("utf-8"))
+    except Exception as e:
+        print("failed", e)
 
 
-def install_deps():
-    install_choco()
-    if has_binary("choco.exe"):
-        install_anaconda3()
-        install_git()
-        install_wsl2()
-        install_ubuntu_wsl2()
-        install_docker_desktop()
+def ask_install(requirement: Requirement) -> bool:
+    val = input(f"Do you want to install {requirement.full_name} (Y/n): ")
+    if "y" in val.lower():
+        return True
+    return False
+
+
+def check_all(requirements: List[Requirement]) -> List[Requirement]:
+    missing = []
+    for req in requirements:
+        if not req.detect(req):
+            missing.append(req)
+    return missing
+
+
+def main() -> None:
+    print("\nHAGrid Windows Dependency Installer")
+    print("===================================\n")
+    print("Searching your computer for:")
+    missing_deps = check_all(requirements=requirements)
+
+    if len(missing_deps) > 0:
+        print("\nWe were unable to find the following dependencies:")
+        print("-----------------------------------")
+        for dep in missing_deps:
+            print(f"{dep.full_name}")
+
+    print("")
+    desired = []
+    choco_required = False
+    for dep in missing_deps:
+        if ask_install(dep):
+            if dep.choco_name == "choco":
+                choco_required = True
+            else:
+                desired.append(dep)
+        elif dep.choco_name == "choco":
+            print("You must install Chocolatey to install other dependencies")
+            return
+
+    if choco_required:
+        install_choco()
+
+    if len(desired) > 0:
+        install_deps(desired)
+
+    print("")
+    still_missing = check_all(requirements=missing_deps)
+    if len(still_missing) > 0:
+        print("We were still unable to find the following dependencies:")
+        print("-----------------------------------")
+        for dep in missing_deps:
+            print(f"{dep.full_name}")
+        print("Please try again.")
     else:
-        print("not found?")
+
+        print("\nCongratulations. All done.")
+        print("===================================\n")
+        print("Now you can run HAGrid on Windows!")
 
 
-install_deps()
+if __name__ == "__main__":
+    main()
