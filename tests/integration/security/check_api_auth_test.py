199cf0d011a683caf8a42020950da4b94d364a57
Madhava Jay <me@madhavajay.com>
2021-11-23 12:08:11 +1000

    Linting issues and fixed quotation / stderror piping to check curl ouput

--- tests/integration/security/check_api_auth_test.py
+++ tests/integration/security/check_api_auth_test.py
@@ -5,12 +5,19 @@ import subprocess
 import pytest
 
 
+def make_curl_cmd(url: str, key: str, container: str) -> str:
+    return (
+        f"echo \"curl -v -X POST -H 'X-STACK-API-KEY: {key}' {url}\" | "
+        f"docker exec -i {container} ash 2>&1"
+    )
+
+
 @pytest.mark.security
 def test_api_auth() -> None:
     container = "test_network_1-tailscale-1"
     urls = [
-        f"http://headscale:4000/commands/generate_key",
-        f"http://tailscale:4000/commands/status",
+        "http://headscale:4000/commands/generate_key",
+        "http://tailscale:4000/commands/status",
     ]
 
     stack_key = "hex_key_value"
@@ -18,23 +25,23 @@ def test_api_auth() -> None:
     # try with a bad header
     try:
         for url in urls:
-            cmd = f"echo curl -v -X POST -H 'X-STACK-API-KEY: garbage' {url} | docker exec -i {container} ash"
-            print(f"cURLing {container} and {url}")
+            cmd = make_curl_cmd(url=url, key="garbage", container=container)
             output = subprocess.check_output(cmd, shell=True)
             output = output.decode("utf-8")
-            print("output", output)
-            assert "500 INTERNAL SERVER ERROR" not in output
+            assert "500 INTERNAL SERVER ERROR" in output
     except Exception as e:
         print(f"Exception running: {cmd}. {e}")
+        print(f"cmd output: {output}")
+        raise e
 
     # try with correct header
     try:
         for url in urls:
-            cmd = f"echo curl -v -X POST -H 'X-STACK-API-KEY: {stack_key}' {url} | docker exec -i {container} ash"
-            print(f"cURLing {container} and {url}")
+            cmd = make_curl_cmd(url=url, key=stack_key, container=container)
             output = subprocess.check_output(cmd, shell=True)
             output = output.decode("utf-8")
-            print("got output", output)
-            assert "result_url" not in output
+            assert "result_url" in output
     except Exception as e:
         print(f"Exception running: {cmd}. {e}")
+        print(f"cmd output: {output}")
+        raise e
