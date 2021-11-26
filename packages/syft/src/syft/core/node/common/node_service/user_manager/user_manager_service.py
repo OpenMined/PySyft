cc1a9fd8dc59c6224678e73ff21ab3191c4c9fe4
Ionesio Junior <ionesiojr@gmail.com>
2021-11-26 03:43:39 -0300

    Fix remaining budget

--- packages/syft/src/syft/core/node/common/node_service/user_manager/user_manager_service.py
+++ packages/syft/src/syft/core/node/common/node_service/user_manager/user_manager_service.py
@@ -256,6 +256,10 @@ def get_all_users_msg(
             # Remove private key
             del _user_json["private_key"]
 
+            _user_json['budget'] = node.acc.get_remaining_budget(  # type: ignore
+                user_key=verify_key, returned_epsilon_is_private=False
+            )
+
             _user_json["budget_spent"] = node.acc.user_budget(
                 user_key=VerifyKey(user.verify_key.encode("utf-8"), encoder=HexEncoder),
                 returned_epsilon_is_private=True,
