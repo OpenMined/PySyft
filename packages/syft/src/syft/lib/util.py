ba1f7f48a72d0d95f93aeccc95262346f20a89d7
Ionesio Junior <ionesiojr@gmail.com>
2022-01-13 07:55:22 -0300

    ADD/UPDATE docstrings

--- packages/syft/src/syft/lib/util.py
+++ packages/syft/src/syft/lib/util.py
@@ -1,3 +1,4 @@
+''' A set of util methods used by the syft.lib submodule. '''
 # stdlib
 import inspect
 from types import ModuleType
@@ -13,12 +14,20 @@ from ..core.node.abstract.node import AbstractNodeClient
 # this gets called on global ast as well as clients
 # anything which wants to have its ast updated and has an add_attr method
 
-
 def generic_update_ast(
     lib_name: str,
     create_ast: Callable,
     ast_or_client: TypeUnion[Globals, AbstractNodeClient],
 ) -> None:
+    """
+    Update the abstract syntax tree data structure used by the Globals or AbstractNodeClient entities.
+
+    :param str lib_name: 
+    :param Callable create_ast:
+    :param Union[Globals, AbstractNodeClient] ast_or_client:
+
+    :raises ValueError: raises a ValueError exception if ast_or_client isn't a Globals or AbstractNodeClient instance.
+    """
     if isinstance(ast_or_client, Globals):
         ast = ast_or_client
         new_lib_ast = create_ast(None)
@@ -35,7 +44,8 @@ def generic_update_ast(
 
 
 def is_static_method(klass: type, attr: str) -> bool:
-    """Test if a value of a class is static method.
+    """
+    Test if a value of a class is static method.
 
     Example:
 
@@ -46,12 +56,12 @@ def is_static_method(klass: type, attr: str) -> bool:
             def method():
                 ...
 
-    Args:
-        klass (type): the class on which we want to check whether the method is statically implemented
-        attr (str): the name of the method we want to check.
+    
+    :param type klass: the class on which we want to check whether the method is statically implemented
+    :param str attr: the name of the method we want to check.
 
-    Returns:
-        bool: whether or not a method named <attr> is a static method of class <klass>
+    :return: whether or not a method named <attr> is a static method of class <klass>
+    :rtype: bool
     """
 
     if not inspect.isclass(klass):
@@ -75,16 +85,16 @@ def is_static_method(klass: type, attr: str) -> bool:
 
 
 def copy_static_methods(from_class: type, to_class: type) -> None:
-    """Copies all static methods from one class to another class
+    """
+    Copies all static methods from one class to another class
 
     This utility was initialized during the creation of the Constructor for PyTorch's "th.Tensor" class. Since we
     replace each original constructor (th.Tensor) with on we implement (torch.UppercaseTensorConstructor), we also
     need to make sure that our new constructor has any static methods which were previously stored on th.Tensor.
     Otherwise, the library might look for them there, not find them, and then trigger an error.
 
-    Args:
-        from_class (Type): the class on which we look for static methods co copy
-        to_class (Type): the class onto which we copy all static methods found in <from_class>
+    :param type from_class: the class on which we look for static methods co copy
+    :param type to_class: the class onto which we copy all static methods found in <from_class>
 
     """
     # there are no static methods if from_class itself is not a type (sometimes funcs get passed in)
@@ -95,7 +105,8 @@ def copy_static_methods(from_class: type, to_class: type) -> None:
 
 
 def get_original_constructor_name(object_name: str) -> str:
-    """Generate name for original constructor
+    """
+    Generate name for original constructor
 
     For each custom constructor, we move the original constructor to a consistent location relative to
     the original constructor so that each custom constructor automatically knows where to find the original
@@ -103,10 +114,10 @@ def get_original_constructor_name(object_name: str) -> str:
     module as the original constructor. This method specifies the naming convention that we use to name
     the original constructor when it is moved.
 
-      Args:
-          object_name (str): the original constructor's original name
+    :param str object_name: the original constructor's original name
+    :return: a modified string containing the original_{object_name}
+    :rtype: str
     """
-
     return f"original_{object_name}"
 
 
@@ -116,23 +127,30 @@ def replace_classes_in_module(
     to_class: Callable,
     ignore_prefix: Optional[str] = None,
 ) -> None:
-    """Recursively replace occurrence of `from_class` to `to_class` inside module.
+    """
+    Recursively replace occurrence of `from_class` to `to_class` inside module.
 
     For example, when syft replaces torch.nn.parameter.Parameter constructor,
     there's also need to replace the same constructor in other modules that has already
     imported it.
 
-    Args:
-        module (ModuleType): top-level module to traverse
-        from_class: Original constructor
-        to_class: syft's ObjectConstructor
+    :params ModuleType module: top-level module to traverse.
+    :params Callable from_class: Original constructor.
+    :param Callable to_class: syft's ObjectConstructor.
+    :param str ignore_prefix: string value containing a prefix that should be ignored.
     """
     visited_modules = []
 
+    # inner function definition to update a module recursively
     def recursive_update(
         module: ModuleType, attr_name: Union[str, None] = None
     ) -> None:
+        """
+        Updates a specific module recursively.
 
+        :param ModuleType module: module to be updated.
+        :param str attr_name: module's attribute name to be updated. 
+        """
         # check if we need to skip this attribute to preserve our unmodified
         # original copy
         if (
@@ -156,8 +174,10 @@ def replace_classes_in_module(
 
 
 def full_name_with_qualname(klass: type) -> str:
+    """ Returns the klass module name + klass qualname."""
     return f"{klass.__module__}.{klass.__qualname__}"
 
 
 def full_name_with_name(klass: type) -> str:
+    """ Returns the klass module name + klass name.  """
     return f"{klass.__module__}.{klass.__name__}"
