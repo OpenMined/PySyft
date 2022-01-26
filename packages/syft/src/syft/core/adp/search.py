29e9ccb9d8381e7042535945dd2f484b64b8eb9e
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2022-01-26 10:58:55 +0530

    re-enable search algorithm exception

--- packages/syft/src/syft/core/adp/search.py
+++ packages/syft/src/syft/core/adp/search.py
@@ -183,9 +183,8 @@ def minimize_function(
         )
         results.append(shgo_results)
 
-    # if not shgo_results.success:
-    #     raise Exception("Search algorithm wasn't solvable... abort")
-    print("Search algorithm wasn't solvable... abort")
+    if not shgo_results.success:
+        raise Exception("Search algorithm wasn't solvable... abort")
 
     return results
 
