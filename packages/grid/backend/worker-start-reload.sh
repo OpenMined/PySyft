450393d80ceda981f3ebcf10505d811c50afea62
Madhava Jay <me@madhavajay.com>
2022-02-01 13:41:33 +1000

    WIP: Added scalene into containers with SCALENE environment variable
    - celeryworker doesnt work yet
    - backend containers need to filter uninteresting long running code

--- packages/grid/backend/worker-start-reload.sh
+++ packages/grid/backend/worker-start-reload.sh
@@ -15,4 +15,8 @@ pip install --user -e /app/syft[dev]
 python3 -c "print('---Monkey Patching: Gevent---\n');from gevent import monkey;monkey.patch_all()"
 python /app/grid/backend_prestart.py
 
-watchmedo auto-restart --directory=/app --pattern=*.py --recursive -- celery -A grid.worker worker -l info -Q main-queue --pool=gevent -c 500
+if [ -z $SCALENE ] ; then
+    watchmedo auto-restart --directory=/app --pattern=*.py --recursive -- celery -A grid.worker worker -l info -Q main-queue --pool=gevent -c 500
+else
+    scalene --json --outfile "/tmp/profile/${SERVICE_NAME}.json" /root/.local/bin/celery -A grid.worker worker -l info -Q main-queue --pool=solo
+fi
