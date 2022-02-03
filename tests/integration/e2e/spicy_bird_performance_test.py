b64e9e7e0d7569dfe66aacf231fb7e94184cd539
Ubuntu <azureuser@RuchiBhathia.x1vk4lbpnxoebio5knk4zw1cha.ax.internal.cloudapp.net>
2022-02-03 18:18:46 +0000

    Fix util functions not being detected in spicy bird test

--- tests/integration/e2e/spicy_bird_performance_test.py
+++ tests/integration/e2e/spicy_bird_performance_test.py
@@ -6,19 +6,103 @@ from typing import Any
 from typing import Dict
 from typing import List
 from typing import Tuple
+from typing import Union
+from typing import Optional
+from contextlib import contextmanager
 
 # third party
 import numpy as np
 import pandas as pd
 import pytest
+import os
 
 # syft absolute
 import syft as sy
 from syft import Domain
 from syft.core.adp.entity import Entity
-from syft.util import download_file
-from syft.util import get_root_data_path
-from syft.util import get_tracer
+# from syft.util import download_file
+# from syft.util import get_root_data_path
+# from syft.util import get_tracer
+
+
+def str_to_bool(bool_str: Optional[str]) -> bool:
+    result = False
+    bool_str = str(bool_str).lower()
+    if bool_str == "true" or bool_str == "1":
+        result = True
+    return result
+
+def get_root_data_path() -> Path:
+    # get the PySyft / data directory to share datasets between notebooks
+    # on Linux and MacOS the directory is: ~/.syft/data"
+    # on Windows the directory is: C:/Users/$USER/.syft/data
+
+    data_dir = Path.home() / ".syft" / "data"
+
+    os.makedirs(data_dir, exist_ok=True)
+    return data_dir
+
+
+def download_file(url: str, full_path: Union[str, Path]) -> Path:
+    if not os.path.exists(full_path):
+        # TODO: Rasswanth fix the SSL Error.
+        r = requests.get(url, allow_redirects=True, verify=verify_tls())  # nosec
+        path = os.path.dirname(full_path)
+        os.makedirs(path, exist_ok=True)
+        with open(full_path, "wb") as f:
+            f.write(r.content)
+    return Path(full_path)
+
+
+_tracer = None
+
+
+def get_tracer(service_name: Optional[str] = None) -> Any:
+    global _tracer
+    if _tracer is not None:  # type: ignore
+        return _tracer  # type: ignore
+
+    PROFILE_MODE = str_to_bool(os.environ.get("PROFILE", "False"))
+    PROFILE_MODE = False
+    if not PROFILE_MODE:
+
+        class NoopTracer:
+            @contextmanager
+            def start_as_current_span(*args: Any, **kwargs: Any) -> Any:
+                yield None
+
+        _tracer = NoopTracer()
+        return _tracer
+    
+    print("Profile mode with OpenTelemetry enabled")
+    if service_name is None:
+        service_name = os.environ.get("SERVICE_NAME", "client")
+
+    jaeger_host = os.environ.get("JAEGER_HOST", "localhost")
+    jaeger_port = int(os.environ.get("JAEGER_PORT", "6831"))
+
+    # third party
+    from opentelemetry import trace
+    from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+    from opentelemetry.sdk.resources import Resource
+    from opentelemetry.sdk.resources import SERVICE_NAME
+    from opentelemetry.sdk.trace import TracerProvider
+    from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+    trace.set_tracer_provider(
+        TracerProvider(resource=Resource.create({SERVICE_NAME: service_name}))
+    )
+
+    jaeger_exporter = JaegerExporter(
+        agent_host_name=jaeger_host,
+        agent_port=jaeger_port,
+    )
+
+    trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(jaeger_exporter))
+
+    _tracer = trace.get_tracer(__name__)
+    return _tracer
+
 
 
 def download_spicy_bird_benchmark() -> Tuple[Dict[str, Path], List[str]]:
@@ -59,6 +143,10 @@ def upload_subset(
     tweets_data = sy.Tensor(impressions).private(
         min_val=0, max_val=30, entities=entities
     )
+    
+    from syft.core.tensor.autodp.row_entity_phi import RowEntityPhiTensor
+    
+    assert isinstance(tweets_data.child, RowEntityPhiTensor)
 
     print("tweets_data serde_concurrency default", tweets_data.child.serde_concurrency)
     tweets_data.child.serde_concurrency = 1
@@ -140,7 +228,7 @@ def get_all_chunks(domain: Domain, unique_key: str) -> List[int]:
     return ids
 
 
-DOMAIN1_PORT = 9082
+DOMAIN1_PORT = 8083
 # DOMAIN1_PORT = 8081
 
 
