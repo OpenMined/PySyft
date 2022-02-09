c6b5391d71f5aeca1c155d99f16082767ed7c1e1
Ionesio Junior <ionesiojr@gmail.com>
2022-02-09 09:01:28 -0300

    - UPDATE/FIX User Service - ADD proper Pydantic Payload Request/Reply for each service - ADD new permission checks - Fix broken Messages (DeleteUserMessage and CreateUserMessage)

--- packages/syft/src/syft/core/node/common/node_service/user_manager/new_user_messages.py
+++ packages/syft/src/syft/core/node/common/node_service/user_manager/new_user_messages.py
@@ -1,4 +1,5 @@
 # stdlib
+import email
 from typing import Any
 from typing import Dict
 from typing import List
@@ -9,6 +10,7 @@ from typing import Union
 from nacl.encoding import HexEncoder
 from nacl.signing import SigningKey
 from nacl.signing import VerifyKey
+from pydantic import EmailStr
 from typing_extensions import final
 
 # relative
@@ -20,150 +22,299 @@ from ...exceptions import AuthorizationError
 from ...exceptions import MissingRequestKeyError
 from ...exceptions import UserNotFoundError
 from ...node_table.utils import model_to_json
+from ...permissions.permissions import check_permissions
+from ...permissions.user_permissions import UserCanCreateUsers
+from ...permissions.user_permissions import UserCanTriageRequest
+from ..generic_payload.syft_message import ReplyPayload
+from ..generic_payload.syft_message import RequestPayload
 from ..generic_payload.syft_message import SyftMessage
-from ..permissions import check_permissions
-from .user_permissions import UserCanTriageRequest
+
+
+@serializable(recursive_serde=True)
+@final
+class CreateUserMessage(SyftMessage, DomainMessageRegistry):
+
+    # Pydantic Inner class to define expected request payload fields.
+    class Request(RequestPayload):
+        """Payload fields and types used during a User Creation Request."""
+
+        email: EmailStr
+        password: str
+        name: str
+        role: Optional[str] = "Data Scientist"
+        institution: Optional[str]
+        website: Optional[str]
+        budget: Optional[float] = 0.0
+        daa_pdf: Optional[bytes] = b""
+
+    # Pydantic Inner class to define expected reply payload fields.
+    class Reply(ReplyPayload):
+        """Payload fields and types used during a User Creation Response."""
+
+        message: str = "User created successfully!"
+
+    request_payload_type = (
+        Request  # Converts generic syft dict into a CreateUserMessage.Request object.
+    )
+    reply_payload_type = (
+        Reply  # Creates a proper Reply payload message structure as a response.
+    )
+
+    def run(  # type: ignore
+        self, node: DomainInterface, verify_key: Optional[VerifyKey] = None
+    ) -> Union[List, Dict[str, Any]]:
+        # Check key permissions
+        if node.setup.first(domain_name=node.name).daa and not self.payload.daa_pdf:
+            raise AuthorizationError(
+                message="You can't apply a new User without a DAA document!"
+            )
+
+        # Check if email/password fields are empty
+        if not self.payload.email or not self.payload.password:
+            raise MissingRequestKeyError(
+                message="Invalid request payload, empty fields (email/password)!"
+            )
+
+        # Check if this email was already registered
+        try:
+            node.users.first(email=self.payload.email)
+            # If the email has already been registered, raise exception
+            raise AuthorizationError(
+                message="You can't create a new User using this email!"
+            )
+        except UserNotFoundError:
+            # If email not registered, a new user can be created.
+            pass
+
+        app_id = node.users.create_user_application(
+            name=self.payload.name,
+            email=self.payload.email,
+            password=self.payload.password,
+            daa_pdf=self.payload.daa_pdf,
+            institution=self.payload.institution,
+            website=self.payload.website,
+            budget=self.payload.budget,
+        )
+
+        user_role_id = -1
+        try:
+            user_role_id = node.users.role(verify_key=verify_key).id
+        except Exception as e:
+            print("verify_key not in db", e)
+
+        if node.roles.can_create_users(role_id=user_role_id):
+            node.users.process_user_application(
+                candidate_id=app_id, status="accepted", verify_key=verify_key
+            )
+
+        return CreateUserMessage.Reply()
 
 
 @serializable(recursive_serde=True)
 @final
 class GetUserMessage(SyftMessage, DomainMessageRegistry):
+
+    # Pydantic Inner class to define expected request payload fields.
+    class Request(RequestPayload):
+        user_id: int
+
+    # Pydantic Inner class to define expected reply payload fields.
+    class Reply(ReplyPayload):
+        id: int
+        name: str
+        email: str
+        role: Union[int, str]
+        budget: float
+        created_at: str
+        budget_spent: Optional[float] = 0.0
+        institution: Optional[str] = ""
+        website: Optional[str] = ""
+        added_by: Optional[str] = ""
+
     permission_classes = [UserCanTriageRequest]
+    request_payload_type = Request
+    reply_payload_type = Reply
 
     @check_permissions(permission_classes=permission_classes)
     def run(
         self, node: NodeServiceInterface, verify_key: Optional[VerifyKey] = None
-    ) -> Union[List, Dict[str, Any]]:
-        if not verify_key:
-            return {}
+    ) -> Union[None, ReplyPayload]:
+        # Retrieve User Model
+        user = node.users.first(id=self.payload.user_id)  # type: ignore
 
-        # TODO: Segregate permissions to a different level (make it composable)
-        # _allowed = node.users.can_triage_requests(verify_key=verify_key)  # type: ignore
-        # if not _allowed:
-        #     raise AuthorizationError(
-        #         "get_user_msg You're not allowed to get User information!"
-        #     )
-
-        # Extract User Columns
-        user = node.users.first(id=self.kwargs["user_id"])  # type: ignore
-        _msg = model_to_json(user)
+        # Build Reply
+        reply = GetUserMessage.Reply(**model_to_json(user))
 
         # Use role name instead of role ID.
-        _msg["role"] = node.roles.first(id=_msg["role"]).name  # type: ignore
-
-        # Remove private key
-        del _msg["private_key"]
+        reply.role = node.roles.first(id=reply.role).name  # type: ignore
 
         # Get budget spent
-        _msg["budget_spent"] = node.acc.user_budget(  # type: ignore
+        reply.budget_spent = node.acc.user_budget(  # type: ignore
             user_key=VerifyKey(user.verify_key.encode("utf-8"), encoder=HexEncoder)
         )
-        return _msg
+        return reply
 
 
 @serializable(recursive_serde=True)
 @final
 class GetUsersMessage(SyftMessage, DomainMessageRegistry):
+
+    # Pydantic Inner class to define expected request payload fields.
+    class Request(RequestPayload):
+        pass
+
+    # Pydantic Inner class to define expected reply payload fields.
+    class Reply(ReplyPayload):
+        users: List[GetUserMessage.Reply] = []
+
+    permission_classes = [UserCanTriageRequest]
+    request_payload_type = Request
+    reply_payload_type = Reply
+
+    @check_permissions(permission_classes=permission_classes)
     def run(  # type: ignore
         self, node: NodeServiceInterface, verify_key: Optional[VerifyKey] = None
-    ) -> Union[List, Dict[str, Any]]:
-        # Check key permissions
-        _allowed = node.users.can_triage_requests(verify_key=verify_key)
-        if not _allowed:
-            raise AuthorizationError(
-                "get_all_users_msg You're not allowed to get User information!"
+    ) -> Union[None, ReplyPayload]:
+        # Get All Users
+        users = node.users.all()
+        users_list = list()
+        for user in users:
+            user_model = GetUserMessage.Reply(**model_to_json(user))
+
+            # Use role name instead of role ID.
+            user_model.role = node.roles.first(id=user_model.role).name
+
+            # Remaining Budget
+            # TODO:
+            # Rename it from budget_spent to remaining budget
+            user_model.budget_spent = node.acc.get_remaining_budget(  # type: ignore
+                user_key=VerifyKey(user.verify_key.encode("utf-8"), encoder=HexEncoder),
+                returned_epsilon_is_private=False,
             )
+            users_list.append(user_model)
+
+        reply = GetUsersMessage.Reply.construct()
+        reply.users = users_list
+        return reply
+
+
+@serializable(recursive_serde=True)
+@final
+class DeleteUserMessage(SyftMessage, DomainMessageRegistry):
+
+    # Pydantic Inner class to define expected request payload fields.
+    class Request(RequestPayload):
+        user_id: int
+
+    # Pydantic Inner class to define expected reply payload fields.
+    class Reply(ReplyPayload):
+        message: str = "User deleted successfully!"
+
+    permission_classes = [UserCanCreateUsers]
+    request_payload_type = Request
+    reply_payload_type = Reply
+
+    @check_permissions(permission_classes=permission_classes)
+    def run(  # type: ignore
+        self, node: NodeServiceInterface, verify_key: Optional[VerifyKey] = None
+    ) -> Union[None, ReplyPayload]:
+
+        _target_user = node.users.first(id=self.payload.user_id)
+        _not_owner = (
+            node.roles.first(id=_target_user.role).name != node.roles.owner_role.name
+        )
+
+        if _not_owner:
+            node.users.delete(id=self.payload.user_id)
         else:
-            # Get All Users
-            users = node.users.all()
-            _msg = []
-            for user in users:
-                _user_json = model_to_json(user)
-                # Use role name instead of role ID.
-                _user_json["role"] = node.roles.first(id=_user_json["role"]).name
+            raise AuthorizationError(
+                "You're not allowed to delete this user information!"
+            )
 
-                # Remove private key
-                del _user_json["private_key"]
-
-                # Remaining Budget
-                # TODO:
-                # Rename it from budget_spent to remaining budget
-                _user_json["budget_spent"] = node.acc.get_remaining_budget(  # type: ignore
-                    user_key=VerifyKey(
-                        user.verify_key.encode("utf-8"), encoder=HexEncoder
-                    ),
-                    returned_epsilon_is_private=False,
-                )
-                _msg.append(_user_json)
-
-            return _msg
+        return DeleteUserMessage.Reply()
 
 
 @serializable(recursive_serde=True)
 @final
 class UpdateUserMessage(SyftMessage, DomainMessageRegistry):
+
+    # Pydantic Inner class to define expected request payload fields.
+    class Request(RequestPayload):
+        user_id: int
+        name: Optional[str] = ""
+        email: Optional[EmailStr] = ""
+        institution: Optional[str] = ""
+        website: Optional[str] = ""
+        password: Optional[str] = ""
+        role: Optional[str] = ""
+
+    # Pydantic Inner class to define expected reply payload fields.
+    class Reply(ReplyPayload):
+        message: str = "User updated successfully!"
+
+    permission_classes = [UserCanCreateUsers]
+    request_payload_type = Request
+    reply_payload_type = Reply
+
+    @check_permissions(permission_classes=permission_classes)
     def run(  # type: ignore
         self, node: NodeServiceInterface, verify_key: Optional[VerifyKey] = None
-    ) -> Union[List, Dict[str, Any]]:
-        # Check key permissions
-
-        if not verify_key:
-            raise AuthorizationError("You're not allowed to change other user data!")
-
-        msg = type("message", (object,), self.kwargs.upcast())()
+    ) -> Union[None, ReplyPayload]:
 
         _valid_parameters = (
-            msg.email
-            or msg.password
-            or msg.role
-            or msg.groups
-            or msg.name
-            or msg.budget
+            self.payload.email
+            or self.payload.password
+            or self.payload.role
+            or self.payload.name
+            or self.payload.institution
+            or self.payload.website
         )
-        _allowed = node.users.can_create_users(verify_key=verify_key)
-        # Change own information
-        if msg.user_id == 0:
-            msg.user_id = int(node.users.get_user(verify_key).id)  # type: ignore
 
-        _valid_user = node.users.contain(id=msg.user_id)
+        # Change own information
+        if self.payload.user_id == 0:
+            self.payload.user_id = int(node.users.get_user(verify_key).id)  # type: ignore
+
+        _valid_user = node.users.contain(id=self.payload.user_id)
 
         if not _valid_parameters:
             raise MissingRequestKeyError(
                 "Missing json fields ( email,password,role,groups, name )"
             )
 
-        if not _allowed:
-            raise AuthorizationError("You're not allowed to change other user data!")
-
         if not _valid_user:
             raise UserNotFoundError
 
-        if msg.institution:
-            node.users.set(user_id=str(msg.user_id), institution=msg.institution)
+        # Change Institution
+        if self.payload.institution:
+            node.users.set(
+                user_id=str(self.payload.user_id), institution=self.payload.institution
+            )
 
-        if msg.website:
-            node.users.set(user_id=str(msg.user_id), website=msg.website)
-
-        if msg.budget:
-            node.users.set(user_id=str(msg.user_id), budget=msg.budget)
+        # Change Website
+        if self.payload.website:
+            node.users.set(
+                user_id=str(self.payload.user_id), website=self.payload.website
+            )
 
         # Change Email Request
-        elif msg.email:
-            node.users.set(user_id=str(msg.user_id), email=msg.email)
+        elif self.payload.email:
+            node.users.set(user_id=str(self.payload.user_id), email=self.payload.email)
 
         # Change Password Request
-        elif msg.password:
-            node.users.set(user_id=str(msg.user_id), password=msg.password)
+        elif self.payload.password:
+            node.users.set(
+                user_id=str(self.payload.user_id), password=self.payload.password
+            )
 
         # Change Name Request
-        elif msg.name:
-            node.users.set(user_id=str(msg.user_id), name=msg.name)
+        elif self.payload.name:
+            node.users.set(user_id=str(self.payload.user_id), name=self.payload.name)
 
         # Change Role Request
-        elif msg.role:
-            target_user = node.users.first(id=msg.user_id)
+        elif self.payload.role:
+            target_user = node.users.first(id=self.payload.user_id)
             _allowed = (
-                msg.role != node.roles.owner_role.name  # Target Role != Owner
+                self.payload.role != node.roles.owner_role.name  # Target Role != Owner
                 and target_user.role
                 != node.roles.owner_role.id  # Target User Role != Owner
                 and node.users.can_create_users(
@@ -173,15 +324,15 @@ class UpdateUserMessage(SyftMessage, DomainMessageRegistry):
 
             # If all premises were respected
             if _allowed:
-                new_role_id = node.roles.first(name=msg.role).id
-                node.users.set(user_id=msg.user_id, role=new_role_id)  # type: ignore
+                new_role_id = node.roles.first(name=self.payload.role).id
+                node.users.set(user_id=self.payload.user_id, role=new_role_id)  # type: ignore
             elif (  # Transfering Owner's role
-                msg.role == node.roles.owner_role.name  # target role == Owner
+                self.payload.role == node.roles.owner_role.name  # target role == Owner
                 and node.users.role(verify_key=verify_key).name
                 == node.roles.owner_role.name  # Current user is the current node owner.
             ):
-                new_role_id = node.roles.first(name=msg.role).id
-                node.users.set(user_id=str(msg.user_id), role=new_role_id)
+                new_role_id = node.roles.first(name=self.payload.role).id
+                node.users.set(user_id=str(self.payload.user_id), role=new_role_id)
                 current_user = node.users.get_user(verify_key=verify_key)
                 node.users.set(user_id=current_user.id, role=node.roles.admin_role.id)  # type: ignore
                 # Updating current node keys
@@ -201,91 +352,4 @@ class UpdateUserMessage(SyftMessage, DomainMessageRegistry):
             else:
                 raise AuthorizationError("You're not allowed to change User roles!")
 
-        return {"resp_msg": "User updated successfully!"}
-
-
-@serializable(recursive_serde=True)
-@final
-class CreateUserMessage(SyftMessage, DomainMessageRegistry):
-    def run(  # type: ignore
-        self, node: DomainInterface, verify_key: Optional[VerifyKey] = None
-    ) -> Union[List, Dict[str, Any]]:
-        if not verify_key:
-            raise AuthorizationError("You're not allowed to change other user data!")
-
-        msg = type("message", (object,), dict(self.kwargs.upcast()))()
-        # Check key permissions
-        if node.setup.first(domain_name=node.name).daa and not msg.daa_pdf:
-            raise AuthorizationError(
-                message="You can't apply a new User without a DAA document!"
-            )
-
-        # Check if email/password fields are empty
-        if not msg.email or not msg.password:
-            raise MissingRequestKeyError(
-                message="Invalid request payload, empty fields (email/password)!"
-            )
-
-        # Check if this email was already registered
-        try:
-            node.users.first(email=msg.email)
-            # If the email has already been registered, raise exception
-            raise AuthorizationError(
-                message="You can't create a new User using this email!"
-            )
-        except UserNotFoundError:
-            # If email not registered, a new user can be created.
-            pass
-
-        app_id = node.users.create_user_application(
-            name=msg.name,
-            email=msg.email,
-            password=msg.password,
-            daa_pdf=msg.daa_pdf,
-            institution=msg.institution,
-            website=msg.website,
-            budget=msg.budget,
-        )
-
-        user_role_id = -1
-        try:
-            user_role_id = node.users.role(verify_key=verify_key).id
-        except Exception as e:
-            print("verify_key not in db", e)
-
-        if node.roles.can_create_users(role_id=user_role_id):
-            node.users.process_user_application(
-                candidate_id=app_id, status="accepted", verify_key=verify_key
-            )
-
-        return {"resp_msg": "User created successfully!"}
-
-
-@serializable(recursive_serde=True)
-@final
-class DeleteUserMessage(SyftMessage, DomainMessageRegistry):
-    def run(  # type: ignore
-        self, node: NodeServiceInterface, verify_key: Optional[VerifyKey] = None
-    ) -> Union[List, Dict[str, Any]]:
-        if not verify_key:
-            raise AuthorizationError("You're not allowed to change other user data!")
-
-        msg = type("message", (object,), dict(self.kwargs))()
-
-        _target_user = node.users.first(id=msg.user_id)
-        _not_owner = (
-            node.roles.first(id=_target_user.role).name != node.roles.owner_role.name
-        )
-
-        _allowed = (
-            node.users.can_create_users(verify_key=verify_key)  # Key Permission
-            and _not_owner  # Target user isn't the node owner
-        )
-        if _allowed:
-            node.users.delete(id=msg.user_id)
-        else:
-            raise AuthorizationError(
-                "You're not allowed to delete this user information!"
-            )
-
-        return {"resp_msg": "User deleted successfully!"}
+        return UpdateUserMessage.Reply()
