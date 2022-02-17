c93f23e406b60f5088e2b7ce81a0903544a5c8b9
Madhava Jay <me@madhavajay.com>
2022-02-18 07:52:18 +1000

    Re-added hotreloading to dev mode for frontend
    - Fixed db script migration error

--- packages/grid/backend/alembic/versions/2022-01-25_ada15f85c3d0.py
+++ packages/grid/backend/alembic/versions/2022-01-25_ada15f85c3d0.py
@@ -1,17 +1,18 @@
 """store write permissions as meta information
 
 Revision ID: ada15f85c3d0
-Revises: 6ebc05b27174
+Revises: 07fb4c284883
 Create Date: 2022-01-25 10:30:07.890360
 
 """
+import sqlalchemy as sa
+
 # third party
 from alembic import op  # type: ignore
-import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "ada15f85c3d0"
-down_revision = "6ebc05b27174"
+down_revision = "07fb4c284883"
 branch_labels = None
 depends_on = None
 
--- packages/grid/backend/alembic/versions/2022-01-31_6ebc05b27174.py
+++ packages/grid/backend/alembic/versions/2022-01-31_6ebc05b27174.py
@@ -1,17 +1,18 @@
 """remove group table
 
 Revision ID: 6ebc05b27174
-Revises: 07fb4c284883
+Revises: ada15f85c3d0
 Create Date: 2022-01-31 07:11:32.471320
 
 """
+import sqlalchemy as sa
+
 # third party
 from alembic import op  # type: ignore
-import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "6ebc05b27174"
-down_revision = "07fb4c284883"
+down_revision = "ada15f85c3d0"
 branch_labels = None
 depends_on = None
 
