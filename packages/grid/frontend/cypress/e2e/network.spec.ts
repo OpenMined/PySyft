941d7bbb1dc1c1d18a08c1b13142860946744692
Madhava Jay <me@madhavajay.com>
2022-02-18 13:20:26 +1000

    Added cypress front end ui tests to GitHub CI
    - Merged Cypress tests into single test for now
    - Added ability to override NEXT_PUBLIC_HOST during testing

--- packages/grid/frontend/cypress/e2e/network.spec.ts
+++
@@ -1,6 +0,0 @@
-describe('PyGrid Network integration test', () => {
-  it('should be able to login the default user', () => {
-    cy.visit('http://localhost:9081/login')
-    cy.findByText('type: network')
-  })
-})
