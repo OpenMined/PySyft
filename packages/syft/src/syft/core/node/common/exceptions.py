cb9b859fbfe0a3461e9a8a1e3a20f244e5200316
Ionesio Junior <ionesiojr@gmail.com>
2022-02-24 14:38:45 -0300

    ADD BadPayloadException exception

--- packages/syft/src/syft/core/node/common/exceptions.py
+++ packages/syft/src/syft/core/node/common/exceptions.py
@@ -12,6 +12,13 @@ class PermissionsNotDefined(PyGridError):
             message = "This message hasn't permissions defined, Please set up message permissions."
         super().__init__(message)
 
+
+class BadPayloadException(PyGridError):
+    def __init__(self, message: str = "") -> None:
+        if not message:
+            message = "Missing mandatory message fields or wrong message format and type."
+        super().__init__(message)
+
 class AuthorizationError(PyGridError):
     def __init__(self, message: str = "") -> None:
         if not message:
