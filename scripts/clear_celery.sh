fdcd40c93b347a43ff53adfd70e84e0c6d1d13a6
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2022-02-28 15:30:45 +0530

    modified script

--- scripts/clear_celery.sh
+++ scripts/clear_celery.sh
@@ -1,1 +1,1 @@
-docker ps --format '{{.Names}}' | grep "celeryworker" |  xargs -I '{}' bash -c 'echo "{} -> $(docker exec -i {} bash < input.sh) "' - {}
+docker ps --format '{{.Names}}' | grep "celeryworker" |  xargs -I '{}' bash -c 'echo "{} -> $(docker exec -i {} bash < $OLDPWD/input.sh) "' - {}
