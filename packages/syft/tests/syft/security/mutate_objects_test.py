0675d2b72ef5a3c17bdf2af52e246e9038d6842e
baye <11983328+bayegaspard@users.noreply.github.com>
2022-02-28 07:11:19 -0500

    test for mutation bug shows bug is real

--- packages/syft/tests/syft/security/mutate_objects_test.py
+++ packages/syft/tests/syft/security/mutate_objects_test.py
@@ -7,7 +7,7 @@ import torch as th
 # syft absolute
 import syft as sy
 
-# from syft.core.node.common.action.run_class_method_action import RunClassMethodAction
+from syft.core.node.common.action.run_class_method_action import RunClassMethodAction
 
 
 def test_store_object_mutation(
@@ -74,40 +74,40 @@ def test_store_object_mutation(
     assert all(new_result == (y + y)) is True
 
 
-# TODO: Fix
-# def test_store_overwrite_key(
-#     client: sy.VirtualMachineClient, root_client: sy.VirtualMachineClient
-# ) -> None:
-#     """Check if someone can overwrite any address."""
+#TODO: Fix
+def test_store_overwrite_key(
+    client: sy.VirtualMachineClient, root_client: sy.VirtualMachineClient
+) -> None:
+    """Check if someone can overwrite any address."""
 
-#     # root creates two tensors one visible one not
-#     x = th.tensor([1, 2, 3])
-#     x_ptr = x.send(root_client, pointable=True, tags=["visible"])
+    # root creates two tensors one visible one not
+    x = th.tensor([1, 2, 3])
+    x_ptr = x.send(root_client, pointable=True, tags=["visible"])
 
-#     y = th.tensor([3, 6, 9])
-#     y_ptr = y.send(root_client, pointable=False, tags=["invisible"])
+    y = th.tensor([3, 6, 9])
+    y_ptr = y.send(root_client, pointable=False, tags=["invisible"])
 
-#     # guest gets a pointer to the visible one
-#     guest_x = client.store[x_ptr.id_at_location]
-#     guest_x.add_(guest_x)
+    # guest gets a pointer to the visible one
+    guest_x = client.store[x_ptr.id_at_location]
+    guest_x.add_(guest_x)
 
-#     # guest constructs a pointer to the guessed hidden object
-#     guest_y = copy(guest_x)
-#     target_uid = sy.common.UID.from_string(y_ptr.id_at_location.no_dash)
-#     guest_y.id_at_location = target_uid
-#     guest_y.add_(guest_y)
+    # guest constructs a pointer to the guessed hidden object
+    guest_y = copy(guest_x)
+    target_uid = sy.common.UID.from_string(y_ptr.id_at_location.no_dash)
+    guest_y.id_at_location = target_uid
+    guest_y.add_(guest_y)
 
-#     # guest should not be able to overwrite a destination with RunClassMethodAction
-#     cmd = RunClassMethodAction(
-#         path="torch.Tensor.add_",
-#         _self=guest_y,
-#         args=[guest_y],
-#         kwargs={},
-#         id_at_location=target_uid,  # normal destination changed
-#         address=client.address,
-#     )
-#     client.send_immediate_msg_without_reply(msg=cmd)
+    # guest should not be able to overwrite a destination with RunClassMethodAction
+    cmd = RunClassMethodAction(
+        path="torch.Tensor.add_",
+        _self=guest_y,
+        args=[guest_y],
+        kwargs={},
+        id_at_location=target_uid,  # normal destination changed
+        address=client.address,
+    )
+    client.send_immediate_msg_without_reply(msg=cmd)
 
-#     # y should not have changed
-#     y_result = y_ptr.get(delete_obj=False)
-#     assert all(y_result == y) is True
+    # y should not have changed
+    y_result = y_ptr.get(delete_obj=False)
+    assert all(y_result == y) is True
