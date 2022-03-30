d7ec43372d3735aec6be39f0afd64af678d8d90a
baye <11983328+bayegaspard@users.noreply.github.com>
2022-03-30 01:23:11 -0400

    added a secure random password generator  into hagrid cli

---
+++ packages/hagrid/hagrid/rand_sec.py
@@ -0,0 +1,20 @@
+import secrets
+import string
+from os import urandom
+
+
+
+def generate_sec_random_password(length):
+    if not isinstance(length, int) or length < 10:
+        raise ValueError("Password should have a positive safe length of at least 10 characters!")
+
+    alphabet = string.ascii_letters + string.digits+string.punctuation
+    print(alphabet)
+    
+
+    # original Python 2 (urandom returns str)
+    # return "".join(chars[ord(c) % len(chars)] for c in urandom(length))
+
+    # Python 3 (urandom returns bytes)
+    return "".join(alphabet[c % len(alphabet)] for c in urandom(length))
+
