3d19d297704256b8cf849e07c7965bec0da31e47
Ionesio Junior <ionesiojr@gmail.com>
2022-03-31 22:29:07 -0300

    - ADD get_s3_resource method - ADD delete_incomplete_s3_object_upload method - UPDATE presigned_url_upload method to rollback incomplete uploads

--- packages/syft/src/syft/core/node/common/util.py
+++ packages/syft/src/syft/core/node/common/util.py
@@ -4,6 +4,8 @@ from typing import Any
 from typing import Generator
 from typing import List
 
+from sympy import E
+
 # third party
 import boto3
 from botocore.client import Config
@@ -108,6 +110,17 @@ def upload_result_to_s3(
     return proxy_obj
 
 
+def delete_incomplete_s3_object_upload(client: Any, name: str) -> None:
+    from .node_service.upload_service.upload_service_messages import DeleteIncompleteDataUploadMessage
+    # Step 2 - Send a message to PyGrid to inform of the data being uploaded,
+    # and get presigned url for each chunk of data being uploaded.
+    upload_response = client.datasets.perform_api_request_generic(
+        syft_msg=DeleteIncompleteDataUploadMessage,
+        content={
+            "upload_id": name,
+        },
+    )
+
 def upload_to_s3_using_presigned(
     client: Any,
     data: Any,
@@ -146,74 +159,83 @@ def upload_to_s3_using_presigned(
     from .node_service.upload_service.upload_service_messages import UploadDataMessage
 
     dataset_name = str(dataset_name)
+    
+    # Somehow they're different
+    id_at_location = str(asset_name.value)
+    asset_name = asset_name
 
     # Step 1 - Convert data to be uploaded to binary
     binary_dataset: bytes = serialize(data, to_bytes=True)  # type: ignore
     file_size = len(binary_dataset)
 
-    # Step 2 - Send a message to PyGrid to inform of the data being uploaded,
-    # and get presigned url for each chunk of data being uploaded.
-    upload_response = client.datasets.perform_api_request_generic(
-        syft_msg=UploadDataMessage,
-        content={
-            "filename": f"{dataset_name}/{asset_name}",
-            "file_size": file_size,
-            "chunk_size": chunk_size,
-            "address": client.address,
-            "reply_to": client.address,
-        },
-    )
+    try:
+        # Step 2 - Send a message to PyGrid to inform of the data being uploaded,
+        # and get presigned url for each chunk of data being uploaded.
+        upload_response = client.datasets.perform_api_request_generic(
+            syft_msg=UploadDataMessage,
+            content={
+                "filename": f"{dataset_name}/{asset_name}",
+                "file_size": file_size,
+                "chunk_size": chunk_size,
+                "address": client.address,
+                "reply_to": client.address,
+            },
+        )
 
-    # Step 3 - Starts to upload binary data into Seaweed.
-    binary_buffer = BytesIO(binary_dataset)
-    parts = sorted(upload_response.payload.parts, key=lambda x: x["part_no"])
-    etag_chunk_no_pairs = list()
-    data_chunks = zip(read_chunks(binary_buffer, chunk_size), parts)
-    for _ in tqdm(parts, desc=data_upload_description):
-        data_chunk, part = next(data_chunks)
-        presigned_url = part["url"]
-        part_no = part["part_no"]
-        client_url = client.url_from_path(presigned_url)
-        part["client_url"] = client_url
+        # Step 3 - Starts to upload binary data into Seaweed.
+        binary_buffer = BytesIO(binary_dataset)
+        parts = sorted(upload_response.payload.parts, key=lambda x: x["part_no"])
+        etag_chunk_no_pairs = list()
+        data_chunks = zip(read_chunks(binary_buffer, chunk_size), parts)
+        for _ in tqdm(parts, desc=data_upload_description):
+            data_chunk, part = next(data_chunks)
+            presigned_url = part["url"]
+            part_no = part["part_no"]
+            client_url = client.url_from_path(presigned_url)
+            part["client_url"] = client_url
 
-        res = requests.put(client_url, data=data_chunk)
+            res = requests.put(client_url, data=data_chunk)
 
-        if not res.ok:  # raise an error if upload fails
-            error_message = (
-                f"\n\nFailed to upload `{asset_name}` to store\n"
-                + f"Status code: {res.status_code} {res.reason}\n"
-                + f"Error: {str(res.text)}"
-            )
-            raise DatasetUploadError(message=error_message)
-        etag = res.headers["ETag"]
-        etag_chunk_no_pairs.append(
-            {"ETag": etag, "PartNumber": part_no}
-        )  # maintain list of part no and ETag
+            if not res.ok:  # raise an error if upload fails
+                error_message = (
+                    f"\n\nFailed to upload `{asset_name}` to store\n"
+                    + f"Status code: {res.status_code} {res.reason}\n"
+                    + f"Error: {str(res.text)}"
+                )
+                raise DatasetUploadError(message=error_message)
+            etag = res.headers["ETag"]
+            etag_chunk_no_pairs.append(
+                {"ETag": etag, "PartNumber": part_no}
+            )  # maintain list of part no and ETag
 
-    # Step 4 - Send a message to PyGrid informing about dataset upload complete!
-    upload_response = client.datasets.perform_request(
-        syft_msg=UploadDataCompleteMessage,
-        content={
-            "upload_id": upload_response.payload.upload_id,
-            "filename": f"{dataset_name}/{asset_name}",
-            "parts": etag_chunk_no_pairs,
-        },
-    )
+        # Step 4 - Send a message to PyGrid informing about dataset upload complete!
+        upload_response = client.datasets.perform_request(
+            syft_msg=UploadDataCompleteMessage,
+            content={
+                "upload_id": upload_response.payload.upload_id,
+                "filename": f"{dataset_name}/{asset_name}",
+                "parts": etag_chunk_no_pairs,
+            },
+        )
 
-    # Step 5 - Create a proxy dataset for the uploaded data.
-    # Retrieve fully qualified name to  use for pointer creation.
-    obj_public_kwargs = getattr(data, "proxy_public_kwargs", None)
-    data_fqn = str(get_fully_qualified_name(data))
-    data_dtype = str(type(data))
-    proxy_data = ProxyDataset(
-        asset_name=asset_name,
-        dataset_name=dataset_name,
-        node_id=client.id,
-        dtype=data_dtype,
-        fqn=data_fqn,
-        shape=data.shape,
-        obj_public_kwargs=obj_public_kwargs,
-    )
+        # Step 5 - Create a proxy dataset for the uploaded data.
+        # Retrieve fully qualified name to  use for pointer creation.
+        obj_public_kwargs = getattr(data, "proxy_public_kwargs", None)
+        data_fqn = str(get_fully_qualified_name(data))
+        data_dtype = str(type(data))
+        proxy_data = ProxyDataset(
+            asset_name=asset_name,
+            dataset_name=dataset_name,
+            node_id=client.id,
+            dtype=data_dtype,
+            fqn=data_fqn,
+            shape=data.shape,
+            obj_public_kwargs=obj_public_kwargs,
+        )
+    except (Exception, KeyboardInterrupt) as e:
+        file_name = f'{dataset_name}/{id_at_location}'
+        delete_incomplete_s3_object_upload(client=client, name=file_name)
+        raise e
 
     return proxy_data
 
@@ -235,6 +257,22 @@ def get_s3_client(settings: BaseSettings) -> "boto3.client.S3":
         print(f"Failed to create S3 Client with {s3_endpoint} {s3_port} {s3_grid_url}")
         raise e
 
+def get_s3_resource(settings: BaseSettings) -> "boto3.resource.s3":
+    try:
+        s3_endpoint = settings.S3_ENDPOINT
+        s3_port = settings.S3_PORT
+        s3_grid_url = GridURL(host_or_ip=s3_endpoint, port=s3_port)
+        return boto3.resource(
+            "s3",
+            endpoint_url=s3_grid_url.url,
+            aws_access_key_id=settings.S3_ROOT_USER,
+            aws_secret_access_key=settings.S3_ROOT_PWD,
+            config=Config(signature_version="s3v4"),
+            region_name=settings.S3_REGION,
+        )
+    except Exception as e:
+        print(f"Failed to create S3 Resource with {s3_endpoint} {s3_port} {s3_grid_url}")
+        raise e
 
 def check_send_to_blob_storage(obj: Any, use_blob_storage: bool = False) -> bool:
     """Check if the data needs to be send to Seaweed storage depending upon its size and type.
