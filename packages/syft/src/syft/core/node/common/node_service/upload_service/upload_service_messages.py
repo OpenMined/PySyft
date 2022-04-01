a20240926a2222ccf16f9ebb99f51b4f08ab2996
Ionesio Junior <ionesiojr@gmail.com>
2022-03-31 22:30:39 -0300

    CREATE DeleteIncompleteDataUploadMessage

--- packages/syft/src/syft/core/node/common/node_service/upload_service/upload_service_messages.py
+++ packages/syft/src/syft/core/node/common/node_service/upload_service/upload_service_messages.py
@@ -19,7 +19,7 @@ from ....domain.domain_interface import DomainInterface
 from ....domain.registry import DomainMessageRegistry
 from ...permissions.permissions import BasePermission
 from ...permissions.user_permissions import NoRestriction
-from ...util import get_s3_client
+from ...util import get_s3_client, get_s3_resource
 from ..generic_payload.syft_message import NewSyftMessage as SyftMessage
 from ..generic_payload.syft_message import ReplyPayload
 from ..generic_payload.syft_message import RequestPayload
@@ -134,6 +134,50 @@ class UploadDataCompleteMessage(SyftMessage, DomainMessageRegistry):
             MultipartUpload={"Parts": self.payload.parts},
             UploadId=self.payload.upload_id,
         )
+        return UploadDataCompleteMessage.Reply()
+
+    def get_permissions(self) -> List[Type[BasePermission]]:
+        """Returns the list of permission classes."""
+        return [NoRestriction]
+
+
+
+@serializable(recursive_serde=True)
+@final
+class DeleteIncompleteDataUploadMessage(SyftMessage, DomainMessageRegistry):
+
+    # Pydantic Inner class to define expected request payload fields.
+    class Request(RequestPayload):
+        """Payload fields and types used during a User Creation Request."""
+
+        upload_id: str
+    
+    # Pydantic Inner class to define expected reply payload fields.
+    class Reply(ReplyPayload):
+        """Payload fields and types used during a User Creation Response."""
+
+        message: str = "Deletion Complete!"
+
+    request_payload_type = (
+        Request  # Converts generic syft dict into a CreateUserMessage.Request object.
+    )
+    reply_payload_type = (
+        Reply  # Creates a proper Reply payload message structure as a response.
+    )
+
+    def run(  # type: ignore
+        self, node: DomainInterface, verify_key: Optional[VerifyKey] = None
+    ) -> ReplyPayload:  # type: ignore
+
+        # TODO: Move to permissions
+        # user_role = node.roles.first(**{"id": current_user.role})
+        # if not user_role.can_upload_data:
+        #    return {"message": "You're not authorized to do this."}
+
+        client: boto3.client.S3 = get_s3_resource(settings=node.settings)
+        bucket = client.Bucket(node.id.no_dash)
+        prefix_string = f"/uploads{self.payload.upload_id}"
+        bucket.objects.filter(Prefix=prefix_string).delete()
         return UploadDataCompleteMessage.Reply()
 
     def get_permissions(self) -> List[Type[BasePermission]]:
