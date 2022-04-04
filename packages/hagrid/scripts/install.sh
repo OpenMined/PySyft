69455df31d1901a0709d910da92b3ebc74fb10c3
Madhava Jay <me@madhavajay.com>
2022-04-04 14:19:51 +1000

    Removed unused code from install.sh

--- packages/hagrid/scripts/install.sh
+++ packages/hagrid/scripts/install.sh
@@ -11,50 +11,6 @@ but credit (and pull requests) appreciated.
 ------------------------------------------------------------------------
 EOF
 
-log_prefix() {
-  echo "$0"
-}
-_logp=7
-log_set_priority() {
-  _logp="$1"
-}
-log_priority() {
-  if test -z "$1"; then
-    echo "$_logp"
-    return
-  fi
-  [ "$1" -le "$_logp" ]
-}
-log_tag() {
-  case $1 in
-    0) echo "emerg" ;;
-    1) echo "alert" ;;
-    2) echo "crit" ;;
-    3) echo "err" ;;
-    4) echo "warning" ;;
-    5) echo "notice" ;;
-    6) echo "info" ;;
-    7) echo "debug" ;;
-    *) echo "$1" ;;
-  esac
-}
-log_debug() {
-  log_priority 7 || return 0
-  echoerr "$(log_prefix)" "$(log_tag 7)" "$@"
-}
-log_info() {
-  log_priority 6 || return 0
-  echoerr "$(log_prefix)" "$(log_tag 6)" "$@"
-}
-log_err() {
-  log_priority 3 || return 0
-  echoerr "$(log_prefix)" "$(log_tag 3)" "$@"
-}
-log_crit() {
-  log_priority 2 || return 0
-  echoerr "$(log_prefix)" "$(log_tag 2)" "$@"
-}
-
 is_command() {
   command -v "$1" >/dev/null
 }
