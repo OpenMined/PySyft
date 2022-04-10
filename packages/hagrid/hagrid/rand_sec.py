d7d47cdbce4f0c456d8ad6f8c8dd6a41521bcfb4
baye <11983328+bayegaspard@users.noreply.github.com>
2022-04-10 08:44:26 -0400

    fixed punctiation bugs

--- packages/hagrid/hagrid/rand_sec.py
+++ packages/hagrid/hagrid/rand_sec.py
@@ -10,7 +10,7 @@ def generate_sec_random_password(length):
             "Password should have a positive safe length of at least 10 characters!"
         )
 
-    alphabet = string.ascii_letters + string.digits + string.punctuation
+    alphabet = string.ascii_letters + string.digits #+ string.punctuation
 
     # original Python 2 (urandom returns str)
     # return "".join(chars[ord(c) % len(chars)] for c in urandom(length))
@@ -19,6 +19,9 @@ def generate_sec_random_password(length):
     return "".join(alphabet[c % len(alphabet)] for c in urandom(length))
 
 if __name__ == "__main__":
-    pwd_length = 15  # creates a 15 character random password
+    pwd_length = 15  # creates a 16 characters random password
     #generate_sec_random_password(pwd_length)
     print(generate_sec_random_password(pwd_length))
+
+
+
