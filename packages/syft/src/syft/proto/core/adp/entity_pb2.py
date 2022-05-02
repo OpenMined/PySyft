7f52f00e7fe328f877ae36775bdc9c0831516ab4
Madhava Jay <me@madhavajay.com>
2022-05-02 10:42:59 +1000

    Regenerate protobufs

--- packages/syft/src/syft/proto/core/adp/entity_pb2.py
+++ packages/syft/src/syft/proto/core/adp/entity_pb2.py
@@ -5,9 +5,8 @@
 # third party
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
-from google.protobuf import message as _message
-from google.protobuf import reflection as _reflection
 from google.protobuf import symbol_database as _symbol_database
+from google.protobuf.internal import builder as _builder
 
 # @@protoc_insertion_point(imports)
 
@@ -23,31 +22,10 @@ DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
     b'\n\x1bproto/core/adp/entity.proto\x12\rsyft.core.adp\x1a%proto/core/common/common_object.proto"\x16\n\x06\x45ntity\x12\x0c\n\x04name\x18\x01 \x01(\t"^\n\x10\x44\x61taSubjectGroup\x12!\n\x02id\x18\x01 \x01(\x0b\x32\x15.syft.core.common.UID\x12\'\n\x08\x65ntities\x18\x02 \x03(\x0b\x32\x15.syft.core.adp.Entityb\x06proto3'
 )
 
-
-_ENTITY = DESCRIPTOR.message_types_by_name["Entity"]
-_DATASUBJECTGROUP = DESCRIPTOR.message_types_by_name["DataSubjectGroup"]
-Entity = _reflection.GeneratedProtocolMessageType(
-    "Entity",
-    (_message.Message,),
-    {
-        "DESCRIPTOR": _ENTITY,
-        "__module__": "proto.core.adp.entity_pb2"
-        # @@protoc_insertion_point(class_scope:syft.core.adp.Entity)
-    },
+_builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
+_builder.BuildTopDescriptorsAndMessages(
+    DESCRIPTOR, "proto.core.adp.entity_pb2", globals()
 )
-_sym_db.RegisterMessage(Entity)
-
-DataSubjectGroup = _reflection.GeneratedProtocolMessageType(
-    "DataSubjectGroup",
-    (_message.Message,),
-    {
-        "DESCRIPTOR": _DATASUBJECTGROUP,
-        "__module__": "proto.core.adp.entity_pb2"
-        # @@protoc_insertion_point(class_scope:syft.core.adp.DataSubjectGroup)
-    },
-)
-_sym_db.RegisterMessage(DataSubjectGroup)
-
 if _descriptor._USE_C_DESCRIPTORS == False:
 
     DESCRIPTOR._options = None
