cf3573f0a62d0543dad48f2b78bd0f09ca6f6899
Shubham Gupta <shubhamgupta3121@gmail.com>
2022-05-04 19:26:49 +0530

    fix `syft.register` method not creating user enter in database. - grid: refactor register api in grid to create new user - syft: refactor register method in grid to convert url to grid url

--- packages/grid/backend/grid/api/auth/register.py
+++ packages/grid/backend/grid/api/auth/register.py
@@ -6,9 +6,27 @@ from fastapi import APIRouter
 from fastapi import Body
 from fastapi.responses import JSONResponse
 
+# syft absolute
+from syft import flags
+from syft.core.node.common.node_manager.role_manager import RoleManager
+from syft.core.node.common.node_manager.user_manager import UserManager
+
 # grid absolute
+from grid.api.users.models import UserPrivate
 from grid.core.config import settings
 from grid.core.node import node
+from grid.utils import send_message_with_reply
+
+if flags.USE_NEW_SERVICE:
+    # syft absolute
+    from syft.core.node.common.node_service.user_manager.new_user_messages import (
+        CreateUserMessage,
+    )
+else:
+    # syft absolute
+    from syft.core.node.common.node_service.user_manager.user_manager_service import (
+        CreateUserMessage,
+    )
 
 router = APIRouter()
 
@@ -31,13 +49,23 @@ def register(data: dict = Body(..., example="sheldon@caltech.edu")) -> Any:
     if "password" not in data:
         return {"error": "Missing 'password' attribute. Please try again"}
 
-    node.get_root_client().users.create(
-        **{
-            "name": data["name"],
-            "email": data["email"],
-            "password": data["password"],
-            "budget": 0,
-        }
+    new_user = {
+        "name": data["name"],
+        "email": data["email"],
+        "password": data["password"],
+        "budget": 0.0,
+    }
+
+    owner_role = RoleManager(node.db_engine).owner_role
+    root_user = UserManager(node.db_engine).first(role=owner_role.id)
+    root_user = UserPrivate.from_orm(root_user)
+
+    reply = send_message_with_reply(
+        signing_key=root_user.get_signing_key(),
+        message_type=CreateUserMessage,
+        **dict(new_user)
     )
 
-    return {"message": "Success! You've now created a new user!"}
+    if flags.USE_NEW_SERVICE:
+        return reply.message
+    return reply.resp_msg
