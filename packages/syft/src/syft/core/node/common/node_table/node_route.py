7c3a3283b3da0c01da012bb823d781036d1847b6
Ionesio Junior <ionesiojr@gmail.com>
2022-05-09 01:57:34 -0300

    ADD vpn_endpoint and vpn_key columns

--- packages/syft/src/syft/core/node/common/node_table/node_route.py
+++ packages/syft/src/syft/core/node/common/node_table/node_route.py
@@ -14,5 +14,7 @@ class NodeRoute(Base):
 
     id = Column(Integer(), primary_key=True, autoincrement=True)
     node_id = Column(Integer, ForeignKey("node.id"))
-    host_or_ip = Column(String(255))
+    host_or_ip = Column(String(255), default="")
     is_vpn = Column(Boolean(), default=False)
+    vpn_endpoint = Column(String(255), default="")
+    vpn_key = Column(String(255), default="")
