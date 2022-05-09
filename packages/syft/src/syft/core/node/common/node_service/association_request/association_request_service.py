6b54043095f3868a73fdf0a39141f9997ed5212b
Ionesio Junior <ionesiojr@gmail.com>
2022-05-09 03:01:34 -0300

    ADD node/node-route creation to association-request service again, since VPNJoinMessage is used only in Domain side, we also need to create node/node-route tables at Network side

--- packages/syft/src/syft/core/node/common/node_service/association_request/association_request_service.py
+++ packages/syft/src/syft/core/node/common/node_service/association_request/association_request_service.py
@@ -186,6 +186,19 @@ def recv_association_request_msg(
         )
         node.association_requests.set(source=msg.source, target=msg.target, response=msg.response)  # type: ignore
 
+    # get or create a new node and node_route which represents the opposing node which
+    # is supplied in the metadata
+    try:
+        node_id = node.node.create_or_get_node(  # type: ignore
+            node_uid=msg.metadata["node_id"], node_name=msg.metadata["node_name"]
+        )
+        is_vpn = check_if_is_vpn(host_or_ip=msg.metadata["host_or_ip"])
+        node.node_route.update_route_for_node(  # type: ignore
+            node_id=node_id, host_or_ip=msg.metadata["host_or_ip"], is_vpn=is_vpn
+        )
+    except Exception as e:
+        error(f"Failed to save the node and node_route rows. {e}")
+
     return SuccessResponseMessage(
         address=msg.reply_to,
         resp_msg="Association request received!",
