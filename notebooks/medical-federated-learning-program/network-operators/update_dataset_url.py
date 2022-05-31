a264e59348f7c85d449b0159f75d23e8e2b8b770
Madhava Jay <me@madhavajay.com>
2022-05-31 14:48:12 +1000

    Updated notebook

--- notebooks/medical-federated-learning-program/network-operators/update_dataset_url.py
+++ notebooks/medical-federated-learning-program/network-operators/update_dataset_url.py
@@ -48,8 +48,21 @@ if __name__ == "__main__":
     status = replace_variable(data_search_string, data_url)
     print("replacing data url", status)
     if len(sys.argv) > 2:
-        institution = sys.argv[2]
+        host_ip = sys.argv[2]
+        host_ip_search = "auto_detect_domain_host_ip()"
+        host_ip_replace = f'"{host_ip}"'
+        status = replace_variable(host_ip_search, host_ip_replace)
+        print("replacing host ip", status)
+    if len(sys.argv) > 3:
+        institution = sys.argv[3]
         institution_search_string = 'DOMAIN_NAME = "My Institution Name"'
         institution_name = f'DOMAIN_NAME = "{institution}"'
         status = replace_variable(institution_search_string, institution_name)
         print("replacing institution name", status)
+
+    if len(sys.argv) > 4:
+        network_name = sys.argv[4]
+        network_search = 'NETWORK_NAME = ""'
+        network_variable = f'NETWORK_NAME = "{network_name}"'
+        status = replace_variable(network_search, network_variable)
+        print("replacing network name", status)
