14892f3e25065f85fcca953eac681f50880c0c48
IonesioJunior <ionesiojr@gmail.com>
2022-06-03 15:29:31 -0300

    ADD new Syft ErrorResponseMessage

--- packages/syft/src/syft/proto/grid/messages/success_resp_message_pb2.py
+++ packages/syft/src/syft/proto/grid/messages/success_resp_message_pb2.py
@@ -20,7 +20,7 @@ from syft.proto.core.common import (
 from syft.proto.core.io import address_pb2 as proto_dot_core_dot_io_dot_address__pb2
 
 DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
-    b'\n.proto/grid/messages/success_resp_message.proto\x12\x12syft.grid.messages\x1a%proto/core/common/common_object.proto\x1a\x1bproto/core/io/address.proto"y\n\x16SuccessResponseMessage\x12%\n\x06msg_id\x18\x01 \x01(\x0b\x32\x15.syft.core.common.UID\x12\x10\n\x08resp_msg\x18\x03 \x01(\t\x12&\n\x07\x61\x64\x64ress\x18\x04 \x01(\x0b\x32\x15.syft.core.io.Addressb\x06proto3'
+    b'\n.proto/grid/messages/success_resp_message.proto\x12\x12syft.grid.messages\x1a%proto/core/common/common_object.proto\x1a\x1bproto/core/io/address.proto"y\n\x16SuccessResponseMessage\x12%\n\x06msg_id\x18\x01 \x01(\x0b\x32\x15.syft.core.common.UID\x12\x10\n\x08resp_msg\x18\x03 \x01(\t\x12&\n\x07\x61\x64\x64ress\x18\x04 \x01(\x0b\x32\x15.syft.core.io.Address"w\n\x14\x45rrorResponseMessage\x12%\n\x06msg_id\x18\x01 \x01(\x0b\x32\x15.syft.core.common.UID\x12\x10\n\x08resp_msg\x18\x03 \x01(\t\x12&\n\x07\x61\x64\x64ress\x18\x04 \x01(\x0b\x32\x15.syft.core.io.Addressb\x06proto3'
 )
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
@@ -32,4 +32,6 @@ if _descriptor._USE_C_DESCRIPTORS == False:
     DESCRIPTOR._options = None
     _SUCCESSRESPONSEMESSAGE._serialized_start = 138
     _SUCCESSRESPONSEMESSAGE._serialized_end = 259
+    _ERRORRESPONSEMESSAGE._serialized_start = 261
+    _ERRORRESPONSEMESSAGE._serialized_end = 380
 # @@protoc_insertion_point(module_scope)
