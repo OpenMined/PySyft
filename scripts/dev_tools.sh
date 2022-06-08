76467c92c6e8324e86fba714c2a52aa40ffcaa55
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2022-06-07 18:52:18 -0700

    Search for SeaweedFS port

--- scripts/dev_tools.sh
+++ scripts/dev_tools.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ -z "$1" ]]; then
-    # list db, redis and rabbitmq ports
-    docker ps --format '{{.Names}}' | grep "db\|redis\|queue" | xargs -I '{}' bash -c 'echo "{} -> $(python3 -c "import sys; [print(l.split(\":\")[-1]) for l in sys.argv[1].split(\"\n\") if l.split(\"/tcp\")[0] in sys.argv[2].split(\",\")]" "$(docker port {})" "15672,5432,6379")"' - {}
+    # list db, redis, rabbitmq, and seaweedfs ports
+    docker ps --format '{{.Names}}' | grep "db\|redis\|queue\|seaweedfs" | xargs -I '{}' bash -c 'echo "{} -> $(python3 -c "import sys; [print(l.split(\":\")[-1]) for l in sys.argv[1].split(\"\n\") if l.split(\"/tcp\")[0] in sys.argv[2].split(\",\")]" "$(docker port {})" "15672,5432,6379,9333")"' - {}
 else
     PORT=$1
     if docker ps | grep ":${PORT}" | grep -q 'redis'; then
