49795859891f129fc8a894e6bfea491ca527c296
IshanMi <mishra.ishan@gmail.com>
2022-06-20 22:15:59 -0400

    Add nn linear layer

--- packages/syft/src/syft/core/tensor/nn/__init__.py
+++ packages/syft/src/syft/core/tensor/nn/__init__.py
@@ -3,5 +3,6 @@ from .pooling import MaxPool2d
 from .pooling import AvgPool2d
 from .batch_norm import BatchNorm2d
 from .conv_layers import Conv2d
+from .linear import Linear
 
-__all__ = ["MaxPool2d", "AvgPool2d", "BatchNorm2d", "Conv2d"]
+__all__ = ["MaxPool2d", "AvgPool2d", "BatchNorm2d", "Conv2d", "Linear"]
---
+++ packages/syft/src/syft/core/tensor/nn/linear.py
@@ -0,0 +1,22 @@
+from ..autodp.phi_tensor import PhiTensor
+from ...adp.data_subject_list import DataSubjectList
+
+from torch import Tensor
+from torch import nn
+
+
+def Linear(image: PhiTensor, in_features, out_features, bias=True):
+    linear_layer = nn.Linear(in_features, out_features, bias=True)
+    data = linear_layer(Tensor(image.child.decode())).detach().numpy()
+    minv = linear_layer(Tensor(image.ones_like() * image.min_vals.data)).detach().numpy()
+    maxv = linear_layer(Tensor(image.ones_like() * image.max_vals.data)).detach().numpy()
+
+    return PhiTensor(
+        child=data,
+        data_subjects=DataSubjectList(
+            one_hot_lookup=image.data_subjects.one_hot_lookup,
+            data_subjects_indexed=image.data_subjects.zeros_like(data)
+        ),
+        min_vals=minv,
+        max_vals=maxv
+    )
