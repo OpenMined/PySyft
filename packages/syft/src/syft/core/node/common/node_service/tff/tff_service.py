3cfbedb4a9fa6652397c8facc1941a87859ee0c5
Milea Teodor Daniel <teo.milea97@gmail.com>
2022-06-22 18:50:22 +0300

    working program logic with fixed model

--- packages/syft/src/syft/core/node/common/node_service/tff/tff_service.py
+++ packages/syft/src/syft/core/node/common/node_service/tff/tff_service.py
@@ -2,6 +2,7 @@
 import asyncio
 import collections
 import functools
+import io
 import logging
 import os
 from typing import Any
@@ -9,55 +10,50 @@ from typing import List
 from typing import Optional
 from typing import Tuple
 from typing import Type
+import zipfile
 
 # third party
+from absl.testing import absltest
 from nacl.signing import VerifyKey
+import numpy as np
 import tensorflow as tf
 import tensorflow_federated as tff
 from tensorflow_federated.proto.v0 import computation_pb2 as pb
+
+# from tensorflow_federated.python.core.impl.context_stack import context_stack_test_utils
+from tensorflow_federated.python.core.backends.native import compiler
+from tensorflow_federated.python.core.impl.context_stack import set_default_context
 from tensorflow_federated.python.core.impl.execution_contexts import (
     async_execution_context,
 )
 
+# from tensorflow_federated.python.core.impl.executors import executor_test_utils
+from tensorflow_federated.python.core.impl.executors import eager_tf_executor
+from tensorflow_federated.python.core.impl.executors import executor_stacks
+from tensorflow_federated.python.core.impl.types import computation_types
+from tensorflow_federated.python.learning.models import functional
+
+# from pybind11_abseil import status
+import torch as th
+
 # relative
+from ......core.common.uid import UID
+
 # import torch as th
+from ......logger import debug
 from ......util import traceback_and_raise
 from .....tensor.tensor import Tensor
 from ....abstract.node import AbstractNode
-from ......core.common.uid import UID
-import numpy as np
-import zipfile
-import io
-
 from ..auth import service_auth
 from ..node_service import ImmediateNodeServiceWithReply
+from .data_backend import MedNISTBackend
+from .data_backend import PySyftDataBackend
+from .data_backend import TestDataBackend
 
 # from .data_backend import MedNISTBackend
 # from .data_backend import PySyftDataBackend
 from .tff_messages import TFFMessage
 from .tff_messages import TFFReplyMessage
-from .data_backend import TestDataBackend, PySyftDataBackend, MedNISTBackend
-from tensorflow_federated.proto.v0 import computation_pb2 as pb
-import tensorflow as tf
-from absl.testing import absltest
-import asyncio
-from tensorflow_federated.python.core.impl.executors import eager_tf_executor
-from tensorflow_federated.python.core.impl.types import computation_types
-# from tensorflow_federated.python.core.impl.executors import executor_test_utils
-from tensorflow_federated.python.core.impl.executors import executor_stacks
-from tensorflow_federated.python.core.impl.context_stack import set_default_context
-# from tensorflow_federated.python.core.impl.context_stack import context_stack_test_utils
-from tensorflow_federated.python.core.backends.native import compiler
-from tensorflow_federated.python.core.impl.execution_contexts import async_execution_context
-from tensorflow_federated.python.learning.models import functional
-from syft.core.tensor.tensor import Tensor
-import logging
-import collections
-import os
-import functools
-
-# from pybind11_abseil import status
-import torch as th
 
 METRICS_TOTAL_SUM = 'total_sum'
 @tff.tf_computation()
@@ -135,7 +131,7 @@ def create_keras_model():
       tf.keras.layers.Dense(6, kernel_initializer='zeros'),
       tf.keras.layers.Softmax(),
   ])
-  
+
 def model_fn(input_spec):
   keras_model = create_keras_model()
   return tff.learning.from_keras_model(
@@ -144,7 +140,6 @@ def model_fn(input_spec):
       loss=tf.keras.losses.SparseCategoricalCrossentropy(),
       metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
 
-@tff.tf_computation
 async def tff_train_federated(
     initialize: tff.Computation,
     train: tff.Computation,
@@ -158,22 +153,20 @@ async def tff_train_federated(
     model_output_manager: tff.program.ReleaseManager,
     program_state_manager: tff.program.ProgramStateManager,
 ):
-    tff.program.check_in_federated_context()
-    logging.info("Running program logic")
+    # tff.program.check_in_federated_context()
+
 
     if program_state_manager is not None:
         structure = initialize()
-        program_state, version = program_state_manager.load_latest(structure)
+        program_state, version = await program_state_manager.load_latest(structure)
     else:
         program_state = None
 
+
     if program_state is not None:
-        logging.info("Loaded program state at version %d", version)
 
         state, start_round = program_state
-
     else:
-        logging.info("Initializing state")
         state = initialize()
         start_round = 1
 
@@ -182,25 +175,30 @@ async def tff_train_federated(
         train_data_iterator = train_data_source.iterator()
 
         for round_number in range(start_round, total_rounds + 1):
+            # debug(f"Train round {round_number}")
             tasks.add_callable(
                 functools.partial(
                     logging.info, "Running round %d of training", round_number
                 )
             )
-
+            
             train_data = train_data_iterator.select(number_of_clients)
-            state, metrics = train(state, train_data)
-            
-            # if train_output_managers is not None:
-            #     tasks.add_all(*[m.release(metrics, round_number) for m in train_output_managers])
-            
+            # state, metrics = train(state, train_data)
+            output = train(state, train_data)
+            state = output.state
+            metrics = output.metrics
+
+            if train_output_managers is not None:
+                tasks.add_all(*[m.release(metrics, round_number) for m in train_output_managers])
+
             # if program_state_manager is not None:
             #     program_state = (state, start_round)
             #     tasks.add(program_state_manager.save(program_state, round_number))
-            value = await metrics['train']['sparse_categorical_accuracy'].get_value()
-            print(value)
-            
-                
+            # print(metrics)
+            # value = await metrics['train']['sparse_categorical_accuracy'].get_value()
+            # print(value)
+
+
         tasks.add_callable(
             functools.partial(logging.info, "Running one round of evaluation")
         )
@@ -208,29 +206,30 @@ async def tff_train_federated(
         evaluation_data_iterator = evaluation_data_source.iterator()
         evaluation_data = evaluation_data_iterator.select(number_of_clients)
         evaluation_metrics = evaluation(state, evaluation_data)
-        
-        
+
+
         # if evaluation_output_managers is not None:
         #     tasks.add_all(*[
         #         m.release(evaluation_metrics, round_number)
         #         for m in train_output_managers
         #     ])
-        
+
         # if model_output_manager is not None:
         #     tasks.add(model_output_manager.release(state))
 
+
 def tff_program(
-    node, 
+    node,
     params,
     model
-):    
-    dataset_id = str(params['dataset_id']) 
-    total_rounds = int(params['rounds']) 
+):
+    dataset_id = str(params['dataset_id'])
+    total_rounds = int(params['rounds'])
     number_of_clients = int(params['no_clients'])
     OUTPUT_DIR = str(params['OUTPUT_DIR'])
     noise_multiplier = float(params['noise_multiplier'])
     clients_per_round = int(params['clients_per_round'])
-    
+
     # tff.backends.native.execution_contexts.set_local_async_python_execution_context(reference_resolving_clients=True)
     context = tff.backends.native.create_local_async_python_execution_context()
     context = tff.program.NativeFederatedContext(context)
@@ -248,31 +247,34 @@ def tff_program(
             ]
 
     dataset = tf.data.Dataset.from_tensor_slices((images, labels))
-    
-    datasets = [dataset.map(preprocess)] * number_of_clients 
 
+    datasets = [dataset.map(preprocess)] * number_of_clients
 
     train_data_source = tff.program.DatasetDataSource(datasets)
     evaluation_data_source = tff.program.DatasetDataSource(datasets)
 
+    debug("Data sources created")
+
     # TODO parametrize this
     input_spec = collections.OrderedDict(
-            x=tf.TensorSpec(shape=(1,64*64), dtype=tf.int32, name=None),
-            y=tf.TensorSpec(shape=(1,1), dtype=tf.int32, name=None),
+            x=tf.TensorSpec(shape=(1,64*64), dtype=tf.float64, name=None),
+            y=tf.TensorSpec(shape=(1,1), dtype=tf.float64, name=None),
     )
 
     aggregation_factory = tff.learning.model_update_aggregator.dp_aggregator(
       noise_multiplier, clients_per_round)
-    
-    iterative_process = tff.learning.build_federated_averaging_process(
-    lambda: model_fn(input_spec=input_spec),
+
+    iterative_process = tff.learning.algorithms.build_unweighted_fed_avg(
+    lambda: model_fn(input_spec),
     client_optimizer_fn=lambda: tf.keras.optimizers.SGD(learning_rate=0.02),
     server_optimizer_fn=lambda: tf.keras.optimizers.SGD(learning_rate=1.0),
-    model_update_aggregation_factory=aggregation_factory)
+    )
 
     initialize = iterative_process.initialize
     train = iterative_process.next
     evaluation = tff.learning.build_federated_evaluation(lambda: model_fn(input_spec))
+
+    debug('TFF computations createad')
 
     train_output_managers = [tff.program.LoggingReleaseManager()]
     evaluation_output_managers = [tff.program.LoggingReleaseManager()]
@@ -293,7 +295,9 @@ def tff_program(
         program_state_dir
     )
 
-    asyncio.ensure_future(tff_train_federated(
+    debug('Managers created - training beginning')
+        
+    asyncio.run(tff_train_federated(
         initialize=initialize,
         train=train,
         train_data_source=train_data_source,
@@ -306,7 +310,20 @@ def tff_program(
         model_output_manager=model_output_manager,
         program_state_manager=program_state_manager
     ))
-
+    
+    # tff_train_federated_sync(
+    #         initialize=initialize,
+    #         train=train,
+    #         train_data_source=train_data_source,
+    #         evaluation=evaluation,
+    #         evaluation_data_source=evaluation_data_source,
+    #         total_rounds=total_rounds,
+    #         number_of_clients=number_of_clients,
+    #         train_output_managers=train_output_managers,
+    #         evaluation_output_managers=evaluation_output_managers,
+    #         model_output_manager=model_output_manager,
+    #         program_state_manager=program_state_manager
+    #     )
 
 class TFFService(ImmediateNodeServiceWithReply):
     @staticmethod
@@ -316,25 +333,22 @@ class TFFService(ImmediateNodeServiceWithReply):
     ) -> TFFReplyMessage:
         if verify_key is None:
             traceback_and_raise("Can't process TFFService with no verification key.")
-        
+
         logging.basicConfig(level=logging.INFO)
         # logger = logging.getLogger('absl')
         # logger.setLevel(level=logging.INFO)
-        
+
         # parse params
         params = msg.payload.params
         print(params)
-        
+
         # read model
         zf = zipfile.ZipFile(io.BytesIO(msg.payload.model_bytes), 'r')
         zf.extractall('tmp_dir')
         functional_model_reloaded = tff.learning.models.load_functional_model('tmp_dir')
         model = functional.model_from_functional(functional_model_reloaded)
-        
+
         tff_program(node, params, model)
-        
-        
-        
         result = msg.payload.run(node=node, verify_key=verify_key)
         return TFFReplyMessage(payload=result, address=msg.reply_to)
 
