22a3ec553d915d43833d47445949bf81fec99838
IonesioJunior <ionesiojr@gmail.com>
2022-06-23 22:42:49 -0300

    Remove unused import

--- packages/syft/src/syft/core/node/network_interface.py
+++ packages/syft/src/syft/core/node/network_interface.py
@@ -4,7 +4,6 @@ from pydantic import BaseSettings
 # relative
 from .abstract.node_service_interface import NodeServiceInterface
 from .common.node_manager.association_request_manager import AssociationRequestManager
-from .common.node_manager.dataset_manager import DatasetManager
 from .common.node_manager.role_manager import RoleManager
 from .common.node_manager.setup_manager import SetupManager
 from .common.node_manager.user_manager import UserManager
