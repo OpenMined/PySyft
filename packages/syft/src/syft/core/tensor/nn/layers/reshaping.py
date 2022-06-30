fixed backprop for linear

packages/syft/src/syft/core/tensor/nn/layers/reshaping.py
@@ -30,7 +30,9 @@ class Flatten(Layer):
     def forward(self, input: PhiTensor, *args, **kwargs):
         self.last_input_shape = input.shape
 
-        flattened_shape = input.shape[: self.outdim - 1] + (-1,)
+        to_flatten = np.prod(self.last_input_shape[self.outdim - 1 :])
+        flattened_shape = input.shape[: self.outdim - 1] + (to_flatten,)
+        # flattened_shape = input.shape[: self.outdim - 1] + (-1,)
         return input.reshape(flattened_shape)
 
     def backward(self, pre_grad: PhiTensor, *args, **kwargs):