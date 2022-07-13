1d3e602898d13b29c76f168a7eaf7140dd034cd9
Shubham Gupta <shubhamgupta3121@gmail.com>
2022-07-13 16:32:09 +0530

    add python script to upload dataset

--- scripts/aa_demo/update_domain.sh
+++ scripts/aa_demo/update_domain.sh
@@ -1,8 +1,27 @@
+# $1 domain ip
+# $2 dataset url
+# install syft in dev mode
+pip install -e packages/syft
+
+# get domain name
 DOMAIN_NAME="$(sudo docker ps --format '{{.Names}}' | grep "celery" |rev | cut -c 16- | rev)"
 echo "Domain Name: ${DOMAIN_NAME}"
 echo "Nuking  Domain ..  >:)";
-hagrid land all;
-echo "Launching Domain .. hahaha >:)";
-hagrid launch ${DOMAIN_NAME} to docker:80  --tail=false --dev
-echo "finished"
 
+# destroy current domain
+hagrid land all
+echo "Launching Domain .. hahaha >:)";
+
+# re-launch domain
+hagrid launch ${DOMAIN_NAME} to docker:80  --tail=false --dev
+
+# wait for domain to be up
+hagrid check --wait --silent
+
+echo "Domain lauch succeeded."
+echo "Starting to upload dataset"
+
+# upload dataset
+python upload_dataset.py $1 $2
+
+echo "Upload dataset script complete."
