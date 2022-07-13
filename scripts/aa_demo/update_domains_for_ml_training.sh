3f75463a8e18fb413912ce5c3dce341208190551
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2022-07-13 12:23:08 +0000

    final script for upload

--- scripts/aa_demo/update_domains_for_ml_training.sh
+++ scripts/aa_demo/update_domains_for_ml_training.sh
@@ -9,16 +9,16 @@ do
 	pass=$(echo $line | cut -d "," -f 3)
 	dataset_url=$(echo $line | cut -d "," -f 4)
 
-	COMMAND="cd PySyft &&git restore scripts && git stash && \
+	COMMAND="cd PySyft &&sudo git restore scripts &&sudo  git stash && \
 			sudo git fetch origin model_training_tests && \
-			 git pull origin model_training_tests && \
-			 git stash apply stash@{0} && \
-			  chmod +x scripts/aa_demo/update_domain.sh &&\
+			sudo  git pull origin model_training_tests && \
+			sudo git stash apply stash@{0} && \
+			sudo  chmod +x scripts/aa_demo/update_domain.sh &&\
 			  ./scripts/aa_demo/update_domain.sh $ip $dataset_url"
 	echo $ip, $usn, $pass
 	# TODO: Replace azureuser to om and use its proper password
 	# sshpass -p $pass ssh $usn@$ip $(cat update_syft.sh)
-	sshpass -o StrictHostKeyChecking=no -p $pass ssh $usn@$ip  "sudo runuser -l om -c '${COMMAND}'"
+	sshpass  -p $pass ssh $usn@$ip -o StrictHostKeyChecking=no "sudo runuser -l om -c '${COMMAND}'"
 
 done
 
