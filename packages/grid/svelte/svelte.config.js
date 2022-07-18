a283501a8734cf3308fdc50759e155fcca14829d
Thiago Costa Porto <thiago@queenslab.se>
2022-07-18 10:49:20 +0100

    refactor: start the svelte frontend, with playwright and tailwindcss

---
+++ packages/grid/svelte/svelte.config.js
@@ -0,0 +1,14 @@
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: preprocess({
+    postcss: true
+  }),
+  kit: {
+    adapter: adapter()
+  }
+};
+
+export default config;
