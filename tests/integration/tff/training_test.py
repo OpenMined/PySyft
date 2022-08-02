20de3b9165633cae95505cb66574270f24d76c92
teo <teo.milea97@gmail.com>
2022-08-02 13:00:19 +0300

    lowered the dataset and network size

--- tests/integration/tff/training_test.py
+++ tests/integration/tff/training_test.py
@@ -10,65 +10,29 @@ from enum import Enum
 from collections import defaultdict
 import numpy as np
 from syft.core.adp.data_subject_list import DataSubjectList
-
-def create_keras_model():
-  return tf.keras.models.Sequential([
-        tf.keras.layers.InputLayer(input_shape=(64,64,1), name='input'),
-        tf.keras.layers.Conv2D(32, kernel_size=(5, 5), activation="relu", name='conv1'),
-        tf.keras.layers.MaxPooling2D(pool_size=(2, 2), name='pool1'),
-      #   tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu", name='conv2'),
-      #   tf.keras.layers.MaxPooling2D(pool_size=(2, 2), name='pool2'),
-      #   tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation="relu", name='conv3'),
-      #   tf.keras.layers.MaxPooling2D(pool_size=(2, 2), name='pool3'),
-      #   tf.keras.layers.Conv2D(256, kernel_size=(3, 3), activation="relu", name='conv4'),
-      #   tf.keras.layers.MaxPooling2D(pool_size=(2, 2), name='pool4'),
-        tf.keras.layers.Flatten(),
-        # tf.keras.layers.Dropout(0.5),
-        tf.keras.layers.Dense(32, kernel_initializer='zeros'),
-        tf.keras.layers.ReLU(),
-        tf.keras.layers.Dense(6, kernel_initializer='zeros'),
-        tf.keras.layers.Softmax(),
-  ])
   
-def get_label_mapping():
-    # the data uses the following mapping
-    mapping = {
-        "AbdomenCT": 0, 
-        "BreastMRI": 1, 
-        "CXR": 2, 
-        "ChestCT": 3, 
-        "Hand": 4, 
-        "HeadCT": 5
-    }
-    return mapping
+def create_keras_model():
+      return tf.keras.models.Sequential([
+            tf.keras.layers.InputLayer(input_shape=(100,)),
+            tf.keras.layers.Dense(10, kernel_initializer='zeros'),
+            tf.keras.layers.Softmax(),
+      ])
 
 def load_dataset(domain):
-    df = pd.read_pickle("./MedNIST.pkl")
-    mapping = get_label_mapping()
+    data_subjects = DataSubjectList.from_objs(['Test'])
 
-    subset_idx = []
-    size = 100
-    step = 10000
-    for i in range(len(mapping)):
-        subset_idx.extend(list(range(step * i, step* i + size)))
+    train_data = np.random.randint(256,size=(1,100))
+    label_data = np.array([0])
 
-
-    data_subjects = DataSubjectList.from_series(df['patient_id'][subset_idx])
-
-    images = df['image'][subset_idx]
-    images = np.dstack(images.values).astype(np.int64)
-    images = np.rollaxis(images,-1)
-
-    labels = df['label'][subset_idx].to_numpy().astype("int64")
-    train_image_data = sy.Tensor(images).annotated_with_dp_metadata(
+    train_image_data = sy.Tensor(train_data).annotated_with_dp_metadata(
     min_val=0, max_val=255, data_subjects=data_subjects
     )
-    train_label_data = sy.Tensor(labels).annotated_with_dp_metadata(
+    train_label_data = sy.Tensor(label_data).annotated_with_dp_metadata(
         min_val=0, max_val=5, data_subjects=data_subjects
     )
 
     domain.load_dataset(
-        name='Mixed MedNIST 64 fo real',
+        name='Test',
         assets={
             'images': train_image_data,
             "labels": train_label_data
@@ -82,7 +46,7 @@ def test_tff():
     
 @pytest.mark.tff
 def test_training():
-    domain = sy.login(email="info@openmined.org", password="changethis", port=9082)    
+    domain = sy.login(email="info@openmined.org", password="changethis", port=9081)    
     load_dataset(domain)
     
     # Set params
