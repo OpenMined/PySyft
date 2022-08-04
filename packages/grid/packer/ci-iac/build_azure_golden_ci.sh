740f43cab5053ea84c5d7df5ee9bb314ac26c316
bitsofsteve <cyberlexix@gmail.com>
2022-08-04 18:28:04 +0100

    fixed build.sh

--- packages/grid/packer/ci-iac/build_azure_golden_ci.sh
+++ packages/grid/packer/ci-iac/build_azure_golden_ci.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 packer init ci-golden.pkr.hcl
-PACKER_LOG=1 PACKER_LOG_PATH=./packer.log packer build -var-file=azure_vars.json -on-error=ask -var "subscription_id=${1}" ci-golden.pkr.hcl
+packer build -var-file azure_vars.json  ci-golden.pkr.hcl
