918a3f594627b21752bbde96197ffc146f75b436
IonesioJunior <ionesiojr@gmail.com>
2022-09-07 15:35:54 -0300

    REMOVE/Comment SyftUser foreign key from the remaining SQL tables

--- packages/syft/src/syft/core/node/common/node_table/user_environment.py
+++ packages/syft/src/syft/core/node/common/node_table/user_environment.py
@@ -11,11 +11,11 @@ class UserEnvironment(Base):
     __tablename__ = "userenvironment"
 
     id = Column(Integer(), primary_key=True, autoincrement=True)
-    user = Column(Integer, ForeignKey("syft_user.id"))
+    # user = Column(Integer, ForeignKey("syft_user.id"))
     environment = Column(Integer, ForeignKey("environment.id"))
 
     def __str__(self) -> str:
         return (
-            f"<UserGroup id: {self.id}, user: {self.user}, "
+            f"<UserGroup id: {self.id},"
             f"group: {self.environment}>"
         )
