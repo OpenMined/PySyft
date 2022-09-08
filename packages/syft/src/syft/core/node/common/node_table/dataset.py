e559703b9151b3c2e384dfb203d29eed6ee2508d
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2022-09-08 17:37:48 +0530

    started porting  dataset table to  NoSQL

--- packages/syft/src/syft/core/node/common/node_table/dataset.py
+++ packages/syft/src/syft/core/node/common/node_table/dataset.py
@@ -1,3 +1,7 @@
+# stdlib
+from typing import Dict
+from typing import List
+
 # third party
 from sqlalchemy import Column
 from sqlalchemy import JSON
@@ -5,6 +9,8 @@ from sqlalchemy import String
 
 # relative
 from . import Base
+from ....common.uid import UID
+from .syft_object import SyftObject
 
 
 class Dataset(Base):
@@ -17,3 +23,27 @@ class Dataset(Base):
     tags = Column(JSON())
     str_metadata = Column(JSON())
     blob_metadata = Column(JSON())
+
+
+class NoSQLAssociationRequest(SyftObject):
+    # version
+    __canonical_name__ = "AssociationRequest"
+    __version__ = 1
+
+    # fields
+    id: UID  # We do not store id in attr_state, as it stores as _id (MongoDB ObjectID)
+    name: str
+    manifest: str
+    description: str
+    tags: Dict
+    str_metadata: Dict
+    blob_metadata: Dict
+
+    # serde / storage rules
+    __attr_state__ = [
+        "name",
+        "manifest" "description" "tags",
+        "str_metadata" "blob_metadata",
+    ]
+    __attr_searchable__: List[str] = []
+    __attr_unique__: List[str] = []
