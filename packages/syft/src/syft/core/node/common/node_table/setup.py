dd0d73580b6cc298b3278e01c9e40cf656d1b819
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2022-09-08 09:26:39 +0530

    removed id_int in setup

--- packages/syft/src/syft/core/node/common/node_table/setup.py
+++ packages/syft/src/syft/core/node/common/node_table/setup.py
@@ -13,7 +13,6 @@ class NoSQLSetup(SyftObject):
     __version__ = 1
 
     # fields
-    id_int: int
     domain_name: str = ""
     description: str = ""
     contact: str = ""
@@ -26,20 +25,21 @@ class NoSQLSetup(SyftObject):
 
     # serde / storage rules
     __attr_state__ = [
-        "id_int",
         "domain_name",
         "description",
-        "contact" "daa" "node_uid",
-        "daa_document" "tags",
+        "contact",
+        "daa",
+        "node_uid",
+        "daa_document",
+        "tags",
         "deployed_on",
         "signing_key",
     ]
-    __attr_searchable__ = ["node_uid", "id_int", "signing_key", "domain_name"]
+    __attr_searchable__ = ["node_uid", "signing_key", "domain_name"]
     __attr_unique__ = ["node_uid"]
 
     def to_dict(self) -> Dict[Any, Any]:
         attr_dict = super().to_dict()
         del attr_dict["id"]
-        del attr_dict["id_int"]
         del attr_dict["daa_document"]
         return attr_dict
