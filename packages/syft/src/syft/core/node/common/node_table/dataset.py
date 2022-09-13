8a508cdac63cd6729ce47d08d7aa1751d3cce48e
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2022-09-13 09:34:28 +0530

    removed SQL Dataset Table and Dataset Manger class

--- packages/syft/src/syft/core/node/common/node_table/dataset.py
+++ packages/syft/src/syft/core/node/common/node_table/dataset.py
@@ -5,28 +5,10 @@ from typing import Dict
 from typing import List
 from typing import Sequence
 
-# third party
-from sqlalchemy import Column
-from sqlalchemy import JSON
-from sqlalchemy import String
-
 # relative
-from . import Base
 from .....lib.python.dict import Dict as SyDict
 from ....common.uid import UID
 from .syft_object import SyftObject
-
-
-class Dataset(Base):
-    __tablename__ = "dataset"
-
-    id = Column(String(256), primary_key=True)
-    name = Column(String(256))
-    manifest = Column(String(2048))
-    description = Column(String(2048))
-    tags = Column(JSON())
-    str_metadata = Column(JSON())
-    blob_metadata = Column(JSON())
 
 
 class NoSQLBinObjDataset(SyftObject):
