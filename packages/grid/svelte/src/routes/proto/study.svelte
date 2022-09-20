6e0664835db6e3699dad3e37e1ef7eea173566c4
Dylan Hrebenach <dhrebenach@fubo.tv>
2022-09-20 16:35:24 -0700

    feat: add study screen and local json for default checklist items

---
+++ packages/grid/svelte/src/routes/proto/study.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { defaultItems } from '$lib/checklist.json';
+</script>
+
+<h1>General Checklist</h1>
+<ol>
+  {#each Object.values(defaultItems) as item}
+    <li>{item}</li>
+  {/each}
+</ol>
