402f25f80f25a6e6df56db628b1280552b51c284
Dylan Hrebenach <dhrebenach@fubo.tv>
2022-09-20 18:17:28 -0700

    chore: add study route to landing page

--- packages/grid/svelte/src/routes/index.svelte
+++ packages/grid/svelte/src/routes/index.svelte
@@ -1,4 +1,8 @@
 <h1 class="text-3xl font-semibold">PyGrid UI</h1>
 <p>Svelte app, 0.8.0</p>
+
 <a href="/login">Login</a>
+<br />
 <a href="/signup">Signup</a>
+<br />
+<a href="/proto/study">Study Prototype</a>
