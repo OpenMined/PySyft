39ad3a7cae3a71903ff4011e0edb8e663719a6c5
IshanMi <mishra.ishan@gmail.com>
2022-09-27 11:29:45 -0400

    Add more test skeletons

--- packages/syft/tests/syft/core/tensor/adp/publish_test.py
+++ packages/syft/tests/syft/core/tensor/adp/publish_test.py
@@ -26,6 +26,21 @@ def test_phi_inputs() -> None:
     pass
 
 
+def test_imaginary_bounds_for_mechanism() -> None:
+    """Test that the system fails with grace when l2_norm is imaginary for some reason"""
+    pass
+
+
+def test_sigma_too_large() -> None:
+    """Test that sigma being too large results in low PB spend"""
+    pass
+
+
+def test_sigma_too_small() -> None:
+    """Test that sigma being too small results in high PB spend"""
+    pass
+
+
 def test_ledger_creating() -> None:
     """Test that a new Data Subject Ledger was initialized properly"""
     pass
@@ -74,4 +89,32 @@ def test_cache_gigantic_expansion() -> None:
 
 def test_cache_index_negative() -> None:
     """Test the cache throws an Exception of some kind when the indices are negative."""
+    pass
+
+
+def test_cache_correct_mapping() -> None:
+    """Test that the cache maps RDP_constants to the proper indices."""
+    pass
+
+
+def test_cache_rounding_small_numbers() -> None:
+    """Test the error in epsilon spend when rdp_constant is a float, and has to be
+    turned into an integer for the index."""
+    pass
+
+
+def test_cache_rounding_large_numbers() -> None:
+    """Test the error in epsilon spend when rdp_constant is a float, and has to be
+    turned into an integer for the index."""
+    pass
+
+
+def test_correct_epsilon_spends() -> None:
+    """Test that for a given value, the epsilon is as expected."""
+    pass
+
+
+def test_multiple_publish_single_value() -> None:
+    """Test that when publish filters values, and we're only publishing a single value,
+    calling .all() doesn't result in system failure"""
     pass
