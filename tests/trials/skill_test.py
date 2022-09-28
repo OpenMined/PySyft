74e711057af0361e7c0b9f6d57b0b263b84dc713
Madhava Jay <me@madhavajay.com>
2022-09-28 18:55:45 +1000

    Added initial PR padawan trial

---
+++ tests/trials/skill_test.py
@@ -0,0 +1,13 @@
+# "Do or do not. There is no try." — Yoda
+
+
+def get_padawans(cohort):
+    # add yourself to the temple trial roster
+    data = { 
+        "R2Q4" : { "skywalker": "PASSED" }
+    }
+    return data[cohort]
+
+
+def test_trial_of_skill():
+    assert get_padawans("R2Q4")["skywalker"] ==  "FAILED"
