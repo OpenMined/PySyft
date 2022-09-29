e561b0bd0c49f3878620bc1851ec7ae698d164f5
Madhava Jay <me@madhavajay.com>
2022-09-29 15:27:10 +1000

    🛕

--- packages/syft/tests/trials/skill_test.py
+++ packages/syft/tests/trials/skill_test.py
@@ -1,13 +1,11 @@
 # "Do or do not. There is no try." — Yoda
-# stdlib
-from typing import Dict
 
-
-def get_padawans(cohort: str) -> Dict[str, str]:
+def get_padawans(cohort):
     # add yourself to the temple trial roster
-    data = {"R2Q4": {"skywalker": "PASSED"}}
+    data = {
+        "R2Q4": {"skywalker": "PASSED" }}
     return data[cohort]
 
 
-def test_trial_of_skill() -> None:
-    assert get_padawans("R2Q4")["skywalker"] == "PASSED"
+def test_trial_of_skill():
+    assert get_padawans("R2Q4")["skywalker"]  == ""
