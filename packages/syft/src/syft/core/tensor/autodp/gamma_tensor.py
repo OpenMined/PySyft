2ea77bde56dfff08a0ac473a5c15c8c1ac4166d0
IshanMi <mishra.ishan@gmail.com>
2022-10-03 14:27:09 -0400

    Working GT trace, min, max tests

--- packages/syft/src/syft/core/tensor/autodp/gamma_tensor.py
+++ packages/syft/src/syft/core/tensor/autodp/gamma_tensor.py
@@ -2710,15 +2710,12 @@ class GammaTensor:
         sources = dict()
         sources[self.id] = self
         result = self.child.trace(offset, axis1, axis2)
+        num = np.ones_like(self.child).trace(offset, axis1, axis2)
         return GammaTensor(
             child=result,
             data_subjects=self.data_subjects.trace(offset, axis1, axis2),
-            min_vals=lazyrepeatarray(
-                data=self.min_vals.data * result.size, shape=result.shape
-            ),
-            max_vals=lazyrepeatarray(
-                data=self.max_vals.data * result.size, shape=result.shape
-            ),
+            min_vals=lazyrepeatarray(data=self.min_vals.data * num, shape=result.shape),
+            max_vals=lazyrepeatarray(data=self.max_vals.data * num, shape=result.shape),
             func_str=GAMMA_TENSOR_OP.TRACE.value,
             sources=sources,
         )
