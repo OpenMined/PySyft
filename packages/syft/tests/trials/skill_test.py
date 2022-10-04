6d8c70adf4d99446901cc1124f8c1f99b093f4ae
vinalb-oblivious <vinal@oblivious.ai>
2022-10-04 17:16:06 +0530

    Padawan Session 2

--- packages/syft/tests/trials/skill_test.py
+++ packages/syft/tests/trials/skill_test.py
@@ -5,10 +5,11 @@ from typing import Dict
 
 def get_padawans(cohort: str) -> Dict[str, str]:
     # add yourself to the temple trial roster
-    data = {"R2Q4": {"skywalker": "PASSED", "saffron": "PASSED"}}
+    data = {"R2Q4": {"skywalker": "PASSED", "saffron": "PASSED", "vinal": "PASSED"}}
     return data[cohort]
 
 
 def test_trial_of_skill() -> None:
     assert get_padawans("R2Q4")["skywalker"] == "PASSED"
     assert get_padawans("R2Q4")["saffron"] == "PASSED"
+    assert get_padawans("R2Q4")["vinal"] == "PASSED"
