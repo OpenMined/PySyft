6e7ee5f90eb3a1a7f3272b6356f0e0c5219ff04b
Yang yuqiao <yyqfaust@gmail.com>
2022-10-04 18:14:00 +0900

    pass the trial

--- packages/syft/tests/trials/skill_test.py
+++ packages/syft/tests/trials/skill_test.py
@@ -1,12 +1,12 @@
 # "Do or do not. There is no try." — Yoda
 
-def get_padawans(cohort):
+
+def get_padawans(cohort: str) -> dict[str, str]:
     # add yourself to the temple trial roster
-    data = {
-        "R2Q4": {"skywalker": "PASSED" }}
+    data = {"R2Q4": {"skywalker": "PASSED", "yangyuqiao": "PASSED"}}
     return data[cohort]
 
 
-def test_trial_of_skill():
-    assert get_padawans("R2Q4")["skywalker"]  == ""
+def test_trial_of_skill() -> None:
+    assert get_padawans("R2Q4")["skywalker"] == "PASSED"
     assert len(get_padawans("R2Q4")) > 1
