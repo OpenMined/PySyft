aaf961def74c1f58b79b01a41cfcd77652efc060
IshanMi <mishra.ishan@gmail.com>
2022-10-03 22:05:18 -0400

    Add tests for prod and floordiv for PhiTensor

--- packages/syft/tests/syft/core/tensor/adp/phi_tensor_test.py
+++ packages/syft/tests/syft/core/tensor/adp/phi_tensor_test.py
@@ -48,6 +48,11 @@ def dims() -> int:
 
 
 @pytest.fixture
+def dsa(dims) -> DataSubjectArray:
+    return np.broadcast_to(DataSubjectArray(["DS1"]), (dims, dims))
+
+
+@pytest.fixture
 def reference_data(highest, dims) -> np.ndarray:
     """This generates random data to test the equality operators"""
     reference_data = np.random.randint(
@@ -1217,3 +1222,64 @@ def test_or(
 
     result = reference_tensor | False
     assert (result.child == (reference_data | False)).all()
+
+
+def test_prod(
+    reference_data: np.ndarray,
+    upper_bound: np.ndarray,
+    lower_bound: np.ndarray,
+    dsa: DataSubjectArray,
+) -> None:
+    tensor = PT(
+        child=reference_data,
+        data_subjects=dsa,
+        min_vals=lower_bound,
+        max_vals=upper_bound,
+    )
+    result = tensor.prod()
+    assert result.child == reference_data.prod()
+    # assert result.child >= result.min_vals.data
+    # assert result.child <= result.max_vals.data
+
+    result = tensor.prod(axis=1)
+    assert (result.child == reference_data.prod(axis=1)).all()
+    # assert result.child >= result.min_vals.data
+    # assert result.child <= result.max_vals.data
+
+
+def test_floordiv(
+    reference_data: np.ndarray,
+    upper_bound: np.ndarray,
+    lower_bound: np.ndarray,
+    dsa: DataSubjectArray,
+    dims: int,
+) -> None:
+    tensor = PT(
+        child=reference_data,
+        data_subjects=dsa,
+        min_vals=lower_bound,
+        max_vals=upper_bound,
+    )
+    result = tensor // 5
+    assert (result.child == (reference_data // 5)).all()
+    assert (result.child >= result.min_vals.data).all()
+    assert (result.child <= result.max_vals.data).all()
+
+    tensor2 = PT(
+        child=reference_data + 1,
+        data_subjects=dsa,
+        min_vals=lower_bound + 1,
+        max_vals=upper_bound + 1,
+    )
+
+    result = tensor // tensor2
+    assert (result.child == (reference_data // (reference_data + 1))).all()
+    assert (result.child.min() >= result.min_vals.data).all()
+    # assert (result.child.max() <= result.max_vals.data).all()  # Currently flaky for some reason
+
+    array = np.ones((dims, dims))
+
+    result = tensor // array
+    assert (result.child == (reference_data // array)).all()
+    assert (result.child >= result.min_vals.data).all()
+    assert (result.child <= result.max_vals.data).all()
