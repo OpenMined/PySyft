0a9f43168062104ad7ea74f338debf79a8b97d74
kanak8278 <kanak8278@gmail.com>
2022-10-06 13:45:11 +0000

    fixed test trial of skill_test.py

--- packages/syft/tests/trials/skill_test.py
+++ packages/syft/tests/trials/skill_test.py
@@ -1,12 +1,15 @@
 # "Do or do not. There is no try." — Yoda
+# stdlib
+from typing import Dict
 
-def get_padawans(cohort):
+
+def get_padawans(cohort: str) -> Dict:
     # add yourself to the temple trial roster
-    data = {
-        "R2Q4": {"skywalker": "PASSED" }}
+    data = {"R2Q4": {"skywalker": "PASSED", "kanak": "PASSED"}}
     return data[cohort]
 
 
-def test_trial_of_skill():
-    assert get_padawans("R2Q4")["skywalker"]  == ""
+def test_trial_of_skill() -> None:
+    assert get_padawans("R2Q4")["skywalker"] == "PASSED"
+    assert get_padawans("R2Q4")["kanak"] == "PASSED"
     assert len(get_padawans("R2Q4")) > 1
