f33e3d9d42ce8520d53e7b47691baca244fe98c3
annareithmeir <anna.reithmeir@gmail.com>
2022-10-10 12:49:11 +0200

    trial

--- packages/syft/tests/trials/skill_test.py
+++ packages/syft/tests/trials/skill_test.py
@@ -1,12 +1,15 @@
 # "Do or do not. There is no try." — Yoda
+# stdlib
+from typing import Dict
 
-def get_padawans(cohort):
+
+def get_padawans(cohort: str) -> Dict[str, str]:
     # add yourself to the temple trial roster
-    data = {
-        "R2Q4": {"skywalker": "PASSED" }}
+    data = {"R2Q4": {"skywalker": "PASSED", "anna": "PASSED"}}
     return data[cohort]
 
 
-def test_trial_of_skill():
-    assert get_padawans("R2Q4")["skywalker"]  == ""
+def test_trial_of_skill() -> None:
+    assert get_padawans("R2Q4")["skywalker"] == "PASSED"
+    assert get_padawans("R2Q4")["anna"] == "PASSED"
     assert len(get_padawans("R2Q4")) > 1
