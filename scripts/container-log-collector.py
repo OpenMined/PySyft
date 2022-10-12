d51bde36d47e691a6e63dd4488041b9f3852f731
bitsofsteve <cyberlexix@gmail.com>
2022-10-12 16:41:20 +0100

    Steup log collector for CI on faiure - added a script to collect logs - added a step in tox to collect logs before landing nodes - added CI tasks to upload artifact

---
+++ scripts/container-log-collector.py
@@ -0,0 +1,20 @@
+#Loop through all containers and prinnt container id and container name using subprocess module
+import subprocess
+import os
+
+#Get all containers
+containers = subprocess.check_output("docker ps -a -q", shell=True).decode("utf-8").split()
+# print("Containers: ", containers)
+
+#Store logs of each container in a file
+for container in containers:
+    #Get container name
+    container_name = subprocess.check_output("docker inspect --format '{{.Name}}' " + container, shell=True).decode("utf-8").strip()
+    print("Container Name: ", container_name)
+    #Get container logs
+    container_logs = subprocess.check_output("docker logs " + container, shell=True).decode("utf-8")
+    
+    #Store container logs in a file
+    with open(f"./{container_name}.log", "w") as f:
+        f.write(container_logs)
+        f.close()
