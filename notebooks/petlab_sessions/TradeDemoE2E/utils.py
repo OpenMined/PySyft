c35751ccf995a4f0d6ccd6e7f7645d36345b1023
Shubham Gupta <shubhamgupta3121@gmail.com>
2022-10-12 14:30:49 +0530

    fix header images to have correct numbering and heading name clean up utils.py add comments to trade notebook

--- notebooks/petlab_sessions/TradeDemoE2E/utils.py
+++ notebooks/petlab_sessions/TradeDemoE2E/utils.py
@@ -1,21 +1,22 @@
 # stdlib
-from enum import Enum
-import json
-import os
 import subprocess
 import sys
-import uuid
 
 # third party
-from IPython import get_ipython
-from IPython.display import Javascript  # noqa: F401
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import requests
 
-# syft absolute
-from syft.core.adp.data_subject_list import DataSubjectList  # noqa: F401
+
+class color:
+    PURPLE = "\033[95m"
+    CYAN = "\033[96m"
+    DARKCYAN = "\033[36m"
+    BLUE = "\033[94m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    END = "\033[0m"
 
 
 def auto_detect_domain_host_ip(silent: bool = False) -> str:
@@ -37,40 +38,15 @@ def download_dataset_as_dataframe(dataset_url):
     def load_data_as_df(file_path):
         df = pd.read_csv(file_path)
         unique_trade_flows = df["Trade Flow"].unique()
-        print("Data shape:", df.shape, end="\n\n")
-        print("Columns:", df.columns, end="\n\n")
-        print("Unique Trade Flows:", unique_trade_flows, end="\n\n")
+        print()
+        print(f"{color.BOLD}Data shape: {color.END}", df.shape, end="\n\n")
+        print(f"{color.BOLD}Data Columns: {color.END}", list(df.columns), end="\n\n")
+        print(
+            f"{color.BOLD}Unique Trade Flows: {color.END}", unique_trade_flows, end="\n"
+        )
 
         return df
 
     data = load_data_as_df(dataset_url)
 
     return data
-
-
-def validate_ds_credentials(ds_credentials):
-
-    valid = True
-    for key, val in ds_credentials.items():
-        if not val:
-            print(f"Please set a value for '{key}'.")
-            valid = False
-        elif key != "budget" and type(val) != str:
-            print(f"Value for {key} needs to be a string.")
-            valid = False
-
-    if not valid:
-        print("Please set the missing/incorrect values and re-run this cell")
-    else:
-        print("Data Scientist credentials are valid. Move to the next step.")
-
-
-def output_dataset_url():
-    return """
-    var a = window.location.href.split('#')
-    if (a.length > 1) {
-        element.textContent = 'MY_DATASET_URL="https://raw.githubusercontent.com/OpenMined/datasets/main/TissueMNIST/subsets/TissueMNIST-' + a[1] + '.pkl"'
-    } else {
-        element.textContent = 'Unable to automatically get MY_DATASET_URL please locate it from your session details.'
-    }
-    """  # noqa: E501
