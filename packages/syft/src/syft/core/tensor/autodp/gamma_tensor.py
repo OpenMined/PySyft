Fix incorrect trace min/max calculation in TensorPointer

packages/syft/src/syft/core/tensor/autodp/gamma_tensor.py
@@ -615,14 +615,15 @@ class TensorWrappedGammaTensorPointer(Pointer, PassthroughTensor):
         """
         attr_path_and_name = "syft.core.tensor.tensor.Tensor.trace"
         data_subjects = np.array(self.data_subjects).trace(*args, **kwargs)  # type: ignore
+        num = np.ones(np.array(self.data_subjects).shape).trace(*args, **kwargs)
 
         result = TensorWrappedGammaTensorPointer(
             data_subjects=data_subjects,
             min_vals=lazyrepeatarray(
-                data=self.min_vals.data, shape=data_subjects.shape
+                data=self.min_vals.data * num, shape=data_subjects.shape
             ),
             max_vals=lazyrepeatarray(
-                data=self.max_vals.data, shape=data_subjects.shape
+                data=self.max_vals.data * num, shape=data_subjects.shape
             ),
             client=self.client,
         )

packages/syft/src/syft/core/tensor/autodp/phi_tensor.py
@@ -803,8 +803,8 @@ class TensorWrappedPhiTensorPointer(Pointer, PassthroughTensor):
             args=[],
             kwargs={},
         )
-        dummy_res = np.ones(self.public_shape)
-        result.public_shape = dummy_res.shape
+
+        result.public_shape = data_subjects.shape
         result.public_dtype = self.public_dtype
 
         return result
@@ -875,8 +875,7 @@ class TensorWrappedPhiTensorPointer(Pointer, PassthroughTensor):
             args=[],
             kwargs={},
         )
-        dummy_res = np.ones(self.public_shape)
-        result.public_shape = dummy_res.shape
+        result.public_shape = data_subjects.shape
         result.public_dtype = self.public_dtype
 
         return result
@@ -2761,7 +2760,6 @@ class PhiTensor(PassthroughTensor, ADPTensor):
                 If a is 2-D, the sum along the diagonal is returned.
                 If a has larger dimensions, then an array of sums along diagonals is returned.
         """
-        print("WE ARE INSIDE PHI TENSOR")
         result = self.child.trace(offset, axis1, axis2)
 
         # This is potentially expensive