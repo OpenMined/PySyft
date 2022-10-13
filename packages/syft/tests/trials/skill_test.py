6ca239426f2549950b658b3feb12b73216b26d4a
osobky <omarelsobky97@gmail.com>
2022-10-13 08:58:21 +0200

    DONE My really cool commit message
    - Added my name to skill-test.py

--- packages/syft/tests/trials/skill_test.py
+++ packages/syft/tests/trials/skill_test.py
@@ -5,7 +5,14 @@ from typing import Dict
 
 def get_padawans(cohort: str) -> Dict[str, str]:
     # add yourself to the temple trial roster
-    data = {"R2Q4": {"skywalker": "PASSED", "saffron": "PASSED", "yash": "PASSED"}}
+    data = {
+        "R2Q4": {
+            "skywalker": "PASSED",
+            "saffron": "PASSED",
+            "yash": "PASSED",
+            "omar": "PASSED",
+        }
+    }
     return data[cohort]
 
 
@@ -13,3 +20,4 @@ def test_trial_of_skill() -> None:
     assert get_padawans("R2Q4")["skywalker"] == "PASSED"
     assert get_padawans("R2Q4")["saffron"] == "PASSED"
     assert get_padawans("R2Q4")["yash"] == "PASSED"
+    assert get_padawans("R2Q4")["omar"] == "PASSED"
