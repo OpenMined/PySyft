8d30b0190a1259619db6ba8f9a39fae75c4c6fa5
teo <teo.milea97@gmail.com>
2022-10-16 22:10:09 +0300

    added copy function for TensorPointer

--- packages/syft/src/syft/core/tensor/autodp/phi_tensor.py
+++ packages/syft/src/syft/core/tensor/autodp/phi_tensor.py
@@ -880,6 +880,74 @@ class TensorWrappedPhiTensorPointer(Pointer, PassthroughTensor):
 
         return result
 
+    def copy(self, *args: Any, **kwargs: Any) -> TensorWrappedPhiTensorPointer:
+        """
+        Return an array copy of the given object.
+
+        Parameters
+            order: {‘C’, ‘F’, ‘A’, ‘K’}, optional
+                Controls the memory layout of the copy.
+                ‘C’ means C-order, ‘F’ means F-order, 
+                ‘A’ means ‘F’ if a is Fortran contiguous, ‘C’ otherwise. 
+                ‘K’ means match the layout of a as closely as possible. 
+                (Note that this function and ndarray.copy are very similar, 
+                but have different default values for their order= arguments.)
+                
+        Returns
+            a_copy: PhiTensor
+                Array interpretation of a.
+        """
+        attr_path_and_name = "syft.core.tensor.tensor.Tensor.copy"
+        result: TensorWrappedPhiTensorPointer
+
+        result = TensorWrappedPhiTensorPointer(
+            data_subjects=self.data_subjects.copy(*args, **kwargs),
+            min_vals=self.min_vals.copy(*args, **kwargs),
+            max_vals=self.max_vals.copy(*args, **kwargs),
+            client=self.client,
+        )
+
+        # QUESTION can the id_at_location be None?
+        result_id_at_location = getattr(result, "id_at_location", None)
+
+        if result_id_at_location is not None:
+            # first downcast anything primitive which is not already PyPrimitive
+            (
+                downcast_args,
+                downcast_kwargs,
+            ) = lib.python.util.downcast_args_and_kwargs(args=args, kwargs=kwargs)
+
+            # then we convert anything which isnt a pointer into a pointer
+            pointer_args, pointer_kwargs = pointerize_args_and_kwargs(
+                args=downcast_args,
+                kwargs=downcast_kwargs,
+                client=self.client,
+                gc_enabled=False,
+            )
+
+            cmd = RunClassMethodAction(
+                path=attr_path_and_name,
+                _self=self,
+                args=pointer_args,
+                kwargs=pointer_kwargs,
+                id_at_location=result_id_at_location,
+                address=self.client.address,
+            )
+            self.client.send_immediate_msg_without_reply(msg=cmd)
+
+        inherit_tags(
+            attr_path_and_name=attr_path_and_name,
+            result=result,
+            self_obj=self,
+            args=[],
+            kwargs={},
+        )
+        result.public_shape = self.public_shape
+        result.public_dtype = self.public_dtype
+
+        return result
+
+
     def exp(
         self,
     ) -> Union[TensorWrappedPhiTensorPointer, MPCTensor]:
