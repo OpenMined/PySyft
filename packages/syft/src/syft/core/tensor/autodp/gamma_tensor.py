-Added relaxed xor bound in PhiTensor/GammaTensor - Working Tensorpointer for xor function - Adding xor to lazyrepeatarray calculation. - Added xor test in GammaTensor - Fixed XOR test in GammaTensor.

packages/syft/src/syft/core/tensor/autodp/gamma_tensor.py
@@ -839,6 +839,22 @@ class TensorWrappedGammaTensorPointer(Pointer, PassthroughTensor):
         """
         return self._apply_self_tensor_op("prod", *args, **kwargs)
 
+    def __xor__(
+        self,
+        other: Union[
+            TensorWrappedGammaTensorPointer, MPCTensor, int, float, np.ndarray
+        ],
+    ) -> Union[TensorWrappedGammaTensorPointer, MPCTensor]:
+        """Apply the "xor" operation between "self" and "other"
+
+        Args:
+            y (Union[TensorWrappedGammaTensorPointer,MPCTensor,int,float,np.ndarray]) : second operand.
+
+        Returns:
+            Union[TensorWrappedGammaTensorPointer,MPCTensor] : Result of the operation.
+        """
+        return TensorWrappedGammaTensorPointer._apply_op(self, other, "__xor__")
+
     def __pow__(
         self,
         *args: Any,
@@ -4076,28 +4092,38 @@ class GammaTensor:
 
         if is_acceptable_simple_type(other):
             sources["0"] = other
-            return GammaTensor(
-                child=(self.child ^ other) * 1,
-                data_subjects=self.data_subjects,
-                min_vals=lazyrepeatarray(data=0, shape=self.shape),
-                max_vals=lazyrepeatarray(data=1, shape=self.shape),
-                func_str=GAMMA_TENSOR_OP.XOR.value,
-                sources=sources,
-            )
+            child = self.child ^ other
+            data_subjects = self.data_subjects
+            if isinstance(other, np.ndarray):
+                other_min, other_max = other.min(), other.max()
+            else:
+                other_min, other_max = other, other
         elif isinstance(other, PhiTensor):
             return self ^ other.gamma
         elif isinstance(other, GammaTensor):
             sources[other.id] = other
-            return GammaTensor(
-                child=self.child ^ other.child,
-                data_subjects=self.data_subjects + other.data_subjects,
-                min_vals=lazyrepeatarray(data=0, shape=self.shape),
-                max_vals=lazyrepeatarray(data=1, shape=self.shape),
-                func_str=GAMMA_TENSOR_OP.XOR.value,
-                sources=sources,
-            )
+            child = self.child ^ other.child
+            data_subjects = self.data_subjects + other.data_subjects
+            other_min, other_max = other.min_vals.data, other.max_vals.data
         else:
             raise NotImplementedError(f"xor is not implemented for type: {type(other)}")
+
+        # TODO: should modify for a tighter found for xor
+        _max = int(max(self.max_vals.data, other_max))
+        _min = int(min(self.min_vals.data, other_min))
+        _max_vals = max(
+            (2 ** (_min ^ _max).bit_length()) - 1, (2 ** (_max).bit_length()) - 1
+        )
+        _min_vals = min(0, _min)
+
+        return GammaTensor(
+            child=child,
+            data_subjects=data_subjects,
+            min_vals=lazyrepeatarray(data=_min_vals, shape=self.shape),
+            max_vals=lazyrepeatarray(data=_max_vals, shape=self.shape),
+            func_str=GAMMA_TENSOR_OP.XOR.value,
+            sources=sources,
+        )
 
     def __round__(self, n: int = 0) -> GammaTensor:
         sources = dict()

packages/syft/src/syft/core/tensor/autodp/phi_tensor.py
@@ -2804,28 +2804,38 @@ class PhiTensor(PassthroughTensor, ADPTensor):
 
     def __xor__(self, other: Any) -> Union[PhiTensor, GammaTensor]:
         if is_acceptable_simple_type(other):
-            return PhiTensor(
-                child=(self.child ^ other),
-                data_subjects=self.data_subjects,
-                min_vals=lazyrepeatarray(data=0, shape=self.shape),
-                max_vals=lazyrepeatarray(data=1, shape=self.shape),
-            )
+            if isinstance(other, np.ndarray):
+                other_min, other_max = other.min(), other.max()
+            else:
+                other_min, other_max = other, other
+            child = self.child ^ other
         elif isinstance(other, GammaTensor):
             return self.gamma ^ other
         elif isinstance(other, PhiTensor):
-            if self.data_subjects.sum() != other.data_subjects.sum():
+            if (self.data_subjects != other.data_subjects).any():
                 return self.gamma ^ other.gamma
             else:
-                return PhiTensor(
-                    child=(self.child ^ other.child),
-                    data_subjects=self.data_subjects + other.data_subjects,
-                    min_vals=lazyrepeatarray(data=0, shape=self.shape),
-                    max_vals=lazyrepeatarray(data=1, shape=self.shape),
-                )
+                child = self.child ^ other.child
+                other_min, other_max = other.min_vals.data, other.max_vals.data
         else:
             raise NotImplementedError(
                 f"__xor__ not implemented between PhiTensor and {type(other)}."
             )
+
+        # TODO: should modify for a tighter found for xor
+        _max = int(max(self.max_vals.data, other_max))
+        _min = int(min(self.min_vals.data, other_min))
+        _max_vals = max(
+            (2 ** (_min ^ _max).bit_length()) - 1, (2 ** (_max).bit_length()) - 1
+        )
+        _min_vals = min(0, _min)
+
+        return PhiTensor(
+            child=child,
+            data_subjects=self.data_subjects,
+            min_vals=lazyrepeatarray(data=_min_vals, shape=self.shape),
+            max_vals=lazyrepeatarray(data=_max_vals, shape=self.shape),
+        )
 
     def searchsorted(self, v: Any) -> Union[PhiTensor, GammaTensor]:
         """

packages/syft/src/syft/core/tensor/lazy_repeat_array.py
@@ -473,7 +473,6 @@ def compute_min_max(
         "__ge__",
         "__eq__",
         "__ne__",
-        "__xor__",
     ]:
         min_vals = x_min_vals * 0
         max_vals = (x_max_vals * 0) + 1
@@ -600,7 +599,7 @@ def compute_min_max(
             other_min = other.min_vals.data  # type: ignore
         else:
             raise NotImplementedError(
-                f"__lshift__ not implemented between PhiTensor and {type(other)}."
+                f"{op_str} not implemented in LazyRepeatArray for {type(other)}."
             )
 
         min_min = x_min_vals.data << other_min
@@ -610,6 +609,29 @@ def compute_min_max(
 
         _min_vals = np.min([min_min, min_max, max_min, max_max], axis=0)  # type: ignore
         _max_vals = np.max([min_min, min_max, max_min, max_max], axis=0)  # type: ignore
+
+        min_vals = lazyrepeatarray(data=_min_vals, shape=x_min_vals.shape)
+        max_vals = lazyrepeatarray(data=_max_vals, shape=x_max_vals.shape)
+
+    elif op_str == "__xor__":
+        if is_acceptable_simple_type(other):
+            if isinstance(other, np.ndarray):
+                other_max, other_min = other.max(), other.min()
+            else:
+                other_max, other_min = other, other
+        elif hasattr(other, "min_vals") and hasattr(other, "max_vals"):
+            other_max = other.max_vals.data  # type: ignore
+            other_min = other.min_vals.data  # type: ignore
+        else:
+            raise NotImplementedError(
+                f"{op_str} not implemented in LazyRepeatArray for {type(other)}."
+            )
+
+        # TODO: should modify for a tighter found for xor
+        _max = int(max(x_max_vals.data, other_max))
+        _min = int(min(x_min_vals.data, other_min))
+        _max_vals = (2 ** (_min ^ _max).bit_length()) - 1
+        _min_vals = min(0, _min)
 
         min_vals = lazyrepeatarray(data=_min_vals, shape=x_min_vals.shape)
         max_vals = lazyrepeatarray(data=_max_vals, shape=x_max_vals.shape)

packages/syft/tests/syft/core/tensor/adp/phi_tensor_test.py
@@ -1857,47 +1857,47 @@ def test_matmul(
 
 
 def test_xor(
-    reference_binary_data: np.ndarray,
+    reference_data: np.ndarray,
     upper_bound: np.ndarray,
     lower_bound: np.ndarray,
     ishan: DataSubjectArray,
 ) -> None:
-    ishan = np.broadcast_to(ishan, reference_binary_data.shape)
+    ishan = np.broadcast_to(ishan, reference_data.shape)
     reference_tensor = PT(
-        child=np.array([reference_binary_data]),
+        child=np.array([reference_data]),
         data_subjects=np.array([ishan]),
         max_vals=upper_bound,
         min_vals=lower_bound,
     )
-    other = np.ones_like(reference_binary_data)
+    other = np.ones_like(reference_data)
     result = reference_tensor ^ other
 
-    assert (result.child == (reference_binary_data ^ other)).all()
-    assert (result.child.max() <= 1).all()
-    assert (result.child.max() >= 0).all()
+    assert (result.child == (reference_data ^ other)).all()
+    assert (result.child.max() <= result.max_vals.data).all()
+    assert (result.child.max() >= result.min_vals.data).all()
 
-    other = np.zeros_like(reference_binary_data)
+    other = np.zeros_like(reference_data)
     result = reference_tensor ^ other
-    assert (result.child == (reference_binary_data ^ other)).all()
-    assert (result.child.max() <= 1).all()
-    assert (result.child.max() >= 0).all()
+    assert (result.child == (reference_data ^ other)).all()
+    assert (result.child.max() <= result.max_vals.data).all()
+    assert (result.child.max() >= result.min_vals.data).all()
 
     result = reference_tensor ^ reference_tensor
-    assert (result.child == (reference_binary_data ^ reference_binary_data)).all()
-    assert (result.child.max() <= 1).all()
-    assert (result.child.max() >= 0).all()
+    assert (result.child == (reference_data ^ reference_data)).all()
+    assert (result.child.max() <= result.max_vals.data).all()
+    assert (result.child.max() >= result.min_vals.data).all()
 
     other = PT(
-        child=reference_binary_data,
-        data_subjects=DataSubjectArray.from_objs(np.ones_like(reference_binary_data)),
+        child=reference_data,
+        data_subjects=DataSubjectArray.from_objs(np.ones_like(reference_data)),
         min_vals=lower_bound,
         max_vals=upper_bound,
     )
 
     result = reference_tensor ^ other
-    assert (result.child == (reference_binary_data ^ other)).all()
-    assert (result.child.max() <= 1).all()
-    assert (result.child.max() >= 0).all()
+    assert (result.child == (reference_data ^ other.child)).all()
+    assert (result.child.max() <= result.max_vals.data).all()
+    assert (result.child.max() >= result.min_vals.data).all()
 
 
 def test_lshift(