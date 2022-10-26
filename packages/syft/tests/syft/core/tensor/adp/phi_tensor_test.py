804b5913c412f828f7c080653f67849b79655ee2
IshanMi <mishra.ishan@gmail.com>
2022-10-26 14:22:23 -0400

    Fix or tests to account for bitwise behaviour

--- packages/syft/tests/syft/core/tensor/adp/phi_tensor_test.py
+++ packages/syft/tests/syft/core/tensor/adp/phi_tensor_test.py
@@ -1413,8 +1413,12 @@ def test_or_single_data_subjects(
     # TODO: As we currently convert all operations to gamma tensor,
     # so we include gammatensor for the assert, it should be reverted back to PhiTensor
     assert isinstance(result, (PT, GammaTensor)), "Or of two PTs is wrong type"
-    assert result.max_vals.data == 1, "Or of two PTs results in incorrect max_vals"
-    assert result.min_vals.data == 0, "Or of two PTs results in incorrect min_vals"
+    assert (
+        result.child.max() <= result.max_vals.data
+    ), "Or of two PTs results in incorrect max_vals"
+    assert (
+        result.child.min() >= result.min_vals.data
+    ), "Or of two PTs results in incorrect min_vals"
 
 
 @pytest.mark.arithmetic
@@ -1436,18 +1440,18 @@ def test_or_public(
     output = reference_tensor | 5
     assert output.shape == reference_tensor.shape
     assert (output.child == reference_data | 5).all()
-    assert output.min_vals.data == 0
+    assert output.child.min() >= output.min_vals.data
     assert output.min_vals.shape == reference_tensor.shape
-    assert output.max_vals.data == 1
+    assert output.child.max() <= output.max_vals.data
     assert output.max_vals.shape == reference_tensor.shape
     assert (output.data_subjects == reference_tensor.data_subjects).all()
 
     output = reference_tensor | -5
     assert output.shape == reference_tensor.shape
     assert (output.child == reference_data | -5).all()
-    assert output.min_vals.data == 0
+    assert output.child.min() >= output.min_vals.data
     assert output.min_vals.shape == reference_tensor.shape
-    assert output.max_vals.data == 1
+    assert output.child.max() <= output.max_vals.data
     assert output.max_vals.shape == reference_tensor.shape
     assert (output.data_subjects == reference_tensor.data_subjects).all()
 
@@ -1482,9 +1486,9 @@ def test_or_private(
     output = reference_tensor | tensor2
     assert output.shape == reference_tensor.shape
     assert (output.child == reference_data | new_reference_data).all()
-    assert output.min_vals.data == 0
+    assert output.child.min() >= output.min_vals.data
     assert output.min_vals.shape == reference_tensor.shape
-    assert output.max_vals.data == 1
+    assert output.child.max() <= output.max_vals.data
     assert output.max_vals.shape == reference_tensor.shape
     assert (output.data_subjects == reference_tensor.data_subjects).all()
 
