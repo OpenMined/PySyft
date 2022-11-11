6a5fd0d7aecf85732808b35503a3aa0ad989086b
teo <teo.milea97@gmail.com>
2022-11-11 13:11:22 +0200

    added FULL contact information

--- packages/syft/src/syft/core/adp/vectorized_publish.py
+++ packages/syft/src/syft/core/adp/vectorized_publish.py
@@ -162,6 +162,10 @@ def publish(
                 raise Exception(
                     "NaN or Inf values in bounds not allowed in PySyft for safety reasons."
                     "Please contact the OpenMined support team for help."
+                    "For that you can either:"
+                    " * describe your issue on our Slack #support channel. To join: https://openmined.slack.com/"
+                    " * send us an email describing your problem at support@openmined.org"
+                    " * leave us an issue here: https://github.com/OpenMined/PySyft/issues"
                 )
             rdp_constants = compute_rdp_constant(rdp_params, private=False)
         else:
@@ -174,6 +178,10 @@ def publish(
             raise Exception(
                 "Negative budget spend not allowed in PySyft for safety reasons."
                 "Please contact the OpenMined support team for help."
+                "For that you can either:"
+                " * describe your issue on our Slack #support channel. To join: https://openmined.slack.com/"
+                " * send us an email describing your problem at support@openmined.org"
+                " * leave us an issue here: https://github.com/OpenMined/PySyft/issues"
             )
 
         epsilon_spend = max(
@@ -187,6 +195,10 @@ def publish(
             raise Exception(
                 "Negative budget spend not allowed in PySyft for safety reasons."
                 "Please contact the OpenMined support team for help."
+                "For that you can either:"
+                " * describe your issue on our Slack #support channel. To join: https://openmined.slack.com/"
+                " * send us an email describing your problem at support@openmined.org"
+                " * leave us an issue here: https://github.com/OpenMined/PySyft/issues"
             )
 
         # Step 3: Check if the user has enough privacy budget for this query
