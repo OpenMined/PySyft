9601f2dc23b77be5b5e68494014c5ff6c80ef0a6
Dylan Hrebenach <dhrebenach@fubo.tv>
2022-11-12 13:07:15 -0800

    fix: remove gitignore for svelte UI

--- packages/grid/svelte/src/routes/__layout.svelte
+++ packages/grid/svelte/src/routes/__layout.svelte
@@ -1,6 +1,6 @@
 <script>
   import '../app.css';
-  import NavBar from '$lib/components/NavBar.svelte';
+  import NavBar from '$lib/components/Navigation/NavBar.svelte';
 </script>
 
 <div class="flex h-auto min-h-screen">
