394cb403876085496e991a9c7ec2219f1f28b74f
Dylan Hrebenach <dhrebenach@fubo.tv>
2022-11-12 12:43:15 -0800

    chore: add work from account settings PR

--- packages/grid/svelte/src/routes/__layout.svelte
+++ packages/grid/svelte/src/routes/__layout.svelte
@@ -1,5 +1,9 @@
 <script>
   import '../app.css';
+  import NavBar from '$lib/components/NavBar.svelte';
 </script>
 
-<slot />
+<div class="flex h-auto min-h-screen">
+  <NavBar />
+  <slot />
+</div>
