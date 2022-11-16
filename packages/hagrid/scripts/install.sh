c5a69645532a8248fd55c2047186c4edfc64ff82
Madhava Jay <me@madhavajay.com>
2022-11-16 16:30:21 +1000

    Updated the 1 liner hagrid install
    - briefly tested on macOS

--- packages/hagrid/scripts/install.sh
+++ packages/hagrid/scripts/install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# run with:
+# curl https://raw.githubusercontent.com/OpenMined/PySyft/dev/packages/hagrid/scripts/install.sh | sh
+
 set -e
 
 cat /dev/null <<EOF
@@ -11,8 +14,14 @@ but credit (and pull requests) appreciated.
 ------------------------------------------------------------------------
 EOF
 
+OS_NOT_SUPPORTED="Install script currently only supports macOS & Ubuntu"
+
 is_command() {
-  command -v "$1" >/dev/null
+    set +e
+    command -v "$1" >/dev/null
+    RETURN_CODE=$?
+    set +e
+    return $RETURN_CODE
 }
 
 check_ubuntu() {
@@ -21,120 +30,203 @@ check_ubuntu() {
         . /etc/os-release
         if [ "$ID" = "ubuntu" ]
         then
-            echo " 🐧 $PRETTY_NAME detected"
-        else
-            echo "Install script currently only supports Ubuntu"
-            exit 1
+            return 0
         fi
-    else
-        echo "Install script currently only supports MacOS & Ubuntu"
-        exit 1
     fi
+    return 1
 }
+
 check_macos() {
     if [ "$(uname)" = "Darwin" ]
     then
-        echo "🖥  MacOs $(uname -mrs) detected"
-    else
-        echo "Install script currently only supports MacOS"
-        exit 1
+        return 0
     fi
+    return 1
 }
 
-check_macos_ubuntu() {
-    echo "Checking OS ..."
-    if is_command brew
+check_os_supported() {
+    echo "Checking OS..."
+    if check_macos
     then
-        check_macos
-    elif is_command apt-get
+        echo "✅ macOS detected"
+        return 0
+    elif check_ubuntu
     then
-        check_ubuntu
-    else
-        echo "Install script currently only supports MacOS & Ubuntu, also check if brew or apt-get is installed"
-        exit 1
+        echo "✅ Ubuntu detected"
+        return 0
     fi
+    echo $OS_NOT_SUPPORTED
+    exit 1
 }
 
 apt_install() {
-    sudo apt-get -qq -o=Dpkg::Use-Pty=0 update -y > /dev/null
-    sudo apt-get -qq -o=Dpkg::Use-Pty=0 install $1 -y > /dev/null
+    execute_sudo "apt-get -qq -o=Dpkg::Use-Pty=0 update -y"
+    execute_sudo "apt-get -qq -o=Dpkg::Use-Pty=0 install $1 -y"
 }
 
 brew_install() {
-    brew install $1 > /dev/null
+    if is_command "brew"
+    then
+        # echo "Would run: brew install $1"
+        HOMEBREW_NO_AUTO_UPDATE=1 brew install $1
+    else
+        echo "\nWe require brew to install packages.\nYou must install brew first: https://brew.sh/"
+        exit 1
+    fi
 }
 
 hagrid_install() {
-    pip install --quiet hagrid ansible ansible-core
+    echo "\nChecking hagrid ..."
+    if is_command "hagrid"
+    then
+        echo "✅ hagrid detected"
+    else
+        echo "Installing hagrid"
+        pip install --quiet -U hagrid
+    fi
+}
+
+# from: https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+unset HAVE_SUDO_ACCESS # unset this from the environment
+
+abort() {
+    printf "%s\n" "$@" >&2
+    exit 1
+}
+
+
+have_sudo_access() {
+    if [[ ! -x "/usr/bin/sudo" ]]
+    then
+        return 1
+    fi
+
+    local -a SUDO=("/usr/bin/sudo")
+    if [[ -n "${SUDO_ASKPASS-}" ]]
+    then
+        SUDO+=("-A")
+    elif [[ -n "${NONINTERACTIVE-}" ]]
+    then
+        SUDO+=("-n")
+    fi
+
+    if [[ -z "${HAVE_SUDO_ACCESS-}" ]]
+    then
+        if [[ -n "${NONINTERACTIVE-}" ]]
+        then
+            "${SUDO[@]}" -l mkdir &>/dev/null
+        else
+            "${SUDO[@]}" -v && "${SUDO[@]}" -l mkdir &>/dev/null
+        fi
+        HAVE_SUDO_ACCESS="$?"
+    fi
+
+    if [[ -n "${HOMEBREW_ON_MACOS-}" ]] && [[ "${HAVE_SUDO_ACCESS}" -ne 0 ]]
+    then
+        abort "Need sudo access on macOS (e.g. the user ${USER} needs to be an Administrator)!"
+    fi
+
+    return "${HAVE_SUDO_ACCESS}"
+}
+
+execute() {
+    if ! "$@"
+    then
+        abort "$(printf "Failed during: %s" "$(shell_join "$@")")"
+    fi
+}
+
+execute_sudo() {
+    local -a args=("$@")
+    if have_sudo_access
+    then
+        if [[ -n "${SUDO_ASKPASS-}" ]]
+        then
+            args=("-A" "${args[@]}")
+        fi
+        echo "/usr/bin/sudo" "${args[@]}"
+        execute "/usr/bin/sudo" "${args[@]}"
+    else
+        echo "${args[@]}"
+        execute "${args[@]}"
+    fi
 }
 
 check_and_install() {
-    echo "Checking $1 ..."
-    is_command $1
-    BINARY_EXISTS=$?
-    if [ "$BINARY_EXISTS" != "0" ]
+    echo "\nChecking $1 ..."
+    if is_command $1
     then
+        echo "✅ $1 detected"
+        return 0
+    else
         echo "Installing missing dependency $2"
-        echo "Checking Sudo"
-        sudo -n true
-        HAS_SUDO=$?
-        if [ "$HAS_SUDO" != "0" ]
-        then
-            echo "Installing missing dependency requires sudo"
-            exit 1
-        fi
         if check_macos
         then
             brew_install $2
-            . ~/.profile
         elif check_ubuntu
         then
             apt_install $2
-            . ~/.profile
-        fi        
+        fi
+    fi
+
+    if is_command $1
+    then
+        echo "✅ $1 detected"
+        return 0
+    else
+        echo "Failed to install $1. Please manually install it."
     fi
 }
 
-# spinner="
-# 🧙🏿‍♀️oo
-# o🧙‍♂️o
-# oo🧙🏽
-# "
+check_install_python() {
+    check_and_install python3 python3
+}
 
-# spin() {
-#     while [ 1 ]
-#     do
-#         for i in $spinner do
-#         do
-#             clear && printf "\rHAGrid is installing:\n$i"
-#             sleep 0.2
-#         done
-#     done
-# }
+check_install_pip() {
+    echo "\nChecking pip ..."
+    if is_command "pip"
+    then
+        echo "✅ pip detected"
+        return 0
+    else
+        if check_macos
+        then
+            echo "Installing missing dependency pip"
+            python3 -m ensurepip
+        else
+            check_and_install pip python3-pip
+        fi
+    fi
+
+    if is_command "pip"
+    then
+        echo "✅ pip detected"
+    else
+        echo "Failed to install pip. Please manually install it."
+    fi
+}
+
+check_install_git() {
+    check_and_install git git
+}
 
 execute() {
-    check_macos_ubuntu
-    # spin &
-    pid=$!
-    set +e
-    check_and_install python3 python3
-    check_and_install pip python3-pip
-    check_and_install git git
-    . ~/.profile
+    check_os_supported
+    check_install_python
+    check_install_pip
+    check_install_git
+
     hagrid_install
-    hagrid >/dev/null
-    #kill $pid
-    #clear
-    is_command hagrid
-    BINARY_EXISTS=$?
-    set -e
-    if [ "$BINARY_EXISTS" != "0" ]
+
+    if is_command "hagrid"
     then
-        echo "HAGrid failed to install. Please try again"
+        echo "\n🧙‍♂️ HAGrid is installed!\n"
+        echo "To get started run: \n$ hagrid quickstart\n"
+    else
+        echo "\nHAGrid failed to install. Please try manually with:"
+        echo "pip install -U hagrid"
         exit 1
     fi
-    echo "HAGrid is installed! 🧙‍♂️"
-    exec $SHELL
 }
 
 execute
