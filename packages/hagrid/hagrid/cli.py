131e17d744b1c631d36baf24f8025d1e8941afef
Bipin Krishanan P <bipinkrishna.p@gmail.com>
2022-11-18 14:47:40 +0000

    replace ctop explanation hyperlink with raw url

--- packages/hagrid/hagrid/cli.py
+++ packages/hagrid/hagrid/cli.py
@@ -2999,7 +2999,7 @@ def check_status(
         print("You can view your container logs using the following tool:")
         print("Tool: [link=https://ctop.sh]Ctop[/link]")
         print(
-            "Video Explanation: [link=https://youtu.be/BJhlCxerQP4]How to use Ctop[/link]\n"
+            "Video Explanation: https://youtu.be/BJhlCxerQP4 \n"
         )
 
 
