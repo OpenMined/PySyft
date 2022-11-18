9912a15badfdc47491d878daec8e9c71efc8ae96
teo <teo.milea97@gmail.com>
2022-11-18 16:48:59 +0200

    commented all references to garbage collection

--- packages/syft/src/syft/core/pointer/garbage_collection/gc_strategy.py
+++ packages/syft/src/syft/core/pointer/garbage_collection/gc_strategy.py
@@ -1,54 +1,54 @@
-"""The strategy that all GC Heuristics should implement."""
-# stdlib
-from abc import ABC
-from abc import abstractmethod
-from typing import Dict
-from typing import Type
+# """The strategy that all GC Heuristics should implement."""
+# # stdlib
+# from abc import ABC
+# from abc import abstractmethod
+# from typing import Dict
+# from typing import Type
 
-# relative
-from ..pointer import Pointer
+# # relative
+# from ..pointer import Pointer
 
 
-class GCStrategy(ABC):
-    """The Strategy that all GC Heuristics should inherit."""
+# class GCStrategy(ABC):
+#     """The Strategy that all GC Heuristics should inherit."""
 
-    REGISTERED_GC_STRATEGIES: Dict[str, Type["GCStrategy"]] = {}
+#     REGISTERED_GC_STRATEGIES: Dict[str, Type["GCStrategy"]] = {}
 
-    @staticmethod
-    def _register(cls: Type["GCStrategy"]) -> None:
-        """Add a concrete implementation for the GC Strategy to a dictionary
-        of known strategies.
+#     @staticmethod
+#     def _register(cls: Type["GCStrategy"]) -> None:
+#         """Add a concrete implementation for the GC Strategy to a dictionary
+#         of known strategies.
 
-        This is used to have a simpler way when we initialize the GarbageCollection
-        class.
+#         This is used to have a simpler way when we initialize the GarbageCollection
+#         class.
 
-        Args:
-            cls (Type[GCStrategy]): The subclass that inherits from GCStrategy
+#         Args:
+#             cls (Type[GCStrategy]): The subclass that inherits from GCStrategy
 
-        Return:
-            None
-        """
-        gc_strategy_name = cls.__name__.lower()
-        if gc_strategy_name in GCStrategy.REGISTERED_GC_STRATEGIES:
-            raise ValueError(f"{gc_strategy_name} already registered!")
+#         Return:
+#             None
+#         """
+#         gc_strategy_name = cls.__name__.lower()
+#         if gc_strategy_name in GCStrategy.REGISTERED_GC_STRATEGIES:
+#             raise ValueError(f"{gc_strategy_name} already registered!")
 
-        GCStrategy.REGISTERED_GC_STRATEGIES[gc_strategy_name] = cls
+#         GCStrategy.REGISTERED_GC_STRATEGIES[gc_strategy_name] = cls
 
-    def __init_subclass__(cls: Type["GCStrategy"]) -> None:
-        """Called when a class inherits the GCStrategy.
+#     def __init_subclass__(cls: Type["GCStrategy"]) -> None:
+#         """Called when a class inherits the GCStrategy.
 
-        Args:
-            cls (Type[GCStrategy]): The subclass that inherits from GCStrategy
+#         Args:
+#             cls (Type[GCStrategy]): The subclass that inherits from GCStrategy
 
-        Return:
-            None
-        """
-        super().__init_subclass__()
-        GCStrategy._register(cls)
+#         Return:
+#             None
+#         """
+#         super().__init_subclass__()
+#         GCStrategy._register(cls)
 
-    @abstractmethod
-    def reap(self, pointer: Pointer) -> None:
-        """What happens when a Ponter is deleted.
-        It should be implemented in the GCStrategy that extends this class
-        """
-        pass
+#     @abstractmethod
+#     def reap(self, pointer: Pointer) -> None:
+#         """What happens when a Ponter is deleted.
+#         It should be implemented in the GCStrategy that extends this class
+#         """
+#         pass
