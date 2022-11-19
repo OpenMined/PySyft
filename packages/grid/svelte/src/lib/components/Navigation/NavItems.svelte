de21fb88954b4a3a41835b6a4e80cf2243d745b7
Dylan Hrebenach <dhrebenach@fubo.tv>
2022-11-19 10:45:46 -0800

    chore: add untracked files

---
+++ packages/grid/svelte/src/lib/components/Navigation/NavItems.svelte
@@ -0,0 +1,8 @@
+<script context="module">
+  // these are our 'pages' to navigate through (in order for this export to work the script tag MUST include context="module")
+  export const navItems = [
+    { page: 'Login', slug: '/Login' },
+    { page: 'Signup', component: '/Signup' }
+    // other navigation pages can go here
+  ];
+</script>
