bc108bce79c672ac08450a9c61f6d56de8f89707
Ionesio Junior <ionesiojr@gmail.com>
2022-11-30 18:33:31 -0300

    Fix lint issues

--- packages/grid/frontend/network_simple_server.py
+++ packages/grid/frontend/network_simple_server.py
@@ -1,15 +1,20 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+# stdlib
+from http.server import BaseHTTPRequestHandler
+from http.server import HTTPServer
+
 
 class CustomHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
+    def do_GET(self) -> None:
         self.send_response(200)
-        self.send_header('content-type','text/html')
+        self.send_header("content-type", "text/html")
         self.end_headers()
-        self.wfile.write('Network Node running.'.encode())
+        self.wfile.write("Network Node running.".encode())
 
-def main():
-    srv = HTTPServer(('',80), CustomHandler)
+
+def main() -> None:
+    srv = HTTPServer(("", 80), CustomHandler)
     srv.serve_forever()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- packages/hagrid/hagrid/cli.py
+++ packages/hagrid/hagrid/cli.py
@@ -1746,7 +1746,9 @@ def create_launch_docker_cmd(
         "VERSION": version_string,
         "VERSION_HASH": version_hash,
         "USE_BLOB_STORAGE": use_blob_storage,
-        "NETWORK_FRONTEND": "network_frontend" if str(node_type.input) == "network" else "",
+        "NETWORK_FRONTEND": "network_frontend"
+        if str(node_type.input) == "network"
+        else "",
         "STACK_API_KEY": str(
             generate_sec_random_password(length=48, special_chars=False)
         ),
