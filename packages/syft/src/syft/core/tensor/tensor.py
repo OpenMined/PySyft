ebf4a30a0bf1918343b382d6d93606319251af3b
Shubham Gupta <shubhamgupta3121@gmail.com>
2022-12-06 13:00:40 +0530

    fix public_shape overridden by public_dtype of the result

--- packages/syft/src/syft/core/tensor/tensor.py
+++ packages/syft/src/syft/core/tensor/tensor.py
@@ -192,9 +192,7 @@ class TensorPointer(Pointer):
                 )
 
         # calculate the dtype of the result based on the op_str
-        result_public_dtype = result_public_shape = utils.get_dtype(
-            op_str, self.public_shape, other_shape
-        )
+        result_public_dtype = utils.get_dtype(op_str, self.public_shape, other_shape)
 
         result.public_shape = result_public_shape
         result.public_dtype = result_public_dtype
