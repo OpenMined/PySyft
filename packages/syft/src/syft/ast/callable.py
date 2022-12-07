a97fad413be57f85bf598a007e20dea717d919d6
rasswanth <43314053+rasswanth-s@users.noreply.github.com>
2022-12-07 11:02:13 +0530

    added syft ast and registry files

--- packages/syft/src/syft/ast/callable.py
+++ packages/syft/src/syft/ast/callable.py
@@ -7,7 +7,6 @@ from typing import Any
 from typing import Callable as CallableT
 from typing import List
 from typing import Optional
-from typing import Tuple
 from typing import Union
 
 # relative
@@ -56,7 +55,7 @@ class Callable(ast.attribute.Attribute):
 
     def __call__(
         self,
-        *args: Tuple[Any, ...],
+        *args: Any,
         **kwargs: Any,
     ) -> Optional[Union["Callable", CallableT]]:
         """The `__call__` method on a `Callable` has two possible roles, e.g.
@@ -115,6 +114,8 @@ class Callable(ast.attribute.Attribute):
                     args=args,
                     kwargs=kwargs,
                 )
+
+                self.client.processing_pointers[ptr.id_at_location] = True  # type: ignore
                 return ptr
 
         if "path" not in kwargs or "index" not in kwargs:
