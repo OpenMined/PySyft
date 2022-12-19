e879017e0449931e3ea5fbe2a61b77da83763309
Madhava Jay <me@madhavajay.com>
2022-12-19 18:56:44 +1000

    committing more crappy conversational pseudo code

--- packages/syft/src/syft/core/node/new/service_store.py
+++ packages/syft/src/syft/core/node/new/service_store.py
@@ -228,3 +228,106 @@ class ServiceStore:
 # class Pointer(ActionStoreView):
 #     public_shape: shape
 #     ...
+
+
+# class CRUD:
+#     def update_or_inset()
+
+
+# class Service:
+#     @service(permission=Union[can_create_users)
+#     def create_user(user: UserType) -> PublicView
+
+
+# def safe_readcsv(path) -> pd.DataFrame:
+#     return pd.read_csv(santize_path(path))
+
+
+# def construct_api(credential: SyftCredential) -> SyftAPI:
+#     if user.role
+#     API = {
+#         sevices: {
+#             user: {
+#                 create: function(args: kwargs) -> Result[Ok, Err]
+#                 delete: function(args: kwargs)
+#             },
+#             action: {
+#                 ast: {
+#                     pandas: {
+#                         read_csv: safe_readcsv
+#                     }
+#                     torch: {
+
+#                     }
+#                     numpy: {}
+#                 }
+#                 store: {
+#                     get_pointer: function()
+#                 }
+#             }
+#             codejail: {
+#                 __andsfkjsadfjk__my_func: get_func_from_code_jail()
+#             }
+#         }
+#     }
+
+#     return API
+
+
+# def get_func_from_code_jail(uid: UID):
+#     allowed_users = codejail[uid].permissions
+
+#     @permission(allowed_users)
+#     def execute_custom_function:
+#         return codejail[uid]
+#     return execute_custom_function
+
+# def __andsfkjsadfjk__my_func()
+
+
+# class Pointer(ActionStoreView):
+#     public_shape: shape
+#     ...
+
+# import syft as sy
+
+
+# -> functional
+# -> method
+
+# client.service.action.create()
+
+# client.service.user.create()
+
+# user = client.service.user.get_uiser()
+
+# user.delete()
+
+
+# class User:
+
+#     @service(permission=[GUEST, can_do_Stuff])
+#     def delete(self: Union[UID, User, UserView]) -> Result[bool, err]:
+
+
+# python client
+
+# type(client.service.user) == class UserAPI
+
+
+# class PublicAPI:
+#     @staticmethod
+#     def make_public_api_class(cls, class_)
+#         inspect.signature(foo)
+#         method = Dict[str, dict[]] = {
+#             "create": {
+#                 "args": [int, float]
+#                 "kwargs": ["user_id": UID]
+#             }
+#         }
+
+# >>> import inspect
+# >>> print(inspect.signature(foo))
+# >>> def foo(a: int, b: float, x='blah'):
+# ...     pass
+# >>> print(inspect.signature(foo))
