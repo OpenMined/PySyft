f27841ef7ff19e6a364378e3c981fe4687e3764f
IshanMi <mishra.ishan@gmail.com>
2022-12-20 12:24:37 -0500

    Add PhiTensor publishing test

--- packages/syft/tests/syft/core/adp/publish_api_test.py
+++ packages/syft/tests/syft/core/adp/publish_api_test.py
@@ -3,8 +3,11 @@ from typing import Any
 
 # third party
 import numpy as np
+from jax.numpy import DeviceArray
 
 # syft absolute
+import pytest
+
 import syft as sy
 from syft.core.adp.data_subject_ledger import DataSubjectLedger
 from syft.core.adp.data_subject_ledger import convert_constants_to_indices
@@ -29,6 +32,11 @@ def deduct_epsilon_for_user(
 ) -> None:
     user_budget.budget = old_budget - epsilon_spend
     user_budget.current_spend = epsilon_spend
+
+
+@pytest.fixture
+def dataset() -> np.ndarray:
+    return np.random.randint(low=1, high=8, size=(5, 5))
 
 
 def test_privacy_budget_spend_on_publish():
@@ -118,12 +126,98 @@ def test_privacy_budget_spend_on_publish():
     assert pub_result_comb4 is not None
 
 
+def test_publish_phi_tensor(dataset: np.ndarray) -> None:
+    """ Test that you can still publish PhiTensors"""
+    tensor = sy.Tensor(dataset).annotate_with_dp_metadata(lower_bound=0, upper_bound=10, data_subject="Mr. Potato")
+    ledger_store = DictLedgerStore()
+    user_key = b"6874"
+    ledger = DataSubjectLedger.get_or_create(store=ledger_store, user_key=user_key)
+    result = tensor.publish(
+        get_budget_for_user=get_budget_for_user,
+        deduct_epsilon_for_user=deduct_epsilon_for_user,
+        ledger=ledger,
+        sigma=2,
+        private=True,
+    )
+    assert result is not None
+    assert isinstance(result, (np.ndarray, DeviceArray))
+    assert result.shape == dataset.shape
+    print(ledger._rdp_constants)
+    assert ledger._rdp_constants is not None
+    assert user_budget.current_spend > 0
+
+
 def test_publish_new_subjects() -> None:
-    """Test that publishing works when data of new data subjects are published, i.e. ledger expands, PB unchanged if
-    new data subjects have the same epsilon, etc"""
+    """Test that the ledger updates correctly when data of new data subjects are published"""
+    fred_nums = np.array([25, 35, 21])
+    sally_nums = np.array([8, 11, 10])
+
+    fred_tensor = sy.Tensor(fred_nums).annotate_with_dp_metadata(
+        lower_bound=0, upper_bound=122, data_subject="fred"
+    )
+
+    sally_tensor = sy.Tensor(sally_nums).annotate_with_dp_metadata(
+        lower_bound=0, upper_bound=122, data_subject="sally"
+    )
+
+    result = fred_tensor + sally_tensor
+
+    ledger_store = DictLedgerStore()
+
+    user_key = b"1231"
+
+    ledger = DataSubjectLedger.get_or_create(store=ledger_store, user_key=user_key)
+
+    pub_result_sally = sally_tensor.publish(
+        get_budget_for_user=get_budget_for_user,
+        deduct_epsilon_for_user=deduct_epsilon_for_user,
+        ledger=ledger,
+        sigma=50,
+        private=True,
+    )
+
+    assert pub_result_sally is not None
+
+    eps_spend_for_sally = user_budget.current_spend
+
+    pub_result_fred = sally_tensor.publish(
+        get_budget_for_user=get_budget_for_user,
+        deduct_epsilon_for_user=deduct_epsilon_for_user,
+        ledger=ledger,
+        sigma=50,
+        private=True,
+    )
+
+    assert pub_result_fred is not None
+
+    eps_spend_for_fred = user_budget.current_spend
+
+    # Epsilon spend for sally should be equal to fred
+    # since they impact the same of values in the data independently
+    assert eps_spend_for_sally == eps_spend_for_fred
+
+    pub_result_comb = result.publish(
+        get_budget_for_user=get_budget_for_user,
+        deduct_epsilon_for_user=deduct_epsilon_for_user,
+        ledger=ledger,
+        sigma=50,
+        private=True,
+    )
+
+    assert pub_result_comb is not None
+    pass
+
+
+def test_publish_unchanged_pb() -> None:
+    """ When publishing two queries with different data subjects but identical data and sigma, no PB should be spent."""
     pass
 
 
 def test_publish_existing_subjects() -> None:
     """Test the ledger is updated correctly when existing data subjects have more data published"""
+    pass
+
+
+def test_publish_sigma_affects_pb() -> None:
+    """ Test that increasing sigma decreases the privacy budget spent. """
     pass
