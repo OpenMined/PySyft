5402ca07e6be99dd3fdea3a95c6e9726ce2f6aad
Ionesio Junior <ionesiojr@gmail.com>
2023-01-04 09:36:50 -0300

    Update task_submission messages/service

--- packages/syft/src/syft/core/node/common/node_service/task_submission/task_submission.py
+++ packages/syft/src/syft/core/node/common/node_service/task_submission/task_submission.py
@@ -1,18 +1,13 @@
 # stdlib
 from datetime import date
-from io import StringIO
-import sys
 from typing import Any
 from typing import Dict
 from typing import List
 from typing import Optional
 from typing import Type
-from typing import Union
 
 # third party
-from nacl.encoding import HexEncoder
 from nacl.signing import VerifyKey
-from pydantic import EmailStr
 from typing_extensions import final
 
 # relative
@@ -20,28 +15,17 @@ from ......core.common.uid import UID
 from .....common.serde.serializable import serializable
 from ....domain_interface import DomainInterface
 from ....domain_msg_registry import DomainMessageRegistry
-from ...node_table.utils import model_to_json
+from ...node_table.task import NoSQLTask
 from ...permissions.permissions import BasePermission
 from ...permissions.user_permissions import NoRestriction
 from ...permissions.user_permissions import UserIsOwner
 from ..generic_payload.syft_message import NewSyftMessage as SyftMessage
 from ..generic_payload.syft_message import ReplyPayload
 from ..generic_payload.syft_message import RequestPayload
-
-
-def run_task(code: str) -> None:
-    # create file-like string to capture output
-    codeOut = StringIO()
-    codeErr = StringIO()
-
-    sys.stdout = codeOut
-    sys.stderr = codeErr
-    exec(code)
-    # restore stdout and stderr
-    sys.stdout = sys.__stdout__
-    sys.stderr = sys.__stderr__
-    print("Error: ", codeErr.getvalue())
-    print("Std Output: ", codeOut.getvalue())
+from .enum import EXECUTION_STATUS
+from .enum import TASK_SERVICE_DEFAULT_MESSAGES
+from .enum import TASK_SERVICE_FIELDS
+from .enum import TASK_SERVICE_STATUS
 
 
 @serializable(recursive_serde=True)
@@ -52,13 +36,15 @@ class CreateTask(SyftMessage, DomainMessageRegistry):
     class Request(RequestPayload):
         """Payload fields and types used during a User Creation Request."""
 
+        load_vars: Dict[str, str]
         code: str
+        save_vars: List[str]
 
     # Pydantic Inner class to define expected reply payload fields.
     class Reply(ReplyPayload):
         """Payload fields and types used during a User Creation Response."""
 
-        message: str = "Your task was successfully submited!"
+        message: str = TASK_SERVICE_DEFAULT_MESSAGES.CREATE_TASK.value
 
     request_payload_type = (
         Request  # Converts generic syft dict into a CreateUserMessage.Request object.
@@ -72,27 +58,22 @@ class CreateTask(SyftMessage, DomainMessageRegistry):
     ) -> ReplyPayload:  # type: ignore
         user = node.users.get_user(verify_key=verify_key)
 
-        if not hasattr(node, "tasks"):
-            node.tasks = {}
-
-        if not node.tasks.get(user.id, None):
-            node.tasks[user.id] = []
-
-        user_role = node.roles.first(id=user.role).name
-        if user_role:
-            # Mock, replace it by database calls
-            node.tasks[user.id].append(
-                {
-                    "id": UID().to_string(),
-                    "user": {"name": user.name, "role": user_role, "email": user.email},
-                    "code": self.payload.code,
-                    "status": "pending",
-                    "created_at": date.today().strftime("%d/%m/%Y"),
-                    "updated_at": "--",
-                    "reviewed_by": "--",
-                    "execution": "--",
-                }
-            )
+        task = NoSQLTask(
+            uid=UID().to_string(),
+            user=user.id.to_string(),
+            load_vars=self.payload.load_vars,
+            saved_vars={var: " -- " for var in self.payload.save_vars},
+            owner={"name": user.name, "role": user.role["name"], "email": user.email},
+            code=self.payload.code,
+            status=TASK_SERVICE_STATUS.PENDING.value,
+            created_at=date.today().strftime("%d/%m/%Y"),
+            updated_at=" -- ",
+            reviewed_by=" -- ",
+            execution={
+                TASK_SERVICE_FIELDS.STATUS.value: EXECUTION_STATUS.WAITING.value
+            },
+        )
+        node.tasks.add(task)
         return CreateTask.Reply()
 
     def get_permissions(self) -> List[Type[BasePermission]]:
@@ -124,8 +105,65 @@ class GetTasks(SyftMessage, DomainMessageRegistry):
     ) -> ReplyPayload:  # type: ignore
         user = node.users.get_user(verify_key=verify_key)
 
-        tasks = node.tasks[user.id]
+        if user.role["name"] == node.roles.owner_role["name"]:
+            tasks = node.tasks.all()
+        else:
+            tasks = node.tasks.find(
+                search_params={TASK_SERVICE_FIELDS.USER.value: user.id.to_string()}
+            )
         return GetTasks.Reply(tasks=tasks)
+
+    def get_permissions(self) -> List[Type[BasePermission]]:
+        """Returns the list of permission classes."""
+        return [NoRestriction]
+
+
+@serializable(recursive_serde=True)
+@final
+class GetTask(SyftMessage, DomainMessageRegistry):
+
+    # Pydantic Inner class to define expected request payload fields.
+    class Request(RequestPayload):
+        task_uid: str
+
+    # Pydantic Inner class to define expected reply payload fields.
+    class Reply(ReplyPayload):
+        code: str
+        status: str
+        owner: Dict[str, str]
+        created_at: str
+        updated_at: str
+        reviewed_by: str
+        execution: Dict[str, str]
+        uid: str
+        reason: str
+        load_vars: Dict[str, str] = {}
+        saved_vars: Dict[str, str] = {}
+
+    request_payload_type = (
+        Request  # Converts generic syft dict into a CreateUserMessage.Request object.
+    )
+    reply_payload_type = (
+        Reply  # Creates a proper Reply payload message structure as a response.
+    )
+
+    def run(  # type: ignore
+        self, node: DomainInterface, verify_key: Optional[VerifyKey] = None
+    ) -> ReplyPayload:  # type: ignore
+        user = node.users.get_user(verify_key=verify_key)
+        if user.role["name"] == node.roles.owner_role["name"]:
+            task = node.tasks.find_one(
+                search_params={TASK_SERVICE_FIELDS.UID.value: self.payload.task_uid}
+            )
+        else:
+            task = node.tasks.find_one(
+                search_params={
+                    TASK_SERVICE_FIELDS.USER.value: user.id.to_string(),
+                    TASK_SERVICE_FIELDS.UID.value: self.payload.task_uid,
+                }
+            )
+
+        return GetTask.Reply(**task)
 
     def get_permissions(self) -> List[Type[BasePermission]]:
         """Returns the list of permission classes."""
@@ -138,13 +176,13 @@ class ReviewTask(SyftMessage, DomainMessageRegistry):
 
     # Pydantic Inner class to define expected request payload fields.
     class Request(RequestPayload):
-        task_id: str
+        task_uid: str
         reason: str
         status: str
 
     # Pydantic Inner class to define expected reply payload fields.
     class Reply(ReplyPayload):
-        message: str = "Review submitted!"
+        message: str = TASK_SERVICE_DEFAULT_MESSAGES.REVIEW_TASK.value
 
     request_payload_type = (
         Request  # Converts generic syft dict into a CreateUserMessage.Request object.
@@ -160,16 +198,21 @@ class ReviewTask(SyftMessage, DomainMessageRegistry):
 
         status = self.payload.status.lower()
 
-        for user_tasks in node.tasks.values():
-            for task in user_tasks:
-                if task["id"] == self.payload.task_id:
-                    task["status"] = status
-                    task["reason"] = self.payload.reason
-                    task["reviewed_by"] = user.name
-                    task["updated_at"] = date.today().strftime("%d/%m/%Y")
-                    if status == "accepted":
-                        run_task(task["code"])
-                        task["execution"] = {"status": "enqueued"}
+        update_values = {
+            TASK_SERVICE_FIELDS.STATUS.value: status,
+            TASK_SERVICE_FIELDS.REASON.value: self.payload.reason,
+            TASK_SERVICE_FIELDS.REVIEWED_BY.value: user.name,
+            TASK_SERVICE_FIELDS.UPDATED_AT.value: date.today().strftime("%d/%m/%Y"),
+            TASK_SERVICE_FIELDS.EXECUTION.value: {}
+            if status != TASK_SERVICE_STATUS.ACCEPTED.value
+            else {TASK_SERVICE_FIELDS.STATUS.value: EXECUTION_STATUS.ENQUEUED.value},
+        }
+
+        node.tasks.update(
+            search_params={TASK_SERVICE_FIELDS.UID.value: self.payload.task_uid},
+            updated_args=update_values,
+        )
+
         return CreateTask.Reply()
 
     def get_permissions(self) -> List[Type[BasePermission]]:
