8949aaedf688efb3fa4e3dd0db4c7328f150f95b
Dylan Hrebenach <dhrebenach@fubo.tv>
2023-01-16 12:43:24 -0800

    fix: css formatting & svelte config

--- packages/grid/svelte/src/lib/components/Badge.svelte
+++ packages/grid/svelte/src/lib/components/Badge.svelte
@@ -11,46 +11,14 @@
     @apply h-5;
   }
   .gray {
-    @apply bg-gray-100
-    text-gray-800
-    px-1.5
-    py-0.5
-    rounded-sm
-    items-center
-    inline-flex
-    font-bold
-    font-roboto
-    text-xs
-    leading-normal;
+    @apply bg-gray-100 text-gray-800 px-1.5 py-0.5 rounded-sm items-center inline-flex font-bold font-roboto text-xs leading-normal;
   }
 
   .primary-light {
-    @apply bg-primary-100
-    text-primary-600
-    px-1.5
-    py-0.5
-    rounded-sm
-    items-center
-    inline-flex
-    font-bold
-    font-roboto
-    text-xs
-    leading-normal
-    w-fit;
+    @apply bg-primary-100text-primary-600 px-1.5 py-0.5 rounded-sm items-center inline-flex font-bold font-roboto text-xs leading-normal w-fit;
   }
 
   .primary-dark {
-    @apply bg-gray-800
-    text-primary-200
-    px-1.5
-    py-0.5
-    rounded-sm
-    items-center
-    inline-flex
-    font-bold
-    font-roboto
-    text-xs
-    leading-normal
-    w-fit;
+    @apply bg-gray-800 text-primary-200 px-1.5 py-0.5 rounded-sm items-center inline-flex font-bold font-roboto text-xs leading-normal w-fit;
   }
 </style>
