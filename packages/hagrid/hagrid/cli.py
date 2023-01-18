b376d3904d88317fbd86878f12bdca7e20a5855b
Kien Dang <mail@kien.ai>
2023-01-18 09:23:49 +0800

    Support extracting notebooks from a zip file for quickstart

--- packages/hagrid/hagrid/cli.py
+++ packages/hagrid/hagrid/cli.py
@@ -85,6 +85,7 @@ from .mode import EDITABLE_MODE
 from .parse_template import render_templates
 from .parse_template import setup_from_manifest_template
 from .quickstart_ui import fetch_notebooks_for_url
+from .quickstart_ui import fetch_notebooks_from_zipfile
 from .quickstart_ui import quickstart_download_notebook
 from .rand_sec import generate_sec_random_password
 from .style import RichGroup
@@ -3029,6 +3030,7 @@ def run_quickstart(
     branch: str = DEFAULT_BRANCH,
     commit: Optional[str] = None,
     python: Optional[str] = None,
+    zip_file: Optional[str] = None,
 ) -> None:
     try:
         quickstart_art()
@@ -3054,7 +3056,13 @@ def run_quickstart(
                 python=python,
             )
         downloaded_files = []
-        if url:
+        if zip_file:
+            downloaded_files = fetch_notebooks_from_zipfile(
+                zip_file,
+                directory=directory,
+                reset=reset,
+            )
+        elif url:
             downloaded_files = fetch_notebooks_for_url(
                 url=url,
                 directory=directory,
@@ -3401,26 +3409,9 @@ def add_intro_notebook(directory: str, reset: bool = False) -> str:
 
 
 @click.command(help="Walk the Path", context_settings={"show_default": True})
-@click.option(
-    "--repo",
-    default=DEFAULT_REPO,
-    help="Choose a repo to fetch the notebook from or just use OpenMined/PySyft",
-)
-@click.option(
-    "--branch",
-    default=DEFAULT_BRANCH,
-    help="Choose a branch to fetch from or just use dev",
-)
-@click.option(
-    "--commit",
-    help="Choose a specific commit to fetch the notebook from",
-)
-def dagobah(
-    repo: str = DEFAULT_REPO,
-    branch: str = DEFAULT_BRANCH,
-    commit: Optional[str] = None,
-) -> None:
-    return run_quickstart(url="padawan", repo=repo, branch=branch, commit=commit)
+@click.argument("zip_file", type=str, default="padawan.zip", metavar="ZIPFILE")
+def dagobah(zip_file: str) -> None:
+    return run_quickstart(zip_file=zip_file)
 
 
 cli.add_command(dagobah)
