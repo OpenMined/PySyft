0dda71abe185553f595ce4841c883887a152f937
rasswanth <43314053+rasswanth-s@users.noreply.github.com>
2023-01-26 09:11:21 +0530

    modified oblv tests to run on self host runners

--- scripts/port_check.py
+++ scripts/port_check.py
@@ -1,24 +1,26 @@
 """Script to pause execution until uvicorn start for oblv tox tests"""
 # stdlib
-import socket as sock
 import sys
 import time
 
+# third party
+import requests
+from requests.exceptions import ConnectionError
+
 port = int(sys.argv[1])
+
 TIMEOUT = 300  # 5 minutes
 ctr = 0
-
 while ctr <= TIMEOUT:
-    socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
-    res = socket.connect_ex(("0.0.0.0", port))
-    if res == 0:
-        break
-    else:
+    try:
+        res = requests.get(f"http://0.0.0.0:{port}/")
+        if res.status_code == 200:
+            break
+    except ConnectionError:
         time.sleep(1)
         ctr += 1
     if ctr % 5 == 0:
         print("Waiting for uvicorn process to start...")
-    socket.close()
 
 if ctr > TIMEOUT:
     print("Uvicorn process check timed out... ")
