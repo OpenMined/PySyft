cbb193ac17134773f9b7c49408c3706a9f4e7a1b
Madhava Jay <me@madhavajay.com>
2023-01-31 08:56:35 +1000

    Fixed formatting of skill_test.py file
    - Removed some unused steps in the tox lint task to speed things up

--- packages/syft/tests/trials/skill_test.py
+++ packages/syft/tests/trials/skill_test.py
@@ -34,7 +34,7 @@ def get_padawans(cohort: str) -> Dict[str, str]:
             "Oleksandr": "PASSED",
             "Khoa": "PASSED",
             "Ajinkya": "PASSED",
-	          "Hussein": "PASSED",
+            "Hussein": "PASSED",
             "Peter": "PASSED",
         },
     }
@@ -72,6 +72,5 @@ def test_trial_of_skill() -> None:
     assert get_padawans("R3Q1")["Ajinkya"] == "PASSED"
     assert get_padawans("R3Q1")["Hussein"] == "PASSED"
     assert get_padawans("R3Q1")["Peter"] == "PASSED"
-
 
     assert len(get_padawans("R3Q1")) > 1
