42e130090d99c8198e88c18107140738974ea9ea
Ionesio Junior <ionesiojr@gmail.com>
2023-02-02 22:13:21 -0300

    Serialize and deserialize UUIDs

--- js/jsserde.js
+++ js/jsserde.js
@@ -2,16 +2,25 @@ import * as capnp from "capnp-ts";
 import { RecursiveSerde } from "./capnp/recursive_serde.capnp.cjs";
 import { KVIterable } from "./capnp/kv_iterable.capnp.cjs";
 import { Iterable } from "./capnp/iterable.capnp.cjs";
+import { v4 as uuidv4 } from 'uuid';
+import { stringify as uuidStringify } from 'uuid';
+import { parse as uuidParse } from 'uuid';
 
-class SimpleObject{
-	constructor(first, second, third, fourth, fifth) {
-		this.first = first
-		this.second = second
-		this.third = third
-		this.fourth = fourth
-		this.sixth = 78.45
-		this.fifth = new Map(Object.entries(fifth))
-		this.fqn = "grid.api.syft.syft.SimpleObject"
+class UUID {
+	constructor(value) {
+		this.value = value
+		this.fqn = "syft.core.common.uid.UID"
+	}	
+}
+
+class CreateUserMessage {
+	constructor(address, reply_to, reply, kwargs) {
+		this.address = new UUID(address);
+		this.id = new UUID(uuidv4());
+		this.reply_to = new UUID(reply_to);
+		this.reply = reply
+		this.kwargs = new Map(Object.entries(kwargs))
+		this.fqn = "syft.core.node.common.node_service.user_manager.new_user_messages.CreateUserMessage"
 	}
 }
 	
@@ -93,7 +102,35 @@ class JSSerde {
 					]
 				this.type_bank['builtins.str'] = [ true, function (text) { return  new TextEncoder().encode(text)}, function (buffer) {return new TextDecoder().decode(buffer)}, null, {}];
 				this.type_bank['builtins.bytes'] = [ true, function (bytes) { return  bytes.buffer}, function (buffer) {return new Uint8Array(buffer)}, null, {}];
+				this.type_bank["syft.core.common.uid.UID"] = [false, function (uuid){
+					const message = new capnp.Message()
+					const rs = message.initRoot(RecursiveSerde)
+					const fields = rs.initFieldsName(1)
+					const data = rs.initFieldsData(1)
+					rs.setFullyQualifiedName("syft.core.common.uid.UID")
 
+					fields.set(0, 'value')
+					let serializedObj = _this.serialize(uuidParse(uuid.value))
+					let dataMsg = new capnp.Message(serializedObj, false);
+					let dataCapnpObj = capnp.Struct.initData(0, serializedObj.byteLength, dataMsg.getRoot(capnp.Data))
+					dataCapnpObj.copyBuffer(serializedObj)
+					data.set(0, dataCapnpObj)						
+
+					return message.toArrayBuffer()
+				}, function (buffer){
+					let uuidMap = new Map()
+					const message = new capnp.Message(buffer, false);
+					const rs = message.getRoot(RecursiveSerde);
+					const fieldsName = rs.getFieldsName()
+					const fieldsData = rs.getFieldsData()
+					for (let index = 0; index < fieldsName.getLength(); index++) {
+						const key = fieldsName.get(index)
+						const bytes = fieldsData.get(index)
+						const obj = uuidStringify( _this.deserialize(bytes.toArrayBuffer()))
+						uuidMap.set(key, obj)
+					}
+					return uuidMap
+				}, null, {}]
 				this.type_bank['builtins.bool'] = [true,
 					function (boolean) { return ( (boolean) ? new Uint8Array([49]).buffer : new Uint8Array(48).buffer)},
 					function (buffer) { return ( (new Uint8Array(buffer)[0] == 49) ? true : false)},
@@ -179,8 +216,9 @@ class JSSerde {
 		}
 		else if (Array.isArray(obj)){
 			fqn = 'builtins.list'
-		}
-		else if (obj instanceof Map){
+		} else if (obj && obj.byteLength !== undefined) {
+			fqn = 'builtins.bytes'
+		} else if (obj instanceof Map) {
 			fqn = 'builtins.dict'
 		}
 		else {
@@ -191,8 +229,16 @@ class JSSerde {
 		let objSerdeProps = this.type_bank[fqn]
 		let notRecursive = objSerdeProps[0]
 		rs.setFullyQualifiedName(fqn)
-		if (!notRecursive) {
-			delete obj.fqn
+
+		delete obj.fqn
+		if (notRecursive) {
+			let serializedObj = this.type_bank[fqn][1](obj)
+			let data = rs.initNonrecursiveBlob(serializedObj.byteLength)
+			data.copyBuffer(serializedObj)
+		} 
+		else if (objSerdeProps[1] !== null) {
+			return  this.type_bank[fqn][1](obj)
+		} else {
 			const txt = rs.initFieldsName(Object.keys(obj).length)
 			const data = rs.initFieldsData(Object.keys(obj).length)
 			let count = 0
@@ -205,11 +251,6 @@ class JSSerde {
 				data.set(count, dataCapnpObj)
 				count += 1
 			}
-		} else{
-			let serializedObj = this.type_bank[fqn][1](obj)
-			let data = rs.initNonrecursiveBlob(serializedObj.byteLength)
-			data.copyBuffer(serializedObj)
-
 		}
 
 		return message.toArrayBuffer()
@@ -221,9 +262,11 @@ class JSSerde {
 		const fieldsName = rs.getFieldsName()
 		const size = fieldsName.getLength()
 		const fqn = rs.getFullyQualifiedName()
-		let obj_properties = this.type_bank[fqn]
+		let objSerdeProps = this.type_bank[fqn]
 		if (size < 1) {
 			return this.type_bank[fqn][2](rs.getNonrecursiveBlob().toArrayBuffer())
+		} else if (objSerdeProps[2] !== null) { 
+			return this.type_bank[fqn][2](buffer)
 		} else {
 			let kv_iterable = new Map()
 			const fieldsData = rs.getFieldsData()
@@ -243,9 +286,10 @@ class JSSerde {
 }
 const js = new JSSerde('http://localhost:8081/api/v1/syft/serde')
 await js.loadTypeBank();
+
 const response = await fetch('http://localhost:8081/api/v1/syft/js')
 const bytes_arr = await response.arrayBuffer()
-let result = js.deserialize(js.serialize(new SimpleObject(589462615899, true, 'hello world', [1,2.5,3.78,4,5], {'Hello': 'World', 'field1': 159.78})))
-//console.log(typeof result.get('fourth')[0])
-//console.log(" Object Sent From PySyft to JavaScript : ", js.deserialize(bytes_arr))
-//const response2 = await fetch('http://localhost:8081/api/v1/syft/js', {method: "POST", headers: {"content-type": "application/octect-stream"}, body: js.serialize(new SimpleObject(true, 'JavaScript SimpleObject'))})
+let result = js.deserialize(bytes_arr)
+console.log(result)
+let msg = new CreateUserMessage(uuidv4(),uuidv4(),false, {'email': 'info@openmined.org', 'password': 'pwd123', 'name': 'Jana Doe', 'role': 'Data Scientist', 'institution': "DPUK"})
+console.log(js.deserialize(js.serialize(msg)))
