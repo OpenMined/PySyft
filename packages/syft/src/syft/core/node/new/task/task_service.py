4a4699da081bd4436c856d65e5564a57abfe2619
rasswanth <43314053+rasswanth-s@users.noreply.github.com>
2023-02-06 08:51:44 +0530

    finished enclave code approval

--- packages/syft/src/syft/core/node/new/task/task_service.py
+++ packages/syft/src/syft/core/node/new/task/task_service.py
@@ -1,17 +1,23 @@
 # stdlib
 from datetime import date
+import os
 from typing import Dict
 from typing import List
 from typing import Optional
+from typing import cast
 
 # third party
+import requests
 from result import Err
 from result import Ok
 from result import Result
 
 # relative
+from .....oblv.constants import LOCAL_MODE
+from ....common.serde.deserialize import _deserialize as deserialize
 from ....common.serde.serializable import serializable
 from ....common.uid import UID
+from ..api import SyftAPI
 from ..context import AuthedServiceContext
 from ..document_store import DocumentStore
 from ..service import AbstractService
@@ -19,6 +25,8 @@ from ..service import service_method
 from .task import NodeView
 from .task import Task
 from .task_stash import TaskStash
+
+DOMAIN_CONNECTION_PORT = int(os.getenv("DOMAIN_CONNECTION_PORT", 3030))
 
 
 @serializable(recursive_serde=True)
@@ -139,8 +147,52 @@ class TaskService(AbstractService):
             return task.err()
 
         task.reason = reason
-        task.status[task.owners[0]] = approve
+        task.status[task.owners[0]] = "Approved" if approve else "Denied"
         res = self.task_stash.update(task)
+
+        # If we are in the Enclave and have metadata for enclaves
+        # Sent the task status to the connected enclaves
+        if task.oblv_metadata:
+            if LOCAL_MODE:
+                api = self._get_api(
+                    f"http://host.docker.internal:{DOMAIN_CONNECTION_PORT}"
+                )
+                enclave_res = api.services.task.send_status_to_enclave(task)
+                if isinstance(enclave_res, bool) and enclave_res:
+                    return Ok(f"Sent task: {task_id} status to enclave")
+                return Err(f"{enclave_res}")
+            else:
+                # TODO 🟣 Add code for real enclave deployment
+                pass
+
         if res.is_ok():
             return Ok(f"Task: {task_id}  - {approve}")
         return res.err()
+
+    @staticmethod
+    def _get_api(connection_string: str) -> SyftAPI:
+
+        req = requests.get(
+            connection_string + "/worker/api",
+        )
+        obj = deserialize(req.content, from_bytes=True)
+        obj.api_url = f"{connection_string}/worker/syft_api_call"
+        return cast(SyftAPI, obj)
+
+    @service_method(path="task.send_status_to_enclave", name="send_status_to_enclave")
+    def send_status_to_enclave(
+        self, context: AuthedServiceContext, task: Task
+    ) -> Result[Ok, Err]:
+        enclave_task = self.task_stash.get_by_uid(task.id)
+        if enclave_task.is_ok():
+            # 🟡 TODO: To Remove double nesting of result variable in Collection access
+            enclave_task = enclave_task.ok().ok()
+        else:
+            return enclave_task.err()
+        if task.owners[0] in enclave_task.owners:
+            enclave_task.status[task.owners[0]] = task.status[task.owners[0]]
+            return Ok(True)
+        else:
+            return Err(
+                f"Task: {task.id.no_dash} in enclave does not contain {task.owners[0].name} "
+            )
