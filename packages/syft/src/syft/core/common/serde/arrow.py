ab665309f76c9f93ae85d0338f0a2ce1411df2ff
rasswanth <43314053+rasswanth-s@users.noreply.github.com>
2023-02-07 09:48:47 +0530

    added numpy serialization for string array

--- packages/syft/src/syft/core/common/serde/arrow.py
+++ packages/syft/src/syft/core/common/serde/arrow.py
@@ -1,4 +1,6 @@
 # stdlib
+from typing import Tuple
+from typing import Union
 from typing import cast
 
 # third party
@@ -29,11 +31,12 @@ def arrow_serialize(obj: np.ndarray) -> bytes:
     return cast(bytes, _serialize((numpy_bytes, buffer.size, dtype), to_bytes=True))
 
 
-def arrow_deserialize(buf: bytes) -> np.ndarray:
-    (numpy_bytes, decompressed_size, dtype) = _deserialize(buf, from_bytes=True)
+def arrow_deserialize(
+    numpy_bytes: bytes, decompressed_size: int, dtype: str
+) -> np.ndarray:
     original_dtype = np.dtype(dtype)
     if flags.APACHE_ARROW_COMPRESSION is ApacheArrowCompression.NONE:
-        reader = pa.BufferReader(buf)
+        reader = pa.BufferReader(numpy_bytes)
         numpy_bytes = reader.read_buffer()
     else:
         numpy_bytes = pa.decompress(
@@ -46,3 +49,80 @@ def arrow_deserialize(buf: bytes) -> np.ndarray:
     np_array = result.to_numpy()
     np_array.setflags(write=True)
     return np_array.astype(original_dtype)
+
+
+def numpyutf8toarray(input_index: Tuple[np.ndarray, np.ndarray]) -> np.ndarray:
+    """Decodes utf-8 encoded numpy array to numpy array.
+
+    Args:
+        string_index (Tuple[np.ndarray, np.ndarray]): encoded array
+
+    Returns:
+        np.ndarray: decoded NumpyArray.
+    """
+    shape_length = int(input_index[-1])
+    shape = tuple(input_index[-(shape_length + 1) : -1])  # noqa
+    string_index = input_index[: -(shape_length + 1)]
+    index_length = int(string_index[-1])
+    index_array = string_index[-(index_length + 1) : -1]  # noqa
+    string_array: np.ndarray = string_index[: -(index_length + 1)]
+    output_bytes: bytes = string_array.astype(np.uint8).tobytes()
+    output_list = []
+    last_offset = 0
+    for offset in index_array:
+        chars = output_bytes[last_offset:offset]
+        final_string = chars.decode("utf-8")
+        last_offset = offset
+        output_list.append(final_string)
+    return np.array(output_list).reshape(shape)
+
+
+def arraytonumpyutf8(string_list: Union[str, np.ndarray]) -> bytes:
+    """Encodes Numpyarray  to utf-8 encoded numpy array.
+
+    Args:
+        string_array (np.ndarray): NumpyArray to be encoded
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: utf-8 encoded int Numpy array
+    """
+    array_shape = np.array(string_list).shape
+    string_list = np.array(string_list).flatten()
+    bytes_list = []
+    indexes = []
+    offset = 0
+    # print("dsl list ", string_list)
+    for item in string_list:
+        name_bytes = item.encode("utf-8")
+        offset += len(name_bytes)
+        indexes.append(offset)
+        bytes_list.append(name_bytes)
+
+    np_bytes = np.frombuffer(b"".join(bytes_list), dtype=np.uint8)
+    np_bytes = np_bytes.astype(np.uint64)
+    np_indexes = np.array(indexes, dtype=np.uint64)
+    index_length = np.array([len(np_indexes)], dtype=np.uint64)
+    shape = np.array(array_shape, dtype=np.uint64)
+    shape_length = np.array([len(shape)], dtype=np.uint64)
+    output_array = np.concatenate(
+        [np_bytes, np_indexes, index_length, shape, shape_length]
+    )
+
+    return cast(bytes, _serialize(output_array, to_bytes=True))
+
+
+def numpy_serialize(obj: np.ndarray) -> bytes:
+    if obj.dtype.type != np.str_:
+        return arrow_serialize(obj)
+    else:
+        return arraytonumpyutf8(obj)
+
+
+def numpy_deserialize(buf: bytes) -> np.ndarray:
+    deser = _deserialize(buf, from_bytes=True)
+    if isinstance(deser, tuple):
+        return arrow_deserialize(*deser)
+    elif isinstance(deser, np.ndarray):
+        return numpyutf8toarray(deser)
+    else:
+        raise ValueError(f"Invalid type:{type(deser)} for numpy deserialization")
