60ebcb0276b70fad4cea71b8f230cfc8b887ab9a
Ionesio Junior <ionesiojr@gmail.com>
2023-02-08 19:42:33 -0300

    ADD node adapter for production mode

--- packages/grid/pygrid-ui/svelte.config.js
+++ packages/grid/pygrid-ui/svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
