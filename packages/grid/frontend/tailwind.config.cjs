69ad7ef09a23329a9716c6e63c85acce3a3a32cb
Ionesio Junior <ionesiojr@gmail.com>
2023-02-08 21:11:30 -0300

    Run yarn format

--- packages/grid/frontend/tailwind.config.cjs
+++ packages/grid/frontend/tailwind.config.cjs
@@ -1,17 +1,15 @@
 const config = {
-  content: [
-    "./src/**/*.{html,js,svelte,ts}",
-    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
-  ],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
 
-  theme: {
-    extend: {},
-  },
+	theme: {
+		extend: {}
+	},
 
-  plugins: [
-    require('flowbite/plugin')
-  ],
-  darkMode: 'class',
+	plugins: [require('flowbite/plugin')],
+	darkMode: 'class'
 };
 
 module.exports = config;
