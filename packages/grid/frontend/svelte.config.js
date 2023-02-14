7df49637b4ecb42c51865fb4bf7c6465628a04bc
Thiago Costa Porto <thiagocostaporto@gmail.com>
2023-02-14 22:48:09 +0000

    style: prettier pass
    On a side note, we removed a couple of lines of code referring to dead
    environment variables and useless tests.
    
    Co-authored-by: Ionesio Jr <ionesio@openmined.org>
    Co-authored-by: Madhava Jay <madhava@openmined.org>

--- packages/grid/frontend/svelte.config.js
+++ packages/grid/frontend/svelte.config.js
@@ -4,18 +4,16 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [
-		vitePreprocess(),
-		preprocess({
-			postcss: true
-		})
-	],
+  preprocess: [
+    vitePreprocess(),
+    preprocess({
+      postcss: true
+    })
+  ],
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter()
+  }
 };
 
 export default config;
--- packages/grid/frontend/vite.config.ts
+++ packages/grid/frontend/vite.config.ts
@@ -2,8 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [sveltekit()],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  }
 });
