0049b1cdb15383a79fb3c7a6b9aa87f9ebc9cfe3
teo <teo.milea97@gmail.com>
2023-02-15 09:20:27 +0200

    saved progress

--- packages/syft/src/syft/core/tensor/autodp/new_phi_tensor.py
+++ packages/syft/src/syft/core/tensor/autodp/new_phi_tensor.py
@@ -7,7 +7,10 @@ from typing import List
 from typing import Optional
 from typing import Union
 from typing import Tuple
+from typing import Any
 from ...common.uid import UID
+from ..passthrough import is_acceptable_simple_type
+from ..broadcastable import is_broadcastable
 
 class Bounds():
     lower_bound: int
@@ -23,10 +26,56 @@ class Bounds():
             new_upper_bound = self.upper_bound + other.upper_bound
             return Bounds(new_lower_bound, new_upper_bound)
 
+class BoundsArray():
+    lower_bounds: np.ndarray
+    upper_bounds: np.ndarray
+    shape: Tuple[int, ...]
+
+    def __init__(self, lower_bounds, upper_bounds, shape):
+        self.lower_bounds = lower_bounds
+        self.upper_bounds = upper_bounds
+        self.shape = shape
+    
+    @staticmethod
+    def from_phi_tensors(phi_tensors):
+        shape = (len(phi_tensors), *phi_tensors[0].shape)
+        lower_bounds = np.array([phi_tensor.min_vals.data for phi_tensor in phi_tensors])
+        upper_bounds = np.array([phi_tensor.max_vals.data for phi_tensor in phi_tensors])
+        return BoundsArray(lower_bounds=lower_bounds, upper_bounds=upper_bounds, shape=shape)
+
+    def __add__(self, other: Any):
+        if is_acceptable_simple_type(other):
+            return self.__class__(
+                    lower_bounds=self.lower_bounds + other, 
+                    upper_bounds=self.upper_bounds + other, 
+                    shape=self.shape
+                )
+
+        if not is_broadcastable(self.shape, other.shape):
+            raise Exception(
+                f"Cannot broadcast arrays with shapes: {self.shape} & {other.shape}"
+            )
+
+        # if self.data.shape == other.data.shape:
+        return self.__class__(
+                lower_bounds=self.lower_bounds + other.lower_bounds, 
+                upper_bounds=self.upper_bounds + other.upper_bounds, 
+                shape=self.shape
+            )
+        # else:
+        #     return self.__class__(data=self.data + other.data, shape=self.shape)
+
+    def sum(self, axis: Optional[Union[int, Tuple[int, ...]]]):
+        shape = ()
+        prod_shape = np.prod(self.shape)
+        lower_bounds = np.sum(self.lower_bounds * prod_shape)
+        upper_bounds = np.sum(self.upper_bounds * prod_shape) 
+        return BoundsArray(lower_bounds=lower_bounds,upper_bounds=upper_bounds,shape=shape)
+
 class RowPhiTensors():
     data: np.array
     data_subjects: List[DataSubject]
-    bounds: lazyrepeatarray
+    bounds: BoundsArray
     # source_ids: List[PhiTensor]
     
     def __init__(
@@ -47,14 +96,14 @@ class RowPhiTensors():
     def from_phi_tensors(phi_tensors) -> None:
         data = np.array([phi_tensor.child for phi_tensor in phi_tensors])
         data_subjects = [phi_tensor.data_subject for phi_tensor in phi_tensors]
-        bounds = [Bounds(phi_tensor.min_vals.data, phi_tensor.max_vals.data) for phi_tensor in phi_tensors]
+        bounds = BoundsArray.from_phi_tensors(phi_tensors)
         # self.source_phi_tensors = phi_tensors
         return RowPhiTensors(data=data, data_subjects=data_subjects, bounds=bounds)
     
     def broadcast_op(self, op, *args, **kwargs):
         new_data = getattr(self.data, op)(*args, **kwargs)
-        # new_bounds = getattr(self.bounds, op)(*args, **kwargs)
-        return RowPhiTensors(data=new_data, bounds=[], data_subjects=self.data_subjects)
+        new_bounds = getattr(self.bounds, op)(*args, **kwargs)
+        return RowPhiTensors(data=new_data, bounds=new_bounds, data_subjects=self.data_subjects)
     
     def reduce_op(self, op, *args, **kwargs):
         new_data = getattr(self.data, op)(*args, **kwargs)
