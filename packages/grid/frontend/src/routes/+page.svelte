e280bca4e384e5e99bf6336076fc8d87a3eefafe
Ionesio Junior <ionesiojr@gmail.com>
2023-02-15 16:02:38 -0300

    Update root endpoint to use getClient method

--- packages/grid/frontend/src/routes/+page.svelte
+++ packages/grid/frontend/src/routes/+page.svelte
@@ -1,18 +1,12 @@
 <script>
-  import { store, getSerde } from '../lib/store.js';
+  import { getClient } from '../lib/store.js';
   import { goto } from '$app/navigation';
-
-  let credentials = null;
-
-  store.subscribe((value) => {
-    credentials = value.credentials;
-  });
 </script>
 
 <main>
   <h1>PyGrid</h1>
-  {#await getSerde() then jsserde}
-    {#if JSON.stringify(credentials) === '{}'}
+  {#await getClient() then client}
+    {#if !client.access_token }
       {goto('/login')}
     {:else}
       {goto('/home')}
