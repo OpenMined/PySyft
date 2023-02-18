23c56b506ed9046c86cb1ba0d8b04ac2ca7c446d
Madhava Jay <me@madhavajay.com>
2023-02-18 10:05:13 +1000

    Removing print

--- packages/syft/src/syft/gevent_patch.py
+++ packages/syft/src/syft/gevent_patch.py
@@ -38,5 +38,5 @@ def is_notebook() -> bool:
 jupyter_notebook = is_notebook()
 
 if jupyter_notebook:
-    print("Patching Gevent in Jupyter")
+    # print("Patching Gevent in Jupyter")
     monkey.patch_all(thread=False)
