59f365ff81857db8aca195afb0102ebb71bc65ee
Ionesio Junior <ionesiojr@gmail.com>
2023-02-20 20:46:43 -0300

    Update root layout

--- packages/grid/frontend/src/routes/+layout.svelte
+++ packages/grid/frontend/src/routes/+layout.svelte
@@ -1,18 +1,7 @@
+
 <script>
   import '../app.css';
-  import NavBar from '$lib/components/Navigation/NavBar.svelte';
-  import AppRouter from '$lib/components/Router.svelte';
-  import { Router } from 'svelte-routing';
-
-  export let url = '';
 </script>
 
-<div class="flex h-auto min-h-screen">
-  <Router {url}>
-    <NavBar />
-    <AppRouter />
-  </Router>
-
-  <!-- Suppress console slot warning -->
-  {#if false}<slot />{/if}
-</div>
+<title>PyGrid</title>
+<slot />
