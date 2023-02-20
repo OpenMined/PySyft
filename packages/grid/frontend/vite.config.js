aba87747469c722c9ba73768a9ee6cb88cf0c544
Ionesio Junior <ionesiojr@gmail.com>
2023-02-20 19:27:35 -0300

    Update vite.config.js to allow use public src path

--- packages/grid/frontend/vite.config.js
+++ packages/grid/frontend/vite.config.js
@@ -2,7 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server:{
+    fs:{
+      allow: ['..']
+    }
+  }
 };
 
 export default config;
