bab5a5131038b9ca71f3dff74a9a2d5c388d73b8
Madhava Jay <me@madhavajay.com>
2023-02-23 15:58:08 +1000

    Add node_uid to serde of SyftAPI

--- packages/syft/src/syft/core/node/new/api.py
+++ packages/syft/src/syft/core/node/new/api.py
@@ -259,7 +259,7 @@ class SyftAPI(SyftObject):
     # version
     __canonical_name__ = "SyftAPI"
     __version__ = SYFT_OBJECT_VERSION_1
-    __attr_allowlist__ = ["endpoints"]
+    __attr_allowlist__ = ["endpoints", "node_uid"]
 
     # fields
     connection: Optional[NodeConnection] = None
@@ -268,7 +268,7 @@ class SyftAPI(SyftObject):
     api_module: Optional[APIModule] = None
     signing_key: Optional[SyftSigningKey] = None
     # serde / storage rules
-    __attr_state__ = ["endpoints"]
+    __attr_state__ = ["endpoints", "node_uid"]
 
     # def __post_init__(self) -> None:
     #     pass
