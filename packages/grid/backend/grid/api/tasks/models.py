3cd5ff165b42853518fb83ff5370a36e62f2d141
teo <teo.milea97@gmail.com>
2023-02-27 23:18:06 +0200

    remove .next frontend

--- packages/grid/backend/grid/api/tasks/models.py
+++ packages/grid/backend/grid/api/tasks/models.py
@@ -1,67 +1,67 @@
-# stdlib
-from typing import Dict
-from typing import List
-from typing import Optional
+# # stdlib
+# from typing import Dict
+# from typing import List
+# from typing import Optional
 
-# third party
-from pydantic import BaseModel
-
-
-class CreateTaskModel(BaseModel):
-    code: str
-    inputs: Dict[str, str] = {}
-    outputs: List[str] = []
-    policy_code: str
-    policy_name: str
+# # third party
+# from pydantic import BaseModel
 
 
-class ReviewTaskModel(BaseModel):
-    status: str
-    reason: str
-    task_uid: Optional[str] = ""
-
-class RunTaskModel(BaseModel):
-    task_uid: str
-    poicy_args: str
-
-class TaskOutputs(BaseModel):
-    outputs: Dict[str,str]
-
-class Task(BaseModel):
-    code: str
-    uid: str
-    owner: Dict[str, str]
-    created_at: str
-    updated_at: str
-    reviewed_by: str
-    status: str
-    execution: Dict[str, str]
-    reason: Optional[str]
-    inputs: Dict[str, str] = {}
-    outputs: Dict[str, str] = {}
-    policy_code: str
-    policy_name: str
-    # poicy_args: str
-    state: str
+# class CreateTaskModel(BaseModel):
+#     code: str
+#     inputs: Dict[str, str] = {}
+#     outputs: List[str] = []
+#     policy_code: str
+#     policy_name: str
 
 
-class GetTasks(BaseModel):
-    tasks: List[Task]
+# class ReviewTaskModel(BaseModel):
+#     status: str
+#     reason: str
+#     task_uid: Optional[str] = ""
+
+# class RunTaskModel(BaseModel):
+#     task_uid: str
+#     poicy_args: str
+
+# class TaskOutputs(BaseModel):
+#     outputs: Dict[str,str]
+
+# class Task(BaseModel):
+#     code: str
+#     uid: str
+#     owner: Dict[str, str]
+#     created_at: str
+#     updated_at: str
+#     reviewed_by: str
+#     status: str
+#     execution: Dict[str, str]
+#     reason: Optional[str]
+#     inputs: Dict[str, str] = {}
+#     outputs: Dict[str, str] = {}
+#     policy_code: str
+#     policy_name: str
+#     # poicy_args: str
+#     state: str
 
 
-class TaskErrorResponse(BaseModel):
-    error: str
+# class GetTasks(BaseModel):
+#     tasks: List[Task]
 
 
-class GetTask(BaseModel):
-    code: str
-    code_id: str
-    created_at: str
-    updated_at: str
-    reviewed_by: str
-    status: str
-    execution: Dict[str, str]
+# class TaskErrorResponse(BaseModel):
+#     error: str
 
 
-class StdResponseMessage(BaseModel):
-    message: str
+# class GetTask(BaseModel):
+#     code: str
+#     code_id: str
+#     created_at: str
+#     updated_at: str
+#     reviewed_by: str
+#     status: str
+#     execution: Dict[str, str]
+
+
+# class StdResponseMessage(BaseModel):
+#     message: str
