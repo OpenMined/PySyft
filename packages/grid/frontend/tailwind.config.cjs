33cf654daa4421a0dc24699264ccd047ae0752f3
Thiago Costa Porto <thiagocostaporto@gmail.com>
2023-03-01 19:42:42 +0000

    chore: remove dead code from tailwind config

--- packages/grid/frontend/tailwind.config.cjs
+++ packages/grid/frontend/tailwind.config.cjs
@@ -1,12 +1,5 @@
 const openminedColors = require('./src/lib/colors/hsl-colors.cjs');
 
-const config = {
-  content: [
-    './src/**/*.{html,js,svelte,ts}',
-    './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
-  ]
-};
-
 module.exports = {
   content: [
     './src/**/*.{html,js,svelte,ts}',
