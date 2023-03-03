12b930152df3443bd568922d8c53d3591d6eddcf
Ionesio Junior <ionesiojr@gmail.com>
2023-03-03 03:21:37 -0300

    - UPDATE / Improve JSClient - Update class constructor - Update login method - Update metadata getter - Update user getter - Update send method

--- packages/grid/frontend/src/lib/client/jsclient/jsClient.svelte
+++ packages/grid/frontend/src/lib/client/jsclient/jsClient.svelte
@@ -1,80 +1,104 @@
 <script context="module">
   import { JSSerde } from '../jsserde.svelte';
+  import { UUID } from '../objects/uid';
   import { APICall } from '../messages/syftMessage.ts';
   import sodium from 'libsodium-wrappers';
   export class JSClient {
+
+    /**
+     * Constructs a new instance of the class.
+     * @returns {Promise} A promise that resolves to an instance of the class.
+     */
     constructor() {
       return (async () => {
-        const url = window.location.protocol + '//' + window.location.host;
-        await fetch(url + '/api/v1/syft/serde')
-          .then((response) => response.json())
-          .then((response) => {
-            this.serde = new JSSerde(response['bank']);
-          });
+        const url = `${window.location.protocol}//${window.location.host}`;
+        try {
+          // Fetch the SerDe from the server and create a new JSSerde instance.
+          const response = await fetch(`${url}/api/v1/syft/serde`);
+          const { bank } = await response.json();
+          this.serde = new JSSerde(bank);
+        } catch (error) {
+          console.error('Error fetching serde:', error);
+        }
+
+        // Set the URL and message URL properties.
         this.url = url;
-        this.msg_url = url + '/api/v1/syft/js';
-        this.node_id = await this.metadata.then((metadata) => {
-          return metadata.get('id').get('value');
-        });
+        this.msg_url = `${url}/api/v1/new/api_call`;
+
+        try {
+          // Get the metadata and extract the node ID value.
+          const metadata = await this.metadata;
+          this.nodeId = metadata.id.value;
+        } catch (error) {
+          console.error('Error getting metadata:', error);
+        }
+
+        // Return an instance of the class.
         return this;
       })();
     }
 
-    login(email, password) {
-      return fetch(this.url + '/api/v1/new/login', {
+    /**
+     * Log in with the provided email and password.
+     *
+     * @param {string} email - The user's email address.
+     * @param {string} password - The user's password.
+     * @throws {Error} - If the login request returns an error response.
+     */
+    async login(email, password) {
+      // Send a POST request to the login API endpoint with the email and password.
+      const response = await fetch(`${this.url}/api/v1/new/login`, {
         method: 'POST',
         headers: { 'content-type': 'application/json' },
-        body: JSON.stringify({ email: email, password: password })
-      }).then((response) => {
-        if (response.status === 401) {
-          throw new Error('Incorred email or password!');
-        } else {
-          return response.arrayBuffer().then((body) => {
-            response = this.serde.deserialize(body);
-            let private_key_seed = response.get('signing_key').get('signing_key');
-            const keypair = sodium.crypto_sign_seed_keypair(private_key_seed);
-            this.key = keypair;
-          });
-        }
+        body: JSON.stringify({ email, password })
       });
-    }
 
-    get user() {
-      if (!this.access_token) {
-        throw new Error('User not authenticated!');
-      } else {
-        return fetch(this.url + '/api/v1/users/me', {
-          method: 'GET',
-          headers: { Authorization: this.access_token }
-        })
-          .then((response) => response.json())
-          .then((body) => {
-            return body;
-          });
+      // Get the response body as an ArrayBuffer and deserialize it using the provided Serde.
+      const body = await response.arrayBuffer();
+      const responseData = this.serde.deserialize(body);
+
+      try {
+        this.userId = new UUID(responseData.id.value)
+
+        // Extract the private key seed from the response data and generate a keypair using sodium.
+        const { signing_key: { signing_key: private_key_seed } } = responseData;
+
+        const keypair = sodium.crypto_sign_seed_keypair(private_key_seed);
+
+        // Set the keypair as the key for this instance.
+        this.key = keypair;
+      } catch (error) {
+        // If an error occurs while extracting the private key seed or generating the keypair, throw an error with the response data's error message.
+        throw new Error(responseData.Error);
       }
     }
-
-    get metadata() {
-      return fetch(this.url + '/api/v1/new/metadata_capnp')
-        .then((response) => response.arrayBuffer())
-        .then((response) => {
-          let metadata = this.serde.deserialize(response);
-
-          let nodeAddrObj = {};
-          metadata.get('id').forEach((value, key) => {
-            nodeAddrObj[key] = value;
-          });
-
-          let metadataObj = {};
-          metadata.forEach((value, key) => {
-            metadataObj[key] = value;
-          });
-
-          metadataObj.id = nodeAddrObj;
-          window.sessionStorage.setItem('metadata', JSON.stringify(metadataObj));
-          return metadata;
-        });
+    
+    
+    get user() {
+      return ( async () => { 
+        return await this.send([], {'uid': this.userId }, "user.view")
+      } )()
     }
+
+    /**
+     * Returns metadata from the server.
+     *
+     * @returns {Promise<object>} A Promise that resolves to an object containing metadata information.
+     */
+    get metadata() {
+      return (async () => {
+        const response = await fetch(`${this.url}/api/v1/new/metadata_capnp`);
+        const metadataBuffer = await response.arrayBuffer();
+        const metadata = this.serde.deserialize(metadataBuffer);
+
+        // Store the metadata in session storage.
+        window.sessionStorage.setItem('metadata', JSON.stringify(metadata));
+
+        // Return the metadata map.
+        return metadata;
+      })();
+    }
+
 
     createUser(parameters) {
       return this.send(
@@ -97,23 +121,43 @@
       );
     }
 
-    send(args, kwargs, path) {
-      const signed_call = new APICall(this.node_id, path, args, kwargs).sign(this.key, this.serde);
+    /**
+     * Sends an API call to the server.
+     *
+     * @param {Array} args - An array of arguments to pass to the API call.
+     * @param {Object} kwargs - An object of keyword arguments to pass to the API call.
+     * @param {string} path - The API endpoint to call.
+     * @returns {Promise<object>} A Promise that resolves to an object containing the API call response.
+     * @throws {Error} An error is thrown if the message signature and public key don't match.
+     */
+    async send(args, kwargs, path) {
+      const signedCall = new APICall(this.nodeId, path, args, kwargs).sign(this.key, this.serde);
 
-      return fetch(this.url + '/api/v1/new/api_call', {
-        method: 'POST',
-        headers: { 'content-type': 'application/octect-stream' },
-        body: this.serde.serialize(signed_call)
-      })
-        .then((response) => response.arrayBuffer())
-        .then((response) => {
-          const signed_msg = this.serde.deserialize(response);
-          if (!signed_msg.valid) {
-            throw new Error("Message signature and public key doesn't match!");
-          } else {
-            return signed_msg.message(this.serde);
-          }
+      try {
+         // Make a POST request to the server with the signed call.
+        const response = await fetch(this.msg_url, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/octet-stream' },
+          body: this.serde.serialize(signedCall)
         });
+
+         // Deserialize the response and check its signature.
+        const responseBuffer = await response.arrayBuffer();
+        const signedMsg = this.serde.deserialize(responseBuffer);
+
+        /**
+        if (!signedMsg.valid) {
+          throw new Error("Message signature and public key don't match!");
+        }
+
+        // Return the message contained in the response.
+        return signedMsg.message(this.serde);
+        */
+       return signedMsg
+      } catch (error) {
+        console.error('Error occurred in send()', error);
+        throw error;
+      }
     }
-  }
+}
 </script>
