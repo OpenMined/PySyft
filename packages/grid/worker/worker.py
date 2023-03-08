66fb868bce175438a72dead463e13fa097a248c2
rasswanth s <43314053+rasswanth-s@users.noreply.github.com>
2023-03-08 12:39:06 +0530

    removed extra admin creation on the new worker instance

--- packages/grid/worker/worker.py
+++ packages/grid/worker/worker.py
@@ -5,13 +5,10 @@ from fastapi import FastAPI
 from new_routes import make_routes
 
 # syft absolute
+from syft.core.node.new.client import API_PATH
 from syft.core.node.new.sqlite_document_store import SQLiteStoreClientConfig
 from syft.core.node.new.sqlite_document_store import SQLiteStoreConfig
 from syft.core.node.worker import Worker
-from syft.core.node.worker import create_admin_new
-
-API_PATH = "/api/v1/new"
-
 
 client_config = SQLiteStoreClientConfig(path="/storage/")
 sql_store_config = SQLiteStoreConfig(client_config=client_config)
@@ -20,12 +17,6 @@ worker = Worker(
 )
 
 router = make_routes(worker=worker)
-
-create_admin_new(
-    name="Jane Doe", email="info@openmined.org", password="changethis", node=worker
-)
-print("worker", worker.id, worker.signing_key)
-
 
 app = FastAPI(title="Worker")
 app.include_router(router, prefix=API_PATH)
