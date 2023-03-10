4aef1615b48de824ed2ae4808de440ba29240f38
rasswanth s <43314053+rasswanth-s@users.noreply.github.com>
2023-03-10 17:22:29 +0530

    fixed volumes issue with persistence and add sqlite_path for named method in Worker

--- packages/grid/worker/worker.py
+++ packages/grid/worker/worker.py
@@ -1,4 +1,5 @@
 # stdlib
+import os
 
 # third party
 from fastapi import FastAPI
@@ -6,16 +7,11 @@ from new_routes import make_routes
 
 # syft absolute
 from syft.core.node.new.client import API_PATH
-from syft.core.node.new.sqlite_document_store import SQLiteStoreClientConfig
-from syft.core.node.new.sqlite_document_store import SQLiteStoreConfig
 from syft.core.node.worker import Worker
 
-client_config = SQLiteStoreClientConfig(path="/storage/")
-sql_store_config = SQLiteStoreConfig(client_config=client_config)
-worker = Worker(
-    action_store_config=sql_store_config, document_store_config=sql_store_config
-)
-
+node_name = os.environ.get("NODE_NAME", "default_node_name")
+worker = Worker.named(name=node_name, local_db=True, sqlite_path="/storage/")
+print("Worker signing key", worker.signing_key)
 router = make_routes(worker=worker)
 
 app = FastAPI(title="Worker")
