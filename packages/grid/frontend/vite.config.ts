83000155ff468ae599f172810debdd5028b05e07
Dylan Hrebenach <dhrebenach@fubo.tv>
2023-03-10 15:10:05 -0800

    chore: configure vite/vitest

--- packages/grid/frontend/vite.config.ts
+++ packages/grid/frontend/vite.config.ts
@@ -1,7 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import path from 'path';
 
+/// <reference types="vitest" />
 export default defineConfig({
   plugins: [sveltekit()],
   test: {
