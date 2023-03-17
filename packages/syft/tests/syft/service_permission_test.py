c904e77b5cc1372a22c43ac35a673a26d0249435
Shubham Gupta <shubhamgupta3121@gmail.com>
2023-03-17 12:47:39 +0530

    update service_permission_test to use existing fixtures

--- packages/syft/tests/syft/service_permission_test.py
+++ packages/syft/tests/syft/service_permission_test.py
@@ -2,46 +2,43 @@
 import pytest
 
 # syft absolute
-import syft as sy
 from syft import SyftError
 from syft.core.node.new.api import SyftAPICall
-from syft.core.node.new.user import UserUpdate
 
 
 @pytest.fixture
-def worker_domain_guest():
-    worker = sy.Worker.named("test-domain-service-permissions", processes=0, reset=True)
-    domain_client = worker.root_client
-    guest_domain_client = domain_client.guest()
-    guest_domain_client.register(
-        name="Alice", email="alice@caltech.edu", password="abc123"
-    )
-    return [worker, domain_client, guest_domain_client]
+def guest_mock_user(user_stash, guest_user):
+    result = user_stash.partition.set(guest_user)
+    assert result.is_ok()
+
+    user = result.ok()
+    assert user is not None
+
+    return user
 
 
-def test_call_service_syftapi_with_permission(worker_domain_guest):
-    worker, _, guest_domain_client = worker_domain_guest
-    user_id = worker.document_store.partitions["User"].all().value[-1].id
-    res = guest_domain_client.api.services.user.update(
-        user_id, UserUpdate(user_id=user_id, name="")
-    )
+def test_call_service_syftapi_with_permission(
+    guest_domain_client, guest_mock_user, update_user
+):
+    user_id = guest_mock_user.id
+    res = guest_domain_client.api.services.user.update(user_id, update_user)
     assert res
 
 
 # this throws an AttributeError, maybe we want something more clear?
-def test_call_service_syftapi_no_permission(worker_domain_guest):
-    _, __, guest_domain_client = worker_domain_guest
+def test_call_service_syftapi_no_permission(guest_domain_client):
     with pytest.raises(AttributeError):
         guest_domain_client.api.services.user.get_all()
 
 
-def test_directly_call_service_with_permission(worker_domain_guest):
-    worker, _, guest_domain_client = worker_domain_guest
-    user_id = worker.document_store.partitions["User"].all().value[-1].id
+def test_directly_call_service_with_permission(
+    guest_domain_client, guest_mock_user, update_user
+):
+    user_id = guest_mock_user.id
     api_call = SyftAPICall(
-        node_uid=worker.id,
+        node_uid=guest_domain_client.id,
         path="user.update",
-        args=[user_id, UserUpdate(name="")],
+        args=[user_id, update_user],
         kwargs={},
     )
     signed_call = api_call.sign(guest_domain_client.api.signing_key)
@@ -50,9 +47,10 @@ def test_directly_call_service_with_permission(worker_domain_guest):
     assert result
 
 
-def test_directly_call_service_no_permission(worker_domain_guest):
-    worker, _, guest_domain_client = worker_domain_guest
-    api_call = SyftAPICall(node_uid=worker.id, path="user.get_all", args=[], kwargs={})
+def test_directly_call_service_no_permission(guest_domain_client):
+    api_call = SyftAPICall(
+        node_uid=guest_domain_client.id, path="user.get_all", args=[], kwargs={}
+    )
     signed_call = api_call.sign(guest_domain_client.api.signing_key)
     signed_result = guest_domain_client.api.connection.make_call(signed_call)
     result = signed_result.message.data
