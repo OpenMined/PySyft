d0e7ab493cad30d4a436f93b14b6a0e7fe9e30f7
Shubham Gupta <shubhamgupta3121@gmail.com>
2023-03-23 17:42:02 +0530

    add node_uid in SyftAPI to be serialized

--- packages/syft/src/syft/core/node/new/api.py
+++ packages/syft/src/syft/core/node/new/api.py
@@ -109,7 +109,7 @@ class SignedSyftAPICall(SyftObject):
 
 
 @instrument
-@serializable(attrs=["path", "args", "kwargs"])
+@serializable(attrs=["path", "args", "kwargs", "node_uid"])
 class SyftAPICall(SyftObject):
     # version
     __canonical_name__ = "SyftAPICall"
