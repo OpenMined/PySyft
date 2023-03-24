255d4eef949f22784c5f736a227af2d20aefc2ee
khoaguin <dkn.work@protonmail.com>
2023-03-24 19:52:29 +0700

    on doing metadata service test

--- packages/syft/tests/syft/metadata/fixtures.py
+++ packages/syft/tests/syft/metadata/fixtures.py
@@ -3,6 +3,7 @@ import pytest
 
 # syft absolute
 from syft.__init__ import __version__
+from syft.core.node.new.metadata_service import MetadataService
 from syft.core.node.new.metadata_stash import MetadataStash
 from syft.core.node.new.node_metadata import NodeMetadata
 from syft.core.node.new.node_metadata import NodeMetadataUpdate
@@ -34,3 +35,8 @@ def update_metadata(faker) -> NodeMetadataUpdate:
         description=faker.text(),
         on_board=faker.boolean(),
     )
+
+
+@pytest.fixture
+def metadata_service(document_store):
+    return MetadataService(store=document_store)
---
+++ packages/syft/tests/syft/metadata/metadata_service_test.py
@@ -0,0 +1,14 @@
+# syft absolute
+from syft.core.node.new.metadata_service import MetadataService
+
+
+def test_metadataservice_get_success(metadata_service: MetadataService):
+    print("aaa")
+    assert False
+
+
+def test_metadataservice_get_error():
+    assert False
+
+
+# write test for metadata service .get function
