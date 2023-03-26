e954f34e79a31a53fcd9e7b29254d2388d3a81b5
Oleksandr Lytvyn <letvyk99@gmail.com>
2023-03-26 23:29:09 +0200

    added unit tests

---
+++ packages/syft/tests/syft/dataset/fixtures.py
@@ -0,0 +1,21 @@
+# third party
+import pytest
+
+# syft absolute
+from syft.core.node.new.dataset import Dataset
+from syft.core.node.new.dataset_stash import DatasetStash
+from syft.core.node.new.document_store import BaseStash
+from syft.core.node.new.document_store import BaseUIDStoreStash
+from syft.core.node.new.uid import UID
+
+
+@pytest.fixture
+def dataset_stash(document_store) -> DatasetStash:
+    return DatasetStash(store=document_store)
+
+@pytest.fixture
+def empty_dataset(dataset_stash):
+    return  Dataset(
+        id=UID(), 
+        name='test_dataset',
+    )
