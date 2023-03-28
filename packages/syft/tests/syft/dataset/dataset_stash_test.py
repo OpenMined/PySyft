8340e97dbb4ca226a754fe1a87a805d3100425d6
Oleksandr Lytvyn <letvyk99@gmail.com>
2023-03-28 20:43:35 +0200

    inter commit

--- packages/syft/tests/syft/dataset/dataset_stash_test.py
+++ packages/syft/tests/syft/dataset/dataset_stash_test.py
@@ -3,6 +3,7 @@ from typing import List
 
 # syft absolute
 from syft.core.node.new.dataset import Dataset
+from syft.core.node.new.dataset import DatasetUpdate
 from syft.core.node.new.dataset_stash import ActionIDsPartitionKey
 from syft.core.node.new.dataset_stash import NamePartitionKey
 from syft.core.node.new.document_store import QueryKey
@@ -10,34 +11,47 @@ from syft.core.node.new.document_store import QueryKeys
 from syft.core.node.new.uid import UID
 
 
-def test_dataset_namepartitionkey(dataset_stash):
+def test_dataset_namepartitionkey():
     name_partition_key = QueryKeys(qks=NamePartitionKey).all[0]
     assert isinstance(name_partition_key, QueryKey)
     assert name_partition_key.key == "name"
     assert name_partition_key.type_ == str
 
 
-def test_dataset_actionidpartitionkey(dataset_stash):
+def test_dataset_actionidpartitionkey():
     action_ids_partition_key = QueryKeys(qks=ActionIDsPartitionKey).all[0]
     assert isinstance(action_ids_partition_key, QueryKey)
     assert action_ids_partition_key.key == "action_ids"
     assert action_ids_partition_key.type_ == List[UID]
 
 
-def test_dataset_get_by_name(dataset_stash, empty_dataset):
-    partitions = dataset_stash.set(empty_dataset)
+def test_dataset_get_by_name(mock_dataset_stash, mock_dataset):
+    # retrieving existing dataset
+    result = mock_dataset_stash.get_by_name(mock_dataset.name)
+    assert result.is_ok(), f"Dataset could not be retrieved, result: {result}"
+    assert isinstance(result.ok(), Dataset)
+    assert result.ok().id == mock_dataset.id
 
-    print(type(partitions), partitions)
-
-    result = dataset_stash.get_by_name("empty_dataset")
-
-    print(type(result), result)
-    assert result.is_ok()
-    assert isinstance(result, Dataset)
+    # retrieving non-existing dataset
+    result = mock_dataset_stash.get_by_name("non_existing_dataset")
+    assert result.is_ok(), f"Dataset could not be retrieved, result: {result}"
+    assert result.ok() is None
 
 
-# def test_dataset_update():
-#     pass
+def test_dataset_update(mock_dataset_stash, mock_dataset, mock_dataset_update):
+    result = mock_dataset_stash.update(dataset_update=mock_dataset_update)
+    assert result.is_ok(), f"Dataset could not be retrieved, result: {result}"
+    assert isinstance(result.ok(), Dataset)
+    # assert mock_dataset.id == result.ok().id
+
+    other_obj = object() 
+    result = mock_dataset_stash.update(dataset_update=other_obj)
+    assert result.err(), f"Dataset was updated with non-DatasetUpdate object," \
+                         f"result: {result}"
+    
+
+
+
 
 # def test_dataset_serach_action_ids():
 #     pass
--- packages/syft/tests/syft/dataset/fixtures.py
+++ packages/syft/tests/syft/dataset/fixtures.py
@@ -3,18 +3,22 @@ import pytest
 
 # syft absolute
 from syft.core.node.new.dataset import Dataset
+from syft.core.node.new.dataset import DatasetUpdate
 from syft.core.node.new.dataset_stash import DatasetStash
 from syft.core.node.new.uid import UID
 
 
 @pytest.fixture
-def dataset_stash(document_store) -> DatasetStash:
+def mock_dataset_stash(document_store) -> DatasetStash:
     return DatasetStash(store=document_store)
 
 
 @pytest.fixture
-def empty_dataset(dataset_stash):
-    return Dataset(
-        id=UID(),
-        name="test_dataset",
-    )
+def mock_dataset(mock_dataset_stash):
+    mock_dataset = Dataset(id=UID(), name="test_dataset")
+    result = mock_dataset_stash.partition.set(mock_dataset)
+    return result.ok()
+
+@pytest.fixture
+def mock_dataset_update(mock_dataset):
+    return DatasetUpdate()
