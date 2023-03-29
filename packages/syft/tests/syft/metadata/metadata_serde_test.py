2d6d65e9c0685d2120e0abe1cccfc725027aec26
khoaguin <dkn.work@protonmail.com>
2023-03-29 15:11:46 +0700

    start writing tests for serder for classes in node_metadat.py

---
+++ packages/syft/tests/syft/metadata/metadata_serde_test.py
@@ -0,0 +1,10 @@
+def test_node_metadata_serde() -> None:
+    assert True
+
+
+def test_node_metadata_update() -> None:
+    assert True
+
+
+def test_node_metadata_json() -> None:
+    assert True
---
+++ packages/syft/tests/syft/metadata/metadata_test.py
@@ -0,0 +1,2 @@
+def test_check_version() -> None:
+    assert False
