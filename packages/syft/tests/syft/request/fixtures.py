234ed4212bfa78385af796f22999bcc8f5701a76
khoaguin <dkn.work@protonmail.com>
2023-04-03 14:12:53 +0700

    on writing unit tests for the RequestStash class in request_stash.py

--- packages/syft/tests/syft/request/fixtures.py
+++ packages/syft/tests/syft/request/fixtures.py
@@ -1,1 +1,11 @@
 # something here
+# third party
+import pytest
+
+# syft absolute
+from syft.core.node.new.request_stash import RequestStash
+
+
+@pytest.fixture
+def request_stash(document_store) -> RequestStash:
+    return RequestStash(store=document_store)
--- packages/syft/tests/syft/request/request_stash_test.py
+++ packages/syft/tests/syft/request/request_stash_test.py
@@ -1,4 +1,13 @@
-def test_requeststash_get_all_for_verify_key():
+# syft absolute
+from syft.core.node.new.request_stash import RequestStash
+from syft.core.node.new.request_stash import RequestingUserVerifyKeyPartitionKey
+
+
+def test_requeststash_get_all_for_verify_key(request_stash: RequestStash):
+    RequestingUserVerifyKeyPartitionKey.with_obj("test")
+    request_stash.get_all_for_verify_key(verify_key="test")
+    print(request_stash)
+
     print("Hello World")
     assert False
 
