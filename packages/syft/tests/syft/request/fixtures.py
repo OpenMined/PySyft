37b1d887fbf9734060e37d952a93fc4bfa66ccd2
khoaguin <dkn.work@protonmail.com>
2023-04-04 15:58:23 +0700

    on writing tests for RequestStash

--- packages/syft/tests/syft/request/fixtures.py
+++ packages/syft/tests/syft/request/fixtures.py
@@ -3,9 +3,28 @@
 import pytest
 
 # syft absolute
+from syft.core.node.new.client import SyftClient
+from syft.core.node.new.context import AuthedServiceContext
+from syft.core.node.new.document_store import DocumentStore
 from syft.core.node.new.request_stash import RequestStash
+from syft.core.node.new.user import User
+from syft.core.node.worker import Worker
 
 
 @pytest.fixture
-def request_stash(document_store) -> RequestStash:
+def request_stash(document_store: DocumentStore) -> RequestStash:
     return RequestStash(store=document_store)
+
+
+@pytest.fixture
+def authed_context(admin_user: User, worker: Worker) -> AuthedServiceContext:
+    return AuthedServiceContext(credentials=admin_user.verify_key, node=worker)
+
+
+@pytest.fixture
+def authed_context_client(
+    guest_domain_client: SyftClient, worker: Worker
+) -> AuthedServiceContext:
+    return AuthedServiceContext(
+        credentials=guest_domain_client.credentials.verify_key, node=worker
+    )
--- packages/syft/tests/syft/request/request_stash_test.py
+++ packages/syft/tests/syft/request/request_stash_test.py
@@ -1,15 +1,40 @@
+# stdlib
+
+# third party
+
 # syft absolute
+from syft.core.node.new.client import SyftClient
+from syft.core.node.new.credentials import SyftVerifyKey
 from syft.core.node.new.request_stash import RequestStash
-from syft.core.node.new.request_stash import RequestingUserVerifyKeyPartitionKey
+
+# def add_mock_requests(
+#     request_stash: RequestStash, worker: Worker, guest_domain_client
+# ) -> None:
+#     print(worker)
+#     print()
+#     print(guest_domain_client)
+#     print()
+#     print(guest_domain_client.credentials)
+
+#     request = SubmitRequest(changes=[CODE_EXECUTE])
+#     result = request_stash.set(request.to(Request, context=context))
 
 
-def test_requeststash_get_all_for_verify_key(request_stash: RequestStash):
-    RequestingUserVerifyKeyPartitionKey.with_obj("test")
-    request_stash.get_all_for_verify_key(verify_key="test")
-    print(request_stash)
+def test_requeststash_get_all_for_verify_key_no_requests(
+    request_stash: RequestStash,
+    guest_domain_client: SyftClient,
+) -> None:
+    # test when there are no requests from a client
 
-    print("Hello World")
-    assert False
+    verify_key: SyftVerifyKey = guest_domain_client.credentials.verify_key
+    requests = request_stash.get_all_for_verify_key(verify_key=verify_key)
+    assert requests.is_ok() is True
+    assert len(requests.ok()) == 0
+
+
+# def test_requeststash_get_all_for_verify_key_error
 
 
 #  def test_requeststash_get_all_for_status
+
+#  def test_requeststash_get_all_for_status_error
