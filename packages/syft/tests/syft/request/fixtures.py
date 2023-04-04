f0d865973ac98b77e4b080bb1abab0302c40f627
khoaguin <dkn.work@protonmail.com>
2023-04-04 17:25:51 +0700

    done testing when getting requests using verify key succeed

--- packages/syft/tests/syft/request/fixtures.py
+++ packages/syft/tests/syft/request/fixtures.py
@@ -5,6 +5,7 @@ import pytest
 # syft absolute
 from syft.core.node.new.client import SyftClient
 from syft.core.node.new.context import AuthedServiceContext
+from syft.core.node.new.credentials import SyftVerifyKey
 from syft.core.node.new.document_store import DocumentStore
 from syft.core.node.new.request_stash import RequestStash
 from syft.core.node.new.user import User
@@ -22,9 +23,8 @@ def authed_context(admin_user: User, worker: Worker) -> AuthedServiceContext:
 
 
 @pytest.fixture
-def authed_context_client(
+def authed_context_guest_domain_client(
     guest_domain_client: SyftClient, worker: Worker
 ) -> AuthedServiceContext:
-    return AuthedServiceContext(
-        credentials=guest_domain_client.credentials.verify_key, node=worker
-    )
+    verify_key: SyftVerifyKey = guest_domain_client.credentials.verify_key
+    return AuthedServiceContext(credentials=verify_key, node=worker)
--- packages/syft/tests/syft/request/request_stash_test.py
+++ packages/syft/tests/syft/request/request_stash_test.py
@@ -4,20 +4,11 @@
 
 # syft absolute
 from syft.core.node.new.client import SyftClient
+from syft.core.node.new.context import AuthedServiceContext
 from syft.core.node.new.credentials import SyftVerifyKey
+from syft.core.node.new.request import Request
+from syft.core.node.new.request import SubmitRequest
 from syft.core.node.new.request_stash import RequestStash
-
-# def add_mock_requests(
-#     request_stash: RequestStash, worker: Worker, guest_domain_client
-# ) -> None:
-#     print(worker)
-#     print()
-#     print(guest_domain_client)
-#     print()
-#     print(guest_domain_client.credentials)
-
-#     request = SubmitRequest(changes=[CODE_EXECUTE])
-#     result = request_stash.set(request.to(Request, context=context))
 
 
 def test_requeststash_get_all_for_verify_key_no_requests(
@@ -32,9 +23,40 @@ def test_requeststash_get_all_for_verify_key_no_requests(
     assert len(requests.ok()) == 0
 
 
+def test_requeststash_get_all_for_verify_key_success(
+    request_stash: RequestStash,
+    guest_domain_client: SyftClient,
+    authed_context_guest_domain_client: AuthedServiceContext,
+) -> None:
+    # test when there is one request
+    submit_request: SubmitRequest = SubmitRequest(changes=[])
+    stash_set_result = request_stash.set(
+        submit_request.to(Request, context=authed_context_guest_domain_client)
+    )
+
+    verify_key: SyftVerifyKey = guest_domain_client.credentials.verify_key
+    requests = request_stash.get_all_for_verify_key(verify_key)
+
+    assert requests.is_ok() is True
+    assert len(requests.ok()) == 1
+    assert requests.ok()[0] == stash_set_result.ok()
+
+    # add another request
+    submit_request_2: SubmitRequest = SubmitRequest(changes=[])
+    stash_set_result_2 = request_stash.set(
+        submit_request_2.to(Request, context=authed_context_guest_domain_client)
+    )
+    requests = request_stash.get_all_for_verify_key(verify_key)
+
+    assert requests.is_ok() is True
+    assert len(requests.ok()) == 2
+    assert requests.ok()[1] == stash_set_result_2.ok()
+
+
 # def test_requeststash_get_all_for_verify_key_error
 
 
 #  def test_requeststash_get_all_for_status
+
 
 #  def test_requeststash_get_all_for_status_error
