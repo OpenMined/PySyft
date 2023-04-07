a0cc22dab359117ea0f566bb222ec31cf7e7a74e
khoaguin <dkn.work@protonmail.com>
2023-04-07 14:54:41 +0700

    done testing RequestStash

--- packages/syft/tests/syft/request/fixtures.py
+++ packages/syft/tests/syft/request/fixtures.py
@@ -8,18 +8,12 @@ from syft.core.node.new.context import AuthedServiceContext
 from syft.core.node.new.credentials import SyftVerifyKey
 from syft.core.node.new.document_store import DocumentStore
 from syft.core.node.new.request_stash import RequestStash
-from syft.core.node.new.user import User
 from syft.core.node.worker import Worker
 
 
 @pytest.fixture
 def request_stash(document_store: DocumentStore) -> RequestStash:
     return RequestStash(store=document_store)
-
-
-@pytest.fixture
-def authed_context(admin_user: User, worker: Worker) -> AuthedServiceContext:
-    return AuthedServiceContext(credentials=admin_user.verify_key, node=worker)
 
 
 @pytest.fixture
--- packages/syft/tests/syft/request/request_stash_test.py
+++ packages/syft/tests/syft/request/request_stash_test.py
@@ -1,14 +1,20 @@
 # stdlib
 
 # third party
+from pytest import MonkeyPatch
+from result import Err
 
 # syft absolute
 from syft.core.node.new.client import SyftClient
 from syft.core.node.new.context import AuthedServiceContext
 from syft.core.node.new.credentials import SyftVerifyKey
+from syft.core.node.new.document_store import QueryKeys
 from syft.core.node.new.request import Request
+from syft.core.node.new.request import RequestStatus
 from syft.core.node.new.request import SubmitRequest
 from syft.core.node.new.request_stash import RequestStash
+from syft.core.node.new.request_stash import RequestingUserVerifyKeyPartitionKey
+from syft.core.node.new.request_stash import StatusPartitionKey
 
 
 def test_requeststash_get_all_for_verify_key_no_requests(
@@ -57,23 +63,112 @@ def test_requeststash_get_all_for_verify_key_success(
     )
 
 
-# def test_requeststash_get_all_for_verify_key_error(
-#     request_stash: RequestStash,
-#     monkeypatch: MonkeyPatch,
-# ) -> None:
-#     qk = "Query Key"
-#     Err(
-#         f"{qk} not in {type(request_stash.store.partition)} unique or searchable keys"
-#     )
+def test_requeststash_get_all_for_verify_key_fail(
+    request_stash: RequestStash,
+    monkeypatch: MonkeyPatch,
+    guest_domain_client: SyftClient,
+) -> None:
+    verify_key: SyftVerifyKey = guest_domain_client.credentials.verify_key
+    mock_error_message = (
+        "verify key not in the document store's unique or searchable keys"
+    )
 
-# def mock_stash_get_all_error() -> Err:
-#     return Err(mock_error_message)
+    def mock_query_all_error(qks: QueryKeys) -> Err:
+        return Err(mock_error_message)
 
-# monkeypatch.setattr(request_stash.stash, "get_all", mock_stash_get_all_error)
+    monkeypatch.setattr(request_stash, "query_all", mock_query_all_error)
 
-# assert False
+    requests = request_stash.get_all_for_verify_key(verify_key)
 
-#  def test_requeststash_get_all_for_status
+    assert requests.is_err() is True
+    assert requests.err() == mock_error_message
 
 
-#  def test_requeststash_get_all_for_status_error
+def test_requeststash_get_all_for_verify_key_find_index_fail(
+    request_stash: RequestStash,
+    monkeypatch: MonkeyPatch,
+    guest_domain_client: SyftClient,
+) -> None:
+    verify_key: SyftVerifyKey = guest_domain_client.credentials.verify_key
+    qks = QueryKeys(qks=[RequestingUserVerifyKeyPartitionKey.with_obj(verify_key)])
+
+    mock_error_message = f"Failed to query index or search with {qks.all[0]}"
+
+    def mock_find_index_or_search_keys_error(
+        index_qks: QueryKeys, search_qks: QueryKeys
+    ) -> Err:
+        return Err(mock_error_message)
+
+    monkeypatch.setattr(
+        request_stash.partition,
+        "find_index_or_search_keys",
+        mock_find_index_or_search_keys_error,
+    )
+
+    requests = request_stash.get_all_for_verify_key(verify_key)
+
+    assert requests.is_err() is True
+    assert requests.err() == mock_error_message
+
+
+def test_requeststash_get_all_for_status_pending(
+    request_stash: RequestStash,
+    authed_context_guest_domain_client: AuthedServiceContext,
+) -> None:
+    submit_request = SubmitRequest(changes=[])
+    request_stash.set(
+        submit_request.to(Request, context=authed_context_guest_domain_client)
+    )
+    pending_status = RequestStatus.PENDING
+    queried_requests = request_stash.get_all_for_status(pending_status)
+    queried_requests = queried_requests.ok()
+
+    assert len(queried_requests) == 1
+    assert type(queried_requests[0]) == Request
+    assert queried_requests[0].status == pending_status
+
+
+def test_requeststash_get_all_for_status_approved(
+    request_stash: RequestStash,
+    authed_context_guest_domain_client: AuthedServiceContext,
+) -> None:
+    # test when there is one request with APPROVED status.
+    # this is similar to when the status is REJECTED.
+    submit_request = SubmitRequest(changes=[])
+    request = submit_request.to(Request, context=authed_context_guest_domain_client)
+    status = RequestStatus.APPROVED  # can also be REJECTED
+    request.status = status
+    stash_set_result = request_stash.set(request)
+    assert stash_set_result.ok().status == status
+
+    queried_requests = request_stash.get_all_for_status(status)
+    queried_requests = queried_requests.ok()
+
+    assert len(queried_requests) == 1
+    assert type(queried_requests[0]) == Request
+    assert queried_requests[0].status == status
+
+
+def test_requeststash_get_all_for_status_fail(
+    request_stash: RequestStash,
+    monkeypatch: MonkeyPatch,
+) -> None:
+    pending_status = RequestStatus.PENDING
+    qks = QueryKeys(qks=[StatusPartitionKey.with_obj(pending_status)])
+    mock_error_message = f"Failed to query index or search with {qks.all[0]}"
+
+    def mock_find_index_or_search_keys_error(
+        index_qks: QueryKeys, search_qks: QueryKeys
+    ) -> Err:
+        return Err(mock_error_message)
+
+    monkeypatch.setattr(
+        request_stash.partition,
+        "find_index_or_search_keys",
+        mock_find_index_or_search_keys_error,
+    )
+
+    requests = request_stash.get_all_for_status(pending_status)
+
+    assert requests.is_err() is True
+    assert requests.err() == mock_error_message
