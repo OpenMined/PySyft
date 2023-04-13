bb28b264074fd65c69e6efa82645577de58620e9
Antti Kalliokoski <antti.a.kalliokoski@gmail.com>
2023-04-13 15:36:36 +0300

    Initial haiku L2 script

---
+++ notebooks/haiku/L2/scrap.py
@@ -0,0 +1,35 @@
+# syft absolute
+import syft as sy
+
+sy.requires(">=0.8-beta")
+
+# third party
+import jax
+import jax.numpy as jnp
+
+# syft absolute
+from syft.core.node.new.jax import DeviceArrayObject
+
+# from syft import jax
+
+x = jnp.arange(10)
+y = jnp.ones(10)
+
+# syft absolute
+from syft.core.node.new.action_object import ActionObject
+
+action_object = ActionObject.from_obj(x)
+
+
+def f(x):
+    if len(x) < 4:
+        return x
+    else:
+        return 2 * x
+
+
+f_jit = jax.jit(f)
+# print("No wrapper", f_jit(x))
+print("Wrapper", f_jit(action_object))
+print("No wrapper jnp", jnp.add(x, x))
+print("wrapper jnp", jnp.add(action_object, action_object))
