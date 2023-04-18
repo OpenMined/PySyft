fb2fd8bb7db79b9574254bacc7faa73980186b29
teo <teo.milea97@gmail.com>
2023-04-18 13:00:25 +0300

    added _device to dont wrap attrs

--- packages/syft/src/syft/core/node/new/jax.py
+++ packages/syft/src/syft/core/node/new/jax.py
@@ -29,18 +29,32 @@ class DeviceArrayObjectPointer:
 #         return self.syft_action_data.id
 
 @serializable()
-class DeviceArrayObject(ActionObject):
+class DeviceArrayObject(ActionObject, np.lib.mixins.NDArrayOperatorsMixin):
     __canonical_name__ = "DeviceArrayObject"
     __version__ = SYFT_OBJECT_VERSION_1
     
     syft_internal_type: ClassVar[Type[Any]] = DeviceArray
     syft_pointer_type = DeviceArrayObjectPointer
     syft_passthrough_attrs = ["__jax_array__"]
-    syft_dont_wrap_attrs = ["__jax_array__", "device", "devices"]
+    syft_dont_wrap_attrs = ["__jax_array__", "device", "devices", "dtype", "shape", "_device"]
          
     def __jax_array__(self) -> DeviceArray:
         self.syft_action_data._device = jax.devices()[0]
         return self.syft_action_data
+
+    def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
+        inputs = tuple(
+            x.syft_action_data
+            if isinstance(x, DeviceArrayObject)
+            else x
+            for x in inputs
+        )
+
+        result = getattr(ufunc, method)(*inputs, **kwargs)
+        if type(result) is tuple:
+            return tuple(DeviceArrayObject(syft_action_data=x) for x in result)
+        else:
+            return DeviceArrayObject(syft_action_data=result)
 
 action_types[DeviceArray] = DeviceArrayObject
     
