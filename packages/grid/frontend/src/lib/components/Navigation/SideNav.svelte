26c68e13000baa3d51438770c20d6a28f18d4d7f
Thiago Costa Porto <thiagocostaporto@gmail.com>
2023-04-19 08:45:08 +0100

    feat: update navigation component for app pages

--- packages/grid/frontend/src/lib/components/Navigation/SideNav.svelte
+++ packages/grid/frontend/src/lib/components/Navigation/SideNav.svelte
@@ -9,7 +9,9 @@
   };
 </script>
 
-<nav class="flex flex-col w-[76px] md:w-72 h-screen fixed border-r border-gray-100">
+<nav
+  class="hidden tablet:flex flex-col w-[76px] desktop:w-72 h-screen fixed border-r border-gray-100"
+>
   <section
     class="relative flex flex-col justify-center items-center pt-6 px-8 pb-[46px] gap-3 flex-shrink-0"
   >
@@ -20,7 +22,7 @@
       <div class="w-16">
         <Avatar initials="L" blackBackground />
       </div>
-      <p class="leading-[1.2] font-bold">{metadata.name}</p>
+      <p class="leading-[1.2] font-bold hidden desktop:inline-block">{metadata.name}</p>
     </div>
     <button class="text-sm text-gray-600 underline w-min">logout</button>
   </section>
@@ -30,14 +32,17 @@
   </section>
   <hr class="border-gray-100" />
   <section class="flex flex-col gap-16 flex-shrink-0 p-8 pt-10">
-    <a href="https://openmined.org" target="_blank" rel="noreferrer noopener">
-      <SideNavDOHandbook />
-    </a>
+    <SideNavDOHandbook />
     <a class="text-center" href="https://openmined.org" target="_blank" rel="noreferrer noopener">
+      <img
+        src="/assets/branded/logo.png"
+        alt="OpenMined"
+        class="w-6 h-6 object-contain desktop:hidden transform scale-[2.3]"
+      />
       <img
         src="/assets/branded/openmined.png"
         alt="Empowered by OpenMined"
-        class="w-full h-6 object-contain"
+        class="w-full h-6 object-contain hidden desktop:inline"
       />
     </a>
   </section>
--- packages/grid/frontend/src/lib/components/Navigation/TopNav.svelte
+++ packages/grid/frontend/src/lib/components/Navigation/TopNav.svelte
@@ -10,10 +10,15 @@
   ];
 </script>
 
-<div class="w-full py-2 px-6 flex justify-end">
+<div
+  class="w-full py-2 px-6 flex items-center justify-between tablet:justify-end shadow-topbar-1 tablet:shadow-none"
+>
+  <div class="w-12 h-12 tablet:hidden">
+    <Avatar initials="L" blackBackground smallText />
+  </div>
   <ul class="flex w-min items-center text-primary-500">
     {#each links as link}
-      <li class="flex items-center justify-center w-13 h-13">
+      <li class="items-center justify-center w-13 h-13 hidden tablet:flex">
         <a class="block w-6 h-6" href={link.href} target="_blank" rel="noopener noreferrer">
           <svelte:component this={link.icon} />
         </a>
