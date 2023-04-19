0ee0c6676ba35f98d2c4887bd3aa813c3a739b47
Thiago Costa Porto <thiagocostaporto@gmail.com>
2023-04-19 07:12:38 +0100

    refactor: break login and metadata calls away from jsClient

---
+++ packages/grid/frontend/src/lib/api/metadata.ts
@@ -0,0 +1,15 @@
+import ky from 'ky';
+import { deserialize } from './deserializer';
+import { API_BASE_URL } from '../constants';
+
+export async function getMetadata() {
+  try {
+    const res = await ky.get(`${API_BASE_URL}/new/metadata_capnp`);
+    const metadata = await deserialize(res);
+    window.localStorage.setItem('metadata', JSON.stringify(metadata));
+    window.localStorage.setItem('nodeId', metadata?.id?.value);
+    return metadata;
+  } catch (error) {
+    console.log(error);
+  }
+}
