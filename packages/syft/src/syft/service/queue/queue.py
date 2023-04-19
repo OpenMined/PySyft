d45f050367b336e6ee32b749ebea921c9e03fa46
Shubham Gupta <shubhamgupta3121@gmail.com>
2023-04-20 00:27:37 +0530

    refactor Proxy to create a QueueServer - use green thread safe zmq - start QueueServer in a thread - add option to start subscriber in a thread - add options to close/kill thread - add a logger to QueueServer

--- packages/syft/src/syft/service/queue/queue.py
+++ packages/syft/src/syft/service/queue/queue.py
@@ -1,23 +1,27 @@
-# Espresso Pattern
-
 # stdlib
 import binascii
 import os
-from threading import Thread
+from typing import Optional
 
 # third party
-import zmq
-from zmq.devices import monitored_queue
+import gevent
+from zmq import Context
+from zmq import Socket
+import zmq.green as zmq
+
+# relative
+from ...serde.deserialize import _deserialize as deserialize
 
 PUBLISHER_PORT = 6000
 SUBSCRIBER_PORT = 6001
 
 
 class Publisher:
-    def __init__(self, port: int, address: str = "tcp://*") -> None:
+    def __init__(self, address: str) -> None:
         ctx = zmq.Context.instance()
+        self.address = address
         self._publisher = ctx.socket(zmq.PUB)
-        self._publisher.bind("tcp://*:6000")
+        self._publisher.bind(address)
 
     def send(self, message: bytes):
         try:
@@ -35,86 +39,142 @@ class Publisher:
 
 
 class Subscriber:
-    def __init__(self, port: int, address: str = "tcp://localhost") -> None:
+    def __init__(self, address: str, prefix: str = "") -> None:
         ctx = zmq.Context.instance()
         self._subscriber = ctx.socket(zmq.SUB)
-        self._subscriber.connect("tcp://localhost:6001")
-        self._subscriber.setsockopt(zmq.SUBSCRIBE, b"")
+        self.address = address
+        self._subscriber.connect(address)
+        self._subscriber.setsockopt_string(zmq.SUBSCRIBE, prefix)
 
-    def receive(self):
-        # TODO: make this non blocking by running it in a thread
+    @staticmethod
+    def _receive(subscriber: Socket):
         try:
-            message = self._subscriber.recv_multipart()
-            print("HEllo message recieved: ", message)
+            message = subscriber.recv_multipart()
+            print("HEllo message received: ", message)
         except zmq.ZMQError as e:
             if e.errno == zmq.ETERM:
                 print("Subscriber connection Terminated")
             else:
                 raise e
 
+    def receive(self, blocking: bool = False):
+        # TODO: make this non blocking by running it in a thread
+        if blocking:
+            return self._receive(self._subscriber)
+
+        self.recv_thread = gevent.spawn(self._receive, self._subscriber)
+        self.recv_thread.start()
+
+    def process_syft_msg(message: bytes):
+        deserialize(message, from_bytes=True)
+        # TODO: Perform the API calls over here.
+
     def close(self):
+        gevent.sleep(0)
+        self.recv_thread.kill()
         self._subscriber.close()
 
 
-def zpipe(ctx):
-    """build inproc pipe for talking to threads
-    mimic pipe used in czmq zthread_fork.
-    Returns a pair of PAIRs connected via inproc
-    """
-    a = ctx.socket(zmq.PAIR)
-    b = ctx.socket(zmq.PAIR)
-    a.linger = b.linger = 0
-    a.hwm = b.hwm = 1
-    iface = "inproc://%s" % binascii.hexlify(os.urandom(8))
-    a.bind(iface)
-    b.connect(iface)
-    return a, b
-
-
-def listener_thread(pipe):
-    # Print everything that arrives on pipe
-    while True:
-        try:
-            print(pipe.recv_multipart())
-        except zmq.ZMQError as e:
-            if e.errno == zmq.ETERM:
-                break  # Interrupted
-
-
-def run_mon_queue(queue, *args, **kwargs):
-    try:
-        queue(*args, **kwargs)
-    except Exception as e:
-        raise e
-
-
-class Proxy:
-    def __init__(self, address: str, pub_port: int, sub_port: int):
-        self.address = address
-        self.pub_port = pub_port
-        self.sub_port = sub_port
+class QueueServer:
+    def __init__(self, pub_addr: str, sub_addr: str):
+        self.pub_addr = pub_addr
+        self.sub_addr = sub_addr
         self.context = zmq.Context.instance()
+        self.logger_thread = None
+        self.thread = None
+
+    @staticmethod
+    def _setup_monitor(ctx: Context):
+        mon_addr = "inproc://%s" % binascii.hexlify(os.urandom(8))
+        mon_pub = ctx.socket(zmq.PAIR)
+        mon_sub = ctx.socket(zmq.PAIR)
+
+        mon_sub.linger = mon_sub.linger = 0
+
+        mon_sub.hwm = mon_sub.hwm = 1
+        mon_pub.bind(mon_addr)
+        mon_sub.connect(mon_addr)
+        return mon_pub, mon_sub, mon_addr
+
+    def _setup_connections(self):
         self.xsub = self.context.socket(zmq.XSUB)
         self.xpub = self.context.socket(zmq.XPUB)
 
-    def _init_connection(self):
-        self.xsub.connect("tcp://localhost:6000")
-        self.xpub.bind("tcp://*:6001")
-        pipe = zpipe(self.context)
-        l_thread = Thread(target=listener_thread, args=(pipe[1],))
-        l_thread.start()
-        self.monitored_thread = Thread(
-            target=run_mon_queue,
-            args=(monitored_queue, self.xpub, self.xsub, pipe[0], b"pub", b"sub"),
-            daemon=True,
+        self.xsub.connect(self.pub_addr)
+        self.xpub.bind(self.sub_addr)
+
+        self.mon_pub, self.mon_sub, self.mon_addr = self._setup_monitor(self.context)
+
+    @staticmethod
+    def _start_logger(mon_sub: Socket):
+        print("Logging...")
+        while True:
+            try:
+                message_bytes = mon_sub.recv_multipart()
+                message_str = " ".join(mess.decode() for mess in message_bytes)
+                print(message_str)
+            except zmq.ZMQError as e:
+                if e.errno == zmq.ETERM:
+                    break  # Interrupted
+
+    @staticmethod
+    def _start(
+        in_socket: Socket,
+        out_socket: Socket,
+        mon_socket: Socket,
+        in_prefix: bytes,
+        out_prefix: bytes,
+    ):
+        poller = zmq.Poller()
+        poller.register(in_socket, zmq.POLLIN)
+        poller.register(out_socket, zmq.POLLIN)
+
+        while True:
+            events = dict(poller.poll())
+
+            if in_socket in events:
+                message = in_socket.recv_multipart()
+                out_socket.send_multipart(message)
+                mon_socket.send_multipart([in_prefix] + message)
+
+            if out_socket in events:
+                message = out_socket.recv_multipart()
+                in_socket.send_multipart(message)
+                mon_socket.send_multipart([out_prefix] + message)
+
+    def start(self, in_prefix: bytes = b"pub", out_prefix: bytes = b"sub"):
+        self._setup_connections()
+        self.logger_thread = gevent.spawn(self._start_logger, self.mon_sub)
+        self.thread = gevent.spawn(
+            self._start,
+            self.xpub,
+            self.xsub,
+            self.mon_pub,
+            in_prefix,
+            out_prefix,
         )
+
+        self.logger_thread.start()
+        self.thread.start()
+
+    @staticmethod
+    def create(pub_addr: str, sub_addr: str):
+        return QueueServer(pub_addr=pub_addr, sub_addr=sub_addr)
+
+    def check_logs(self, timeout: Optional[int]):
         try:
-            self.monitored_thread.start()
-        except Exception as e:
-            raise e
+            if self.logger_thread:
+                self.logger_thread.join(timeout=timeout)
+        except KeyboardInterrupt:
+            pass
 
     def close(self):
         # self.monitored_thread.exit()
+        gevent.sleep(0)
+        self.thread.kill()
+        self.logger_thread.kill()
         self.xpub.close()
         self.xpub.close()
+        self.mon_pub.close()
+        self.mon_sub.close()
         self.context.destroy()
