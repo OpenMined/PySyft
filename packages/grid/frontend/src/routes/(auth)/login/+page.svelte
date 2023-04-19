0017234755936e4884072ee3dfd6ace9dc0221be
Thiago Costa Porto <thiagocostaporto@gmail.com>
2023-04-19 07:26:44 +0100

    fix: remove await from login and base layout for auth

--- packages/grid/frontend/src/routes/(auth)/login/+page.svelte
+++ packages/grid/frontend/src/routes/(auth)/login/+page.svelte
@@ -1,69 +1,88 @@
 <script lang="ts">
+  import { metadata as _metadata } from '$lib/store';
+  import { onMount } from 'svelte';
+  import { getMetadata } from '$lib/api/metadata';
+  import { login } from '$lib/api/login';
   import Button from '$lib/components/Button.svelte';
   import Modal from '$lib/components/Modal.svelte';
   import DomainMetadataPanel from '$lib/components/authentication/DomainMetadataPanel.svelte';
   import Input from '$lib/components/Input.svelte';
   import DomainOnlineIndicator from '$lib/components/DomainOnlineIndicator.svelte';
-  import { getClient } from '$lib/store';
-  import { goto } from '$app/navigation';
   import type { DomainOnlineStatus } from '../../../types/domain/onlineIndicator';
-  let status: DomainOnlineStatus = 'online';
 
-  async function login({ email, password }, client) {
-    await client
-      .login(email.value, password.value)
-      .then(() => {
-        goto('/home');
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  let status: DomainOnlineStatus = 'online';
+  let email = '';
+  let password = '';
+
+  let metadata = {};
+
+  async function handleSubmit() {
+    await login({ email, password })
+      .then((res) => console.log(res))
+      .catch((error) => console.log(error));
   }
+
+  onMount(async () => {
+    metadata = await getMetadata();
+  });
+
+  _metadata.subscribe((value) => {
+    metadata = value;
+  });
 </script>
 
 <div class="flex flex-col xl:flex-row w-full h-full xl:justify-around items-center gap-12">
-  {#await getClient() then client}
-    {#await client.metadata then metadata}
-      <DomainMetadataPanel {metadata} />
-      <form class="contents" on:submit|preventDefault={(e) => login(e.target, client)}>
-        <Modal maxWidth="max-w-[681px]">
-          <div
-            class="flex flex-shrink-0 justify-between p-4 pb-0 flex-nowrap w-full h-min"
-            slot="header"
-          >
-            <span class="block text-center w-full">
-              <p class="text-2xl font-bold text-gray-800">Welcome</p>
-            </span>
-          </div>
-          <div class="contents" slot="body">
-            <div class="flex justify-center items-center gap-2">
-              <DomainOnlineIndicator />
-              <p class="text-600">
-                {#if status === 'pending'}
-                  Checking connection
-                {:else}
-                  Domain {status}
-                {/if}
-              </p>
-            </div>
-            <Input
-              label="Email"
-              type="email"
-              id="email"
-              placeholder="info@openmined.org"
-              required
-            />
-            <Input label="Password" type="password" id="password" placeholder="******" required />
-            <p class="text-center">
-              Don't have an account yet? Apply for an account <a
-                href="/signup"
-                class="text-primary-600 underline hover:opacity-50">here</a
-              >.
+  <DomainMetadataPanel {metadata} />
+  <form class="contents" on:submit|preventDefault={handleSubmit}>
+    <section class="w-full max-w-[681px]">
+      <Modal>
+        <div
+          class="flex flex-shrink-0 justify-between p-4 pb-0 flex-nowrap w-full h-min"
+          slot="header"
+        >
+          <span class="block text-center w-full">
+            <p class="text-2xl font-bold text-gray-800">Welcome</p>
+          </span>
+        </div>
+        <div class="contents" slot="body">
+          <div class="flex justify-center items-center gap-2">
+            <DomainOnlineIndicator />
+            <p class="text-600">
+              {#if status === 'pending'}
+                Checking connection
+              {:else}
+                Domain {status}
+              {/if}
             </p>
           </div>
-          <Button variant="secondary" slot="button-group">Login</Button>
-        </Modal>
-      </form>
-    {/await}
-  {/await}
+          <Input
+            label="Email"
+            type="email"
+            id="email"
+            placeholder="info@openmined.org"
+            bind:value={email}
+            required
+          />
+          <Input
+            label="Password"
+            type="password"
+            id="password"
+            placeholder="******"
+            bind:value={password}
+            required
+          />
+          <p class="text-center">
+            Don't have an account yet? Apply for an account <a
+              href="/signup"
+              class="text-primary-600 underline hover:opacity-50"
+            >
+              here
+            </a>
+            .
+          </p>
+        </div>
+        <Button variant="secondary" slot="button-group">Login</Button>
+      </Modal>
+    </section>
+  </form>
 </div>
