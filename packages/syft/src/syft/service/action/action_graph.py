839be977a5d8dce4979f226a7b6f5733e046e75c
Shubham Gupta <shubhamgupta3121@gmail.com>
2023-04-21 00:43:55 +0530

    add CRUD methods to InMemoryActionGraphStore fix arguments and return types for NetworkXBackingStore define CRUD method to ActionGraphService

--- packages/syft/src/syft/service/action/action_graph.py
+++ packages/syft/src/syft/service/action/action_graph.py
@@ -7,11 +7,15 @@ from typing import Any
 from typing import Iterable
 from typing import List
 from typing import Optional
+from typing import Set
 from typing import Type
 
 # third party
 import networkx as nx
 import pydantic
+from result import Err
+from result import Ok
+from result import Result
 from typing_extensions import Self
 
 # relative
@@ -76,16 +80,19 @@ class BaseGraphStore:
     graph_type: Any
     client_config: Optional[StoreClientConfig]
 
-    def set(self, node: Any) -> None:
+    def set(self, uid: Any, data: Any) -> None:
         raise NotImplementedError
 
-    def delete(self, node: Any) -> None:
+    def get(self, uid: Any) -> Any:
         raise NotImplementedError
 
-    def find_neighbors(self, node: Any) -> List[Any]:
+    def delete(self, uid: Any) -> None:
         raise NotImplementedError
 
-    def update(self, node: Any) -> None:
+    def find_neighbors(self, uid: Any) -> List[Any]:
+        raise NotImplementedError
+
+    def update(self, uid: Any, data: Any) -> None:
         raise NotImplementedError
 
     def add_edge(self, parent: Any, child: Any) -> None:
@@ -106,7 +113,10 @@ class BaseGraphStore:
     def save(self) -> None:
         raise NotImplementedError
 
-    def is_parent(self, parent: Any, child: Any):
+    def get_predecessors(self, uid: UID) -> List:
+        raise NotImplementedError
+
+    def exists(self, uid: Any) -> bool:
         raise NotImplementedError
 
 
@@ -124,7 +134,7 @@ class NetworkXBackingStore(BaseGraphStore):
         self.file_path = store_config.client_config.file_path
 
         if os.path.exists(self.file_path):
-            self._db = self.load_from_path(self.file_path)
+            self._db = self.load_from_path(str(self.file_path))
         else:
             self._db = nx.DiGraph()
 
@@ -132,46 +142,61 @@ class NetworkXBackingStore(BaseGraphStore):
     def db(self) -> nx.Graph:
         return self._db
 
-    def set(self, node: ActionGraphNode) -> None:
-        self.db.add_node(node)
+    def set(self, uid: UID, data: Any) -> None:
+        if self.exists(uid=uid):
+            self.update(uid=uid, data=data)
+        else:
+            self.db.add_node(uid, data=data)
 
-    def delete(self, node: ActionGraphNode) -> None:
-        self.graph.remove_node(node)
+    def get(self, uid: UID) -> Any:
+        return self.db.nodes.get(uid)
 
-    def find_neighbors(self, node: ActionGraphNode) -> List[ActionGraphNode]:
-        return self.graph.neighbors(node)
+    def delete(self, uid: UID) -> None:
+        if self.exists(uid=uid):
+            self.db.remove_node(uid)
 
-    def update(self, updated_node: ActionGraphNode) -> None:
-        self.graph.update(updated_node)
+    def find_neighbors(self, uid: UID) -> Optional[List]:
+        if self.exists(uid=uid):
+            neighbors = self.graph.neighbors(uid)
+            return neighbors
 
-    def add_edge(self, parent: ActionGraphNode, child: ActionGraphNode) -> None:
-        self.graph.add_edge(parent, child)
+    def update(self, uid: UID, data: Any) -> None:
+        if self.exists(uid=uid):
+            node_data = self.get(uid=uid)
+            node_data["data"] = data
 
-    def remove_edge(self, parent: ActionGraphNode, child: ActionGraphNode) -> None:
-        self.graph.remove_edge(parent, child)
+    def add_edge(self, parent: Any, child: Any) -> None:
+        self.db.add_edge(parent, child)
+
+    def remove_edge(self, parent: Any, child: Any) -> None:
+        self.db.remove_edge(parent, child)
 
     def visualize(self) -> None:
         return nx.draw_networkx(self.graph, with_labels=True)
 
     def nodes(self) -> Iterable:
-        return self.graph.nodes()
+        return self.db.nodes()
 
     def edges(self) -> Iterable:
-        return self.graph.edges()
+        return self.db.edges()
+
+    def get_predecessors(self, uid: UID) -> List:
+        return list(self.db.predecessors(uid))
+
+    def is_parent(self, parent: Any, child: Any) -> bool:
+        parents = self.graph.predecessors(child)
+        return parent in parents
 
     def save(self) -> None:
-        bytes = _serialize(self.graph, to_bytes=True)
+        bytes = _serialize(self.db, to_bytes=True)
         with open(str(self.path), "wb") as f:
             f.write(bytes)
 
-    def load_from_path(file_path: str) -> None:
+    @staticmethod
+    def _load_from_path(file_path: str) -> None:
         with open(file_path, "rb") as f:
             bytes = f.read()
         return _deserialize(blob=bytes, from_bytes=True)
-
-    def is_parent(self, parent: ActionGraphNode, child: ActionGraphNode) -> bool:
-        parents = list(self.graph.predecessors(child))
-        return parent in parents
 
 
 class InMemoryGraphConfig(StoreConfig):
@@ -186,20 +211,108 @@ class ActionGraphStore:
 
 class InMemoryActionGraphStore(ActionGraphStore):
     def __init__(self, store_config: StoreConfig):
-        self.store_config = store_config
-        self.graph = self.store_config.store_type(self.store_config)
+        self.store_config: StoreConfig = store_config
+        self.graph: Type[BaseGraphStore] = self.store_config.store_type(
+            self.store_config
+        )
 
-    def set(self, action: Action, credentials: SyftVerifyKey):
-        #     node = ActionGraphNode.from_action(action, credentials)
-        #     self._search_parents_for(node)
-        #     self.graph.set(node)
-        pass
+    def set(
+        self,
+        action: Action,
+        credentials: SyftVerifyKey,
+    ) -> Result[ActionGraphNode, str]:
+        node = ActionGraphNode.from_action(action, credentials)
 
-    def get(self, action: Action, credentials: SyftVerifyKey):
-        pass
+        if self.graph.exists(uid=action.id):
+            return Err(f"Action already exists in the graph: {action.id}")
 
-    def delete(self, action: Action, credentials: SyftVerifyKey):
-        pass
+        parents = self._search_parents_for(node)
+        self.graph.set(uid=node.id, data=node)
+        for parent in parents:
+            result = self.add_edge(
+                parent=parent,
+                child=node,
+                credentials=credentials,
+            )
+            if result.is_err():
+                return result
 
-    def update(self, action: Action, credentials: SyftVerifyKey):
-        pass
+        return Ok(node)
+
+    def get(
+        self,
+        uid: UID,
+        credentials: SyftVerifyKey,
+    ) -> Result[ActionGraphNode, str]:
+        # 🟡 TODO: Add permission check
+        node_data = self.graph.get(uid=uid)
+        return Ok(node_data)
+
+    def delete(
+        self,
+        uid: UID,
+        credentials: SyftVerifyKey,
+    ) -> Result[bool, str]:
+        # 🟡 TODO: Add permission checks
+        if self.graph.exists(uid=uid):
+            self.graph.delete(uid=uid)
+            return Ok(True)
+        return Err(f"Node does not exists with id: {uid}")
+
+    def update(
+        self,
+        uid: UID,
+        data: ActionGraphNode,
+        credentials: SyftVerifyKey,
+    ) -> Result[ActionGraphNode, str]:
+        # 🟡 TODO: Add permission checks
+        if self.graph.exists(uid=uid):
+            self.graph.update(uid=uid, data=data)
+            return Ok(data)
+        return Err(f"Node does not exists for uid: {uid}")
+
+    def add_edge(
+        self,
+        parent: UID,
+        child: UID,
+        credentials: SyftVerifyKey,
+    ) -> Result[bool, str]:
+        if not self.graph.exists(parent):
+            return Err(f"Node does not exists for uid: {parent}")
+
+        if not self.graph.exists(child):
+            return Err(f"Node does not exists for uid: {child}")
+
+        self.graph.add_edge(parent=parent, child=child)
+
+        return Ok(True)
+
+    def _search_parents_for(self, node: ActionGraphNode) -> Set:
+        input_ids = []
+        parents = set()
+        if node.action.remote_self:
+            input_ids.append(node.action.remote_self)
+        input_ids.extend(node.action.args)
+        input_ids.extend(node.action.kwargs.values())
+
+        # search for parents in the existing nodes
+        for _node in self.graph.nodes:
+            if _node.action.result_id in input_ids:
+                parents.add(_node)
+
+        return parents
+
+    def is_parent(self, parent: UID, child: UID) -> Result[bool, str]:
+        if self.graph.exists(child):
+            parents = self.graph.get_predecessors(child)
+            result = parent in parents
+            return Ok(result)
+        return Err(f"Node doesn't exists for id: {child}")
+
+    @property
+    def nodes(self) -> Result[List, str]:
+        return Ok(self.graph.nodes())
+
+    @property
+    def edges(self) -> Result[List, str]:
+        return Ok(self.graph.edges())
--- packages/syft/src/syft/service/action/action_graph_service.py
+++ packages/syft/src/syft/service/action/action_graph_service.py
@@ -1,11 +1,15 @@
-# third party
-from result import Result
+# stdlib
+from typing import List
+from typing import Union
 
 # relative
 from ...serde.serializable import serializable
 from ..context import AuthedServiceContext
+from ..response import SyftError
+from ..response import SyftSuccess
 from ..service import AbstractService
 from ..service import service_method
+from .action_graph import ActionGraphNode
 from .action_graph import ActionGraphStore
 from .action_object import Action
 
@@ -20,52 +24,32 @@ class ActionGraphService(AbstractService):
     @service_method(path="graph.add", name="add")
     def add_action(
         self, context: AuthedServiceContext, action: Action
-    ) -> Result[Action, str]:
-        self.store.set(credentials=context.credentials, action=action)
+    ) -> Union[ActionGraphNode, SyftError]:
+        result = self.store.set(credentials=context.credentials, action=action)
+        if result.is_err():
+            SyftError(message=result.err())
+        return result.ok()
 
-    # def add_action(self, action: Action) -> None:
-    #     # TODO: Handle Duplication
-    #     node = ActionGraphNode.from_action(action)
-    #     self._search_parents_for(node)
-    #     self.client.add_node(node)
+    def remove_action(
+        self, context: AuthedServiceContext, action: Action
+    ) -> Union[SyftSuccess, SyftError]:
+        result = self.store.delete(uid=action.id, credentials=context.credentials)
+        if result.is_ok():
+            return SyftSuccess(
+                f"Succesfully delete action with uid: {action.id} from graph"
+            )
 
-    # def _search_parents_for(self, node: ActionGraphNode) -> None:
-    #     input_ids = []
-    #     parents = set()
-    #     if node.action.remote_self:
-    #         input_ids.append(node.action.remote_self)
-    #     input_ids.extend(node.action.args)
-    #     input_ids.extend(node.action.kwargs.values())
-    #     # search for parents in the existing nodes
-    #     for _node in self.client.nodes:
-    #         if _node.action.result_id in input_ids:
-    #             parents.add(_node)
+        return SyftError(message=result.err())
 
-    #     for parent in parents:
-    #         self.client.add_edge(parent, node)
+    def get_all_nodes(self, context: AuthedServiceContext) -> Union[List, SyftError]:
+        result = self.store.nodes
+        if result.is_ok():
+            return result.ok()
 
-    # def remove_action(self, action: Action):
-    #     node = ActionGraphNode.from_action(action)
-    #     self.client.remove_node(node)
+        return SyftError(message="Failed to fetch nodes from the graph")
 
-    # def draw_graph(self):
-    #     return self.client.visualize()
-
-    # @property
-    # def nodes(self):
-    #     return self.client.nodes
-
-    # @property
-    # def edges(self):
-    #     return self.client.edges
-
-    # def is_parent(self, parent: Action, child: Action) -> bool:
-    #     parent_node: ActionGraphNode = ActionGraphNode.from_action(parent)
-    #     child_node: ActionGraphNode = ActionGraphNode.from_action(child)
-    #     return self.client.is_parent(parent_node, child_node)
-
-    # def save(self) -> None:
-    #     self.client.save()
-
-    # def load(self) -> None:
-    #     self.client.load()
+    def get_all_edges(self, context: AuthedServiceContext) -> Union[List, SyftError]:
+        result = self.store.edges
+        if result.is_ok():
+            return result.ok()
+        return SyftError(message="Failed to fetch nodes from the graph")
