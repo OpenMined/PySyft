6bbdfd69d96c3c80b3de554bb8cca77bf79dee29
Antti Kalliokoski <antti.a.kalliokoski@gmail.com>
2023-04-20 08:39:36 +0300

    Add jnp.ndarray DeviceArrayObject mapping

--- packages/syft/src/syft/core/node/new/jax.py
+++ packages/syft/src/syft/core/node/new/jax.py
@@ -1,6 +1,6 @@
 from typing import ClassVar
 from typing import Type
-from typing import Any 
+from typing import Any
 from jaxlib.xla_extension import DeviceArray
 from jaxlib.xla_extension import Device
 import numpy as np
@@ -13,14 +13,16 @@ from .syft_object import SYFT_OBJECT_VERSION_1
 from .action_types import action_types
 from .numpy import NumpyArrayObject
 
+
 class DeviceArrayObjectPointer:
     pass
+
 
 # @serializable()
 # class DeviceObject(ActionObject):
 #     __canonical_name__ = "DeviceObject"
 #     __version__ = SYFT_OBJECT_VERSION_1
-    
+
 #     syft_internal_type: ClassVar[Type[Any]] = Device
 #     syft_passthrough_attrs = ["id"]
 #     syft_dont_wrap_attrs = []
@@ -28,25 +30,31 @@ class DeviceArrayObjectPointer:
 #     def id(self):
 #         return self.syft_action_data.id
 
+
 @serializable()
 class DeviceArrayObject(ActionObject, np.lib.mixins.NDArrayOperatorsMixin):
     __canonical_name__ = "DeviceArrayObject"
     __version__ = SYFT_OBJECT_VERSION_1
-    
+
     syft_internal_type: ClassVar[Type[Any]] = DeviceArray
     syft_pointer_type = DeviceArrayObjectPointer
     syft_passthrough_attrs = ["__jax_array__"]
-    syft_dont_wrap_attrs = ["__jax_array__", "device", "devices", "dtype", "shape", "_device"]
-         
+    syft_dont_wrap_attrs = [
+        "__jax_array__",
+        "device",
+        "devices",
+        "dtype",
+        "shape",
+        "_device",
+    ]
+
     def __jax_array__(self) -> DeviceArray:
         self.syft_action_data._device = jax.devices()[0]
         return self.syft_action_data
 
     def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
         inputs = tuple(
-            x.syft_action_data
-            if isinstance(x, DeviceArrayObject)
-            else x
+            x.syft_action_data if isinstance(x, DeviceArrayObject) else x
             for x in inputs
         )
 
@@ -56,5 +64,6 @@ class DeviceArrayObject(ActionObject, np.lib.mixins.NDArrayOperatorsMixin):
         else:
             return DeviceArrayObject(syft_action_data=result)
 
+
 action_types[DeviceArray] = DeviceArrayObject
-    
+action_types[jnp.ndarray] = DeviceArrayObject
