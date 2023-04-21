ceb7a2aae1344a07e2e8a49c1c6a4807bbde722d
Ionesio Junior <ionesiojr@gmail.com>
2023-04-21 15:13:29 -0300

    Update UserUpdate/UserCreate and NodeMetadataUpdate object serde path

--- packages/grid/frontend/src/lib/client/jsclient/jsClient.svelte
+++ packages/grid/frontend/src/lib/client/jsclient/jsClient.svelte
@@ -152,7 +152,7 @@
       // Create a new object called 'newMetadata' with updated fields and a new property called 'fqn' with a value.
       const updateMetadata = {
         ...newMetadata,
-        fqn: 'syft.core.node.new.node_metadata.NodeMetadataUpdate'
+        fqn: 'syft.service.metadata.node_metadata.NodeMetadataUpdate'
       };
 
       // Create a new object called 'reqFields' with one property: 'metadata',  which is set to 'updateMetadata'.
@@ -175,7 +175,7 @@
     register(newUser) {
       return (async () => {
         // Create a register payload object by copying the newUser object and adding a fully-qualified name (fqn) property.
-        const registerPayload = { ...newUser, fqn: 'syft.core.node.new.user.UserCreate' };
+        const registerPayload = { ...newUser, fqn: 'syft.service.user.user.UserCreate' };
 
         // Make a POST request to the server with the register payload.
         const response = await fetch(`${this.url}/api/v1/new/register`, {
@@ -209,8 +209,8 @@
      * */
     updateCurrentUser(updatedFields) {
       // Create a new object called 'userUpdate' with updated fields and a new property called 'fqn' with a value.
-      const userUpdate = { ...updatedFields, fqn: 'syft.core.node.new.user.UserUpdate' };
-
+      const userUpdate = { ...updatedFields, fqn: 'syft.service.user.user.UserUpdate' };
+      
       // Create a new object called 'reqFields' with two properties: 'uid', which is set to the value of 'userId', and 'user_update', which is set to 'userUpdate'.
       const reqFields = { uid: this.userId, user_update: userUpdate };
 
