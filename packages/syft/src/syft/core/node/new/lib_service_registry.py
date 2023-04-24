2393b7c8103a97cfc5134d2c055dcf572cedf97e
Koen van der Veen <koenlennartvanderveen@gmail.com>
2023-04-24 11:41:04 +0200

    small fixes in tests

--- packages/syft/src/syft/core/node/new/lib_service_registry.py
+++ packages/syft/src/syft/core/node/new/lib_service_registry.py
@@ -343,11 +343,3 @@ action_execute_registry_libs = CMPTree(
         ),
     ]
 ).build()
-
-
-# function_signatures_registry = {
-#     "concatenate": "concatenate(a1,a2, *args,axis=0,out=None,dtype=None,casting='same_kind')",
-#     "set_numeric_ops": "set_numeric_ops(op1=func1,op2=func2, *args)",
-#     "geterrorobj": "geterrobj()",
-#     "source": "source(object, output)",
-# }
