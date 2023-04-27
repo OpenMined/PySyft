63f400db295c19bbfc0bea129b236d8d209001a1
Thiago Costa Porto <thiagocostaporto@gmail.com>
2023-04-27 03:47:43 +0100

    feat: update utils by removing dead functions and a format call

--- packages/grid/frontend/src/lib/utils.ts
+++ packages/grid/frontend/src/lib/utils.ts
@@ -1,27 +1,5 @@
+import { onDestroy } from 'svelte';
 import { ServiceRoles } from '../types/domain/users';
-
-export function prettyName(name) {
-  let nameList = name.split('_');
-  for (var i = 0; i < nameList.length; i++) {
-    nameList[i] = nameList[i].charAt(0).toUpperCase() + nameList[i].slice(1);
-  }
-  return nameList.join(' ');
-}
-
-export function shortName(name) {
-  let nameList = name.split(' ');
-  let letters = '';
-  nameList[0].charAt(0).toUpperCase();
-  if (nameList.length < 2) {
-    letters += nameList[0][0];
-  } else {
-    nameList[1].charAt(0).toUpperCase();
-    letters += nameList[0][0];
-    letters += nameList[1][0];
-  }
-
-  return letters;
-}
 
 export function getPath() {
   return window.location.pathname;
@@ -30,12 +8,12 @@ export function getPath() {
 export function getInitials(name: string) {
   return name
     ? name
-        .split(' ')
-        .map((n, index, arr) => {
-          if (index === 0 || index === arr.length - 1) return n[0];
-        })
-        .filter((n) => n)
-        .join('')
+      .split(' ')
+      .map((n, index, arr) => {
+        if (index === 0 || index === arr.length - 1) return n[0];
+      })
+      .filter((n) => n)
+      .join('')
     : '';
 }
 
