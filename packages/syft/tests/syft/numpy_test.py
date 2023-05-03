4a51edc5cae335920834598dd433d4ee03e06a0e
IshanMi <mishra.ishan@gmail.com>
2023-05-03 15:53:51 -0400

    Add unit test with next 6 NumPy exceptions

--- packages/syft/tests/syft/numpy_test.py
+++ packages/syft/tests/syft/numpy_test.py
@@ -59,3 +59,67 @@ def exception2_numpy_methods_that_return_tuples(mock_object: ActionObject) -> No
     assert result.syft_lineage_id == result[0].syft_lineage_id
     assert result.syft_history_hash == result[0].syft_history_hash
     assert result[0].syft_lineage_id == result[1].syft_lineage_id
+
+
+def exception3_numpy_methods_returning_new_arrays(mock_object: ActionObject) -> None:
+    """
+    NumPy methods such as `np.pad()` that take an array and return a new array will not return an ActionObject
+    even if the inputs are ActionObjects.
+
+    Some methods that "extract" such as `np.diag()` also suffer from this issue.
+    """
+
+    result = np.pad(mock_object, pad_width=1)
+    assert isinstance(result, ActionObject), "The Result is no longer an action object"
+
+    result2 = np.diag(mock_object)
+    assert isinstance(result2, ActionObject), "The Result is no longer an action object"
+
+
+def exception4_nan_behaviour(mock_object: ActionObject) -> None:
+    """
+    NaNs have strange behaviour and our mock objects don't work as intended with them for some reason
+    """
+
+    array = ActionObject.from_obj(np.empty_like(mock_object) * np.nan)
+    assert np.nan in array, "The array is full of NaNs but a NaN was not detected"
+
+
+def exception5_metadata_is_action_object(mock_object: ActionObject) -> None:
+    """
+    Perhaps things like `.shape` shouldn't return ActionObjects at all?
+    """
+
+    assert isinstance(mock_object.shape, tuple)
+
+
+def exception6_action_object_integers(mock_object: ActionObject) -> None:
+    """
+    In certain places it seems that ActionObjects aren't a perfect replacement for integers and such
+    """
+    _ = np.unravel_index(12, mock_object.shape)
+
+
+def exception7_chaining_operations(mock_object: ActionObject) -> None:
+    """
+    In examples such as the one below, each of the operations works individually but when done
+    all at once, we are rewarded with an error :):
+    """
+
+    mock_object - mock_object.mean()
+    mock_object.std()
+
+    (mock_object - mock_object.mean()) / mock_object.std()
+
+
+def exception8_inplace_modifications_kill_kernel(mock_object: ActionObject) -> None:
+    """
+    All of the expressions below will indeed kill your kernel
+    """
+
+    mock_object += 1
+    np.add(mock_object, mock_object, out=mock_object)
+    np.sub(mock_object, mock_object, out=mock_object)
+    np.multiply(mock_object, mock_object, out=mock_object)
+    np.divide(mock_object, mock_object, out=mock_object)
+    mock_object[mock_object > 0.5] *= 2
