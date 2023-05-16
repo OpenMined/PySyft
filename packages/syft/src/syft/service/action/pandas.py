2534ea8a890d6900b634fc6aa6d1e5ec1b0ceab6
Koen van der Veen <koenlennartvanderveen@gmail.com>
2023-05-16 14:04:07 +0200

    remove pandas magic that is not necessary for level 0

--- packages/syft/src/syft/service/action/pandas.py
+++ packages/syft/src/syft/service/action/pandas.py
@@ -22,72 +22,12 @@ class PandasDataFrameObject(ActionObject):
     __version__ = SYFT_OBJECT_VERSION_1
 
     syft_internal_type: ClassVar[Type[Any]] = DataFrame
-    syft_passthrough_attrs = BASE_PASSTHROUGH_ATTRS + [
-        "_typ",
-        "shape",
-        "ndim",
-        "_get_axis",
-        "axis",
-        "axes",
-        "_get_axis_number",
-        "_get_block_manager_axis",
-        "_mgr",
-        "_constructor",
-        "attrs",
-    ]
+    syft_passthrough_attrs = BASE_PASSTHROUGH_ATTRS
     # this is added for instance checks for dataframes
-    _typ = "dataframe"
     # syft_dont_wrap_attrs = ["shape"]
 
     def __dataframe__(self, *args: Any, **kwargs: Any) -> Any:
         return self.__dataframe__(*args, **kwargs)
-
-    def _get_axis(self, axis):
-        if isinstance(axis, ActionObject):
-            axis = axis.syft_action_data
-        return self.syft_action_data._get_axis(axis)
-
-    def _get_block_manager_axis(self, axis):
-        if isinstance(axis, ActionObject):
-            axis = axis.syft_action_data
-        return self.syft_action_data._get_block_manager_axis(axis)
-
-    def _get_axis_number(self, axis):
-        if isinstance(axis, ActionObject):
-            axis = axis.syft_action_data
-        return self.syft_action_data._get_axis_number(axis)
-
-    @property
-    def attrs(self):
-        return self.syft_action_data.attrs
-
-    @property
-    def _constructor(self):
-        def wrapper(*args, **kwargs):
-            obj = self.syft_action_data._constructor(*args, **kwargs)
-            return ActionObject.from_obj(obj)
-
-        return wrapper
-
-    @property
-    def _mgr(self):
-        return self.syft_action_data._mgr
-
-    @property
-    def axis(self):
-        return self.syft_action_data.axis
-
-    @property
-    def axes(self):
-        return self.syft_action_data.axes
-
-    @property
-    def shape(self):
-        return self.syft_action_data.shape
-
-    @property
-    def ndim(self):
-        return self.syft_action_data.ndim
 
     def __getattribute__(self, name: str) -> Any:
         return super().__getattribute__(name)
@@ -108,87 +48,16 @@ class PandasSeriesObject(ActionObject):
     __version__ = SYFT_OBJECT_VERSION_1
 
     syft_internal_type = Series
-    syft_passthrough_attrs = BASE_PASSTHROUGH_ATTRS + [
-        "_typ",
-        "shape",
-        "ndim",
-        "_get_axis",
-        "axis",
-        "axes",
-        "_get_axis_number",
-        "_get_block_manager_axis",
-        "_mgr",
-        "_constructor",
-        "attrs",
-        "_constructor_expanddim",
-    ]
+    syft_passthrough_attrs = BASE_PASSTHROUGH_ATTRS
 
     name: Optional[str] = None
     # syft_dont_wrap_attrs = ["shape"]
-    _typ = "series"
-
-    def __series__(self, *args: Any, **kwargs: Any) -> Any:
-        return self.__series__(*args, **kwargs)
-
-    def __dataframe__(self, *args: Any, **kwargs: Any) -> Any:
-        return self.__dataframe__(*args, **kwargs)
-
-    def _get_axis(self, axis):
-        if isinstance(axis, ActionObject):
-            axis = axis.syft_action_data
-        return self.syft_action_data._get_axis(axis)
-
-    def _get_block_manager_axis(self, axis):
-        if isinstance(axis, ActionObject):
-            axis = axis.syft_action_data
-        return self.syft_action_data._get_block_manager_axis(axis)
-
-    def _get_axis_number(self, axis):
-        if isinstance(axis, ActionObject):
-            axis = axis.syft_action_data
-        return self.syft_action_data._get_axis_number(axis)
 
     def __getattribute__(self, name: str) -> Any:
         return super().__getattribute__(name)
 
     def syft_get_property(self, obj: Any, method: str) -> Any:
         return getattr(self.syft_action_data, method)
-
-    @property
-    def shape(self):
-        return self.syft_action_data.shape
-
-    @property
-    def _constructor_expanddim(self):
-        return self.syft_action_data._constructor_expanddim
-
-    @property
-    def attrs(self):
-        return self.syft_action_data.attrs
-
-    @property
-    def _constructor(self):
-        def wrapper(*args, **kwargs):
-            obj = self.syft_action_data._constructor(*args, **kwargs)
-            return ActionObject.from_obj(obj)
-
-        return wrapper
-
-    @property
-    def _mgr(self):
-        return self.syft_action_data._mgr
-
-    @property
-    def axis(self):
-        return self.syft_action_data.axis
-
-    @property
-    def axes(self):
-        return self.syft_action_data.axes
-
-    @property
-    def ndim(self):
-        return self.syft_action_data.ndim
 
     def syft_is_property(self, obj: Any, method: str) -> bool:
         if method in ["str"]:
