20c9e69d3a5056ba3cf817b258513ae0961fdbbb
Ionesio Junior <ionesiojr@gmail.com>
2023-05-23 16:24:49 -0300

    ADD required fields in NodeSettings

--- packages/syft/src/syft/service/settings/settings.py
+++ packages/syft/src/syft/service/settings/settings.py
@@ -1,14 +1,20 @@
+# stdlib
 from typing import Optional
 
+# relative
 from ...serde.serializable import serializable
-from ...types.syft_object import SyftObject
 from ...types.syft_object import SYFT_OBJECT_VERSION_1
+from ...types.syft_object import SyftObject
+from ...types.uid import UID
+
 
 @serializable()
 class NodeSettingsUpdate(SyftObject):
     __canonical_name__ = "NodeSettingsUpdate"
     __version__ = SYFT_OBJECT_VERSION_1
 
+    id: Optional[UID]
+    name: Optional[str]
     organization: Optional[str]
     description: Optional[str]
     on_board: Optional[bool]
@@ -19,6 +25,7 @@ class NodeSettings(SyftObject):
     __canonical_name__ = "NodeSettings"
     __version__ = SYFT_OBJECT_VERSION_1
 
+    name: str = "Node"
     deployed_on: str
     organization: str = "OpenMined"
     on_board: bool = False
