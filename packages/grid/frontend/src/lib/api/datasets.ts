7a10028ecc75b9a3e036944e97ff7c57762cd437
Ionesio Junior <ionesiojr@gmail.com>
2023-05-26 10:26:46 -0300

    add searchDataset in frontend api calls

--- packages/grid/frontend/src/lib/api/datasets.ts
+++ packages/grid/frontend/src/lib/api/datasets.ts
@@ -11,3 +11,10 @@ export async function getDataset(uid: string) {
 export async function deleteDataset(uid: string) {
   return await syftCall({ path: 'dataset.delete_by_id', payload: { uid: makeSyftUID(uid) } });
 }
+
+export async function searchDataset(name: string, chunk_size: number = 0, chunk_index: number = 0) {
+  return await syftCall({
+    path: 'dataset.search',
+    payload: { name: name, chunk_size: chunk_size, chunk_index: chunk_index }
+  });
+}
