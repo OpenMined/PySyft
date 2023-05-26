332e64401875841f72b242c2ab97a9540f0a9e77
Ionesio Junior <ionesiojr@gmail.com>
2023-05-26 15:02:59 -0300

    ADD support for pagination in user.get_all and dataset.get_all frontend API calls

--- packages/grid/frontend/src/lib/api/users.ts
+++ packages/grid/frontend/src/lib/api/users.ts
@@ -1,8 +1,11 @@
 import { getUserIdFromStorage } from './keys';
 import { makeSyftUID, syftCall } from './syft-api-call';
 
-export async function getAllUsers() {
-  return await syftCall({ path: 'user.get_all', payload: {} });
+export async function getAllUsers(chunk_size: number = 0, chunk_index: number = 0) {
+  return await syftCall({
+    path: 'user.get_all',
+    payload: { chunk_size: chunk_size, chunk_index: chunk_index }
+  });
 }
 
 export async function getUser(uid: string) {
