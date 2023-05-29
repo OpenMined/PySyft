70f2974dfd5420ae3459692b1a2cdf422299d78b
Antti Kalliokoski <antti.a.kalliokoski@gmail.com>
2023-05-29 10:44:06 +0300

    Prevent reviewing mutations, action store changes

--- notebooks/tutorials/jax-101/helpers.py
+++ notebooks/tutorials/jax-101/helpers.py
@@ -1,22 +1,29 @@
 def review_request(message):
     request = message.link
-    func = request.changes[0].link
-    func_name = func.service_func_name
-    print(func_name)
-    print(func.raw_code)
+    for change in request.changes:
+        if not hasattr(change, "link"):
+            continue
+        func = change.link
+        func_name = func.service_func_name
+        print(func_name)
+        print(func.raw_code)
 
 
 def run_submitted_function(message):
     request = message.link
-    func = request.changes[0].link
-    user_func = func.unsafe_function
-    real_result = user_func()
-    print(real_result)
-    return real_result
+    for change in request.changes:
+        if not hasattr(change, "link"):
+            continue
+        func = change.link
+        user_func = func.unsafe_function
+        real_result = user_func()
+        print(real_result)
+        return real_result
 
 
 def accept_request(message, real_result):
     request = message.link
     request.approve()
-    result = request.accept_by_depositing_result(real_result)
-    print(result)
+    if real_result is not None:
+        result = request.accept_by_depositing_result(real_result)
+        print(result)
