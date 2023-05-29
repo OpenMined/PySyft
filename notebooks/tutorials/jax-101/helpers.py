7e82aafa76cc8f199fbf88491884fc3ab46c1fec
Antti Kalliokoski <antti.a.kalliokoski@gmail.com>
2023-05-29 12:44:58 +0300

    Use dev_mode temporarily to properly clear domains

--- notebooks/tutorials/jax-101/helpers.py
+++ notebooks/tutorials/jax-101/helpers.py
@@ -1,8 +1,6 @@
 def review_request(message):
     request = message.link
     for change in request.changes:
-        if not hasattr(change, "link"):
-            continue
         func = change.link
         func_name = func.service_func_name
         print(func_name)
@@ -12,8 +10,6 @@ def review_request(message):
 def run_submitted_function(message):
     request = message.link
     for change in request.changes:
-        if not hasattr(change, "link"):
-            continue
         func = change.link
         user_func = func.unsafe_function
         real_result = user_func()
@@ -25,5 +21,6 @@ def accept_request(message, real_result):
     request = message.link
     request.approve()
     if real_result is not None:
+        print("******", real_result, type(real_result))
         result = request.accept_by_depositing_result(real_result)
         print(result)
