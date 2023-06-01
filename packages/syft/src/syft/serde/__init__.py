07bf9cbf69a757d9fc502ae23f1d4675dcf2589d
Yash Gorana <yash.gorana@hotmail.com>
2023-06-01 18:04:36 +0530

    [syft] add __init__.py to serde

--- packages/syft/src/syft/serde/__init__.py
+++ packages/syft/src/syft/serde/__init__.py
@@ -1,4 +1,25 @@
 # relative
-from .array import NOTHING  # noqa: F401 F811
-from .recursive import NOTHING  # noqa: F401 F811
-from .third_party import NOTHING  # noqa: F401 F811
+from .deserialize import _deserialize
+from .lib_permissions import CMPCRUDPermission
+from .lib_permissions import CMPCompoundPermission
+from .lib_permissions import CMPPermission
+from .lib_permissions import CMPUserPermission
+from .lib_service_registry import CMPBase
+from .lib_service_registry import CMPClass
+from .lib_service_registry import CMPFunction
+from .lib_service_registry import CMPMethod
+from .lib_service_registry import CMPModule
+from .lib_service_registry import CMPProperty
+from .lib_service_registry import CMPTree
+from .lib_service_registry import action_execute_registry_libs
+from .mock import CachedFaker
+from .recursive import TYPE_BANK
+from .recursive import index_syft_by_module_name
+from .recursive import recursive_serde_register
+from .recursive_primitives import recursive_serde_register_type
+from .serializable import serializable
+from .serialize import _serialize
+from .signature import generate_signature
+from .signature import get_signature
+from .signature import signature_remove_context
+from .signature import signature_remove_self
