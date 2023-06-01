0e633cc52e2ffe889ba9f592c1fed728d33a3d1f
khoaguin <dkn.work@protonmail.com>
2023-06-01 20:11:03 +0700

    start writing tests for aciton graph integration

--- packages/syft/tests/syft/action_graph/action_graph_integration_test.py
+++ packages/syft/tests/syft/action_graph/action_graph_integration_test.py
@@ -1,3 +1,40 @@
 """
 Tests for the integration of action graph into action and action object creation / update
 """
+
+# third party
+import pytest
+
+
+@pytest.mark.skip
+def test_node_creation_dataset_upload() -> None:
+    """
+    Create a node in the graph when a dataset is uploaded
+    """
+    pass
+
+
+@pytest.mark.skip
+def test_node_creation_action_obj_send() -> None:
+    """
+    Create a node in the graph when the action_obj.send method is called
+    """
+    pass
+
+
+@pytest.mark.skip
+def test_node_creation_generate_remote_lib_function() -> None:
+    """
+    Create a graph node when an Action is
+    created when a client generate a remote function
+    """
+    pass
+
+
+@pytest.mark.skip
+def test_node_creation_syft_make_action() -> None:
+    """
+    Create a graph node when an Action is
+    created in the syft_make_action method of ActionObject
+    """
+    pass
