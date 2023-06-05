c3c057bd77c5b82af955af39325544763a0bfad3
Ionesio Junior <ionesiojr@gmail.com>
2023-06-05 11:14:38 -0300

    ADD filterRequest api call

--- packages/grid/frontend/src/lib/api/requests.ts
+++ packages/grid/frontend/src/lib/api/requests.ts
@@ -8,6 +8,17 @@ export async function getAllRequests() {
   return await syftCall({ path: 'request.get_all_info', payload: {} });
 }
 
+export async function filterRequests(name: string, page_index: number = 0, page_size: number = 0) {
+  return await syftCall({
+    path: 'request.filter_all_info',
+    payload: {
+      request_filter: { name: name, fqn: 'syft.service.request.request.RequestInfoFilter' },
+      page_size: page_size,
+      page_index: page_index
+    }
+  });
+}
+
 export async function getCodeRequest(uid: string) {
   return await syftCall({ path: 'code.get_by_id', payload: { uid: makeSyftUID(uid) } });
 }
