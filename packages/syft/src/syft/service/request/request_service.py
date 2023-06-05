417b3561591e7fff9376053de1442a4ce6987bea
Ionesio Junior <ionesiojr@gmail.com>
2023-06-05 11:20:59 -0300

    ADD filter service in requests and enable pagination

--- packages/syft/src/syft/service/request/request_service.py
+++ packages/syft/src/syft/service/request/request_service.py
@@ -30,6 +30,7 @@ from ..user.user_roles import GUEST_ROLE_LEVEL
 from ..user.user_service import UserService
 from .request import Request
 from .request import RequestInfo
+from .request import RequestInfoFilter
 from .request import RequestStatus
 from .request import SubmitRequest
 from .request_stash import RequestStash
@@ -104,11 +105,12 @@ class RequestService(AbstractService):
         requests = result.ok()
         return requests
 
-    @service_method(
-        path="request.get_all_info", name="get_all_info", roles=GUEST_ROLE_LEVEL
-    )
+    @service_method(path="request.get_all_info", name="get_all_info")
     def get_all_info(
-        self, context: AuthedServiceContext
+        self,
+        context: AuthedServiceContext,
+        page_index: Optional[int] = 0,
+        page_size: Optional[int] = 0,
     ) -> Union[List[RequestInfo], SyftError]:
         """Get a Dataset"""
         result = self.stash.get_all(context.credentials)
@@ -121,9 +123,43 @@ class RequestService(AbstractService):
                 user = method(req.requesting_user_verify_key).to(UserView)
                 message = get_message(context=context, obj_uid=req.id)
                 requests.append(RequestInfo(user=user, request=req, message=message))
+
+            # If chunk size is defined, then split list into evenly sized chunks
+            if page_size:
+                requests = [
+                    requests[i : i + page_size]
+                    for i in range(0, len(requests), page_size)
+                ]
+                # Return the proper slice using chunk_index
+                requests = requests[page_index]
+
             return requests
 
         return SyftError(message=result.err())
+
+    @service_method(path="request.filter_all_info", name="filter_all_info")
+    def filter_all_info(
+        self,
+        context: AuthedServiceContext,
+        request_filter: RequestInfoFilter,
+        page_index: Optional[int] = 0,
+        page_size: Optional[int] = 0,
+    ) -> Union[List[RequestInfo], SyftError]:
+        """Get a Dataset"""
+        result = self.get_all_info(context)
+        requests = list(
+            filter(lambda res: (request_filter.name in res.user.name), result)
+        )
+
+        # If chunk size is defined, then split list into evenly sized chunks
+        if page_size:
+            requests = [
+                requests[i : i + page_size] for i in range(0, len(requests), page_size)
+            ]
+            # Return the proper slice using chunk_index
+            requests = requests[page_index]
+
+        return requests
 
     @service_method(path="request.get_all_for_status", name="get_all_for_status")
     def get_all_for_status(
