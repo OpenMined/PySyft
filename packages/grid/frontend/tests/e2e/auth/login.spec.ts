a49719f74a0db3a28f6344bacd7fa1129ca36067
Dylan Hrebenach <dhrebenach@fubo.tv>
2023-06-06 09:37:46 -0700

    fix: replace user credentials with test account

--- packages/grid/frontend/tests/e2e/auth/login.spec.ts
+++ packages/grid/frontend/tests/e2e/auth/login.spec.ts
@@ -9,8 +9,8 @@ test.describe('User Login', () => {
   });
 
   test('should login an existing user', async ({ page }) => {
-    await page.getByTestId('email').fill('dylan@openmined.org');
-    await page.getByTestId('password').fill('dylan');
+    await page.getByTestId('email').fill('info@openmined.org');
+    await page.getByTestId('password').fill('changethis');
 
     await page.getByRole('button', { name: /login/i }).click();
 
