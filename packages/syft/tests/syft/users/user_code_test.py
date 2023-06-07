d3836069bf4fc27e02c89eb9bbe11f07602df3ed
Ivoline Ngong <ivolinengong@gmail.com>
2023-06-07 09:01:36 -0400

    fix linting errors

--- packages/syft/tests/syft/users/user_code_test.py
+++ packages/syft/tests/syft/users/user_code_test.py
@@ -1,15 +1,19 @@
+# syft absolute
 import syft as sy
 from syft.service.action.action_object import ActionObject
 from syft.service.user.user import User
 
-@sy.syft_function(input_policy=sy.ExactMatch(),
-                    output_policy=sy.SingleExecutionExactOutput())
+
+@sy.syft_function(
+    input_policy=sy.ExactMatch(), output_policy=sy.SingleExecutionExactOutput()
+)
 def test_func():
     return 1
 
-def test_user_code( worker, guest_client: User) -> None:
+
+def test_user_code(worker, guest_client: User) -> None:
     test_func()
-    x = guest_client.api.services.code.request_code_execution(test_func)
+    guest_client.api.services.code.request_code_execution(test_func)
 
     root_domain_client = worker.root_client
     message = root_domain_client.notifications[-1]
@@ -23,4 +27,3 @@ def test_user_code( worker, guest_client: User) -> None:
 
     real_result = result.get_from(guest_client)
     assert isinstance(real_result, int)
-   
