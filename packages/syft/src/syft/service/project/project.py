ccb43b433e7969f3f431595ebcba77afcbb52724
teo <teo.milea97@gmail.com>
2023-06-07 12:44:56 +0300

    added custom repr for project and project submit

--- packages/syft/src/syft/service/project/project.py
+++ packages/syft/src/syft/service/project/project.py
@@ -598,6 +598,15 @@ class DemocraticConsensusModel(ConsensusModel):
 class Project(SyftObject):
     __canonical_name__ = "Project"
     __version__ = SYFT_OBJECT_VERSION_1
+    __attr_custom_repr__ = [
+        "id",
+        "name",
+        "description",
+        "user_email_address",
+        "events",
+        "user_signing_key"
+    ]
+
 
     id: Optional[UID]
     name: str
@@ -1009,6 +1018,13 @@ class ProjectSubmit(SyftObject):
     __canonical_name__ = "ProjectSubmit"
     __version__ = SYFT_OBJECT_VERSION_1
     __attr_repr_cols__ = ["name"]
+    __attr_custom_repr__ = [
+        "id",
+        "name",
+        "description",
+        "user_email_address",
+    ]
+
 
     id: Optional[UID]
     name: str
