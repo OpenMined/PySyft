55061a31b3a598b78432d1f0f03672c68f73d3da
teo <teo.milea97@gmail.com>
2023-06-07 09:49:29 +0300

    fix lint

--- packages/syft/src/syft/types/syft_object.py
+++ packages/syft/src/syft/types/syft_object.py
@@ -15,9 +15,9 @@ from typing import Sequence
 from typing import Tuple
 from typing import Type
 import warnings
-import itables
 
 # third party
+import itables
 import pydantic
 from pydantic import BaseModel
 from pydantic import EmailStr
@@ -472,16 +472,13 @@ def list_dict_repr_html(self) -> str:
             collection_type = (
                 f"{type(self).__name__.capitalize()} - Size: {len(self)}\n"
             )
-            
+
             # This can be customize however we want
             css = """
             .itables table { margin: 0 auto; float: left; }
             """
-            html_datatable = itables.to_html_datatable(
-                                df=x,
-                                css=css
-                            )
-            
+            html_datatable = itables.to_html_datatable(df=x, css=css)
+
             return collection_type + html_datatable
             # return collection_type + x._repr_html_()
     except Exception as e:
