2bf2b0983c964aaa0d5c0b66d0e67dd53566e72a
Oleksandr Lytvyn <letvyk99@gmail.com>
2023-06-11 13:06:34 +0200

    finished numpy tests

--- packages/syft/tests/syft/serde/numpy_functions_test.py
+++ packages/syft/tests/syft/serde/numpy_functions_test.py
@@ -3,11 +3,7 @@ import numpy as np
 import pytest
 
 # syft absolute
-import syft
 from syft import ActionObject
-
-# from syft.serde.lib_permissions import *
-# from syft.serde.lib_service_registry import *
 from syft.service.response import SyftAttributeError
 
 PYTHON_ARRAY = [0, 1, 1, 2, 2, 3]
@@ -18,57 +14,82 @@ NP_2dARRAY = np.array([[3, 4, 5, 2], [6, 7, 2, 6]])
 @pytest.mark.parametrize(
     "func, func_arguments",
     [
-        ("array", [0, 1, 1, 2, 2, 3]),
+        ("array", "[0, 1, 1, 2, 2, 3]"),
         ("linspace", "10,10,10"),
         ("arange", "5,10,2"),
         ("logspace", "0,2"),
         ("zeros", "(1,2)"),
         ("identity", "4"),
-        ("unique", [0, 1, 1, 2, 2, 3]),
-        ("mean", [0, 1, 1, 2, 2, 3]),
-        ("median", [0, 1, 1, 2, 2, 3]),
+        ("unique", "[0, 1, 1, 2, 2, 3]"),
+        ("mean", "[0, 1, 1, 2, 2, 3]"),
+        ("median", "[0, 1, 1, 2, 2, 3]"),
         ("digitize", "[0, 1, 1, 2, 2, 3], [0,1,2,3]"),
         ("reshape", "[0, 1, 1, 2, 2, 3], (6,1)"),
-        ("squeeze", [0, 1, 1, 2, 2, 3]),
-        ("count_nonzero", [0, 1, 1, 2, 2, 3]),
-        ("argwhere", [0, 1, 1, 2, 2, 3]),
-        ("argmax", [0, 1, 1, 2, 2, 3]),
-        ("argmin", [0, 1, 1, 2, 2, 3]),
-        ("sort", list(reversed([0, 1, 1, 2, 2, 3]))),
-        ("absolute", [0, 1, 1, 2, 2, 3]),
+        ("squeeze", "[0, 1, 1, 2, 2, 3]"),
+        ("count_nonzero", "[0, 1, 1, 2, 2, 3]"),
+        ("argwhere", "[0, 1, 1, 2, 2, 3]"),
+        ("argmax", "[0, 1, 1, 2, 2, 3]"),
+        ("argmin", "[0, 1, 1, 2, 2, 3]"),
+        ("sort", "list(reversed([0, 1, 1, 2, 2, 3]))"),
+        ("absolute", "[0, 1, 1, 2, 2, 3]"),
         ("clip", "[0, 1, 1, 2, 2, 3], 0, 2"),
-        ("put", f"{NP_2dARRAY}, [1,2], [7,8]"),
+        ("put", " np.array([[3, 4, 5, 2], [6, 7, 2, 6]]), [1,2], [7,8]"),
         ("intersect1d", "[0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3])"),
         ("setdiff1d", "[0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3])"),
         ("setxor1d", "[0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3])"),
-        ("hsplit", "np.array([[3, 4, 5, 2], [6, 7, 2, 6]]), 4"),
-        ("vsplit", f"{NP_2dARRAY}, 2"),
-        ("hstack", f"{PYTHON_ARRAY}, {NP_ARRAY}"),
-        ("vstack", f"{PYTHON_ARRAY}, {NP_ARRAY}"),
-        ("allclose", f"{PYTHON_ARRAY}, {NP_ARRAY}, 0.5"),
-        ("equal", f"{PYTHON_ARRAY}, {NP_ARRAY}"),
+        ("hstack", "([0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3]))"),
+        ("vstack", "([0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3]))"),
+        ("allclose", "[0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3]), 0.5"),
+        ("equal", "[0, 1, 1, 2, 2, 3], [0, 1, 1, 2, 2, 3]"),
         ("repeat", "2023, 4"),
-        ("std", PYTHON_ARRAY),
-        ("var", PYTHON_ARRAY),
-        ("percentile", f"{PYTHON_ARRAY}, 2"),
-        ("var", PYTHON_ARRAY),
-        # np.unique(arr, return_counts=True) #throws error
-        # Not Working
-        # ("min", [0, 1, 1, 2, 2, 3]),
-        # ("max", [0, 1, 1, 2, 2, 3]),
-        # ("min", [0, 1, 1, 2, 2, 3]),
-        # ("where", f"np.array([0, 1, 1, 2, 2, 3])"),  # required condition
+        ("std", "[0, 1, 1, 2, 2, 3]"),
+        ("var", "[0, 1, 1, 2, 2, 3]"),
+        ("percentile", "[0, 1, 1, 2, 2, 3], 2"),
+        ("var", "[0, 1, 1, 2, 2, 3]"),
+        ("amin", "[0, 1, 1, 2, 2, 3]"),  # alias for min not exist in Syft
+        ("amax", "[0, 1, 1, 2, 2, 3]"),  # alias for max not exist in Syft
+        ("where", "a > 5, a, -1"),  # required condition
+        #     # Not Working
+        pytest.param(
+            "hsplit",
+            "np.array([[3, 4, 5, 2], [6, 7, 2, 6]]), 4",
+            marks=pytest.mark.xfail(
+                raises=ValueError, reason="Value error insinde Syft"
+            ),
+        ),
+        pytest.param(
+            "vsplit",
+            "np.array([[3, 4, 5, 2], [6, 7, 2, 6]]), 2",
+            marks=pytest.mark.xfail(
+                raises=ValueError, reason="Value error insinde Syft"
+            ),
+        ),
+        pytest.param(
+            "unique",
+            "np.array([0, 1, 1, 5, 5, 3]), return_counts=True",
+            marks=pytest.mark.xfail(
+                raises=(ValueError, AssertionError),
+                reason="Kwargs Can not be properly unpacked",
+            ),
+        ),
     ],
 )
-def test_numpy_functions(func, func_arguments):
-    syft.Worker(name="Command_center")
+def test_numpy_functions(func, func_arguments, request):
+    # the problem is that ruff removes the unsued variable,
+    # but this test case np_sy and a are considered as unused, though used in the eval string
+    exec("np_sy = request.getfixturevalue('numpy_syft_instance')")
     try:
+        if func == "where":
+            exec(
+                "a = np.array([[3, 4, 5, 2], [6, 7, 2, 6]])"
+            )  # this is needed for where test
         result = eval(f"np_sy.{func}({func_arguments})")
+
     except Exception as e:
         assert (
             e == SyftAttributeError
-        ), f"Can not evalute function {func} with arguments {func_arguments}"
-        pass
+        ), f"Can not evaluate {func}({func_arguments}) with {e}"
+        print(e)
     else:
         original_result = eval(f"np.{func}({func_arguments})")
 
