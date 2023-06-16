f08f37a273993c31e4fe526700691b99caf04db5
Ivoline Ngong <ivolinengong@gmail.com>
2023-06-15 22:17:14 -0400

    modified error message

--- packages/syft/src/syft/service/dataset/dataset.py
+++ packages/syft/src/syft/service/dataset/dataset.py
@@ -504,9 +504,8 @@ class CreateDataset(Dataset):
             if existing_asset.name == asset.name:
                 if not force_replace:
                     print(
-                        f'WARNING: Asset "{asset.name}" already exists. To replace it,\
-                            use add_asset(asset, force_replace=True)'
-                    )
+                        f'WARNING: Asset "{asset.name}" exists.'
+                        ' Use add_asset(asset, force_replace=True) to replace.')
                     return
                 else:
                     self.asset_list[i] = asset
