ff20a7a8b25dd70ed593ad395cc0390e24bdbead
Madhava Jay <madhava@openmined.org>
2023-06-30 22:53:33 +0000

    bump version

--- packages/hagrid/setup.py
+++ packages/hagrid/setup.py
@@ -5,7 +5,7 @@ import platform
 from setuptools import find_packages
 from setuptools import setup
 
-__version__ = "0.3.44"
+__version__ = "0.3.45"
 
 DATA_FILES = {"img": ["hagrid/img/*.png"], "hagrid": ["*.yml"]}
 
