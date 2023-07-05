a1eed8060559169072e1207995866df21f5e444d
Ionesio Junior <ionesiojr@gmail.com>
2023-07-05 10:30:11 -0300

    Fix code styling

--- packages/syftjs/src/serde/primitives.ts
+++ packages/syftjs/src/serde/primitives.ts
@@ -1,18 +1,15 @@
-// @ts-ignore
-import * as capnp from 'capnp-ts';
-
-
-export const PRIMITIVE_MAP = new Map<string,string>([
-  ['builtins.str','string']
-])
+export const PRIMITIVE_MAP = new Map<string, string>([
+  ['builtins.str', 'string'],
+]);
 
 export const PRIMITIVES = {
-  string:  { serialize: (text: string) => {
-                  return new TextEncoder().encode(text);
-             },
-             deserialize: (buffer: ArrayBuffer) => {
-                  return new TextDecoder().decode(buffer);
-             },
-             fqn: 'builtins.str'
-           }
+  string: {
+    serialize: (text: string) => {
+      return new TextEncoder().encode(text);
+    },
+    deserialize: (buffer: ArrayBuffer) => {
+      return new TextDecoder().decode(buffer);
+    },
+    fqn: 'builtins.str',
+  },
 };
