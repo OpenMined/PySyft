e9eb3febc8cb1e42265d42b0e2925192cb7721c5
Ionesio Junior <ionesiojr@gmail.com>
2023-07-05 10:29:22 -0300

    Export serialize/deserialize methods

--- packages/syftjs/src/serde/index.ts
+++ packages/syftjs/src/serde/index.ts
@@ -1,3 +1,3 @@
 export { PRIMITIVES } from './primitives';
-export {serialize} from './serialize';
-export {deserialize} from './deserialize';
+export { serialize } from './serialize';
+export { deserialize } from './deserialize';
