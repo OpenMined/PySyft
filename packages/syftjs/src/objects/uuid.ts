73284793f3b39b9d87c4ca269ab6381d60dd44a6
Ionesio Junior <ionesiojr@gmail.com>
2023-07-07 10:32:38 -0300

    Replace Interface by abstract class extension to allow us handle static classFqn

--- packages/syftjs/src/objects/uuid.ts
+++ packages/syftjs/src/objects/uuid.ts
@@ -1,18 +1,26 @@
 import { parse as uuidParse } from 'uuid';
 import { v4 as uuidv4 } from 'uuid';
-
+import { stringify } from 'uuid';
 import { ObjectInterface } from './object';
 
-export class UID implements ObjectInterface {
-  value?: Uint8Array;
-  fqn = 'syft.types.uid.UID';
+export class UID extends ObjectInterface {
+  value: Uint8Array;
+  fqn: string;
+  public static readonly classFqn: string = 'syft.types.uid.UID';
 
   constructor(value: Uint8Array | undefined = undefined) {
+    super();
+
     if (value) {
       this.value = value;
     } else {
       const uuid = uuidv4();
       this.value = uuidParse(uuid);
     }
+    this.fqn = UID.classFqn;
+  }
+
+  get hash() {
+    return stringify(this.value);
   }
 }
