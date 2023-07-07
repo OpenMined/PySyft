b9b3165fdb99d61e635163c79c2d3ce577946430
Ionesio Junior <ionesiojr@gmail.com>
2023-07-07 11:12:25 -0300

    ADD class Documentation

--- packages/syftjs/src/objects/uuid.ts
+++ packages/syftjs/src/objects/uuid.ts
@@ -3,11 +3,32 @@ import { v4 as uuidv4 } from 'uuid';
 import { stringify } from 'uuid';
 import { ObjectInterface } from './object';
 
+/**
+ * UID class extends the ObjectInterface.
+ * A class to handle universally unique identifiers (UUID) within the application.
+ */
 export class UID extends ObjectInterface {
+  /**
+   * The value of the UID object which will be a Uint8Array representation of UUID.
+   */
   value: Uint8Array;
+
+  /**
+   * The fully qualified name of the UID class.
+   */
   fqn: string;
+
+  /**
+   * The static, readonly property of class fully qualified name.
+   * It represents the class path in PySyft library. This can be accessed without instantiating the class.
+   */
   public static readonly classFqn: string = 'syft.types.uid.UID';
 
+  /**
+   * Constructs a new instance of UID.
+   *
+   * @param value A Uint8Array value to be assigned to the UID object. If not provided, a new UUIDv4 value will be generated.
+   */
   constructor(value: Uint8Array | undefined = undefined) {
     super();
 
@@ -20,6 +41,11 @@ export class UID extends ObjectInterface {
     this.fqn = UID.classFqn;
   }
 
+  /**
+   * Get the stringified value of the UID object.
+   *
+   * @returns The stringified value of the UID.
+   */
   get hash() {
     return stringify(this.value);
   }
