55b2978c1c53a9f2795e674502d1e7734e42c327
Dylan Hrebenach <dhrebenach@fubo.tv>
2023-07-15 12:26:02 -0700

    feat: add babel for backwards compatibility across all browsers

--- packages/syftjs/rollup.config.js
+++ packages/syftjs/rollup.config.js
@@ -1,4 +1,6 @@
 import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
 
 export default {
   input: "src/index.ts",
@@ -7,5 +9,5 @@ export default {
     format: "cjs",
   },
   external: ["capnp-ts", "uuid"],
-  plugins: [typescript()],
+  plugins: [typescript(), resolve(), babel({ babelHelpers: "bundled" })],
 };
