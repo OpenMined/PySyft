c2fdd52b5c0e4fb94d3de3efa3c270d5b1446b51
Shubham Gupta <shubhamgupta3121@gmail.com>
2023-07-16 23:07:31 +0530

    fix warnings flag in hagrid cli

--- packages/hagrid/hagrid/cli.py
+++ packages/hagrid/hagrid/cli.py
@@ -1272,7 +1272,7 @@ def create_launch_cmd(
     else:
         parsed_kwargs["node_side_type"] = NodeSideType.LOW_SIDE.value
 
-    parsed_kwargs["enable_warnings"] = kwargs["enable-warnings"]
+    parsed_kwargs["enable_warnings"] = kwargs["enable_warnings"]
 
     # choosing deployment type
     parsed_kwargs["deployment_type"] = "container_stack"
