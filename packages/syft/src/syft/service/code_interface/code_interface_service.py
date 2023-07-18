4ef4c7adb57190ae5e659f71dfe0d9121e4940ac
Ivoline Ngong <ivolinengong@gmail.com>
2023-07-18 19:39:56 -0400

    wip- implementing get_by_func_name and user_id or email

--- packages/syft/src/syft/service/code_interface/code_interface_service.py
+++ packages/syft/src/syft/service/code_interface/code_interface_service.py
@@ -1,7 +1,9 @@
 # stdlib
-# stdlib
 from typing import List
 from typing import Union
+
+# third party 
+from result import Result, Err, Ok
 
 # relative
 from ...store.document_store import DocumentStore
@@ -14,6 +16,7 @@ from ..response import SyftSuccess
 from ..service import AbstractService
 from ..service import service_method
 from ..user.user_roles import DATA_SCIENTIST_ROLE_LEVEL
+from ..user.user import User
 from .code_interface import CodeInterface
 from .code_interface_stash import CodeInterfaceStash
 
@@ -101,8 +104,8 @@ class CodeInterfaceService(AbstractService):
             code_interface = result.ok()
             return code_interface
         return SyftError(message=result.err())
-
-
+        
+        
     @service_method(
         path="code_interface.delete_by_id",
         name="delete_by_id"
@@ -113,3 +116,84 @@ class CodeInterfaceService(AbstractService):
             return result.ok()
         else:
             return SyftError(message=result.err())
+        
+    # @service_method(path="code_interface.get_by_name_and_user_id", name="get_by_name_and_user_id")
+    # def get_by_name_and_user_id(self, context: AuthedServiceContext, service_func_name: str, user_id: UID
+    # ) -> Union[SyftSuccess, SyftError]:
+        
+    #     user = self.verify_user_id(context=context, user_id=user_id)
+    #     print("USER: ", user.value)
+    #     if user.err():
+    #         return user
+        
+    #     kwargs = {
+    #         "id": user_id,
+    #         "verify_key": user.value.verify_key,
+    #         "service_func_name": service_func_name
+    #         }
+
+    #     #    kwargs = user_search.to_dict(exclude_empty=True)
+        
+          
+    #     # UserExperience
+    #     result = self.stash.find_all(credentials=context.credentials, **kwargs)
+    #     print("Results", result)
+    #     if result.is_err(): #or len(result) > 1
+    #         return result
+        
+    @service_method(path="code_interface.get_by_name_and_user_email", name="get_by_name_and_user_email")
+    def get_by_func_name_and_user_email(self, context: AuthedServiceContext, service_func_name: str, 
+                                   user_email: str,
+                                   user_id: UID,
+    ) -> Union[SyftSuccess, SyftError]:
+        
+        user_service = context.node.get_service("userservice")
+        user_verify_key = user_service.user_verify_key(user_email) 
+
+        # user = self.verify_user_id(context=context, user_id=user_id)
+        print("USER: ", user_verify_key)
+        if isinstance(user_verify_key, SyftError):
+            return user_verify_key
+        
+        kwargs = {
+            "id": user_id,
+            "email": user_email,
+            "verify_key": user_verify_key,
+            "service_func_name": service_func_name
+            }
+        
+        result = self.stash.find_all(credentials=context.credentials, **kwargs)
+        print("Results", result)
+        if result.is_err(): #or len(result) > 1
+            return result
+
+    @service_method(path="code_interface.get_by_name", name="get_by_name", roles=DATA_SCIENTIST_ROLE_LEVEL)
+    def get_by_func_name(self, context: AuthedServiceContext, service_func_name: str, user_email: str):
+        user_service = context.node.get_service("userservice")
+        user_verify_key = user_service.user_verify_key(user_email) 
+
+    #     def get_by_name(
+    #     self, credentials: SyftVerifyKey, name: str
+    # ) -> Result[Optional[Dataset], str]:
+        qks = QueryKeys(qks=[NamePartitionKey.with_obj(name)])
+        return self.query_one(credentials=credentials, qks=qks)
+
+
+
+    # def verify_user_id(self, context: AuthedServiceContext, user_id: UID)-> Result[Ok, Err]:
+    #     user_service = context.node.get_service("userservice")
+    #     user = user_service.stash.get_by_uid(context.node.signing_key.verify_key, uid=user_id)
+    #     if user.is_err():
+    #         return SyftError(message=f"User with id {user_id} not found")
+        
+    #     return user #Ok(Ok(True))
+    
+
+    # TODO
+    # verify the user => get_service from user_service
+    # create kwargs => get syft_Verify key from the user
+    # try find_all 
+    # make sure we get atmost one result => error
+    # Write another function for data scientist => get_by_name
+    # Another function to check for the syft_verify key
+    # 
