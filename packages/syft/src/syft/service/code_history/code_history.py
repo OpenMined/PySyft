5ae2e8c451098920ce01069dbd3dd0803e8e8019
teo <teo.milea97@gmail.com>
2023-07-27 11:36:03 +0300

    fix repr

--- packages/syft/src/syft/service/code_history/code_history.py
+++ packages/syft/src/syft/service/code_history/code_history.py
@@ -1,4 +1,5 @@
 # stdlib
+import json
 from typing import Any
 from typing import Dict
 from typing import List
@@ -11,7 +12,6 @@ from ...serde.serializable import serializable
 from ...types.syft_object import SYFT_OBJECT_VERSION_1
 from ...types.syft_object import SyftObject
 from ...types.syft_object import SyftVerifyKey
-from ...types.syft_object import list_dict_repr_html
 from ...types.uid import UID
 from ..code.user_code import UserCode
 
@@ -40,13 +40,6 @@ class CodeHistory(SyftObject):
             comment = ""
         self.comment_history.append(comment)
 
-    # def __getitem__(self, key: int):
-    #     api = APIRegistry.api_for(
-    #         self.node_uid,
-    #         self.user_verify_key,
-    #     )
-    #     return api.services.code.get_by_id(self.user_code_history[key])
-
 
 @serializable()
 class CodeVersions(SyftObject):
@@ -59,13 +52,11 @@ class CodeVersions(SyftObject):
     service_func_name: str
     comment_history: Optional[List[str]] = []
 
+    def _coll_repr_(self):
+        return {"Number of versions": len(self.user_code_history)}
+
     def _repr_html_(self):
         return self.user_code_history._repr_html_()
-
-    # def __init__(self, user_code_history=None, service_func_name="", comment_history = []) -> None:
-    #     self.user_code_history = user_code_history
-    #     self.service_func_name = service_func_name
-    #     self.comment_history = comment_history
 
     def __getitem__(self, key: int):
         return self.user_code_history[key]
@@ -81,7 +72,9 @@ class CodeHistoryDict(SyftObject):
     code_versions: Optional[Dict[str, CodeVersions]] = {}
 
     def _repr_html_(self):
-        return self.code_versions._repr_html_()
+        return f"""
+            {self.code_versions._repr_html_()}
+            """
 
     def add_func(self, versions: CodeVersions) -> Any:
         self.code_versions[versions.service_func_name] = versions
@@ -106,8 +99,11 @@ class UserHistoryDict(SyftObject):
     node_uid: UID
     user_dict: Optional[Dict[str, List[str]]] = {}
 
-    def _repr_html_(self):
-        return list_dict_repr_html(self.user_dict)
+    __repr_attrs__ = ["available_keys"]
+
+    @property
+    def available_keys(self):
+        return json.dumps(self.user_dict, sort_keys=True, indent=4)
 
     def __getitem__(self, key: int):
         api = APIRegistry.api_for(self.node_uid, self.syft_client_verify_key)
