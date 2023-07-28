d9e65a5ae3061dd09ad7f4cbce7713fdf17d8631
Kien Dang <mail@kien.ai>
2023-07-28 19:27:23 +0800

    Simplify default hostname

--- packages/syft/src/syft/service/queue/zmq_queue.py
+++ packages/syft/src/syft/service/queue/zmq_queue.py
@@ -8,7 +8,6 @@ from typing import Union
 
 # third party
 import gevent
-from pydantic import validator
 import zmq.green as zmq
 
 # relative
@@ -114,11 +113,7 @@ class ZMQClientConfig(SyftObject, QueueClientConfig):
     __version__ = SYFT_OBJECT_VERSION_1
 
     id: Optional[UID]
-    hostname: Optional[str]
-
-    @validator("hostname", pre=True, always=True)
-    def get_hostname(cls, v: Optional[str]) -> str:
-        return "127.0.0.1" if v is None else v
+    hostname: str = "127.0.0.1"
 
 
 @serializable(attrs=["host"])
