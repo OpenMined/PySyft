dc0aa77f513a0c6291f5724512df9e492a53c14e
Thiago Costa Porto <thiagocostaporto@gmail.com>
2023-09-21 16:50:41 +0100

    feat: add node id to notification html repr

--- packages/syft/src/syft/store/linked_obj.py
+++ packages/syft/src/syft/store/linked_obj.py
@@ -34,7 +34,7 @@ class LinkedObject(SyftObject):
             type(self.resolve) if self.object_type == None else self.object_type
         )
         return (
-            f"{resolved_obj_type.__name__}: {self.object_uid} @ Node<{self.node_uid}>"
+            f"{resolved_obj_type.__name__}: {self.object_uid} @ Node {self.node_uid}"
         )
 
     @property
