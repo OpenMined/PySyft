68cac4ffd635e9faa187f1c8155c58f423ffad6b
Thiago Costa Porto <thiagocostaporto@gmail.com>
2023-09-21 18:13:34 +0100

    fix: remove role matching in service/dataset

--- packages/syft/src/syft/service/dataset/dataset.py
+++ packages/syft/src/syft/service/dataset/dataset.py
@@ -21,7 +21,6 @@ from pydantic import validator
 from result import Err
 from result import Ok
 from result import Result
-from syft.service.user.roles import Roles
 
 # relative
 from ...serde.serializable import serializable
@@ -131,7 +130,7 @@ class Asset(SyftObject):
     mock_is_real: bool = False
     shape: Optional[Tuple]
     created_at: DateTime = DateTime.now()
-    uploader: Contributor
+    uploader: Optional[Contributor]
 
     __repr_attrs__ = ["name", "shape"]
 
@@ -156,6 +155,7 @@ class Asset(SyftObject):
         from ...service.action.action_object import ActionObject
 
         uploaded_by_line = f"<p><strong>Uploaded by: </strong>{self.uploader.name} ({self.uploader.email})</p>" if self.uploader else ""
+
         if isinstance(self.data, ActionObject):
             data_table_line = itables.to_html_datatable(
                 df=self.data.syft_action_data, css=itables_css
@@ -333,8 +333,6 @@ class CreateAsset(SyftObject):
                 name=name, role=_role_str, email=email, phone=phone, note=note
             )
             self.contributors.append(contributor)
-            if _role_str == Roles.UPLOADER.value:
-                self.uploader = contributor
             return SyftSuccess(
                 message=f"Contributor '{name}' added to '{self.name}' Asset."
             )
@@ -618,8 +616,6 @@ class CreateDataset(Dataset):
                 name=name, role=_role_str, email=email, phone=phone, note=note
             )
             self.contributors.append(contributor)
-            if _role_str == Roles.UPLOADER.value:
-                self.uploader = contributor
             return SyftSuccess(
                 message=f"Contributor '{name}' added to '{self.name}' Dataset."
             )
