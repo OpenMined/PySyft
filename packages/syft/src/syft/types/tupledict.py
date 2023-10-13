6277dde326d253ab4589ee1dcfb955c6dabb5ccc
Kien Dang <mail@kien.ai>
2023-10-13 12:17:16 +0800

    Add DictTuple

--- packages/syft/src/syft/types/tupledict.py
+++ packages/syft/src/syft/types/tupledict.py
@@ -1,8 +1,18 @@
 # stdlib
 from collections import OrderedDict
-from typing import Iterator
+from collections.abc import Collection
+from collections.abc import Iterable
+from collections.abc import Iterator
+from collections.abc import Mapping
+from typing import Generic
+from typing import Optional
+from typing import SupportsIndex
 from typing import TypeVar
 from typing import Union
+from typing import overload
+
+# third party
+from typing_extensions import Self
 
 _KT = TypeVar("_KT")
 _VT = TypeVar("_VT")
@@ -19,3 +29,114 @@ class TupleDict(OrderedDict[_KT, _VT]):
 
     def __iter__(self) -> Iterator[_VT]:
         yield from self.values()
+
+
+class _Meta(type):
+    def __call__(
+        cls, __value: Optional[Iterable] = None, __key: Optional[Collection] = None, /
+    ):
+        if __value is None and __key is None:
+            obj = cls.__new__(cls)
+            obj.__init__()
+            return obj
+
+        if isinstance(__value, Mapping) and __key is None:
+            keys = OrderedDict()
+            values = []
+
+            for i, k in enumerate(__value.keys()):
+                keys[k] = i
+                values.append(__value[k])
+
+            obj = cls.__new__(cls, values)
+            obj.__init__(keys)
+
+            return obj
+
+        if isinstance(__value, Iterable) and __key is None:
+            keys = OrderedDict()
+            values = []
+
+            for i, (k, v) in enumerate(__value):
+                keys[k] = i
+                values.append(v)
+
+            obj = cls.__new__(cls, values)
+            obj.__init__(keys)
+
+            return obj
+
+        if isinstance(__value, Iterable) and __key is not None:
+            if len(__key) != len(__value):
+                raise ValueError("`__keys` has to be of the same length as `__values`")
+
+            keys = OrderedDict((k, i) for i, k in enumerate(__key))
+
+            obj = cls.__new__(cls, __value)
+            obj.__init__(keys)
+
+            return obj
+
+        raise NotImplementedError
+
+
+class DictTuple(tuple[_VT, ...], Generic[_KT, _VT], metaclass=_Meta):
+    __mapping: OrderedDict[_KT, int]
+
+    @overload
+    def __init__(self) -> None:
+        ...
+
+    @overload
+    def __init__(self, __value: Iterable[tuple[_KT, _VT]]) -> None:
+        ...
+
+    @overload
+    def __init__(self, __value: Mapping[_KT, _VT]) -> None:
+        ...
+
+    @overload
+    def __init__(self, __value: Iterable[_VT], __key: Collection[_KT]) -> None:
+        ...
+
+    def __init__(
+        self, __value: Optional[Union[Mapping[_KT, int], Iterable[_KT]]] = None, /
+    ) -> None:
+        match __value:
+            case OrderedDict():
+                self.__mapping = __value
+            case Mapping():
+                self.__mapping = OrderedDict(__value)
+            case Iterable():
+                self.__mapping = OrderedDict((k, i) for i, k in enumerate(__value))
+            case _:
+                pass
+
+        super().__init__()
+
+    @overload
+    def __getitem__(self, __key: _KT) -> _VT:
+        ...
+
+    @overload
+    def __getitem__(self, __key: slice) -> Self:
+        ...
+
+    @overload
+    def __getitem__(self, __key: SupportsIndex) -> _VT:
+        ...
+
+    def __getitem__(self, __key):
+        if isinstance(__key, slice):
+            return self.__class__(
+                super().__getitem__(__key),
+                list(self.__mapping.keys()).__getitem__(__key),
+            )
+
+        if isinstance(__key, SupportsIndex):
+            return super().__getitem__(__key)
+
+        return super().__getitem__(self.__mapping[__key])
+
+    def keys(self):
+        return self.__mapping.keys()
