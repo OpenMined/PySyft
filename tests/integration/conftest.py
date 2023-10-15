b6dc9626146780baea30009ca80b56c5a553b505
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2023-10-15 17:43:16 +0530

    added new gateway connection tests for kubernetes fixed stack integration tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,7 +1,23 @@
 # third party
 import _pytest
+import pytest
 
 
 def pytest_configure(config: _pytest.config.Config) -> None:
     config.addinivalue_line("markers", "frontend: frontend integration tests")
     config.addinivalue_line("markers", "network: network integration tests")
+
+
+@pytest.fixture
+def gateway_port() -> int:
+    return 9081
+
+
+@pytest.fixture
+def domain_1_port() -> int:
+    return 9082
+
+
+@pytest.fixture
+def domain_2_port() -> int:
+    return 9083
