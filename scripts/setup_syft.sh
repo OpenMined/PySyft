4b48a43a31fb233bb757452363a2f147000e561e
bitsofsteve <cyberlexix@gmail.com>
2023-10-19 13:08:44 -0400

    base script

--- scripts/setup_syft.sh
+++ scripts/setup_syft.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Check if the required arguments are provided
-if [[ $# -lt 2 ]]; then
-    echo "Usage: $0 <version> <node_name>"
+if [[ $# -lt 4 ]]; then
+    echo "Usage: $0 <version> <node_name> <node_side_type> <port>"
     exit 1
 fi
 
 VERSION=$1
 NODE_NAME=$2
-FILE_ID="1ffWtZa-aJkJYsAG8Wkosmvrt3Uvm1bYM" # Replace with your actual file ID
+NODE_SIDE_TYPE=$3
+PORT=$4
+FILE_ID="1ffWtZa-aJkJYsAG8Wkosmvrt3Uvm1bYM" 
 
 # Check if gdown is installed, if not, install it using pip
 if ! command -v gdown &> /dev/null; then
@@ -34,20 +36,24 @@ case "$(uname)" in
 esac
 
 # Assuming the .env file is in the current directory after unzipping
-# Update the VERSION and NODE_NAME values based on the OS
+# Update the VERSION, NODE_NAME, NODE_SIDE_TYPE, and PORT values based on the OS
 if [[ "$OS" == "mac" ]]; then
     sed -i '' "s/^VERSION=.*$/VERSION=$VERSION/" .env
     sed -i '' "s/^NODE_NAME=.*$/NODE_NAME=$NODE_NAME/" .env
+    sed -i '' "s/^NODE_SIDE_TYPE=.*$/NODE_SIDE_TYPE=$NODE_SIDE_TYPE/" .env
+    sed -i '' "s/^PORT=.*$/PORT=$PORT/" .env
 else
     sed -i "s/^VERSION=.*$/VERSION=$VERSION/" .env
     sed -i "s/^NODE_NAME=.*$/NODE_NAME=$NODE_NAME/" .env
+    sed -i "s/^NODE_SIDE_TYPE=.*$/NODE_SIDE_TYPE=$NODE_SIDE_TYPE/" .env
+    sed -i "s/^PORT=.*$/PORT=$PORT/" .env
 fi
 
 # Run the docker compose command
-docker compose --env-file ./.env -p "$NODE_NAME" --profile blob-storage --profile frontend --file docker-compose.yml up -d
+docker-compose --env-file ./.env -p "$NODE_NAME" --profile blob-storage --profile frontend --file docker-compose.yml up -d
 
 # Clean up the downloaded file
 rm -f file.tgz
 
-# Start Jupyter Notebook
-docker run --rm -it --network=host "openmined/grid-node-jupyter:$VERSION" "jupyter" "notebook" "--port=8888" "--ip=0.0.0.0" "--allow-root"
+# Run the docker command for the Jupyter notebook using the specified PORT
+#docker run --rm -it --network=host "openmined/grid-node-jupyter:$VERSION" "jupyter" "notebook" "--port=$PORT" "--ip=0.0.0.0" "--allow-root"
