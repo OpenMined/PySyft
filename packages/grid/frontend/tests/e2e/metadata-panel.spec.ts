d1dea3184ac425b8fb3562ecf84ee5e6b2113cc5
Thiago Costa Porto <thiagocostaporto@gmail.com>
2023-10-26 05:00:03 +0200

    test: fix

--- packages/grid/frontend/tests/e2e/metadata-panel.spec.ts
+++ packages/grid/frontend/tests/e2e/metadata-panel.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('should display domain metadata, has a valid id', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('/login');
   const metadataPanel = page.getByTestId('domain-metadata-panel');
   expect(await metadataPanel.isVisible()).toBe(true);
 
