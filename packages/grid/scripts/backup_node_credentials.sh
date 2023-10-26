47d995ded6b40d9ef11ef4c8ae5304ed0ad99fac
Peter Chung <peterchung241@gmail.com>
2023-10-26 20:49:11 +1100

    added sha256 checksum to verify copied of files

--- packages/grid/scripts/backup_node_credentials.sh
+++ packages/grid/scripts/backup_node_credentials.sh
@@ -3,6 +3,15 @@
 ROOT_DATA_PATH="$HOME/.syft/data"
 
 
+calculate_checksum() {
+    # Calculate the checksum of a file
+    local file="$1"
+    local checksum
+    checksum=$(sha256sum "$file" | awk '{print $1}')
+    echo "$checksum"
+}
+
+
 docker_cp() {
     # Exit in case of error
     set -e
@@ -31,14 +40,19 @@ docker_cp() {
         # Create the directory for the specific container
         mkdir -p "$DESTINATION_PATH"
 
+        # Calculate the checksum of the source file
+        SOURCE_CHECKSUM=$(calculate_checksum "$DESTINATION_PATH/credentials.json")
+
         # Copy the credentials.json file from the container to the host
         docker cp "${CONTAINER_NAME}:${SOURCE_PATH}" "$DESTINATION_PATH"
 
-        # Check if the copy was successful
-        if [ $? -eq 0 ]; then
-            echo "Copied credentials.json from $CONTAINER_NAME to $DESTINATION_PATH"
+        # Calculate the checksum of the destination file
+        DESTINATION_CHECKSUM=$(calculate_checksum "$DESTINATION_PATH/credentials.json")
+
+        if [ $? -eq 0 ] && [ "$SOURCE_CHECKSUM" = "$DESTINATION_CHECKSUM" ]; then
+            echo "Copied credentials.json from $CONTAINER_NAME to $DESTINATION_PATH."
         else
-            echo "Failed to copy credentials.json from $CONTAINER_NAME"
+            echo "Failed to copy credentials.json from $CONTAINER_NAME, or checksum mismatch."
         fi
     done
 }
@@ -72,14 +86,20 @@ k8s_cp() {
 
                 mkdir -p "$ROOT_DATA_PATH/$NAMESPACE"
 
+                # Calculate the checksum of the source file
+                SOURCE_CHECKSUM=$(calculate_checksum "$ROOT_DATA_PATH/$NAMESPACE/credentials.json")
+
                 # Copy the file (suppress error message from kubectl cp command: "tar: Removing leading `/' from member names")
                 kubectl cp "$NAMESPACE/$POD_NAME:$FILE_PATH_IN_POD" "$ROOT_DATA_PATH/$NAMESPACE/credentials.json" &>/dev/null
 
-                # Check if the copy was successful
-                if [ $? -eq 0 ]; then
-                    echo "Copied credentials.json from $POD_NAME in namespace $NAMESPACE to $ROOT_DATA_PATH/$NAMESPACE in context $CONTEXT"
+                # Calculate the checksum of the destination file
+                DESTINATION_CHECKSUM=$(calculate_checksum "$ROOT_DATA_PATH/$NAMESPACE/credentials.json")
+
+                # Check if the copy was successful and if the checksums match
+                if [ $? -eq 0 ] && [ "$SOURCE_CHECKSUM" = "$DESTINATION_CHECKSUM" ]; then
+                    echo "Copied credentials.json from $POD_NAME in namespace $NAMESPACE to $ROOT_DATA_PATH/$NAMESPACE in context $CONTEXT."
                 else
-                    echo "Failed to copy credentials.json from $POD_NAME in namespace $NAMESPACE in context $CONTEXT"
+                    echo "Failed to copy credentials.json from $POD_NAME in namespace $NAMESPACE in context $CONTEXT, or checksum mismatch."
                 fi
             done
         fi
