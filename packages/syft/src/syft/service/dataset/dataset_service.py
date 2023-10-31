0e38ee4c0d64e289ae6ed07dc4b2ce30a6583e06
Kien Dang <mail@kien.ai>
2023-10-31 18:49:42 +0800

    Minor optimization

--- packages/syft/src/syft/service/dataset/dataset_service.py
+++ packages/syft/src/syft/service/dataset/dataset_service.py
@@ -58,7 +58,7 @@ def _paginate_dataset_collection(
 ) -> Union[DictTuple[str, Dataset], DatasetPageView]:
     slice_ = _paginate_collection(datasets, page_size=page_size, page_index=page_index)
     chunk = datasets[slice_] if slice_ is not None else datasets
-    results = DictTuple((dataset.name, dataset) for dataset in chunk)
+    results = DictTuple(chunk, lambda dataset: dataset.name)
 
     return (
         results
