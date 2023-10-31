818abfbcb8d72c8f43895265574706f0c6d145c2
Thiago Costa Porto <thiagocostaporto@gmail.com>
2023-10-31 16:12:54 +0100

    fix: remove inspector

--- packages/grid/frontend/svelte.config.js
+++ packages/grid/frontend/svelte.config.js
@@ -5,9 +5,9 @@ import { vitePreprocess } from "@sveltejs/kit/vite"
 const config = {
   preprocess: [vitePreprocess()],
   kit: { adapter: adapter() },
-  vitePlugin: {
-    inspector: true,
-  },
+  // vitePlugin: {
+  //   inspector: true,
+  // },
 }
 
 export default config
