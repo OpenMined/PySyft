e575789808e7f2d1593887a832db76d3f6f2ab65
teo <teo.milea97@gmail.com>
2023-11-15 13:44:09 +0200

    added launch_job visitor

--- packages/syft/src/syft/service/code/code_parse.py
+++ packages/syft/src/syft/service/code/code_parse.py
@@ -7,3 +7,20 @@ class GlobalsVisitor(ast.NodeVisitor):
         if isinstance(node, ast.Global):
             raise Exception("No Globals allowed!")
         ast.NodeVisitor.generic_visit(self, node)
+
+nested_calls = []
+
+def reset_nested_calls():
+    global nested_calls
+    nested_calls = []
+
+def get_nested_calls():
+    return nested_calls
+
+class LaunchJobVisitor(ast.NodeVisitor):
+    def generic_visit(self, node):
+        if isinstance(node, ast.Call):
+            if isinstance(node.func, ast.Attribute):
+                if node.func.value.id == "domain" and node.func.attr == "launch_job": 
+                    nested_calls.append(node.args[0].id)
+        ast.NodeVisitor.generic_visit(self, node)
