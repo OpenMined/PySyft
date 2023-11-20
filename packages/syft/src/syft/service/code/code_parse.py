4c840935f85a3385f82d6149449d049433f515fd
teo <teo.milea97@gmail.com>
2023-11-20 17:35:16 +0200

    improved visitor search for launch_job

--- packages/syft/src/syft/service/code/code_parse.py
+++ packages/syft/src/syft/service/code/code_parse.py
@@ -1,5 +1,7 @@
 # stdlib
+from _ast import Module
 import ast
+from typing import Any
 
 
 class GlobalsVisitor(ast.NodeVisitor):
@@ -18,9 +20,18 @@ def get_nested_calls():
     return nested_calls
 
 class LaunchJobVisitor(ast.NodeVisitor):
-    def generic_visit(self, node):
-        if isinstance(node, ast.Call):
-            if isinstance(node.func, ast.Attribute):
+    def visit_Module(self, node: Module) -> Any:
+        self.nested_calls = []
+        self.generic_visit(node)
+    
+    def visit_Call(self, node):
+        if isinstance(node.func, ast.Attribute):
                 if node.func.value.id == "domain" and node.func.attr == "launch_job": 
-                    nested_calls.append(node.args[0].id)
-        ast.NodeVisitor.generic_visit(self, node)
+                    self.nested_calls.append(node.args[0].id)
+    
+    # def generic_visit(self, node):
+    #     if isinstance(node, ast.Call):
+    #         if isinstance(node.func, ast.Attribute):
+    #             if node.func.value.id == "domain" and node.func.attr == "launch_job": 
+    #                 nested_calls.append(node.args[0].id)
+    #     ast.NodeVisitor.generic_visit(self, node)
