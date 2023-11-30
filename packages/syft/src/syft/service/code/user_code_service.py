c974f8bfe8e5f57c40e03227a46b7fcf1ae7b288
khoaguin <dkn.work@protonmail.com>
2023-11-30 17:29:18 +0700

    fix(syft/code): check for code uniqueness (name+content) before adding to the stash

--- packages/syft/src/syft/service/code/user_code_service.py
+++ packages/syft/src/syft/service/code/user_code_service.py
@@ -75,6 +75,18 @@ class UserCodeService(AbstractService):
             x in user_code.input_owner_verify_keys for x in user_code.output_readers
         ):
             raise ValueError("outputs can only be distributed to input owners")
+
+        # check if the code with the same name and content already exists in the stash
+        find_results = self.stash.get_all(user_code.user_verify_key)
+        if find_results.is_err():
+            return SyftError(message=str(find_results.err()))
+        find_results = find_results.ok()
+        unique_func_names = [c.unique_func_name for c in find_results]
+        if user_code.unique_func_name in unique_func_names:
+            return SyftError(
+                message="The code to be submitted (name and content) already exists"
+            )
+
         result = self.stash.set(context.credentials, user_code)
         if result.is_err():
             return SyftError(message=str(result.err()))
