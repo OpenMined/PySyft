255aee7b9416d07bb2d1bdc155e2057f63bcad3a
Kien Dang <mail@kien.ai>
2023-12-01 14:06:19 +0800

    Make seaweed scripts stop early on error

--- packages/grid/seaweedfs/mount_command.sh
+++ packages/grid/seaweedfs/mount_command.sh
@@ -1,7 +1,4 @@
-echo "remote.configure -name=$1 -type=azure -azure.account_name=$2 -azure.account_key=$5" | weed shell
-
-echo "s3.bucket.create -name=$3" | weed shell
-
-echo "remote.mount -dir=/buckets/$3 -remote=$1/$4" | weed shell
-
+echo "remote.configure -name=$1 -type=azure -azure.account_name=$2 -azure.account_key=$5" | weed shell && \
+echo "s3.bucket.create -name=$3" | weed shell && \
+echo "remote.mount -dir=/buckets/$3 -remote=$1/$4" | weed shell && \
 weed filer.remote.sync
--- packages/grid/seaweedfs/start.sh
+++ packages/grid/seaweedfs/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "s3.configure -access_key $S3_ROOT_USER -secret_key $S3_ROOT_PWD \
--user iam -actions Read,Write,List,Tagging,Admin -apply" | weed shell > /dev/null 2>&1 &
+-user iam -actions Read,Write,List,Tagging,Admin -apply" | weed shell > /dev/null 2>&1 && \
 weed server -s3 -s3.port=$S3_PORT -volume.max=500 -master.volumeSizeLimitMB=$S3_VOLUME_SIZE_MB &
 flask run -p $SEAWEED_MOUNT_PORT --host=0.0.0.0
