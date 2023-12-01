75a4d93b6128e3c1530a107694675e6469994650
Kien Dang <mail@kien.ai>
2023-12-01 13:16:23 +0800

    Remove unused STACK_API_KEY

--- packages/grid/seaweedfs/start.sh
+++ packages/grid/seaweedfs/start.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-echo "got api key"
-echo ${STACK_API_KEY}
-export STACK_API_KEY=${STACK_API_KEY}
-
 echo "s3.configure -access_key $S3_ROOT_USER -secret_key $S3_ROOT_PWD \
 -user iam -actions Read,Write,List,Tagging,Admin -apply" | weed shell > /dev/null 2>&1 &
 weed server -s3 -s3.port=$S3_PORT -volume.max=500 -master.volumeSizeLimitMB=$S3_VOLUME_SIZE_MB &
