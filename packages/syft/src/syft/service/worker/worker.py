38811d9740492737d3c74f91cc698637d44a5fe0
Thiago Costa Porto <thiagocostaporto@gmail.com>
2023-12-14 10:18:49 +0000

    feat: improve worker repr

--- packages/syft/src/syft/service/worker/worker.py
+++ packages/syft/src/syft/service/worker/worker.py
@@ -1,4 +1,5 @@
 # relative
+import os
 from ...serde.serializable import serializable
 from ...store.document_store import SYFT_OBJECT_VERSION_1
 from ...store.document_store import SyftObject
@@ -11,10 +12,20 @@ class DockerWorker(SyftObject):
     __canonical_name__ = "ContainerImage"
     __version__ = SYFT_OBJECT_VERSION_1
 
-    __attr_searchable__ = ["container_id", "container_name"]
-    __attr_unique__ = ["container_id", "container_name"]
-    __repr_attrs__ = ["container_id", "container_name", "created_at"]
+    __attr_searchable__ = ["container_id"]
+    __attr_unique__ = ["container_id"]
+    __repr_attrs__ = ["container_id", "created_at"]
 
-    container_name: str
     container_id: str
     created_at: DateTime = DateTime.now()
+
+    def get_name(self):
+        return os.getenv("DOCKER_WORKER_NAME", None)
+
+    def _coll_repr_(self):
+        return {
+            "container_name": self.get_name(),
+            "container_id": self.container_id,
+            "created_at": self.created_at,
+            "jobs": "TODO"
+        }
