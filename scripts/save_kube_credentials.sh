bd4529f06a5443ad589568c769b0502b2897f617
Ionesio Junior <ionesiojr@gmail.com>
2024-01-20 11:02:19 -0300

    ADD checking for unique context (user and cluster set) instead of just user match

--- scripts/save_kube_credentials.sh
+++ scripts/save_kube_credentials.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # The string to match with context.user
-MATCH_STRING="$1"
+USERNAME="$1"
+CLUSTER_NAME="$2"
 
 # File path to kubeconfig
 KUBECONFIG_FILE="~/.kube/config"
@@ -9,28 +10,25 @@ KUBECONFIG_FILE="~/.kube/config"
 # Parse and find user and cluster details from the kubectl config
 parse_kubectl_config() {
     # Finding the context with the matching user
-    CONTEXT_NAME=$(yq e ".contexts[] | select(.context.user == \"$MATCH_STRING\") | .name" ~/.kube/config)
+    CONTEXT_NAME=$(yq e ".contexts[] | select(.context.user == \"$USERNAME\" and .context.cluster == \"$CLUSTER_NAME\") | .name" ~/.kube/config)
 
     # Check if the context was found
     if [ -z "$CONTEXT_NAME" ]; then
-       echo "No matching user found."
+       echo "No matching context found."
        exit 1
     fi
 
-    # Extracting cluster name from the context
-    CLUSTER_NAME=$(yq e ".contexts[] | select(.name == \"$CONTEXT_NAME\") | .context.cluster" ~/.kube/config)
-
     # Extracting user details
-    CLIENT_CERTIFICATE_DATA=$(yq e ".users[] | select(.name == \"$MATCH_STRING\") | .user.\"client-certificate-data\"" ~/.kube/config)
-    CLIENT_KEY_DATA=$(yq e ".users[] | select(.name == \"$MATCH_STRING\") | .user.\"client-key-data\"" ~/.kube/config)
-    TOKEN=$(yq e ".users[] | select(.name == \"$MATCH_STRING\") | .user.token" ~/.kube/config)
+    CLIENT_CERTIFICATE_DATA=$(yq e ".users[] | select(.name == \"$USERNAME\") | .user.\"client-certificate-data\"" ~/.kube/config)
+    CLIENT_KEY_DATA=$(yq e ".users[] | select(.name == \"$USERNAME\") | .user.\"client-key-data\"" ~/.kube/config)
+    TOKEN=$(yq e ".users[] | select(.name == \"$USERNAME\") | .user.token" ~/.kube/config)
 
     # Extracting cluster details
     CERTIFICATE_AUTHORITY_DATA=$(yq e ".clusters[] | select(.name == \"$CLUSTER_NAME\") | .cluster.\"certificate-authority-data\"" ~/.kube/config)
     SERVER=$(yq e ".clusters[] | select(.name == \"$CLUSTER_NAME\") | .cluster.server" ~/.kube/config)
 
     # Creating a new item in 1Password
-    op item create --category="Api Credential" --title="$MATCH_STRING" CLIENT_CERTIFICATE_DATA=$CLIENT_CERTIFICATE_DATA USER_NAME=$MATCH_STRING CLIENT_KEY_DATA=$CLIENT_KEY_DATA TOKEN=$TOKEN CERTIFICATE_AUTHORITY_DATA=$CERTIFICATE_AUTHORITY_DATA SERVER=$SERVER CLUSTER_NAME=$CLUSTER_NAME
+    op item create --category="Api Credential" --title="$USERNAME-$CLUSTER_NAME" CLIENT_CERTIFICATE_DATA=$CLIENT_CERTIFICATE_DATA USER_NAME=$USERNAME CLIENT_KEY_DATA=$CLIENT_KEY_DATA TOKEN=$TOKEN CERTIFICATE_AUTHORITY_DATA=$CERTIFICATE_AUTHORITY_DATA SERVER=$SERVER CLUSTER_NAME=$CLUSTER_NAME
 
     # Check exit status of the op item creation command
     if [ $? -ne 0 ]; then
@@ -38,10 +36,10 @@ parse_kubectl_config() {
     fi
 }
 
-# Ensure script is called with one argument
-if [ -z "$1" ]; then
-    echo "No username provided. Please provide it."
-    echo "eg. ./save_aks_credentials.sh <username>"
+# Ensure script is called with two arguments
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Insufficient arguments provided. Please provide both username and cluster name."
+    echo "eg. ./save_aks_credentials.sh <username> <clustername>"
     exit 1
 fi
 
