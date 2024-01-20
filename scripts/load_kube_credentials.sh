ed6276899c26e419269f1c1bc5d0253d916bcebc
Ionesio Junior <ionesiojr@gmail.com>
2024-01-20 10:47:52 -0300

    Improve load credentials workflow by replacing yaml parsing for kubectl api calls

--- scripts/load_kube_credentials.sh
+++ scripts/load_kube_credentials.sh
@@ -50,88 +50,13 @@ CLUSTER_NAME=$(echo $jsonData | jq -r '.[] | select(.label=="CLUSTER_NAME") | .v
 CERTIFICATE_AUTHORITY_DATA=$(echo $jsonData | jq -r '.[] | select(.label=="CERTIFICATE_AUTHORITY_DATA") | .value')
 USER_NAME=$(echo $jsonData | jq -r '.[] | select(.label=="USER_NAME") | .value')
 
-# Check if the file does not exist
-if [ ! -f ~/.kube/config ]; then
-    # Create a new kubeconfig file
-    CONFIG_TEMPLATE="apiVersion: v1
-clusters:
-  - cluster:
-      certificate-authority-data: <ca-placeholder>
-      server: <server-placeholder>
-    name: <cluster-name-placeholder>
-contexts:
-  - context:
-      cluster: <cluster-name-placeholder>
-      user: <user-placeholder>
-    name: <cluster-name-placeholder>
-current-context: <cluster-name-placeholder>
-kind: Config
-preferences: {}
-users:
-  - name: <user-placeholder>
-    user:
-      client-certificate-data: <cc-placeholder>
-      client-key-data: <ck-placeholder>
-      token: <token-placeholder>"
-
-    # Replace placeholders with actual values
-    echo "$CONFIG_TEMPLATE" | sed -e "s|<ca-placeholder>|$CERTIFICATE_AUTHORITY_DATA|g" \
-        -e "s|<server-placeholder>|$SERVER|g" \
-        -e "s|<cluster-name-placeholder>|$CLUSTER_NAME|g" \
-        -e "s|<user-placeholder>|$USER_NAME|g" \
-        -e "s|<cc-placeholder>|$CLIENT_CERTIFICATE_DATA|g" \
-        -e "s|<ck-placeholder>|$CLIENT_KEY_DATA|g" \
-        -e "s|<token-placeholder>|$TOKEN|g" \
-        >> ~/.kube/config
-    echo "Config File Created."
-else
-    # Append cluster Information to kubeconfig
-    CLUSTER_UPDATED_JSON=$(jq -n \
-        --arg ca_data "$CERTIFICATE_AUTHORITY_DATA" \
-        --arg srv "$SERVER" \
-        --arg cl_name "$CLUSTER_NAME" \
-        '{
-            "cluster": {
-                "certificate-authority-data": $ca_data,
-                "server": $srv
-            },
-            "name": $cl_name
-        }')
-    yq eval -i ".clusters += [$CLUSTER_UPDATED_JSON]" ~/.kube/config
-
-
-    # Append context information to kubeconfig
-    CONTEXT_UPDATED_JSON=$(jq -n \
-        --arg cl_name "$CLUSTER_NAME" \
-        --arg usr "$USER_NAME" \
-        '{
-            "context": {
-                    "cluster": $cl_name,
-                    "user": $usr
-            },
-            "name": $cl_name
-        }')
-    yq eval -i ".contexts += [$CONTEXT_UPDATED_JSON]" ~/.kube/config
-
-
-    # Append user information to kubeconfig
-    USER_UPDATED_JSON=$(jq -n \
-        --arg cc_data "$CLIENT_CERTIFICATE_DATA" \
-        --arg ck_data "$CLIENT_KEY_DATA" \
-        --arg tkn "$TOKEN" \
-        --arg usr "$USER_NAME" \
-        '{
-            "name": $usr,
-            "user":{
-                    "client-certificate-data": $cc_data,
-                    "client-key-data": $ck_data,
-                    "token": $tkn
-            }
-        }')
-    yq eval -i ".users += [$USER_UPDATED_JSON]" ~/.kube/config
-
-    yq eval -i ".current-context = \"$CLUSTER_NAME\" " ~/.kube/config
-fi
+kubectl config set-cluster "$CLUSTER_NAME" --server="$SERVER"
+kubectl config set-credentials "$USER_NAME" --token="$TOKEN"
+kubectl config set-context "$CLUSTER_NAME" --cluster="$CLUSTER_NAME" --user="$USER_NAME" --namespace=default
+kubectl config use-context "$CLUSTER_NAME"
+kubectl config set clusters."$CLUSTER_NAME".certificate-authority-data "$CERTIFICATE_AUTHORITY_DATA"
+kubectl config set users."$USER_NAME".client-certificate-data "$CLIENT_CERTIFICATE_DATA"
+kubectl config set users."$USER_NAME".client-key-data "$CLIENT_KEY_DATA"
 
 
 echo "Updated kubeconfig with new cluster, context, and user"
