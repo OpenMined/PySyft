e181aaa3eee1c643fea4c609636e0ff9eb2304f2
Thiago Costa Porto <thiagocostaporto@gmail.com>
2024-01-29 15:29:51 +0100

    refactor: remove path from SyftObjectRetrieval in ondisk.py:read

--- packages/syft/src/syft/store/blob_storage/on_disk.py
+++ packages/syft/src/syft/store/blob_storage/on_disk.py
@@ -63,7 +63,6 @@ class OnDiskBlobStorageConnection(BlobStorageConnection):
         return SyftObjectRetrieval(
             syft_object=file_path.read_bytes(),
             file_name=file_path.name,
-            path=file_path,
             type_=type_,
         )
 
