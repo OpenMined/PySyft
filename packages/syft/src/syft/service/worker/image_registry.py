558edb423519d4299e63bc90e456e66e4a3ae3ef
Kien Dang <mail@kien.ai>
2024-02-14 15:51:41 +0800

    Use typing.Self instead of hard-coded class

--- packages/syft/src/syft/service/worker/image_registry.py
+++ packages/syft/src/syft/service/worker/image_registry.py
@@ -4,6 +4,7 @@ from urllib.parse import urlparse
 
 # third party
 from pydantic import validator
+from typing_extensions import Self
 
 # relative
 from ...serde.serializable import serializable
@@ -38,7 +39,7 @@ class SyftImageRegistry(SyftObject):
         return val
 
     @classmethod
-    def from_url(cls, full_str: str) -> "SyftImageRegistry":
+    def from_url(cls, full_str: str) -> Self:
         # this is only for urlparse
         if "://" not in full_str:
             full_str = f"http://{full_str}"
