1454a67acaa660c6d33f57eb407a4ee10f2485d1
Yash Gorana <yash.gorana@hotmail.com>
2024-02-14 21:31:27 +0530

    [helm] proxy only seaweedfs

--- packages/grid/seaweedfs/start.sh
+++ packages/grid/seaweedfs/start.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+S3_PORT=${S3_PORT:-8333}
+S3_VOLUME_SIZE_MB=${S3_VOLUME_SIZE_MB:-1024}
+S3_ROOT_USER=${S3_ROOT_USER:-admin}
+S3_ROOT_PWD=${S3_ROOT_PWD:-password}
+SEAWEED_MOUNT_PORT=${SEAWEED_MOUNT_PORT:-4001}
+
 weed server -s3 -s3.port="$S3_PORT" -volume.max=500 -master.volumeSizeLimitMB="$S3_VOLUME_SIZE_MB" &
 echo "s3.configure -access_key $S3_ROOT_USER -secret_key $S3_ROOT_PWD \
 -user iam -actions Read,Write,List,Tagging,Admin -apply" | weed shell > /dev/null 2>&1
