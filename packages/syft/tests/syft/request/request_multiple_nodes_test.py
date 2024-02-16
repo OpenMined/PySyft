22b4c3946000b3be4973d19b8513b0b9a576ad43
khoaguin <dkn.work@protonmail.com>
2024-02-16 15:17:51 +0700

    [tests] skip `test_transfer_request_nonblocking` on windows due to flakiness

--- packages/syft/tests/syft/request/request_multiple_nodes_test.py
+++ packages/syft/tests/syft/request/request_multiple_nodes_test.py
@@ -1,5 +1,6 @@
 # stdlib
 import secrets
+import sys
 from textwrap import dedent
 
 # third party
@@ -149,6 +150,7 @@ def test_transfer_request_blocking(
 
 
 @pytest.mark.flaky(reruns=2, reruns_delay=1)
+@pytest.mark.skipif(sys.platform == "win32", reason="very flaky on windows")
 def test_transfer_request_nonblocking(
     client_ds_1, client_do_1, client_do_2, dataset_1, dataset_2
 ):
