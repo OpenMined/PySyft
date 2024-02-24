9df8500d17fe82e72c529732971e745d73f1664d
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2024-02-24 16:04:05 +0530

    added initial health check endpoint

--- packages/grid/veilid/server/main.py
+++ packages/grid/veilid/server/main.py
@@ -1,9 +1,30 @@
 # third party
 from fastapi import FastAPI
+import veilid
 
 app = FastAPI(title="Veilid")
+
+HOST = "localhost"
+PORT = 5959
 
 
 @app.get("/")
 async def read_root() -> dict[str, str]:
     return {"message": "Hello World"}
+
+
+async def get_veilid_conn() -> veilid.VeilidAPI:
+    async def noop_callback(update: veilid.VeilidUpdate) -> None:
+        pass
+
+    return await veilid.json_api_connect(HOST, PORT, noop_callback)
+
+
+@app.get("/healthcheck")
+async def healthcheck() -> dict[str, str]:
+    async with await get_veilid_conn() as conn:
+        state = await conn.get_state()
+        if state.network.started:
+            return {"message": "OK"}
+        else:
+            return {"message": "FAIL"}
