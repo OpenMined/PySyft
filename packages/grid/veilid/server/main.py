dbc38f74ff205868e37dd23be34953f219f1a4c3
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2024-02-24 15:34:33 +0530

    Revert "Optimized Veilid Docker Installation by installing pre-built packages"
    This reverts commit f820db1de23d8de4ffdbd3cf8e44ddd58f52c027.

--- packages/grid/veilid/server/main.py
+++
@@ -1,9 +0,0 @@
-# third party
-from fastapi import FastAPI
-
-app = FastAPI(title="Veilid")
-
-
-@app.get("/")
-async def read_root() -> dict[str, str]:
-    return {"message": "Hello World"}
--- packages/grid/veilid/start.sh
+++ packages/grid/veilid/start.sh
@@ -1,23 +1,4 @@
 #!/usr/bin/env bash
-set -e
-export PATH="/root/.local/bin:${PATH}"
 
-APP_MODULE=server.main:app
-LOG_LEVEL=${LOG_LEVEL:-info}
-HOST=${HOST:-0.0.0.0}
-PORT=${PORT:-4000}
-RELOAD=""
-VEILID_FLAGS=""
-
-if [[ ${DEV_MODE} == "True" ]];
-then
-    echo "DEV_MODE Enabled"
-    RELOAD="--reload"
-    VEILID_FLAGS="--debug"
-fi
-
-/usr/bin/veilid-server -c /veilid/veilid-server.conf  $VEILID_FLAGS &
-
-exec uvicorn $RELOAD --host $HOST --port $PORT --log-level $LOG_LEVEL "$APP_MODULE"
-
+/veilid/veilid-server -c /veilid/veilid-server.conf --debug
 
