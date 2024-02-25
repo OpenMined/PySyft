b2f4547079550c4ab50a87b83e0e4b0c606b74d2
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2024-02-25 10:22:33 +0530

    Added context managers for async functions

--- packages/grid/veilid/server/veilid_core.py
+++ packages/grid/veilid/server/veilid_core.py
@@ -8,6 +8,8 @@ from typing import Union
 from loguru import logger
 import veilid
 from veilid import KeyPair
+from veilid import Sequencing
+from veilid import Stability
 from veilid import TypedKey
 from veilid import ValueData
 from veilid import VeilidUpdate
@@ -35,7 +37,13 @@ async def get_veilid_conn(
     host: str = HOST, port: int = PORT, update_callback: Callable = noop_callback
 ) -> _JsonVeilidAPI:
     return await veilid.json_api_connect(
-        host=HOST, port=PORT, update_callback=noop_callback
+        host=host, port=port, update_callback=update_callback
+    )
+
+
+async def get_routing_context(conn: _JsonVeilidAPI) -> _JsonRoutingContext:
+    return await (await conn.new_routing_context()).with_sequencing(
+        veilid.Sequencing.ENSURE_ORDERED
     )
 
 
@@ -69,8 +77,8 @@ class VeilidConnectionSingleton:
 
 async def create_private_route(
     conn: _JsonVeilidAPI,
-    stability: veilid.Stability.RELIABLE = veilid.Stability.RELIABLE,
-    sequencing: veilid.Sequencing = veilid.Sequencing.ENSURE_ORDERED,
+    stability: Stability = veilid.Stability.RELIABLE,
+    sequencing: Sequencing = veilid.Sequencing.ENSURE_ORDERED,
 ) -> Tuple[RouteId, bytes]:
     route_id, route_blob = await conn.new_custom_private_route(
         [veilid.CryptoKind.CRYPTO_KIND_VLD0],
@@ -83,37 +91,33 @@ async def create_private_route(
 
 async def generate_dht_key() -> dict[str, str]:
     logger.info("Generating DHT Key")
-    # TODO: Use a context manager for the connection
-    conn = await get_veilid_conn()
 
-    if await load_dht_key(conn):
-        return {"message": "DHT Key already exists"}
+    async with await get_veilid_conn() as conn:
+        if await load_dht_key(conn):
+            return {"message": "DHT Key already exists"}
 
-    # TODO: Use a context manager for the router
-    router = await (await conn.new_routing_context()).with_default_safety()
+        async with await get_routing_context(conn) as router:
+            dht_record = await router.create_dht_record(veilid.DHTSchema.dflt(1))
+            _, route_blob = await create_private_route(conn)
+            await router.set_dht_value(dht_record.key, 0, route_blob)
 
-    async with router:
-        dht_record = await router.create_dht_record(veilid.DHTSchema.dflt(1))
-        _, route_blob = await create_private_route(conn)
-        await router.set_dht_value(dht_record.key, 0, route_blob)
+            keypair = KeyPair.from_parts(
+                key=dht_record.owner, secret=dht_record.owner_secret
+            )
 
-    keypair = KeyPair.from_parts(key=dht_record.owner, secret=dht_record.owner_secret)
-
-    await store_dht_key(conn, dht_record.key)
-    await store_dht_key_creds(conn, keypair)
+            await store_dht_key(conn, dht_record.key)
+            await store_dht_key_creds(conn, keypair)
 
     return {"message": "DHT Key generated successfully"}
 
 
 async def retrieve_dht_key() -> dict[str, str]:
-    # TODO: Use a context manager for the connection
-    conn = await get_veilid_conn()
+    async with await get_veilid_conn() as conn:
+        dht_key = await load_dht_key(conn)
 
-    dht_key = await load_dht_key(conn)
-
-    if dht_key is None:
-        return {"message": "DHT Key does not exist"}
-    return {"message": str(dht_key)}
+        if dht_key is None:
+            return {"message": "DHT Key does not exist"}
+        return {"message": str(dht_key)}
 
 
 async def get_dht_value(
@@ -136,19 +140,18 @@ async def get_dht_value(
 
 
 async def app_message(dht_key: str, message: bytes) -> dict[str, str]:
-    conn = await get_veilid_conn()
-    router = await (await conn.new_routing_context()).with_default_safety()
+    async with await get_veilid_conn() as conn:
+        async with await get_routing_context(conn) as router:
+            dht_key = veilid.TypedKey(dht_key)
+            dht_value = await get_dht_value(router, dht_key, 0)
 
-    dht_key = veilid.TypedKey(dht_key)
-    dht_value = await get_dht_value(router, dht_key, 0)
+            if isinstance(dht_value, dict):
+                return dht_value
 
-    if isinstance(dht_value, dict):
-        return dht_value
+            # Private Router to peer
+            prr_peer = await conn.import_remote_private_route(dht_value.data)
 
-    # Private Router to peer
-    prr_peer = await conn.import_remote_private_route(dht_value.data)
+            # Send message to peer
+            await router.app_message(prr_peer, message)
 
-    # Send message to peer
-    await router.app_message(prr_peer, message)
-
-    return {"message": "Message sent successfully"}
+            return {"message": "Message sent successfully"}
