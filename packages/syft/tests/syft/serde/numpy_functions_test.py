64b2be52e5b9283b9e71934c327c49e7aeeeeed6
Kien Dang <mail@kien.ai>
2024-03-06 12:06:08 +0800

    Mark xfail for numpy tests not yet passing for python 3.12

--- packages/syft/tests/syft/serde/numpy_functions_test.py
+++ packages/syft/tests/syft/serde/numpy_functions_test.py
@@ -1,3 +1,6 @@
+# stdlib
+import sys
+
 # third party
 import numpy as np
 import pytest
@@ -10,67 +13,84 @@ PYTHON_ARRAY = [0, 1, 1, 2, 2, 3]
 NP_ARRAY = np.array([0, 1, 1, 5, 5, 3])
 NP_2dARRAY = np.array([[3, 4, 5, 2], [6, 7, 2, 6]])
 
+PYTHON_AT_LEAST_3_12 = sys.version_info >= (3, 12)
+
+NOT_WORK_YET_ON_NUMPY_1_26_PYTHON_3_12: list[tuple[str, str]] = [
+    ("linspace", "10,10,10"),
+    ("logspace", "0,2"),
+    ("unique", "[0, 1, 1, 2, 2, 3]"),
+    ("mean", "[0, 1, 1, 2, 2, 3]"),
+    ("median", "[0, 1, 1, 2, 2, 3]"),
+    ("digitize", "[0, 1, 1, 2, 2, 3], [0,1,2,3]"),
+    ("reshape", "[0, 1, 1, 2, 2, 3], (6,1)"),
+    ("squeeze", "[0, 1, 1, 2, 2, 3]"),
+    ("count_nonzero", "[0, 1, 1, 2, 2, 3]"),
+    ("argwhere", "[0, 1, 1, 2, 2, 3]"),
+    ("argmax", "[0, 1, 1, 2, 2, 3]"),
+    ("argmin", "[0, 1, 1, 2, 2, 3]"),
+    ("sort", "list(reversed([0, 1, 1, 2, 2, 3]))"),
+    ("clip", "[0, 1, 1, 2, 2, 3], 0, 2"),
+    ("put", " np.array([[3, 4, 5, 2], [6, 7, 2, 6]]), [1,2], [7,8]"),
+    ("intersect1d", "[0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3])"),
+    ("setdiff1d", "[0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3])"),
+    ("setxor1d", "[0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3])"),
+    ("hstack", "([0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3]))"),
+    ("vstack", "([0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3]))"),
+    ("allclose", "[0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3]), 0.5"),
+    ("repeat", "2023, 4"),
+    ("std", "[0, 1, 1, 2, 2, 3]"),
+    ("var", "[0, 1, 1, 2, 2, 3]"),
+    ("percentile", "[0, 1, 1, 2, 2, 3], 2"),
+    ("amin", "[0, 1, 1, 2, 2, 3]"),  # alias for min not exist in Syft
+    ("amax", "[0, 1, 1, 2, 2, 3]"),  # alias for max not exist in Syft
+    ("where", "a > 5, a, -1"),  # required condition
+]
+
 
 @pytest.mark.parametrize(
     "func, func_arguments",
     [
         ("array", "[0, 1, 1, 2, 2, 3]"),
-        # ("linspace", "10,10,10"),
         ("arange", "5,10,2"),
-        # ("logspace", "0,2"),
         ("zeros", "(1,2)"),
         ("identity", "4"),
-        # ("unique", "[0, 1, 1, 2, 2, 3]"),
-        # ("mean", "[0, 1, 1, 2, 2, 3]"),
-        # ("median", "[0, 1, 1, 2, 2, 3]"),
-        # ("digitize", "[0, 1, 1, 2, 2, 3], [0,1,2,3]"),
-        # ("reshape", "[0, 1, 1, 2, 2, 3], (6,1)"),
-        # ("squeeze", "[0, 1, 1, 2, 2, 3]"),
-        # ("count_nonzero", "[0, 1, 1, 2, 2, 3]"),
-        # ("argwhere", "[0, 1, 1, 2, 2, 3]"),
-        # ("argmax", "[0, 1, 1, 2, 2, 3]"),
-        # ("argmin", "[0, 1, 1, 2, 2, 3]"),
-        # ("sort", "list(reversed([0, 1, 1, 2, 2, 3]))"),
         ("absolute", "[0, 1, 1, 2, 2, 3]"),
-        # ("clip", "[0, 1, 1, 2, 2, 3], 0, 2"),
-        # ("put", " np.array([[3, 4, 5, 2], [6, 7, 2, 6]]), [1,2], [7,8]"),
-        # ("intersect1d", "[0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3])"),
-        # ("setdiff1d", "[0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3])"),
-        # ("setxor1d", "[0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3])"),
-        # ("hstack", "([0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3]))"),
-        # ("vstack", "([0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3]))"),
-        # ("allclose", "[0, 1, 1, 2, 2, 3], np.array([0, 1, 1, 5, 5, 3]), 0.5"),
         ("equal", "[0, 1, 1, 2, 2, 3], [0, 1, 1, 2, 2, 3]"),
-        # ("repeat", "2023, 4"),
-        # ("std", "[0, 1, 1, 2, 2, 3]"),
-        # ("var", "[0, 1, 1, 2, 2, 3]"),
-        # ("percentile", "[0, 1, 1, 2, 2, 3], 2"),
-        # ("amin", "[0, 1, 1, 2, 2, 3]"),  # alias for min not exist in Syft
-        # ("amax", "[0, 1, 1, 2, 2, 3]"),  # alias for max not exist in Syft
-        # ("where", "a > 5, a, -1"),  # required condition
-        #     # Not Working
-        # pytest.param(
-        #     "hsplit",
-        #     "np.array([[3, 4, 5, 2], [6, 7, 2, 6]]), 4",
-        #     marks=pytest.mark.xfail(
-        #         raises=ValueError, reason="Value error insinde Syft"
-        #     ),
-        # ),
-        # pytest.param(
-        #     "vsplit",
-        #     "np.array([[3, 4, 5, 2], [6, 7, 2, 6]]), 2",
-        #     marks=pytest.mark.xfail(
-        #         raises=ValueError, reason="Value error insinde Syft"
-        #     ),
-        # ),
-        # pytest.param(
-        #     "unique",
-        #     "np.array([0, 1, 1, 5, 5, 3]), return_counts=True",
-        #     marks=pytest.mark.xfail(
-        #         raises=(ValueError, AssertionError),
-        #         reason="Kwargs Can not be properly unpacked",
-        #     ),
-        # ),
+        pytest.param(
+            "hsplit",
+            "np.array([[3, 4, 5, 2], [6, 7, 2, 6]]), 4",
+            marks=pytest.mark.xfail(
+                raises=ValueError if not PYTHON_AT_LEAST_3_12 else AssertionError,
+                reason="Value error inside Syft",
+            ),
+        ),
+        pytest.param(
+            "vsplit",
+            "np.array([[3, 4, 5, 2], [6, 7, 2, 6]]), 2",
+            marks=pytest.mark.xfail(
+                raises=ValueError if not PYTHON_AT_LEAST_3_12 else AssertionError,
+                reason="Value error insinde Syft",
+            ),
+        ),
+        pytest.param(
+            "unique",
+            "np.array([0, 1, 1, 5, 5, 3]), return_counts=True",
+            marks=pytest.mark.xfail(
+                raises=(ValueError, AssertionError),
+                reason="Kwargs Can not be properly unpacked",
+            ),
+        ),
+    ]
+    + [
+        pytest.param(
+            func,
+            func_arguments,
+            marks=pytest.mark.xfail(
+                PYTHON_AT_LEAST_3_12,
+                reason="Does not work yet on Python>=3.12 and numpy>=1.26",
+            ),
+        )
+        for func, func_arguments in NOT_WORK_YET_ON_NUMPY_1_26_PYTHON_3_12
     ],
 )
 def test_numpy_functions(func, func_arguments, request):
