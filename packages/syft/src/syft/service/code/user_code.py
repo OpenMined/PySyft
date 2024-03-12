23a8118bbfd7f88b8f58250e63b7ab920eba7d11
Shubham Gupta <shubhamgupta3121@gmail.com>
2024-03-12 11:17:46 +0530

    revert version 2 to version for newly defined classes

--- packages/syft/src/syft/service/code/user_code.py
+++ packages/syft/src/syft/service/code/user_code.py
@@ -42,6 +42,7 @@ from ...serde.serialize import _serialize
 from ...store.document_store import PartitionKey
 from ...store.linked_obj import LinkedObject
 from ...types.datetime import DateTime
+from ...types.syft_object import SYFT_OBJECT_VERSION_1
 from ...types.syft_object import SYFT_OBJECT_VERSION_2
 from ...types.syft_object import SYFT_OBJECT_VERSION_4
 from ...types.syft_object import SyftObject
@@ -109,7 +110,7 @@ class UserCodeStatus(Enum):
 @serializable()
 class UserCodeStatusCollection(SyftObject):
     __canonical_name__ = "UserCodeStatusCollection"
-    __version__ = SYFT_OBJECT_VERSION_2
+    __version__ = SYFT_OBJECT_VERSION_1
 
     __repr_attrs__ = ["approved", "status_dict"]
 
@@ -1266,7 +1267,7 @@ class UserCodeExecutionResult(SyftObject):
 class UserCodeExecutionOutput(SyftObject):
     # version
     __canonical_name__ = "UserCodeExecutionOutput"
-    __version__ = SYFT_OBJECT_VERSION_2
+    __version__ = SYFT_OBJECT_VERSION_1
 
     id: UID
     user_code_id: UID
