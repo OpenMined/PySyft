8e8ad983460616583fca461e0fce83f1ab8d3054
khoaguin <dkn.work@protonmail.com>
2024-03-19 14:48:13 +0700

    [tests/integration] remove the dataset after uploaded to the domain in `test_dataset_search`
    Co-authored-by: Shubham Gupta <shubhamgupta3121@gmail.com>

--- tests/integration/network/gateway_test.py
+++ tests/integration/network/gateway_test.py
@@ -119,29 +119,29 @@ def test_dataset_search(domain_1_port, gateway_port):
         "syft.client.registry.NetworkRegistry.load_network_registry_json",
         return_value=mock_load_network_registry_json("localhost", gateway_port),
     ):
+        # login to the domain and gateway
         gateway_client: GatewayClient = sy.login_as_guest(port=gateway_port)
-
         domain_client: DomainClient = sy.login(
             port=domain_1_port, email="info@openmined.org", password="changethis"
         )
 
+        # connect the domain to the gateway
         result = domain_client.connect_to_gateway(gateway_client)
         assert isinstance(result, SyftSuccess)
-
         assert len(sy.gateways.all_networks) == len(sy.gateways.online_networks) == 1
         assert len(sy.domains.all_domains) == len(sy.domains.online_domains) == 1
 
+        # the domain client uploads a dataset
         input_data = np.array([1, 2, 3])
         mock_data = np.array([4, 5, 6])
-
         asset_name = _random_hash()
         asset = sy.Asset(name=asset_name, data=input_data, mock=mock_data)
         dataset_name = _random_hash()
         dataset = sy.Dataset(name=dataset_name, asset_list=[asset])
-
         dataset_res = domain_client.upload_dataset(dataset)
         assert isinstance(dataset_res, SyftSuccess)
 
+        # test if the dataset can be searched by the syft network
         right_search = sy.search(dataset_name)
         assert isinstance(right_search, SearchResults)
         assert len(right_search) == 1
@@ -151,8 +151,12 @@ def test_dataset_search(domain_1_port, gateway_port):
         assert isinstance(dataset.assets[0].mock, np.ndarray)
         assert dataset.assets[0].data is None
 
+        # search a wrong dataset should return an empty list
         wrong_search = sy.search(_random_hash())
         assert len(wrong_search) == 0
+
+        # the domain client delete the dataset
+        domain_client.api.services.dataset.delete_dataset(uid=dataset.id)
 
 
 def test_domain_gateway_user_code(domain_1_port, gateway_port):
@@ -160,24 +164,23 @@ def test_domain_gateway_user_code(domain_1_port, gateway_port):
         "syft.client.registry.NetworkRegistry.load_network_registry_json",
         return_value=mock_load_network_registry_json("localhost", gateway_port),
     ):
+        # login to the domain and gateway
         gateway_client: GatewayClient = sy.login_as_guest(port=gateway_port)
-
         domain_client: DomainClient = sy.login(
             port=domain_1_port, email="info@openmined.org", password="changethis"
         )
 
+        # the domain client uploads a dataset
         input_data = np.array([1, 2, 3])
         mock_data = np.array([4, 5, 6])
-
         asset_name = _random_hash()
         asset = sy.Asset(name=asset_name, data=input_data, mock=mock_data)
         dataset_name = _random_hash()
         dataset = sy.Dataset(name=dataset_name, asset_list=[asset])
-
         dataset_res = domain_client.upload_dataset(dataset)
-
         assert isinstance(dataset_res, SyftSuccess)
 
+        # the domain client registers a data data scientist account on its domain
         random_name: str = str(_random_hash())
         user_create_res = domain_client.register(
             name=random_name,
@@ -187,20 +190,21 @@ def test_domain_gateway_user_code(domain_1_port, gateway_port):
             institution="Caltech",
             website="https://www.caltech.edu/",
         )
-
         assert isinstance(user_create_res, SyftSuccess)
 
+        # the domain client connects to the gateway
         gateway_con_res = domain_client.connect_to_gateway(gateway_client)
         assert isinstance(gateway_con_res, SyftSuccess)
 
+        # get the proxy client to the domain, login to the data scientist account
         proxy_client = gateway_client.domains[0]
-
         proxy_ds = proxy_client.login(
             email=f"{random_name}@caltech.edu",
             password="changethis",
             password_verify="changethis",
         )
 
+        # submits a request for code execution
         asset = proxy_ds.datasets[0].assets[0]
 
         @sy.syft_function_single_use(asset=asset)
@@ -208,17 +212,15 @@ def test_domain_gateway_user_code(domain_1_port, gateway_port):
             return asset + 1
 
         mock_function.code = dedent(mock_function.code)
-
         request_res = proxy_ds.code.request_code_execution(mock_function)
         assert isinstance(request_res, Request)
 
+        # domain client approves the request
         assert len(domain_client.requests.get_all()) == 1
-
         req_approve_res = domain_client.requests[-1].approve()
         assert isinstance(req_approve_res, SyftSuccess)
 
+        # the proxy data scientist client executes the code and gets the result
         result = proxy_ds.code.mock_function(asset=asset)
-
         final_result = result.get()
-
         assert (final_result == input_data + 1).all()
