b6070cfdb72547922dbc5d6de096e18cc40700a7
teo <teo.milea97@gmail.com>
2024-04-22 16:24:29 +0300

    added download fonts script in job repr

--- packages/syft/src/syft/service/job/job_stash.py
+++ packages/syft/src/syft/service/job/job_stash.py
@@ -778,7 +778,27 @@ class Job(SyncableSyftObject):
         {onclick_html}
         """
 
+        download_fonts = """
+<script>
+function addCss(fileName) {
+
+  var head = document.head;
+  var link = document.createElement("link");
+
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.href = fileName;
+
+  head.appendChild(link);
+}
+
+addCss("https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap");
+
+</script>
+        """
+
         repr_html = f"""
+        {download_fonts}
         <style>
         {style}
         </style>
