08f6c8b96ff1a2fdf0ce7084964f24e0b140d993
Koen van der Veen <koenlennartvanderveen@gmail.com>
2024-04-22 15:17:58 +0200

    css

--- packages/syft/src/syft/util/notebook_ui/notebook_addons.py
+++ packages/syft/src/syft/util/notebook_ui/notebook_addons.py
@@ -8,6 +8,24 @@ from typing import Any
 from ...types.uid import UID
 
 CSS_CODE = """
+<script>
+function addCss(fileName) {
+
+  var head = document.head;
+  var link = document.createElement("link");
+
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.href = fileName;
+
+  head.appendChild(link);
+}
+
+addCss("https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap");
+
+</script>
+
+
 <style>
   body[data-jp-theme-light='false'] {
         --primary-color: #111111;
