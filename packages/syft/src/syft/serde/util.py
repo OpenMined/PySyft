2a8ef5d9f504e116ca6fda51720e45c88727636c
Madhava Jay <me@madhavajay.com>
2024-04-24 17:41:11 +1000

    Disabled new serde optimization which makes SyftAPI / login slow

--- packages/syft/src/syft/serde/util.py
+++ packages/syft/src/syft/serde/util.py
@@ -3,4 +3,5 @@ from sys import platform
 
 
 def compatible_with_large_file_writes_capnp() -> bool:
-    return platform not in ["darwin", "win32"]
+    return False
+    # return platform not in ["darwin", "win32"]
