2bf8d4b4bc015d97772487b5a607883c79a6d162
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-04-28 19:14:11 +0200

    Update setup.py
    Lint

--- packages/syft/setup.py
+++ packages/syft/setup.py
@@ -15,7 +15,9 @@ def install(package):
 if __name__ == "__main__":
     setuptools.setup()
     try:
+        # third party     
         import google.colab
+     
         install("numpy==1.25.2")  # version for Google Colab
     except ImportError:
         pass  # version specified in setup.cfg will be used for other environments
