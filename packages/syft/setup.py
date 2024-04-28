4076bbe3e74c3f2944f0992d7d4e5a680cb1efbc
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-04-28 19:11:38 +0200

    Update setup.py

--- packages/syft/setup.py
+++ packages/syft/setup.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
-import setuptools
+# stdlib
 import os
 import subprocess
+ 
+# third party
+import setuptools
+
 
 def install(package):
     subprocess.call([sys.executable, "-m", "pip", "install", package])
+
 
 if __name__ == "__main__":
     setuptools.setup()
     try:
         import google.colab
-        install('numpy==1.25.2')  # version for Google Colab
+        install("numpy==1.25.2")  # version for Google Colab
     except ImportError:
         pass  # version specified in setup.cfg will be used for other environments
