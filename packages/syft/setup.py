fde935bae03c0db9337b0837043cce51e019f4e4
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-04-28 19:06:29 +0200

    Update setup.py
    Google Colab compatibility

--- packages/syft/setup.py
+++ packages/syft/setup.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
-# third party
 import setuptools
+import os
+import subprocess
+
+def install(package):
+    subprocess.call([sys.executable, "-m", "pip", "install", package])
 
 if __name__ == "__main__":
     setuptools.setup()
+    try:
+        import google.colab
+        install('numpy==1.25.2')  # version for Google Colab
+    except ImportError:
+        pass  # version specified in setup.cfg will be used for other environments
