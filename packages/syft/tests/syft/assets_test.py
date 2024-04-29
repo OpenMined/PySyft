519bfac5eb8d400e52fc0f6525a6a2d1bbcd7c6d
eelcovdw <eelcovdw@gmail.com>
2024-04-29 20:51:00 +0200

    fix tests

--- packages/syft/tests/syft/assets_test.py
+++ packages/syft/tests/syft/assets_test.py
@@ -5,24 +5,24 @@ import pytest
 from syft.util.assets import load_css
 from syft.util.assets import load_png_base64
 from syft.util.assets import load_svg
+from syft.util.notebook_ui.icons import Icon
 
 
-def test_load_png_base64():
+def test_load_assets():
     png = load_png_base64("logo.png")
     assert isinstance(png, str)
 
     with pytest.raises(FileNotFoundError):
         load_png_base64("non_existent.png")
 
-
-def test_load_svg():
-    svg = load_svg("copy.svg").strip()
+    svg = load_svg("copy.svg")
     assert isinstance(svg, str)
 
-    assert svg.startswith("<svg")
-    assert svg.endswith("</svg>")
-
-
-def test_load_css():
     css = load_css("style.css")
     assert isinstance(css, str)
+
+
+def test_icons():
+    for icon in Icon:
+        assert isinstance(icon.svg, str)
+        assert isinstance(icon.js_escaped_svg, str)
