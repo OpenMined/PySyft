79bcc23c1f5cedff639c7712b63a1cc54ce77628
Shubham Gupta <shubhamgupta3121@gmail.com>
2024-05-02 20:15:21 +0530

    update configmap to seperate path of conf loaded and used in rathole add a script to copy toml file from loaded path to used path cleanup start.sh

--- packages/grid/rathole/start.sh
+++ packages/grid/rathole/start.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
-
-APP_MODULE=server.main:app
-LOG_LEVEL=${LOG_LEVEL:-info}
-HOST=${HOST:-0.0.0.0}
-PORT=${APP_PORT:-5555}
-RELOAD="--reload"
-DEBUG_CMD=""
 RATHOLE_MODE=${RATHOLE_MODE:-server}
 
-apt update && apt install -y nginx
-nginx & exec python -m $DEBUG_CMD uvicorn $RELOAD --host $HOST --port $APP_PORT --log-level $LOG_LEVEL "$APP_MODULE" &
+cp -L -r -f /conf/* conf/
 
+#!/bin/bash
 if [[ $RATHOLE_MODE == "server" ]]; then
-  /app/rathole server.toml
+  /app/rathole conf/server.toml &
 elif [[ $RATHOLE_MODE = "client" ]]; then
-  /app/rathole client.toml
+  /app/rathole conf/client.toml &
 else
   echo "RATHOLE_MODE is set to an invalid value. Exiting."
 fi
+
+while true
+do
+    # Execute your script here
+    cp -L -r -f /conf/* conf/
+    # Sleep for 10 seconds
+    sleep 10
+done
