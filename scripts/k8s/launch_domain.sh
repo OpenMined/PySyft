2c15bd0f152c41855c63c4a1da20fa5b1f124597
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2024-05-08 10:06:10 +0530

    added convinient scripts for launching multiple stacks of domain, enclave, gateway

--- scripts/k8s/launch_domain.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Domain Node
-bash -c '\
-    export CLUSTER_NAME=testdomain1 CLUSTER_HTTP_PORT=9082 && \
-    tox -e dev.k8s.start && \
-    tox -e dev.k8s.hotreload'
--- scripts/k8s/launch_enclave.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Enclave Node
-bash -c '\
-    export CLUSTER_NAME=testenclave1 CLUSTER_HTTP_PORT=9083 DEVSPACE_PROFILE=enclave && \
-    tox -e dev.k8s.start -- --volume /sys/kernel/security:/sys/kernel/security --volume /dev/tmprm0:/dev/tmprm0 && \
-    tox -e dev.k8s.hotreload'
