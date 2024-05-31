e7c7e7495aa653f6b3d9b1d72b307cb40a5827aa
Thiago Costa Porto <thiagocostaporto@gmail.com>
2024-05-31 17:16:27 +0200

    wip: feat(syft_exc): trim it down

--- packages/syft/src/syft/service/errors.py
+++ packages/syft/src/syft/service/errors.py
@@ -1,144 +1,66 @@
 # stdlib
-from enum import Enum
 from typing import Literal
 
 # syft absolute
-from syft.service.context import AuthedServiceContext
 from syft.service.user.user_roles import ServiceRole
 
-class SyftErrorCodes(Enum):
-    INVALID_WORKER_POOL = "invalid-worker-pool",
-    PERMISSION_ERROR = "permission-error",
-    USER_CODE_NOT_APPROVED = "user-code-not-approved"
+SyftErrorCodes = Literal[
+    'not-found',
+    'not-permitted',
+    'stash-error',
+    'worker',
+    'worker-invalid-pool',
+    'usercode-not-approved'
+]
 
 SyftErrorVisibility = tuple[str, ServiceRole, str | None] # message, role, public_message
 
 syft_errors: dict[SyftErrorCodes, SyftErrorVisibility] = {
-    SyftErrorCodes.INVALID_WORKER_POOL: (
-        "You tried to run a syft function attached to a worker pool in blocking mode,"
-        " which is currently not supported. Run your function with `blocking=False` to run"
-        " as a job on your worker poolInvalid worker",
+    'not-permitted': (
+        "You do not have permission to perform this operation.",
         ServiceRole.ADMIN,
         None
     ),
-    SyftErrorCodes.PERMISSION_ERROR: (
-        "",
-        ServiceRole.ADMIN,
-        "You do not have permission to perform this operation.",
+    'not-found': (
+        "The requested object was not found.",
+        ServiceRole.DATA_SCIENTIST,
+        None
     ),
-    SyftErrorCodes.USER_CODE_NOT_APPROVED: (
+    'worker-invalid-pool': (
+        (
+            "You tried to run a syft function attached to a worker pool in blocking mode,"
+            " which is currently not supported. Run your function with `blocking=False` to run"
+            " as a job on your worker pool"
+        ),
+        ServiceRole.ADMIN,
+        None
+    ),
+    'usercode-not-approved': (
         "Your code has not been approved yet.",
         ServiceRole.DATA_SCIENTIST,
         None
-    )
+    ),
+
 }
 
-
-class SyftPublicException(Exception):
+class SyftError(Exception):
     def __init__(
         self,
-        /,
-        context: AuthedServiceContext,
-        message: str,
+        message: str | None = None,
         code: SyftErrorCodes | None = None,
         public_message: str | None = None,
-        min_visible_role: ServiceRole = ServiceRole.NONE,
-        *args,
-        **kwargs,
-    ) -> None:
-        is_role_type_wrong = type(min_visible_role) is not ServiceRole
-        is_not_visible_to_role = context.role.value < min_visible_role.value
-
-        if is_role_type_wrong or is_not_visible_to_role or context.is_admin:
-            message = public_message if public_message else "An error occurred. Contact the admin."
-        else:
-            message = message if not code else f"{message} [code: {code}]"
-
-        super().__init__(message, *args, **kwargs)
-
-
-class SyftException(Exception):
-    def __init__(
-        self,
-        message: str,
-        /,
-        code: SyftErrorCodes | None = None,
-        public_message: str | None = None,
-        min_visible_role: ServiceRole | None = ServiceRole.ADMIN,
-        *args,
-        **kwargs,
+        min_visible_role: ServiceRole = ServiceRole.ADMIN,
     ) -> None:
         self.message = message
         self.code = code
         self.public_message = public_message
         self.min_visible_role = min_visible_role
-        super().__init__(message, *args, **kwargs)
+        super().__init__(message)
 
+    @property
+    def public(self):
+        return self.public_message if self.public_message else self.message
 
-class WorkerError(SyftException):
-    def __init__(
-        self,
-        /,
-        code: SyftErrorCodes,
-        message: str | None = None,
-        *args,
-        **kwargs,
-    ) -> None:
-        original_message, min_role, public_message = syft_errors[code]
-
-        if message is None:
-            message = original_message
-
-        super().__init__(
-            message=message,
-            code=code,
-            public_message=public_message,
-            min_visible_role=min_role,
-            *args,
-            **kwargs,
-        )
-
-class PermissionError(SyftException):
-    def __init__(
-        self,
-        /,
-        message: str,
-        public_message: str | None = None,
-        *args,
-        **kwargs,
-    ) -> None:
-        _, min_role, _= syft_errors["permission-error"]
-
-        super().__init__(
-            message,
-            code="permission-error",
-            public_message=public_message,
-            min_visible_role=min_role,
-            *args,
-            **kwargs,
-        )
-
-class UserCodeError(SyftException):
-    def __init__(
-        self,
-        /,
-        message: str,
-        code: SyftErrorCodes,
-        public_message: str | None = None,
-        *args,
-        **kwargs,
-    ) -> None:
-        message, min_role, public_message = syft_errors[code]
-        super().__init__(
-            message,
-            code=code,
-            public_message=public_message,
-            min_visible_role=min_role,
-            *args,
-            **kwargs,
-        )
-
-class InvalidWorkerPoolException(WorkerException):
-    def __init__(self, message: str | None = None, *args, **kwargs):
-        super().__init__(message=message, code="invalid-worker-pool", *args, **kwargs)
+    def can_see_message(self, role: ServiceRole) -> bool:
+        return role.value >= self.min_visible_role.value
 
