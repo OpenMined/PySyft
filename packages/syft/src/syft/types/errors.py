f842868b3b62b4053b1c7be7fc6d290af066d070
Thiago Costa Porto <thiagocostaporto@gmail.com>
2024-06-02 14:43:32 +0200

    lint: ruff

--- packages/syft/src/syft/types/errors.py
+++ packages/syft/src/syft/types/errors.py
@@ -1,23 +1,25 @@
 # stdlib
 from typing import Literal
 
+# relative
 from ..service.user.user_roles import ServiceRole
 
 SyftErrorCodes = Literal[
-    'not-found',
-    'not-permitted',
-    'database-error',
-    'serde-serialization-error',
-    'serde-deserialization-error',
-    'stash-error',
-    'worker-invalid-pool',
-    'usercode-not-approved',
-    'usercode-bad-input-policy',
-    'usercode-bad-output-policy'
+    "not-found",
+    "not-permitted",
+    "database-error",
+    "serde-serialization-error",
+    "serde-deserialization-error",
+    "stash-error",
+    "worker-invalid-pool",
+    "usercode-not-approved",
+    "usercode-bad-input-policy",
+    "usercode-bad-output-policy",
 ]
 
+
 class SyftError(Exception):
-    __match_args__ = ('code')
+    __match_args__ = "code"
 
     def __init__(
         self,
@@ -37,9 +39,11 @@ class SyftError(Exception):
     @property
     def public(self):
         if self.private:
-            return self.public_message or "An error occurred. Contact your admininstrator for more information."
+            return (
+                self.public_message
+                or "An error occurred. Contact your admininstrator for more information."
+            )
         return self.public_message or self.message
 
     def can_see_message(self, role: ServiceRole) -> bool:
         return role.value >= self.min_visible_role.value
-
