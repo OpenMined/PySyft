a23122100ff70ddc88d9c76f634721cec49b090a
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-06-05 10:55:08 +0200

    Update start.sh

--- packages/grid/backend/grid/start.sh
+++ packages/grid/backend/grid/start.sh
@@ -5,14 +5,7 @@ echo "Running Syft with RELEASE=${RELEASE} and $(id)"
 
 APP_MODULE=grid.main:app
 LOG_LEVEL=${LOG_LEVEL:-info}
-if grep -q ":" /proc/net/if_inet6 ; then
-    # IPv6 is available
-    HOST=${HOST:-[::]}
-else
-    # Only IPv4 is available
-    HOST=${HOST:-0.0.0.0}
-fi
-
+HOST=${HOST:-[::]}
 PORT=${PORT:-80}
 NODE_TYPE=${NODE_TYPE:-domain}
 APPDIR=${APPDIR:-$HOME/app}
