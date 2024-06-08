6904715299c2fcf7f6a898ae9fb67eae2448ec74
Tauquir <30658453+itstauq@users.noreply.github.com>
2024-06-08 23:18:56 +0530

    Refactoring

--- packages/syft/src/syft/service/project/distributed_project.py
+++ packages/syft/src/syft/service/project/distributed_project.py
@@ -1,19 +1,24 @@
 # stdlib
-from dataclasses import dataclass
-from dataclasses import field
 from typing import Any
 
 # third party
+from pydantic import BaseModel
+from pydantic import ConfigDict
+from pydantic import Field
 from pydantic import field_validator
+from pydantic import model_validator
 from typing_extensions import Self
 
 # relative
 from ...client.api import NodeIdentity
 from ...client.client import SyftClient
 from ...client.client import SyftClientSessionCache
+from ...types.uid import UID
 from ...util import options
 from ...util.colors import SURFACE
 from ..code.user_code import SubmitUserCode
+from ..code.user_code import UserCode
+from ..enclave.enclave import EnclaveInstance
 from ..metadata.node_metadata import NodeMetadata
 from ..request.request import Request
 from ..request.request import RequestStatus
@@ -24,25 +29,21 @@ from .project import ProjectRequest
 from .project import ProjectSubmit
 
 
-@dataclass
-class DistributedProject:
+class DistributedProject(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
     name: str
     description: str = ""
-    code: SubmitUserCode | None = None  # only one code per project for this prototype
-    clients: list[SyftClient] | None = None  # List of member clients, not persisted
-    members: list[NodeIdentity] | None = (
-        None  # List of member client identities, persisted
-    )
-    created_by: str | None = None
-    username: str | None = None
-    all_projects: list[Project] = field(default_factory=list)
-    project_permissions: set[str] = field(default_factory=set)  # Unused at the moment
+    code: UserCode | SubmitUserCode  # only one code per project for this prototype
+    clients: dict[UID, SyftClient] = Field(default_factory=dict)
+    members: dict[UID, NodeIdentity] = Field(default_factory=dict)
+    all_projects: list[Project] = Field(default_factory=list)
+    project_permissions: set[str] = Field(default_factory=set)  # Unused at the moment
 
     def _coll_repr_(self) -> dict:
         return {
             "name": self.name,
             "description": self.description,
-            "created by": self.created_by,
             "pending requests": self.pending_requests,
         }
 
@@ -56,35 +57,26 @@ class DistributedProject:
             + "<div class='syft-project'>"
             + f"<h3>{self.name}</h3>"
             + f"<p>{self.description}</p>"
-            + f"<p><strong>Created by: </strong>{self.username} ({self.created_by})</p>"
             + self.requests._repr_html_()
             + "</div>"
         )
 
-    def __post_init__(self) -> None:
-        if self.code:
-            self.add_code(self.code)
-
     @classmethod
     def get_by_name(cls, name: str, clients: list[SyftClient]) -> Self:
-        instance = cls(name=name)
+        all_projects = []
         for client in clients:
             project = client.projects.get_by_name(name)
             if isinstance(project, SyftError):
                 raise SyftException(project.message)
-            instance.all_projects.append(project)
+            all_projects.append(project)
 
         # TODO verify that all the projects in the `all_projects` list are the same
-        project = instance.all_projects[0]
-
-        instance.description = project.description
-        instance.code = project.requests[0].code  # TODO fix possible errors
-        instance.clients = clients
-        instance.members = list(map(instance._to_node_identity, clients))
-        instance.created_by = project.created_by
-        instance.username = project.username
-
-        return instance
+        project = all_projects[0]
+        description = project.description
+        code = project.requests[0].code  # TODO fix possible errors
+        return cls(
+            name=name, description=description, code=code, all_projects=all_projects
+        )
 
     @property
     def requests(self) -> list[Request]:
@@ -103,18 +95,41 @@ class DistributedProject:
             [request.status == RequestStatus.PENDING for request in self.requests]
         )
 
-    def add_code(self, code: SubmitUserCode) -> None:
-        self.code = code
-        self.clients = self._get_clients_from_code(code)
-        self.members = list(map(self._to_node_identity, self.clients))
-        self.created_by = self.clients[0].logged_in_user
-        self.username = self.clients[0].logged_in_username
+    @field_validator("code", mode="before")
+    @classmethod
+    def verify_code(cls, code: SubmitUserCode) -> SubmitUserCode:
+        if not code.deployment_policy_init_kwargs:
+            raise ValueError("Deployment policy not found in code.")
+        provider = code.deployment_policy_init_kwargs.get("provider")
+        if not provider:
+            raise ValueError("Provider not found in deployment policy.")
+        if not isinstance(provider, EnclaveInstance):
+            raise SyftException(
+                "Only `EnclaveInstance` is supported as provider for now."
+            )
+        return code
+
+    @field_validator("clients", mode="before")
+    @classmethod
+    def verify_clients(cls, val: list[SyftClient]) -> list[SyftClient]:
+        # SyftClients must be logged in by the same emails
+        if len(val) > 0:
+            emails = {client.logged_in_user for client in val}
+            if len(emails) > 1:
+                raise ValueError(
+                    f"All clients must be logged in from the same account. Found multiple: {emails}"
+                )
+        return val
+
+    @model_validator(mode="after")
+    def _populate_auto_generated_fields(self) -> Self:
+        self.clients = self._get_clients_from_code()
+        self.members = self._get_members_from_clients()
+        return self
 
     def submit(self) -> Self:
-        if not self.code:
-            raise SyftException("Cannot submit project without code.")
-        self._pre_submit_checks(self.clients)
-        projects_map = self._submit_project_to_all_clients(self.clients)
+        self._pre_submit_checks()
+        projects_map = self._submit_project_to_all_clients()
         self.all_projects = list(projects_map.values())
         return self
 
@@ -129,44 +144,37 @@ class DistributedProject:
         # 7. Cleanup the Enclave
         pass
 
-    @staticmethod
-    def _get_clients_from_code(code: SubmitUserCode) -> list[SyftClient]:
-        if not code.input_policy_init_kwargs:
-            return []
+    def _get_clients_from_code(self) -> dict[UID, SyftClient]:
+        if not self.code or not self.code.input_policy_init_kwargs:
+            return {}
 
-        clients = [
-            client
-            for policy in code.input_policy_init_kwargs.keys()
+        clients = {
+            policy.node_id: client
+            for policy in self.code.input_policy_init_kwargs.keys()
             if (
                 client := SyftClientSessionCache.get_client_for_node_uid(policy.node_id)
             )
-        ]
+        }
         return clients
 
-    @field_validator("clients", mode="before")
-    @classmethod
-    def verify_clients(cls, val: list[SyftClient]) -> list[SyftClient]:
-        # SyftClients must be logged in by the same emails
-        if len(val) > 0:
-            emails = {client.logged_in_user for client in val}
-            if len(emails) > 1:
-                raise ValueError(
-                    f"All clients must be logged in from the same account. Found multiple: {emails}"
-                )
-        return val
+    def _get_members_from_clients(self) -> dict[UID, NodeIdentity]:
+        return {
+            node_id: self._to_node_identity(client)
+            for node_id, client in self.clients.items()
+        }
 
     @staticmethod
-    def _to_node_identity(val: SyftClient) -> NodeIdentity:
-        if isinstance(val, SyftClient) and val.metadata is not None:
-            metadata = val.metadata.to(NodeMetadata)
+    def _to_node_identity(client: SyftClient) -> NodeIdentity:
+        if isinstance(client, SyftClient) and client.metadata is not None:
+            metadata = client.metadata.to(NodeMetadata)
             return metadata.to(NodeIdentity)
         else:
-            raise SyftException(f"members must be SyftClient. Received: {type(val)}")
+            raise SyftException(f"members must be SyftClient. Received: {type(client)}")
 
-    def _pre_submit_checks(self, clients: list[SyftClient]) -> bool:
+    def _pre_submit_checks(self) -> bool:
         try:
             # Check if the user can create projects
-            for client in clients:
+            for client in self.clients.values():
                 result = client.api.services.project.can_create_project()
                 if isinstance(result, SyftError):
                     raise SyftException(result.message)
@@ -175,11 +183,9 @@ class DistributedProject:
 
         return True
 
-    def _submit_project_to_all_clients(
-        self, clients: list[SyftClient]
-    ) -> dict[SyftClient, Project]:
+    def _submit_project_to_all_clients(self) -> dict[SyftClient, Project]:
         projects_map: dict[SyftClient, Project] = {}
-        for client in clients:
+        for client in self.clients.values():
             # Creating projects and code requests separately across each client
             # TODO handle failures in between
             new_project = ProjectSubmit(
