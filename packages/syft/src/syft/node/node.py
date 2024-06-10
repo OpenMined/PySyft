f36a5cc2b8f9601297547a9cb21f767723a5b369
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-06-10 14:57:43 +0200

    Update node.py

--- packages/syft/src/syft/node/node.py
+++ packages/syft/src/syft/node/node.py
@@ -575,9 +575,10 @@ class Node(AbstractNode):
                 address: str | None = producer.address
             else:
                 port = queue_config.client_config.queue_port
-                address: str | None = producer.address = (
-                    get_queue_address(port) if port is not None else None
-                )
+                if port is not None:
+                    address = get_queue_address(port)
+                else:
+                    address = None
 
             if address is None and queue_config.client_config.n_consumers > 0:
                 raise ValueError("address unknown for consumers")
