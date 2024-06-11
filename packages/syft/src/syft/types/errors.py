58036bc9e584779174cf709ae000cc82de075448
Thiago Costa Porto <thiagocostaporto@gmail.com>
2024-06-11 11:25:11 +0200

    wip: feat(syft_exc): add context and get_message
    co-authored-by: Koen van der Veen <koenlennartvanderveen@gmail.com>

--- packages/syft/src/syft/types/errors.py
+++ packages/syft/src/syft/types/errors.py
@@ -1,4 +1,10 @@
+from syft.service.context import AuthedServiceContext
+from syft.service.user.user_roles import ServiceRole
+
 class SyftException(Exception):
-    def public(self) -> str:
+    def public(self, context: AuthedServiceContext) -> str:
         return "An error occurred. Contact your admininstrator for more information."
 
+    def get_message(self, context: AuthedServiceContext):
+        if context.role.value <= ServiceRole.DATA_OWNER.value:
+            return self.public(context)
