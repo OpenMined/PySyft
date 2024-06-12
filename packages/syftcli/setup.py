a5f5f32bf60c030d4d8a3aff3aa23bc80b6cb2c8
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-06-12 20:45:00 +0200

    Update setup.py

--- packages/syftcli/setup.py
+++ packages/syftcli/setup.py
@@ -7,9 +7,9 @@ __version__ = "0.1.11"
 packages = [
     "requests==2.32.3",
     "pyyaml==6.0.1",
-    "packaging==21.3",
-    "typer[all]==0.9.0",
-    "typing_extensions==4.6.3",
+    "packaging==24.0",
+    "typer[all]==0.12.3",
+    "typing_extensions==4.12.1",
 ]
 
 build_packages = [
