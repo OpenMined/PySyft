c93b8ffc9f71754d87863f22c9478b71182ab8e5
Thiago Costa Porto <thiagocostaporto@gmail.com>
2024-06-12 12:52:50 +0200

    feat(syft_exc): custom exception

--- packages/syft/src/syft/types/errors.py
+++ packages/syft/src/syft/types/errors.py
@@ -4,9 +4,51 @@ from ..service.user.user_roles import ServiceRole
 
 
 class SyftException(Exception):
-    def public(self, context: AuthedServiceContext) -> str:
-        return "An error occurred. Contact your admininstrator for more information."
+    """
+    A Syft custom exception class with distinct public and private messages.
+    
+    Attributes:
+        private_message (str): Detailed error message intended for administrators.
+        public_message (str): General error message for end-users.
+    """
 
-    def get_message(self, context: AuthedServiceContext):
-        if context.role.value <= ServiceRole.DATA_OWNER.value:
-            return self.public(context)
+    public_message = "An error occurred. Contact the admin for more information."
+
+    def __init__(
+        self, private_message: str, public_message: str | None = None, *args, **kwargs
+    ):
+        if public_message:
+            self.public_message = public_message
+        self.__private_message = private_message
+        super().__init__(self.public, *args, **kwargs)
+
+    @property
+    def public(self) -> str:
+        """
+        Returns the public error message.
+
+        Returns:
+            str: The public error message.
+        """
+        return self.public_message
+
+    def get_message(self, context: AuthedServiceContext) -> str:
+        """
+        Retrieves the appropriate message based on the user's role, obtained via
+        `context.role`.
+
+        Args:
+            context (AuthedServiceContext): The context containing user role information.
+
+        Returns:
+            str: The private or public message based on the role.
+        """
+        if context.role.value >= ServiceRole.DATA_OWNER.value:
+            return self.__private_message
+        return self.public
+
+    def __repr__(self) -> str:
+        return self.public
+
+    def __str__(self) -> str:
+        return self.public
