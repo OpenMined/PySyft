0c08dbc7c6f904ecf5d503ff31d95349cc90b534
Thiago Costa Porto <thiagocostaporto@gmail.com>
2024-06-12 13:22:35 +0200

    lint

--- packages/syft/src/syft/types/errors.py
+++ packages/syft/src/syft/types/errors.py
@@ -1,3 +1,6 @@
+# stdlib
+from typing import Any
+
 # relative
 from ..service.context import AuthedServiceContext
 from ..service.user.user_roles import ServiceRole
@@ -6,7 +9,7 @@ from ..service.user.user_roles import ServiceRole
 class SyftException(Exception):
     """
     A Syft custom exception class with distinct public and private messages.
-    
+
     Attributes:
         private_message (str): Detailed error message intended for administrators.
         public_message (str): General error message for end-users.
@@ -15,8 +18,12 @@ class SyftException(Exception):
     public_message = "An error occurred. Contact the admin for more information."
 
     def __init__(
-        self, private_message: str, public_message: str | None = None, *args, **kwargs
-    ):
+        self,
+        private_message: str,
+        public_message: str | None = None,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
         if public_message:
             self.public_message = public_message
         self._private_message = private_message
--- packages/syft/tests/syft/types/errors_test.py
+++ packages/syft/tests/syft/types/errors_test.py
@@ -1,14 +1,18 @@
-import pytest
+# stdlib
 from unittest.mock import Mock
 
-from syft.service.context import AuthedServiceContext
-from syft.types.errors import SyftException
-from syft.service.user.user_roles import ServiceRole
+# third party
+import pytest
 
+# syft absolute
+from syft.service.context import AuthedServiceContext
+from syft.service.user.user_roles import ServiceRole
+from syft.types.errors import SyftException
 
 default_public_message = SyftException.public_message
 public_message = "An error occurred. Contact the admin for more information."
 private_message = "Private admin error."
+
 
 def test_default_public_message():
     default_public_message = SyftException.public_message
@@ -17,17 +21,20 @@ def test_default_public_message():
     assert exception.public == default_public_message
     assert exception._private_message == private_message
 
+
 def test_custom_public_message():
     exception = SyftException(private_message, public_message=public_message)
 
     assert exception.public == public_message
     assert exception._private_message == private_message
 
+
 def test_public_message_property():
     default_public_message = SyftException.public_message
     exception = SyftException(private_message)
 
     assert exception.public == default_public_message
+
 
 @pytest.mark.parametrize(
     "role,private_msg,public_msg,expected_message",
@@ -36,8 +43,8 @@ def test_public_message_property():
         (ServiceRole.GUEST, private_message, None, default_public_message),
         (ServiceRole.DATA_SCIENTIST, private_message, None, default_public_message),
         (ServiceRole.DATA_OWNER, private_message, None, private_message),
-        (ServiceRole.ADMIN, private_message, None, private_message)
-    ]
+        (ServiceRole.ADMIN, private_message, None, private_message),
+    ],
 )
 def test_get_message(role, private_msg, public_msg, expected_message):
     mock_context = Mock(AuthedServiceContext)
@@ -45,21 +52,24 @@ def test_get_message(role, private_msg, public_msg, expected_message):
     exception = SyftException(private_msg, public_message=public_msg)
     assert exception.get_message(mock_context) == expected_message
 
+
 def test_repr_method():
     default_public_message = SyftException.public_message
     exception = SyftException(private_message)
     assert repr(exception) == default_public_message
 
+
 def test_repr_method_custom_msg():
     exception = SyftException(private_message, public_message=public_message)
     assert repr(exception) == public_message
+
 
 def test_str_method():
     default_public_message = SyftException.public_message
     exception = SyftException(private_message)
     assert str(exception) == default_public_message
 
+
 def test_str_method_custom_msg():
     exception = SyftException(private_message, public_message=public_message)
     assert str(exception) == public_message
-
