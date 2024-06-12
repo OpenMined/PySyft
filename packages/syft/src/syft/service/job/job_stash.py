c9500e31c5f3ee6e59dd163643f3fc4ead826367
Aziz Berkay Yesilyurt <abyesilyurt@gmail.com>
2024-06-12 08:43:34 +0200

    handle error

--- packages/syft/src/syft/service/job/job_stash.py
+++ packages/syft/src/syft/service/job/job_stash.py
@@ -659,8 +659,7 @@ class Job(SyncableSyftObject):
             )
 
         workers = api.services.worker.get_all()
-        worker_pool = api.services.worker_pool.get_all()
-        if len(workers) == 0 and len(worker_pool) == 0:
+        if not isinstance(workers, SyftError) and len(workers) == 0:
             return SyftError(
                 message="This node has no workers. "
                 "You need to start a worker to run jobs "
