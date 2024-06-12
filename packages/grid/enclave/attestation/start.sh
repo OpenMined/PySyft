e91c69e23554969c7b0c4c3210d9c1121ee93118
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-06-12 21:14:00 +0200

    Update start.sh

--- packages/grid/enclave/attestation/start.sh
+++ packages/grid/enclave/attestation/start.sh
@@ -5,13 +5,6 @@ export PATH="/root/.local/bin:${PATH}"
 APP_MODULE=server.attestation_main:app
 APP_LOG_LEVEL=${APP_LOG_LEVEL:-info}
 UVICORN_LOG_LEVEL=${UVICORN_LOG_LEVEL:-info}
-if grep -q ":" /proc/net/if_inet6 ; then
-    # IPv6 is available
-    HOST=${HOST:-[::]}
-else
-    # Only IPv4 is available
-    HOST=${HOST:-0.0.0.0}
-fi
 PORT=${PORT:-4455}
 RELOAD=""
 
@@ -21,5 +14,14 @@ then
     RELOAD="--reload"
 fi
 
+if grep -q ":" /proc/net/if_inet6 ; then
+    # IPv6 is available
+    HOST=${HOST:-[::]}
+    exec uvicorn $RELOAD --host $HOST --port $PORT --log-level $UVICORN_LOG_LEVEL "$APP_MODULE"
+fi
 
-exec uvicorn $RELOAD --host $HOST --port $PORT --log-level $UVICORN_LOG_LEVEL "$APP_MODULE"
+if ! grep -q ":" /proc/net/if_inet6 ; then
+    # IPv4 is available
+    HOST=${HOST:-0.0.0.0}
+    exec uvicorn $RELOAD --host $HOST --port $PORT --log-level $UVICORN_LOG_LEVEL "$APP_MODULE"
+fi
