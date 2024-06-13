707d792bac31b8d8bc1b65acec2f60aec39d622d
Thiago Costa Porto <thiagocostaporto@gmail.com>
2024-06-13 17:33:11 +0200

    feat(syft_exc): new way to drop unwanted frames from traceback

--- packages/syft/src/syft/types/errors.py
+++ packages/syft/src/syft/types/errors.py
@@ -1,5 +1,12 @@
 # stdlib
+from collections.abc import Callable
+from importlib import import_module
+import inspect
+from types import CodeType
+from types import TracebackType
 from typing import Any
+from typing import Self
+from typing import TypeVar
 
 # relative
 from ..service.context import AuthedServiceContext
@@ -16,6 +23,12 @@ class SyftException(Exception):
     """
 
     public_message = "An error occurred. Contact the admin for more information."
+
+    def __new__(cls, *args: Any, **kwargs: Any) -> Self:
+        exc = super().__new__(cls, *args, **kwargs)
+        # removes irrelevant frames from the traceback (e.g. as_result decorator)
+        process_traceback(exc)
+        return exc
 
     def __init__(
         self,
@@ -53,3 +66,102 @@ class SyftException(Exception):
         if context.role.value >= ServiceRole.DATA_OWNER.value:
             return self._private_message
         return self.public
+
+    @classmethod
+    def from_exception(
+        cls, exc: BaseException, public_message: str | None = None
+    ) -> Self:
+        """
+        Builds an instance of SyftException from an existing exception, incorporating
+        the message from the original exception as a private message. It also allows
+        setting a public message for end users.
+
+        Args:
+            exception (BaseException): The original exception from which to create
+                the new instance. The message from this exception will be used as
+                the base message for the new instance.
+
+            public_message (str, optional): An optional message intended for public
+                display. This message can provide user-friendly information about
+                the error. If not provided, the default is None.
+
+        Returns:
+            Self: A new instance of the class. The new instance retains the traceback
+                of the original exception.
+        """
+        new_exc = cls(str(exc), public_message=public_message)
+        new_exc.__traceback__ = exc.__traceback__
+        new_exc = process_traceback(new_exc)
+        return new_exc
+
+
+class ExceptionFilter(tuple):
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    def __new__(cls, path: str) -> Self:
+        module = import_module(path)
+
+        exceptions = tuple(
+            obj
+            for _, obj in inspect.getmembers(module, inspect.isclass)
+            if issubclass(obj, BaseException) and obj.__module__ == module.__name__
+        )
+
+        instance = super().__new__(cls, exceptions)
+
+        instance.path = path
+
+        return instance
+
+
+_excluded_code_objects: set[CodeType] = set()
+
+F = TypeVar("F", bound=Callable[..., object])
+
+
+def exclude_from_traceback(f: F) -> F:
+    """
+    Decorator to mark a function to be removed from the traceback when an
+    exception is raised. This is useful for functions that are not relevant to
+    the error message and would only clutter the traceback.
+    """
+    _excluded_code_objects.add(f.__code__)
+    return f
+
+
+E = TypeVar("E", bound=BaseException)
+
+
+def process_traceback(exc: E) -> E:
+    """
+    Adjusts the traceback of an exception to remove specific frames related to
+    the as_result decorator and the unwrap() call, for cleaner and more relevant
+    error messages.
+
+    Args:
+        exc (E): The exception whose traceback is to be adjusted.
+
+    Returns:
+        E: The same exception with an adjusted traceback.
+    """
+    # We want to adjust the traceback so we can remove frames which contain
+    # a function marked with the _excluded_code_objects decorator, improving
+    # the stacktrace of the error messages.
+    tb = exc.__traceback__
+    frames: list[TracebackType] = []
+
+    while tb is not None:
+        if tb.tb_frame.f_code not in _excluded_code_objects:
+            frames.append(tb)
+        tb = tb.tb_next
+
+    # Before being done, we need to adjust the traceback.tb_next so that
+    # the frames are linked together properly.
+    for i, tb in enumerate(frames):
+        if i + 1 == len(frames):
+            tb.tb_next = None
+        else:
+            tb.tb_next = frames[i + 1]
+
+    return exc
--- packages/syft/src/syft/types/result.py
+++ packages/syft/src/syft/types/result.py
@@ -1,13 +1,17 @@
 # stdlib
 from collections.abc import Callable
 import functools
-from types import TracebackType
 from typing import Generic
 from typing import Literal
 from typing import NoReturn
 from typing import ParamSpec
 from typing import TypeAlias
 from typing import TypeVar
+
+# relative
+from .errors import SyftException
+from .errors import exclude_from_traceback
+from .errors import process_traceback
 
 T = TypeVar("T", covariant=True)
 E = TypeVar("E", covariant=True, bound=BaseException)
@@ -71,6 +75,7 @@ class Err(Generic[E]):
     def is_ok(self) -> Literal[False]:
         return False
 
+    @exclude_from_traceback
     def unwrap(self) -> NoReturn:
         if isinstance(self.value, BaseException):
             raise self.value
@@ -81,74 +86,33 @@ Result: TypeAlias = Ok[T] | Err[E]
 
 
 def as_result(
-    *exceptions: type[BE],
+    *exceptions: type[BE], convert_to_syft_exception: bool = False
 ) -> Callable[[Callable[P, T]], Callable[P, Result[T, BE]]]:
     if not exceptions or not all(
         issubclass(exception, BaseException) for exception in exceptions
     ):
         raise TypeError("The as_result() decorator only accepts exceptions")
 
+    class _AsResultError(Exception): ...
+
     def decorator(func: Callable[P, T]) -> Callable[P, Result[T, BE]]:
+        @exclude_from_traceback
         @functools.wraps(func)
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> Result[T, BE]:
             try:
                 output = func(*args, **kwargs)
                 if isinstance(output, Ok) or isinstance(output, Err):
-                    raise TypeError(
+                    raise _AsResultError(
                         f"Functions decorated with `as_result` should not return Result.\n"
                         f"output: {output}"
                     )
                 return Ok(func(*args, **kwargs))
             except exceptions as exc:
-                strip_asresult_from_exception_traceback(exc)
+                if convert_to_syft_exception and not isinstance(exc, SyftException):
+                    exc = SyftException.from_exception(exc)
+                process_traceback(exc)
                 return Err(exc)
 
         return wrapper
 
     return decorator
-
-
-def strip_asresult_from_exception_traceback(exc: BaseException) -> BaseException:
-    """
-    Adjusts the traceback of an exception to remove specific frames related to
-    the as_result decorator and the unwrap() call, for cleaner and more relevant
-    error messages.
-
-    This function modifies the traceback of the provided exception by keeping the first
-    two frames (which show the immediate context of the error) and then skipping the
-    next two frames (which contain decorator and wrapper frames). The rest of the frames
-    are preserved.
-
-    Args:
-        exc (BaseException): The exception whose traceback is to be adjusted.
-
-    Returns:
-        BaseException: The same exception with an adjusted traceback.
-    """
-    # We want to adjust the traceback so we can remove the decorator
-    # and the wrapper from the traceback for nicer error messages.
-    tb = exc.__traceback__
-    frames: list[TracebackType] = []
-
-    while tb is not None:
-        frames.append(tb)
-        tb = tb.tb_next
-
-    # The first two frames we always want to keep as they show the immediate
-    # context of the error: the current except block and the caller
-    frames_to_keep = frames[0:2]
-
-    # Next, we skip two frames that contains the decorator and the wrapper.
-    # They'll appear when unwrap() is called. We don't need them.
-    if len(frames) > 2:
-        frames_to_keep += frames[4:]
-
-    # Before being done, we need to adjust the traceback.tb_next so that
-    # the frames are linked together properly.
-    for i, tb in enumerate(frames_to_keep):
-        if i + 1 == len(frames_to_keep):
-            tb.tb_next = None
-        else:
-            tb.tb_next = frames_to_keep[i + 1]
-
-    return exc
