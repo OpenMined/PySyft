2110642109a28c1deb3a3dcfb26c91d73309981b
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-06-14 06:35:19 +0200

    Update detect_ip.sh

--- packages/grid/seaweedfs/scripts/detect_ip.sh
+++ packages/grid/seaweedfs/scripts/detect_ip.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# Détecter si IPv4 est disponible
+# Detect if IPv4 is available
 if ip -4 addr show scope global | grep -q inet; then
     IPV4_AVAILABLE=true
 else
     IPV4_AVAILABLE=false
 fi
 
-# Détecter si IPv6 est disponible
+# Detect if IPv6 is available
 if ip -6 addr show scope global | grep -q inet6; then
     IPV6_AVAILABLE=true
 else
     IPV6_AVAILABLE=false
 fi
 
-# Démarrer uvicorn avec les options appropriées
+# Start hypercorn with the appropriate options
 if [ "$IPV4_AVAILABLE" = true ] && [ "$IPV6_AVAILABLE" = true ]; then
-    exec uvicorn --host=0.0.0.0 --host=:: --port=$MOUNT_API_PORT --log-config=logging.yaml src.api:app
+    exec hypercorn --bind 0.0.0.0:$MOUNT_API_PORT --bind [::]:$MOUNT_API_PORT --log-config logging.yaml src.api:app
 elif [ "$IPV4_AVAILABLE" = true ]; then
-    exec uvicorn --host=0.0.0.0 --port=$MOUNT_API_PORT --log-config=logging.yaml src.api:app
+    exec hypercorn --bind 0.0.0.0:$MOUNT_API_PORT --log-config logging.yaml src.api:app
 elif [ "$IPV6_AVAILABLE" = true ]; then
-    exec uvicorn --host=:: --port=$MOUNT_API_PORT --log-config=logging.yaml src.api:app
+    exec hypercorn --bind [::]:$MOUNT_API_PORT --log-config logging.yaml src.api:app
 else
     echo "Neither IPv4 nor IPv6 is available. Exiting."
     exit 1
