f7c76ba4bc3d36487294021cd40f1576c96f6f52
Thiago Costa Porto <thiagocostaporto@gmail.com>
2024-06-17 20:53:30 +0200

    feat(syft_exc): add todo for logging modulenotfound

--- packages/syft/src/syft/types/errors.py
+++ packages/syft/src/syft/types/errors.py
@@ -120,6 +120,7 @@ class ExceptionFilter(tuple):
         try:
             imported_module = import_module(module)
         except ModuleNotFoundError:
+            # TODO: log warning
             exceptions = ()
         else:
             exceptions = (
