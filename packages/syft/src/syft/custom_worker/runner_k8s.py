1117d97c4dd0075517312776e1b162507d3e9909
Julian Cardonnet <jcardonnet@gmail.com>
2024-06-18 11:31:42 -0300

    Add livenessProbe and startupProbe to worker pool containers

--- packages/syft/src/syft/custom_worker/runner_k8s.py
+++ packages/syft/src/syft/custom_worker/runner_k8s.py
@@ -230,6 +230,24 @@ class KubernetesRunner:
                                     "image": tag,
                                     "env": env_vars,
                                     "volumeMounts": volume_mounts,
+                                    "livenessProbe": {
+                                        "httpGet": {
+                                            "path": "/api/v2/?probe=livenessProbe",
+                                            "port": 80,
+                                        },
+                                        "initialDelaySeconds": 30,
+                                        "periodSeconds": 15,
+                                        "timeoutSeconds": 5,
+                                        "failureThreshold": 3,
+                                    },
+                                    "startupProbe": {
+                                        "httpGet": {
+                                            "path": "/api/v2/metadata?probe=startupProbe",
+                                            "port": 80,
+                                        },
+                                        "failureThreshold": 30,
+                                        "periodSeconds": 10,
+                                    },
                                 }
                             ],
                             "volumes": volumes,
