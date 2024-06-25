c30424e9dfca1fb5c895d4c3a48cdbab7c6c1095
Sameer Wagh <snwagh@gmail.com>
2024-06-25 10:28:15 -0400

    Instructions modified

--- scripts/reloadable_run.sh
+++ scripts/reloadable_run.sh
@@ -5,10 +5,20 @@
 # bash scripts/reloadable_run.sh --port 9082 --name "italy-domain"
 # bash scripts/reloadable_run.sh --port 9083 --name "canada-enclave" --node_type "enclave"
 
-# And get the NodeHandlers as follows (note that the handlers point to the correct objects but are buggy in their attributes)
-# canada_domain = sy.orchestra.launch(port=9081, deploy_to="remote")
-# italy_domain = sy.orchestra.launch(port=9082, deploy_to="remote")
-# canada_enclave = sy.orchestra.launch(port=9083, deploy_to="remote")
+# And get the NodeHandlers as follows
+# from syft.orchestra import NodeHandle
+# canada_node = NodeHandle(
+#         node_type="domain",
+#         deployment_type="python",
+#         name="canada-domain",
+#         port=9081,
+#         url="localhost",
+#         node_side_type="high",
+#     )
+
+# Or directly login using
+# do_italy_client = sy.login(email="info@openmined.org", password="changethis",
+#                             url="localhost", port=ITALY_DOMAIN_PORT)
 
 # Default values
 PORT=9694
