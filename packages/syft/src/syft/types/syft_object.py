3ac23c0da53c18c8b4fb7fd32955dc0b6b0d22ce
Aziz Berkay Yesilyurt <abyesilyurt@gmail.com>
2024-06-25 16:00:40 +0200

    cached type hints

--- packages/syft/src/syft/types/syft_object.py
+++ packages/syft/src/syft/types/syft_object.py
@@ -7,6 +7,7 @@ from collections.abc import KeysView
 from collections.abc import Mapping
 from collections.abc import Sequence
 from collections.abc import Set
+from functools import cache
 from hashlib import sha256
 import inspect
 from inspect import Signature
@@ -227,6 +228,11 @@ class SyftObjectRegistry:
             f"No mapping found for: {type_from} to {type_to} in"
             f"the registry: {cls.__object_transform_registry__.keys()}"
         )
+
+
+@cache
+def cached_get_type_hints(cls: type) -> dict[str, Any]:
+    return typing.get_type_hints(cls)
 
 
 class SyftMigrationRegistry:
@@ -578,7 +584,7 @@ class SyftObject(SyftBaseObject, SyftObjectRegistry, SyftMigrationRegistry):
             return
         # Validate and set private attributes
         # https://github.com/pydantic/pydantic/issues/2105
-        annotations = typing.get_type_hints(self.__class__)
+        annotations = cached_get_type_hints(self.__class__)
         for attr, decl in self.__private_attributes__.items():
             value = kwargs.get(attr, decl.get_default())
             var_annotation = annotations.get(attr)
