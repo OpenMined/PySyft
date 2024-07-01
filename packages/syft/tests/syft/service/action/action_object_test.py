3e0b64ab69ec5d80af6486b3d1be3e733dbdec1a
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-07-01 04:50:08 +0200

    Linter

--- packages/syft/tests/syft/service/action/action_object_test.py
+++ packages/syft/tests/syft/service/action/action_object_test.py
@@ -729,7 +729,7 @@ def test_actionobject_syft_getattr_str(worker, scenario):
     assert "C" in obj
     assert "z" not in obj
     assert obj[0] == orig_obj[0]  # noqa
-    assert f"test {obj}" == f"test {orig_obj}" 
+    assert f"test {obj}" == f"test {orig_obj}"
     assert obj > "a"
     assert obj < "zzzz"
     for idx, c in enumerate(obj):
