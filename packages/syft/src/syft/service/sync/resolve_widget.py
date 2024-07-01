83af00939637c9597dcb9a85be4f85c73680cd32
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-07-01 20:03:32 +0200

    revert

--- packages/syft/src/syft/service/sync/resolve_widget.py
+++ packages/syft/src/syft/service/sync/resolve_widget.py
@@ -85,8 +85,7 @@ def create_diff_html(
 
     for attr, val in properties.items():
         status = statuses[attr]
-        if val is None:  # noqa
-            val = ""  # noqa
+        val = val if val is not None else ""
         style = f"background-color: {background_colors[status]}; color: {colors[status]}; display: block; white-space: pre-wrap; margin-bottom: 5px;"  # noqa: E501
         content = html.escape(f"{attr}: {val}")
         html_str += f"<div style='{style}'>{content}</div>"
