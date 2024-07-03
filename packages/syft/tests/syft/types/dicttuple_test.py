c396c1ed6da3baa8ea82bff2c8b15cb36b47c034
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-07-03 22:54:17 +0200

    noqa PD011

--- packages/syft/tests/syft/types/dicttuple_test.py
+++ packages/syft/tests/syft/types/dicttuple_test.py
@@ -197,7 +197,7 @@ class TestDictTupleProperties:
 
     def test_int_indexing(self, dict_tuple: DictTuple, case: Case) -> None:
         for i in range(len(dict_tuple)):
-            assert dict_tuple[i] == case.values[i]
+            assert dict_tuple[i] == case.values[i]  # noqa PD011
 
     def test_key_indexing(self, dict_tuple: DictTuple, case: Case) -> None:
         for k in case.keys:
