4f5479857ddf24f9b2881bcdc6799728f8e615a3
Thiago Costa Porto <thiagocostaporto@gmail.com>
2024-07-04 13:50:26 +0200

    refactor(stash): LogStash and SettingsStash

--- packages/syft/src/syft/service/settings/settings_stash.py
+++ packages/syft/src/syft/service/settings/settings_stash.py
@@ -6,7 +6,7 @@ from result import Result
 # relative
 from ...node.credentials import SyftVerifyKey
 from ...serde.serializable import serializable
-from ...store.document_store import BaseUIDStoreStash
+from ...store.document_store import BaseUIDStoreStash, NewBaseUIDStoreStash
 from ...store.document_store import DocumentStore
 from ...store.document_store import PartitionKey
 from ...store.document_store import PartitionSettings
@@ -21,7 +21,7 @@ ActionIDsPartitionKey = PartitionKey(key="action_ids", type_=list[UID])
 
 @instrument
 @serializable()
-class SettingsStash(BaseUIDStoreStash):
+class SettingsStash(NewBaseUIDStoreStash):
     object_type = NodeSettings
     settings: PartitionSettings = PartitionSettings(
         name=NodeSettings.__canonical_name__, object_type=NodeSettings
@@ -30,28 +30,11 @@ class SettingsStash(BaseUIDStoreStash):
     def __init__(self, store: DocumentStore) -> None:
         super().__init__(store=store)
 
-    def set(
-        self,
-        credentials: SyftVerifyKey,
-        settings: NodeSettings,
-        add_permission: list[ActionObjectPermission] | None = None,
-        add_storage_permission: bool = True,
-        ignore_duplicates: bool = False,
-    ) -> Result[NodeSettings, str]:
-        res = self.check_type(settings, self.object_type)
-        # we dont use and_then logic here as it is hard because of the order of the arguments
-        if res.is_err():
-            return res
-        return super().set(credentials=credentials, obj=res.ok())
-
     def update(
         self,
         credentials: SyftVerifyKey,
         settings: NodeSettings,
         has_permission: bool = False,
-    ) -> Result[NodeSettings, str]:
-        res = self.check_type(settings, self.object_type)
-        # we dont use and_then logic here as it is hard because of the order of the arguments
-        if res.is_err():
-            return res
-        return super().update(credentials=credentials, obj=res.ok())
+    ) -> NodeSettings:
+        obj = self.check_type(settings, self.object_type).unwrap()
+        return super().update(credentials=credentials, obj=obj).unwrap()
