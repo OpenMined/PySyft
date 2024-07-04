8f9b7120e33f3969771967009afa5f1488861988
Thiago Costa Porto <thiagocostaporto@gmail.com>
2024-07-04 13:54:10 +0200

    refactor(services): association_request apply and _run

--- packages/syft/src/syft/service/network/association_request.py
+++ packages/syft/src/syft/service/network/association_request.py
@@ -4,12 +4,13 @@ from typing import cast
 
 # third party
 from result import Err
-from result import Ok
 from result import Result
 
 # relative
 from ...client.client import SyftClient
 from ...serde.serializable import serializable
+from ...types.errors import SyftException
+from ...types.result import as_result
 from ...types.syft_object import SYFT_OBJECT_VERSION_1
 from ..context import ChangeContext
 from ..request.request import Change
@@ -30,6 +31,7 @@ class AssociationRequestChange(Change):
 
     __repr_attrs__ = ["self_node_route", "remote_peer"]
 
+    @as_result(SyftException)
     def _run(
         self, context: ChangeContext, apply: bool
     ) -> Result[tuple[bytes, NodePeer], SyftError]:
@@ -48,8 +50,8 @@ class AssociationRequestChange(Change):
 
         if not apply:
             # TODO: implement undo for AssociationRequestChange
-            return Err(
-                SyftError(message="Undo not supported for AssociationRequestChange")
+            raise SyftException(
+                public_message="Undo not supported for AssociationRequestChange"
             )
 
         # Get the network service
@@ -75,13 +77,13 @@ class AssociationRequestChange(Change):
         else:
             # Pinging the remote peer to verify the connection
             try:
+                # FIX: unwrap client_with_context?
                 remote_client: SyftClient = self.remote_peer.client_with_context(
                     context=service_ctx
                 )
                 if remote_client.is_err():
-                    return SyftError(
-                        message=f"Failed to create remote client for peer: "
-                        f"{self.remote_peer.id}. Error: {remote_client.err()}"
+                    raise SyftException(
+                        public_message=f"Failed to create remote client for peer: {self.remote_peer.id}. Error: {remote_client.err()}"
                     )
                 remote_client = remote_client.ok()
                 random_challenge = secrets.token_bytes(16)
@@ -89,7 +91,9 @@ class AssociationRequestChange(Change):
                     challenge=random_challenge
                 )
             except Exception as e:
-                return SyftError(message="Remote Peer cannot ping peer:" + str(e))
+                raise SyftException(
+                    public_message="Remote Peer cannot ping peer:" + str(e)
+                )
 
             if isinstance(remote_res, SyftError):
                 return Err(remote_res)
@@ -102,7 +106,7 @@ class AssociationRequestChange(Change):
                     random_challenge, challenge_signature
                 )
             except Exception as e:
-                return Err(SyftError(message=str(e)))
+                raise SyftException(public_message=str(e))
 
         # Adding the remote peer to the network stash
         result = network_stash.create_or_update_peer(
@@ -110,25 +114,24 @@ class AssociationRequestChange(Change):
         )
 
         if result.is_err():
-            return Err(SyftError(message=str(result.err())))
+            raise SyftException(public_message=str(result.err()))
 
         # this way they can match up who we are with who they think we are
         # Sending a signed messages for the peer to verify
         self_node_peer = self.self_node_route.validate_with_context(context=service_ctx)
 
         if isinstance(self_node_peer, SyftError):
-            return Err(self_node_peer)
+            raise SyftException(public_message=self_node_peer)
 
-        return Ok(
-            SyftSuccess(
-                message=f"Routes successfully added for peer: {self.remote_peer.name}"
-            )
+        return SyftSuccess(
+            message=f"Routes successfully added for peer: {self.remote_peer.name}"
         )
 
-    def apply(self, context: ChangeContext) -> Result[SyftSuccess, SyftError]:
+    # TODO: Check if calls are expecting SyftError as return type
+    def apply(self, context: ChangeContext) -> SyftSuccess:
         return self._run(context, apply=True)
 
-    def undo(self, context: ChangeContext) -> Result[SyftSuccess, SyftError]:
+    def undo(self, context: ChangeContext) -> SyftSuccess:
         return self._run(context, apply=False)
 
     def __repr_syft_nested__(self) -> str:
