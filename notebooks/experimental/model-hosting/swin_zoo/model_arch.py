4584aabe3f47d39895854766141bb78ad452e01f
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2024-07-05 13:57:54 +0530

    added notebooks for training a simple model

---
+++ notebooks/experimental/model-hosting/swin_zoo/model_arch.py
@@ -0,0 +1,12 @@
+# third party
+import torch.nn as nn
+
+
+# Define a simple linear model
+class SimpleModel(nn.Module):
+    def __init__(self):
+        super(SimpleModel, self).__init__()
+        self.linear = nn.Linear(1, 1)
+
+    def forward(self, x):
+        return self.linear(x)
