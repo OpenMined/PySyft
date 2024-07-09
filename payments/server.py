a243d8250a87982ba909f56b15265fd6e2178d7e
Peter McHale <peter.thomas.mchale@gmail.com>
2024-07-09 13:07:26 -0600

    make tab into four spaces

--- payments/server.py
+++ payments/server.py
@@ -1,21 +1,21 @@
 import syft as sy
 
 def main():
-  sy.requires(">=0.8.6,<0.8.7")
+    sy.requires(">=0.8.6,<0.8.7")
 
-  node = sy.orchestra.launch(
-      name="test-domain-1",
-      port=8080,
-      dev_mode=True,
-      reset=True
-  )
+    node = sy.orchestra.launch(
+        name="test-domain-1",
+        port=8080,
+        dev_mode=True,
+        reset=True
+    )
 
-  print(node)
+    print(node)
 
-  node.shutdown()
+    node.shutdown()
 
 # Protect the entry point of the program
 # This ensures that the multiprocessing code is not run
 # when the module is imported as a part of the import of another module.
 if __name__ == '__main__':
-  main()
+    main()
