203f156c05becf25b72033bbc2a0c6bd032eac5c
Shubham Gupta <shubhamgupta3121@gmail.com>
2024-07-24 11:28:53 +0530

    add a migration method to add notification_enable field with some default val

--- packages/syft/src/syft/service/settings/settings.py
+++ packages/syft/src/syft/service/settings/settings.py
@@ -15,6 +15,7 @@ from ...types.syft_object import SYFT_OBJECT_VERSION_1
 from ...types.syft_object import SYFT_OBJECT_VERSION_2
 from ...types.syft_object import SyftObject
 from ...types.transforms import drop
+from ...types.transforms import make_set_default
 from ...types.uid import UID
 from ...util import options
 from ...util.colors import SURFACE
@@ -174,7 +175,7 @@ class ServerSettings(SyftObject):
 
 @migrate(ServerSettingsV1, ServerSettings)
 def migrate_server_settings_v1_to_v2() -> list[Callable]:
-    return []
+    return [make_set_default("notifications_enabled", False)]
 
 
 @migrate(ServerSettings, ServerSettingsV1)
