cc44acd277323ef0fce1ec016d905841fab748b9
eelcovdw <eelcovdw@gmail.com>
2024-08-01 11:36:47 +0200

    add action table

--- packages/syft/src/syft/service/job/job_sql.py
+++ packages/syft/src/syft/service/job/job_sql.py
@@ -10,15 +10,19 @@ from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import declared_attr
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
+from sqlalchemy.types import JSON
 
 # syft absolute
 import syft as sy
+from syft.service.action.action_object import Action, ActionType
 
 # relative
-from ...types.uid import UID
+from ...types.uid import UID, LineageID
 from .job_stash import Job
 from .job_stash import JobStatus
 from .job_stash import JobType
+
+from typing_extensions import Self
 
 
 class Base(DeclarativeBase):
@@ -75,6 +79,12 @@ def wrap_uid(uid: uuid.UUID | None) -> UID | None:
     return UID(value=uid)
 
 
+def wrap_lineage_id(uid: uuid.UUID | None) -> LineageID | None:
+    if uid is None:
+        return None
+    return LineageID(value=uid)
+
+
 class JobDB(CommonMixin, Base):
     id: Mapped[uuid.UUID] = mapped_column(sa.Uuid, primary_key=True, default=uuid.uuid4)
     server_uid: Mapped[uuid.UUID | None] = mapped_column(default=None)
@@ -84,7 +94,10 @@ class JobDB(CommonMixin, Base):
     status: Mapped[JobStatus] = mapped_column(default=JobStatus.CREATED)
     n_iters: Mapped[int | None] = mapped_column(default=None)
     current_iter: Mapped[int | None] = mapped_column(default=None)
-    action: Mapped[bytes | None] = mapped_column(default=None)
+    action_id: Mapped[uuid.UUID | None] = mapped_column(
+        ForeignKey("actiondb.id"), default=None
+    )
+    action: Mapped["ActionDB | None"] = relationship("ActionDB", lazy="joined")
 
     parent_id: Mapped[uuid.UUID | None] = mapped_column(
         ForeignKey("jobdb.id"), default=None
@@ -116,7 +129,7 @@ class JobDB(CommonMixin, Base):
             status=obj.status,
             n_iters=obj.n_iters,
             current_iter=obj.current_iter,
-            action=sy.serialize(obj.action, to_bytes=True),
+            action=ActionDB.from_obj(obj.action) if obj.action is not None else None,
             parent_id=unwrap_uid(obj.parent_job_id),
             job_pid=obj.job_pid,
             job_worker_id=unwrap_uid(obj.job_worker_id),
@@ -136,7 +149,7 @@ class JobDB(CommonMixin, Base):
             status=self.status,
             n_iters=self.n_iters,
             current_iter=self.current_iter,
-            action=sy.deserialize(self.action, from_bytes=True),
+            action=self.action.to_obj() if self.action is not None else None,
             parent_job_id=wrap_uid(self.parent_id),
             job_pid=self.job_pid,
             job_worker_id=wrap_uid(self.job_worker_id),
@@ -144,4 +157,55 @@ class JobDB(CommonMixin, Base):
             user_code_id=wrap_uid(self.user_code_id),
             requested_by=wrap_uid(self.requested_by),
             job_type=self.job_type,
+        )
+
+
+# Needed Action fields
+# path: str | None = None
+# op: str | None = None
+# remote_self: LineageID | None = None
+# args: list[LineageID]
+# kwargs: dict[str, LineageID]
+# result_id: LineageID = Field(default_factory=lambda: LineageID(UID()))
+# action_type: ActionType | None = None
+# create_object: SyftObject | None = None
+# user_code_id: UID | None = None
+
+
+class ActionDB(CommonMixin, Base):
+    id: Mapped[uuid.UUID] = mapped_column(sa.Uuid, primary_key=True, default=uuid.uuid4)
+    path: Mapped[str | None] = mapped_column(default=None)
+    op: Mapped[str | None] = mapped_column(default=None)
+    remote_self: Mapped[uuid.UUID | None] = mapped_column(default=None)
+    args: Mapped[list[str]] = mapped_column(JSON, default=[])
+    kwargs: Mapped[dict[str, str]] = mapped_column(JSON, default={})
+    result_id: Mapped[uuid.UUID] = mapped_column()
+    action_type: Mapped[ActionType | None] = mapped_column(default=None)
+    user_code_id: Mapped[uuid.UUID | None] = mapped_column(default=None)
+
+    @classmethod
+    def from_obj(cls, obj: Action) -> Self:
+        return cls(
+            id=unwrap_uid(obj.id),
+            path=obj.path,
+            op=obj.op,
+            remote_self=unwrap_uid(obj.remote_self),
+            args=[arg.value.hex for arg in obj.args],
+            kwargs={k: v.value.hex for k, v in obj.kwargs.items()},
+            result_id=unwrap_uid(obj.result_id),
+            action_type=obj.action_type,
+            user_code_id=unwrap_uid(obj.user_code_id),
+        )
+
+    def to_obj(self) -> Action:
+        return Action(
+            id=wrap_uid(self.id),
+            path=self.path,
+            op=self.op,
+            remote_self=wrap_lineage_id(self.remote_self),
+            args=[wrap_lineage_id(arg) for arg in self.args],
+            kwargs={k: wrap_lineage_id(v) for k, v in self.kwargs.items()},
+            result_id=wrap_lineage_id(self.result_id),
+            action_type=self.action_type,
+            user_code_id=wrap_uid(self.user_code_id),
         )
