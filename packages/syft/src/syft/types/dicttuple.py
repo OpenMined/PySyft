9391c0f82db0b06fbe2406ffdd31ca7207c2bf2d
dk <dkn.work@protonmail.com>
2024-08-16 15:19:55 +0700

    [syft/dicttuple] return `[]` as repr for empty DictTuple

--- packages/syft/src/syft/types/dicttuple.py
+++ packages/syft/src/syft/types/dicttuple.py
@@ -236,14 +236,15 @@ class DictTuple(tuple[_VT, ...], Generic[_KT, _VT], metaclass=_Meta):
         return super().__getitem__(self.__mapping[__key])
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__qualname__}{super().__repr__()}"
+        # return f"{self.__class__.__qualname__}{super().__repr__()}"
+        return "[]"
 
     def _repr_html_(self) -> str:
         super_repr_html = getattr(super(), "_repr_html_", None)
         if super_repr_html is None:
-            return ""
-        if super_repr_html() == self.__repr__():
-            return ""
+            return super().__repr__()
+        if super_repr_html() == self.__repr__():  # empty DictTuple
+            return "[]"
         return super_repr_html()
 
     def keys(self) -> KeysView[_KT]:
