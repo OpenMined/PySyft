fdffdf050293c04000a95c31b736282e344d6ffe
teo <teo.milea97@gmail.com>
2024-08-28 15:32:17 +0300

    rename test

--- tests/integration/local/policy_test.py
+++ tests/integration/local/policy_test.py
@@ -1,4 +1,5 @@
 # stdlib
+from secrets import token_hex
 import uuid
 
 # third party
@@ -19,37 +20,33 @@ from syft.service.user.user import User
 from syft.service.user.user_roles import ServiceRole
 from syft.types.errors import SyftException
 
-@sy.api_endpoint_method()
-def private_query_function(
-    context,
-    query_str: str
-) -> str:
-    return query_str
 
 @sy.api_endpoint_method()
-def mock_query_function(
-    context,
-    query_str: str
-) -> str:
+def private_query_function(context, query_str: str) -> str:
     return query_str
 
-@sy.api_endpoint(
-    path='test.submit_query',
-    endpoint_timeout=5
-)
+
+@sy.api_endpoint_method()
+def mock_query_function(context, query_str: str) -> str:
+    return query_str
+
+
+@sy.api_endpoint(path="test.submit_query", endpoint_timeout=5)
 def submit_query(
     context,
-    func_name : str,
-    query:str,
+    func_name: str,
+    query: str,
 ) -> str:
+    # syft absolute
     import syft as sy
+
     @sy.syft_function(
         name=func_name,
         input_policy=sy.MixedInputPolicy(
             endpoint=sy.Constant(val=context.admin_client.api.services.test.test_query),
             query=sy.Constant(val=query),
             client=context.admin_client,
-        )
+        ),
     )
     def execute_query(query: str, endpoint):
         res = endpoint(query_str=query)
@@ -65,27 +62,35 @@ def submit_query(
     )
 
 
-def test_mixed_policy(worker: Worker, ds_client: DatasiteClient,) -> None:
-    root_client = worker.root_client
+@pytest.mark.local_server
+def test_compound_api() -> None:
+    server = sy.orchestra.launch(
+        name=token_hex(8),
+        dev_mode=True,
+        reset=True,
+        local_db=True,
+        create_producer=True,
+        n_consumers=1,
+    )
+    root_client = server.login(email="info@openmined.org", password="changethis")
     new_endpoint = sy.TwinAPIEndpoint(
         path="test.test_query",
         description="Test",
         private_function=private_query_function,
         mock_function=mock_query_function,
     )
-    
+
     res = root_client.custom_api.add(endpoint=new_endpoint)
     print(res)
 
-
     res = root_client.custom_api.add(endpoint=submit_query)
     print(res)
-    res = root_client.users.create(email="newuser@openmined.org", name="John Doe", password="pw")
+    res = root_client.users.create(
+        email="newuser@openmined.org", name="John Doe", password="pw"
+    )
     ds_client = root_client.login(email="newuser@openmined.org", password="pw")
-    try:
-        ds_client.api.services.test.submit_query(func_name="func_test", query="test query")
-    except:
-        print(ds_client.jobs.get_all())
+    ds_client.api.services.test.submit_query(func_name="func_test", query="test query")
     root_client.requests[0].approve()
-    print(ds_client.code.func_test())
-    assert False
+    assert ds_client.code.func_test().get() == "test query"
+    server.python_server.cleanup()
+    server.land()
