3efb2505576a23bd42794442095fb5c9290b2278
Brendan Schell <brendanschell1@gmail.com>
2024-09-18 10:38:47 -0400

    add security skips on worker_helpers Co-authored-by: Sameer Wagh <snwagh@gmail.com>

--- packages/syft/src/syft/util/test_helpers/worker_helpers.py
+++ packages/syft/src/syft/util/test_helpers/worker_helpers.py
@@ -14,17 +14,17 @@ def build_and_launch_worker_pool_from_docker_str(
     scale_to: int,
 ):
     result = client.api.services.image_registry.add(external_registry)
-    assert "success" in result.message
+    assert "success" in result.message  # nosec: B101
 
     # For some reason, when using k9s, result.value is empty so can't use the below line
     # local_registry = result.value
     local_registry = client.api.services.image_registry[0]
 
     docker_config = sy.DockerWorkerConfig(dockerfile=worker_dockerfile)
-    assert docker_config.dockerfile == worker_dockerfile
+    assert docker_config.dockerfile == worker_dockerfile  # nosec: B101
     submit_result = client.api.services.worker_image.submit(worker_config=docker_config)
     print(submit_result.message)
-    assert "success" in submit_result.message
+    assert "success" in submit_result.message  # nosec: B101
 
     worker_image = submit_result.value
 
@@ -66,10 +66,10 @@ def launch_worker_pool_from_docker_tag_and_registry(
     scale_to: int = 1,
 ):
     res = client.api.services.image_registry.add(external_registry)
-    assert "success" in res.message
+    assert "success" in res.message  # nosec: B101
     docker_config = sy.PrebuiltWorkerConfig(tag=docker_tag)
     image_result = client.api.services.worker_image.submit(worker_config=docker_config)
-    assert "success" in res.message
+    assert "success" in res.message  # nosec: B101
     worker_image = image_result.value
 
     launch_result = client.api.services.worker_pool.launch(
