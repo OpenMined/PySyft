ba216dc51ed4536c657f198836fd3ac4e437a69f
Brendan Schell <brendanschell1@gmail.com>
2024-09-18 15:15:20 -0400

    revert host back to 0.0.0.0 - fix this later
    Co-authored-by: Sameer Wagh <snwagh@gmail.com>

--- packages/syft/src/syft/util/test_helpers/email_helpers.py
+++ packages/syft/src/syft/util/test_helpers/email_helpers.py
@@ -220,7 +220,7 @@ def user_exists(root_client, email: str) -> bool:
 class SMTPTestServer:
     def __init__(self, email_server):
         self.port = 9025
-        self.hostname = "127.0.0.1"
+        self.hostname = "0.0.0.0"
         self._stop_event = asyncio.Event()
 
         # Simple email handler class
